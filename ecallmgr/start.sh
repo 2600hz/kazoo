d6805914a07fdcf23fcce76f115428050e4c425a
root <root@fs1.voicebus.net>
2010-09-09 02:02:13 +0400

    start in background

---
+++ ecallmgr/start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+cd `dirname $0`
+exec erl -detached -heart -setcookie ClueCon -pa $PWD/ebin -pa $PWD/deps/*/ebin \
+    -boot start_sasl -name ecallmgr -s ecallmgr
+#    -kernel error_logger '{file, "log/error_log"}' \
