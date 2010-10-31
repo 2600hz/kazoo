7db70118e943f6a0b9a1a83ddc5ae50320d08058
James Aimonetti <james@sfvoip.com>
2010-10-31 15:54:24 -0700

    format the dialstring

--- applications/trunkstore/start-dev.sh
+++ applications/trunkstore/start-dev.sh
@@ -3,6 +3,6 @@
 
 cd `dirname $0`
 exec erl -setcookie ClueCon -pa $PWD/ebin -pa $PWD/deps/*/ebin \
-    -boot start_sasl -name ts -s trunkstore
+    -boot start_sasl -sname ts -s trunkstore
 #    -kernel error_logger '{file, "log/error_log"}' \
 
