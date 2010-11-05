a76ed4541e8d16f9515e718e65187eb69f177e32
James Aimonetti <james@sfvoip.com>
2010-11-04 17:21:32 -0700

    fixed a couple bugs, like bad format and type conversion

--- amqp/src/amqp_manager.erl
+++ amqp/src/amqp_manager.erl
@@ -300,9 +300,8 @@ create_amqp_params(Host, Port) ->
 
 -spec(get_new_connection/1 :: (P :: tuple()) -> tuple(pid(), reference())).
 get_new_connection(#'amqp_params'{}=P) ->
-    Localhost = net_adm:localhost(),
     {ok, Connection} = amqp_connection:start(network, P),
-    format_log(info, "AMQP_MGR(~p): Conn ~p started.~n", [Connection]),
+    format_log(info, "AMQP_MGR(~p): Conn ~p started.~n", [self(), Connection]),
     MRefConn = erlang:monitor(process, Connection),
     {Connection, MRefConn}.
 
