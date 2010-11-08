f9947e55416e7752b34b96b88b537b701ea1d6fe
James Aimonetti <james@sfvoip.com>
2010-11-08 14:45:16 -0800

    cdr using amqp as a proper app

---
+++ applications/cdr/start.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+cd `dirname $0`
+exec erl -detached -heart -setcookie `cat ../../fs_conf/autoload_configs/.erlang.cookie` -pa $PWD/ebin \
+    -boot start_sasl -name cdr -s cdr \
+    -kernel error_logger '{file, "log/error_log"}'
