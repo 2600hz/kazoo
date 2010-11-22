a435d5e8c06665402e73957a858cd1b452cf9d05
James Aimonetti <james@sfvoip.com>
2010-11-22 15:08:51 -0800

    beams

--- diagnostics/start.sh
+++ diagnostics/start.sh
@@ -3,6 +3,6 @@
 
 cd `dirname $0`
 exec erl -setcookie `cat ../fs_conf/autoload_configs/.erlang.cookie` -pa $PWD/ebin -pa $PWD/deps/*/ebin \
-    -boot start_sasl -name diagnostics -s diagnostics
+    -boot start_sasl -name diagnostics@work-lappy.voipinc.com -s diagnostics
 #    -kernel error_logger '{file, "log/error_log"}' \
 
