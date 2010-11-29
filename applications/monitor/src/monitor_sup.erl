91e76ed6547eb145268f9ecfb7a0be8276b9bb12
K Anderson <karl@2600hz.com>
2010-11-29 18:28:29 -0500

    started creation of sup

--- applications/monitor/src/monitor_sup.erl
+++ applications/monitor/src/monitor_sup.erl
@@ -31,5 +31,4 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
-
+    {ok, { {one_for_one, 5, 10}, [?CHILD(monitor_master, worker), ?CHILD(monitor_agent_sup, supervisor), ?CHILD(monitor_job_sup, supervisor)]} }.
