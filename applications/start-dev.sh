63ea775b2486ec873e654c83c96ee77d5f0831d4
whistle@erl001 <whistle@whistle-erl001-fmt.2600hz.org>
2010-12-10 21:56:56 +0000

    updated configs and ignore

--- applications/start-dev.sh
+++ applications/start-dev.sh
@@ -6,9 +6,10 @@ cd `dirname $0`
 sname="whistle_apps"
 [ ! -z "$1" ] && sname="$1"
 
+
 exec erl -setcookie `cat ../confs/fs_conf/autoload_configs/.erlang.cookie` \
     -pa $PWD/ebin -pa $PWD/deps/*/ebin -pa $PWD/apps/*/ebin \
-    -sasl errlog_type error \
+#    -sasl errlog_type error \
     -boot start_sasl -sname $sname -s whistle_apps
 #    -kernel error_logger '{file, "log/error_log"}' \
 
