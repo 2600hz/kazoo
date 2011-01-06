662eac28358cd07cd3f6f853315903385aefaebb
James Aimonetti <james@sfvoip.com>
2011-01-05 18:27:57 -0800

    update start scripts to add lib to code path

--- ecallmgr/start.sh
+++ ecallmgr/start.sh
@@ -2,7 +2,8 @@
 
 
 cd `dirname $0`
-exec erl -detached -heart -setcookie `cat ../confs/fs_conf/autoload_configs/.erlang.cookie` -pa $PWD/ebin -pa $PWD/deps/*/ebin \
+exec erl -detached -heart -setcookie `cat ../confs/fs_conf/autoload_configs/.erlang.cookie` \
+    -pa $PWD/ebin -pa $PWD/deps/*/ebin $PWD/lib/*/ebin \
     -kernel error_logger '{file, "log/error_log"}' \
     -boot start_sasl -name ecallmgr@`hostname -f` -s ecallmgr
 
