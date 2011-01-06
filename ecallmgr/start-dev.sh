25ac826b4d0d04285d0470e2f607c43ed59c1c22
James Aimonetti <james@sfvoip.com>
2011-01-06 09:49:02 -0800

    updated start scripts

--- ecallmgr/start-dev.sh
+++ ecallmgr/start-dev.sh
@@ -2,7 +2,10 @@
 
 
 cd `dirname $0`
-exec erl -setcookie `cat ../confs/fs_conf/autoload_configs/.erlang.cookie` -pa $PWD/ebin -pa $PWD/deps/*/ebin \
+export ERL_LIBS=$PWD/../lib/
+
+exec erl -setcookie `cat ../confs/fs_conf/autoload_configs/.erlang.cookie` \
+    -pa $PWD/ebin -pa $PWD/deps/*/ebin \
     -boot start_sasl -name ecallmgr@`hostname -f` -s ecallmgr
 #    -kernel error_logger '{file, "log/error_log"}' \
 
--- ecallmgr/start.sh
+++ ecallmgr/start.sh
@@ -2,7 +2,9 @@
 
 
 cd `dirname $0`
-exec erl -detached -heart -setcookie `cat ../confs/fs_conf/autoload_configs/.erlang.cookie` -pa $PWD/ebin -pa $PWD/deps/*/ebin \
-    -kernel error_logger '{file, "log/error_log"}' \
-    -boot start_sasl -name ecallmgr@`hostname -f` -s ecallmgr
+export ERL_LIBS=$PWD/../lib/
 
+exec erl -detached -heart -setcookie `cat ../confs/fs_conf/autoload_configs/.erlang.cookie` \
+    -pa $PWD/ebin -pa $PWD/deps/*/ebin \
+    -boot start_sasl -name ecallmgr@`hostname -f` -s ecallmgr \
+    -kernel error_logger '{file, "log/error_log"}'
