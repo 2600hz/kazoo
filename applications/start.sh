985205bbaadbe21037eb4157f31d9aa21ee61bbd
James Aimonetti <james@sfvoip.com>
2011-01-06 12:10:17 -0800

    updated start scripts

--- applications/start.sh
+++ applications/start.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-
 cd `dirname $0`
+export ERL_LIBS=$PWD/../lib/
+
 exec erl -detached -heart -setcookie `cat ../confs/fs_conf/autoload_configs/.erlang.cookie` \
     -pa $PWD/ebin -pa $PWD/deps/*/ebin -pa $PWD/apps/*/ebin \
     -boot start_sasl -name whistle_apps -s whistle_apps \
