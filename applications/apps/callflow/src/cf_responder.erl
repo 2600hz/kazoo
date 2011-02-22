bb4ea04b444f36caa1ecc9bf89bfa9a60766eb54
vova <vova@2600hz.com>
2011-02-22 13:05:48 -0800

    callflow 0.0.2

--- applications/apps/callflow/src/cf_responder.erl
+++ applications/apps/callflow/src/cf_responder.erl
@@ -6,11 +6,12 @@
 %%% inbound calls with valid callflows
 %%%
 %%% @end
-%%% Created :  3 Feb 2011 by Vladimir Darmin <vova@2600hz.org>
+%%% Created :      3  Feb 2011 by Vladimir Darmin <vova@2600hz.org>
+%%% Last Modified: 17 Feb 2011 by Vladimir Darmin <vova@2600hz.org>
 %%%============================================================================
 %%%
 
--module ( cf_route_handler ).
+-module ( cf_responder ).
 
 -behaviour ( gen_server ).
 
@@ -30,7 +31,12 @@
 -define ( APP_NAME, <<"callflow">> ).
 -define ( APP_VERSION, <<"0.2">> ).
 
--record ( state, {amqp_host = "" :: string(), callmgr_q = <<>> :: binary()} ).
+-record ( state, {
+   pid = <<>> :: binary(),
+   amqp_host = "" :: string(),
+   callmgr_q = <<>> :: binary(),
+   flows = [] :: proplist()
+} ).
 
 
 %%-----------------------------------------------------------------------------
@@ -85,10 +91,29 @@ init ( [] ) -> { ok, #state{}, 0 }.
    | tuple(stop, Reason :: term(), State :: term())
    | tuple(stop, Reason :: term(), Reply :: term(), State :: term())
 ).
+handle_call ( {add_flow, CallId, Flow}, _, #state{flows=Flows}=State ) ->
+   format_log(
+      info,
+      "CF RESPONDER (~p): Adding callflow for call: ~p~n~p~n",
+      [self(), CallId, Flow]
+   ),
+   spawn(
+      receive
+      after 1000 -> fun() -> gen_server:call(State#state.pid, {remove_flow, CallId}) end
+      end
+   ),
+   { reply, ok, State#state{flows=Flows++[{CallId, Flow}]} };
+handle_call ( {remove_flow, CallId}, _, #state{flows=Flows}=State ) ->
+   format_log(
+      info,
+      "CF RESPONDER (~p): Removing callflow for call: ~p~n",
+      [self(), CallId]
+   ),
+   { reply, ok, State#state{flows=[E || {K, _} = E <- Flows, K =/= CallId]} };
 handle_call ( Request, From, State ) ->
    format_log(
       error,
-      "CF ROUTE HANDLER (~p): Unhandled call message:~nRequest: ~p~nFrom: ~p~n",
+      "CF RESPONDER (~p): Unhandled call message:~nRequest: ~p~nFrom: ~p~n",
       [self(), Request, From]
    ),
    { reply, ok, State }
@@ -109,7 +134,7 @@ handle_call ( Request, From, State ) ->
 handle_cast ( Msg, State ) ->
    format_log(
       error,
-      "CF ROUTE HANDLER (~p): Unhandled cast message:~nMessage: ~p~n",
+      "CF RESPONDER (~p): Unhandled cast message:~nMessage: ~p~n",
       [self(), Msg]
    ),
    { noreply, State }
@@ -128,25 +153,37 @@ handle_cast ( Msg, State ) ->
    | tuple(stop, Reason :: term(), State :: term())
 ).
 handle_info ( timeout, State ) ->
-   format_log(info, "CF ROUTE HANDLER (~p): timeout...~n", [self()]),
+   format_log(info, "CF RESPONDER (~p): timeout...~n", [self()]),
    AHost = whapps_controller:get_amqp_host(),
    {ok, CQ} = start_amqp(AHost),
-   { noreply, State#state{amqp_host=AHost, callmgr_q=CQ} };
+   { noreply, State#state{amqp_host=AHost, callmgr_q=CQ, pid=self()} };
 handle_info ( #'basic.consume_ok'{}, State ) -> 
-   format_log(info, "CF ROUTE HANDLER (~p): basic consume ok...~n", [self()]),
+   format_log(info, "CF RESPONDER (~p): basic consume ok...~n", [self()]),
    { noreply, State };
 handle_info ( {_, #amqp_msg{props=Proplist, payload=Payload}}, #state{}=State ) ->
-   format_log(
-      info,
-      "CF ROUTE HANDLER (~p): handling route request...~nProplist: ~p~nPayload:~p~n",
-      [self(), Proplist, Payload]
-   ),
-   spawn(fun() -> handle_req(Proplist#'P_basic'.content_type, Payload, State) end),
+   {struct, Prop} = mochijson2:decode(binary_to_list(Payload)),
+   Event = proplists:get_value(<<"Event-Name">>, Prop),
+   case Event of
+      <<"route_win">> ->
+         format_log(
+            info,
+            "CF RESPONDER (~p): spawning callflow execution process...~n",
+            [self()]
+         )
+         spawn(fun() -> {cf_exe:start()} end);
+      <<"route_req">> ->
+         format_log(
+            info,
+            "CF RESPONDER (~p): handling route request...~nProplist: ~p~nPayload:~p~n",
+            [self(), Proplist, Payload]
+         ),
+         spawn(fun() -> handle_req(Proplist#'P_basic'.content_type, Payload, State) end)
+   end,
    { noreply, State };
 handle_info ( Info, State ) ->
    format_log(
       error,
-      "CF ROUTE HANDLER (~p): Unhandled info message:~nInfo: ~p~n",
+      "CF RESPONDER (~p): Unhandled info message:~nInfo: ~p~n",
       [self(), Info]
    ),
    { noreply, State }
@@ -201,7 +238,7 @@ code_change ( _OldVsn, State, _Extra ) -> { ok, State }.
 start_amqp ( AHost ) ->
    format_log(
       info,
-      "CF ROUTE HANDLER (~p): Starting AMPQ: ~p~n",
+      "CF RESPONDER (~p): Starting AMPQ: ~p~n",
       [self(), AHost]
    ),
 
@@ -210,16 +247,14 @@ start_amqp ( AHost ) ->
    amqp_util:callevt_exchange(AHost),
    CallmgrQueue = amqp_util:new_callmgr_queue(AHost, <<>>),
 
-   % Bind the queue to an exchange
    amqp_util:bind_q_to_callmgr(AHost, CallmgrQueue, ?KEY_ROUTE_REQ),
-   %amqp_util:bind_q_to_targeted(AHost, CallmgrQueue, CallmgrQueue),
+   amqp_util:bind_q_to_targeted(AHost, CallmgrQueue, CallmgrQueue),
 
-   % Register a consumer to listen to the queue
    amqp_util:basic_consume(AHost, CallmgrQueue),
 
    format_log(
       info,
-      "CF ROUTE HANDLER (~p): Consuming on call manager queue: ~p~n",
+      "CF RESPONDER (~p): Consuming on call manager queue: ~p~n",
       [self(), CallmgrQueue]
    ),
    { ok, CallmgrQueue }
@@ -244,7 +279,7 @@ handle_req ( <<"application/json">>, Payload, State ) ->
    {struct, JSON} = mochijson2:decode(binary_to_list(Payload)),
    format_log(
       info,
-      "CF ROUTE HANDLER (~p): Received JSON: ~p~n",
+      "CF RESPONDER (~p): Received JSON: ~p~n",
       [self(), JSON]
    ),
    process_req(
@@ -256,10 +291,10 @@ handle_req ( <<"application/json">>, Payload, State ) ->
       JSON,
       State
    );
-handle_req ( ContentType, _Payload, _State ) ->
+handle_req ( ContentType, _Payload, _ ) ->
    format_log(
       error,
-      "CF ROUTE HANDLER (~p): Unknown content type: ~p~n",
+      "CF RESPONDER (~p): Unknown content type: ~p~n",
       [self(), ContentType]
    )
 .
@@ -272,26 +307,26 @@ handle_req ( ContentType, _Payload, _State ) ->
 % @end
 %------------------------------------------------------------------------------
 -spec ( process_req/3 :: (Msg_type :: tuple(binary(), binary(), To :: string()), Proplist :: proplist(), State :: #state{}) -> no_return() ).
-process_req ( {<<"dialplan">>, <<"route_req">>, To}, Proplist, State ) ->
+process_req ( {<<"dialplan">>, <<"route_req">>, To}, Proplist, #state{}=State ) ->
    format_log(
       progress,
-      "CF ROUTE HANDLER (~p): Processing route request to: ~p~n",
+      "CF RESPONDER (~p): Processing route request to: ~p~n",
       [self(), To]
    ),
    case validate(To) of
       { success, Flow } ->
-         format_log(info, "CF ROUTE HANDLER (~p): Required callflow exists! Responding...~n", [self()]),
+         format_log(info, "CF RESPONDER (~p): Required callflow exists! Responding...~n", [self()]),
          RespQ = proplists:get_value(<<"Server-ID">>, Proplist),
          respond(RespQ, State, Proplist, Flow);
       { error, Msg }    ->
-         format_log(error, "CF ROUTE HANDLER (~p): ~p~n", [self(), Msg]);
+         format_log(error, "CF RESPONDER (~p): ~p~n", [self(), Msg]);
       Unknown           ->
-         format_log(error, "CF ROUTE HANDLER (~p): Unknown validation response: ~p~n", [self(), Unknown])
+         format_log(error, "CF RESPONDER (~p): Unknown validation response: ~p~n", [self(), Unknown])
    end;
-process_req ( Msg_type, Proplist, _State ) ->
+process_req ( Msg_type, Proplist, _ ) ->
    format_log(
       error,
-      "CF ROUTE HANDLER (~p): Unknown route request:~nType: ~p~nProplist: ~p~n",
+      "CF RESPONDER (~p): Unknown route request:~nType: ~p~nProplist: ~p~n",
       [self(), Msg_type, Proplist]
    )
 .
@@ -303,15 +338,17 @@ process_req ( Msg_type, Proplist, _State ) ->
 %
 % @end
 %------------------------------------------------------------------------------
--spec ( respond/4 :: (RespQ :: binary(), State :: tuple(), ReqProp :: proplist(), Flow :: proplist()) -> none() ).
-respond ( RespQ, #state{amqp_host=AHost}, ReqProp, Flow ) ->
-   {ok, Pid} = cf_call_sup:start_proc([whapps_controller:get_amqp_host(), ReqProp, Flow]),
+-spec ( respond/4 :: (RespQ :: binary(), State :: proplist(), ReqProp :: proplist(), Flow :: proplist()) -> none() ).
+respond ( RespQ, #state{amqp_host=AHost}=State, ReqProp, Flow ) ->
+   CallId = proplists:get_value(<<"Call-ID">>, ReqProp),
+   spawn(fun() -> gen_server:call(State#state.pid, {add_flow, CallId, Flow}) end),
+
    Prop = [
       {<<"Test">>, <<"Testing was done successfully">>},
       {<<"Msg-ID">>, proplists:get_value(<<"Msg-ID">>, ReqProp)},
       {<<"Routes">>, []},
       {<<"Method">>, <<"park">>}
-      | whistle_api:default_headers(cf_call_handler:get_q(Pid), <<"dialplan">>, <<"route_resp">>, ?APP_NAME, ?APP_VERSION)
+      | whistle_api:default_headers(State#state.callmgr_q, <<"dialplan">>, <<"route_resp">>, ?APP_NAME, ?APP_VERSION)
    ],
    {ok, JSON} = whistle_api:route_resp(Prop),
    amqp_util:targeted_publish(AHost, RespQ, JSON, <<"application/json">>)
