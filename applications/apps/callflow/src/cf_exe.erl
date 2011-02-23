556299c75263780ffe7574279be52043090ea949
vova <vova@2600hz.com>
2011-02-22 16:08:17 -0800

    callflow execution process and callflow hrl record

--- applications/apps/callflow/src/cf_exe.erl
+++ applications/apps/callflow/src/cf_exe.erl
@@ -11,8 +11,6 @@
 
 -module ( cf_exe ).
 
--behaviour ( gen_server ).
-
 %% API
 -export ( [start/2] ).
 
@@ -23,7 +21,27 @@
 %-include ( "../include/amqp_client/include/amqp_client.hrl" ).
 
 start ( Call, Flow ) ->
-   
+   Module = proplists:get_value(<<"module">>, Flow),
+   {struct, Data} = proplists:get_value(<<"data">>, Flow),
+   %calling handling module here...
+   %cf_Module:handle(Module, Data, Call#cf_call{pid=self()}),
+   receive
+      continue        -> self() ! { continue, 0 };
+      { continue, N } ->
+         {struct, NewFlow} = case proplists:get_value(<<"children">>, Flow) of
+            []       -> { struct, [] };
+            Children -> lists:nth(N, Children)
+         end,
+         case NewFlow of
+            [] ->
+               spawn(fun() -> start (Call, [{<<"module">>, <<"hangup">>}, {<<"data">>, {struct, []}}]) end),
+               exit("end of the flow, hanging up");
+            _  ->
+               spawn(fun() -> start (Call, NewFlow) end),
+               exit("moving on")
+         end;
+      stop            -> exit("Requested stop")
+   end
 .
 
 %%%
