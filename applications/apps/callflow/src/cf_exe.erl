64dce796f59a36da75d4812178b12c6ee08e18c9
vova <vova@2600hz.com>
2011-02-22 17:16:28 -0800

    working callflow

--- applications/apps/callflow/src/cf_exe.erl
+++ applications/apps/callflow/src/cf_exe.erl
@@ -2,10 +2,11 @@
 %%% @author Vladimir Darmin <vova@2600hz.org>
 %%% @copyright (C) 2011, Vladimir Darmin
 %%% @doc
-%%% Callflow call handler, waits for winning routes to spawn callflow processes
+%%% Callflow executioner, executes the head node and waits for further
+%%% instructions
 %%%
 %%% @end
-%%% Created :  3 Feb 2011 by Vladimir Darmin <vova@2600hz.org>
+%%% Created :  21 Feb 2011 by Vladimir Darmin <vova@2600hz.org>
 %%%============================================================================
 %%%
 
@@ -17,17 +18,18 @@
 -import ( logger, [format_log/3] ).
 
 -include ( "callflow.hrl" ).
-%-include ( "../../../utils/src/whistle_amqp.hrl" ).
-%-include ( "../include/amqp_client/include/amqp_client.hrl" ).
 
 start ( Call, Flow ) ->
+   format_log(info, "TEST: ~p~n", [Flow]),
    Module = proplists:get_value(<<"module">>, Flow),
    {struct, Data} = proplists:get_value(<<"data">>, Flow),
+   format_log(info, "CF EXECUTIONER (~p): Executing ~p...~n", [self(), Module]),
    %calling handling module here...
    %cf_Module:handle(Module, Data, Call#cf_call{pid=self()}),
    receive
       continue        -> self() ! { continue, 0 };
       { continue, N } ->
+         format_log(info, "CF EXECUTIONER (~p): Advancing to the next node...~n", [self()]),
          {struct, NewFlow} = case proplists:get_value(<<"children">>, Flow) of
             []       -> { struct, [] };
             Children -> lists:nth(N, Children)
@@ -35,12 +37,15 @@ start ( Call, Flow ) ->
          case NewFlow of
             [] ->
                spawn(fun() -> start (Call, [{<<"module">>, <<"hangup">>}, {<<"data">>, {struct, []}}]) end),
+               format_log(info, "CF EXECUTIONER (~p): Child node doesn't exist, hanging up...~n", [self()]),
                exit("end of the flow, hanging up");
             _  ->
                spawn(fun() -> start (Call, NewFlow) end),
                exit("moving on")
          end;
-      stop            -> exit("Requested stop")
+      stop            ->
+         format_log(info, "CF EXECUTIONER (~p): Callflow execution has been stopped~n", [self()]),
+         exit("Requested stop")
    end
 .
 
