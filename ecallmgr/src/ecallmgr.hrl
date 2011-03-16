6ebe41e0d4a5eb4293e738e2052b49fa80489526
K Anderson <karl@2600hz.com>
2011-03-15 23:02:33 -0400

    clean up following lib includes

--- ecallmgr/src/ecallmgr.hrl
+++ ecallmgr/src/ecallmgr.hrl
@@ -1,5 +1,5 @@
--include("../include/amqp_client/include/amqp_client.hrl").
--include("whistle_types.hrl").
+-include_lib("rabbitmq_erlang_client/include/amqp_client.hrl").
+-include_lib("whistle/include/whistle_types.hrl").
 
 -record(handler_stats, {lookups_success = 0 :: integer()
 			,lookups_failed = 0 :: integer()
