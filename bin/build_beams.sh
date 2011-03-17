29587572619277df73dd7012255b7b29cda9c075
K Anderson <karl@2600hz.com>
2011-03-16 21:47:40 -0400

    compile the whistle libs too

--- bin/build_beams.sh
+++ bin/build_beams.sh
@@ -1,11 +1,18 @@
 #!/bin/sh
-WDIR="$PWD/`dirname $0`/../"
+WDIR="$PWD/`dirname $0`/.."
 
 export ERL_LIBS=$WDIR/lib/
 
 echo "Compiling utils"
 cd $WDIR/utils/diagnostics
 rebar clean compile
+
+echo "Compiling Whistle LIBS"
+for WLIB in $WDIR/lib/whistle*
+do 
+    cd $WLIB
+    rebar clean compile 
+done
 
 echo "Compiling ecallmgr"
 cd $WDIR/ecallmgr
