3dd2758506d1f50545672ec72e36b0b1079742e8
K Anderson <karl@2600hz.com>
2011-03-18 20:06:16 -0400

    minor tweaks to the start up scripts

--- ecallmgr/start.sh
+++ ecallmgr/start.sh
@@ -3,10 +3,16 @@
 cd `dirname $0`
 export ERL_LIBS=$PWD/../lib
 
+if [ -n $1 ]; then
+    SHELL_NAME="$1"
+else
+    SHELL_NAME="ecallmgr@`hostname`"
+fi
+
 exec erl -detached -heart -setcookie `cat ../confs/fs_conf/autoload_configs/.erlang.cookie` \
     -pa $PWD/ebin -pa $PWD/deps/*/ebin \
     -riak_err term_max_size 8192 fmt_max_bytes 9000 \
     -sasl errlog_type error \
     -sasl sasl_error_logger '{file, "log/error_log.sasl"}' \
     -kernel error_logger '{file, "log/error_log"}' \
-    -boot start_sasl -name ecallmgr@`hostname -f` -s reloader -s ecallmgr
+    -boot start_sasl -name $SHELL_NAME -s reloader -s ecallmgr
