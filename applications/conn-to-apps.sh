bb51250c772c59b569de99a722b68d4ce335d0ad
K Anderson <karl@2600hz.com>
2011-03-18 20:21:51 -0400

    accept the remote shell and this shell, also support multiple shells

--- applications/conn-to-apps.sh
+++ applications/conn-to-apps.sh
@@ -1,4 +1,17 @@
 #!/bin/sh
 
 cd `dirname $0`
-exec erl -setcookie `cat ../confs/fs_conf/autoload_configs/.erlang.cookie` -name apps_conn -remsh whistle_apps@`hostname`
+
+if [ -n "$1" ]; then
+    REMOTE_SHELL="$1"
+else
+    REMOTE_SHELL="whistle_apps@`hostname`"
+fi
+
+if [ -n "$2" ]; then
+    SHELL_NAME="$2"
+else
+    SHELL_NAME="app_con_$(date +%s)@`hostname`"
+fi
+
+exec erl -setcookie `cat ../confs/fs_conf/autoload_configs/.erlang.cookie` -name ${SHELL_NAME} -remsh ${REMOTE_SHELL}
