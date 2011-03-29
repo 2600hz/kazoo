fedfcb49fef69b2666ecaa5928bbae9afd3b3a00
Karl Anderson <karl@2600hz.com>
2011-03-29 17:43:43 -0400

    fresh

---
+++ applications/apps/crossbar/src/modules/conferences.erl
@@ -0,0 +1,346 @@
+%%%-------------------------------------------------------------------
+%%% @author Karl Anderson <karl@2600hz.org>
+%%% @copyright (C) 2011, Karl Anderson
+%%% @doc
+%%% Conferences module
+%%%
+%%% Handle client requests for conference documents
+%%%
+%%% @end
+%%% Created : 05 Jan 2011 by Karl Anderson <karl@2600hz.org>
+%%%-------------------------------------------------------------------
+-module(conferences).
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/0]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+	 terminate/2, code_change/3]).
+
+-import(logger, [format_log/3]).
+
+-include("../../include/crossbar.hrl").
+
+-define(SERVER, ?MODULE).
+
+-define(VIEW_FILE, <<"views/conferences.json">>).
+
+-define(CONFERENCES_LIST, {"conferences", "listing_by_id"}).
+
+-record(state, {}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+-spec(init/1 :: (_) -> tuple(ok, #state{})).
+init([]) ->
+    bind_to_crossbar(),
+    accounts:update_all_accounts(?VIEW_FILE),
+    {ok, #state{}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info({binding_fired, Pid, <<"v1_resource.allowed_methods.conferences">>, Payload}, State) ->
+    spawn(fun() ->
+		  {Result, Payload1} = allowed_methods(Payload),
+                  Pid ! {binding_result, Result, Payload1}
+	  end),
+    {noreply, State};
+
+handle_info({binding_fired, Pid, <<"v1_resource.resource_exists.conferences">>, Payload}, State) ->
+    spawn(fun() ->
+		  {Result, Payload1} = resource_exists(Payload),
+                  Pid ! {binding_result, Result, Payload1}
+	  end),
+    {noreply, State};
+
+handle_info({binding_fired, Pid, <<"v1_resource.validate.conferences">>, [RD, Context | Params]}, State) ->
+    spawn(fun() ->
+                crossbar_util:binding_heartbeat(Pid),
+                Context1 = validate(Params, Context),
+                Pid ! {binding_result, true, [RD, Context1, Params]}
+	 end),
+    {noreply, State};
+
+handle_info({binding_fired, Pid, <<"v1_resource.execute.post.conferences">>, [RD, Context | Params]}, State) ->
+    spawn(fun() ->
+                  Context1 = crossbar_doc:save(Context),
+                  Pid ! {binding_result, true, [RD, Context1, Params]}
+	  end),
+    {noreply, State};
+
+handle_info({binding_fired, Pid, <<"v1_resource.execute.put.conferences">>, [RD, Context | Params]}, State) ->
+    spawn(fun() ->
+                  Context1 = crossbar_doc:save(Context),
+                  Pid ! {binding_result, true, [RD, Context1, Params]}
+	  end),
+    {noreply, State};
+
+handle_info({binding_fired, Pid, <<"v1_resource.execute.delete.conferences">>, [RD, Context | Params]}, State) ->
+    spawn(fun() ->
+                  Context1 = crossbar_doc:delete(Context),
+                  Pid ! {binding_result, true, [RD, Context1, Params]}
+	  end),
+    {noreply, State};
+
+handle_info({binding_fired, Pid, <<"account.created">>, _Payload}, State) ->    
+    Pid ! {binding_result, true, ?VIEW_FILE},
+    {noreply, State};
+
+handle_info({binding_fired, Pid, _Route, Payload}, State) ->
+    Pid ! {binding_result, true, Payload},
+    {noreply, State};
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function binds this server to the crossbar bindings server,
+%% for the keys we need to consume.
+%% @end
+%%--------------------------------------------------------------------
+-spec(bind_to_crossbar/0 :: () ->  ok | tuple(error, exists)).
+bind_to_crossbar() ->
+    _ = crossbar_bindings:bind(<<"v1_resource.allowed_methods.conferences">>),
+    _ = crossbar_bindings:bind(<<"v1_resource.resource_exists.conferences">>),
+    _ = crossbar_bindings:bind(<<"v1_resource.validate.conferences">>),
+    _ = crossbar_bindings:bind(<<"v1_resource.execute.#.conferences">>),
+    _ = crossbar_bindings:bind(<<"account.created">>).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function determines the verbs that are appropriate for the
+%% given Nouns.  IE: '/accounts/' can only accept GET and PUT
+%%
+%% Failure here returns 405
+%% @end
+%%--------------------------------------------------------------------
+-spec(allowed_methods/1 :: (Paths :: list()) -> tuple(boolean(), http_methods())).
+allowed_methods([]) ->
+    {true, ['GET', 'PUT']};
+allowed_methods([_]) ->
+    {true, ['GET', 'POST', 'DELETE']};
+allowed_methods(_) ->
+    {false, []}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function determines if the provided list of Nouns are valid.
+%%
+%% Failure here returns 404
+%% @end
+%%--------------------------------------------------------------------
+-spec(resource_exists/1 :: (Paths :: list()) -> tuple(boolean(), [])).
+resource_exists([]) ->
+    {true, []};
+resource_exists([_]) ->
+    {true, []};
+resource_exists(_) ->
+    {false, []}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function determines if the parameters and content are correct
+%% for this request
+%%
+%% Failure here returns 400
+%% @end
+%%--------------------------------------------------------------------
+-spec(validate/2 :: (Params :: list(), Context :: #cb_context{}) -> #cb_context{}).
+validate([], #cb_context{req_verb = <<"get">>}=Context) ->
+    load_conference_summary([], Context);
+validate([], #cb_context{req_verb = <<"put">>}=Context) ->
+    create_conference(Context);
+validate([DocId], #cb_context{req_verb = <<"get">>}=Context) ->
+    load_conference(DocId, Context);
+validate([DocId], #cb_context{req_verb = <<"post">>}=Context) ->
+    update_conference(DocId, Context);
+validate([DocId], #cb_context{req_verb = <<"delete">>}=Context) ->
+    load_conference(DocId, Context);
+validate(_, Context) ->
+    crossbar_util:response_faulty_request(Context).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Attempt to load list of accounts, each summarized.  Or a specific
+%% account summary.
+%% @end
+%%--------------------------------------------------------------------
+-spec(load_conference_summary/2 :: (DocId :: binary() | [], Context :: #cb_context{}) -> #cb_context{}).
+load_conference_summary([], Context) ->
+    crossbar_doc:load_view(?CONFERENCES_LIST, [], Context, fun normalize_view_results/2);
+load_conference_summary(DocId, Context) ->
+    crossbar_doc:load_view(?CONFERENCES_LIST, [
+         {<<"startkey">>, [DocId]}
+        ,{<<"endkey">>, [DocId, {struct, []}]}
+    ], Context, fun normalize_view_results/2).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Create a new conference document with the data provided, if it is valid
+%% @end
+%%--------------------------------------------------------------------
+-spec(create_conference/1 :: (Context :: #cb_context{}) -> #cb_context{}).
+create_conference(#cb_context{req_data=JObj}=Context) ->
+    case is_valid_doc(JObj) of
+        {false, Fields} ->
+            crossbar_util:response_invalid_data(Fields, Context);
+        {true, []} ->
+            Context#cb_context{
+                 doc=whapps_json:set_value(<<"pvt_type">>, <<"conference">>, JObj)
+                ,resp_status=success
+            }
+    end.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Load a conference document from the database
+%% @end
+%%--------------------------------------------------------------------
+-spec(load_conference/2 :: (DocId :: binary(), Context :: #cb_context{}) -> #cb_context{}).
+load_conference(DocId, Context) ->
+    crossbar_doc:load(DocId, Context).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Update an existing conference document with the data provided, if it is
+%% valid
+%% @end
+%%--------------------------------------------------------------------
+-spec(update_conference/2 :: (DocId :: binary(), Context :: #cb_context{}) -> #cb_context{}).
+update_conference(DocId, #cb_context{req_data=JObj}=Context) ->
+    case is_valid_doc(JObj) of
+        {false, Fields} ->
+            crossbar_util:response_invalid_data(Fields, Context);
+        {true, []} ->
+            crossbar_doc:load_merge(DocId, JObj, Context)
+    end.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Normalizes the resuts of a view
+%% @end
+%%--------------------------------------------------------------------
+-spec(normalize_view_results/2 :: (JObj :: json_object(), Acc :: json_objects()) -> json_objects()).
+normalize_view_results(JObj, Acc) ->
+    [whapps_json:get_value(<<"value">>, JObj)|Acc].    
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec(is_valid_doc/1 :: (Data :: json_object()) -> tuple(boolean(), json_objects())).
+is_valid_doc({struct, Data}) ->
+    Schema = [
+               {["base", "name"], [{not_empty, []}]}
+             ],
+    Failed = crossbar_validator:validate(Schema, Data),
+    {Failed =:= [], Failed}.
