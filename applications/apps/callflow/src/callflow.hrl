f9c2503a3519a481f0950b5c8dcb7aae4300c275
Karl Anderson <karl@2600hz.com>
2011-04-01 15:44:15 -0400

    fix spec, remove amqp host, and change domain to realm

--- applications/apps/callflow/src/callflow.hrl
+++ applications/apps/callflow/src/callflow.hrl
@@ -2,8 +2,6 @@
 -include_lib("whistle/include/whistle_amqp.hrl").
 -include_lib("rabbitmq_erlang_client/include/amqp_client.hrl").
 
--define ( CALLFLOW_DB, "callflow" ).
-
 -define ( DIALPLAN_MAP, [
    { <<"tone">>, <<"tones">> }
 ] ).
@@ -11,15 +9,14 @@
 -type cf_exe_response() :: tuple(stop) | tuple (continue) | tuple(continue, integer()) | tuple(heartbeat).
 
 -record (cf_call, {
-             amqp_h = <<>> :: binary()                            %% The AMPQ host the ctrl_q and bdst_q exist in
-            ,amqp_q = <<>> :: binary()                            %% The AMPQ queue that we consume on
+             amqp_q = <<>> :: binary()                            %% The AMPQ queue that we consume on
             ,ctrl_q = <<>> :: binary()                            %% The control queue for this request
             ,bdst_q = <<>> :: binary()                            %% The broadcast queue the request was recieved on
             ,call_id = <<>> :: binary()                           %% The call-id of this request
             ,cf_pid = undefined :: pid() | undefined              %% PID of the callflow tree processor, who we should pass control back to
             ,from_number = <<>>
-            ,from_domain = <<>>
+            ,from_realm = <<>>
             ,to_number = <<>>
-            ,to_domain = <<>>
-            ,route_request = undefined :: proplist() | undefined  %% The initial route request that spawned this call flow
+            ,to_realm = <<>>
+            ,route_request = undefined :: json_object() | undefined  %% The initial route request that spawned this call flow
            }).
