54f38f725e66427b8df0f4cdc75c5ed2857b7182
James Aimonetti <james@sfvoip.com>
2011-04-08 15:54:33 -0700

    add fs_req to api

--- lib/whistle-1.0.0/src/whistle_api.hrl
+++ lib/whistle-1.0.0/src/whistle_api.hrl
@@ -691,6 +691,18 @@
                          ]).
 -define(NOOP_REQ_TYPES, [{<<"Msg-ID">>, fun is_binary/1}]).
 
+
+%% The AMQP passthrough of FS commands
+-define(FS_COMMAND_WHITELIST, [<<"set">>, <<"hangup">>, <<"bridge">>]).
+
+-define(FS_REQ_HEADERS, [<<"Application-Name">>, <<"Args">>]).
+-define(OPTIONAL_FS_REQ_HEADERS, [<<"Insert-At">>]).
+-define(FS_REQ_VALUES, [{<<"Event-Category">>, <<"fs">>}
+                               ,{<<"Event-Name">>, <<"command">>}
+                              ]).
+-define(FS_REQ_TYPES, [{<<"Application-Name">>, fun(<<"uuid_", _/binary>>) -> true; (App) -> lists:member(App, ?FS_COMMAND_WHITELIST) end}]).
+
+
 %% [{FreeSWITCH-Flage-Name, Whistle-Flag-Name}]
 %% Conference-related entry flags
 %% convert from FS conference flags to Whistle conference flags
