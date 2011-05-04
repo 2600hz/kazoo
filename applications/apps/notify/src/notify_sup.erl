43b9ebed6c6ef4f1c38f631f62508467bb86d469
James Aimonetti <james@sfvoip.com>
2011-05-04 11:57:06 -0700

    beginnings of the notify whapp

---
+++ applications/apps/notify/src/notify_sup.erl
@@ -0,0 +1,38 @@
+%%%-------------------------------------------------------------------
+%%% @author James Aimonetti <james@2600hz.org>
+%%% @copyright (C) 2011, VoIP INC
+%%% @doc
+%%%
+%%% @end
+%%% Created :  3 May 2011 by James Aimonetti <james@2600hz.org>
+%%%-------------------------------------------------------------------
+-module(notify_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+init([]) ->
+    {ok, { {one_for_one, 5, 10}, [
+				  ?CHILD(notify_vm, worker)
+				 ]}
+    }.
+
