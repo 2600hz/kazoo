8fb7451299a8bd9e76faeeee4e18fd96fc03e533
k anderson <karl@2600hz.com>
2011-05-06 01:02:36 +0000

    use dest to route outbound

--- applications/apps/callflow/src/module/cf_resources.erl
+++ applications/apps/callflow/src/module/cf_resources.erl
@@ -34,8 +34,8 @@ handle(_, #cf_call{cf_pid=CFPid}=Call) ->
 %% @end
 %%--------------------------------------------------------------------
 -spec(bridge_to_gateways/2 :: (Resources :: proplist(), Call :: #cf_call{}) -> no_return()).
-bridge_to_gateways([{To, Gateways, CIDType}|T], Call) ->
-    case b_bridge([create_endpoint(To, Gtw) || Gtw <- Gateways], <<"60">>, CIDType, Call) of
+bridge_to_gateways([{DestNum, Gateways, CIDType}|T], Call) ->
+    case b_bridge([create_endpoint(DestNum, Gtw) || Gtw <- Gateways], <<"60">>, CIDType, Call) of
         {ok, _} ->
             wait_for_unbridge();
         {error, _} ->
@@ -47,11 +47,11 @@ bridge_to_gateways([{To, Gateways, CIDType}|T], Call) ->
 %% @doc
 %% @end
 %%--------------------------------------------------------------------
--spec(create_endpoint/2 :: (To :: binary(), Gateway :: json_object()) -> json_object()).
-create_endpoint(To, JObj) ->
+-spec(create_endpoint/2 :: (DestNum :: binary(), Gateway :: json_object()) -> json_object()).
+create_endpoint(DestNum, JObj) ->
     Rule = <<"sip:"
               ,(wh_json:get_value(<<"prefix">>, JObj, <<>>))/binary
-              ,To/binary
+              ,DestNum/binary
               ,(wh_json:get_value(<<"suffix">>, JObj, <<>>))/binary
               ,$@ ,(wh_json:get_value(<<"server">>, JObj))/binary>>,
     Endpoint = [
@@ -71,14 +71,14 @@ create_endpoint(To, JObj) ->
 %% @end
 %%--------------------------------------------------------------------
 -spec(find_gateways/1 :: (Call :: #cf_call{}) -> tuple(ok, proplist()) | tuple(error, atom())).
-find_gateways(#cf_call{account_db=Db, to_number=To}) ->
+find_gateways(#cf_call{account_db=Db, dest_number=DestNum}) ->
     case couch_mgr:get_results(Db, ?VIEW_BY_RULES, []) of
         {ok, Resources} ->
             {ok, [ {Number
                     ,wh_json:get_value([<<"value">>, <<"gateways">>], Resource, [])
                     ,wh_json:get_value([<<"value">>, <<"caller_id_options">>, <<"type">>], Resource, <<"external">>)}
                    || Resource <- Resources
-			 , Number <- evaluate_rules(wh_json:get_value(<<"key">>, Resource), To)
+			 , Number <- evaluate_rules(wh_json:get_value(<<"key">>, Resource), DestNum)
 			 , Number =/= []
                  ]};
         {error, _}=E ->
@@ -90,12 +90,12 @@ find_gateways(#cf_call{account_db=Db, to_number=To}) ->
 %% @doc
 %% @end
 %%--------------------------------------------------------------------
--spec(evaluate_rules/2 :: (Key :: list(), To :: binary()) -> list()).
-evaluate_rules([_, Regex], To) ->
-    case re:run(To, Regex) of
+-spec(evaluate_rules/2 :: (Key :: list(), DestNum:: binary()) -> list()).
+evaluate_rules([_, Regex], DestNum) ->
+    case re:run(DestNum, Regex) of
         {match, [_, {Start,End}|_]} ->
-            [binary:part(To, Start, End)];
+            [binary:part(DestNum, Start, End)];
         {match, _} ->
-            [To];
+            [DestNum];
         _ -> []
     end.
