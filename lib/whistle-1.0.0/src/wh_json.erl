80a696441e1f972e5e49341d6197323e46e4675f
James Aimonetti <james@sfvoip.com>
2011-05-08 16:41:28 -0700

    add a delete_key to wh_json; fix a unit test or three

--- lib/whistle-1.0.0/src/wh_json.erl
+++ lib/whistle-1.0.0/src/wh_json.erl
@@ -10,6 +10,10 @@
 
 -export([get_value/2, get_value/3]).
 -export([set_value/3]).
+-export([delete_key/2]).
+
+%% not for public use
+-export([prune/2, no_prune/2]).
 
 -include_lib("whistle/include/whistle_types.hrl").
 
@@ -18,6 +22,12 @@ get_value(Key, Doc) ->
     get_value(Key, Doc, undefined).
 
 -spec(get_value/3 :: (Key :: term(), Doc :: json_object() | json_objects(), Default :: term()) -> term()).
+get_value([Key|Ks], [{struct, _}|_]=L, Default) ->
+    try
+	get_value1(Ks, lists:nth(Key, L), Default)
+    catch
+	_:_ -> Default
+    end;
 get_value(Key, L, Default) when is_list(L) ->
     get_value1(Key, {struct, L}, Default);
 get_value(K, Doc, Default) ->
@@ -97,6 +107,87 @@ set_value1([Key|T], Value, {struct, Props}) ->
 %% There are no more keys to iterate through! Override the value here...
 set_value1([], Value, _Doc) -> Value.
 
+%% delete_key(foo, {struct, [{foo, bar}, {baz, biz}]}) -> {struct, [{baz, biz}]}
+%% delete_key([foo, far], {struct, [{foo, {struct, [{far, away}]}}, {baz, biz}]}) -> {struct, [{foo, {struct, []}}, {baz, biz}]}
+
+-spec(delete_key/2 :: (Key :: list() | binary(), JObj :: json_object() | json_objects()) -> json_object() | json_objects()).
+delete_key(Key, JObj) when not is_list(Key) ->
+    delete_key([Key], JObj, no_prune);
+delete_key(Keys, JObj) ->
+    delete_key(Keys, JObj, no_prune).
+
+-spec(delete_key/3 :: (Keys :: list() | binary(), JObj :: json_object() | json_objects(), PruneOpt :: prune | no_prune) -> json_object() | json_objects()).
+delete_key(Key, JObj, PruneOpt) when not is_list(Key) ->
+    apply(?MODULE, PruneOpt, [[Key], JObj]);
+delete_key(Keys, JObj, PruneOpt) ->
+    apply(?MODULE, PruneOpt, [Keys, JObj]).
+
+prune([], JObj) ->
+    JObj;
+prune([K], {struct, Doc}) ->
+    case lists:keydelete(K, 1, Doc) of
+	[] -> ?EMPTY_JSON_OBJECT;
+	L -> {struct, L}
+    end;
+prune([K|T], {struct, Doc}=JObj) ->
+    case props:get_value(K, Doc) of
+	undefined -> JObj;
+	V ->
+	    case prune(T, V) of
+		?EMPTY_JSON_OBJECT ->
+		    {struct, lists:keydelete(K, 1, Doc)};
+		[] ->
+		    {struct, lists:keydelete(K, 1, Doc)};
+		V1 ->
+		    {struct, [{K, V1} | lists:keydelete(K, 1, Doc)]}
+	    end
+    end;
+prune(_, []) -> [];
+prune([K|T], [_|_]=JObjs) -> 
+    V = lists:nth(K, JObjs),
+    case prune(T, V) of
+	?EMPTY_JSON_OBJECT ->
+	    replace_in_list(K, undefined, JObjs, []);
+	V ->
+	    replace_in_list(K, undefined, JObjs, []);
+	V1 ->
+	    replace_in_list(K, V1, JObjs, [])
+    end.
+
+no_prune([], JObj) ->
+    JObj;
+no_prune([K], {struct, Doc}) ->
+    case lists:keydelete(K, 1, Doc) of
+	[] -> ?EMPTY_JSON_OBJECT;
+	L -> {struct, L}
+    end;
+no_prune([K|T], {struct, Doc}=JObj) ->
+    case props:get_value(K, Doc) of
+	undefined -> JObj;
+	%% {struct, _}=V ->
+	%%     {struct, lists:keydelete(K, 1, no_prune(T, V))};
+	V ->
+	    {struct, [{K, no_prune(T, V)} | lists:keydelete(K, 1, Doc)]}
+    end;
+no_prune(_, []) -> [];
+no_prune([K|T], [_|_]=JObjs) when is_integer(K) -> 
+    V = lists:nth(K, JObjs),
+    V1 = no_prune(T, V),
+    case V1 =:= V of
+	true ->
+	    replace_in_list(K, undefined, JObjs, []);
+	false ->
+	    replace_in_list(K, V1, JObjs, [])
+    end.
+
+replace_in_list(N, _, _, _) when N < 1 ->
+    exit(badarg);
+replace_in_list(1, undefined, [_OldV | Vs], Acc) ->
+    lists:reverse(Acc) ++ Vs;
+replace_in_list(1, V1, [_OldV | Vs], Acc) ->
+    lists:reverse([V1 | Acc]) ++ Vs;
+replace_in_list(N, V1, [V | Vs], Acc) ->
+    replace_in_list(N-1, V1, Vs, [V | Acc]).
 
 %% EUNIT TESTING
 -include_lib("eunit/include/eunit.hrl").
@@ -106,6 +197,44 @@ set_value1([], Value, _Doc) -> Value.
 -define(D2, {struct, [{<<"d2k1">>, 1}, {<<"d2k2">>, 3.14}, {<<"sub_d1">>, ?D1}]}).
 -define(D3, {struct, [{<<"d3k1">>, <<"d3v1">>}, {<<"d3k2">>, []}, {<<"sub_docs">>, [?D1, ?D2]}]}).
 -define(D4, [?D1, ?D2, ?D3]).
+
+-define(D6, {struct, [{<<"d2k1">>, 1}
+		      ,{<<"d2k2">>, 3.14}
+		      ,{<<"sub_d1">>, {struct, [{<<"d1k1">>, "d1v1"}]}}
+		     ]
+	    }).
+-define(D7, {struct, [{<<"d1k1">>, <<"d1v1">>}]}).
+
+%% delete results
+-define(D1_AFTER_K1, {struct, [{<<"d1k2">>, d1v2}, {<<"d1k3">>, ["d1v3.1", "d1v3.2", "d1v3.3"]}]}).
+-define(D1_AFTER_K3_V2, {struct, [{<<"d1k3">>, ["d1v3.1", "d1v3.3"]}, {<<"d1k1">>, "d1v1"}, {<<"d1k2">>, d1v2}]}).
+
+-define(D6_AFTER_SUB, {struct, [{<<"sub_d1">>, {struct, []}}
+				,{<<"d2k1">>, 1}
+				,{<<"d2k2">>, 3.14}
+			       ]
+			 }).
+-define(D6_AFTER_SUB_PRUNE, {struct, [{<<"d2k1">>, 1}
+				      ,{<<"d2k2">>, 3.14}
+				     ]
+			    }).
+
+-spec(delete_key_test/0 :: () -> no_return()).
+delete_key_test() ->
+    ?assertEqual(?EMPTY_JSON_OBJECT, delete_key(<<"foo">>, ?EMPTY_JSON_OBJECT)),
+    ?assertEqual(?EMPTY_JSON_OBJECT, delete_key(<<"foo">>, ?EMPTY_JSON_OBJECT, prune)),
+    ?assertEqual(?EMPTY_JSON_OBJECT, delete_key([<<"foo">>], ?EMPTY_JSON_OBJECT)),
+    ?assertEqual(?EMPTY_JSON_OBJECT, delete_key([<<"foo">>], ?EMPTY_JSON_OBJECT, prune)),
+    ?assertEqual(?EMPTY_JSON_OBJECT, delete_key([<<"foo">>, <<"bar">>], ?EMPTY_JSON_OBJECT)),
+    ?assertEqual(?EMPTY_JSON_OBJECT, delete_key([<<"foo">>, <<"bar">>], ?EMPTY_JSON_OBJECT, prune)),
+    ?assertEqual(?EMPTY_JSON_OBJECT, delete_key([<<"d1k1">>], ?D7)),
+    ?assertEqual(?EMPTY_JSON_OBJECT, delete_key([<<"d1k1">>], ?D7, prune)),
+    ?assertEqual(?D1_AFTER_K1, delete_key([<<"d1k1">>], ?D1)),
+    ?assertEqual(?D1_AFTER_K1, delete_key([<<"d1k1">>], ?D1, prune)),
+    ?assertEqual(?D1_AFTER_K3_V2, delete_key([<<"d1k3">>, 2], ?D1)),
+    ?assertEqual(?D1_AFTER_K3_V2, delete_key([<<"d1k3">>, 2], ?D1, prune)),
+    ?assertEqual(?D6_AFTER_SUB, delete_key([<<"sub_d1">>, <<"d1k1">>], ?D6)),
+    ?assertEqual(?D6_AFTER_SUB_PRUNE, delete_key([<<"sub_d1">>, <<"d1k1">>], ?D6, prune)).
 
 -spec(get_value_test/0 :: () -> no_return()).
 get_value_test() ->
