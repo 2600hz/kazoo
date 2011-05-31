b5f8f7e33edd41105d8011fdb4d8fae1ce88afc4
James Aimonetti <james@sfvoip.com>
2011-05-31 15:57:55 -0700

    beams for whistle_apps

---
+++ whistle_apps/apps/crossbar/src/t/fixtures.hrl
@@ -0,0 +1,10 @@
+-define(CB_URL, "http://localhost:8000/v1/accounts").
+
+-define(IBROWSE_HEADERS, [{"X-Auth-Token", "fixtures-test"}
+			  ,{"Content-Type", "application/json"}
+			  ,{"Accept", "application/json"}
+			 ]).
+
+-define(ACCT_JOBJ, {struct, [{<<"name">>, <<"fixtures account">>}
+			     ,{<<"realm">>, whistle_util:to_binary(net_adm:localhost())}
+			    ]}).
