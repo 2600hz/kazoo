7718986751f98c3584f49191e4798dc120a0936c
karl anderson <karl@2600hz.com>
2011-07-16 03:48:59 +0000

    new goto callflow action to execute another callflow

---
+++ whistle_apps/apps/callflow/src/module/cf_callflow.erl
@@ -0,0 +1,33 @@
+%%%-------------------------------------------------------------------
+%%% @author Karl Anderson <karl@2600hz.org>
+%%% @copyright (C) 2011, VoIP INC
+%%% @doc
+%%%
+%%% @end
+%%% Created : 15 Jul 2011 by Karl Anderson <karl@2600hz.org>
+%%%-------------------------------------------------------------------
+-module(cf_callflow).
+
+-include("../callflow.hrl").
+
+-export([handle/2]).
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Entry point for this module
+%% @end
+%%--------------------------------------------------------------------
+-spec(handle/2 :: (Data :: json_object(), Call :: #cf_call{}) -> no_return()).
+handle(Data, #cf_call{call_id=CallId, account_db=AccountDb, cf_pid=CFPid}) ->
+    put(callid, CallId),
+    Id = wh_json:get_value(<<"id">>, Data),
+    case couch_mgr:open_doc(AccountDb, Id) of
+        {ok, JObj} ->
+            ?LOG("branching to callflow ~s", [Id]),
+            Flow = wh_json:get_value(<<"flow">>, JObj, ?EMPTY_JSON_OBJECT),
+            CFPid ! {branch, Flow};
+        {error, R} ->
+            ?LOG("could not branch to callflow ~s, ~p", [Id, R]),
+            {continue}
+    end.
