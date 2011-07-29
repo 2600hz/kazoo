4a047828a605f41e0dc1749c5f9bcaeab9b65461
Edouard Swiac <edouard@2600hz.com>
2011-07-29 18:37:39 +0000

    WHISTLE-105 app layout

---
+++ whistle_apps/apps/hotdesking/src/hotdesking.hrl
@@ -0,0 +1,8 @@
+-include_lib("couchbeam/include/couchbeam.hrl").
+-include_lib("rabbitmq_erlang_client/include/amqp_client.hrl").
+-include_lib("whistle/include/whistle_types.hrl").
+-include_lib("whistle/include/whistle_amqp.hrl").
+-include_lib("whistle/include/wh_log.hrl").
+
+-define(APP_NAME, <<"hotdesking">>).
+-define(APP_VSN, <<"0.1.0">>).
---
+++ whistle_apps/apps/hotdesking/src/hotdesking_app.erl
@@ -0,0 +1,29 @@
+%%%-------------------------------------------------------------------
+%%% @author Edouard Swiac <edouard@2600hz.org>
+%%% @copyright (C) 2011, VoIP INC
+%%% @doc
+%%%
+%%% @end
+%%% Created :  29 Jul 2011 by Edouard Swiac <edouard@2600hz.org>
+%%%-------------------------------------------------------------------
+-module(hotdesking_app).
+
+-behaviour(application).
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+
+-spec(start/2 :: (StartType :: term(), StartArgs :: term()) -> tuple(ok, pid()) | tuple(error, term())).
+start(_StartType, _StartArgs) ->
+    case jonny5:start_link() of
+	{ok, P} -> {ok, P};
+	{error, {already_started, P} } -> {ok, P};
+	{error, _}=E -> E
+    end.
+
+stop(_State) ->
+    ok.
