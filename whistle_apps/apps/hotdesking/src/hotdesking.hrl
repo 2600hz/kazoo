0805ca71a53a2c727f6874f6f0e65167dba4c3ae
Edouard Swiac <edouard@2600hz.com>
2011-08-04 18:31:43 +0000

    WHISTLE-105 better as a callflow module than a whapp

--- whistle_apps/apps/hotdesking/src/hotdesking.hrl
+++
@@ -1,4 +0,0 @@
--include_lib("rabbitmq_erlang_client/include/amqp_client.hrl").
--include_lib("whistle/include/whistle_types.hrl").
--include_lib("whistle/include/whistle_amqp.hrl").
--include_lib("whistle/include/wh_log.hrl").
