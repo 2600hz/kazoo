07e0ace67bec0ac7493d014d22da03d66021381b
James Aimonetti <james@sfvoip.com>
2011-08-15 12:27:09 -0700

    WHISTLE-169: undo accidental search/replace of whistle_* to wh_*

--- whistle_apps/apps/monitor/src/monitor.erl
+++ whistle_apps/apps/monitor/src/monitor.erl
@@ -29,7 +29,7 @@ start_deps() ->
     monitor_deps:ensure(),
     ensure_started(sasl),
     ensure_started(crypto),
-    ensure_started(wh_amqp),
+    ensure_started(whistle_amqp),
     ensure_started(couchbeam),
     ensure_started(whistle_couch).
 
