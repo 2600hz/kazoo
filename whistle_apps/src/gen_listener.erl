673149289c95ec9403f90cc19f8a04334cf300c5
James Aimonetti <james@sfvoip.com>
2011-08-22 16:23:25 -0700

    WHISTLE-530: rework jonny5 to use gen_listener and have graceful stopping as part of the update

--- whistle_apps/src/gen_listener.erl
+++ whistle_apps/src/gen_listener.erl
@@ -216,11 +216,11 @@ handle_cast({rm_responder, Responder, Keys}, #state{responders=Responders}=State
     {noreply, State#state{responders=listener_utils:rm_responder(Responders, Responder, Keys)}};
 
 handle_cast({add_binding, Binding, Props}, #state{queue=Q}=State) ->
-    add_binding_to_q(Q, Binding, Props),
+    queue_bindings:add_binding_to_q(Q, Binding, Props),
     {noreply, State};
 
 handle_cast({rm_binding, Binding}, #state{queue=Q}=State) ->
-    rm_binding_from_q(Q, Binding),
+    queue_bindings:rm_binding_from_q(Q, Binding),
     {noreply, State};
 
 handle_cast(Message, #state{module=Module, module_state=ModState}=State) ->
@@ -319,10 +319,10 @@ start_amqp(Bindings, Props) ->
     QueueProps = props:get_value(queue_options, Props, []),
     QueueName = props:get_value(queue_name, Props, <<>>),
     Q = amqp_util:new_queue(QueueName, QueueProps),
-     
+
     ConsumeProps = props:get_value(consume_options, Props, []),
 
-    _ = [ add_binding_to_q(Q, Type, BindProps) || {Type, BindProps} <- Bindings ],
+    _ = [ queue_bindings:add_binding_to_q(Q, Type, BindProps) || {Type, BindProps} <- Bindings ],
 
     amqp_util:basic_consume(Q, ConsumeProps),
     {ok, Q}.
@@ -332,30 +332,5 @@ start_amqp(Bindings, Props) ->
       Bindings :: bindings().
 stop_amqp(<<>>, _) -> ok;
 stop_amqp(Q, Bindings) ->
-    _ = [ rm_binding_from_q(Q, Type) || {Type, _} <- Bindings],
+    _ = [ queue_bindings:rm_binding_from_q(Q, Type) || {Type, _} <- Bindings],
     amqp_util:queue_delete(Q).
-
--type bind_types() :: authentication | registrations.
-
--spec add_binding_to_q/3 :: (Q, Type, Props) -> ok when
-      Q :: binary(),
-      Type :: bind_types(),
-      Props :: proplist().
-add_binding_to_q(Q, authentication, _Props) ->
-    amqp_util:callmgr_exchange(),
-    amqp_util:bind_q_to_callmgr(Q, ?KEY_AUTHN_REQ),
-    ok;
-add_binding_to_q(Q, registrations, _Props) ->
-    amqp_util:callmgr_exchange(),
-    amqp_util:bind_q_to_callmgr(Q, ?KEY_REG_SUCCESS),
-    amqp_util:bind_q_to_callmgr(Q, ?KEY_REG_QUERY),
-    ok.
-
--spec rm_binding_from_q/2 :: (Q, Type) -> ok when
-      Q :: binary(),
-      Type :: bind_types().
-rm_binding_from_q(Q, authentication) ->
-    amqp_util:unbind_q_from_callmgr(Q, ?KEY_AUTHN_REQ);
-rm_binding_from_q(Q, registrations) ->
-    amqp_util:unbind_q_from_callmgr(Q, ?KEY_REG_SUCCESS),
-    amqp_util:unbind_q_from_callmgr(Q, ?KEY_REG_QUERY).
