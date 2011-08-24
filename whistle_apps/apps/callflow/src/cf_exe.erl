4bcd1db76ffc2e02f3b973c93474975bebf7a930
karl anderson <karl@2600hz.com>
2011-08-24 18:35:35 +0000

    added check to verify the channel periodically, hangup on termination to be sure, removed debug statement

--- whistle_apps/apps/callflow/src/cf_exe.erl
+++ whistle_apps/apps/callflow/src/cf_exe.erl
@@ -47,7 +47,8 @@ init(Parent, #cf_call{authorizing_id=AuthId}=Call, Flow) ->
     proc_lib:init_ack(Parent, {ok, self()}),
     _ = next(Call#cf_call{cf_pid=self()
                           ,amqp_q=AmqpQ
-                          ,owner_id=cf_attributes:owner_id(AuthId, Call)}, Flow).
+                          ,owner_id=cf_attributes:owner_id(AuthId, Call)}
+             ,Flow, <<"active">>).
 
 %%--------------------------------------------------------------------
 %% @private
@@ -56,10 +57,11 @@ init(Parent, #cf_call{authorizing_id=AuthId}=Call, Flow) ->
 %% then waits for the modules reply, unexpected death, or timeout.
 %% @end
 %%--------------------------------------------------------------------
--spec next/2 :: (Call, Flow) -> no_return() when
+-spec next/3 :: (Call, Flow, CallStatus) -> no_return() when
       Call :: #cf_call{},
-      Flow :: json_object().
-next(#cf_call{last_action=LastAction}=Call, Flow) ->
+      Flow :: json_object(),
+      CallStatus :: undefined | binary().
+next(#cf_call{last_action=LastAction}=Call, Flow, CallStatus) ->
     Module = <<"cf_", (wh_json:get_value(<<"module">>, Flow))/binary>>,
     Data = wh_json:get_value(<<"data">>, Flow),
     {Pid, Action} =
@@ -73,7 +75,7 @@ next(#cf_call{last_action=LastAction}=Call, Flow) ->
                 self() ! {continue, <<"_">>},
                 {undefined, LastAction}
         end,
-    wait(Call#cf_call{last_action=Action}, Flow, Pid).
+    wait(Call#cf_call{last_action=Action}, Flow, Pid, CallStatus).
 
 %%--------------------------------------------------------------------
 %% @private
@@ -82,19 +84,20 @@ next(#cf_call{last_action=LastAction}=Call, Flow) ->
 %% unexpectly die, or timeout and advance the call flow accordingly
 %% @end
 %%--------------------------------------------------------------------
--spec wait/3 :: (Call, Flow, Pid) -> no_return() when
+-spec wait/4 :: (Call, Flow, Pid, CallStatus) -> no_return() when
       Call :: #cf_call{},
       Flow :: json_object(),
-      Pid :: undefined | pid().
-wait(Call, Flow, Pid) ->
+      Pid :: undefined | pid(),
+      CallStatus :: undefined | binary().
+wait(#cf_call{amqp_q=Q, call_id=CallId}=Call, Flow, Pid, CallStatus) ->
     receive
         {'EXIT', Pid, Reason} when Reason =/= normal ->
             ?LOG("action ~w died unexpectedly, ~p", [Pid, Reason]),
             self() ! {continue, <<"_">>},
-            wait(Call, Flow, Pid);
+            wait(Call, Flow, Pid, CallStatus);
         {continue} ->
             self() ! {continue, <<"_">>},
-            wait(Call, Flow, Pid);
+            wait(Call, Flow, Pid, CallStatus);
         {continue, Key} ->
             ?LOG("continuing to child ~s", [Key]),
             case wh_json:get_value([<<"children">>, Key], Flow) of
@@ -104,85 +107,81 @@ wait(Call, Flow, Pid) ->
                 undefined ->
                     ?LOG("requested child does not exist, trying wild card", [Key]),
                     self() ! {continue},
-                    wait(Call, Flow, Pid);
+                    wait(Call, Flow, Pid, CallStatus);
                 ?EMPTY_JSON_OBJECT ->
                     ?LOG_END("unexpected end of callflow"),
                     cf_call_command:hangup(Call);
                 NewFlow ->
-                    next(Call, NewFlow)
+                    next(Call, NewFlow, CallStatus)
             end;
         {get_branch_keys} ->
             {struct, Children} = wh_json:get_value(<<"children">>, Flow, ?EMPTY_JSON_OBJECT),
             Pid ! {branch_keys, lists:delete(<<"_">>, proplists:get_keys(Children))},
-            wait(Call, Flow, Pid);
+            wait(Call, Flow, Pid, CallStatus);
         {get_branch_keys, all} ->
             {struct, Children} = wh_json:get_value(<<"children">>, Flow, ?EMPTY_JSON_OBJECT),
             Pid ! {branch_keys, proplists:get_keys(Children)},
-            wait(Call, Flow, Pid);
+            wait(Call, Flow, Pid, CallStatus);
         {attempt} when not is_pid(Pid) ->
             self() ! {continue, <<"_">>},
-            wait(Call, Flow, Pid);
+            wait(Call, Flow, Pid, CallStatus);
         {attempt, Key} when not is_pid(Pid) ->
             self() ! {continue, Key},
-            wait(Call, Flow, Pid);
+            wait(Call, Flow, Pid, CallStatus);
         {attempt} ->
             self() ! {attempt, <<"_">>},
-            wait(Call, Flow, Pid);
+            wait(Call, Flow, Pid, CallStatus);
         {attempt, Key} ->
             ?LOG("attempting child ~s", [Key]),
             case wh_json:get_value([<<"children">>, Key], Flow) of
                 undefined ->
                     Pid ! {attempt_resp, {error, undefined}},
-                    wait(Call, Flow, Pid);
+                    wait(Call, Flow, Pid, CallStatus);
                 ?EMPTY_JSON_OBJECT ->
                     Pid ! {attempt_resp, {error, empty}},
-                    wait(Call, Flow, Pid);
+                    wait(Call, Flow, Pid, CallStatus);
                 NewFlow ->
                     Pid ! {attempt_resp, ok},
-                    next(Call, NewFlow)
+                    next(Call, NewFlow, CallStatus)
             end;
         {branch, NewFlow} ->
             ?LOG("callflow has been branched"),
-            next(Call, NewFlow);
+            next(Call, NewFlow, CallStatus);
         {stop} ->
             ?LOG_END("execution has been stopped"),
-            wait(Call, Flow, Pid);
-        %%           cf_call_command:hangup(Call)
-        {_, #amqp_msg{props = Props, payload = Payload}} when
+            cf_call_command:hangup(Call);
+        {transferred} ->
+            ?LOG_END("execution has been transferred");
+        {#'basic.deliver'{}, #amqp_msg{props = Props, payload = Payload}} when
               Props#'P_basic'.content_type == <<"application/json">> ->
             JObj = mochijson2:decode(Payload),
             case whapps_util:get_event_type(JObj) of
-                { <<"call_event">>, <<"CHANNEL_UNBRIDGE">> } ->
-                    io:format("~p~n", [io_lib:format("/tmp/~s.log", [get(callid)])]),
-                    R = file:write_file(io_lib:format("/tmp/~s.log", [get(callid)]),
-                               io_lib:fwrite("~p~n~n", [JObj]), [append]),
-                    io:format("~p~n", [R]);
-                { <<"call_event">>, <<"CHANNEL_HANGUP">> } ->
-                    io:format("~p~n", [io_lib:format("/tmp/~s.log", [get(callid)])]),
-                    file:write_file(io_lib:format("/tmp/~s.log", [get(callid)]),
-                                    io_lib:fwrite("~p~n~n", [JObj]), [append]);
-                { <<"call_event">>, <<"CHANNEL_EXECUTE_COMPLETE">> } ->
-                    case wh_json:get_value(<<"Application-Name">>, JObj) of
-                        <<"bridge">> ->
-                            file:write_file(io_lib:format("/tmp/~s.log", [get(callid)]),
-                                            io_lib:fwrite("~p~n~n", [JObj]), [append]);
-                        _ ->
-                            ok
-                    end;
+                {<<"call_event">>, <<"status_resp">>} ->
+                    Status = wh_json:get_value(<<"Status">>, JObj),
+                    is_pid(Pid) andalso Pid ! {amqp_msg, JObj},
+                    wait(Call, Flow, Pid, Status);
                 _ ->
-                    ok
-            end,
-            is_pid(Pid) andalso Pid ! {amqp_msg, JObj},
-            wait(Call, Flow, Pid);
+                    is_pid(Pid) andalso Pid ! {amqp_msg, JObj},
+                    wait(Call, Flow, Pid, CallStatus)
+            end;
         _Msg ->
             %% dont let the mailbox grow unbounded if
             %%   this process hangs around...
-            wait(Call, Flow, Pid)
+            wait(Call, Flow, Pid, CallStatus)
     after
-        60000 ->
-            io:format("terminating exec ~s~n", [self()]),
-            ?LOG_SYS("no call events received after 3600 seconds, shuting down")
-%%            cf_call_command:hangup(Call)
+        120000 ->
+            case CallStatus of
+                undefined ->
+                    ?LOG_SYS("no call events or control and status is unknown, shuting down"),
+                    cf_call_command:hangup(Call);
+                _ ->
+                    Command = [{<<"Call-ID">>, CallId}
+                               | wh_api:default_headers(Q, <<"call_event">>, <<"status_req">>, ?APP_NAME, ?APP_VERSION)
+                              ],
+                    {ok, Payload} = wh_api:call_status_req({struct, Command}),
+                    amqp_util:callevt_publish(CallId, Payload, status_req),
+                    wait(Call, Flow, Pid, undefined)
+            end
     end.
 
 %%--------------------------------------------------------------------
@@ -213,7 +212,6 @@ call_info(#cf_call{flow_id=FlowId, call_id=CallId, cid_name=CIDName, cid_number=
                ,request=Request, from=From, to=To, inception=Inception, authorizing_id=AuthorizingId }) ->
     put(callid, CallId),
     ?LOG_START("executing callflow ~s", [FlowId]),
-    io:format("started exec ~p fpr ~s~n", [self(), Request]),
     ?LOG("request ~s", [Request]),
     ?LOG("to ~s", [To]),
     ?LOG("from ~s", [From]),
