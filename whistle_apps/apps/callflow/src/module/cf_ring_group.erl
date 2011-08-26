ded1c4dd948ee712f9b41668dd95ad8d685aea94
karl anderson <karl@2600hz.com>
2011-08-26 00:41:04 +0000

    WHISTLE-132: create a funtion that can wait for a callee to unbridge from the caller ignoring any events that happen such as transfer

--- whistle_apps/apps/callflow/src/module/cf_ring_group.erl
+++ whistle_apps/apps/callflow/src/module/cf_ring_group.erl
@@ -12,7 +12,7 @@
 
 -export([handle/2]).
 
--import(cf_call_command, [b_bridge/6, wait_for_unbridge/0]).
+-import(cf_call_command, [b_bridge/6, wait_for_callee_release/1]).
 
 %%--------------------------------------------------------------------
 %% @public
@@ -32,9 +32,18 @@ handle(Data, #cf_call{cf_pid=CFPid, call_id=CallId}=Call) ->
     case b_bridge(Endpoints, Timeout, <<"internal">>, Strategy, <<"true">>, Call) of
         {ok, _} ->
             ?LOG("bridged to ring group"),
-            _ = wait_for_unbridge(),
-            ?LOG("ring group completed"),
-            CFPid ! { stop };
+            case wait_for_callee_release(Call) of
+                {fail, Reason} ->
+                    {Cause, Code} = whapps_util:get_call_termination_reason(Reason),
+                    ?LOG("bridge to ring group failed ~s:~s", [Code, Cause]),
+                    CFPid ! { continue };
+                {transfer, _} ->
+                    ?LOG("ring group was transferred"),
+                    CFPid ! { transferred };
+                _ ->
+                    ?LOG("ring group was unbridged"),
+                    CFPid ! { stop }
+            end;
         {fail, Reason} ->
             {Cause, Code} = whapps_util:get_call_termination_reason(Reason),
             ?LOG("failed to bridge to ring group ~s:~s", [Code, Cause]),
