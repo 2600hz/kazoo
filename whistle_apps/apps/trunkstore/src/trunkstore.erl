92c52fc4ffbacaf9fa858418856ce2e8c5db4308
James Aimonetti <james@sfvoip.com>
2011-08-26 14:40:10 -0700

    WHISTLE-443: update trivial bits in the erlang app

--- whistle_apps/apps/trunkstore/src/trunkstore.erl
+++ whistle_apps/apps/trunkstore/src/trunkstore.erl
@@ -3,9 +3,11 @@
 -author('James Aimonetti <james@2600hz.org>').
 -export([start/0, start_link/0, stop/0, start_deps/0]).
 
+-include("ts.hrl").
+
 %% @spec start_link() -> {ok,Pid::pid()}
 %% @doc Starts the app for inclusion in a supervisor tree
--spec start_link/0 :: () -> {ok, pid()} | ignore | {error, term()}.
+-spec start_link/0 :: () -> startlink_ret().
 start_link() ->
     start_deps(),
     trunkstore_sup:start_link().
@@ -17,21 +19,13 @@ start() ->
 
 start_deps() ->
     trunkstore_deps:ensure(),
-    ensure_started(sasl),
-    ensure_started(crypto),
-    ensure_started(whistle_amqp),
-    ensure_started(whistle_couch),
-    ensure_started(ibrowse).
+    wh_util:ensure_started(sasl),
+    wh_util:ensure_started(crypto),
+    wh_util:ensure_started(whistle_amqp),
+    wh_util:ensure_started(whistle_couch),
+    wh_util:ensure_started(ibrowse).
 
 %% @spec stop() -> ok
 %% @doc Stop the callmgr server.
 stop() ->
     application:stop(trunkstore).
-
-ensure_started(App) ->
-    case application:start(App) of
-	ok ->
-	    ok;
-	{error, {already_started, App}} ->
-	    ok
-    end.
