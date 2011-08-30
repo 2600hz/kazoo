e41f367b47e4e77ab2d6d6046cee03ba88b14e2f
James Aimonetti <james@sfvoip.com>
2011-08-30 16:16:42 -0700

    WHISTLE-543: replace 86400 with macro for seconds in a day

--- whistle_apps/apps/crossbar/src/v1_resource.erl
+++ whistle_apps/apps/crossbar/src/v1_resource.erl
@@ -799,7 +799,7 @@ get_cors_headers(#cb_context{allow_methods=Allowed}) ->
       {<<"Access-Control-Allow-Origin">>, <<"*">>}
      ,{<<"Access-Control-Allow-Methods">>, string:join([wh_util:to_list(A) || A <- Allowed], ", ")}
      ,{<<"Access-Control-Allow-Headers">>, <<"Content-Type, Depth, User-Agent, X-File-Size, X-Requested-With, If-Modified-Since, X-File-Name, Cache-Control, X-Auth-Token">>}
-     ,{<<"Access-Control-Max-Age">>, <<"86400">>}
+     ,{<<"Access-Control-Max-Age">>, wh_util:to_binary(?SECONDS_IN_DAY)}
     ].
 
 %%--------------------------------------------------------------------
