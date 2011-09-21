248a0790a1a92351866f0224c71644b982893332
karl anderson <karl@2600hz.com>
2011-09-21 23:55:50 +0000

    WHISTLE-400: created a new global_resource API

---
+++ whistle_apps/apps/crossbar/src/modules/cb_global_resources.erl
@@ -0,0 +1,362 @@
+%%%-------------------------------------------------------------------
+%%% @author Karl Anderson <karl@2600hz.org>
+%%% @copyright (C) 2011, VoIP INC
+%%% @doc
+%%%
+%%%
+%%% Handle client requests for global resource documents
+%%%
+%%% @end
+%%% Created : 05 Jan 2011 by Karl Anderson <karl@2600hz.org>
+%%%-------------------------------------------------------------------
+-module(cb_global_resources).
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/0]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+	 terminate/2, code_change/3]).
+
+-include("../../include/crossbar.hrl").
+
+-define(SERVER, ?MODULE).
+-define(PVT_FUNS, [fun add_pvt_type/2]).
+-define(CB_LIST, <<"global_resources/crossbar_listing">>).
+-define(GLOBAL_RESOURCE_DB, <<"offnet">>).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init(_) ->
+    couch_mgr:revise_doc_from_file(?GLOBAL_RESOURCE_DB, crossbar, "views/global_resources.json"),
+    {ok, ok, 0}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info({binding_fired, Pid, <<"v1_resource.allowed_methods.global_resources">>, Payload}, State) ->
+    spawn(fun() ->
+		  {Result, Payload1} = allowed_methods(Payload),
+                  Pid ! {binding_result, Result, Payload1}
+	  end),
+    {noreply, State};
+
+handle_info({binding_fired, Pid, <<"v1_resource.resource_exists.global_resources">>, Payload}, State) ->
+    spawn(fun() ->
+		  {Result, Payload1} = resource_exists(Payload),
+                  Pid ! {binding_result, Result, Payload1}
+	  end),
+    {noreply, State};
+
+handle_info({binding_fired, Pid, <<"v1_resource.validate.global_resources">>, [RD, Context | Params]}, State) ->
+    spawn(fun() ->
+                  crossbar_util:put_reqid(Context),
+		  Context1 = validate(Params, Context#cb_context{db_name=?GLOBAL_RESOURCE_DB}),
+		  Pid ! {binding_result, true, [RD, Context1, Params]}
+	  end),
+    {noreply, State};
+
+handle_info({binding_fired, Pid, <<"v1_resource.execute.post.global_resources">>, [RD, Context | Params]}, State) ->
+    spawn(fun() ->
+                  crossbar_util:put_reqid(Context),
+                  Context1 = crossbar_doc:save(Context),
+                  Pid ! {binding_result, true, [RD, Context1, Params]}
+	  end),
+    {noreply, State};
+
+handle_info({binding_fired, Pid, <<"v1_resource.execute.put.global_resources">>, [RD, Context | Params]}, State) ->
+    spawn(fun() ->
+                  crossbar_util:put_reqid(Context),
+                  Context1 = crossbar_doc:save(Context),
+                  Pid ! {binding_result, true, [RD, Context1, Params]}
+	  end),
+    {noreply, State};
+
+handle_info({binding_fired, Pid, <<"v1_resource.execute.delete.global_resources">>, [RD, Context | Params]}, State) ->
+    spawn(fun() ->
+                  crossbar_util:put_reqid(Context),
+                  Context1 = crossbar_doc:delete(Context),
+                  Pid ! {binding_result, true, [RD, Context1, Params]}
+	  end),
+    {noreply, State};
+
+handle_info({binding_fired, Pid, _, Payload}, State) ->
+    Pid ! {binding_result, false, Payload},
+    {noreply, State};
+
+handle_info(timeout, State) ->
+    bind_to_crossbar(),
+    {noreply, State};
+
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function binds this server to the crossbar bindings server,
+%% for the keys we need to consume.
+%% @end
+%%--------------------------------------------------------------------
+-spec bind_to_crossbar/0 :: () ->  no_return().
+bind_to_crossbar() ->
+    _ = crossbar_bindings:bind(<<"v1_resource.allowed_methods.global_resources">>),
+    _ = crossbar_bindings:bind(<<"v1_resource.resource_exists.global_resources">>),
+    _ = crossbar_bindings:bind(<<"v1_resource.validate.global_resources">>),
+    crossbar_bindings:bind(<<"v1_resource.execute.#.global_resources">>).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function determines the verbs that are appropriate for the
+%% given Nouns.  IE: '/accounts/' can only accept GET and PUT
+%%
+%% Failure here returns 405
+%% @end
+%%--------------------------------------------------------------------
+-spec allowed_methods/1 :: (Paths) -> tuple(boolean(), http_methods()) when
+      Paths :: list().
+allowed_methods([]) ->
+    {true, ['GET', 'PUT']};
+allowed_methods([_]) ->
+    {true, ['GET', 'POST', 'DELETE']};
+allowed_methods(_) ->
+    {false, []}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function determines if the provided list of Nouns are valid.
+%%
+%% Failure here returns 404
+%% @end
+%%--------------------------------------------------------------------
+-spec resource_exists/1 :: (Paths) -> tuple(boolean(), []) when
+      Paths :: list().
+resource_exists([]) ->
+    {true, []};
+resource_exists([_]) ->
+    {true, []};
+resource_exists(_) ->
+    {false, []}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function determines if the parameters and content are correct
+%% for this request
+%%
+%% Failure here returns 400
+%% @end
+%%--------------------------------------------------------------------
+-spec validate/2 :: (Params, Context) -> #cb_context{} when
+      Params :: list(),
+      Context :: #cb_context{}.
+validate([], #cb_context{req_verb = <<"get">>}=Context) ->
+    summary(Context);
+validate([], #cb_context{req_verb = <<"put">>}=Context) ->
+    create(Context);
+validate([Id], #cb_context{req_verb = <<"get">>}=Context) ->
+    read(Id, Context);
+validate([Id], #cb_context{req_verb = <<"post">>}=Context) ->
+    update(Id, Context);
+validate([Id], #cb_context{req_verb = <<"delete">>}=Context) ->
+    read(Id, Context);
+validate(_, Context) ->
+    crossbar_util:response_faulty_request(Context).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Create a new instance with the data provided, if it is valid
+%% @end
+%%--------------------------------------------------------------------
+-spec create/1 :: (Context) -> #cb_context{} when
+      Context :: #cb_context{}.
+create(#cb_context{req_data=JObj}=Context) ->
+    case is_valid_doc(JObj) of
+        {false, Fields} ->
+            crossbar_util:response_invalid_data(Fields, Context);
+        {true, []} ->
+            {JObj1, _} = lists:foldr(fun(F, {J, C}) ->
+                                             {F(J, C), C}
+                                     end, {JObj, Context}, ?PVT_FUNS),
+            Context#cb_context{doc=JObj1, resp_status=success}
+    end.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Load an instance from the database
+%% @end
+%%--------------------------------------------------------------------
+-spec(read/2 :: (Id :: binary(), Context :: #cb_context{}) -> #cb_context{}).
+read(Id, Context) ->
+    crossbar_doc:load(Id, Context).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Update an existing instance with the data provided, if it is
+%% valid
+%% @end
+%%--------------------------------------------------------------------
+-spec(update/2 :: (Id :: binary(), Context :: #cb_context{}) -> #cb_context{}).
+update(Id, #cb_context{req_data=JObj}=Context) ->
+    case is_valid_doc(JObj) of
+        {false, Fields} ->
+            crossbar_util:response_invalid_data(Fields, Context);
+        {true, []} ->
+            {JObj1, _} = lists:foldr(fun(F, {J, C}) ->
+                                             {F(J, C), C}
+                                     end, {JObj, Context}, ?PVT_FUNS),
+            crossbar_doc:load_merge(Id, JObj1, Context)
+    end.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Attempt to load a summarized listing of all instances of this
+%% resource.
+%% @end
+%%--------------------------------------------------------------------
+-spec summary/1 :: (Context) -> #cb_context{} when
+      Context :: #cb_context{}.
+summary(Context) ->
+    crossbar_doc:load_view(?CB_LIST, [], Context, fun normalize_view_results/2).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Normalizes the resuts of a view
+%% @end
+%%--------------------------------------------------------------------
+-spec normalize_view_results/2 :: (Doc, Acc) -> json_objects() when
+      Doc :: json_object(),
+      Acc :: json_object().
+normalize_view_results(JObj, Acc) ->
+    [wh_json:get_value(<<"value">>, JObj)|Acc].
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% NOTICE: This is very temporary, placeholder until the schema work is
+%% complete!
+%% @end
+%%--------------------------------------------------------------------
+-spec is_valid_doc/1 :: (JObj) -> {boolean(), [binary(),...] | []} when
+      JObj :: json_object().
+is_valid_doc(JObj) ->
+    case wh_json:get_value(<<"gateways">>, JObj) of
+	undefined -> {false, [<<"gateways">>]};
+	_ -> {true, []}
+    end.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% These are the pvt funs that add the necessary pvt fields to every
+%% instance
+%% @end
+%%--------------------------------------------------------------------
+-spec add_pvt_type/2 :: (JObj, Context) -> json_object() when
+      JObj :: json_object(),
+      Context :: #cb_context{}.
+
+add_pvt_type(JObj, _) ->
+    wh_json:set_value(<<"pvt_type">>, <<"resource">>, JObj).
