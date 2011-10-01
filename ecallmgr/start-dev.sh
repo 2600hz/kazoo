6a16a5b2ee758191251cc5d434f98571670ae9a7
James Aimonetti <james@sfvoip.com>
2011-10-01 15:15:15 -0700

    WHISTLE-664: remove reloader reference from start-dev

--- ecallmgr/start-dev.sh
+++ ecallmgr/start-dev.sh
@@ -3,4 +3,4 @@
 cd `dirname $0`
 
 export ERL_LIBS=$PWD/../lib
-exec erl -args_file $PWD/conf/vm.args -pa $PWD/ebin -s reloader -s ecallmgr
+exec erl -args_file $PWD/conf/vm.args -pa $PWD/ebin -s ecallmgr
