27cd2ce8913aff91cb08bd1ffa64636ce1b9da17
karl anderson <karl@2600hz.com>
2011-10-05 17:07:28 +0000

    include a application name on the status response, for consistency with call events

--- lib/whistle-1.0.0/src/wh_api.hrl
+++ lib/whistle-1.0.0/src/wh_api.hrl
@@ -334,10 +334,12 @@
 
 %% Call Status Response
 -define(CALL_STATUS_RESP_HEADERS, [<<"Call-ID">>, <<"Status">>]).
--define(OPTIONAL_CALL_STATUS_RESP_HEADERS, [<<"Custom-Channel-Vars">>, <<"Error-Msg">>, <<"Node">>]).
+-define(OPTIONAL_CALL_STATUS_RESP_HEADERS, [<<"Custom-Channel-Vars">>, <<"Error-Msg">>, <<"Node">>
+                                                ,<<"Application-Name">>, <<"Application-Response">>]).
 -define(CALL_STATUS_RESP_VALUES, [{<<"Event-Category">>, <<"call_event">>}
 				  ,{<<"Event-Name">>, <<"status_resp">>}
 				  ,{<<"Status">>, [<<"active">>, <<"tmpdown">>]}
+                                  ,{<<"Application-Name">>, <<"status">>}
 				 ]).
 -define(CALL_STATUS_RESP_TYPES, [{<<"Custom-Channel-Vars">>, ?IS_JSON_OBJECT}]).
 
