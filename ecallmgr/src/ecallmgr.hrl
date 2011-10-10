9d5b6e36e4b71b63957124d85668da4bf603a748
James Aimonetti <james@sfvoip.com>
2011-10-10 11:34:38 -0700

    WHISTLE-539: update type info

--- ecallmgr/src/ecallmgr.hrl
+++ ecallmgr/src/ecallmgr.hrl
@@ -12,11 +12,11 @@
 			,lookups_failed = 0 :: integer()
                         ,lookups_timeout = 0 :: integer()
                         ,lookups_requested = 0 :: integer()
-			,started = {0,0,0} :: tuple(integer(), integer(), integer())
+			,started = {0,0,0} :: wh_now()
 		       }).
 
--record(node_stats, {started = {0,0,0} :: tuple(integer(), integer(), integer())
-		     ,last_heartbeat = {0,0,0} :: tuple(integer(), integer(), integer())
+-record(node_stats, {started = {0,0,0} :: wh_now()
+		     ,last_heartbeat = {0,0,0} :: wh_now()
                      ,created_channels = 0 :: integer()
 		     ,destroyed_channels = 0 :: integer()
 		     ,fs_uptime = 0 :: integer() % in microseconds
