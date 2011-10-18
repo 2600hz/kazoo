0776d3507defc0f7d3ba319d7644d3894d90fc2b
karl anderson <karl@2600hz.com>
2011-10-18 03:22:25 +0000

    WHISTLE-697: be more aggressive creating the account definition in accounts

--- whistle_apps/apps/crossbar/src/modules/cb_accounts.erl
+++ whistle_apps/apps/crossbar/src/modules/cb_accounts.erl
@@ -164,7 +164,8 @@ handle_info({binding_fired, Pid, <<"v1_resource.execute.post.accounts">>, [RD, C
                   crossbar_util:put_reqid(Context),
                   crossbar_util:binding_heartbeat(Pid),
                   Context1 = crossbar_doc:save(Context#cb_context{db_name=whapps_util:get_db_name(AccountId, encoded)}),
-		  whapps_util:replicate_from_account(whapps_util:get_db_name(AccountId, unencoded), ?AGG_DB, ?AGG_FILTER),
+                  couch_mgr:ensure_saved(?AGG_DB, Context1#cb_context.doc),
+		  %% whapps_util:replicate_from_account(whapps_util:get_db_name(AccountId, unencoded), ?AGG_DB, ?AGG_FILTER),
                   Pid ! {binding_result, true, [RD, Context1, Params]}
           end),
     {noreply, State};
@@ -416,10 +417,10 @@ load_account(AccountId, Context) ->
 -spec(update_account/2 :: (AccountId :: binary(), Context :: #cb_context{}) -> #cb_context{}).
 update_account(AccountId, #cb_context{req_data=Data}=Context) ->
     case is_valid_doc(Data) of
-        {false, Fields} ->
+        {error, Fields} ->
 	    ?LOG_SYS("Failed to validate JSON"),
              crossbar_util:response_invalid_data(Fields, Context);
-        {true, _} ->
+        {ok, _} ->
             case is_unique_realm(AccountId, Context) of
                 true ->
 		    %% Update the aggregate accounts DB (/accounts/AB/CB)
@@ -703,7 +704,8 @@ create_new_account_db(#cb_context{doc=Doc}=Context) ->
                 #cb_context{resp_status=success}=Context1 ->
                     _ = crossbar_bindings:map(<<"account.created">>, Db),
                     couch_mgr:revise_docs_from_folder(Db, crossbar, "account"),
-                    whapps_util:replicate_from_account(whapps_util:get_db_name(Db, unencoded), ?AGG_DB, ?AGG_FILTER),
+                    couch_mgr:ensure_saved(?AGG_DB, Doc),
+                    %% whapps_util:replicate_from_account(whapps_util:get_db_name(Db, unencoded), ?AGG_DB, ?AGG_FILTER),
                     Context1;
                 Else ->
 		    ?LOG_SYS("Other PUT resp: ~s: ~p~n", [Else#cb_context.resp_status, Else#cb_context.doc]),
