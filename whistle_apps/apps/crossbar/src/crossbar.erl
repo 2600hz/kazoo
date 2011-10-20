a87e2d5882fee694978c903299810168f6a2294d
karl anderson <karl@2600hz.com>
2011-10-20 02:18:56 +0000

    create a maintenance view for admins and allow refresh of the aggregate dbs

--- whistle_apps/apps/crossbar/src/crossbar.erl
+++ whistle_apps/apps/crossbar/src/crossbar.erl
@@ -8,7 +8,7 @@
 %%%-------------------------------------------------------------------
 -module(crossbar).
 
--include_lib("whistle/include/wh_types.hrl").
+-include("../include/crossbar.hrl").
 
 -export([start_link/0, stop/0]).
 
@@ -16,7 +16,6 @@
 
 -define(ACCOUNT_AGG_DB, <<"accounts">>).
 -define(ACCOUNT_AGG_VIEW_FILE, <<"views/accounts.json">>).
--define(ACCOUNT_AGG_FILTER, <<"account/export">>).
 
 -define(SIP_AGG_DB, <<"sip_auth">>).
 -define(SIP_AGG_FILTER, <<"devices/export_sip">>).
@@ -73,11 +72,9 @@ start_deps() ->
 
 refresh() ->
     spawn(fun() ->
-                  couch_mgr:db_create(?SIP_AGG_DB),
-                  couch_mgr:db_create(?ACCOUNT_AGG_DB),
-		  couch_mgr:db_create(?SCHEMAS_DB),
-                  couch_mgr:revise_docs_from_folder(?SCHEMAS_DB, crossbar, "schemas"),
-                  couch_mgr:revise_doc_from_file(?ACCOUNT_AGG_DB, crossbar, ?ACCOUNT_AGG_VIEW_FILE),
+                  refresh(?SIP_AGG_DB),
+                  refresh(?SCHEMAS_DB),
+                  refresh(?ACCOUNT_AGG_DB),
                   lists:foreach(fun(AccountDb) ->
                                         timer:sleep(2000),
                                         refresh(AccountDb)
@@ -85,9 +82,27 @@ refresh() ->
           end),
     started.
 
+refresh(Account) when not is_binary(Account) ->
+    refresh(wh_util:to_binary(Account));
+refresh(?SIP_AGG_DB) ->
+    couch_mgr:db_create(?SIP_AGG_DB);
+refresh(?SCHEMAS_DB) ->
+    couch_mgr:db_create(?SCHEMAS_DB),
+    couch_mgr:revise_docs_from_folder(?SCHEMAS_DB, crossbar, "schemas");
+refresh(?ACCOUNT_AGG_DB) ->
+    couch_mgr:db_create(?ACCOUNT_AGG_DB),
+    couch_mgr:revise_doc_from_file(?ACCOUNT_AGG_DB, crossbar, ?ACCOUNT_AGG_VIEW_FILE),
+    couch_mgr:revise_doc_from_file(?ACCOUNT_AGG_DB, crossbar, ?MAINTENANCE_VIEW_FILE),
+    ok;
 refresh(Account) ->
     AccountDb = whapps_util:get_db_name(Account, encoded),
     couch_mgr:revise_docs_from_folder(AccountDb, crossbar, "account"),
-    whapps_util:replicate_from_account(AccountDb, ?ACCOUNT_AGG_DB, ?ACCOUNT_AGG_FILTER),
+    couch_mgr:revise_doc_from_file(AccountDb, crossbar, ?MAINTENANCE_VIEW_FILE),
+    case couch_mgr:open_doc(AccountDb, Account) of
+        {error, not_found} ->
+            ?LOG("account ~s is missing its local account definition!", [Account]);
+        {ok, JObj} ->
+            couch_mgr:ensure_saved(?ACCOUNT_AGG_DB, JObj)
+    end,
     whapps_util:replicate_from_account(AccountDb, ?SIP_AGG_DB, ?SIP_AGG_FILTER),
     ok.
