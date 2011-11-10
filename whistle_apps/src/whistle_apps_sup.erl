b5dd0b947e80ebcda1e4292436948bf2913172b4
karl anderson <karl@2600hz.com>
2011-11-10 00:38:07 +0000

    new module for managing the whapps configuration

--- whistle_apps/src/whistle_apps_sup.erl
+++ whistle_apps/src/whistle_apps_sup.erl
@@ -4,13 +4,14 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0]).
+-export([start_link/0, config_cache_proc/0]).
 
 %% Supervisor callbacks
 -export([init/1]).
 
 %% Helper macro for declaring children of supervisor
 -define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+-define(CACHE(Name), {Name, {wh_cache, start_link, [Name]}, permanent, 5000, worker, [wh_cache]}).
 
 %% ===================================================================
 %% API functions
@@ -19,16 +20,22 @@
 start_link() ->
     supervisor:start_link({local, ?MODULE}, ?MODULE, []).
 
+-spec config_cache_proc/0 :: () -> {'ok', pid()}.
+config_cache_proc() ->
+    [P] = [P || {Mod, P, _, _} <- supervisor:which_children(?MODULE),
+		Mod =:= config_cache],
+    {ok, P}.
+
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
 
 init([]) ->
     {ok, { {one_for_one, 5, 10}
-	   , [
-	      ?CHILD(whapps_sup, supervisor)
-	      ,?CHILD(whapps_controller, worker)
-	      ,?CHILD(wh_cache, worker)
+	   , [?CHILD(wh_cache, worker)
 	      ,?CHILD(wh_timer, worker)
+              ,?CACHE(config_cache)
+	      ,?CHILD(whapps_sup, supervisor)
+	      ,?CHILD(whapps_controller, worker)
 	     ]
 	 } }.
