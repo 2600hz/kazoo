24ae5398a4838c3073269bcbb0ab9c665a9a8fb3
James Aimonetti <james@2600hz.org>
2011-12-13 15:11:06 -0800

    WHISTLE-792: bump up timeout on binding heartbeat, update specs in a bunch of functions

--- whistle_apps/apps/crossbar/include/crossbar_types.hrl
+++ whistle_apps/apps/crossbar/include/crossbar_types.hrl
@@ -1,23 +1,23 @@
 -ifndef(CROSSBAR_TYPES_INCLUDED).
 
--type crossbar_status() :: success | error | fatal.
--type crossbar_module_result() :: tuple(crossbar_status(), proplist())
-				  | tuple(crossbar_status(), proplist(), string())
-				  | tuple(crossbar_status(), proplist(), string(), integer()).
+-type crossbar_status() :: 'success' | 'error' | 'fatal'.
+-type crossbar_module_result() :: {crossbar_status(), proplist()}
+				  | {crossbar_status(), proplist(), string()}
+				  | {crossbar_status(), proplist(), string(), integer()}.
 
--type crossbar_content_handler() :: tuple(atom(), list(string())).
+-type crossbar_content_handler() :: {atom(), [string(),...]}.
 
 -type http_method() :: 'POST' | 'GET' | 'PUT' | 'DELETE'.
 -type http_methods() :: [http_method()].
 
--type validator() :: required | not_empty | is_type | is_format | numeric_min | numeric_max | numeric_between | width | width.
--type validator_rule() :: tuple(validator(), list() | []).
--type validator_rules() :: list(validator_rule()).
+-type validator() :: 'required' | 'not_empty' | 'is_type' | 'is_format' | 'numeric_min' | 'numeric_max' | 'numeric_between' | 'width'.
+-type validator_rule() :: {validator(), list() | []}.
+-type validator_rules() :: [validator_rule(),...] | [].
 
 -type couch_doc_path() :: list(binary()).
--type couch_schema() :: list(tuple(couch_doc_path(), validator_rules())).
+-type couch_schema() :: list({couch_doc_path(), validator_rules()}).
 
--type proplist_bool() :: list(tuple(boolean(), term())) | [].
+-type proplist_bool() :: [{boolean(), term()},...] | [].
 
--define(CROSSBAR_TYPES_INCLUDED, true).
+-define(CROSSBAR_TYPES_INCLUDED, 'true').
 -endif.
