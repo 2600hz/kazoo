4612dee26a39aec566c04f978b32aef0e118c1bc
James Aimonetti <james@2600hz.org>
2012-01-03 16:26:22 -0800

    WHISTLE-822: updated specs and added get_integer_value/{2,3} to cast the value as an integer

--- lib/whistle-1.0.0/src/props.erl
+++ lib/whistle-1.0.0/src/props.erl
@@ -1,34 +1,28 @@
 %%%-------------------------------------------------------------------
-%%% @author James Aimonetti <>
-%%% @copyright (C) 2010, James Aimonetti
+%%% @author James Aimonetti <james@2600hz.org>
+%%% @copyright (C) 2010, VoIP INC
 %%% @doc
 %%% Replaces proplists get_value/2 and get_value/3 with a faster version
 %%% using lists:keyfind
 %%% @end
-%%% Created :  6 Oct 2010 by James Aimonetti <>
+%%% Created :  6 Oct 2010 by James Aimonetti <james@2600hz.org>
 %%%-------------------------------------------------------------------
 -module(props).
 
 -export([get_value/2, get_value/3, delete/2, is_defined/2]).
+-export([get_integer_value/2, get_integer_value/3]).
 -export([get_keys/1]).
 -export([unique/1]).
 
 -include_lib("whistle/include/wh_types.hrl").
 
--spec get_value/2 :: (Key, Prop) -> term() when
-      Key :: binary() | atom(),
-      Prop :: proplist().
--spec get_value/3 :: (Key, Prop, Default) -> term() when
-      Key :: binary() | atom(),
-      Prop :: proplist(),
-      Default :: term().
+-spec get_value/2 :: (ne_binary() | atom(), proplist()) -> term().
+-spec get_value/3 :: (ne_binary() | atom(), proplist(), Default) -> Default | term().
 get_value(Key, Prop) ->
     get_value(Key, Prop, undefined).
 
 get_value(_Key, [], Def) -> Def;
-get_value(Key, {struct, Prop}, Def) ->
-    get_value(Key, Prop, Def);
-get_value(Key, Prop, Default) ->
+get_value(Key, Prop, Default) when is_list(Prop) ->
     case lists:keyfind(Key, 1, Prop) of
         false ->
             case lists:member(Key, Prop) of
@@ -41,20 +35,25 @@ get_value(Key, Prop, Default) ->
             Default
     end.
 
--spec get_keys/1 :: (Prop) -> [term(),...] | [] when
-      Prop :: proplist().
+-spec get_integer_value/2 :: (ne_binary() | atom(), proplist()) -> integer() | 'undefined'.
+-spec get_integer_value/3 :: (ne_binary() | atom(), proplist(), Default) -> integer() | Default.
+get_integer_value(Key, Prop) ->
+    get_integer_value(Key, Prop, undefined).
+get_integer_value(Key, Prop, Default) ->
+    case ?MODULE:get_value(Key, Prop, Default) of
+	Default -> Default;
+	Val -> wh_util:to_integer(Val)
+    end.
+
+-spec get_keys/1 :: (proplist()) -> [term(),...] | [].
 get_keys(Prop) ->
     [ K || {K,_} <- Prop].
 
--spec delete/2 :: (Key, Prop) -> proplist() when
-      Key :: binary() | atom(),
-      Prop :: proplist().
+-spec delete/2 :: (ne_binary() | atom(), proplist()) -> proplist().
 delete(K, Prop) ->
     lists:keydelete(K, 1, Prop).
 
--spec is_defined/2 :: (Key, Prop) -> boolean() when
-      Key :: term(),
-      Prop :: proplist().
+-spec is_defined/2 :: (ne_binary() | atom(), proplist()) -> boolean().
 is_defined(Key, Prop) ->
     case lists:keyfind(Key, 1, Prop) of
         {Key,_} -> true;
@@ -63,7 +62,7 @@ is_defined(Key, Prop) ->
 
 -spec unique/1 :: (proplist()) -> proplist().
 unique(List) ->
-    unique(List, []).                                                                                                                                                                                                                                                    
+    unique(List, []).
 
 -spec unique/2 :: (proplist(), proplist()) -> proplist().
 unique([], Uniques) ->
