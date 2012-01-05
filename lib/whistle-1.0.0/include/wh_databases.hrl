25e751afabcf848fac654347b8588b6645798909
karl anderson <karl@2600hz.com>
2012-01-05 19:56:07 +0000

    WHISTLE-278: neglected to check in this header

---
+++ lib/whistle-1.0.0/include/wh_databases.hrl
@@ -0,0 +1,9 @@
+-ifndef(WHISTLE_DBS_INCLUDED).
+
+-define(SYS_CONFIG, <<"system_config">>).
+-define(SYS_SCHEMA, <<"system_schemas">>).
+-define(SYS_MEDIA, <<"system_media">>).
+-define(SIP_AUTH, <<"sip_auth">>).
+
+-define(WHISTLE_DBS_INCLUDED, true).
+-endif.
