a246f11065d021a7342b20b2de1f7737baedbb19
James Aimonetti <james@2600hz.org>
2012-01-11 09:55:03 -0800

    WHISTLE-862: updated prompts with existing media to test with

--- whistle_apps/apps/callflow/src/module/cf_agent.erl
+++ whistle_apps/apps/callflow/src/module/cf_agent.erl
@@ -18,8 +18,11 @@
 -include("../callflow.hrl").
 
 -record(prompts, {
-          pin_prompt = <<"/system_media/agent_pin">>
-         ,welcome_prompt = <<"/system_media/agent_welcome">>
+          pin_prompt = <<"/system_media/hotdesk-enter_pin">>
+         ,invalid_pin_prompt = <<"/system_media/conf-bad_pin">>
+         ,retries_exceeded_prompt = <<"/system_media/conf-to_many_attempts">>
+         ,welcome_prompt = <<"/system_media/vm-setup_complete">>
+         ,logged_in_prompt = <<"/system_media/hotdesk-logged_in">>
          }).
 
 -spec handle/2 :: (json_object(), #cf_call{}) -> 'ok'.
@@ -59,6 +62,7 @@ put_on_hold(Call) ->
 
 play_welcome(Call) ->
     #prompts{welcome_prompt=WelcomePrompt} = #prompts{},
+    
     cf_call_command:play(WelcomePrompt, Call).
 
 prompt_and_get_pin(#prompts{pin_prompt=PinPrompt}, Data, Call) ->
@@ -67,15 +71,24 @@ prompt_and_get_pin(#prompts{pin_prompt=PinPrompt}, Data, Call) ->
 
     cf_call_command:b_play_and_collect_digits(MinPinDigits, MaxPinDigits, PinPrompt, Call).
 
-find_agent(_Data, 0, _) -> {error, retries_exceeded};
+find_agent(_Data, 0, Call) ->
+    #prompts{retries_exceeded_prompt=RetriesPrompt} = #prompts{},
+    cf_call_command:play(RetriesPrompt, Call),
+    {error, retries_exceeded};
 find_agent(Data, Retries, #cf_call{account_db=Db}=Call) ->
     Prompts = #prompts{},
     case prompt_and_get_pin(Prompts, Data, Call) of
         {ok, Pin} -> % Pin = <<"315">>
             case couch_mgr:get_results(Db, <<"agent/listing_by_pin">>, [{<<"include_docs">>, true}, {<<"key">>, Pin}]) of
-                {ok, []} -> find_agent(Data, Retries-1, Call);
+                {ok, []} ->
+                    #prompts{invalid_pin_prompt=InvalidPinPrompt}=Prompts,
+                    cf_call_command:b_play(InvalidPinPrompt, Call),
+                    find_agent(Data, Retries-1, Call);
                 {ok, [AgentJObj]} -> {ok, AgentJObj};
-                {error, _} -> find_agent(Data, Retries-1, Call)
+                {error, _} ->
+                    #prompts{invalid_pin_prompt=InvalidPinPrompt}=Prompts,
+                    cf_call_command:b_play(InvalidPinPrompt, Call),
+                    find_agent(Data, Retries-1, Call)
             end;
         {error, _} ->
             cf_call_command:hangup(Call)
