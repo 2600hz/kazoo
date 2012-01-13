1cc9f296d290fa8cd5a0a62502bd8cb15a743eb5
karl anderson <karl@2600hz.com>
2012-01-13 04:44:01 +0000

    WHISTLE-467: started a simple tool to preform the system updates

---
+++ whistle_apps/src/whapps_update.erl
@@ -0,0 +1,32 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2011, VoIP INC
+%%% @doc
+%%%
+%%% @end
+%%% Created : 13 Jan 2012 by Karl Anderson <karl@2600hz.org>
+%%%-------------------------------------------------------------------
+-module(whapps_update).
+-include_lib("whistle/include/wh_databases.hrl").
+
+
+-export([run/0]).
+
+
+run() ->
+    couch_mgr:db_delete(<<"crossbar_schemas">>),
+    crossbar:blocking_refresh(),
+    whapps_config:flush(),
+    XbarUpdates = [fun(L) -> lists:delete(<<"cb_cdr">>, L) end
+                   ,fun(L) -> lists:delete(<<"cb_signups">>, L) end
+                   ,fun(L) -> lists:delete(<<"cb_resources">>, L) end
+                   ,fun(L) -> [<<"cb_phone_numbers">> | lists:delete(<<"cb_phone_numbers">>, L)] end
+                   ,fun(L) -> [<<"cb_templates">> | lists:delete(<<"cb_templates">>, L)] end
+                   ,fun(L) -> [<<"cb_onboard">> | lists:delete(<<"cb_onboard">>, L)] end
+                  ],
+    StartModules = whapps_config:get(<<"crossbar">>, <<"autoload_modules">>, []),
+    whapps_config:set_default(<<"crossbar">>
+                          ,<<"autoload_modules">>
+                          ,lists:foldr(fun(F, L) -> F(L) end, StartModules, XbarUpdates)),
+    whapps_controller:stop_app(crossbar),
+    whapps_controller:start_app(crossbar),
+    ok.
