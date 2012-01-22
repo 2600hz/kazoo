e84d7a9f3927e53d8562f0312778ccfba39ceb31
James Aimonetti <james@2600hz.org>
2012-01-21 18:36:53 -0800

    WHISTLE-414: migrate ts documents from the ts db into account DBs

---
+++ whistle_apps/apps/trunkstore/src/trunkstore_maintenance.erl
@@ -0,0 +1,134 @@
+%%%-------------------------------------------------------------------
+%%% @author James Aimonetti <james@2600hz.org>
+%%% @copyright (C) 2012, VoIP INC
+%%% @doc
+%%% 
+%%% @end
+%%% Created : 21 Jan 2012 by James Aimonetti <james@2600hz.org>
+%%%-------------------------------------------------------------------
+-module(trunkstore_maintenance).
+
+-export([migrate/0]).
+
+-include("ts.hrl").
+
+migrate() ->
+    ?LOG("migrating trunkstore information from the ts database"),
+
+    {ok, TSAccts} = couch_mgr:get_results(?TS_DB, <<"ts_accounts/crossbar_listing">>, [{<<"include_docs">>, true}]),
+    ?LOG("trying ~b ts accounts", [length(TSAccts)]),
+
+    [maybe_migrate(wh_json:set_value(<<"_rev">>, <<>>, wh_json:get_value(<<"doc">>, Acct))) || Acct <- TSAccts],
+
+    ?LOG("migration complete").
+
+maybe_migrate(AcctJObj) ->
+    Realm = wh_json:get_value([<<"account">>, <<"auth_realm">>], AcctJObj),
+    case account_exists_with_realm(Realm) of
+        {true, AcctDB, AcctID} ->
+            ?LOG("account with realm ~s exists in ~s", [Realm, AcctDB]),
+            move_doc(AcctDB, AcctID, AcctJObj);
+        false ->
+            ?LOG("account with realm ~s does not exist", [Realm]),
+            case create_account(Realm) of
+                {ok, AcctDB, AcctID} ->
+                    ?LOG("account db ~s created for realm ~s", [AcctDB, Realm]),
+                    move_doc(AcctDB, AcctID, AcctJObj);
+                ignore ->
+                    ?LOG("failed to create an account db for realm ~s", [Realm])
+            end;
+        ignore ->
+            ?LOG("ignoring ts account with realm ~s", [Realm])
+    end.
+
+move_doc(AcctDB, AcctID, TSJObj) ->
+    case has_ts_doc(AcctDB, wh_json:get_value(<<"_id">>, TSJObj)) of
+        true -> ?LOG("looks like trunkstore account has been moved already");
+        false ->
+            create_ts_doc(AcctDB, AcctID, TSJObj),
+            create_limit_doc(AcctDB, AcctID, TSJObj),
+            create_credit_doc(AcctDB, AcctID, TSJObj),
+            whapps_maintenance:refresh(AcctID),
+            ok
+    end.
+
+has_ts_doc(AcctDB, ID) ->
+    case couch_mgr:open_doc(AcctDB, ID) of
+        {ok, _} -> true;
+        _ -> false
+    end.
+
+create_ts_doc(AcctDB, AcctID, TSJObj) ->
+    ?LOG("creating the ts doc in ~s", [AcctDB]),
+    JObj = wh_json:set_values([{<<"pvt_type">>, <<"sys_info">>}
+                               ,{<<"pvt_account_db">>, AcctDB}
+                               ,{<<"pvt_account_id">>, AcctID}
+                              ], wh_json:delete_key(<<"_rev">>, TSJObj)),
+    ?LOG("saving ts doc ~s into ~s", [wh_json:get_value(<<"_id">>, JObj), AcctDB]),
+    {ok, _} = couch_mgr:save_doc(AcctDB, JObj).
+
+create_limit_doc(AcctDB, AcctID, TSJObj) ->
+    JObj = wh_json:from_list([{<<"trunks">>, wh_json:get_integer_value([<<"account">>, <<"trunks">>], TSJObj, 0)}
+                              ,{<<"inbound_trunks">>, wh_json:get_integer_value([<<"account">>, <<"inbound_trunks">>], TSJObj, 0)}
+                              ,{<<"pvt_type">>, <<"limits">>}
+                              ,{<<"pvt_account_db">>, AcctDB}
+                              ,{<<"pvt_account_id">>, AcctID}
+                             ]),
+    {ok, _} = couch_mgr:save_doc(AcctDB, JObj).
+
+create_credit_doc(AcctDB, AcctID, TSJObj) ->    
+    Credit = wh_json:get_value([<<"account">>, <<"credits">>, <<"prepay">>], TSJObj, 0.0),
+    Units = wapi_money:dollars_to_units(wh_util:to_float(Credit)),
+    ?LOG("Putting ~p units", [Units]),
+    Transaction = wh_json:from_list([{<<"amount">>, Units}
+                                     ,{<<"pvt_type">>, <<"credit">>}
+                                     ,{<<"pvt_description">>, <<"initial account balance">>}
+                                     ,{<<"pvt_account_db">>, AcctDB}
+                                     ,{<<"pvt_account_id">>, AcctID}
+                                    ]),
+    couch_mgr:save_doc(AcctDB, Transaction).
+    
+account_exists_with_realm(Realm) ->
+    case couch_mgr:get_results(?WH_ACCOUNTS_DB, <<"accounts/listing_by_realm">>, [{<<"key">>, Realm}]) of
+        {ok, []} -> false;
+        {ok, [AcctObj]} -> {true, wh_json:get_value([<<"value">>, <<"account_db">>], AcctObj), wh_json:get_value([<<"value">>, <<"account_id">>], AcctObj)};
+        {error, _E} ->
+            ?LOG("failed to lookup account view: ~p", [_E]),
+            ignore
+    end.
+
+create_account(Realm) ->
+    AcctID = couch_mgr:get_uuid(),
+    AcctDB = wh_util:format_account_id(AcctID, encoded),
+    case couch_mgr:db_create(AcctDB) of
+        true ->
+            ?LOG("created account db: ~s", [AcctDB]),
+
+            case create_account_doc(Realm, AcctID, AcctDB) of
+                ignore ->
+                    ?LOG("failed to create account doc for ~s", [Realm]),
+                    ignore;
+                _ ->
+                    {ok, AcctDB, AcctID}
+            end;
+        false ->
+            ?LOG("failed to create account db: ~s", [AcctDB]),
+            ignore
+    end.
+
+create_account_doc(Realm, AcctID, AcctDB) ->
+    ?LOG("creating the account doc in ~s and ~s", [AcctDB, ?WH_ACCOUNTS_DB]),
+    Doc = wh_json:from_list([{<<"realm">>, Realm}
+                             ,{<<"pvt_account_id">>, AcctID}
+                             ,{<<"pvt_account_db">>, AcctDB}
+                             ,{<<"pvt_type">>, <<"account">>}
+                             ,{<<"pvt_account_from">>, <<"trunkstore">>}
+                             ,{<<"_id">>, AcctID}
+                            ]),
+    case couch_mgr:save_doc(AcctDB, Doc) of
+        {ok, _} ->
+            {ok, _} = couch_mgr:save_doc(?WH_ACCOUNTS_DB, Doc);
+        {error, _E} ->
+            ?LOG("failed to save account doc into ~s: ~p", [AcctDB, _E]),
+            ignore
+    end.
