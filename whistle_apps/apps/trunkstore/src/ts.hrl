6c636aad0c4acddc71aa6bb006755294f6045566
James Aimonetti <james@2600hz.org>
2012-01-21 17:14:21 -0800

    WHISTLE-414: add wh_databases macros to ts.hrl

--- whistle_apps/apps/trunkstore/src/ts.hrl
+++ whistle_apps/apps/trunkstore/src/ts.hrl
@@ -3,6 +3,7 @@
 -include_lib("whistle/include/wh_types.hrl").
 -include_lib("whistle/include/wh_amqp.hrl").
 -include_lib("whistle/include/wh_log.hrl").
+-include_lib("whistle/include/wh_databases.hrl").
 
 -define(APP_NAME, <<"trunkstore">>).
 -define(APP_VERSION, <<"0.9.0">>).
