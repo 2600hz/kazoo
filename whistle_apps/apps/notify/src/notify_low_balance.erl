3a3e71c1ce3d4bfa8c39a24a00e58717833a67a2
James Aimonetti <james@2600hz.org>
2012-02-03 16:54:18 -0800

    WHISTLE-788: receive low balance notifications

---
+++ whistle_apps/apps/notify/src/notify_low_balance.erl
@@ -0,0 +1,116 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2011, VoIP INC
+%%% @doc
+%%% Renders a custom account email template, or the system default,
+%%% and sends the email to the account admin
+%%% @end
+%%%
+%%% @contributors
+%%% James Aimonetti <james@2600hz.org>
+%%% Karl Anderson <karl@2600hz.org>
+%%%
+%%% Created : 22 Dec 2011 by Karl Anderson <karl@2600hz.org>
+%%%-------------------------------------------------------------------
+-module(notify_low_balance).
+
+-export([init/0, handle_req/2]).
+
+-include("notify.hrl").
+
+-define(DEFAULT_TEXT_TMPL, notify_low_balance_text_tmpl).
+-define(DEFAULT_HTML_TMPL, notify_low_balance_html_tmpl).
+-define(DEFAULT_SUBJ_TMPL, notify_low_balance_subj_tmpl).
+
+-define(MOD_CONFIG_CAT, <<(?NOTIFY_CONFIG_CAT)/binary, ".low_balance">>).
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% initialize the module
+%% @end
+%%--------------------------------------------------------------------
+-spec init/0 :: () -> 'ok'.
+init() ->
+    %% ensure the vm template can compile, otherwise crash the processes
+    notify_util:compile_default_text_template(?DEFAULT_TEXT_TMPL, ?MOD_CONFIG_CAT),
+    notify_util:compile_default_html_template(?DEFAULT_HTML_TMPL, ?MOD_CONFIG_CAT),
+    notify_util:compile_default_subject_template(?DEFAULT_SUBJ_TMPL, ?MOD_CONFIG_CAT),
+    ?LOG_SYS("init done for ~s", [?MODULE]).
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% process the AMQP requests
+%% @end
+%%--------------------------------------------------------------------
+-spec handle_req/2 :: (wh_json:json_object(), proplist()) -> 'ok'.
+handle_req(JObj, _Props) ->
+    true = wapi_notifications:low_balance_v(JObj),
+    whapps_util:put_callid(JObj),
+
+    ?LOG_START("account has a low balance, sending email notification"),
+
+    {ok, Account} = notify_util:get_account_doc(JObj),
+
+    DefaultFrom = list_to_binary([<<"no_reply@">>, wh_util:to_binary(net_adm:localhost())]),
+    From = wh_json:get_value([<<"notifications">>, <<"low_balance">>, <<"send_from">>], Account
+                             ,whapps_config:get(?MOD_CONFIG_CAT, <<"default_from">>, DefaultFrom)),
+
+    ?LOG("creating low_balance notice"),
+    
+    Props = [{<<"From">>, From}
+             |create_template_props(JObj, Account)
+            ],
+
+    CustomTxtTemplate = wh_json:get_value([<<"notifications">>, <<"low_balance">>, <<"email_text_template">>], Account),
+    {ok, TxtBody} = notify_util:render_template(CustomTxtTemplate, ?DEFAULT_TEXT_TMPL, Props),
+
+    CustomHtmlTemplate = wh_json:get_value([<<"notifications">>, <<"low_balance">>, <<"email_html_template">>], Account),
+    {ok, HTMLBody} = notify_util:render_template(CustomHtmlTemplate, ?DEFAULT_HTML_TMPL, Props),
+
+    CustomSubjectTemplate = wh_json:get_value([<<"notifications">>, <<"low_balance">>, <<"email_subject_template">>], Account),
+    {ok, Subject} = notify_util:render_template(CustomSubjectTemplate, ?DEFAULT_SUBJ_TMPL, Props),
+  
+    To = wh_json:get_value([<<"notifications">>, <<"low_balance">>, <<"send_to">>], Account
+                           ,whapps_config:get(?MOD_CONFIG_CAT, <<"default_to">>, <<"">>)),  
+    build_and_send_email(TxtBody, HTMLBody, Subject, To, Props).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% create the props used by the template render function
+%% @end
+%%--------------------------------------------------------------------
+-spec create_template_props/2 :: (wh_json:json_object(), wh_json:json_objects()) -> proplist().
+create_template_props(Event, _Account) ->
+    [{<<"current_balance">>, notify_util:json_to_template_props(Event)}
+    ].
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% process the AMQP requests
+%% @end
+%%--------------------------------------------------------------------
+-spec build_and_send_email/5 :: (iolist(), iolist(), iolist(), ne_binary() | [ne_binary(),...], proplist()) -> 'ok'.
+build_and_send_email(TxtBody, HTMLBody, Subject, To, Props) when is_list(To) ->
+    [build_and_send_email(TxtBody, HTMLBody, Subject, T, Props) || T <- To],
+    ok;
+build_and_send_email(TxtBody, HTMLBody, Subject, To, Props) ->
+    From = props:get_value(<<"From">>, Props),
+    %% Content Type, Subtype, Headers, Parameters, Body
+    Email = {<<"multipart">>, <<"mixed">>
+                 ,[{<<"From">>, From}
+                   ,{<<"To">>, To}
+                   ,{<<"Subject">>, Subject}
+                  ]
+             ,[]
+             ,[{<<"multipart">>, <<"alternative">>, [], []
+                ,[{<<"text">>, <<"plain">>, [{<<"Content-Type">>, <<"text/plain">>}], [], iolist_to_binary(TxtBody)}
+                  ,{<<"text">>, <<"html">>, [{<<"Content-Type">>, <<"text/html">>}], [], iolist_to_binary(HTMLBody)}
+                 ]
+               }
+              ]
+            },
+    notify_util:send_email(From, To, Email),
+    ok.
