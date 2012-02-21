04f4861f10fd9df62d4e597f0c666dd333dd8d36
James Aimonetti <james@2600hz.org>
2012-02-20 23:22:01 -0800

    WHISTLE-626: basics for the fifo whapp

---
+++ whistle_apps/apps/fifo/src/fifo_sup.erl
@@ -0,0 +1,38 @@
+-module(fifo_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0, cache_proc/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+-define(CACHE(Name), {Name, {wh_cache, start_link, [Name]}, permanent, 5000, worker, [wh_cache]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+-spec cache_proc/0 :: () -> {'ok', pid()}.
+cache_proc() ->
+    [P] = [P || {Mod, P, _, _} <- supervisor:which_children(?MODULE),
+                Mod =:= fifo_cache],
+    {ok, P}.
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+init([]) ->
+    {ok, { {one_for_one, 5, 10}, [
+                                  ?CACHE(fifo_cache)
+                                  ,?CHILD(fifo_listener, worker)
+                                  ,?CHILD(fifo_queues_sup, supervisor)
+                                 ]
+         } }.
+
