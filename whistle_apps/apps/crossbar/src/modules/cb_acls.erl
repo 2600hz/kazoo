4d6b6a52f62d37ebe528b2b0a2321a88910e66a3
Edouard Swiac <edouard@2600hz.org>
2012-02-26 02:31:16 +0000

    cb_acls is now a cowboy

--- whistle_apps/apps/crossbar/src/modules/cb_acls.erl
+++ whistle_apps/apps/crossbar/src/modules/cb_acls.erl
@@ -1,26 +1,27 @@
 %%%-------------------------------------------------------------------
-%%% @author Edouard Swiac <edouard@2600hz.org>
+%%% @author Karl Anderson <karl@2600hz.org>
 %%% @copyright (C) 2011, VoIP INC
 %%% @doc
 %%%
-%%% Handle client requests for acl documents
+%%% ACLs from 7 to 77
 %%%
 %%% @end
+%%% Created : 05 Jan 2011 by Karl Anderson <karl@2600hz.org>
+%%% Contributors: Karl Anderson
+%%%               James Aimonetti
+%%%               Edouard Swiac 
 %%%-------------------------------------------------------------------
 -module(cb_acls).
 
--behaviour(gen_server).
+-export([init/0
+         ,allowed_methods/0, allowed_methods/1
+         ,resource_exists/0, resource_exists/1
+         ,validate/1, validate/2
+         ,put/1
+         ,delete/2
+        ]).
 
-%% API
--export([start_link/0]).
-
-%% gen_server callbacks
--export([init/1, handle_call/3, handle_cast/2, handle_info/2,
-         terminate/2, code_change/3]).
-
--include("../../include/crossbar.hrl").
-
--define(SERVER, ?MODULE).
+-include_lib("crossbar/include/crossbar.hrl").
 -define(ECALLMGR, <<"ecallmgr">>).
 -define(ECALLMGR_ACLS, <<"acls">>).
 
@@ -29,232 +30,100 @@
 %%%===================================================================
 
 %%--------------------------------------------------------------------
+%% @public
 %% @doc
-%% Starts the server
-%%
-%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% Initializes the bindings this module will respond to.
 %% @end
 %%--------------------------------------------------------------------
-start_link() ->
-    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
-
-%%%===================================================================
-%%% gen_server callbacks
-%%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Initializes the server
-%%
-%% @spec init(Args) -> {ok, State} |
-%%                     {ok, State, Timeout} |
-%%                     ignore |
-%%                     {stop, Reason}
-%% @end
-%%--------------------------------------------------------------------
-init(_) ->
-    bind_to_crossbar(),
-    {ok, ok}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling call messages
-%%
-%% @spec handle_call(Request, From, State) ->
-%%                                   {reply, Reply, State} |
-%%                                   {reply, Reply, State, Timeout} |
-%%                                   {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, Reply, State} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
-handle_call(_Request, _From, State) ->
-    Reply = ok,
-    {reply, Reply, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling cast messages
-%%
-%% @spec handle_cast(Msg, State) -> {noreply, State} |
-%%                                  {noreply, State, Timeout} |
-%%                                  {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
-handle_cast(_Msg, State) ->
-    {noreply, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%%
-%% @spec handle_info(Info, State) -> {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
-handle_info({binding_fired, Pid, <<"v1_resource.allowed_methods.acls">>, Payload}, State) ->
-    spawn(fun() ->
-                  {Result, Payload1} = allowed_methods(Payload),
-                  Pid ! {binding_result, Result, Payload1}
-          end),
-    {noreply, State};
-
-handle_info({binding_fired, Pid, <<"v1_resource.resource_exists.acls">>, Payload}, State) ->
-    spawn(fun() ->
-                  {Result, Payload1} = resource_exists(Payload),
-                  Pid ! {binding_result, Result, Payload1}
-          end),
-    {noreply, State};
-
-handle_info({binding_fired, Pid, <<"v1_resource.validate.acls">>, [RD, Context | Params]}, State) ->
-    spawn(fun() ->
-                  _ = crossbar_util:put_reqid(Context),
-                  Context1 = validate(Params, Context),
-                  Pid ! {binding_result, true, [RD, Context1, Params]}
-          end),
-    {noreply, State};
-
-handle_info({binding_fired, Pid, <<"v1_resource.execute.put.acls">>, [RD, #cb_context{resp_data=JObj}=Context | Params]}, State) ->
-    spawn(fun() ->
-                  _ = crossbar_util:put_reqid(Context),
-                  Acls = whapps_config:get(?ECALLMGR, ?ECALLMGR_ACLS),
-                  Merged = wh_json:set_value(wh_json:get_value(<<"cidr">>, JObj), JObj, Acls),
-                  whapps_config:set_default(?ECALLMGR, ?ECALLMGR_ACLS, Merged),
-                  wapi_switch:reloadacl(),
-                  Pid ! {binding_result, true, [RD, Context, Params]}
-          end),
-    {noreply, State};
-
-handle_info({binding_fired, Pid, <<"v1_resource.execute.get.acls">>, [RD, Context | Params]}, State) ->
-    spawn(fun() ->
-                  _ = crossbar_util:put_reqid(Context),
-                  Pid ! {binding_result, true, [RD, Context, Params]}
-          end),
-    {noreply, State};
-
-handle_info({binding_fired, Pid, <<"v1_resource.execute.delete.acls">>, [RD, #cb_context{resp_data=Acl}=Context | Params]}, State) ->
-    spawn(fun() ->
-                  _ = crossbar_util:put_reqid(Context),
-                  Cidr =  wh_json:get_value(<<"cidr">>, Acl),
-                  Acls = whapps_config:get(?ECALLMGR, ?ECALLMGR_ACLS),
-                  whapps_config:set_default(?ECALLMGR, ?ECALLMGR_ACLS, wh_json:delete_key(Cidr, Acls)),
-                  Pid ! {binding_result, true, [RD, Context, Params]}
-          end),
-    {noreply, State};
-
-handle_info({binding_fired, Pid, _, Payload}, State) ->
-    Pid ! {binding_result, false, Payload},
-    {noreply, State};
-
-handle_info({binding_flushed, B}, State) ->
-    ?LOG("binding ~s flushed", [B]),
-    {noreply, State};
-
-handle_info(_Info, State) ->
-    ?LOG("unhandled message: ~p", [_Info]),
-    {noreply, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
-%%--------------------------------------------------------------------
-terminate(_Reason, _State) ->
+-spec init/0 :: () -> 'ok'.
+init() ->
+    _ = crossbar_bindings:bind(<<"v1_resource.allowed_methods.acls">>, ?MODULE, allowed_methods),
+    _ = crossbar_bindings:bind(<<"v1_resource.resource_exists.acls">>, ?MODULE, resource_exists),
+    _ = crossbar_bindings:bind(<<"v1_resource.validate.acls">>, ?MODULE, validate),
+    _ = crossbar_bindings:bind(<<"v1_resource.put.acls">>, ?MODULE, put),
+    _ = crossbar_bindings:bind(<<"v1_resource.delete.acls">>, ?MODULE, delete),
     ok.
 
 %%--------------------------------------------------------------------
-%% @private
+%% @public
 %% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% Given the path tokens related to this module, what HTTP methods are
+%% going to be responded to.
 %% @end
 %%--------------------------------------------------------------------
-code_change(_OldVsn, State, _Extra) ->
-    {ok, State}.
-
-%%%===================================================================
-%%% Internal functions
-%%%===================================================================
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function binds this server to the crossbar bindings server,
-%% for the keys we need to consume.
-%% @end
-%%--------------------------------------------------------------------
--spec bind_to_crossbar/0 :: () ->  no_return().
-bind_to_crossbar() ->
-    _ = crossbar_bindings:bind(<<"v1_resource.allowed_methods.acls">>),
-    _ = crossbar_bindings:bind(<<"v1_resource.resource_exists.acls">>),
-    _ = crossbar_bindings:bind(<<"v1_resource.validate.acls">>),
-    crossbar_bindings:bind(<<"v1_resource.execute.#.acls">>).
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function determines the verbs that are appropriate for the
-%% given Nouns.  IE: '/accounts/' can only accept GET and PUT
-%%
-%% Failure here returns 405
-%% @end
-%%--------------------------------------------------------------------
--spec allowed_methods/1 :: (path_tokens()) -> {boolean(), http_methods()}.
-allowed_methods([]) ->
-    {true, ['GET', 'PUT']};
-allowed_methods([_]) ->
-    {true, ['GET', 'DELETE']};
+-spec allowed_methods/0 :: () -> http_methods() | [].
+-spec allowed_methods/1 :: (path_token()) -> http_methods() | [].
+allowed_methods() ->
+    ['GET', 'PUT'].
 allowed_methods(_) ->
-    {false, []}.
+    ['GET', 'DELETE'].
 
 %%--------------------------------------------------------------------
-%% @private
+%% @public
 %% @doc
-%% This function determines if the provided list of Nouns are valid.
-%%
-%% Failure here returns 404
+%% Does the path point to a valid resource
+%% So /acls => []
+%%    /acls/foo => [<<"foo">>]
+%%    /acls/foo/bar => [<<"foo">>, <<"bar">>]
 %% @end
 %%--------------------------------------------------------------------
--spec resource_exists/1 :: (path_tokens()) -> {boolean(), []}.
-resource_exists([]) ->
-    {true, []};
-resource_exists([_]) ->
-    {true, []};
-resource_exists(_) ->
-    {false, []}.
+-spec resource_exists/0 :: () -> boolean().
+-spec resource_exists/1 :: (path_tokens()) -> boolean().
+resource_exists() -> true.
+resource_exists(_) -> true.
 
 %%--------------------------------------------------------------------
-%% @private
+%% @public
 %% @doc
-%% This function determines if the parameters and content are correct
-%% for this request
-%%
-%% Failure here returns 400
+%% Check the request (request body, query string params, path tokens, etc)
+%% and load necessary information.
+%% /acls mights load a list of skel objects
+%% /acls/123 might load the skel object 123
+%% Generally, use crossbar_doc to manipulate the cb_context{} record
 %% @end
 %%--------------------------------------------------------------------
--spec validate/2 :: (path_tokens(), #cb_context{}) -> #cb_context{}.
-validate([], #cb_context{req_verb = <<"get">>}=Context) ->
+-spec validate/1 :: (#cb_context{}) -> #cb_context{}.
+-spec validate/2 :: (#cb_context{}, path_token()) -> #cb_context{}.
+validate(#cb_context{req_verb = <<"get">>}=Context) ->
     summary(Context);
-validate([], #cb_context{req_verb = <<"put">>}=Context) ->
+validate(#cb_context{req_verb = <<"put">>}=Context) ->
     create(Context);
-validate([Id], #cb_context{req_verb = <<"get">>}=Context) ->
-    read(Id, Context);
-validate([Id], #cb_context{req_verb = <<"delete">>}=Context) ->
-    read(Id, Context);
-validate(_, Context) ->
+validate(Context) ->
     crossbar_util:response_faulty_request(Context).
+
+validate(#cb_context{req_verb = <<"get">>}=Context, Id) ->
+    read(Id, Context);
+validate(#cb_context{req_verb = <<"delete">>}=Context, Id) ->
+    read(Id, Context);
+validate(Context, _) ->
+    crossbar_util:response_faulty_request(Context).
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% If the HTTP verib is PUT, execute the actual action, usually a db save.
+%% @end
+%%--------------------------------------------------------------------
+-spec put/1 :: (#cb_context{}) -> #cb_context{}.
+put(#cb_context{req_data=JObj}=Context) ->
+    Acls = whapps_config:get(?ECALLMGR, ?ECALLMGR_ACLS),
+    Merged = wh_json:set_value(wh_json:get_value(<<"cidr">>, JObj), JObj, Acls),
+    whapps_config:set_default(?ECALLMGR, ?ECALLMGR_ACLS, Merged),
+    wapi_switch:reloadacl(),
+    Context.
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% If the HTTP verib is DELETE, execute the actual action, usually a db delete
+%% @end
+%%--------------------------------------------------------------------
+-spec delete/2 :: (#cb_context{}, path_token()) -> #cb_context{}.
+delete(#cb_context{resp_data=Acl}=Context, _) ->
+    Cidr = wh_json:get_value(<<"cidr">>, Acl),
+    Acls = whapps_config:get(?ECALLMGR, ?ECALLMGR_ACLS),
+    whapps_config:set_default(?ECALLMGR, ?ECALLMGR_ACLS, wh_json:delete_key(Cidr, Acls)),
+    Context.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -268,7 +137,7 @@ create(#cb_context{req_data=Data}=Context) ->
         {fail, Errors} ->
             crossbar_util:response_invalid_data(Errors, Context);
         {pass, JObj} ->
-            Context#cb_context{resp_data=JObj, resp_status=success}
+            Context#cb_context{doc=JObj, resp_status=success}
     end.
 
 %%--------------------------------------------------------------------
@@ -279,9 +148,9 @@ create(#cb_context{req_data=Data}=Context) ->
 %%--------------------------------------------------------------------
 -spec read/2 :: (ne_binary(), #cb_context{}) -> #cb_context{}.
 read(Id, Context) ->
-    case whapps_config:get(?ECALLMGR, [?ECALLMGR_ACLS, Id]) of 
+    case whapps_config:get(?ECALLMGR, [?ECALLMGR_ACLS, Id]) of
         undefined -> Context#cb_context{resp_status='error', resp_error_code=404};
-        Acl -> Context#cb_context{resp_data=Acl, resp_status='success'}
+        Acl -> Context#cb_context{resp_data=Acl, resp_status=success}
     end.
 
 %%--------------------------------------------------------------------
@@ -294,4 +163,4 @@ read(Id, Context) ->
 -spec summary/1 :: (#cb_context{}) -> #cb_context{}.
 summary(Context) ->
     Acls = whapps_config:get(?ECALLMGR, ?ECALLMGR_ACLS, wh_json:new()),
-    Context#cb_context{resp_data=Acls, resp_status='success'}.
+    Context#cb_context{resp_data=Acls, resp_status=success}.
