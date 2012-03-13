cc18b111ccf8af4fd7b4893f4e8b97e7c02bd7a5
James Aimonetti <james@2600hz.org>
2012-03-13 00:55:22 -0700

    WHISTLE-626: acdc whapp - the automatic call distribution code

---
+++ whistle_apps/apps/acdc/src/acdc.erl
@@ -0,0 +1,31 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2012 VoIP INC
+%%% @doc
+%%% 
+%%% @end
+%%% @contributors
+%%%   James Aimonetti
+%%%-------------------------------------------------------------------
+-module(acdc).
+
+-export([start/0, start_link/0, stop/0]).
+
+%% @spec start_link() -> {ok,Pid::pid()}
+%% @doc Starts the app for inclusion in a supervisor tree
+start_link() ->
+    _ = start_deps(),
+    registrar_sup:start_link().
+
+%% @spec start() -> ok
+%% @doc Start the app
+start() ->
+    application:start(acdc).
+
+%% @spec stop() -> ok
+%% @doc Stop the basicapp server.
+stop() ->
+    application:stop(acdc).
+
+start_deps() ->
+    whistle_apps_deps:ensure(?MODULE), % if started by the whistle_controller, this will exist
+    [wh_util:ensure_started(App) || App <- [sasl, crypto, whistle_amqp]].
