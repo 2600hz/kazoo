afadf8a4a603b0421d1bd4189e4406518041b75f
James Aimonetti <james@2600hz.org>
2012-04-09 18:02:45 -0700

    WHISTLE-1113: fix cf_play to continue in the callflow if the data in the action isn't properly done, rather than crashing

--- whistle_apps/apps/callflow/src/module/cf_play.erl
+++ whistle_apps/apps/callflow/src/module/cf_play.erl
@@ -1,10 +1,10 @@
 %%%-------------------------------------------------------------------
-%%% @author Karl Anderson <karl@2600hz.org>
-%%% @copyright (C) 2011, VoIP INC
+%%% @copyright (C) 2011-2012, VoIP INC
 %%% @doc
 %%%
 %%% @end
-%%% Created : 10 May 2011 by Karl Anderson <karl@2600hz.org>
+%%% @contributors
+%%%   Karl Anderson
 %%%-------------------------------------------------------------------
 -module(cf_play).
 
@@ -18,17 +18,27 @@
 %% Entry point for this module
 %% @end
 %%--------------------------------------------------------------------
--spec handle/2 :: (wh_json:json_object(), whapps_call:call()) -> ok.
+-spec handle/2 :: (wh_json:json_object(), whapps_call:call()) -> 'ok'.
 handle(Data, Call) ->
     AccountId = whapps_call:account_id(Call),
-    Media = case wh_json:get_value(<<"id">>, Data) of
-                <<"/system_media", _/binary>> = Path -> Path;
-                <<"system_media", _/binary>> = Path -> Path;
-                <<"local_stream://",_/binary>> = Path -> Path;
-                Path ->
-                    lager:debug("prepending media ID with /~s/", [AccountId]),
-                    <<$/, (wh_util:to_binary(AccountId))/binary, $/, Path/binary>>
-            end,
+    case wh_json:get_value(<<"id">>, Data) of
+        undefined ->
+            lager:debug("invalid data in the play callflow"),
+            cf_exe:continue(Call);
+        <<"/system_media", _/binary>> = Path -> play(Data, Call, Path);
+        <<"system_media", _/binary>> = Path -> play(Data, Call, Path);
+        <<"local_stream://",_/binary>> = Path -> play(Data, Call, Path);
+        Path when AccountId =/= undefined ->
+            lager:debug("prepending media ID with /~s/", [AccountId]),
+            Path1 = <<$/, (wh_util:to_binary(AccountId))/binary, $/, Path/binary>>,
+            play(Data, Call, Path1);
+        _Path ->
+            lager:debug("unable to play ~s, as account id is undefined", [_Path]),
+            cf_exe:continue(Call)
+    end.
+
+-spec play/3 :: (wh_json:json_object(), whapps_call:call(), ne_binary()) -> 'ok'.
+play(Data, Call, Media) ->
     case wh_json:is_false(<<"answer">>, Data) of
         true -> ok;
         false -> whapps_call_command:answer(Call)
