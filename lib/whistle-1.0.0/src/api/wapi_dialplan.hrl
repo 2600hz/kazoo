b742817cf7dca1a43e574411e0e306970963e0e8
James Aimonetti <james@2600hz.org>
2012-04-23 11:41:28 -0700

    WHISTLE-1121: fix timeout validator to not unpack the binary version of the timeout value

--- lib/whistle-1.0.0/src/api/wapi_dialplan.hrl
+++ lib/whistle-1.0.0/src/api/wapi_dialplan.hrl
@@ -124,12 +124,12 @@
                                 ]).
 -define(TONE_DETECT_REQ_TYPES, [{<<"On-Success">>, fun is_list/1}
                                 ,{<<"Timeout">>, fun(Timeout) ->
-                                                         T2 = case Timeout of
-                                                                  <<$+, T/binary>> -> T;
-                                                                  T -> T
-                                                              end,
-                                                         try wh_util:to_integer(T2), true
-                                                         catch _:_ -> false
+                                                         %% <<"+123">> converts to 123, so yay!
+                                                         try wh_util:to_integer(Timeout) of
+                                                             T when T < 0 -> false;
+                                                             _ -> true
+                                                         catch
+                                                             _:_ -> false
                                                          end
                                                  end}
                                ]).
