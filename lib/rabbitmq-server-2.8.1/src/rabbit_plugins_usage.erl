b923c1d289e6e3bb00f33464b39fe914dbfc0ff9
James Aimonetti <james@2600hz.org>
2012-04-25 09:56:31 -0700

    WHISTLE-1121: udpate rabbit and attendant libs to 2.8.1

---
+++ lib/rabbitmq-server-2.8.1/src/rabbit_plugins_usage.erl
@@ -0,0 +1,13 @@
+%% Generated, do not edit!
+-module(rabbit_plugins_usage).
+-export([usage/0]).
+usage() -> "Usage:
+rabbitmq-plugins <command> [<command options>] 
+
+Commands:
+    list [-v] [-m] [-E] [-e] [<pattern>]
+    enable <plugin> ...
+    disable <plugin> ...
+
+
+".
