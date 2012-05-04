cb996b486f1e4ad797293d11897fb3d94947ba28
karl anderson <karl@2600hz.com>
2012-05-04 23:33:12 +0000

    WHISTLE-42: hotpatch the cowboy lib to support large base64 file uploads

--- lib/cowboy-0.5.0/src/cowboy_tcp_transport.erl
+++ lib/cowboy-0.5.0/src/cowboy_tcp_transport.erl
@@ -20,7 +20,7 @@
 -module(cowboy_tcp_transport).
 
 -export([name/0, messages/0, listen/1, accept/2, recv/3, send/2, setopts/2,
-	controlling_process/2, peername/1, close/1]).
+        controlling_process/2, peername/1, close/1]).
 
 %% @doc Name of this transport API, <em>tcp</em>.
 -spec name() -> tcp.
@@ -46,61 +46,61 @@ messages() -> {tcp, tcp_closed, tcp_error}.
 %%
 %% @see gen_tcp:listen/2
 -spec listen([{port, inet:port_number()} | {ip, inet:ip_address()}])
-	-> {ok, inet:socket()} | {error, atom()}.
+        -> {ok, inet:socket()} | {error, atom()}.
 listen(Opts) ->
-	{port, Port} = lists:keyfind(port, 1, Opts),
-	Backlog = proplists:get_value(backlog, Opts, 1024),
-	ListenOpts0 = [binary, {active, false},
-		{backlog, Backlog}, {packet, raw}, {reuseaddr, true}],
-	ListenOpts =
-		case lists:keyfind(ip, 1, Opts) of
-			false -> ListenOpts0;
-			Ip -> [Ip|ListenOpts0]
-		end,
-	gen_tcp:listen(Port, ListenOpts).
+        {port, Port} = lists:keyfind(port, 1, Opts),
+        Backlog = proplists:get_value(backlog, Opts, 1024),
+        ListenOpts0 = [binary, {active, false},
+                {backlog, Backlog}, {packet, raw}, {reuseaddr, true}],
+        ListenOpts =
+                case lists:keyfind(ip, 1, Opts) of
+                        false -> ListenOpts0;
+                        Ip -> [Ip|ListenOpts0]
+                end,
+        gen_tcp:listen(Port, ListenOpts).
 
 %% @doc Accept an incoming connection on a listen socket.
 %% @see gen_tcp:accept/2
 -spec accept(inet:socket(), timeout())
-	-> {ok, inet:socket()} | {error, closed | timeout | atom()}.
+        -> {ok, inet:socket()} | {error, closed | timeout | atom()}.
 accept(LSocket, Timeout) ->
-	gen_tcp:accept(LSocket, Timeout).
+        gen_tcp:accept(LSocket, Timeout).
 
 %% @doc Receive a packet from a socket in passive mode.
 %% @see gen_tcp:recv/3
 -spec recv(inet:socket(), non_neg_integer(), timeout())
-	-> {ok, any()} | {error, closed | atom()}.
-recv(Socket, Length, Timeout) ->
-	gen_tcp:recv(Socket, Length, Timeout).
+        -> {ok, any()} | {error, closed | atom()}.
+recv(Socket, Length, _Timeout) ->
+        gen_tcp:recv(Socket, Length, infinity).
 
 %% @doc Send a packet on a socket.
 %% @see gen_tcp:send/2
 -spec send(inet:socket(), iolist()) -> ok | {error, atom()}.
 send(Socket, Packet) ->
-	gen_tcp:send(Socket, Packet).
+        gen_tcp:send(Socket, Packet).
 
 %% @doc Set one or more options for a socket.
 %% @see inet:setopts/2
 -spec setopts(inet:socket(), list()) -> ok | {error, atom()}.
 setopts(Socket, Opts) ->
-	inet:setopts(Socket, Opts).
+        inet:setopts(Socket, Opts).
 
 %% @doc Assign a new controlling process <em>Pid</em> to <em>Socket</em>.
 %% @see gen_tcp:controlling_process/2
 -spec controlling_process(inet:socket(), pid())
-	-> ok | {error, closed | not_owner | atom()}.
+        -> ok | {error, closed | not_owner | atom()}.
 controlling_process(Socket, Pid) ->
-	gen_tcp:controlling_process(Socket, Pid).
+        gen_tcp:controlling_process(Socket, Pid).
 
 %% @doc Return the address and port for the other end of a connection.
 %% @see inet:peername/1
 -spec peername(inet:socket())
-	-> {ok, {inet:ip_address(), inet:port_number()}} | {error, atom()}.
+        -> {ok, {inet:ip_address(), inet:port_number()}} | {error, atom()}.
 peername(Socket) ->
-	inet:peername(Socket).
+        inet:peername(Socket).
 
 %% @doc Close a TCP socket.
 %% @see gen_tcp:close/1
 -spec close(inet:socket()) -> ok.
 close(Socket) ->
-	gen_tcp:close(Socket).
+        gen_tcp:close(Socket).
