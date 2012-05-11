29aad8e25ae7e25d2f04353805a8079d602427ea
James Aimonetti <james@2600hz.org>
2012-05-11 14:38:43 -0700

    WHISTLE-1121: add return type for ibrowse:send_req

--- lib/whistle-1.0.0/include/wh_types.hrl
+++ lib/whistle-1.0.0/include/wh_types.hrl
@@ -96,6 +96,7 @@
 %% Ibrowse-related types
 -type ibrowse_ret() :: {'ok', string(), wh_proplist(), string() | binary()} |
                        {'error', 'req_timedout' | 'sel_conn_closed' | {'EXIT', term()}}.
+
 %% When using the stream_to option, ibrowse:send_req returns this tuple ReqID
 -type ibrowse_req_id() :: {pos_integer(), pos_integer(), pos_integer()}.
 
