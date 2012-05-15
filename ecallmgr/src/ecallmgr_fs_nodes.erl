50a35d0829fd863f50bd50ee9a4e95b7a2a5e360
karl anderson <karl@2600hz.com>
2012-05-15 02:23:26 +0000

    WHISTLE-788: ecallmgr now with accurate channel resource usage tracking and the new wh_cache wait_for_key

--- ecallmgr/src/ecallmgr_fs_nodes.erl
+++ ecallmgr/src/ecallmgr_fs_nodes.erl
@@ -26,6 +26,7 @@
 -export([show_channels/0]).
 -export([new_channel/2]).
 -export([channel_node/1]).
+
 -export([channel_account_summary/1]).
 -export([channel_match_presence/1]).
 -export([channel_exists/1]).
@@ -146,6 +147,7 @@ new_channel(Props, Node) ->
         end,
     gen_server:cast(?MODULE, {new_channel, props_to_channel_record(P, Node)}),
     ecallmgr_call_control:add_leg(P),
+
     Authorized = ecallmgr_authz:maybe_authorize_channel(P, Node),
     wh_cache:store_local(?ECALLMGR_UTIL_CACHE, ?AUTHZ_RESPONSE_KEY(CallId), Authorized).
 
@@ -287,6 +289,8 @@ props_to_channel_record(Props, Node) ->
              ,username=props:get_value(?GET_CCV(<<"Username">>), Props
                                        ,props:get_value(<<"variable_user_name">>, Props))
              ,import_moh=props:get_value(<<"variable_hold_music">>, Props) =:= undefined 
+             ,billing_id=props:get_value(<<"variable_", ?CHANNEL_VAR_PREFIX, "Billing-ID">>, Props)
+             ,bridge_id=props:get_value(<<"variable_", ?CHANNEL_VAR_PREFIX, "Bridge-ID">>, Props)
              ,node=Node
              ,timestamp=wh_util:current_tstamp()
             }.
@@ -309,6 +313,8 @@ channel_record_to_json(Channel) ->
                        ,{<<"reseller_billing">>, Channel#channel.reseller_billing}
                        ,{<<"realm">>, Channel#channel.realm}                                          
                        ,{<<"username">>, Channel#channel.username}
+                       ,{<<"billing_id">>, Channel#channel.billing_id}
+                       ,{<<"bridge_id">>, Channel#channel.bridge_id}
                        ,{<<"node">>, Channel#channel.node}
                        ,{<<"timestamp">>, Channel#channel.timestamp}
                       ]).
@@ -412,6 +418,7 @@ handle_cast({rm_fs_node, Node}, State) ->
     {noreply, rm_fs_node(Node, State), hibernate};
 handle_cast({sync_channels, Node, Channels}, State) ->
     lager:debug("ensuring channel cache is in sync with ~s", [Node]),
+
     MatchSpec = [{#channel{uuid = '$1', node = '$2'}
                   ,[{'=:=', '$2', {const, Node}}]
                   ,['$1']}
@@ -723,8 +730,8 @@ summarize_account_usage([{<<"inbound">>, BillingId, undefined, _, _, _}|Channels
                ],
     summarize_account_usage(Channels, lists:foldr(fun(F, A) -> F(A) end, AStats, Routines));
 summarize_account_usage([{_, BillingId, _, _, _, _}|Channels], AStats) ->
-    Routines = [fun(#astats{billing_ids=I}=A) -> 
+    Routines = [fun(#astats{billing_ids=I}=A) ->
                         A#astats{billing_ids=[BillingId|lists:delete(BillingId, I)]}
-                 end
+                end
                ],
     summarize_account_usage(Channels, lists:foldr(fun(F, A) -> F(A) end, AStats, Routines)).
