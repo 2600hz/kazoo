ce7ce28a7b24d90279c81651db1b686d03f898b4
James Aimonetti <james@2600hz.org>
2012-05-17 15:43:23 -0700

    WHISTLE-1210: rework how we handle content_types_accepted/provided when no Content-Type is sent in the request. Default to application/json when comparing what a module provides, but use undefined when passing the from_* functions back to cowboy. This helps prevent errorneous 413/415 errors.

--- whistle_apps/apps/crossbar/src/v1_resource.erl
+++ whistle_apps/apps/crossbar/src/v1_resource.erl
@@ -257,7 +257,10 @@ default_content_types_accepted(Req, #cb_context{content_types_accepted=CTAs}=Con
                                           end, L) % check each type against the default
           ],
 
-    {CTA, Req, Context}.
+    {ok, Req1} = cowboy_http_req:set_resp_header(<<"X-RFC2616">>
+                                                     ,<<"§14.17 (Try it, you'll like it)">>
+                                                     ,Req),
+    {CTA, Req1, Context}.
 
 -spec content_types_accepted/3 :: (content_type(), #http_req{}, #cb_context{}) -> {[{content_type(), atom()},...], #http_req{}, #cb_context{}}.
 content_types_accepted(CT, Req, #cb_context{content_types_accepted=CTAs}=Context) ->
