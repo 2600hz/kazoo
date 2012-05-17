c4d19f1bf9d7ddff0e4d6d3184d59ccd9f4c6d20
James Aimonetti <james@2600hz.org>
2012-05-16 17:40:22 -0700

    WHISTLE-856: initial callflow module to setup the switch to receive a fax

---
+++ whistle_apps/apps/callflow/src/module/cf_receive_fax.erl
@@ -0,0 +1,37 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2011-2012, VoIP INC
+%%% @doc
+%%% Instructs the switch to receive a fax from the caller
+%%% Stores the fax in the database and optionally emails a configured
+%%% user(s).
+%%% @end
+%%% @contributors
+%%%   James Aimonetti
+%%%-------------------------------------------------------------------
+-module(cf_receive_fax).
+
+-include("../callflow.hrl").
+
+-export([handle/2]).
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Entry point for this module
+%% @end
+%%--------------------------------------------------------------------
+-spec handle/2 :: (wh_json:json_object(), whapps_call:call()) -> 'ok'.
+handle(Data, Call) ->
+    lager:debug("receiving a fax"),
+    whapps_call_command:answer(Call),
+    case whapps_call_command:b_receive_fax(Call) of
+        {ok, JObj} ->
+            lager:debug("rxfax resp: ~p", [JObj]),
+            cf_exe:continue(Call);
+        {error, channel_hungup} ->
+            lager:debug("rxfax hungup prematurely"),
+            cf_exe:stop(Call);
+        _Resp ->
+            lager:debug("rxfax unhandled: ~p", [_Resp]),
+            cf_exe:continue(Call)
+    end.
