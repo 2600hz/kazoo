99a0b8b0a21a59194c124fe784efce61fe470934
karl anderson <karl@2600hz.com>
2012-05-22 20:17:53 +0000

    WHISTLE-960: started work for tracking resellers

--- whistle_apps/apps/crossbar/src/modules/cb_accounts.erl
+++ whistle_apps/apps/crossbar/src/modules/cb_accounts.erl
@@ -56,59 +56,39 @@ get_realm_from_db(DBName) ->
 -spec ensure_parent_set/0 :: () -> 'ok' | {'error', 'no_accounts' | atom()}.
 ensure_parent_set() ->
     case couch_mgr:get_results(?WH_ACCOUNTS_DB, ?AGG_VIEW_REALM, [{<<"include_docs">>, true}]) of
+        {error, _}=E -> E;
         {ok, []} -> {error, no_accounts};
         {ok, AcctJObjs} ->
-            DefaultParentID = find_default_parent(AcctJObjs),
-
-            _ = [ensure_parent_set(DefaultParentID, wh_json:get_value(<<"doc">>, AcctJObj))
-                 || AcctJObj <- AcctJObjs,
-                    wh_json:get_value(<<"id">>, AcctJObj) =/= DefaultParentID, % not the default parent
-
-                    (Tree = wh_json:get_value([<<"doc">>, <<"pvt_tree">>], AcctJObj)) =:= [] orelse % empty tree (should have at least the parent)
-                        Tree =:= <<>> orelse % Tree is an empty string only
-                        Tree =:= [""] orelse % Tree is bound in the prior bit, and might be a list of an empty string
-                        Tree =:= [<<>>] orelse % Tree is a list of an empty string
-                        Tree =:= undefined % if the pvt_tree key doesn't exist
-                ],
-            ok;
-        {error, _}=E -> E
+            case whapps_util:get_master_account_id() of
+                {error, _}=E -> E;
+                {ok, MasterAccountId} -> 
+                    _ = [ensure_parent_set(MasterAccountId, wh_json:get_value(<<"doc">>, AcctJObj))
+                         || AcctJObj <- AcctJObjs,
+                            wh_json:get_value(<<"id">>, AcctJObj) =/= MasterAccountId, % not the default parent                            
+                            (Tree = wh_json:get_value([<<"doc">>, <<"pvt_tree">>], AcctJObj)) =:= [] orelse % empty tree (should have at least the parent)
+                                Tree =:= <<>> orelse % Tree is an empty string only
+                                Tree =:= [""] orelse % Tree is bound in the prior bit, and might be a list of an empty string
+                                Tree =:= [<<>>] orelse % Tree is a list of an empty string
+                                Tree =:= undefined % if the pvt_tree key doesn't exist
+                        ],
+                    ok
+            end
     end.
 
 -spec ensure_parent_set/2 :: (ne_binary(), wh_json:json_object()) -> 'ok' | #cb_context{}.
 ensure_parent_set(DefaultParentID, JObj) ->
     ParentTree = [DefaultParentID, wh_json:get_value(<<"_id">>, JObj)],
     lager:debug("pvt_tree before: ~p after: ~p", [wh_json:get_value(<<"pvt_tree">>, JObj), ParentTree]),
-
     [JObj1] = update_doc_tree(ParentTree
                               ,wh_json:set_values([{<<"id">>, wh_json:get_value(<<"_id">>, JObj)}
                                                    ,{<<"pvt_tree">>, []}
                                                   ], JObj)
                               ,[]),
-
     case couch_mgr:save_doc(?WH_ACCOUNTS_DB, JObj1) of
         {ok, _} ->
             lager:debug("saved ~s with updated tree", [wh_json:get_value(<<"_id">>, JObj)]);
         {error, _E} ->
             lager:debug("failed to save ~s with updated tree: ~p", [wh_json:get_value(<<"_id">>, JObj), _E])
-    end.
-
--spec find_default_parent/1 :: (wh_json:json_objects()) -> ne_binary().
-find_default_parent(AcctJObjs) ->
-    case whapps_config:get(?ACCOUNTS_CONFIG_CAT, <<"default_parent">>) of
-        undefined ->
-            First = hd(AcctJObjs),
-            {_, OldestAcctID} = lists:foldl(fun(AcctJObj, {Created, _}=Eldest) ->
-                                                    case wh_json:get_integer_value([<<"doc">>, <<"pvt_created">>], AcctJObj) of
-                                                        Older when Older < Created  -> {Older, wh_json:get_value(<<"id">>, AcctJObj)};
-                                                        _ -> Eldest
-                                                    end
-                                            end
-                                            ,{wh_json:get_integer_value([<<"doc">>, <<"pvt_created">>], First), wh_json:get_value(<<"id">>, First)}
-                                            ,AcctJObjs),
-            lager:debug("setting default parent account to ~s", [OldestAcctID]),
-            whapps_config:set(?ACCOUNTS_CONFIG_CAT, <<"default_parent">>, OldestAcctID),
-            OldestAcctID;
-        Default -> Default
     end.
 
 %% Bindings callbacks
@@ -269,17 +249,13 @@ delete(Context, AccountId) ->
     DbName = wh_util:format_account_id(AccountId, encoded),
     try
         ok = wh_number_manager:free_numbers(AccountId),
-
         %% Ensure the DB that we are about to delete is an account
         case couch_mgr:open_doc(DbName, AccountId) of
             {ok, JObj1} ->
                 ?PVT_TYPE = wh_json:get_value(<<"pvt_type">>, JObj1),
                 lager:debug("opened ~s in ~s", [DbName, AccountId]),
-
-                ok = unassign_rep(AccountId, JObj1),
-
+                _ = wh_reseller:unassign(JObj1),
                 true = couch_mgr:db_delete(DbName),
-
                 lager:debug("deleted db ~s", [DbName]);
             _ -> ok
         end,
@@ -293,7 +269,7 @@ delete(Context, AccountId) ->
         Context
     catch
         _:_E ->
-            lager:debug("Exception while deleting account: ~p", [_E]),
+            lager:debug("exception while deleting account: ~p", [_E]),
             crossbar_util:response_bad_identifier(AccountId, Context)
     end.
 
@@ -308,8 +284,7 @@ delete(Context, AccountId) ->
 load_account_summary([], Context) ->
     crossbar_doc:load_view(?AGG_VIEW_SUMMARY, [], Context, fun normalize_view_results/2);
 load_account_summary(AccountId, Context) ->
-    crossbar_doc:load_view(?AGG_VIEW_SUMMARY, [
-                                               {<<"startkey">>, [AccountId]}
+    crossbar_doc:load_view(?AGG_VIEW_SUMMARY, [{<<"startkey">>, [AccountId]}
                                                ,{<<"endkey">>, [AccountId, wh_json:new()]}
                                               ], Context, fun normalize_view_results/2).
 
@@ -322,19 +297,12 @@ load_account_summary(AccountId, Context) ->
 -spec create_account/1 :: (#cb_context{}) -> #cb_context{}.
 -spec create_account/2 :: (#cb_context{}, 'undefined' | ne_binary()) -> #cb_context{}.
 create_account(Context) ->
-    P = case whapps_config:get(?ACCOUNTS_CONFIG_CAT, <<"default_parent">>) of
-            undefined ->
-                case couch_mgr:get_results(?WH_ACCOUNTS_DB, ?AGG_VIEW_SUMMARY, [{<<"include_docs">>, true}]) of
-                    {ok, [_|_]=AcctJObjs} ->
-                        ParentId = find_default_parent(AcctJObjs),
-                        whapps_config:set(?ACCOUNTS_CONFIG_CAT, <<"default_parent">>, ParentId),
-                        ParentId;
-                    _ -> undefined
-                end;
-            ParentId ->
-                ParentId
-        end,
-    create_account(Context, P).
+    case whapps_util:get_master_account_id() of
+        {ok, MasterAccountId} -> create_account(Context, MasterAccountId);
+        {error, _R} ->
+            lager:debug("unabled to determine the system admin account: ~p", [_R]),
+            create_account(Context, undefined)
+    end.
 
 create_account(#cb_context{req_data=ReqData}=Context, ParentId) ->
     Data = case wh_json:get_ne_value(<<"realm">>, ReqData) of
@@ -668,10 +636,10 @@ create_new_account_db(#cb_context{doc=Doc}=Context) ->
     end,
     case couch_mgr:db_create(AccountDb) of
         false ->
-            lager:debug("Failed to create database: ~s", [AccountDb]),
+            lager:debug("failed to create database: ~s", [AccountDb]),
             crossbar_util:response_db_fatal(Context);
         true ->
-            lager:debug("Created DB for account id ~s", [AccountId]),
+            lager:debug("created DB for account id ~s", [AccountId]),
             Generators = [fun(J) -> wh_json:set_value(<<"_id">>, AccountId, J) end
                           ,fun(J) -> wh_json:set_value(<<"pvt_account_db">>, AccountDb, J) end
                           ,fun(J) -> wh_json:set_value(<<"pvt_account_id">>, AccountId, J) end
@@ -687,22 +655,11 @@ create_new_account_db(#cb_context{doc=Doc}=Context) ->
                     whapps_maintenance:refresh(AccountDb),
                     _ = crossbar_bindings:map(<<"account.created">>, Context1),
                     _ = couch_mgr:ensure_saved(?WH_ACCOUNTS_DB, JObj),
-                    assign_rep(AccountId, JObj),
-                    Credit = whapps_config:get(<<"crossbar.accounts">>, <<"starting_credit">>, 0.0),
-                    Units = wapi_money:dollars_to_units(wh_util:to_float(Credit)),
-                    lager:debug("Putting ~p units", [Units]),
-                    Transaction = wh_json:from_list([{<<"amount">>, Units}
-                                                     ,{<<"pvt_type">>, <<"credit">>}
-                                                     ,{<<"pvt_description">>, <<"initial account balance">>}
-                                                    ]),
-                    case crossbar_doc:save(Context#cb_context{doc=Transaction, db_name=AccountDb}) of
-                        #cb_context{resp_status=success} -> ok;
-                        #cb_context{resp_error_msg=Err} -> lager:debug("failed to save credit doc: ~p", [Err])
-                    end,
-                    notfy_new_account(Context1),
+                    _ = wh_reseller:assign(JObj),
+                    _ = notfy_new_account(Context1),
                     Context1;
                 Else ->
-                    lager:debug("Other PUT resp: ~s: ~p~n", [Else#cb_context.resp_status, Else#cb_context.doc]),
+                    lager:debug("other PUT resp: ~s: ~p~n", [Else#cb_context.resp_status, Else#cb_context.doc]),
                     Else
             end
     end.
@@ -742,68 +699,6 @@ is_unique_realm(AccountId, Context, Realm) ->
             true;
         _ ->
             is_unique_realm(undefined, Context, Realm)
-    end.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Attempt to assign to an account rep in the parent account
-%% @end
-%%--------------------------------------------------------------------
--spec assign_rep/2 :: (ne_binary(), wh_json:json_object()) -> ok.
-assign_rep(AccountId, JObj) ->
-    case wh_json:get_value(<<"pvt_tree">>, JObj, []) of
-        [] ->
-            lager:debug("failed to find a pvt_tree for sub account assignment"),
-            ok;
-        Tree ->
-            Parent = lists:last(Tree),
-            ParentDb = wh_util:format_account_id(Parent, encoded),
-            ViewOptions = [{<<"limit">>, 1}
-                           ,{<<"include_docs">>, true}
-                          ],
-            case couch_mgr:get_results(ParentDb, <<"sub_account_reps/count_assignments">>, ViewOptions) of
-                {ok, [Results]} ->
-                    Rep = wh_json:get_value(<<"doc">>, Results),                    
-                    Assignments = wh_json:get_value(<<"pvt_sub_account_assignments">>, Rep, []), 
-                    couch_mgr:save_doc(ParentDb, wh_json:set_value(<<"pvt_sub_account_assignments">>, [AccountId|Assignments], Rep)),
-                    ok;
-                _E ->
-                    lager:debug("failed to find sub account reps: ~p", [_E]),
-                    ok
-            end
-    end.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Attempt to remove any assignments to an account rep in the parent
-%% account
-%% @end
-%%--------------------------------------------------------------------
--spec unassign_rep/2 :: (ne_binary(), wh_json:json_object()) -> ok.
-unassign_rep(AccountId, JObj) ->
-    case wh_json:get_value(<<"pvt_tree">>, JObj, []) of
-        [] -> ok;
-        Tree ->
-            Parent = lists:last(Tree),
-            ParentDb = wh_util:format_account_id(Parent, encoded),
-            ViewOptions = [{<<"include_docs">>, true}
-                           ,{<<"key">>, AccountId}],
-            case couch_mgr:get_results(ParentDb, <<"sub_account_reps/find_assignments">>, ViewOptions) of
-                {ok, Results} ->
-                    _ = [begin
-                             Rep = wh_json:get_value(<<"doc">>, Result),
-                             Assignments = wh_json:get_value(<<"pvt_sub_account_assignments">>, Rep, []),                          
-                             couch_mgr:save_doc(ParentDb
-                                                ,wh_json:set_value(<<"pvt_sub_account_assignments">>
-                                                                   ,lists:delete(AccountId, Assignments)
-                                                                   ,Rep)
-                                               )
-                         end || Result <- Results],
-                    ok;
-                _E -> ok
-            end
     end.
 
 %%--------------------------------------------------------------------
