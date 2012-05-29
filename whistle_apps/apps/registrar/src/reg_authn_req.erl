83f9d1a270085b009ee48dc68f49b8b36df19f2f
James Aimonetti <james@2600hz.org>
2012-05-29 15:21:38 -0700

    couchbeam-0.7.x: update module header and update lager lines to go to info (temp)

--- whistle_apps/apps/registrar/src/reg_authn_req.erl
+++ whistle_apps/apps/registrar/src/reg_authn_req.erl
@@ -1,10 +1,10 @@
 %%%-------------------------------------------------------------------
-%%% @author James Aimonetti <james@2600hz.org>
-%%% @copyright (C) 2011, VoIP INC
+%%% @copyright (C) 2011-2012, VoIP INC
 %%% @doc
 %%% Handle authn_req messages
 %%% @end
-%%% Created : 19 Aug 2011 by James Aimonetti <james@2600hz.org>
+%%% @contributors
+%%%   James Aimonetti
 %%%-------------------------------------------------------------------
 -module(reg_authn_req).
 
@@ -21,7 +21,7 @@ handle_req(ApiJObj, _Props) ->
 
     put(callid, wh_json:get_value(<<"Msg-ID">>, ApiJObj, <<"000000000000">>)),
 
-    lager:debug("received SIP authentication request"),
+    lager:info("received SIP authentication request"),
 
     AuthU = wapi_authn:get_auth_user(ApiJObj),
     AuthR = wapi_authn:get_auth_realm(ApiJObj),
