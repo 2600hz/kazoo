0b056e65bb1ba03c9f4a414d4e55ced3379a84e7
James Aimonetti <james@2600hz.org>
2012-05-30 05:05:15 -0700

    couchbeam-0.7.x: spec updates, as well as option updates to couch_mgr:get_results calls

--- whistle_apps/apps/stepswitch/src/stepswitch_maintenance.erl
+++ whistle_apps/apps/stepswitch/src/stepswitch_maintenance.erl
@@ -32,18 +32,22 @@ flush() ->
 %% Lookup a number in the route db and return the account ID if known
 %% @end
 %%--------------------------------------------------------------------
--spec refresh/0 :: () -> ok.
+-spec refresh/0 :: () -> 'ok'.
 refresh() ->
     lager:debug("ensuring database ~s exists", [?RESOURCES_DB]),
     couch_mgr:db_create(?RESOURCES_DB),
     Views = whapps_util:get_views_json(stepswitch, "views"),
     whapps_util:update_views(?RESOURCES_DB, Views, true),
-    case couch_mgr:all_docs(?RESOURCES_DB, [{<<"include_docs">>, true}]) of
+    case couch_mgr:all_docs(?RESOURCES_DB, [include_docs]) of
         {ok, JObjs} ->
-            _ = [couch_mgr:del_doc(?RESOURCES_DB, wh_json:get_value(<<"doc">>, JObj)) 
-                 || JObj <- JObjs
-                        ,wh_json:get_value([<<"doc">>, <<"pvt_type">>], JObj) =:= <<"route">>
-                ],
+            _ = couch_mgr:del_docs(?RESOURCES_DB
+                                   ,[Doc
+                                     || JObj <- JObjs,
+                                        begin
+                                            Doc = wh_json:get_value(<<"doc">>, JObj),
+                                            wh_json:get_value(<<"pvt_type">>, Doc) =:= <<"route">>
+                                        end
+                                    ]),
             ok;
         {error, _} ->
             ok
@@ -55,7 +59,7 @@ refresh() ->
 %% Lookup a number in the route db and return the account ID if known
 %% @end
 %%--------------------------------------------------------------------
--spec lookup_number/1 :: (string()) -> {ok, binary()} | {error, atom()}.
+-spec lookup_number/1 :: (string()) -> {'ok', binary()} | {'error', atom()}.
 lookup_number(Number) ->
     gen_server:call(stepswitch_listener, {lookup_number, Number}).
 
@@ -66,7 +70,7 @@ lookup_number(Number) ->
 %% refresh the cache.
 %% @end
 %%--------------------------------------------------------------------
--spec reload_resources/0 :: () -> ok.
+-spec reload_resources/0 :: () -> 'ok'.
 reload_resources() ->
     gen_server:call(stepswitch_listener, {reload_resrcs}).
 
@@ -78,8 +82,8 @@ reload_resources() ->
 %% {Resource ID, Delay (in seconds), SIP URI}
 %% @end
 %%--------------------------------------------------------------------
--spec process_number/1 :: (string()) -> list() | {error, atom()}.
--spec process_number/2 :: (string(), list()) -> list() | {error, atom()}.
+-spec process_number/1 :: (string()) -> list() | {'error', atom()}.
+-spec process_number/2 :: (string(), list()) -> list() | {'error', atom()}.
 
 process_number(Number) ->
     gen_server:call(stepswitch_listener, {process_number, Number}).
