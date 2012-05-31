288dc2f5897c90403328c8a3a99921dca6966a2e
James Aimonetti <james@2600hz.org>
2012-05-31 11:25:35 -0700

    WHISTLE-1121: update couch_mgr:get_results/3 options list and remove unneeded include in ts.hrl

--- whistle_apps/apps/trunkstore/src/ts.hrl
+++ whistle_apps/apps/trunkstore/src/ts.hrl
@@ -1,4 +1,3 @@
--include_lib("couchbeam/include/couchbeam.hrl").
 -include_lib("amqp_client/include/amqp_client.hrl").
 -include_lib("whistle/include/wh_types.hrl").
 -include_lib("whistle/include/wh_amqp.hrl").
