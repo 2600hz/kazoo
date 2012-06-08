abd1186234d38bfa0724616d6914b787917d9c77
James Aimonetti <james@2600hz.org>
2012-06-07 17:36:43 -0700

    WHISTLE-1121: when looking up the URL for media, use the Play command's json as the base object, and write the media_req parameters over the play json

--- ecallmgr/src/ecallmgr_media_registry.erl
+++ ecallmgr/src/ecallmgr_media_registry.erl
@@ -12,7 +12,7 @@
 -behaviour(gen_server).
 
 %% API
--export([start_link/0, lookup_media/2, lookup_media/3,
+-export([start_link/0, lookup_media/3, lookup_media/4,
          register_local_media/2, register_local_media/3, is_local/2]).
 
 %% gen_server callbacks
@@ -50,12 +50,14 @@ register_local_media(<<"local_stream://", FSPath/binary>>, _, _) ->
 register_local_media(MediaName, CallId, Version) when Version =:= path orelse Version =:= url ->
     gen_server:call(?MODULE, {register_local_media, MediaName, CallId, Version}).
 
--spec lookup_media/2 :: (ne_binary(), ne_binary()) -> {'ok', ne_binary()} | {'error', 'not_local'}.
--spec lookup_media/3 :: (ne_binary() , 'extant' | 'new', ne_binary()) -> {'ok', ne_binary()} | {'error', 'not_local'}.
-lookup_media(MediaName, CallId) ->
-    request_media(MediaName, new, CallId).
-lookup_media(MediaName, Type, CallId) ->
-    request_media(MediaName, Type, CallId).
+-spec lookup_media/3 :: (ne_binary(), ne_binary(), wh_json:json_object()) -> {'ok', ne_binary()} |
+                                                                             {'error', 'not_local'}.
+-spec lookup_media/4 :: (ne_binary() , 'extant' | 'new', ne_binary(), wh_json:json_object()) -> {'ok', ne_binary()} |
+                                                                                                {'error', 'not_local'}.
+lookup_media(MediaName, CallId, JObj) ->
+    request_media(MediaName, new, CallId, JObj).
+lookup_media(MediaName, Type, CallId, JObj) ->
+    request_media(MediaName, Type, CallId, JObj).
 
 -spec is_local/2 :: (ne_binary(), ne_binary()) -> {'ok', ne_binary()} | {'error', 'not_local'}.
 is_local(MediaName, CallId) ->
@@ -239,32 +241,37 @@ generate_local_path(MediaName) ->
     M = wh_util:to_binary(MediaName),
     <<?LOCAL_MEDIA_PATH, M/binary>>.
 
--spec request_media/3 :: (ne_binary(), 'extant' | 'new', ne_binary()) -> {'ok', ne_binary()} | {'error', 'not_local'}.
-request_media(MediaName, Type, CallID) ->
+-spec request_media/4 :: (ne_binary(), 'extant' | 'new', ne_binary(), wh_json:json_object()) -> {'ok', ne_binary()} |
+                                                                                                {'error', 'not_local'}.
+request_media(MediaName, Type, CallID, JObj) ->
     case gen_server:call(?MODULE, {lookup_local, MediaName, CallID}, infinity) of
         {ok, Path} ->
             {ok, Srv} = ecallmgr_shout_sup:start_srv(Path),
             Url = ecallmgr_shout:get_srv_url(Srv),
             {ok, Url};
         {error, _} ->
-            lookup_remote(MediaName, Type, CallID)
+            lookup_remote(MediaName, Type, CallID, JObj)
     end.
 
-lookup_remote(MediaName, extant, CallID) ->
-    Request = [{<<"Media-Name">>, MediaName}
-               ,{<<"Stream-Type">>, <<"extant">>}
-               ,{<<"Call-ID">>, CallID}
-               ,{<<"Msg-ID">>, wh_util:to_binary(wh_util:current_tstamp())}
-               | wh_api:default_headers(<<>>, <<"media">>, <<"media_req">>, ?APP_NAME, ?APP_VERSION)
-              ],
+lookup_remote(MediaName, extant, CallID, JObj) ->
+    Request = wh_json:set_values(
+                [{<<"Media-Name">>, MediaName}
+                 ,{<<"Stream-Type">>, <<"extant">>}
+                 ,{<<"Call-ID">>, CallID}
+                 ,{<<"Msg-ID">>, wh_util:to_binary(wh_util:current_tstamp())}
+                 | wh_api:default_headers(<<>>, <<"media">>, <<"media_req">>, ?APP_NAME, ?APP_VERSION)
+                ]
+                ,JObj),
     lookup_remote(MediaName, Request);
-lookup_remote(MediaName, new, CallID) ->
-    Request = [{<<"Media-Name">>, MediaName}
-               ,{<<"Stream-Type">>, <<"new">>}
-               ,{<<"Call-ID">>, CallID}
-               ,{<<"Msg-ID">>, wh_util:to_binary(wh_util:current_tstamp())}
-               | wh_api:default_headers(<<>>, <<"media">>, <<"media_req">>, ?APP_NAME, ?APP_VERSION)
-              ],
+lookup_remote(MediaName, new, CallID, JObj) ->
+    Request = wh_json:set_values(
+                [{<<"Media-Name">>, MediaName}
+                 ,{<<"Stream-Type">>, <<"new">>}
+                 ,{<<"Call-ID">>, CallID}
+                 ,{<<"Msg-ID">>, wh_util:to_binary(wh_util:current_tstamp())}
+                 | wh_api:default_headers(<<>>, <<"media">>, <<"media_req">>, ?APP_NAME, ?APP_VERSION)
+                ]
+                ,JObj),
     lookup_remote(MediaName, Request).
 
 -spec lookup_remote/2 :: (ne_binary(), proplist()) -> {'ok', ne_binary()} | {'error', 'not_local'}.
