c509391a94298e4525d3c24b097ae9f3151a625e
James Aimonetti <james@2600hz.org>
2012-06-15 16:06:52 -0700

    WHISTLE-1186: pivot whapp beginnings

---
+++ whistle_apps/apps/pivot/src/pivot.hrl
@@ -0,0 +1,10 @@
+-ifndef(PIVOT_HRL).
+
+%% Typical includes needed
+-include_lib("whistle/include/wh_amqp.hrl").
+-include_lib("whistle/include/wh_types.hrl").
+-include_lib("whistle/include/wh_log.hrl").
+-include_lib("whistle/include/wh_databases.hrl").
+
+-define(PIVOT_HRL, true).
+-endif.
