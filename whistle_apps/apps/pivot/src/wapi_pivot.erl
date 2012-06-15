7beac19ee7fac2170b2c399b0348c1697384bac7
James Aimonetti <james@2600hz.org>
2012-06-15 16:06:52 -0700

    WHISTLE-1186: pivot whapp beginnings

---
+++ whistle_apps/apps/pivot/src/wapi_pivot.erl
@@ -0,0 +1,71 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2012, VoIP INC
+%%% @doc
+%%% Pivot API
+%%% @end
+%%% @contributors
+%%%   James Aimonetti
+%%%-------------------------------------------------------------------
+-module(wapi_pivot).
+
+-include("pivot.hrl").
+
+-export([req/1, req_v/1
+         ,bind_q/2
+         ,unbind_q/2
+         ,publish_req/1, publish_req/2
+        ]).
+
+-define(KEY_PIVOT_REQ, <<"pivot.req">>).
+
+-define(PIVOT_REQ_HEADERS, [<<"Call">>]).
+-define(OPTIONAL_PIVOT_REQ_HEADERS, []).
+-define(PIVOT_REQ_VALUES, []).
+-define(PIVOT_REQ_TYPES, [{<<"Call">>, fun wh_json:is_json_object/1}]).
+
+-spec req/1 :: (api_terms()) -> {'ok', iolist()} |
+                                {'error', string()}.
+req([_|_]=Prop) ->
+    case req_v(Prop) of
+        false -> {error, "Proplist failed validation for pivot_req"};
+        true -> wh_api:build_message(Prop, ?PIVOT_REQ_HEADERS, ?OPTIONAL_PIVOT_REQ_HEADERS)
+    end;
+req(JObj) ->
+    req(wh_json:to_proplist(JObj)).
+
+-spec req_v/1 :: (api_terms()) -> boolean().
+req_v([_|_]=Prop) ->
+    wh_api:validate(Prop, ?PIVOT_REQ_HEADERS, ?PIVOT_REQ_VALUES, ?PIVOT_REQ_TYPES);
+req_v(JObj) ->
+    req_v(wh_json:to_proplist(JObj)).
+
+-spec bind_q/2 :: (ne_binary(), proplist()) -> 'ok'.
+bind_q(Queue, Props) ->
+    Realm = props:get_value(realm, Props, <<"*">>),
+
+    amqp_util:callmgr_exchange(),
+    amqp_util:bind_q_to_callmgr(Queue, get_pivot_req_routing(Realm)).
+
+-spec unbind_q/2 :: (ne_binary(), proplist()) -> 'ok'.
+unbind_q(Queue, Props) ->
+    Realm = props:get_value(realm, Props, <<"*">>),
+    amqp_util:unbind_q_from_callmgr(Queue, get_pivot_req_routing(Realm)).
+
+get_pivot_req_routing(Realm) when is_binary(Realm) ->
+    list_to_binary([?KEY_PIVOT_REQ, ".", amqp_util:encode(Realm)]);
+get_pivot_req_routing(Api) ->
+    get_route_req_routing(get_from_realm(Api)).
+
+-spec publish_req/1 :: (api_terms()) -> 'ok'.
+-spec publish_req/2 :: (api_terms(), binary()) -> 'ok'.
+publish_req(JObj) ->
+    publish_req(JObj, ?DEFAULT_CONTENT_TYPE).
+publish_req(Req, ContentType) ->
+    {ok, Payload} = wh_api:prepare_api_payload(Req, ?PIVOT_REQ_VALUES, fun req/1),
+    amqp_util:callmgr_publish(Payload, ContentType, get_pivot_req_routing(Req)).
+
+-spec get_from_realm/1 :: (api_terms()) -> ne_binary().
+get_from_realm([_|_]=Prop) ->
+    props:get_value(<<"From-Realm">>, Prop);
+get_from_realm(JObj) ->
+    wh_json:get_value(<<"From-Realm">>, JObj).
