2fd7e6b65d870df09cc6d374eb37e8a578cb7db4
James Aimonetti <james@2600hz.org>
2012-06-18 17:47:59 -0700

    WHISTLE-1186: update the chlid list

--- whistle_apps/apps/pivot/src/pivot_sup.erl
+++ whistle_apps/apps/pivot/src/pivot_sup.erl
@@ -18,7 +18,7 @@
 -define(CHILD(Name, Type), fun(N, cache) -> {N, {wh_cache, start_link, [N]}, permanent, 5000, worker, [wh_cache]};
                               (N, T) -> {N, {N, start_link, []}, permanent, 5000, T, [N]} end(Name, Type)).
 -define(CHILDREN, [{?PIVOT_CACHE, cache}
-                   ,{pivot_call_sup, supervisor}
+                   ,{pivot_calls_sup, supervisor}
                    ,{pivot_listener, worker}
                   ]).
 
