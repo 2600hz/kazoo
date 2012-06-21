a8111b86a4baa7a54c4ec71788a6171c60580a5e
James Aimonetti <james@2600hz.org>
2012-06-20 22:58:09 -0700

    WHISTLE-856: listener for fax requests, supervisor for those requests, the request processor module, and a wapi module for faxing

---
+++ whistle_apps/apps/fax/src/fax_requests_sup.erl
@@ -0,0 +1,70 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2012, VoIP INC
+%%% @doc
+%%%
+%%% @end
+%%% @contributors
+%%%   James Aimonetti
+%%%-------------------------------------------------------------------
+-module(fax_requests_sup).
+
+-behaviour(supervisor).
+
+-include("fax.hrl").
+
+%% API
+-export([start_link/0]).
+-export([new/2]).
+-export([workers/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(Name, Restart, Shutdown, Type),
+        {Name, {Name, start_link, []}, Restart, Shutdown, Type, [Name]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Starts the supervisor
+%% @end
+%%--------------------------------------------------------------------
+-spec start_link/0 :: () -> startlink_ret().
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+-spec new/2 :: (whapps_call:call(), wh_json:json_object()) -> sup_startchild_ret().
+new(Call, JObj) ->
+    supervisor:start_child(?MODULE, [Call, JObj]).
+
+-spec workers/0 :: () -> [pid(),...] | [].
+workers() ->
+    [ Pid || {_, Pid, worker, [_]} <- supervisor:which_children(?MODULE)].
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Whenever a supervisor is started using supervisor:start_link/[2,3],
+%% this function is called by the new process to find out about
+%% restart strategy, maximum restart frequency and child
+%% specifications.
+%% @end
+%%--------------------------------------------------------------------
+-spec init([]) -> sup_init_ret().
+init([]) ->
+    RestartStrategy = simple_one_for_one,
+    MaxRestarts = 0,
+    MaxSecondsBetweenRestarts = 1,
+
+    SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+
+    {ok, {SupFlags, [?CHILD(fax_request, temporary, 2000, worker)]}}.
