7cc84de4fd78f26556dc50ee6966990b02e7b787
karl anderson <karl@2600hz.com>
2012-06-23 18:41:21 +0000

    WHISTLE-1309: placeholders for some other carriers clients are using

---
+++ lib/whistle_number_manager-1.0.0/src/carriers/wnm_other.erl
@@ -0,0 +1,44 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2011, VoIP INC
+%%% @doc
+%%%
+%%% Handle client requests for phone_number documents
+%%%
+%%% @end
+%%%-------------------------------------------------------------------
+-module(wnm_other).
+
+-export([find_numbers/2]).
+-export([acquire_number/1]).
+-export([disconnect_number/1]).
+
+-include("../wh_number_manager.hrl").
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Query the local system for a quanity of available numbers
+%% in a rate center
+%% @end
+%%--------------------------------------------------------------------
+-spec find_numbers/2 :: (ne_binary(), pos_integer()) -> {'error', _}.
+find_numbers(_Number, _Quanity) ->
+    {error, non_available}.
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Acquire a given number from the carrier
+%% @end
+%%--------------------------------------------------------------------
+-spec acquire_number/1 :: (wnm_number()) -> wnm_number().
+acquire_number(Number) -> Number.
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Release a number from the routing table
+%% @end
+%%--------------------------------------------------------------------
+-spec disconnect_number/1 :: (wnm_number()) -> wnm_number().
+disconnect_number(Number) -> Number.
