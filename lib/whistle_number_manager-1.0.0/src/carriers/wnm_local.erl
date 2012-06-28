c352b41d44361208b0961fd677bf3bff7ffb7a33
James Aimonetti <james@2600hz.org>
2012-06-27 18:24:52 -0700

    WHISTLE-620: more dialyzer/spec updates

--- lib/whistle_number_manager-1.0.0/src/carriers/wnm_local.erl
+++ lib/whistle_number_manager-1.0.0/src/carriers/wnm_local.erl
@@ -22,8 +22,8 @@
 %% in a rate center
 %% @end
 %%--------------------------------------------------------------------
--spec find_numbers/2 :: (ne_binary(), pos_integer()) -> {'ok', wh_json:json_object()} |
-                                                        {'error', _}.
+-spec find_numbers/2 :: (ne_binary(), pos_integer()) -> %{'ok', wh_json:json_object()} |
+                                                        {'error', 'non_available'}.
 find_numbers(Number, Quanity) when size(Number) < 5 ->
     find_numbers(<<"+1", Number/binary>>, Quanity);
 find_numbers(_Number, _Quanity) ->
