79291043c236175f5a1220ca915319d5d9ad07e4
James Aimonetti <james@2600hz.org>
2012-07-11 17:44:47 -0700

    WHISTLE-1186: start to support ad-hoc conferencing by including the conf config (for this participant) in the discovery req

--- lib/whistle-1.0.0/src/api/wapi_conference.erl
+++ lib/whistle-1.0.0/src/api/wapi_conference.erl
@@ -3,7 +3,8 @@
 %%% @doc
 %%% 
 %%% @end
-%%% Created : 15 Feb 2012 by Karl Anderson <karl@2600hz.org>
+%%% @contributors
+%%%   Karl Anderson
 %%%-------------------------------------------------------------------
 -module(wapi_conference).
 
@@ -79,7 +80,9 @@
 
 %% Conference Discovery Request
 -define(DISCOVERY_REQ_HEADERS, [<<"Call">>]).
--define(OPTIONAL_DISCOVERY_REQ_HEADERS, [<<"Conference-ID">>, <<"Moderator">>]).
+-define(OPTIONAL_DISCOVERY_REQ_HEADERS, [<<"Conference-ID">>, <<"Moderator">>
+                                             ,<<"Conference-Definition">> % ad-hoc conferencing
+                                        ]).
 -define(DISCOVERY_REQ_VALUES, [{<<"Event-Category">>, <<"conference">>}
                                ,{<<"Event-Name">>, <<"discovery_req">>}
                                ,{<<"Moderator">>, [<<"true">>, <<"false">>]}
