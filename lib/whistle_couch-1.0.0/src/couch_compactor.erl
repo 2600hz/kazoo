4140aba8c7e85ec6fc25ff1338dc51338ac52365
James Aimonetti <james@2600hz.org>
2012-07-16 18:07:11 -0700

    WHISTLE-1388: remove all the conflict strategy code.
    load shards by accessing the admin conn's 'dbs' db to get a list of DBs, and covert the db name into the shards name. This is much speedier than asking for all the DBs from admin conn and filtering from there.

--- lib/whistle_couch-1.0.0/src/couch_compactor.erl
+++ lib/whistle_couch-1.0.0/src/couch_compactor.erl
@@ -16,15 +16,6 @@
          ,compact_db/2
         ]).
 
-%% Conflict resolution-enabled API
--export([compact_all/1
-         ,compact_all/2
-         ,compact_node/2
-         ,compact_node/3
-         ,compact_db/3
-         ,compact_db/4
-        ]).
-
 %% internal API functions
 -export([compact_shard/3]).
 
@@ -44,55 +35,31 @@ init(Parent) ->
 
     lager:debug("starting compactor"),
 
-    case {couch_config:fetch(<<"compact_automatically">>, false), couch_config:fetch(<<"conflict_strategy">>, null)} of
-        {true, null} ->
+    case couch_config:fetch(<<"compact_automatically">>, false) of
+        true ->
             lager:debug("just compacting"),
             proc_lib:init_ack(Parent, {ok, self()}),
             compact_all();
-        {true, Strategy} ->
-            lager:debug("compacting and removing conflicts"),
-            proc_lib:init_ack(Parent, {ok, self()}),
-            compact_all(Strategy);
-        {false, _Strategy} ->
+        false ->
             lager:debug("auto-compaction not enabled"),
-            proc_lib:init_ack(Parent, ignore);
-        {null, _Strategy} ->
-            lager:debug("auto-compaction not enabled"),
-            proc_lib:init_ack(Parent, ignore),
-            couch_config:store(<<"compact_automatically">>, false);
-        _Other ->
-            lager:debug("unexpected values for auto-compaction: ~p", [_Other])
+            proc_lib:init_ack(Parent, ignore)
     end.
 
 -spec compact_all/0 :: () -> 'done'.
--spec compact_all/1 :: (couch_conflict:resolution_strategy()) -> 'done'.
--spec compact_all/2 :: (couch_conflict:resolution_strategy(), couch_conflict:merge_fun()) -> 'done'.
 compact_all() ->
     lager:debug("compacting all nodes"),
     {ok, Nodes} = couch_mgr:admin_all_docs(<<"nodes">>),
     _ = [ compact_node(wh_json:get_value(<<"id">>, Node)) || Node <- shuffle(Nodes)],
     done.
-compact_all(ConflictStrategy) ->
-    lager:debug("compacting all nodes"),
-    {ok, Nodes} = couch_mgr:admin_all_docs(<<"nodes">>),
-    _ = [ compact_node(wh_json:get_value(<<"id">>, Node), ConflictStrategy) || Node <- shuffle(Nodes)],
-    done.
-compact_all(ConflictStrategy, F) ->
-    lager:debug("compacting all nodes"),
-    {ok, Nodes} = couch_mgr:admin_all_docs(<<"nodes">>),
-    _ = [ compact_node(wh_json:get_value(<<"id">>, Node), ConflictStrategy, F) || Node <- shuffle(Nodes)],
-    done.
 
 -spec compact_node/1 :: (ne_binary() | atom()) -> 'done'.
--spec compact_node/2 :: (ne_binary() | atom(), couch_conflict:resolution_strategy()) -> 'done'.
--spec compact_node/3 :: (ne_binary() | atom(), couch_conflict:resolution_strategy(), couch_conflict:merge_fun()) -> 'done'.
 compact_node(Node) when is_atom(Node) ->
     compact_node(wh_util:to_binary(Node));
 compact_node(NodeBin) ->
     put(callid, NodeBin),
     lager:debug("compacting node ~s", [NodeBin]),
     {Conn, AdminConn} = get_node_connections(NodeBin),
-    {ok, DBs} = couch_util:db_info(Conn),
+    {ok, DBs} = node_dbs(AdminConn),
     Total = length(DBs),
     _ = lists:foldl(fun(DB, Count) ->
                             lager:debug("compacting database (~p/~p) '~s'", [Count, Total, DB]),
@@ -101,38 +68,10 @@ compact_node(NodeBin) ->
                     end, 1, shuffle(DBs, Total)),
     done.
 
-compact_node(Node, ConflictStrategy) when is_atom(Node) ->
-    compact_node(wh_util:to_binary(Node), ConflictStrategy);
-compact_node(NodeBin, ConflictStrategy) ->
-    put(callid, NodeBin),
-    lager:debug("compacting node ~s", [NodeBin]),
-
-    {Conn, _AdminConn} = get_node_connections(NodeBin),
-    {ok, DBs} = couch_util:db_info(Conn),
-    lager:debug("found ~b DBs to compact", [length(DBs)]),
-
-    _ = [ compact_db(NodeBin, DB, ConflictStrategy) || DB <- DBs ],
-    done.
-compact_node(Node, ConflictStrategy, F) when is_atom(Node) ->
-    compact_node(wh_util:to_binary(Node), ConflictStrategy, F);
-compact_node(NodeBin, ConflictStrategy, F) ->
-    put(callid, NodeBin),
-    lager:debug("compacting node ~s", [NodeBin]),
-
-    {Conn, _AdminConn} = get_node_connections(NodeBin),
-    {ok, DBs} = couch_util:db_info(Conn),
-    lager:debug("found ~b DBs to compact", [length(DBs)]),
-
-    _ = [ compact_db(NodeBin, DB, ConflictStrategy, F) || DB <- DBs ],
-    done.
-
 %% Use compact_db/1 to compact the DB across all known nodes
 %% Use compact_db/2 to compact the DB on a specific node
 -spec compact_db/1 :: (ne_binary()) -> 'done'.
 -spec compact_db/2 :: (ne_binary() | atom(), ne_binary()) -> 'done'.
--spec compact_db/3 :: (ne_binary() | atom(), ne_binary(), couch_conflict:resolution_strategy()) -> 'done'.
--spec compact_db/4 :: (ne_binary() | atom(), ne_binary(), couch_conflict:resolution_strategy(), couch_conflict:merge_fun()) -> 'done'.
-
 compact_db(DB) ->
     {ok, Nodes} = couch_mgr:admin_all_docs(<<"nodes">>),
     _ = [ compact_db(wh_json:get_value(<<"id">>, Node), DB) || Node <- Nodes],
@@ -145,34 +84,13 @@ compact_db(NodeBin, DB) ->
     ok = compact_node_db(NodeBin, DB, Conn, AdminConn),
     done.
 
-compact_db(Node, DB, ConflictStrategy) when is_atom(Node) ->
-    compact_db(wh_util:to_binary(Node), DB, ConflictStrategy);
-compact_db(NodeBin, DB, ConflictStrategy) ->
-    put(callid, NodeBin),
-    {Conn, AdminConn} = get_node_connections(NodeBin),
-
-    _ = couch_conflict:resolve(Conn, DB, couch_conflict:default_view(), ConflictStrategy),
-
-    ok = compact_node_db(NodeBin, DB, Conn, AdminConn),
-    done.
-
-compact_db(Node, DB, ConflictStrategy, F) when is_atom(Node) ->
-    compact_db(wh_util:to_binary(Node), DB, ConflictStrategy, F);
-compact_db(NodeBin, DB, ConflictStrategy, F) ->
-    put(callid, NodeBin),
-    {Conn, AdminConn} = get_node_connections(NodeBin),
-
-    _ = couch_conflict:resolve(Conn, DB, couch_conflict:default_view(), ConflictStrategy, F),
-
-    ok = compact_node_db(NodeBin, DB, Conn, AdminConn),
-    done.
-
 %% Internal Functions ----------------------------------------------------------
 -spec compact_node_db/4 :: (ne_binary(), ne_binary(), #server{}, #server{}) -> 'ok'.
 compact_node_db(NodeBin, DB, Conn, AdminConn) ->
     DBEncoded = binary:replace(DB, <<"/">>, <<"%2f">>, [global]),
     lager:debug("compacting db ~s node ~s", [DBEncoded, NodeBin]),
-    case couch_util:db_exists(Conn, DBEncoded) andalso get_db_shards(AdminConn, DBEncoded) of
+
+    case couch_util:db_exists(Conn, DBEncoded) andalso get_db_shards(NodeBin, AdminConn, DB, DBEncoded) of
         false -> lager:debug("db ~s not on node ~s", [DBEncoded, NodeBin]);
         [] -> lager:debug("no shards found matching ~s", [DBEncoded]);
         Shards ->
@@ -183,18 +101,18 @@ compact_node_db(NodeBin, DB, Conn, AdminConn) ->
             ok
     end.
 
--spec compact_node_shards/3 :: ([ne_binary(),...], server(), [ne_binary(),...] | []) -> 'ok'. 
+-spec compact_node_shards/3 :: ([ne_binary(),...], server(), [ne_binary(),...] | []) -> 'ok'.
 compact_node_shards(Shards, AdminConn, DesignDocs) ->
     case catch(lists:split(?MAX_COMPACTING_SHARDS, Shards)) of
-        {'EXIT', _} -> 
+        {'EXIT', _} ->
             compact_shards(Shards, AdminConn, DesignDocs),
             ok;
         {Compact, Remaining} ->
             compact_shards(Compact, AdminConn, DesignDocs),
             compact_node_shards(Remaining, AdminConn, DesignDocs)
-    end.             
+    end.
 
--spec compact_shards/3 :: ([ne_binary(),...], server(), [ne_binary(),...] | []) -> 'ok'. 
+-spec compact_shards/3 :: ([ne_binary(),...], server(), [ne_binary(),...] | []) -> 'ok'.
 compact_shards(Shards, AdminConn, DesignDocs) ->
     Pids = [spawn_monitor(?MODULE, compact_shard, [AdminConn, Shard, DesignDocs])
             || Shard <- Shards
@@ -219,23 +137,23 @@ compact_shard(AdminConn, Shard, DesignDocs) ->
     couch_util:db_compact(AdminConn, Shard),
     wait_for_compaction(AdminConn, Shard),
 
-    lager:debug("cleanup views in shard ~s", [Shard]),
+    lager:debug("cleanup views"),
     couch_util:db_view_cleanup(AdminConn, Shard),
     wait_for_compaction(AdminConn, Shard),
 
-    lager:debug("cleanup design docs in shard ~s", [Shard]),
+    lager:debug("cleanup design docs"),
     compact_design_docs(AdminConn, Shard, DesignDocs),
     wait_for_compaction(AdminConn, Shard),
 
-    lager:debug("finished compacting shard ~s", [Shard]).
+    lager:debug("finished compacting shard").
 
 -spec compact_design_docs/3 :: (server(), ne_binary(), [ne_binary(),...] | []) -> 'ok'.
 compact_design_docs(AdminConn, Shard, DesignDocs) ->
     case catch(lists:split(?MAX_COMPACTING_VIEWS, DesignDocs)) of
-        {'EXIT', _} -> 
+        {'EXIT', _} ->
             _ = [begin
-                     lager:debug("cleanup design doc ~s in shard ~s", [Design, Shard]),
-                     couch_util:design_compact(AdminConn, Shard, Design) 
+                     lager:debug("cleanup design doc ~s", [Design]),
+                     couch_util:design_compact(AdminConn, Shard, Design)
                  end
                   || Design <- DesignDocs
                 ],
@@ -244,7 +162,7 @@ compact_design_docs(AdminConn, Shard, DesignDocs) ->
             ok;
         {Compact, Remaining} ->
             _ = [begin
-                     lager:debug("cleanup design doc ~s in shard ~s", [Design, Shard]),
+                     lager:debug("cleanup design doc ~s", [Design]),
                      couch_util:design_compact(AdminConn, Shard, Design)
                  end
                   || Design <- Compact
@@ -262,12 +180,12 @@ wait_for_compaction(AdminConn, Shard) ->
             case wh_json:is_true(<<"compact_running">>, ShardData, false) of
                 false -> ok;
                 true ->
-                    lager:debug("waiting for compaction of shard ~s", [Shard]),
+                    lager:debug("waiting for compaction to finish"),
                     ok = timer:sleep(couch_config:fetch(<<"sleep_between_poll">>, ?SLEEP_BETWEEN_POLL)),
                     wait_for_compaction(AdminConn, Shard)
             end;
         {error, db_not_found} ->
-            lager:debug("db shard ~s not found, skipping", [Shard]);
+            lager:debug("shard not found, skipping");
         {error, _E} ->
             lager:debug("failed to query shard for compaction status: ~p", [_E]),
             ok = timer:sleep(couch_config:fetch(<<"sleep_between_poll">>, ?SLEEP_BETWEEN_POLL)),
@@ -279,31 +197,21 @@ get_db_design_docs(Conn, DBEncoded) ->
     {ok, Designs} = couch_util:all_design_docs(Conn, DBEncoded, []),
     [ binary:replace(wh_json:get_value(<<"id">>, Design), <<"_design/">>, <<>>, [global]) || Design <- Designs ].
 
--spec get_db_shards/2 :: (#server{}, ne_binary()) -> [ne_binary()].
-get_db_shards(AdminConn, DBEncoded) ->
-    case couch_config:fetch(DBEncoded, undefined, ?WH_COUCH_CACHE) of
-        undefined ->
-            case couch_util:db_info(AdminConn) of
-                {ok, []} -> lager:debug("no shards found on admin conn? That's odd"), [];
-                {ok, Shards} ->
-                    Encoded = [ ShardEncoded || Shard <- Shards, is_a_shard(ShardEncoded=binary:replace(Shard, <<"/">>, <<"%2f">>, [global]), DBEncoded) ],
-                    couch_config:store(DBEncoded, Encoded, ?WH_COUCH_CACHE),
-                    lager:debug("cached encoded shards for ~s", [DBEncoded]),
-                    Encoded;
-                {error, _E} ->
-                    %% TODO: check this error for terminal cases
-                    lager:debug("unable to get shards atm: ~p", [_E]),
-                    ok = timer:sleep(couch_config:fetch(<<"sleep_between_poll">>, ?SLEEP_BETWEEN_POLL)),
-                    get_db_shards(AdminConn, DBEncoded)
-            end;
-        Encoded ->
-            lager:debug("pulled encoded shards from cache for ~s", [DBEncoded]),
-            Encoded
+-spec get_db_shards/4 :: (ne_binary(), #server{}, ne_binary(), ne_binary()) -> [ne_binary()].
+get_db_shards(NodeBin, AdminConn, Db, DBEncoded) ->
+    case couch_util:open_cache_doc(AdminConn, <<"dbs">>, Db, []) of
+        {ok, Doc} ->
+            Suffix = wh_json:get_value(<<"shard_suffix">>, Doc),
+            Ranges = wh_json:get_value([<<"by_node">>, NodeBin], Doc, []),
+            [<<"shards%2f", Range/binary, "%2f", DBEncoded/binary, (wh_util:to_binary(Suffix))/binary>>
+                || Range <- Ranges
+            ];
+        {error, _E} ->
+            %% TODO: check this error for terminal cases
+            lager:debug("unable to get shards for ~s atm: ~p", [DBEncoded, _E]),
+            ok = timer:sleep(couch_config:fetch(<<"sleep_between_poll">>, ?SLEEP_BETWEEN_POLL)),
+            get_db_shards(NodeBin, AdminConn, Db, DBEncoded)
     end.
-
--spec is_a_shard/2 :: (ne_binary(), ne_binary()) -> boolean().
-is_a_shard(Shard, DB) ->
-    binary:match(Shard, <<"%2f", DB/binary, ".">>) =/= nomatch.
 
 -spec get_node_connections/1 :: (ne_binary()) -> {#server{}, #server{}}.
 get_node_connections(NodeBin) ->
@@ -357,7 +265,14 @@ get_ports(_Node, pang) ->
 -spec get_conns/5 :: (nonempty_string(), pos_integer(), string(), string(), pos_integer()) -> {#server{}, #server{}}.
 get_conns(Host, Port, User, Pass, AdminPort) ->
     {couch_util:get_new_connection(Host, Port, User, Pass),
-     couch_util:get_new_connection(Host, AdminPort, User, Pass)}.
+     couch_util:get_new_connection(Host, AdminPort, User, Pass)
+    }.
+
+%% returns list of DBs in unencoded format
+-spec node_dbs/1 :: (#server{}) -> {'ok', wh_json:json_strings()}.
+node_dbs(AdminConn) ->
+    {ok, Dbs} = couch_util:all_docs(AdminConn, <<"dbs">>, []),
+    {ok, [wh_json:get_value(<<"id">>, Db) || Db <- Dbs]}.
 
 -spec shuffle/1 :: (list()) -> list().
 -spec shuffle/2 :: (list(), integer()) -> list().
