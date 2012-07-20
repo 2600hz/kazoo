50c6357048f1b382d653c6991926ba757a87c6a6
James Aimonetti <james@2600hz.org>
2012-07-19 18:12:23 -0700

    WHISTLE-1186: break up the return types a bit, to help with specs in the twiml translator

--- lib/kazoo_translator-1.0.0/src/kzt.hrl
+++ lib/kazoo_translator-1.0.0/src/kzt.hrl
@@ -2,11 +2,16 @@
 
 -include_lib("whistle/include/wh_types.hrl").
 
+-type ok_return() :: {'ok', whapps_call:call()}.
+-type stop_return() :: {'stop', whapps_call:call()}.
+
+%% {request, Call, URI, Method, BaseParams}
+-type request_return() :: {'request', whapps_call:call(), ne_binary(), 'get' | 'post', api_terms()}.
+
 -type exec_element_return() ::
-        {'ok', whapps_call:call()} |
-        {'stop', whapps_call:call()} |
-        {'request', whapps_call:call(), ne_binary(), 'get' | 'post', api_terms()}.
-%%      {request, Call, URI, Method, BaseParams}
+        ok_return() |
+        stop_return() |
+        request_return().
 
 -type exec_return() ::
         {'stop', whapps_call:call()} |
