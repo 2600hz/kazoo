2e5406dd5f61af4922bdc6f0e742def789672582
James Aimonetti <james@2600hz.org>
2012-07-30 13:56:21 -0700

    WHISTLE-11: fix how forgot password requests were processed. Match the literal binary <<"recovery">> instead of the list (that was the old way). Explicitly match for authn/authz request params.

--- whistle_apps/apps/crossbar/src/modules/cb_user_auth.erl
+++ whistle_apps/apps/crossbar/src/modules/cb_user_auth.erl
@@ -49,7 +49,7 @@ init() ->
 -spec allowed_methods/1 :: (path_tokens()) -> http_methods().
 allowed_methods() ->
     ['PUT'].
-allowed_methods([<<"recovery">>]) ->
+allowed_methods(<<"recovery">>) ->
     ['PUT'].
 
 %%--------------------------------------------------------------------
@@ -74,7 +74,7 @@ resource_exists(_) -> false.
 -spec authorize/1 :: (#cb_context{}) -> boolean().
 authorize(#cb_context{req_nouns=[{<<"user_auth">>, _}]}) ->
     true;
-authorize(_) ->
+authorize(#cb_context{}) ->
     false.
 
 %%--------------------------------------------------------------------
@@ -85,7 +85,9 @@ authorize(_) ->
 -spec authenticate/1 :: (#cb_context{}) -> boolean().
 authenticate(#cb_context{req_nouns=[{<<"user_auth">>, []}]}) ->
     true;
-authenticate(_) ->
+authenticate(#cb_context{req_nouns=[{<<"user_auth">>, [<<"recovery">>]}]}) ->
+    true;
+authenticate(#cb_context{}) ->
     false.
 
 %%--------------------------------------------------------------------
