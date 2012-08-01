ddb8ae4cfd520128bf402b1b21e4f7aef7d266a7
James Aimonetti <james@2600hz.org>
2012-07-31 18:40:45 -0700

    WHISTLE-1441: default application files

---
+++ whistle_apps/apps/acdc/src/acdc.hrl
@@ -0,0 +1,17 @@
+-ifndef(ACDC_HRL).
+
+%% Typical includes needed
+-include_lib("whistle/include/wh_amqp.hrl").
+-include_lib("whistle/include/wh_types.hrl").
+-include_lib("whistle/include/wh_log.hrl").
+-include_lib("whistle/include/wh_databases.hrl").
+
+-define(CONFIG_CAT, <<"acdc">>).
+
+-define(APP_NAME, <<"acdc">>).
+-define(APP_VERSION, <<"1.0.0">>).
+
+-define(ACDC_CACHE, acdc_cache).
+
+-define(ACDC_HRL, true).
+-endif.
---
+++ whistle_apps/apps/acdc/src/acdc_sup.erl
@@ -0,0 +1,48 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2012, VoIP INC
+%%% @doc
+%%%
+%%% @end
+%%% @contributors
+%%%   James Aimonetti
+%%%-------------------------------------------------------------------
+-module(acdc_sup).
+
+-behaviour(supervisor).
+
+-include("acdc.hrl").
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+
+-define(CHILDREN, [{acdc_agents_sup, supervisor}
+                   ,{acdc_queues_sup, supervisor}
+                   ,{acdc_init, worker}
+                  ]).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+-spec init([]) -> sup_init_ret().
+init([]) ->
+    RestartStrategy = one_for_one,
+    MaxRestarts = 5,
+    MaxSecondsBetweenRestarts = 10,
+
+    SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+    Children = [?CHILD(Name, Type) || {Name, Type} <- ?CHILDREN],
+    {ok, {SupFlags, Children}}.
