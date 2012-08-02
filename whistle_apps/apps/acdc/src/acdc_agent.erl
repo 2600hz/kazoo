72f6ff6b2339f2446b78c42befb0ed82c6a6a575
James Aimonetti <james@2600hz.org>
2012-08-01 23:47:56 -0700

    WHISTLE-1441: sync agent processes amongst each other

--- whistle_apps/apps/acdc/src/acdc_agent.erl
+++ whistle_apps/apps/acdc/src/acdc_agent.erl
@@ -15,6 +15,7 @@
 
 %% Listener callbacks
 -export([handle_status_update/2
+         ,handle_sync_req/2
         ]).
 
 %% gen_server callbacks
@@ -39,11 +40,34 @@
          ,current_call :: whapps_call:call()
          ,agent_id :: ne_binary()
          ,agent_db :: ne_binary()
+         ,account_id :: ne_binary()
          ,endpoints :: wh_json:json_objects()
          ,last_connect :: wh_now() % last connection
          ,last_attempt :: wh_now() % last attempt to connect
+         ,my_id = wh_util:rand_hex_binary() :: ne_binary()
+         ,sync_timer :: reference()
          }).
 
+%%%===================================================================
+%%% Defines for different functionality
+%%%===================================================================
+
+-define(SYNC_TIMER_MESSAGE, sync_timeout).
+-define(SYNC_TIMER_TIMEOUT, 5000).
+
+-define(BINDINGS(AcctDb, AgentId), [{self, []}
+                                    ,{agent, [{agent_db, AcctDb}
+                                              ,{agent_id, AgentId}
+                                             ]}
+                                   ]).
+
+-define(RESPONDERS, [{{?MODULE, handle_agent_status}
+                      ,{<<"agents">>, <<"status_update">>}
+                     }
+                     ,{{?MODULE, handle_sync_req}
+                       ,{<<"agents">>, <<"sync_req">>}
+                      }
+                    ]).
 %%%===================================================================
 %%% API
 %%%===================================================================
@@ -56,19 +80,19 @@
 %% @end
 %%--------------------------------------------------------------------
 start_link(AcctDb, AgentJObj) ->
+    AgentId = wh_json:get_value(<<"_id">>, AgentJObj),
     gen_listener:start_link(?MODULE
-                            ,[{bindings, [{self, []}
-                                          ,{agent, []}
-                                         ]}
-                              ,{responders, [{{?MODULE, handle_agent_status}
-                                              ,{<<"agents">>, <<"status_update">>}
-                                             }
-                                            ]}
+                            ,[{bindings, ?BINDINGS(AcctDb, AgentId)}
+                              ,{responders, ?RESPONDERS}
                              ]
                             ,[AcctDb, AgentJObj]).
 
 -spec handle_status_update/2 :: (wh_json:json_object(), wh_proplist()) -> any().
-handle_status_update(JObj, Props) ->
+handle_status_update(_JObj, _Props) ->
+    ok.
+
+-spec handle_sync_req/2 :: (wh_json:json_object(), wh_proplist()) -> any().
+handle_sync_req(_JObj, _Props) ->
     ok.
 
 %%%===================================================================
@@ -90,13 +114,14 @@ init([AcctDb, AgentJObj]) ->
     AgentId = wh_json:get_value(<<"_id">>, AgentJObj),
     put(callid, AgentId),
 
-    EPs = acdc_util:get_endpoints(AcctDb, AgentId),
+    gen_listener:cast(self(), load_endpoints),
+    gen_listener:cast(self(), send_sync_event),
 
     {ok, #state{
        status = 'init'
        ,agent_db = AcctDb
        ,agent_id = AgentId
-       ,endpoints = EPs
+       ,account_id = wh_json:get_value(<<"pvt_account_id">>, AgentJObj)
       }}.
 
 %%--------------------------------------------------------------------
@@ -114,8 +139,8 @@ init([AcctDb, AgentJObj]) ->
 %% @end
 %%--------------------------------------------------------------------
 handle_call(_Request, _From, State) ->
-    Reply = ok,
-    {reply, Reply, State}.
+    lager:debug("unhandled call from ~p: ~p", [_From, _Request]),
+    {reply, {error, unhandled_call}, State}.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -127,7 +152,24 @@ handle_call(_Request, _From, State) ->
 %%                                  {stop, Reason, State}
 %% @end
 %%--------------------------------------------------------------------
+handle_cast(load_endpoints, #state{
+              agent_db=AcctDb
+              ,agent_id=AgentId
+             }=State) ->
+    {noreply, State#state{endpoints=acdc_util:get_endpoints(AcctDb, AgentId)}};
+handle_cast(send_sync_event, #state{
+              account_id=AcctId
+              ,agent_id=AgentId
+              ,my_id=ProcessId
+             }=State) ->
+    wapi_agent:publish_sync_req(create_sync_api(AcctId, AgentId, ProcessId)),
+    {noreply, State#state{sync_timer=start_sync_timer()}};
+handle_cast({sync_resp, RespJObj}, #state{sync_timer=Ref}=State) ->
+    _ = erlang:cancel_timer(Ref),
+    lager:debug("recv sync resp: ~p", [RespJObj]),
+    {noreply, State#state{sync_timer=undefined}};
 handle_cast(_Msg, State) ->
+    lager:debug("unhandled cast: ~p", [_Msg]),
     {noreply, State}.
 
 %%--------------------------------------------------------------------
@@ -140,6 +182,9 @@ handle_cast(_Msg, State) ->
 %%                                   {stop, Reason, State}
 %% @end
 %%--------------------------------------------------------------------
+handle_info({timeout, Ref, ?SYNC_TIMER_MESSAGE}, #state{sync_timer=Ref}=State) ->
+    lager:debug("sync timer timeout, moving to ready"),
+    {noreply, State#state{status=ready, sync_timer=undefined}};
 handle_info(_Info, State) ->
     lager:debug("unhandled message: ~p", [_Info]),
     {noreply, State}.
@@ -175,3 +220,15 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+-spec create_sync_api/3 :: (ne_binary(), ne_binary(), ne_binary()) ->
+                                   wh_json:json_object().
+create_sync_api(AcctId, AgentId, ProcessId) ->
+    wh_json:from_list([{<<"Account-ID">>, AcctId}
+                       ,{<<"Agent-ID">>, AgentId}
+                       ,{<<"Process-ID">>, ProcessId}
+                       ,{<<"Msg-ID">>, wh_util:rand_hex_binary(6)}
+                      ]).
+
+-spec start_sync_timer/0 :: () -> reference().
+start_sync_timer() ->
+    erlang:start_timer(?SYNC_TIMER_TIMEOUT, self(), ?SYNC_TIMER_MESSAGE).
