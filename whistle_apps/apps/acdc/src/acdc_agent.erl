81eb530736e79aba9f10c631493969cdec59879b
James Aimonetti <james@2600hz.org>
2012-08-01 17:13:18 -0700

    WHISTLE-1441: start of the gen_listener representing an agent

---
+++ whistle_apps/apps/acdc/src/acdc_agent.erl
@@ -0,0 +1,166 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2012, VoIP INC
+%%% @doc
+%%% 
+%%% @end
+%%% @contributors
+%%%   James Aimonetti
+%%%-------------------------------------------------------------------
+-module(acdc_agent).
+
+-behaviour(gen_listener).
+
+%% API
+-export([start_link/2]).
+
+-include("acdc.hrl").
+
+%% gen_server callbacks
+-export([init/1
+         ,handle_call/3
+         ,handle_cast/2
+         ,handle_info/2
+         ,handle_event/2
+         ,terminate/2
+         ,code_change/3
+        ]).
+
+-type agent_status() :: 'init' |      % agent is starting up
+                        'ready' |     % agent is ready to connect
+                        'trying' |    % agent is trying to connect
+                        'connected' | % agent is connected
+                        'paused'.     % agent is away
+-record(state, {
+          status = 'init' :: agent_status()
+         ,current_call :: whapps_call:call()
+         ,agent_id :: ne_binary()
+         ,agent_db :: ne_binary()
+         ,endpoints :: wh_json:json_objects()
+         ,last_connect :: wh_now() % last connection
+         ,last_attempt :: wh_now() % last attempt to connect
+         }).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link(AcctDb, AgentJObj) ->
+    gen_listener:start_link(?MODULE
+                            ,[{bindings, [{self, []}
+                                          ,{agent, []}
+                                         ]}
+                              ,{responders, []}
+                             ]
+                            ,[AcctDb, AgentJObj]).
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([AcctDb, AgentJObj]) ->
+    AgentId = wh_json:get_value(<<"_id">>, AgentJObj),
+    put(callid, AgentId),
+
+    EPs = acdc_util:get_endpoints(AcctDb, AgentId),
+
+    {ok, #state{
+       status = 'init'
+       ,agent_db = AcctDb
+       ,agent_id = AgentId
+       ,endpoints = EPs
+      }}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+    lager:debug("unhandled message: ~p", [_Info]),
+    {noreply, State}.
+
+handle_event(_JObj, #state{status=Status}) ->
+    {reply, [{status, Status}]}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
