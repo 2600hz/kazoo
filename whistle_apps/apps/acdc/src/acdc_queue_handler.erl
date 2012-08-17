3033f15ba480baec5b84c4915a6e2d38fb2da043
James Aimonetti <james@2600hz.org>
2012-08-17 16:59:31 -0700

    WHISTLE-1441: capture the delivery info, needed for ack/nack

--- whistle_apps/apps/acdc/src/acdc_queue_handler.erl
+++ whistle_apps/apps/acdc/src/acdc_queue_handler.erl
@@ -9,7 +9,7 @@
 -module(acdc_queue_handler).
 
 -export([handle_call_event/2
-         ,handle_member_call/2
+         ,handle_member_call/3
          ,handle_member_resp/2
          ,handle_member_accepted/2
          ,handle_member_retry/2
@@ -19,8 +19,8 @@ handle_call_event(JObj, Props) ->
     {Cat, Name} = wh_util:get_event_type(JObj),
     acdc_queue_fsm:call_event(props:get_value(fsm_pid, Props), Cat, Name, JObj).
 
-handle_member_call(JObj, Props) ->
-    acdc_queue_fsm:member_call(props:get_value(fsm_pid, Props), JObj).
+handle_member_call(JObj, Props, Delivery) ->
+    acdc_queue_fsm:member_call(props:get_value(fsm_pid, Props), JObj, Delivery).
 
 handle_member_resp(JObj, Props) ->
     acdc_queue_fsm:member_connect_resp(props:get_value(fsm_pid, Props), JObj).
