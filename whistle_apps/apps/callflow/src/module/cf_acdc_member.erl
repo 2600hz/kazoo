9bd53f7022551bdee618596b664bce9a17b56bc3
James Aimonetti <james@2600hz.org>
2012-08-21 16:27:50 -0700

    WHISTLE-1577: use the pool to request the member_call and wait for either success, failure, or timeout to see if we should continue in the callflow

--- whistle_apps/apps/callflow/src/module/cf_acdc_member.erl
+++ whistle_apps/apps/callflow/src/module/cf_acdc_member.erl
@@ -25,11 +25,31 @@
 handle(Data, Call) ->
     whapps_call_command:answer(Call),
     whapps_call_command:hold(Call),
+
+    QueueId = wh_json:get_value(<<"id">>, Data),
+
     MemberCall = props:filter_undefined(
                    [{<<"Account-ID">>, whapps_call:account_id(Call)}
-                    ,{<<"Queue-ID">>, wh_json:get_value(<<"id">>, Data)}
+                    ,{<<"Queue-ID">>, QueueId}
                     ,{<<"Call">>, whapps_call:to_json(Call)}
                     | wh_api:default_headers(?APP_NAME, ?APP_VERSION)
                    ]),
-    wapi_acdc_queue:publish_member_call(MemberCall),
-    cf_exe:control_usurped(Call).
+
+    {ok, QueueJObj} = couch_mgr:open_cache_doc(whapps_call:call_id(Call), QueueId),
+    MaxWait = wh_json:get_integer_value(<<"max_wait_time">>, QueueJObj),
+
+    case whapps_util:amqp_pool_request(MemberCall
+                                       ,fun wapi_acdc_queue:publish_member_call/1
+                                       ,fun wapi_acdc_queue:member_call_success_v/1
+                                       ,MaxWait * 1000
+                                      ) of
+        {ok, _SuccessJObj} ->
+            lager:debug("agent took the member_call: ~p", [_SuccessJObj]),
+            cf_exe:control_usurped(Call);
+        {error, timeout} ->
+            lager:debug("member_call timed out waiting in the queue for ~p s", [MaxWait]),
+            cf_exe:continue(Call);
+        {error, _Fail} ->
+            lager:debug("failed to process the member_call: ~p", [_Fail]),
+            cf_exe:continue(Call)
+    end.
