bde8af26e99acf5dcc5466d3c63f92120a09dc8f
James Aimonetti <james@2600hz.org>
2012-08-20 17:26:15 -0700

    WHISTLE-1441: formatting change

--- ecallmgr/src/ecallmgr_fs_xml.erl
+++ ecallmgr/src/ecallmgr_fs_xml.erl
@@ -241,9 +241,9 @@ get_channel_vars({<<"Custom-Channel-Vars">>, JObj}, Vars) ->
                             false -> [list_to_binary([?CHANNEL_VAR_PREFIX, wh_util:to_list(K)
                                                       ,"='", wh_util:to_list(V), "'"]) | Acc];
                             {_, <<"group_confirm_file">>} -> [list_to_binary(["group_confirm_file='"
-									     ,wh_util:to_list(ecallmgr_util:media_path(V, extant, get(callid), wh_json:new()))
-									     ,"'"
-									    ]) | Acc];
+                                                                             ,wh_util:to_list(ecallmgr_util:media_path(V, extant, get(callid), wh_json:new()))
+                                                                             ,"'"
+                                                                            ]) | Acc];
                             {_, Prefix} -> [list_to_binary([Prefix, "='", wh_util:to_list(V), "'"]) | Acc]
                         end
                 end, Vars, wh_json:to_proplist(JObj));
