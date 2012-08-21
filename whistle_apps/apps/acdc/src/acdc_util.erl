bacbac8ab921a361730b868419f0ef8023fedf29
James Aimonetti <james@2600hz.org>
2012-08-20 17:29:22 -0700

    WHISTLE-1441: transform the endpoint json docs into API endpoint json objects

--- whistle_apps/apps/acdc/src/acdc_util.erl
+++ whistle_apps/apps/acdc/src/acdc_util.erl
@@ -15,30 +15,34 @@
 
 -include("acdc.hrl").
 
--spec get_endpoints/2 :: (ne_binary(), ne_binary() | couch_mgr:get_results_return()) ->
+-spec get_endpoints/2 :: (whapps_call:call(), ne_binary() | couch_mgr:get_results_return()) ->
                                  wh_json:json_objects().
-get_endpoints(?NE_BINARY = AcctDb, ?NE_BINARY = AgentId) ->
-    get_endpoints(AcctDb
+get_endpoints(Call, ?NE_BINARY = AgentId) ->
+    AcctDb = whapps_call:account_db(Call),
+    get_endpoints(Call
                   ,couch_mgr:get_results(AcctDb
                                          ,<<"cf_attributes/owned">>
                                          ,[{key, [AgentId, <<"device">>]}]
                                         )
                  );
-get_endpoints(_AcctDb, {ok, []}) -> [];
-get_endpoints(_AcctDb, {error, _E}) -> [];
-get_endpoints(AcctDb, {ok, Devices}) ->
-    Call = whapps_call:new(),
-    [begin
-         {ok, EP} = cf_endpoint:build(EPDoc, Call),
-         EP
-     end
-     || Device <- Devices,
-        (EPDoc = get_endpoint(AcctDb, wh_json:get_value(<<"id">>, Device))) =/= undefined
-    ].
+get_endpoints(_Call, {ok, []}) -> [];
+get_endpoints(_Call, {error, _E}) -> [];
+get_endpoints(Call, {ok, Devices}) ->
+    EPDocs = [EPDoc
+              || Device <- Devices,
+                 (EPDoc = get_endpoint(Call, wh_json:get_value(<<"id">>, Device))) =/= undefined,
+                 wh_json:is_true(<<"enabled">>, EPDoc, false)
+             ],
+    lists:foldl(fun(EPDoc, Acc) ->
+                        case cf_endpoint:build(EPDoc, Call) of
+                            {ok, EP} -> EP ++ Acc;
+                            {error, _} -> Acc
+                        end
+                end, [], EPDocs).
 
--spec get_endpoint/2 :: (ne_binary(), ne_binary()) -> wh_json:json_object() | 'undefined'.
-get_endpoint(?NE_BINARY = AcctDb, ?NE_BINARY = EndpointId) ->
-    case couch_mgr:open_doc(AcctDb, EndpointId) of
+-spec get_endpoint/2 :: (whapps_call:call(), ne_binary()) -> wh_json:json_object() | 'undefined'.
+get_endpoint(Call, ?NE_BINARY = EndpointId) ->
+    case couch_mgr:open_doc(whapps_call:account_db(Call), EndpointId) of
         {ok, JObj} -> JObj;
         {error, _R} -> undefined
     end.
@@ -48,14 +52,14 @@ get_endpoint(?NE_BINARY = AcctDb, ?NE_BINARY = EndpointId) ->
 -spec unbind_from_call_events/1 :: (ne_binary() | whapps_call:call()) -> 'ok'.
 bind_to_call_events(?NE_BINARY = CallId) ->
     gen_listener:add_binding(self(), call, [{callid, CallId}
-                                            ,{restrict_to, [events]}
+                                            ,{restrict_to, [events, error]}
                                            ]);
 bind_to_call_events(Call) ->
     bind_to_call_events(whapps_call:call_id(Call)).
 
 unbind_from_call_events(?NE_BINARY = CallId) ->
     gen_listener:rm_binding(self(), call, [{callid, CallId}
-                                           ,{restrict_to, [events]}
+                                           ,{restrict_to, [events, error]}
                                           ]);
 unbind_from_call_events(Call) ->
     unbind_from_call_events(whapps_call:call_id(Call)).
