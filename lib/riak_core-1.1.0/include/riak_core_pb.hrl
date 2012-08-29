532cd4c1f036f500b8132f1921c8e2ce888b703a
James Aimonetti <james@2600hz.org>
2012-08-29 16:34:52 -0700

    WHISTLE-1590: update riak_core header

--- lib/riak_core-1.1.0/include/riak_core_pb.hrl
+++ lib/riak_core-1.1.0/include/riak_core_pb.hrl
@@ -1,6 +1,9 @@
+-ifndef(RIAKOBJECT_PB_PB_H).
+-define(RIAKOBJECT_PB_PB_H, true).
 -record(riakobject_pb, {
     bucket = erlang:error({required, bucket}),
     key = erlang:error({required, key}),
     val = erlang:error({required, val})
 }).
+-endif.
 
