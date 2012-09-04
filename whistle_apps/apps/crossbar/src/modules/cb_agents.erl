364f790a3c6cb542f949570d5d55da5dde5c6e2b
James Aimonetti <james@2600hz.org>
2012-09-04 11:20:28 -0700

    WHISTLE-1575: get rid of POST/DELETE processors...GET only for agents

--- whistle_apps/apps/crossbar/src/modules/cb_agents.erl
+++ whistle_apps/apps/crossbar/src/modules/cb_agents.erl
@@ -2,7 +2,18 @@
 %%% @copyright (C) 2011-2012, VoIP INC
 %%% @doc
 %%%
-%%% Listing of all expected v1 callbacks
+%%% CRUD for call queues
+%%% /agents
+%%%   GET: list all known agents and their queues
+%%%
+%%% /agents/stats
+%%%   GET: stats for all agents
+%%%
+%%% /agents/AID
+%%%   GET: agent details
+%%%
+%%% /agents/AID/stats
+%%%   GET: stats on the agent
 %%%
 %%% @end
 %%% @contributors:
@@ -15,17 +26,11 @@
          ,allowed_methods/0, allowed_methods/1, allowed_methods/2
          ,resource_exists/0, resource_exists/1, resource_exists/2
          ,validate/1, validate/2, validate/3
-         ,get/1, get/2, get/3
-         ,post/2
-         ,delete/2
         ]).
 
 -include("include/crossbar.hrl").
 
--define(PVT_TYPE, <<"agent">>).
--define(PVT_FUNS, [fun add_pvt_type/2]).
 -define(CB_LIST, <<"agents/crossbar_listing">>).
-
 -define(STATS_PATH_TOKEN, <<"stats">>).
 
 %%%===================================================================
@@ -42,11 +47,7 @@
 init() ->
     _ = crossbar_bindings:bind(<<"v1_resource.allowed_methods.agents">>, ?MODULE, allowed_methods),
     _ = crossbar_bindings:bind(<<"v1_resource.resource_exists.agents">>, ?MODULE, resource_exists),
-    _ = crossbar_bindings:bind(<<"v1_resource.validate.agents">>, ?MODULE, validate),
-    _ = crossbar_bindings:bind(<<"v1_resource.execute.get.agents">>, ?MODULE, get),
-    _ = crossbar_bindings:bind(<<"v1_resource.execute.put.agents">>, ?MODULE, put),
-    _ = crossbar_bindings:bind(<<"v1_resource.execute.post.agents">>, ?MODULE, post),
-    _ = crossbar_bindings:bind(<<"v1_resource.execute.delete.agents">>, ?MODULE, delete).
+    _ = crossbar_bindings:bind(<<"v1_resource.validate.agents">>, ?MODULE, validate).
 
 %%--------------------------------------------------------------------
 %% @public
@@ -60,10 +61,8 @@ init() ->
 -spec allowed_methods/2 :: (path_token(), path_token()) -> http_methods() | [].
 allowed_methods() ->
     ['GET'].
-allowed_methods(?STATS_PATH_TOKEN) ->
-    ['GET'];
 allowed_methods(_) ->
-    ['GET', 'POST', 'DELETE'].
+    ['GET'].
 allowed_methods(_Id, ?STATS_PATH_TOKEN) ->
     ['GET'].
 
@@ -103,54 +102,11 @@ validate(#cb_context{req_verb = <<"get">>}=Context, ?STATS_PATH_TOKEN) ->
     %% read all agent stats
     Context;
 validate(#cb_context{req_verb = <<"get">>}=Context, Id) ->
-    read(Id, Context);
-validate(#cb_context{req_verb = <<"post">>}=Context, Id) ->
-    update(Id, Context);
-validate(#cb_context{req_verb = <<"delete">>}=Context, Id) ->
     read(Id, Context).
 
 validate(#cb_context{req_verb = <<"get">>}=Context, _Id, ?STATS_PATH_TOKEN) ->
     %% read agent stats with id=Id
     Context.
-
-%%--------------------------------------------------------------------
-%% @public
-%% @doc
-%% If the HTTP verb is a GET, execute necessary code to fulfill the GET
-%% request. Generally, this will involve stripping pvt fields and loading
-%% the resource into the resp_data, resp_headers, etc...
-%% @end
-%%--------------------------------------------------------------------
--spec get/1 :: (#cb_context{}) -> #cb_context{}.
--spec get/2 :: (#cb_context{}, path_token()) -> #cb_context{}.
--spec get/3 :: (#cb_context{}, path_token(), path_token()) -> #cb_context{}.
-get(#cb_context{}=Context) ->
-    Context.
-get(#cb_context{}=Context, _) ->
-    Context.
-get(#cb_context{}=Context, _, ?STATS_PATH_TOKEN) ->
-    Context.
-
-%%--------------------------------------------------------------------
-%% @public
-%% @doc
-%% If the HTTP verib is POST, execute the actual action, usually a db save
-%% (after a merge perhaps).
-%% @end
-%%--------------------------------------------------------------------
--spec post/2 :: (#cb_context{}, path_token()) -> #cb_context{}.
-post(#cb_context{}=Context, _) ->
-    crossbar_doc:save(Context).
-
-%%--------------------------------------------------------------------
-%% @public
-%% @doc
-%% If the HTTP verib is DELETE, execute the actual action, usually a db delete
-%% @end
-%%--------------------------------------------------------------------
--spec delete/2 :: (#cb_context{}, path_token()) -> #cb_context{}.
-delete(#cb_context{}=Context, _) ->
-    crossbar_doc:delete(Context).
 
 %%--------------------------------------------------------------------
 %% @private
@@ -161,25 +117,6 @@ delete(#cb_context{}=Context, _) ->
 -spec read/2 :: (ne_binary(), #cb_context{}) -> #cb_context{}.
 read(Id, Context) ->
     crossbar_doc:load(Id, Context).
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Update an existing instance with the data provided, if it is
-%% valid
-%% @end
-%%--------------------------------------------------------------------
--spec update/2 :: (ne_binary(), #cb_context{}) -> #cb_context{}.
-update(Id, #cb_context{req_data=Data}=Context) ->
-    case wh_json_validator:is_valid(Data, <<"agents">>) of
-        {fail, Errors} ->
-            crossbar_util:response_invalid_data(Errors, Context);
-        {pass, JObj} ->
-            {JObj1, _} = lists:foldr(fun(F, {J, C}) ->
-                                             {F(J, C), C}
-                                     end, {JObj, Context}, ?PVT_FUNS),
-            crossbar_doc:load_merge(Id, JObj1, Context)
-    end.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -200,15 +137,9 @@ summary(Context) ->
 %%--------------------------------------------------------------------
 -spec normalize_view_results/2 :: (wh_json:json_object(), wh_json:json_objects()) -> wh_json:json_objects().
 normalize_view_results(JObj, Acc) ->
-    [wh_json:get_value(<<"value">>, JObj)|Acc].
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% These are the pvt funs that add the necessary pvt fields to every
-%% instance
-%% @end
-%%--------------------------------------------------------------------
--spec add_pvt_type/2 :: (wh_json:json_object(), #cb_context{}) -> wh_json:json_object().
-add_pvt_type(JObj, _) ->
-    wh_json:set_value(<<"pvt_type">>, ?PVT_TYPE, JObj).
+    [wh_json:set_value(<<"id">>
+                       ,wh_json:get_value(<<"id">>, JObj)
+                       , wh_json:get_value(<<"value">>, JObj)
+                      )
+     | Acc
+    ].
