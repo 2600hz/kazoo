4c3743617e0775db02ac071b4a6024d572b4638e
James Aimonetti <james@2600hz.org>
2012-09-04 17:34:20 -0700

    WHISTLE-1575: add the sync_req and sync_resp APIs for queues

--- whistle_apps/apps/acdc/src/wapi_acdc_queue.erl
+++ whistle_apps/apps/acdc/src/wapi_acdc_queue.erl
@@ -19,6 +19,8 @@
          ,member_connect_retry/1, member_connect_retry_v/1
          ,member_connect_accepted/1, member_connect_accepted_v/1
          ,member_hungup/1, member_hungup_v/1
+         ,sync_req/1, sync_req_v/1
+         ,sync_resp/1, sync_resp_v/1
         ]).
 
 -export([bind_q/2
@@ -36,6 +38,8 @@
          ,publish_member_connect_retry/2, publish_member_connect_retry/3
          ,publish_member_connect_accepted/2, publish_member_connect_accepted/3
          ,publish_member_hungup/2, publish_member_hungup/3
+         ,publish_sync_req/1, publish_sync_req/2
+         ,publish_sync_resp/2, publish_sync_resp/3
         ]).
 
 -include("acdc.hrl").
@@ -354,6 +358,78 @@ member_hungup_v(JObj) ->
     member_hungup_v(wh_json:to_proplist(JObj)).
 
 %%------------------------------------------------------------------------------
+%% Sync Req/Resp
+%%   Depending on the queue strategy, get the other queue's strategy state
+%%------------------------------------------------------------------------------
+-define(SYNC_REQ_KEY, "acdc.queue.sync_req.").
+-spec sync_req_routing_key/1 :: (wh_json:json_object() |
+                                           wh_proplist()
+                                          ) -> ne_binary().
+-spec sync_req_routing_key/2 :: (ne_binary(), ne_binary()) -> ne_binary().
+sync_req_routing_key(Props) when is_list(Props) ->
+    Id = props:get_value(<<"Queue-ID">>, Props, <<"*">>),
+    AcctId = props:get_value(<<"Account-ID">>, Props),
+    sync_req_routing_key(AcctId, Id);
+sync_req_routing_key(JObj) ->
+    Id = wh_json:get_value(<<"Queue-ID">>, JObj, <<"*">>),
+    AcctId = wh_json:get_value(<<"Account-ID">>, JObj),
+    sync_req_routing_key(AcctId, Id).
+
+sync_req_routing_key(AcctId, QID) ->
+    <<?SYNC_REQ_KEY, AcctId/binary, ".", QID/binary>>.
+
+-define(SYNC_REQ_HEADERS, [<<"Account-ID">>, <<"Queue-ID">>]).
+-define(OPTIONAL_SYNC_REQ_HEADERS, [<<"Process-ID">>, <<"Current-Strategy">>]).
+-define(SYNC_REQ_VALUES, [{<<"Event-Category">>, <<"queue">>}
+                          ,{<<"Event-Name">>, <<"sync_req">>}
+                         ]).
+-define(SYNC_REQ_TYPES, []).
+
+-spec sync_req/1 :: (api_terms()) ->
+                                 {'ok', iolist()} |
+                                 {'error', string()}.
+sync_req(Props) when is_list(Props) ->
+    case sync_req_v(Props) of
+        true -> wh_api:build_message(Props, ?SYNC_REQ_HEADERS, ?OPTIONAL_SYNC_REQ_HEADERS);
+        false -> {error, "Proplist failed validation for sync_req"}
+    end;
+sync_req(JObj) ->
+    sync_req(wh_json:to_proplist(JObj)).
+
+-spec sync_req_v/1 :: (api_terms()) -> boolean().
+sync_req_v(Prop) when is_list(Prop) ->
+    wh_api:validate(Prop, ?SYNC_REQ_HEADERS, ?SYNC_REQ_VALUES, ?SYNC_REQ_TYPES);
+sync_req_v(JObj) ->
+    sync_req_v(wh_json:to_proplist(JObj)).
+
+-define(SYNC_RESP_HEADERS, [<<"Account-ID">>, <<"Queue-ID">>
+                                ,<<"Current-Strategy">>, <<"Strategy-State">>
+                           ]).
+-define(OPTIONAL_SYNC_RESP_HEADERS, [<<"Process-ID">>, <<"Current-Strategy">>]).
+-define(SYNC_RESP_VALUES, [{<<"Event-Category">>, <<"queue">>}
+                           ,{<<"Event-Name">>, <<"sync_resp">>}
+                          ]).
+-define(SYNC_RESP_TYPES, []).
+
+-spec sync_resp/1 :: (api_terms()) ->
+                                 {'ok', iolist()} |
+                                 {'error', string()}.
+sync_resp(Props) when is_list(Props) ->
+    case sync_resp_v(Props) of
+        true -> wh_api:build_message(Props, ?SYNC_RESP_HEADERS, ?OPTIONAL_SYNC_RESP_HEADERS);
+        false -> {error, "Proplist failed validation for sync_resp"}
+    end;
+sync_resp(JObj) ->
+    sync_resp(wh_json:to_proplist(JObj)).
+
+-spec sync_resp_v/1 :: (api_terms()) -> boolean().
+sync_resp_v(Prop) when is_list(Prop) ->
+    wh_api:validate(Prop, ?SYNC_RESP_HEADERS, ?SYNC_RESP_VALUES, ?SYNC_RESP_TYPES);
+sync_resp_v(JObj) ->
+    sync_resp_v(wh_json:to_proplist(JObj)).
+
+
+%%------------------------------------------------------------------------------
 %% Bind/Unbind the queue as appropriate
 %%------------------------------------------------------------------------------
 
@@ -363,10 +439,12 @@ bind_q(Q, Props) ->
     AcctId = props:get_value(account_id, Props),
 
     amqp_util:callmgr_exchange(),
+    amqp_util:whapps_exchange(),
 
     bind_q(Q, AcctId, QID, props:get_value(restrict_to, Props)).
 
 bind_q(Q, AcctId, QID, undefined) ->
+    amqp_util:bind_q_to_whapps(Q, sync_req_routing_key(AcctId, QID)),
     amqp_util:bind_q_to_callmgr(Q, member_call_routing_key(AcctId, QID)),
     amqp_util:bind_q_to_callmgr(Q, member_connect_req_routing_key(AcctId, QID));
 bind_q(Q, AcctId, QID, [member_call|T]) ->
@@ -374,6 +452,9 @@ bind_q(Q, AcctId, QID, [member_call|T]) ->
     bind_q(Q, AcctId, QID, T);
 bind_q(Q, AcctId, QID, [member_connect_req|T]) ->
     amqp_util:bind_q_to_callmgr(Q, member_connect_req_routing_key(AcctId, QID)),
+    bind_q(Q, AcctId, QID, T);
+bind_q(Q, AcctId, QID, [sync_req|T]) ->
+    amqp_util:bind_q_to_whapps(Q, sync_req_routing_key(AcctId, QID)),
     bind_q(Q, AcctId, QID, T);
 bind_q(Q, AcctId, QID, [_|T]) ->
     bind_q(Q, AcctId, QID, T);
@@ -388,6 +469,7 @@ unbind_q(Q, Props) ->
     unbind_q(Q, AcctId, QID, props:get_value(restrict_to, Props)).
 
 unbind_q(Q, AcctId, QID, undefined) ->
+    amqp_util:unbind_q_from_whapps(Q, sync_req_routing_key(AcctId, QID)),
     amqp_util:unbind_q_from_callmgr(Q, member_call_routing_key(AcctId, QID)),
     amqp_util:unbind_q_from_callmgr(Q, member_connect_req_routing_key(AcctId, QID));
 unbind_q(Q, AcctId, QID, [member_call|T]) ->
@@ -395,6 +477,9 @@ unbind_q(Q, AcctId, QID, [member_call|T]) ->
     unbind_q(Q, AcctId, QID, T);
 unbind_q(Q, AcctId, QID, [member_connect_req|T]) ->
     amqp_util:unbind_q_from_callmgr(Q, member_connect_req_routing_key(AcctId, QID)),
+    unbind_q(Q, AcctId, QID, T);
+unbind_q(Q, AcctId, QID, [sync_req|T]) ->
+    amqp_util:unbind_q_from_whapps(Q, sync_req_routing_key(AcctId, QID)),
     unbind_q(Q, AcctId, QID, T);
 unbind_q(Q, AcctId, QID, [_|T]) ->
     unbind_q(Q, AcctId, QID, T);
@@ -482,4 +567,20 @@ publish_member_hungup(Q, JObj) ->
     publish_member_hungup(Q, JObj, ?DEFAULT_CONTENT_TYPE).
 publish_member_hungup(Q, API, ContentType) ->
     {ok, Payload} = wh_api:prepare_api_payload(API, ?MEMBER_HUNGUP_VALUES, fun member_hungup/1),
+    amqp_util:targeted_publish(Q, Payload, ContentType).
+
+-spec publish_sync_req/1 :: (api_terms()) -> 'ok'.
+-spec publish_sync_req/2 :: (api_terms(), ne_binary()) -> 'ok'.
+publish_sync_req(JObj) ->
+    publish_sync_req(JObj, ?DEFAULT_CONTENT_TYPE).
+publish_sync_req(API, ContentType) ->
+    {ok, Payload} = wh_api:prepare_api_payload(API, ?SYNC_REQ_VALUES, fun sync_req/1),
+    amqp_util:whapps_publish(sync_req_routing_key(API), Payload, ContentType).
+
+-spec publish_sync_resp/2 :: (ne_binary(), api_terms()) -> 'ok'.
+-spec publish_sync_resp/3 :: (ne_binary(), api_terms(), ne_binary()) -> 'ok'.
+publish_sync_resp(Q, JObj) ->
+    publish_sync_resp(Q, JObj, ?DEFAULT_CONTENT_TYPE).
+publish_sync_resp(Q, API, ContentType) ->
+    {ok, Payload} = wh_api:prepare_api_payload(API, ?SYNC_RESP_VALUES, fun sync_resp/1),
     amqp_util:targeted_publish(Q, Payload, ContentType).
