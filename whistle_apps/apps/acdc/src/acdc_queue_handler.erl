1dad339f879533a20848148036137b9900d97a79
James Aimonetti <james@2600hz.org>
2012-09-10 16:26:47 -0700

    WHISTLE-1441: handle stat queries to the queue manager

--- whistle_apps/apps/acdc/src/acdc_queue_handler.erl
+++ whistle_apps/apps/acdc/src/acdc_queue_handler.erl
@@ -14,7 +14,10 @@
          ,handle_member_accepted/2
          ,handle_member_retry/2
          ,handle_config_change/2
+         ,handle_stat_req/2
         ]).
+
+-include("acdc.hrl").
 
 handle_call_event(JObj, Props) ->
     {Cat, Name} = wh_util:get_event_type(JObj),
@@ -50,8 +53,54 @@ handle_queue_change(JObj, AcctId, QueueId, <<"doc_edited">>) ->
         undefined -> acdc_queues_sup:new(JObj);
         P when is_pid(P) -> acdc_queue_fsm:refresh(acdc_queue_sup:fsm(P), JObj)
     end;
-handle_queue_change(JObj, AcctId, QueueId, <<"doc_deleted">>) ->
+handle_queue_change(_JObj, AcctId, QueueId, <<"doc_deleted">>) ->
     case acdc_queues_sup:find_queue_supervisor(AcctId, QueueId) of
         undefined -> ok;
         P when is_pid(P) -> acdc_queue_sup:stop(P)
     end.
+
+handle_stat_req(JObj, _Props) ->
+    true = wapi_acdc_queue:stats_req_v(JObj),
+    handle_stat_req(wh_json:get_value(<<"Account-ID">>, JObj)
+                    ,wh_json:get_value(<<"Queue-ID">>, JObj)
+                    ,wh_json:get_value(<<"Server-ID">>, JObj)
+                   ).
+
+handle_stat_req(AcctId, undefined, ServerId) ->
+    lager:debug("fetch queue stats for all queues in ~s", [AcctId]),
+    case acdc_queues_sup:find_acct_queue_supervisors(AcctId) of
+        [] -> lager:debug("no queue processes");
+        Ps ->
+            Resp = [{<<"Account-ID">>, AcctId}
+                    ,{<<"Current-Queue-Stats">>, get_queues_stats(Ps)}
+                    ,{<<"Current-Account-Stats">>, acdc_stats:acct_stats(AcctId)}
+                    | wh_util:default_headers(?APP_NAME, ?APP_VERSION)
+                   ],
+            wapi_acdc_queue:publish_stats_resp(ServerId, Resp)
+    end;
+handle_stat_req(AcctId, QueueId, ServerId) ->
+    lager:debug("fetch queue stats for ~s in ~s", [QueueId, AcctId]),
+    case acdc_queues_sup:find_queue_supervisor(AcctId, QueueId) of
+        undefined -> lager:debug("no queue processes");
+        P ->
+            Resp = [{<<"Account-ID">>, AcctId}
+                    ,{<<"Current-Queue-Stats">>, get_queue_stats(P)}
+                    ,{<<"Current-Account-Stats">>, acdc_stats:acct_stats(AcctId, QueueId)}
+                    | wh_util:default_headers(?APP_NAME, ?APP_VERSION)
+                   ],
+            wapi_acdc_queue:publish_stats_resp(ServerId, Resp)
+    end.
+
+-spec get_queues_stats/1 :: ([pid(),...]) -> wh_json:json_objects().
+get_queues_stats(Ps) ->
+    [get_queue_stats(P) || P <- Ps].
+
+-spec get_queue_stats/1 :: (pid()) -> wh_json:json_object().
+get_queue_stats(P) ->
+    QueuePid = acdc_queue_sup:queue(P),
+    {AcctId, QueueId} = acdc_queue:config(QueuePid),
+    QueueSize = wapi_acdc_queue:queue_size(AcctId, QueueId),
+
+    wh_json:set_values([{<<"Queue-ID">>, QueueId}
+                        ,{<<"Queue-Size">>, QueueSize}
+                       ], wh_json:new()).
