e25672af5fe6abdd545b3302028e4aaf9f5879cd
James Aimonetti <james@2600hz.org>
2012-09-12 17:05:27 -0700

    WHISTLE-1441: get realtime stats for agent(s)

--- whistle_apps/apps/crossbar/src/modules/cb_agents.erl
+++ whistle_apps/apps/crossbar/src/modules/cb_agents.erl
@@ -8,12 +8,16 @@
 %%%
 %%% /agents/stats
 %%%   GET: stats for all agents
+%%% /agents/stats/realtime
+%%%   GET: stats for all agents - realtime
 %%%
 %%% /agents/AID
 %%%   GET: agent details
 %%%
 %%% /agents/AID/stats
 %%%   GET: stats on the agent
+%%% /agents/AID/stats/realtime
+%%%   GET: stats on the agent - realtime
 %%%
 %%% @end
 %%% @contributors:
@@ -23,15 +27,16 @@
 -module(cb_agents).
 
 -export([init/0
-         ,allowed_methods/0, allowed_methods/1, allowed_methods/2
-         ,resource_exists/0, resource_exists/1, resource_exists/2
-         ,validate/1, validate/2, validate/3
+         ,allowed_methods/0, allowed_methods/1, allowed_methods/2, allowed_methods/3
+         ,resource_exists/0, resource_exists/1, resource_exists/2, resource_exists/3
+         ,validate/1, validate/2, validate/3, validate/4
         ]).
 
 -include("include/crossbar.hrl").
 
 -define(CB_LIST, <<"agents/crossbar_listing">>).
 -define(STATS_PATH_TOKEN, <<"stats">>).
+-define(REALTIME_PATH_TOKEN, <<"realtime">>).
 
 %%%===================================================================
 %%% API
@@ -59,11 +64,16 @@ init() ->
 -spec allowed_methods/0 :: () -> http_methods() | [].
 -spec allowed_methods/1 :: (path_token()) -> http_methods() | [].
 -spec allowed_methods/2 :: (path_token(), path_token()) -> http_methods() | [].
+-spec allowed_methods/3 :: (path_token(), path_token(), path_token()) -> http_methods() | [].
 allowed_methods() ->
     ['GET'].
 allowed_methods(_) ->
     ['GET'].
 allowed_methods(_Id, ?STATS_PATH_TOKEN) ->
+    ['GET'];
+allowed_methods(?STATS_PATH_TOKEN, ?REALTIME_PATH_TOKEN) ->
+    ['GET'].
+allowed_methods(_Id, ?STATS_PATH_TOKEN, ?REALTIME_PATH_TOKEN) ->
     ['GET'].
 
 %%--------------------------------------------------------------------
@@ -78,9 +88,12 @@ allowed_methods(_Id, ?STATS_PATH_TOKEN) ->
 -spec resource_exists/0 :: () -> 'true'.
 -spec resource_exists/1 :: (path_token()) -> 'true'.
 -spec resource_exists/2 :: (path_token(), path_token()) -> 'true'.
+-spec resource_exists/3 :: (path_token(), path_token(), path_token()) -> 'true'.
 resource_exists() -> true.
 resource_exists(_) -> true.
-resource_exists(_, ?STATS_PATH_TOKEN) -> true.
+resource_exists(_, ?STATS_PATH_TOKEN) -> true;
+resource_exists(?STATS_PATH_TOKEN, ?REALTIME_PATH_TOKEN) -> true.
+resource_exists(_, ?STATS_PATH_TOKEN, ?REALTIME_PATH_TOKEN) -> true.
 
 %%--------------------------------------------------------------------
 %% @public
@@ -95,6 +108,7 @@ resource_exists(_, ?STATS_PATH_TOKEN) -> true.
 -spec validate/1 :: (#cb_context{}) -> #cb_context{}.
 -spec validate/2 :: (#cb_context{}, path_token()) -> #cb_context{}.
 -spec validate/3 :: (#cb_context{}, path_token(), path_token()) -> #cb_context{}.
+-spec validate/4 :: (#cb_context{}, path_token(), path_token(), path_token()) -> #cb_context{}.
 validate(#cb_context{req_verb = <<"get">>}=Context) ->
     summary(Context).
 
@@ -103,9 +117,14 @@ validate(#cb_context{req_verb = <<"get">>}=Context, ?STATS_PATH_TOKEN) ->
 validate(#cb_context{req_verb = <<"get">>}=Context, Id) ->
     read(Id, Context).
 
+validate(#cb_context{req_verb = <<"get">>}=Context, ?STATS_PATH_TOKEN, ?REALTIME_PATH_TOKEN) ->
+    fetch_all_agent_stats(Context, ?REALTIME_PATH_TOKEN);
 validate(#cb_context{req_verb = <<"get">>}=Context, Id, ?STATS_PATH_TOKEN) ->
     %% read agent stats with id=Id
     fetch_agent_stats(Id, Context).
+
+validate(#cb_context{req_verb = <<"get">>}=Context, Id, ?STATS_PATH_TOKEN, ?REALTIME_PATH_TOKEN) ->
+    fetch_agent_stats(Id, Context, ?REALTIME_PATH_TOKEN).
 
 %%--------------------------------------------------------------------
 %% @private
@@ -118,7 +137,26 @@ read(Id, Context) ->
     crossbar_doc:load(Id, Context).
 
 fetch_all_agent_stats(Context) ->
-    crossbar_doc:load_view(<<"acdc_stats/stats_per_agent">>, [], Context, fun normalize_agent_results/2).
+    fetch_all_agent_stats(Context, history).
+fetch_all_agent_stats(Context, history) ->
+    crossbar_doc:load_view(<<"acdc_stats/stats_per_agent">>, [], Context, fun normalize_agent_results/2);
+fetch_all_agent_stats(#cb_context{account_id=AcctId}=Context, ?REALTIME_PATH_TOKEN) ->
+    Req = [{<<"Account-ID">>, AcctId}
+           | wh_api:default_headers(?APP_NAME, ?APP_VERSION)
+          ],
+    case whapps_util:amqp_pool_request(Req
+                                       ,fun wapi_acdc_agent:publish_stats_req/1
+                                       ,fun wapi_acdc_agent:stats_resp_v/1
+                                       ,2000
+                                      ) of
+        {ok, Resp} ->
+            Resp1 = wh_json:normalize(Resp),
+            Context#cb_context{resp_status=success
+                               ,resp_data=Resp1
+                               ,doc=Resp1
+                              };
+        {error, _} -> Context
+    end.
 
 fetch_agent_stats(Id, Context) ->
     crossbar_doc:load_view(<<"acdc_stats/stats_per_agent">>
@@ -129,6 +167,24 @@ fetch_agent_stats(Id, Context) ->
                            ,Context
                            ,fun normalize_agent_results/2
                           ).
+fetch_agent_stats(Id, #cb_context{account_id=AcctId}=Context, ?REALTIME_PATH_TOKEN) ->
+    Req = [{<<"Account-ID">>, AcctId}
+           ,{<<"Agent-ID">>, Id}
+           | wh_api:default_headers(?APP_NAME, ?APP_VERSION)
+          ],
+    case whapps_util:amqp_pool_request(Req
+                                       ,fun wapi_acdc_agent:publish_stats_req/1
+                                       ,fun wapi_acdc_agent:stats_resp_v/1
+                                       ,2000
+                                      ) of
+        {ok, Resp} ->
+            Resp1 = wh_json:normalize(Resp),
+            Context#cb_context{resp_status=success
+                               ,resp_data=Resp1
+                               ,doc=Resp1
+                              };
+        {error, _} -> Context
+    end.
 
 %%--------------------------------------------------------------------
 %% @private
