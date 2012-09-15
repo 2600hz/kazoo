e9a193fc51323a068e9fb05a211d4313af2b8058
James Aimonetti <james@2600hz.org>
2012-09-14 17:14:55 -0700

    WHISTLE-1441: update the filter for binding

--- whistle_apps/apps/acdc/src/acdc_agent_manager.erl
+++ whistle_apps/apps/acdc/src/acdc_agent_manager.erl
@@ -30,7 +30,7 @@
 -define(SERVER, ?MODULE).
 
 -define(BINDINGS, [{conf, [{doc_type, <<"user">>}]}
-                   ,{acdc_agent, [{restrict_to, [status, stats]}]}
+                   ,{acdc_agent, [{restrict_to, [status, stats_req]}]}
                   ]).
 -define(RESPONDERS, [{{acdc_agent_handler, handle_status_update} 
                       ,[{<<"agent">>, <<"login">>}
