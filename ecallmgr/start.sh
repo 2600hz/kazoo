6b2e1e010265c5e3696a301f6ef013fb6e9fbfcb
karl anderson <karl@2600hz.com>
2012-10-09 01:25:57 +0000

    WHISTLE-42: rename the crash dump so they are not overwritten

--- ecallmgr/start.sh
+++ ecallmgr/start.sh
@@ -3,4 +3,5 @@
 cd `dirname $0`
 
 export ERL_LIBS=$PWD/../lib
+export ERL_CRASH_DUMP=$(date +%s)_erl_crash.dump
 exec erl -args_file $PWD/conf/vm.args -pa $PWD/deps/*/ebin $PWD/ebin -heart -detached -s ecallmgr
