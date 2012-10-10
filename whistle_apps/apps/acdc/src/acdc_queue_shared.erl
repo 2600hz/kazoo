80e473954e3dfefa3ef1a7e17c9d589befa0c474
James Aimonetti <james@2600hz.org>
2012-10-10 10:36:56 -0700

    WHISTLE-1441: fix startup config params

--- whistle_apps/apps/acdc/src/acdc_queue_shared.erl
+++ whistle_apps/apps/acdc/src/acdc_queue_shared.erl
@@ -34,7 +34,7 @@
                                                     ,{exclusive, false}
                                                    ]}
                                  ,{basic_qos, 1}
-                                 ,{bind_options, [{exclusive, false}]}
+                                 ,{queue_options, [{exclusive, false}]}
                                 ]).
 
 -define(SHARED_QUEUE_BINDINGS(AcctId, QueueId), [{self, []}]).
