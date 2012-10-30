be192d8459569b1b89d2374abee2c3255d4f9f3f
James Aimonetti <james@2600hz.org>
2012-10-29 17:54:44 -0700

    WHISTLE-1441: use the API status updates instead of rolling it again on Agent BLF toggle

--- whistle_apps/apps/acdc/src/acdc_handlers.erl
+++ whistle_apps/apps/acdc/src/acdc_handlers.erl
@@ -55,10 +55,17 @@ send_route_response(ReqJObj, Call, AcctId, Id, Type) ->
 
 handle_route_win(JObj, _Props) ->
     true = wapi_route:win_v(JObj),
+
     case whapps_call:retrieve(wh_json:get_value(<<"Call-ID">>, JObj)) of
         {ok, Call} ->
-            update_acdc_actor(whapps_call:from_route_win(JObj, Call)),
-            whapps_call_command:hangup(Call);
+            Call1 = whapps_call:from_route_win(JObj, Call),
+            whapps_call_command:answer(Call1),
+
+            case catch update_acdc_actor(Call1) of
+                {'EXIT', _R} -> lager:debug("crash: ~p", [_R]);
+                _R -> timer:sleep(5000), lager:debug("update resulted in ~p", [_R])
+            end,
+            whapps_call_command:hangup(Call1);
         {error, _R} ->
             lager:debug("failed to retrieve cached call: ~p", [_R])
     end.
@@ -68,30 +75,46 @@ update_acdc_actor(Call) ->
                       ,whapps_call:custom_channel_var(<<"ACDc-ID">>, Call)
                       ,whapps_call:custom_channel_var(<<"ACDc-Type">>, Call)
                      ).
-update_acdc_actor(Call, undefined, _) -> ok;
-update_acdc_actor(Call, _, undefined) -> ok;
-update_acdc_actor(Call, QueueId, <<"queue">>) -> whapps_call_command:hangup(Call);
+update_acdc_actor(_Call, undefined, _) -> ok;
+update_acdc_actor(_Call, _, undefined) -> ok;
+update_acdc_actor(Call, _QueueId, <<"queue">>) -> whapps_call_command:hangup(Call);
 update_acdc_actor(Call, AgentId, <<"user">>) ->
     AcctId = whapps_call:account_id(Call),
 
     case acdc_util:agent_status(AcctId, AgentId) of
-        <<"logout">> ->
-            {ok, Doc} = couch_mgr:open_cache_doc(whapps_call:account_db(Call), AgentId),
-            lager:debug("recv win to toggle ~s(~s) from logout", [AgentId, AcctId]),
-            acdc_agents_sup:new(Doc),
-            whapps_call_command:b_prompt(<<"agent-logged_in">>, Call);
-        <<"login">> ->
-            lager:debug("recv win to toggle ~s(~s) from login", [AgentId, AcctId]),
-            Super = acdc_agents_sup:find_agent_supervisor(AcctId, AgentId),
-            whapps_call_command:b_prompt(<<"agent-logged_out">>, Call)
-    ok.
+        <<"logout">> -> update_acdc_agent(Call, AcctId, AgentId, <<"login">>, fun wapi_acdc_agent:publish_login/1);
+        <<"pause">> -> update_acdc_agent(Call, AcctId, AgentId, <<"resume">>, fun wapi_acdc_agent:publish_resume/1);
+        _ -> update_acdc_agent(Call, AcctId, AgentId, <<"logout">>, fun wapi_acdc_agent:publish_logout/1)
+    end.
 
-update_agent_status() ->
+update_acdc_agent(Call, AcctId, AgentId, Status, PubFun) ->
+    lager:debug("going to new status ~s", [Status]),
+
     AcctDb = whapps_call:account_db(Call),
     Doc = wh_json:from_list([{<<"call_id">>, whapps_call:call_id(Call)}
                              ,{<<"agent_id">>, AgentId}
-                             ,{<<"method">>, <<"callflow">>}
+                             ,{<<"method">>, <<"acdc_blf">>}
                              ,{<<"action">>, Status}
                              ,{<<"pvt_type">>, <<"agent_activity">>}
                             ]),
     {ok, _D} = couch_mgr:save_doc(AcctDb, wh_doc:update_pvt_parameters(Doc, AcctDb)),
+    lager:debug("saved status doc ~p", [_D]),
+
+    send_new_status(AcctId, AgentId, PubFun),
+    play_status_prompt(Call, Status).
+
+-spec send_new_status/3 :: (ne_binary(), ne_binary(), wh_amqp_worker:publish_fun()) -> 'ok'.
+send_new_status(AcctId, AgentId, PubFun) ->
+    Update = props:filter_undefined(
+               [{<<"Account-ID">>, AcctId}
+                ,{<<"Agent-ID">>, AgentId}
+                | wh_api:default_headers(?APP_NAME, ?APP_VERSION)
+               ]),
+    PubFun(Update).
+
+play_status_prompt(Call, <<"login">>) ->
+    whapps_call_command:prompt(<<"agent-logged_in">>, Call);
+play_status_prompt(Call, <<"logout">>) ->
+    whapps_call_command:prompt(<<"agent-logged_out">>, Call);
+play_status_prompt(Call, <<"resume">>) ->
+    whapps_call_command:prompt(<<"agent-resume">>, Call).
