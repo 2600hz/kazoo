edd7a8c9172f317169a52abe22893ba3d0ca170e
James Aimonetti <james@2600hz.org>
2012-11-14 14:10:19 -0800

    WHISTLE-1769: add flush, don't cache the fetched doc via couch_mgr

--- whistle_apps/src/whapps_account_config.erl
+++ whistle_apps/src/whapps_account_config.erl
@@ -13,6 +13,7 @@
 
 -export([get/2, get/3, get/4
          ,set/4
+         ,flush/1, flush/2
         ]).
 
 -spec get/2 :: (ne_binary(), ne_binary()) -> wh_json:json_object().
@@ -22,8 +23,8 @@ get(Account, Config) ->
         {ok, JObj} -> JObj;
         {error, not_found} ->
             AccountDb = wh_util:format_account_id(Account, encoded),
-            DocId = <<(?WH_ACCOUNT_CONFIGS)/binary, Config/binary>>,
-            case couch_mgr:open_cache_doc(AccountDb, DocId) of
+            DocId = config_doc_id(Config),
+            case couch_mgr:open_doc(AccountDb, DocId) of
                 {error, _} -> wh_json:set_value(<<"_id">>, DocId, wh_json:new());
                 {ok, JObj} ->
                     wh_cache:store_local(?WHAPPS_CONFIG_CACHE, {?MODULE, Config, AccountId}, JObj),
@@ -31,21 +32,45 @@ get(Account, Config) ->
             end
     end.
 
--spec get/3 :: (ne_binary(), ne_binary(), wh_json:json_string() | wh_json:json_strings()) -> wh_json:json_term() | 'undefined'.
--spec get/4 :: (ne_binary(), ne_binary(), wh_json:json_string() | wh_json:json_strings(), Default) -> wh_json:json_term() | Default.
+-spec flush/1 :: (ne_binary()) -> 'ok'.
+-spec flush/2 :: (ne_binary(), ne_binary()) -> 'ok'.
+flush(Account) ->
+    AccountId = wh_util:format_account_id(Account, raw),
+    wh_cache:filter_local(?WHAPPS_CONFIG_CACHE
+                          ,fun({?MODULE, _Config, AcctId}=K, _V) when AcctId =:= AccountId ->
+                                   wh_cache:erase_local(?WHAPPS_CONFIG_CACHE, K),
+                                   true;
+                              (_, _) ->
+                                   false
+                           end
+                         ),
+    ok.
+flush(Account, Config) ->
+    AccountId = wh_util:format_account_id(Account, raw),
+    wh_cache:erase_local(?WHAPPS_CONFIG_CACHE, {?MODULE, Config, AccountId}).
+    
+
+-spec get/3 :: (ne_binary(), ne_binary(), wh_json:json_string() | wh_json:json_strings()) ->
+                       wh_json:json_term() | 'undefined'.
+-spec get/4 :: (ne_binary(), ne_binary(), wh_json:json_string() | wh_json:json_strings(), Default) ->
+                       wh_json:json_term() | Default.
 get(Account, Config, Key) ->
-    get(Account, Config, Key, undefined).
+    get(Account, Config, Key, 'undefined').
 get(Account, Config, Key, Default) ->
     wh_json:get_value(Key, get(Account, Config), Default).
 
--spec set/4 :: (ne_binary(), ne_binary(), wh_json:json_string() | wh_json:json_strings(), wh_json:json_term()) -> wh_json:json_object().
+-spec set/4 :: (ne_binary(), ne_binary(), wh_json:json_string() | wh_json:json_strings(), wh_json:json_term()) ->
+                       wh_json:json_object().
 set(Account, Config, Key, Value) ->
-    JObj = ?MODULE:get(Account, Config),
-    JObj1 = wh_json:set_value(Key, Value, JObj),
+    JObj = wh_json:set_value(Key, Value, ?MODULE:get(Account, Config)),
 
     AccountId = wh_util:format_account_id(Account, raw),
     AccountDb = wh_util:format_account_id(Account, encoded),
 
-    {ok, JObj2} = couch_mgr:ensure_saved(AccountDb, JObj1),
+    {ok, JObj1} = couch_mgr:ensure_saved(AccountDb
+                                         ,wh_doc:update_pvt_parameters(JObj, AccountDb, [{type, <<"account_config">>}])
+                                        ),
     wh_cache:erase_local(?WHAPPS_CONFIG_CACHE, {?MODULE, Config, AccountId}),
-    JObj2.
+    JObj1.
+
+config_doc_id(Config) -> <<(?WH_ACCOUNT_CONFIGS)/binary, Config/binary>>.
