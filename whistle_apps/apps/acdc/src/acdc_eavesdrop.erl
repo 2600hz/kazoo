fb4b6c1c65e48b64ac633dc2624fe11e53f8aed3
James Aimonetti <james@2600hz.org>
2012-11-30 12:50:35 -0800

    WHISTLE-1441: eavesdrop prototype for testing

---
+++ whistle_apps/apps/acdc/src/acdc_eavesdrop.erl
@@ -0,0 +1,69 @@
+%%%-------------------------------------------------------------------
+%%% @author James Aimonetti <>
+%%% @copyright (C) 2012, James Aimonetti
+%%% @doc
+%%%
+%%% @end
+%%% Created : 29 Nov 2012 by James Aimonetti <>
+%%%-------------------------------------------------------------------
+-module(acdc_eavesdrop).
+
+-include("acdc.hrl").
+
+-export([start/2]).
+
+start(AcctId, CallId) ->
+    EPId = <<"5381e0c5caa8d34eec06e0f75d0b4189">>,
+    {ok, EPDoc} = couch_mgr:open_cache_doc(wh_util:format_account_id(AcctId, encoded), EPId),
+    {ok, [EP]} = cf_endpoint:build(EPDoc, new_call(AcctId)),
+
+    CCVs = props:filter_undefined([{<<"Account-ID">>, AcctId}]),
+
+    Prop = props:filter_undefined(
+             [{<<"Msg-ID">>, wh_util:rand_hex_binary(6)}
+              ,{<<"Custom-Channel-Vars">>, wh_json:from_list(CCVs)}
+              ,{<<"Timeout">>, 8}
+              ,{<<"Endpoints">>, [wh_json:set_values([{<<"Endpoint-Timeout">>, 8}
+                                                      ,{<<"Outgoing-Caller-ID-Name">>, <<"eavesdrop">>}
+                                                      ,{<<"Outgoing-Caller-ID-Number">>, <<"01010101">>}
+                                                     ], EP)
+                                 ]
+               }
+              ,{<<"Export-Custom-Channel-Vars">>, [<<"Account-ID">>
+                                                   ,<<"Retain-CID">>
+                                                   ,<<"Authorizing-ID">>
+                                                   ,<<"Authorizing-Type">>
+                                                  ]}
+              ,{<<"Account-ID">>, AcctId}
+              ,{<<"Resource-Type">>, <<"originate">>}
+              ,{<<"Application-Name">>, <<"eavesdrop">>}
+              ,{<<"Eavesdrop-Call-ID">>, CallId}
+              | wh_api:default_headers(?APP_NAME, ?APP_VERSION)
+             ]),
+
+    lager:debug("sending eavesdrop request: ~p", [Prop]),
+
+    case whapps_util:amqp_pool_request(Prop
+                                       ,fun wapi_resource:publish_originate_req/1
+                                       ,fun wapi_dialplan:originate_ready_v/1
+                                      ) of
+        {ok, JObj} ->
+            lager:debug("originate is ready to execute"),
+            send_originate_execute(JObj, wh_json:get_value(<<"Server-ID">>, JObj));
+        {error, E} ->
+            lager:debug("error originating: ~p", [E])
+    end.
+
+new_call(AcctId) ->
+    whapps_call:from_json(wh_json:from_list([{<<"Account-ID">>, AcctId}
+                                             ,{<<"Account-DB">>, wh_util:format_account_id(AcctId, encoded)}
+                                            ])).
+
+send_originate_execute(JObj, Q) ->
+    Prop = [{<<"Call-ID">>, wh_json:get_value(<<"Call-ID">>, JObj)}
+            ,{<<"Msg-ID">>, wh_json:get_value(<<"Msg-ID">>, JObj)}
+            | wh_api:default_headers(Q, ?APP_NAME, ?APP_VERSION)
+           ],
+    wapi_dialplan:publish_originate_execute(wh_json:get_value(<<"Server-ID">>, JObj), Prop).
+
+
