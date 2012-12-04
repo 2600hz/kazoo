99d2d490d1d5d27d208ec1c3362f1db8f00b747e
James Aimonetti <james@2600hz.org>
2012-12-03 17:34:55 -0800

    WHISTLE-1441: change eavesdrop to use the SpyVsSpy app to make the eavesdrop request

--- whistle_apps/apps/acdc/src/acdc_eavesdrop.erl
+++ whistle_apps/apps/acdc/src/acdc_eavesdrop.erl
@@ -12,59 +12,26 @@
 
 -export([start/3]).
 
-start(MemberCall, AcctId, AgentCallId) ->
-    EPId = <<"5381e0c5caa8d34eec06e0f75d0b4189">>,
-    {ok, EPDoc} = couch_mgr:open_cache_doc(wh_util:format_account_id(AcctId, encoded), EPId),
-    {ok, [EP]} = cf_endpoint:build(EPDoc, new_call(AcctId)),
-
-    CCVs = props:filter_undefined([{<<"Account-ID">>, AcctId}]),
-
-    Prop = props:filter_undefined(
-             [{<<"Msg-ID">>, wh_util:rand_hex_binary(6)}
-              ,{<<"Custom-Channel-Vars">>, wh_json:from_list(CCVs)}
-              ,{<<"Timeout">>, 8}
-              ,{<<"Endpoints">>, [wh_json:set_values([{<<"Endpoint-Timeout">>, 8}
-                                                      ,{<<"Outgoing-Caller-ID-Name">>, <<"eaves:", (whapps_call:caller_id_name(MemberCall))/binary>>}
-                                                      ,{<<"Outgoing-Caller-ID-Number">>, whapps_call:caller_id_number(MemberCall)}
-                                                     ], EP)
-                                 ]
-               }
-              ,{<<"Export-Custom-Channel-Vars">>, [<<"Account-ID">>
-                                                   ,<<"Retain-CID">>
-                                                   ,<<"Authorizing-ID">>
-                                                   ,<<"Authorizing-Type">>
-                                                  ]}
-              ,{<<"Account-ID">>, AcctId}
-              ,{<<"Resource-Type">>, <<"originate">>}
-              ,{<<"Application-Name">>, <<"eavesdrop">>}
-              ,{<<"Eavesdrop-Call-ID">>, AgentCallId}
-              ,{<<"Eavesdrop-Mode">>, <<"full">>}
-              | wh_api:default_headers(?APP_NAME, ?APP_VERSION)
-             ]),
-
-    lager:debug("sending eavesdrop request: ~p", [Prop]),
-
-    case whapps_util:amqp_pool_request(Prop
-                                       ,fun wapi_resource:publish_originate_req/1
-                                       ,fun wapi_dialplan:originate_ready_v/1
-                                      ) of
-        {ok, JObj} ->
-            lager:debug("originate is ready to execute"),
-            send_originate_execute(JObj, wh_json:get_value(<<"Server-ID">>, JObj));
-        {error, E} ->
-            lager:debug("error originating: ~p", [E])
-    end.
-
-new_call(AcctId) ->
-    whapps_call:from_json(wh_json:from_list([{<<"Account-ID">>, AcctId}
-                                             ,{<<"Account-DB">>, wh_util:format_account_id(AcctId, encoded)}
-                                            ])).
-
-send_originate_execute(JObj, Q) ->
-    Prop = [{<<"Call-ID">>, wh_json:get_value(<<"Call-ID">>, JObj)}
-            ,{<<"Msg-ID">>, wh_json:get_value(<<"Msg-ID">>, JObj)}
-            | wh_api:default_headers(Q, ?APP_NAME, ?APP_VERSION)
+start(MCall, AcctId, AgentCallId) ->
+    Prop = [{<<"Eavesdrop-Mode">>, <<"listen">>}
+            ,{<<"Account-ID">>, AcctId}
+            ,{<<"Endpoint-ID">>, <<"5381e0c5caa8d34eec06e0f75d0b4189">>}
+            ,{<<"Eavesdrop-Call-ID">>, AgentCallId}
+            ,{<<"Outgoing-Caller-ID-Name">>, whapps_call:caller_id_name(MCall)}
+            ,{<<"Outgoing-Caller-ID-Number">>, whapps_call:caller_id_number(MCall)}
+            | wh_api:default_headers(?APP_NAME, ?APP_VERSION)
            ],
-    wapi_dialplan:publish_originate_execute(wh_json:get_value(<<"Server-ID">>, JObj), Prop).
+    eavesdrop_req(Prop).
 
-
+eavesdrop_req(Prop) ->
+    lager:debug("Sending eavs ~p", [Prop]),
+    case whapps_util:amqp_pool_request(Prop
+                                       ,fun wapi_resource:publish_eavesdrop_req/1
+                                       ,fun wapi_resource:eavesdrop_resp_v/1
+                                       ,2000
+                                      )
+    of
+        {ok, Resp} -> lager:debug("ok: ~p", [Resp]);
+        {error, timeout} -> lager:debug("err: timeout");
+        {error, E} -> lager:debug("err: ~p", [E])
+    end.
