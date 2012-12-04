3a84d35cd95e1926e2fe7369c42bf7738f20a0c0
James Aimonetti <james@2600hz.org>
2012-12-03 17:35:18 -0800

    WHISTLE-1441: update of types in specs

--- whistle_apps/apps/callflow/src/cf_route_win.erl
+++ whistle_apps/apps/callflow/src/cf_route_win.erl
@@ -12,7 +12,7 @@
 
 -export([handle_req/2]).
 
--spec handle_req/2 :: (wh_json:json_object(), proplist()) -> any().
+-spec handle_req/2 :: (wh_json:object(), proplist()) -> any().
 handle_req(JObj, _Options) ->
     CallId = wh_json:get_value(<<"Call-ID">>, JObj),
     put(callid, CallId),
@@ -32,7 +32,7 @@ handle_req(JObj, _Options) ->
 %%
 %% @end
 %%-----------------------------------------------------------------------------
--spec bootstrap_callflow_executer/2 :: (wh_json:json_object(), whapps_call:call()) -> {'ok', pid()}.
+-spec bootstrap_callflow_executer/2 :: (wh_json:object(), whapps_call:call()) -> {'ok', pid()}.
 bootstrap_callflow_executer(JObj, Call) ->
     lists:foldl(fun(F, C) -> F(C) end
                 ,whapps_call:from_route_win(JObj, Call)
