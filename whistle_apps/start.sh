67845690f670d4472146fc3c52c70a0984ef8338
karl anderson <karl@2600hz.com>
2012-12-11 19:02:18 +0000

    WHISTLE-42: remove any remaining empty lib folders on startup

--- whistle_apps/start.sh
+++ whistle_apps/start.sh
@@ -2,6 +2,8 @@
 
 cd `dirname $0`
 
-export ERL_LIBS=$PWD/../lib:$PWD/lib
+export ERL_LIBS=$PWD/../lib:$PWD/lib:$PWD/apps/*/lib
 export ERL_CRASH_DUMP=$(date +%s)_erl_crash.dump
-exec erl -args_file $PWD/conf/vm.args -pa $PWD/deps/*/ebin $PWD/ebin $PWD/apps/*/ebin -detached -s whistle_apps
+
+for path in $PWD/lib/*; do folder=$(basename $path); if [ "$folder" != "Makefile" ]; then echo "# rm -rf $PWD/../lib/$folder"; rm -rf $PWD/../lib/$folder; fi; done
+exec erl -args_file $PWD/conf/vm.args -pa $PWD/deps/*/ebin $PWD/ebin $PWD/apps/*/ebin $PWD/apps/*/lib/*/ebin -detached -s whistle_apps
