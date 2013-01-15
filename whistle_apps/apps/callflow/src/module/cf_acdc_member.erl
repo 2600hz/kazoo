803d358542869c37f163f1d6e9724240eea466a2
James Aimonetti <james@2600hz.org>
2013-01-14 23:00:25 -0800

    WHISTLE-1720: process member_cancel in callflow

--- whistle_apps/apps/callflow/src/module/cf_acdc_member.erl
+++ whistle_apps/apps/callflow/src/module/cf_acdc_member.erl
@@ -26,7 +26,7 @@
 -spec handle/2 :: (wh_json:json_object(), whapps_call:call()) -> 'ok'.
 handle(Data, Call) ->
     QueueId = wh_json:get_value(<<"id">>, Data),
-    lager:debug("sending call to queue ~s", [QueueId]),
+    lager:info("sending call to queue ~s", [QueueId]),
 
     MemberCall = props:filter_undefined(
                    [{<<"Account-ID">>, whapps_call:account_id(Call)}
@@ -35,7 +35,7 @@ handle(Data, Call) ->
                     | wh_api:default_headers(?APP_NAME, ?APP_VERSION)
                    ]),
 
-    lager:debug("loading ACDc queue: ~s", [QueueId]),
+    lager:info("loading ACDc queue: ~s", [QueueId]),
     {ok, QueueJObj} = couch_mgr:open_cache_doc(whapps_call:account_db(Call), QueueId),
 
     MaxWait = max_wait(wh_json:get_integer_value(<<"connection_timeout">>, QueueJObj, 3600)),
@@ -45,16 +45,16 @@ handle(Data, Call) ->
 
     CurrQueueSize = wapi_acdc_queue:queue_size(whapps_call:account_id(Call1), QueueId),
 
-    lager:debug("max size: ~p curr size: ~p", [MaxQueueSize, CurrQueueSize]),
+    lager:info("max size: ~p curr size: ~p", [MaxQueueSize, CurrQueueSize]),
 
     maybe_enter_queue(Call1, MemberCall, QueueId, MaxWait, is_queue_full(MaxQueueSize, CurrQueueSize)).
 
 -spec maybe_enter_queue/5 :: (whapps_call:call(), wh_proplist(), ne_binary(), max_wait(), boolean()) -> 'ok'.
 maybe_enter_queue(Call, _, _, _, true) ->
-    lager:debug("queue has reached max size"),
+    lager:info("queue has reached max size"),
     cf_exe:continue(Call);
 maybe_enter_queue(Call, MemberCall, QueueId, MaxWait, false) ->
-    lager:debug("asking for an agent, waiting up to ~p ms", [MaxWait]),
+    lager:info("asking for an agent, waiting up to ~p ms", [MaxWait]),
 
     cf_exe:send_amqp(Call, MemberCall, fun wapi_acdc_queue:publish_member_call/1),
     wait_for_bridge(whapps_call:kvs_store(queue_id, QueueId, Call), MaxWait).
@@ -69,10 +69,10 @@ wait_for_bridge(Call, Timeout, Start) ->
         {amqp_msg, JObj} ->
             process_message(Call, Timeout, Start, Wait, JObj, wh_util:get_event_type(JObj));
         _Msg ->
-            lager:debug("popping msg off: ~p", [_Msg]),
+            lager:info("popping msg off: ~p", [_Msg]),
             wait_for_bridge(Call, Timeout, Start)
     after Timeout ->
-            lager:debug("failed to handle the call in time, proceeding"),
+            lager:info("failed to handle the call in time, proceeding"),
             cancel_member_call(Call, <<"member_timeout">>),
             cf_exe:continue(Call)
     end.
@@ -82,15 +82,15 @@ wait_for_bridge(Call, Timeout, Start) ->
                             ,{ne_binary(), ne_binary()}
                            ) -> 'ok'.
 process_message(Call, _, Start, _Wait, _JObj, {<<"call_event">>,<<"CHANNEL_BRIDGE">>}) ->
-    lager:debug("member was bridged to agent, yay! took ~b s", [wh_util:elapsed_s(Start)]),
+    lager:info("member was bridged to agent, yay! took ~b s", [wh_util:elapsed_s(Start)]),
     cf_exe:control_usurped(Call);
 process_message(Call, _, Start, _Wait, _JObj, {<<"call_event">>,<<"CHANNEL_HANGUP_COMPLETE">>}) ->
-    lager:debug("member hungup while waiting in the queue (was there ~b s)", [wh_util:elapsed_s(Start)]),
+    lager:info("member hungup while waiting in the queue (was there ~b s)", [wh_util:elapsed_s(Start)]),
     cancel_member_call(Call, <<"member_hungup">>),
     cf_exe:stop(Call);
 process_message(Call, _, Start, _Wait, JObj, {<<"member">>, <<"call_fail">>}) ->
     Failure = wh_json:get_value(<<"Failure-Reason">>, JObj),
-    lager:debug("call failed to be processed: ~s (took ~b s)"
+    lager:info("call failed to be processed: ~s (took ~b s)"
                 ,[Failure, wh_util:elapsed_s(Start)]
                ),
     cancel_member_call(Call, Failure),
@@ -99,18 +99,23 @@ process_message(Call, Timeout, Start, Wait, JObj, {<<"call_event">>, <<"DTMF">>}
     DigitPressed = wh_json:get_value(<<"DTMF-Digit">>, JObj),
     case DigitPressed =:= whapps_call:kvs_fetch(caller_exit_key, Call) of
         true ->
-            lager:debug("caller pressed the exit key(~s), moving to next callflow action", [DigitPressed]),
+            lager:info("caller pressed the exit key(~s), moving to next callflow action", [DigitPressed]),
             cancel_member_call(Call, <<"dtmf_exit">>),
             cf_exe:continue(Call);
         false ->
-            lager:debug("caller pressed ~s, ignoring", [DigitPressed]),
+            lager:info("caller pressed ~s, ignoring", [DigitPressed]),
             wait_for_bridge(Call, reduce_timeout(Timeout, wh_util:elapsed_ms(Wait)), Start)
     end;
 process_message(Call, _, Start, _Wait, _JObj, {<<"member">>, <<"call_success">>}) ->
-    lager:debug("call was processed by queue (took ~b s)", [wh_util:elapsed_s(Start)]),
+    lager:info("call was processed by queue (took ~b s)", [wh_util:elapsed_s(Start)]),
     cf_exe:control_usurped(Call);
+process_message(Call, _, Start, _Wait, JObj, {<<"member">>, <<"call_cancel">>}) ->
+    lager:info("call was cancelled by queue (took ~b s): ~s, continuing callflow"
+                ,[wh_util:elapsed_s(Start), wh_json:get_value(<<"Reason">>, JObj)]
+               ),
+    cf_exe:continue(Call);
 process_message(Call, Timeout, Start, Wait, _JObj, _Type) ->
-    %%lager:debug("recv unhandled message: ~p: ~s", [_Type, wh_json:get_value(<<"Application-Name">>, _JObj)]),
+    %%lager:info("recv unhandled message: ~p: ~s", [_Type, wh_json:get_value(<<"Application-Name">>, _JObj)]),
     wait_for_bridge(Call, reduce_timeout(Timeout, wh_util:elapsed_ms(Wait)), Start).
 
 -spec reduce_timeout/2 :: (max_wait(), integer()) -> max_wait().
