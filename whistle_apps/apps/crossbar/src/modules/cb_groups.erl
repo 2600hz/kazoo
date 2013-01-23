0c916b68f29b1ca9eb77dc4ef326c51ef40f28c9
karl anderson <karl@2600hz.com>
2013-01-23 01:20:21 +0000

    WHISTLE-1882: start of a simple groups concept

---
+++ whistle_apps/apps/crossbar/src/modules/cb_groups.erl
@@ -0,0 +1,193 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2011-2012, VoIP INC
+%%% @doc
+%%%
+%%% Listing of all expected v1 callbacks
+%%%
+%%% @end
+%%% @contributors:
+%%%   Karl Anderson
+%%%   James Aimonetti
+%%%-------------------------------------------------------------------
+-module(cb_groups).
+
+-export([init/0
+         ,allowed_methods/0, allowed_methods/1
+         ,resource_exists/0, resource_exists/1
+         ,validate/1, validate/2
+         ,put/1
+         ,post/2
+         ,delete/2
+        ]).
+
+-include("include/crossbar.hrl").
+
+-define(CB_LIST, <<"groups/crossbar_listing">>).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Initializes the bindings this module will respond to.
+%% @end
+%%--------------------------------------------------------------------
+-spec init/0 :: () -> 'ok'.
+init() ->
+    _ = crossbar_bindings:bind(<<"v1_resource.allowed_methods.groups">>, ?MODULE, allowed_methods),
+    _ = crossbar_bindings:bind(<<"v1_resource.resource_exists.groups">>, ?MODULE, resource_exists),
+    _ = crossbar_bindings:bind(<<"v1_resource.validate.groups">>, ?MODULE, validate),
+    _ = crossbar_bindings:bind(<<"v1_resource.execute.put.groups">>, ?MODULE, put),
+    _ = crossbar_bindings:bind(<<"v1_resource.execute.post.groups">>, ?MODULE, post),
+    crossbar_bindings:bind(<<"v1_resource.execute.delete.groups">>, ?MODULE, delete).
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Given the path tokens related to this module, what HTTP methods are
+%% going to be responded to.
+%% @end
+%%--------------------------------------------------------------------
+-spec allowed_methods/0 :: () -> http_methods() | [].
+-spec allowed_methods/1 :: (path_token()) -> http_methods() | [].
+allowed_methods() ->
+    ['GET', 'PUT'].
+allowed_methods(_) ->
+    ['GET', 'POST', 'DELETE'].
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Does the path point to a valid resource
+%% So /groups => []
+%%    /groups/foo => [<<"foo">>]
+%%    /groups/foo/bar => [<<"foo">>, <<"bar">>]
+%% @end
+%%--------------------------------------------------------------------
+-spec resource_exists/0 :: () -> 'true'.
+-spec resource_exists/1 :: (path_token()) -> 'true'.
+resource_exists() -> true.
+resource_exists(_) -> true.
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Check the request (request body, query string params, path tokens, etc)
+%% and load necessary information.
+%% /groups mights load a list of group objects
+%% /groups/123 might load the group object 123
+%% Generally, use crossbar_doc to manipulate the cb_context{} record
+%% @end
+%%--------------------------------------------------------------------
+-spec validate/1 :: (#cb_context{}) -> #cb_context{}.
+-spec validate/2 :: (#cb_context{}, path_token()) -> #cb_context{}.
+validate(#cb_context{req_verb = <<"get">>}=Context) ->
+    summary(Context);
+validate(#cb_context{req_verb = <<"put">>}=Context) ->
+    create(Context).
+
+validate(#cb_context{req_verb = <<"get">>}=Context, Id) ->
+    read(Id, Context);
+validate(#cb_context{req_verb = <<"post">>}=Context, Id) ->
+    update(Id, Context);
+validate(#cb_context{req_verb = <<"delete">>}=Context, Id) ->
+    read(Id, Context).
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% If the HTTP verib is PUT, execute the actual action, usually a db save.
+%% @end
+%%--------------------------------------------------------------------
+-spec put/1 :: (#cb_context{}) -> #cb_context{}.
+put(#cb_context{}=Context) ->
+    crossbar_doc:save(Context).
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% If the HTTP verib is POST, execute the actual action, usually a db save
+%% (after a merge perhaps).
+%% @end
+%%--------------------------------------------------------------------
+-spec post/2 :: (#cb_context{}, path_token()) -> #cb_context{}.
+post(#cb_context{}=Context, _) ->
+    crossbar_doc:save(Context).
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% If the HTTP verib is DELETE, execute the actual action, usually a db delete
+%% @end
+%%--------------------------------------------------------------------
+-spec delete/2 :: (#cb_context{}, path_token()) -> #cb_context{}.
+delete(#cb_context{}=Context, _) ->
+    crossbar_doc:delete(Context).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Create a new instance with the data provided, if it is valid
+%% @end
+%%--------------------------------------------------------------------
+-spec create/1 :: (#cb_context{}) -> #cb_context{}.
+create(#cb_context{}=Context) ->
+    OnSuccess = fun(C) -> on_successful_validation(undefined, C) end,
+    cb_context:validate_request_data(<<"groups">>, Context, OnSuccess).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Load an instance from the database
+%% @end
+%%--------------------------------------------------------------------
+-spec read/2 :: (ne_binary(), #cb_context{}) -> #cb_context{}.
+read(Id, Context) ->
+    crossbar_doc:load(Id, Context).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Update an existing menu document with the data provided, if it is
+%% valid
+%% @end
+%%--------------------------------------------------------------------
+-spec update/2 :: (ne_binary(), #cb_context{}) -> #cb_context{}.
+update(Id, #cb_context{}=Context) ->
+    OnSuccess = fun(C) -> on_successful_validation(Id, C) end,
+    cb_context:validate_request_data(<<"groups">>, Context, OnSuccess).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Attempt to load a summarized listing of all instances of this
+%% resource.
+%% @end
+%%--------------------------------------------------------------------
+-spec summary/1 :: (#cb_context{}) -> #cb_context{}.
+summary(Context) ->
+    crossbar_doc:load_view(?CB_LIST, [], Context, fun normalize_view_results/2).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% 
+%% @end
+%%--------------------------------------------------------------------
+-spec on_successful_validation/2 :: ('undefined' | ne_binary(), #cb_context{}) -> #cb_context{}.
+on_successful_validation(undefined, #cb_context{doc=JObj}=Context) ->
+    Context#cb_context{doc=wh_json:set_value(<<"pvt_type">>, <<"group">>, JObj)};
+on_successful_validation(Id, #cb_context{}=Context) ->
+    crossbar_doc:load_merge(Id, Context).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Normalizes the resuts of a view
+%% @end
+%%--------------------------------------------------------------------
+-spec normalize_view_results/2 :: (wh_json:json_object(), wh_json:json_objects()) -> wh_json:json_objects().
+normalize_view_results(JObj, Acc) ->
+    [wh_json:get_value(<<"value">>, JObj)|Acc].
