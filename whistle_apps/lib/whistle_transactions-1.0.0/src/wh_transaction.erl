9e2d0a7c4f80ac28a684a175126b2f5b9ec3db38
Peter Defebvre <peter@2600hz.com>
2013-01-24 00:56:33 +0000

    WHISTLE-1843: Validatios, Sets ...

--- whistle_apps/lib/whistle_transactions-1.0.0/src/wh_transaction.erl
+++ whistle_apps/lib/whistle_transactions-1.0.0/src/wh_transaction.erl
@@ -1,14 +1,30 @@
 -module(wh_transaction).
 
--export([credit/1
-         ,debit/1
+-export([credit/2
+         ,debit/2
         ]).
 
+-export([set_account_id/2
+         ,set_pvt_account_id/2
+         ,set_ff_description/2
+        ]).
 
-credit(Amount) ->
-    wh_transactions:create(Amount, 'credit').
+credit(Amount, Reason) ->
+    wh_transactions:create(Amount, 'credit', Reason).
 
-debit(Amount) ->
-    wh_transactions:create(Amount, 'debit').
 
+debit(Amount, Reason) ->
+    wh_transactions:create(Amount, 'debit', Reason).
+
+
+set_account_id(AccountId, Transaction) ->
+    wh_transactions:set_account_id(AccountId, Transaction).
+
+
+set_pvt_account_id(AccountId, Transaction) ->
+    wh_transactions:set_pvt_account_id(AccountId, Transaction).
+
+
+set_ff_description(Description, Transaction) ->
+    wh_transactions:set_ff_description(Description, Transaction).
 
--- whistle_apps/lib/whistle_transactions-1.0.0/src/wh_transactions.erl
+++ whistle_apps/lib/whistle_transactions-1.0.0/src/wh_transactions.erl
@@ -2,14 +2,20 @@
 
 -include_lib("whistle/include/wh_types.hrl").
 
--export([
-         create/2
+-export([create/3
          ,save/1
         ]).
 
+-export([set_account_id/2
+         ,set_ff_description/2
+         ,set_pvt_account_id/2
+        ]).
+
 -record(wh_transaction, {
+          % or customer ID and pvt ?
           account_id :: ne_binary()
          ,ff_description :: binary()
+         ,pvt_reason :: ne_binary()
          ,pvt_amount :: integer()
          ,pvt_type :: 'credit' | 'debit'
          ,pvt_created :: wh_now()
@@ -19,20 +25,66 @@
 -type wh_transaction() :: #wh_transaction{}.
 -export_type([wh_transaction/0]).
 
-create(Amount, Op) when not is_integer(Amount) ->
-    create(wh_util:to_float(Amount), Op);
-create(Amount, Op) ->
-    #wh_transaction{pvt_type=Op
-                ,pvt_amount=Amount
-               }.
+create(Amount, Op, Reason) when not is_integer(Amount) ->
+    create(wh_util:to_integer(Amount), Op, Reason);
+create(Amount, Op, Reason) when Amount < 0 ->
+    create(Amount*-1, Op, Reason);
+create(Amount, Op, Reason) ->
+    #wh_transaction{pvt_reason=Reason
+                    ,pvt_type=Op
+                    ,pvt_amount=Amount
+                   }.
+
+set_account_id(AccountId, Transaction) ->
+    Transaction#wh_transaction{account_id=AccountId}.
+
+
+set_ff_description(Desc, Transaction) ->
+    Transaction#wh_transaction{ff_description=Desc}.
+
+
+set_pvt_account_id(AccountId, Transaction) ->
+    Transaction#wh_transaction{pvt_account_id=AccountId}.
+
 
 save(#wh_transaction{}=Transaction) ->
-    check_before_save(Transaction).
+    Errors = validate_funs(Transaction),
+    case validate(Errors) of
+        {true, _} ->
+            _Transaction1 = set_private_properties(Transaction),
+            save;
+        {false, _R} ->
+            dont_save
+    end.    
 
-check_before_save(#wh_transaction{}=Tr) ->  
-    case Tr#wh_transaction{} of
-        ok ->
-            ok
+validate(Errors) ->
+    validate(Errors, true, []).
+
+validate([], Valid, Acc) ->
+    {Valid, Acc};
+validate([ok | Errors], Valid, Acc) ->
+    validate(Errors, Valid, Acc);
+validate([{error, Reason} | Errors], _, Acc) ->
+    validate(Errors, false, [Reason | Acc]).
+
+
+validate_funs(#wh_transaction{}=Tr) ->  
+    Funs = [fun validate_account_id/1],
+    lists:foldl(fun(F, Acc) -> [F(Tr) | Acc] end, [], Funs).
+
+            
+validate_account_id(#wh_transaction{account_id=AccountId}) ->
+    case is_binary(AccountId) of
+        true -> ok;
+        false -> {error, account_id}
     end.
 
+            
+set_private_properties(Transaction) ->
+    PvtFuns = [fun set_pvt_created/1 ],
+    lists:foldl(fun(F, C) -> F(C) end, Transaction, PvtFuns).
+
+    
+set_pvt_created(Transaction) ->
+    Transaction#wh_transaction{pvt_created=wh_util:current_tstamp()}.
 
