b91dbc6687f95be521ac590755bf4edfa1380d7a
James Aimonetti <james@2600hz.org>
2013-01-28 18:19:21 -0800

    WHISTLE-1510: don't convert tuples to lists

--- ecallmgr/src/ecallmgr_fs_notify.erl
+++ ecallmgr/src/ecallmgr_fs_notify.erl
@@ -476,7 +476,8 @@ relay_presence(EventName, PresenceId, Props, Node, undefined) ->
                |[{wh_util:to_list(K), wh_util:to_list(V)}
                  || {K, V} <- lists:foldr(fun(Header, Prop) ->
                                                   proplists:delete(Header, Prop)
-                                          end, Props, ?FS_DEFAULT_HDRS)
+                                          end, Props, ?FS_DEFAULT_HDRS),
+                    not is_tuple(V)
                 ]
               ],
     [begin
@@ -493,7 +494,8 @@ relay_presence(EventName, _, Props, Node, Switch) ->
                |[{wh_util:to_list(K), wh_util:to_list(V)}
                  || {K, V} <- lists:foldr(fun(Header, Prop) ->
                                                   proplists:delete(Header, Prop)
-                                          end, Props, ?FS_DEFAULT_HDRS)
+                                          end, Props, ?FS_DEFAULT_HDRS),
+                    not is_tuple(V)
                 ]
               ],
     freeswitch:sendevent(wh_util:to_atom(Switch, true), EventName, Headers).
