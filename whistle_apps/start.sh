fd36456d4a317d76f1c94e4449c238f48aa450c7
James Aimonetti <james@2600hz.org>
2013-01-29 17:05:47 -0800

    WHISTLE-1510: set load paths on startup (remove need for ERL_LIBS to be set properly)

--- whistle_apps/start.sh
+++ whistle_apps/start.sh
@@ -2,8 +2,8 @@
 
 cd `dirname $0`
 
-export ERL_LIBS=$PWD/../lib:$PWD/lib:$PWD/apps/*/lib
 export ERL_CRASH_DUMP=$(date +%s)_erl_crash.dump
 
 for path in $PWD/lib/*; do folder=$(basename $path); if [ "$folder" != "Makefile" ]; then echo "# rm -rf $PWD/../lib/$folder"; rm -rf $PWD/../lib/$folder; fi; done
-exec erl -args_file $PWD/conf/vm.args -pa $PWD/deps/*/ebin $PWD/ebin $PWD/apps/*/ebin $PWD/apps/*/lib/*/ebin -detached -s whistle_apps
+
+exec erl -args_file $PWD/conf/vm.args -pa lib/*/ebin ebin apps/*/ebin ../lib/*/ebin -detached -s whistle_apps
