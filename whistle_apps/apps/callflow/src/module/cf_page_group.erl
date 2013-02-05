b1e640966295395c24120a9c87348857759c181e
karl anderson <karl@2600hz.com>
2013-02-05 20:44:04 +0000

    WHISTLE-1861: refine the concept of groups and incorporate with ring/page groups

--- whistle_apps/apps/callflow/src/module/cf_page_group.erl
+++ whistle_apps/apps/callflow/src/module/cf_page_group.erl
@@ -22,63 +22,103 @@
 %%--------------------------------------------------------------------
 -spec handle/2 :: (wh_json:object(), whapps_call:call()) -> ok.
 handle(Data, Call) ->
-    Endpoints = get_endpoints(wh_json:get_value(<<"endpoints">>, Data, []), Call),
-    Timeout = wh_json:get_binary_value(<<"timeout">>, Data),
+    case get_endpoints(wh_json:get_value(<<"endpoints">>, Data, []), Call) of
+        [] -> 
+            lager:notice("page group has no endpoints, moving to next callflow element"),
+            cf_exe:continue(Call);
+        Endpoints -> 
+            attempt_page(Endpoints, Data, Call)
+    end.           
+
+-spec attempt_page/3 :: (wh_json:objects(), wh_json:object(), whapps_call:call()) -> 'ok'.
+attempt_page(Endpoints, Data, Call) ->
+    Timeout = wh_json:get_binary_value(<<"timeout">>, Data, 5),
     lager:info("attempting page group of ~b members", [length(Endpoints)]),
-    case length(Endpoints) > 0
-        andalso whapps_call_command:b_page(Endpoints, Timeout, Call)
-    of
+    case whapps_call_command:b_page(Endpoints, Timeout, Call) of
         {ok, _} ->
-            lager:info("completed successful bridge to the ring group - call finished normally"),
+            lager:info("completed successful bridge to the page group - call finished normally"),
             cf_exe:stop(Call);
-        _Else ->
-            lager:info("error bridging to ring group", []),
+        {fail, _}=F ->
+            cf_util:handle_bridge_failure(F, Call);
+        {error, _R} ->
+            lager:info("error bridging to page group: ~p", [_R]),
             cf_exe:continue(Call)
     end.
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Loop over the provided endpoints for the callflow and build the
-%% json object used in the bridge API
-%% @end
-%%--------------------------------------------------------------------
 -spec get_endpoints/2 :: (wh_json:objects(), whapps_call:call()) -> wh_json:objects().
-get_endpoints([], _) ->
-    [];
-get_endpoints([_]=Members, Call) ->
-    get_endpoints_direct(Members, Call);
-get_endpoints([_,_]=Members, Call) ->
-    get_endpoints_direct(Members, Call);
 get_endpoints(Members, Call) ->
     S = self(),
     Builders = [spawn(fun() ->
                               put(callid, whapps_call:call_id(Call)),
-                              EPIds = fetch_endpoints(wh_json:get_value(<<"id">>, Member), wh_json:get_value(<<"endpoint_type">>, Member), Call),
-                              Properties = wh_json:set_value(<<"source">>, ?MODULE, Member),
-                              S ! {self(), [(catch cf_endpoint:build(EndpointId, Properties, Call)) || EndpointId <- EPIds]}
+                              S ! {self(), catch cf_endpoint:build(EndpointId, Member, Call)}
                       end)
-                || Member <- Members
+                || {EndpointId, Member} <- resolve_endpoint_ids(Members, Call)
                ],
     lists:foldl(fun(Pid, Acc) ->
-                        BRs = receive {Pid, BuildResults} -> BuildResults end,
-                        lists:foldl(fun fold_eps/2, Acc, BRs)
+                        receive 
+                            {Pid, {ok, EP}} when is_list(EP) ->
+                                EP ++ Acc;
+                            {Pid, {ok, EP}} ->
+                                [EP | Acc];
+                            {Pid, _} -> Acc
+                        end
                 end, [], lists:reverse(Builders)).
 
-get_endpoints_direct(Members, Call) ->
-    lists:foldr(fun(Member, Acc) ->
-                        EPIds = fetch_endpoints(wh_json:get_value(<<"id">>, Member), wh_json:get_value(<<"endpoint_type">>, Member), Call),
-                        Properties = wh_json:set_value(<<"source">>, ?MODULE, Member),
-                        lists:foldl(fun fold_eps/2, Acc, [(catch cf_endpoint:build(EndpointId, Properties, Call)) || EndpointId <- EPIds])
-                end, [], Members).
+-spec resolve_endpoint_ids/2 :: (wh_json:objects(), whapps_call:call()) -> [] | [{ne_binary(), wh_json:object()},...].
+resolve_endpoint_ids(Members, Call) ->
+    [{Id, wh_json:set_value(<<"source">>, ?MODULE, Member)} 
+     || {Type, Id, Member} <- resolve_endpoint_ids(Members, [], Call)
+            ,Type =:= <<"device">>
+            ,Id =/= whapps_call:authorizing_id(Call)            
+    ].
 
-fold_eps({ok, EPs}, Acc0) when is_list(EPs) -> EPs ++ Acc0;
-fold_eps({ok, EP}, Acc0) -> [EP | Acc0];
-fold_eps(_, Acc0) -> Acc0.
+-type endpoint_intermediate() :: {ne_binary(), ne_binary(), 'undefined' | wh_json:object()}.
+-type endpoint_intermediates() :: [] | [endpoint_intermediate(),...].
+-spec resolve_endpoint_ids/3 :: (wh_json:objects(), endpoint_intermediates(), whapps_call:call()) -> endpoint_intermediates().
+resolve_endpoint_ids([], EndpointIds, _) -> 
+    EndpointIds;
+resolve_endpoint_ids([Member|Members], EndpointIds, Call) ->
+    Id = wh_json:get_value(<<"id">>, Member),
+    Type = wh_json:get_value(<<"endpoint_type">>, Member, <<"device">>),
+    case wh_util:is_empty(Id)
+        orelse lists:keymember(Id, 2, EndpointIds)
+        orelse Type
+    of
+        true -> 
+            resolve_endpoint_ids(Members, EndpointIds, Call);
+        <<"group">> ->
+            lager:info("member ~s is a group, merge the group's members", [Id]),
+            GroupMembers = get_group_members(Member, Id, Call),
+            Ids = resolve_endpoint_ids(GroupMembers, EndpointIds, Call),
+            resolve_endpoint_ids(Members, [{Type, Id, undefined}|Ids], Call);
+        <<"user">> ->            
+            lager:info("member ~s is a user, get all the user's endpoints", [Id]),
+            Ids = lists:foldr(fun(EndpointId, Acc) ->
+                                      case lists:keymember(EndpointId, 2, Acc) of
+                                          true -> Acc;
+                                          false ->
+                                              [{<<"device">>, EndpointId, Member}|Acc]
+                                      end
+                              end
+                              ,[{Type, Id, undefined}|EndpointIds]
+                              ,cf_attributes:fetch_owned_by(Id, device, Call)),
+            resolve_endpoint_ids(Members, Ids, Call);
+        <<"device">> ->
+            resolve_endpoint_ids(Members, [{Type, Id, Member}|EndpointIds], Call)
+    end.
 
-fetch_endpoints(MemberId, <<"user">>, Call) ->
-    lager:info("member ~s is a user, get all the user's endpoints", [MemberId]),
-    cf_attributes:fetch_owned_by(MemberId, device, Call);
-fetch_endpoints(MemberId, _, _) ->
-    lager:info("member ~s is not a user, assuming a device", [MemberId]),
-    [MemberId].
+-spec get_group_members/3 :: (wh_json:object(), ne_binary(), whapps_call:call()) -> wh_json:object().    
+get_group_members(Member, Id, Call) ->
+    AccountDb = whapps_call:account_db(Call),
+    case couch_mgr:open_cache_doc(AccountDb, Id) of
+        {ok, JObj} ->
+            Endpoints = wh_json:get_ne_value(<<"endpoints">>, JObj, wh_json:new()),
+            [wh_json:set_values([{<<"endpoint_type">>, wh_json:get_value([Key, <<"type">>], Endpoints)}
+                                 ,{<<"id">>, Key}
+                                ], Member)
+             || Key <- wh_json:get_keys(Endpoints)
+            ];
+        {error, _R} ->
+            lager:warning("unable to lookup members of group ~s: ~p", [Id, _R]),
+            []
+    end.
