10f42dae2a2ae3b8b47a3932b19ae890a495c7aa
James Aimonetti <james@2600hz.org>
2013-02-20 11:07:59 -0800

    KAZOO-557: unbind from cdr binding if we receive a cdr, whether we have a url or not

--- whistle_apps/apps/acdc/src/acdc_agent_handler.erl
+++ whistle_apps/apps/acdc/src/acdc_agent_handler.erl
@@ -27,20 +27,22 @@
 handle_status_update(JObj, _Props) ->
     AcctId = wh_json:get_value(<<"Account-ID">>, JObj),
     AgentId = wh_json:get_value(<<"Agent-ID">>, JObj),
-    Timeout = wh_json:get_integer_value(<<"Time-Limit">>, JObj, whapps_config:get(<<"acdc">>, <<"default_agent_pause_timeout">>, 600)),
+    Timeout = wh_json:get_integer_value(<<"Time-Limit">>, JObj
+                                        ,whapps_config:get(<<"acdc">>, <<"default_agent_pause_timeout">>, 600)
+                                       ),
 
     case wh_json:get_value(<<"Event-Name">>, JObj) of
         <<"login">> ->
-            true = wapi_acdc_agent:login_v(JObj),
+            'true' = wapi_acdc_agent:login_v(JObj),
             maybe_start_agent(AcctId, AgentId);
         <<"logout">> ->
-            true = wapi_acdc_agent:logout_v(JObj),
+            'true' = wapi_acdc_agent:logout_v(JObj),
             maybe_stop_agent(AcctId, AgentId);
         <<"pause">> ->
-            true = wapi_acdc_agent:pause_v(JObj),
+            'true' = wapi_acdc_agent:pause_v(JObj),
             maybe_pause_agent(AcctId, AgentId, Timeout);
         <<"resume">> ->
-            true = wapi_acdc_agent:resume_v(JObj),
+            'true' = wapi_acdc_agent:resume_v(JObj),
             maybe_resume_agent(AcctId, AgentId);
         Event -> maybe_agent_queue_change(AcctId, AgentId, Event
                                           ,wh_json:get_value(<<"Queue-ID">>, JObj)
@@ -60,20 +62,20 @@ maybe_agent_queue_change(AcctId, AgentId, <<"logout_queue">>, QueueId) ->
 maybe_agent_queue_change(_AcctId, _AgentId, _Evt, _QueueId) ->
     lager:debug("unhandled evt: ~s for ~s", [_Evt, _QueueId]).
 
-update_agent(undefined, _, _) -> ok;
+update_agent('undefined', _, _) -> 'ok';
 update_agent(Super, Q, F) when is_pid(Super) -> 
     F(acdc_agent_sup:agent(Super), Q).
 
 maybe_start_agent(AcctId, AgentId) ->
     case acdc_agents_sup:find_agent_supervisor(AcctId, AgentId) of
-        undefined ->
+        'undefined' ->
             lager:debug("agent ~s (~s) not found, starting", [AgentId, AcctId]),
-            case couch_mgr:open_doc(wh_util:format_account_id(AcctId, encoded), AgentId) of
-                {ok, AgentJObj} ->
-                    {ok, _APid} = acdc_agents_sup:new(AgentJObj),
+            case couch_mgr:open_doc(wh_util:format_account_id(AcctId, 'encoded'), AgentId) of
+                {'ok', AgentJObj} ->
+                    {'ok', _APid} = acdc_agents_sup:new(AgentJObj),
                     acdc_stats:agent_active(AcctId, AgentId),
                     lager:debug("started agent at ~p", [_APid]);
-                {error, _E} ->
+                {'error', _E} ->
                     lager:debug("error opening agent doc: ~p", [_E])
             end;
         P when is_pid(P) ->
@@ -83,7 +85,7 @@ maybe_start_agent(AcctId, AgentId) ->
 maybe_stop_agent(AcctId, AgentId) ->
     acdc_util:presence_update(AcctId, AgentId, ?PRESENCE_RED_SOLID),
     case acdc_agents_sup:find_agent_supervisor(AcctId, AgentId) of
-        undefined ->
+        'undefined' ->
             lager:debug("agent ~s (~s) not found, nothing to do", [AgentId, AcctId]);
         P when is_pid(P) ->
             lager:debug("agent ~s(~s) is logging out, stopping ~p", [AcctId, AgentId, P]),
@@ -93,7 +95,7 @@ maybe_stop_agent(AcctId, AgentId) ->
 
 maybe_pause_agent(AcctId, AgentId, Timeout) ->
     case acdc_agents_sup:find_agent_supervisor(AcctId, AgentId) of
-        undefined -> lager:debug("agent ~s (~s) not found, nothing to do", [AgentId, AcctId]);
+        'undefined' -> lager:debug("agent ~s (~s) not found, nothing to do", [AgentId, AcctId]);
         P when is_pid(P) ->
             lager:debug("agent ~s(~s) is pausing for ~p", [AcctId, AgentId, Timeout]),
             acdc_agent_fsm:pause(acdc_agent_sup:fsm(P), Timeout)
@@ -101,7 +103,7 @@ maybe_pause_agent(AcctId, AgentId, Timeout) ->
 
 maybe_resume_agent(AcctId, AgentId) ->
     case acdc_agents_sup:find_agent_supervisor(AcctId, AgentId) of
-        undefined -> lager:debug("agent ~s (~s) not found, nothing to do", [AgentId, AcctId]);
+        'undefined' -> lager:debug("agent ~s (~s) not found, nothing to do", [AgentId, AcctId]);
         P when is_pid(P) ->
             lager:debug("agent ~s(~s) is resuming: ~p", [AcctId, AgentId, P]),
             acdc_agent_fsm:resume(acdc_agent_sup:fsm(P))
@@ -109,55 +111,54 @@ maybe_resume_agent(AcctId, AgentId) ->
 
 -spec handle_sync_req(wh_json:object(), wh_proplist()) -> 'ok'.
 handle_sync_req(JObj, Props) ->
-    true = wapi_acdc_agent:sync_req_v(JObj),
-    acdc_agent_fsm:sync_req(props:get_value(fsm_pid, Props), JObj).
+    'true' = wapi_acdc_agent:sync_req_v(JObj),
+    acdc_agent_fsm:sync_req(props:get_value('fsm_pid', Props), JObj).
 
 -spec handle_sync_resp(wh_json:object(), wh_proplist()) -> 'ok'.
 handle_sync_resp(JObj, Props) ->
-    true = wapi_acdc_agent:sync_resp_v(JObj),
-    acdc_agent_fsm:sync_resp(props:get_value(fsm_pid, Props), JObj).
+    'true' = wapi_acdc_agent:sync_resp_v(JObj),
+    acdc_agent_fsm:sync_resp(props:get_value('fsm_pid', Props), JObj).
 
 -spec handle_call_event(wh_json:object(), wh_proplist()) -> 'ok'.
 handle_call_event(JObj, Props) ->
-    FSM = props:get_value(fsm_pid, Props),
+    FSM = props:get_value('fsm_pid', Props),
     case wapi_call:event_v(JObj) of
-        true ->
+        'true' ->
             {Cat, Name} = wh_util:get_event_type(JObj),
             acdc_agent_fsm:call_event(FSM, Cat, Name, JObj);
-        false ->
-            true = wh_api:error_resp_v(JObj),
+        'false' ->
+            'true' = wh_api:error_resp_v(JObj),
 
             case wh_json:get_value([<<"Request">>, <<"Event-Name">>], JObj) of
-                <<"originate_req">> ->
-                    acdc_agent_fsm:originate_failed(FSM, JObj);
-                _ -> ok
+                <<"originate_req">> -> acdc_agent_fsm:originate_failed(FSM, JObj);
+                _ -> 'ok'
             end
     end.
 
 handle_cdr(JObj, Props) ->
     'true' = wapi_call:cdr_v(JObj),
-    Urls = props:get_value(cdr_urls, Props),
+    _ = wh_util:put_callid(JObj),
+
+    Urls = props:get_value('cdr_urls', Props),
     CallId = wh_json:get_value(<<"Call-ID">>, JObj),
+
     case catch dict:fetch(CallId, Urls) of
-        {'EXIT', _} -> lager:debug("no url for call ~s", [CallId]);
-        Url ->
-            acdc_util:send_cdr(Url, JObj),
-            acdc_agent:unbind_from_cdr(props:get_value(server, Props)
-                                       ,wh_json:get_value(<<"Call-ID">>, JObj)
-                                      )
-    end.
+        {'EXIT', _} -> lager:debug("no cdr url for call ~s", [CallId]);
+        Url -> acdc_util:send_cdr(Url, JObj)
+    end,
+    acdc_agent:unbind_from_cdr(props:get_value('server', Props), CallId).
 
 handle_originate_resp(JObj, Props) ->
     case wh_json:get_value(<<"Event-Name">>, JObj) of
         <<"originate_resp">> ->
-            true = wapi_resource:originate_resp_v(JObj),
-            acdc_agent_fsm:originate_resp(props:get_value(fsm_pid, Props), JObj);
+            'true' = wapi_resource:originate_resp_v(JObj),
+            acdc_agent_fsm:originate_resp(props:get_value('fsm_pid', Props), JObj);
         <<"originate_started">> ->
-            true = wapi_resource:originate_started_v(JObj),
-            acdc_agent_fsm:originate_started(props:get_value(fsm_pid, Props), JObj);
+            'true' = wapi_resource:originate_started_v(JObj),
+            acdc_agent_fsm:originate_started(props:get_value('fsm_pid', Props), JObj);
         <<"originate_uuid">> ->
-            true = wapi_resource:originate_uuid_v(JObj),
-            acdc_agent_fsm:originate_uuid(props:get_value(fsm_pid, Props), JObj)
+            'true' = wapi_resource:originate_uuid_v(JObj),
+            acdc_agent_fsm:originate_uuid(props:get_value('fsm_pid', Props), JObj)
     end.
 
 -spec handle_member_message(wh_json:object(), wh_proplist()) -> 'ok'.
@@ -166,16 +167,16 @@ handle_member_message(JObj, Props) ->
     handle_member_message(JObj, Props, wh_json:get_value(<<"Event-Name">>, JObj)).
 
 handle_member_message(JObj, Props, <<"connect_req">>) ->
-    true = wapi_acdc_queue:member_connect_req_v(JObj),
-    acdc_agent_fsm:member_connect_req(props:get_value(fsm_pid, Props), JObj);
+    'true' = wapi_acdc_queue:member_connect_req_v(JObj),
+    acdc_agent_fsm:member_connect_req(props:get_value('fsm_pid', Props), JObj);
 handle_member_message(JObj, Props, <<"connect_win">>) ->
-    true = wapi_acdc_queue:member_connect_win_v(JObj),
-    acdc_agent_fsm:member_connect_win(props:get_value(fsm_pid, Props), JObj);
+    'true' = wapi_acdc_queue:member_connect_win_v(JObj),
+    acdc_agent_fsm:member_connect_win(props:get_value('fsm_pid', Props), JObj);
 handle_member_message(_, _, EvtName) ->
     lager:debug("not handling member event ~s", [EvtName]).
 
 handle_config_change(JObj, _Props) ->
-    true = wapi_conf:doc_update_v(JObj),
+    'true' = wapi_conf:doc_update_v(JObj),
     handle_agent_change(wh_json:get_value(<<"Database">>, JObj)
                         ,wh_json:get_value(<<"Account-ID">>, JObj)
                         ,wh_json:get_value(<<"ID">>, JObj)
@@ -184,22 +185,22 @@ handle_config_change(JObj, _Props) ->
 
 handle_agent_change(AccountDb, AccountId, AgentId, <<"doc_created">>) ->
     case acdc_agents_sup:find_agent_supervisor(AccountId, AgentId) of
-        undefined -> 
-            {ok, JObj} = couch_mgr:open_doc(AccountDb, AgentId),
+        'undefined' -> 
+            {'ok', JObj} = couch_mgr:open_doc(AccountDb, AgentId),
             acdc_agents_sup:new(JObj);
-        P when is_pid(P) -> ok
+        P when is_pid(P) -> 'ok'
     end;
 handle_agent_change(AccountDb, AccountId, AgentId, <<"doc_edited">>) ->
-    {ok, JObj} = couch_mgr:open_doc(AccountDb, AgentId),
+    {'ok', JObj} = couch_mgr:open_doc(AccountDb, AgentId),
     case acdc_agents_sup:find_agent_supervisor(AccountId, AgentId) of
-        undefined -> 
+        'undefined' -> 
             acdc_agents_sup:new(JObj);
         P when is_pid(P) -> 
             acdc_agent_fsm:refresh(acdc_agent_sup:fsm(P), JObj)
     end;
 handle_agent_change(_, AccountId, AgentId, <<"doc_deleted">>) ->
     case acdc_agents_sup:find_agent_supervisor(AccountId, AgentId) of
-        undefined -> ok;
+        'undefined' -> 'ok';
         P when is_pid(P) ->
             lager:debug("agent ~s(~s) has been deleted, stopping ~p", [AccountId, AgentId, P]),
             _ = acdc_agent_sup:stop(P),
@@ -207,33 +208,32 @@ handle_agent_change(_, AccountId, AgentId, <<"doc_deleted">>) ->
     end.
 
 handle_presence_probe(JObj, _Props) ->
-    true = wapi_notifications:presence_probe_v(JObj),
+    'true' = wapi_notifications:presence_probe_v(JObj),
 
     FromRealm = wh_json:get_value(<<"From-Realm">>, JObj),
     case whapps_util:get_account_by_realm(FromRealm) of
-        {ok, AcctDb} -> maybe_respond_to_presence_probe(JObj, AcctDb);
-        _ -> ok
+        {'ok', AcctDb} -> maybe_respond_to_presence_probe(JObj, AcctDb);
+        _ -> 'ok'
     end.
 
 maybe_respond_to_presence_probe(JObj, AcctDb) ->
     case wh_json:get_value(<<"To-User">>, JObj) of
-        undefined -> lager:debug("no to-user found on json: ~p", [JObj]);
+        'undefined' -> lager:debug("no to-user found on json: ~p", [JObj]);
         AgentId ->
             case couch_mgr:open_doc(AcctDb, AgentId) of
-                {ok, Doc} ->
-                    AcctId = wh_util:format_account_id(AcctDb, raw),
+                {'ok', Doc} ->
+                    AcctId = wh_util:format_account_id(AcctDb, 'raw'),
                     lager:debug("maybe looking for probe for agent ~s(~s)", [AgentId, AcctId]),
                     maybe_update_probe(JObj, AcctId, AgentId, wh_json:get_value(<<"pvt_type">>, Doc));
-                _ -> ok
+                _ -> 'ok'
             end
     end.
 
 maybe_update_probe(JObj, AcctId, AgentId, <<"user">>) ->
     update_probe(JObj, acdc_agents_sup:find_agent_supervisor(AcctId, AgentId));
-maybe_update_probe(_, _, _, _) ->
-    ok.
+maybe_update_probe(_, _, _, _) -> 'ok'.
 
-update_probe(JObj, undefined) ->
+update_probe(JObj, 'undefined') ->
     lager:debug("no agent present, redify!"),
     send_probe(JObj, ?PRESENCE_RED_SOLID);
 update_probe(JObj, P) when is_pid(P) ->
@@ -256,9 +256,9 @@ handle_route_req(JObj, Props) ->
     Call = whapps_call:from_route_req(JObj),
 
     Owner = whapps_call:owner_id(Call),
-    Agent = props:get_value(agent_id, Props),
+    Agent = props:get_value('agent_id', Props),
 
     case Owner =:= Agent of
-        true -> acdc_agent_fsm:route_req(props:get_value(fsm_pid, Props), Call);
-        false -> ok
+        'true' -> acdc_agent_fsm:route_req(props:get_value('fsm_pid', Props), Call);
+        'false' -> 'ok'
     end.
