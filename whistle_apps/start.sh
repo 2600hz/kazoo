41646d1b413592580ba557096f1ab0181a3d9020
karl anderson <karl@2600hz.com>
2013-02-21 01:52:48 +0000

    WHISTLE-1510: revert, without full paths code:priv_dir returns bad_name for root applications

--- whistle_apps/start.sh
+++ whistle_apps/start.sh
@@ -7,4 +7,4 @@ export ERL_LIBS=$PWD/../lib:$PWD/lib
 
 for path in $PWD/lib/*; do folder=$(basename $path); if [ "$folder" != "Makefile" ]; then echo "# rm -rf $PWD/../lib/$folder"; rm -rf $PWD/../lib/$folder; fi; done
 
-exec erl -args_file $PWD/conf/vm.args -pa apps/*/ebin -pa ebin -detached -s whistle_apps
+exec erl -args_file $PWD/conf/vm.args -pa $PWD/ebin $PWD/apps/*/ebin $PWD/apps/*/lib/*/ebin -detached -s whistle_apps
