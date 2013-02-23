c0bee010dafb89e13227c54eeb2657967f8601c8
James Aimonetti <james@2600hz.org>
2013-02-23 13:47:50 -0800

    KAZ))-543: helper for finding DBs for compaction

---
+++ bin/dbs_for_compaction.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+SIZE="100M"
+
+ssh $1 "find /srv/db/shards/ -size +$SIZE -exec ls -s {} \;" | sort -n | awk -F "/" '{printf "%s/%s/%s/%s\n", $6,$7,$8,$9}' | awk -F "." '{printf "%s\n", $1}' | uniq -u
+
