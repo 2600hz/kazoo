612a9443a33e84aba1867999dd9dd824f7256e87
James Aimonetti <james@2600hz.org>
2013-02-26 12:14:54 -0800

    KAZOO-604: formatting, specs

--- whistle_apps/lib/whistle_couch-1.0.0/src/wh_doc.erl
+++ whistle_apps/lib/whistle_couch-1.0.0/src/wh_doc.erl
@@ -1,5 +1,5 @@
 %%%-------------------------------------------------------------------
-%%% @copyright (C) 2011-2012, VoIP INC
+%%% @copyright (C) 2011-2013, VoIP INC
 %%% @doc
 %%% Utilities for manipulating Kazoo/Whistle documents
 %%% @end
@@ -34,49 +34,53 @@
 %% parameters on all crossbar documents
 %% @end
 %%--------------------------------------------------------------------
--spec update_pvt_parameters/2 :: (wh_json:object(), ne_binary()) ->
-                                         wh_json:object().
--spec update_pvt_parameters/3 :: (wh_json:object(), ne_binary(), wh_proplist()) ->
-                                         wh_json:object().
+-spec update_pvt_parameters(wh_json:object(), ne_binary()) ->
+                                   wh_json:object().
+-spec update_pvt_parameters(wh_json:object(), ne_binary(), wh_proplist()) ->
+                                   wh_json:object().
 update_pvt_parameters(JObj0, DBName) ->
-    update_pvt_parameters(JObj0, DBName, [{now, wh_util:current_tstamp()}]).
+    update_pvt_parameters(JObj0, DBName, []).
 update_pvt_parameters(JObj0, DBName, Options) ->
-    Opts = case props:get_value(now, Options) of
-               undefined -> [{now, wh_util:current_tstamp()} | Options];
+    Opts = case props:get_value('now', Options) of
+               'undefined' -> [{'now', wh_util:current_tstamp()} | Options];
                _ -> Options
            end,
     lists:foldl(fun(Fun, JObj) -> Fun(JObj, DBName, Opts) end, JObj0, ?PVT_FUNS).
 
 add_pvt_vsn(JObj, _, Options) ->
-    case props:get_value(crossbar_doc_vsn, Options) of
-        undefined -> JObj;
+    case props:get_value('crossbar_doc_vsn', Options) of
+        'undefined' -> JObj;
         Vsn -> wh_json:set_value(<<"pvt_vsn">>, Vsn, JObj)
     end.
 
-add_pvt_account_db(JObj, DBName, _) ->
-    wh_json:set_value(<<"pvt_account_db">>, DBName, JObj).
+add_pvt_account_db(JObj, DBName, Opts) ->
+    case props:get_value('account_db', Opts) of
+        'undefined' -> wh_json:set_value(<<"pvt_account_db">>, DBName, JObj);
+        Db -> wh_json:set_value(<<"pvt_account_db">>, Db, JObj)
+    end.
 
-add_pvt_account_id(JObj, DBName, _) ->
-    wh_json:set_value(<<"pvt_account_id">>, wh_util:format_account_id(DBName, raw), JObj).
+add_pvt_account_id(JObj, DBName, Opts) ->
+    case props:get_value('account_id', Opts) of
+        'undefined' -> wh_json:set_value(<<"pvt_account_id">>, wh_util:format_account_id(DBName, 'raw'), JObj);
+        Id -> wh_json:set_value(<<"pvt_account_id">>, Id)
+    end.
 
 add_pvt_type(JObj, _, Options) ->
-    case props:get_value(type, Options) of
-        undefined -> JObj;
+    case props:get_value('type', Options) of
+        'undefined' -> JObj;
         Type -> wh_json:set_value(<<"pvt_type">>, Type, JObj)
     end.
 
 add_pvt_created(JObj, _, Opts) ->
     case wh_json:get_value(<<"_rev">>, JObj) of
-        undefined ->
-            wh_json:set_value(<<"pvt_created">>, props:get_value(now, Opts), JObj);
-        _ ->
-            JObj
+        'undefined' -> wh_json:set_value(<<"pvt_created">>, props:get_value('now', Opts), JObj);
+        _ -> JObj
     end.
 
 update_pvt_modified(JObj) ->
-    add_pvt_modified(JObj, undefined, [{now, wh_util:current_tstamp()}]).
+    add_pvt_modified(JObj, 'undefined', [{'now', wh_util:current_tstamp()}]).
 add_pvt_modified(JObj, _, Opts) ->
-    wh_json:set_value(<<"pvt_modified">>, props:get_value(now, Opts), JObj).
+    wh_json:set_value(<<"pvt_modified">>, props:get_value('now', Opts), JObj).
 
 %%--------------------------------------------------------------------
 %% @public
@@ -85,12 +89,10 @@ add_pvt_modified(JObj, _, Opts) ->
 %% json proplist
 %% @end
 %%--------------------------------------------------------------------
--spec public_fields/1 :: (wh_json:object() | wh_json:objects()) ->
-                                 wh_json:object() | wh_json:objects().
-public_fields(JObjs) when is_list(JObjs) ->
-    [public_fields(J) || J <- JObjs];
-public_fields(JObj) ->
-    wh_json:filter(fun({K, _}) -> not is_private_key(K) end, JObj).
+-spec public_fields(wh_json:object() | wh_json:objects()) ->
+                           wh_json:object() | wh_json:objects().
+public_fields(JObjs) when is_list(JObjs) -> [public_fields(J) || J <- JObjs];
+public_fields(JObj) -> wh_json:filter(fun({K, _}) -> not is_private_key(K) end, JObj).
 
 %%--------------------------------------------------------------------
 %% @public
@@ -99,10 +101,10 @@ public_fields(JObj) ->
 %% considered private; otherwise false
 %% @end
 %%--------------------------------------------------------------------
--spec is_private_key/1 :: (binary()) -> boolean().
-is_private_key(<<"_", _/binary>>) -> true;
-is_private_key(<<"pvt_", _/binary>>) -> true;
-is_private_key(_) -> false.
+-spec is_private_key(binary()) -> boolean().
+is_private_key(<<"_", _/binary>>) -> 'true';
+is_private_key(<<"pvt_", _/binary>>) -> 'true';
+is_private_key(_) -> 'false'.
 
 %%--------------------------------------------------------------------
 %% @public
@@ -111,9 +113,7 @@ is_private_key(_) -> false.
 %% json proplist
 %% @end
 %%--------------------------------------------------------------------
--spec private_fields/1 :: (wh_json:object() | wh_json:objects()) ->
-                                  wh_json:object() | wh_json:objects().
-private_fields(JObjs) when is_list(JObjs) ->
-    lists:map(fun public_fields/1, JObjs);
-private_fields(JObj) ->
-    wh_json:filter(fun({K, _}) -> is_private_key(K) end, JObj).
+-spec private_fields(wh_json:object() | wh_json:objects()) ->
+                            wh_json:object() | wh_json:objects().
+private_fields(JObjs) when is_list(JObjs) -> [public_fields(JObj) || JObj <- JObjs];
+private_fields(JObj) -> wh_json:filter(fun({K, _}) -> is_private_key(K) end, JObj).
