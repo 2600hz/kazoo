314b4cf9a983f0e01cc005176e21519906894465
James Aimonetti <james@2600hz.org>
2013-03-03 15:04:01 -0800

    KAZOO-240: formatting

--- whistle_apps/lib/kazoo_translator-1.0.0/src/kzt_receiver.erl
+++ whistle_apps/lib/kazoo_translator-1.0.0/src/kzt_receiver.erl
@@ -33,69 +33,67 @@
 collect_dtmfs(Call, FinishKey, Timeout, N) ->
     collect_dtmfs(Call, FinishKey, Timeout, N, kzt_util:get_digits_collected(Call)).
 collect_dtmfs(Call, _FinishKey, _Timeout, N, Collected) when byte_size(Collected) =:= N ->
-    {ok, Call};
+    {'ok', Call};
 collect_dtmfs(Call, FinishKey, Timeout, N, Collected) ->
     lager:debug("collect_dtmfs: n: ~p collected: ~s", [N, Collected]),
     case whapps_call_command:wait_for_dtmf(Timeout) of
-        {ok, FinishKey} ->
+        {'ok', FinishKey} ->
             lager:debug("finish key pressed"),
-            {ok, dtmf_finish, Call};
-        {ok, <<>>} ->
+            {'ok', 'dtmf_finish', Call};
+        {'ok', <<>>} ->
             lager:debug("no dtmf pressed in time"),
-            {ok, timeout, Call};
-        {ok, DTMF} ->
+            {'ok', 'timeout', Call};
+        {'ok', DTMF} ->
             lager:debug("dtmf pressed: ~s", [DTMF]),
             collect_dtmfs(kzt_util:add_digit_collected(DTMF, Call)
                           ,FinishKey, Timeout, N, <<DTMF/binary, Collected/binary>>
                          );
-        {error, channel_hungup} -> {stop, Call};
-        {error, channel_destroy} -> {stop, Call};
-        {error, E} ->
+        {'error', 'channel_hungup'} -> {'stop', Call};
+        {'error', 'channel_destroy'} -> {'stop', Call};
+        {'error', E} ->
             lager:debug("error: ~p", [E]),
-            {error, E, Call}
+            {'error', E, Call}
     end.
 
--spec say_loop/6 :: (whapps_call:call(), ne_binary(), ne_binary(), ne_binary(), ne_binary(), wh_timeout()) ->
-                            {'ok', whapps_call:call()} |
-                            {'error', _, whapps_call:call()}.
--spec say_loop/7 :: (whapps_call:call(), ne_binary(), ne_binary(), ne_binary(), list() | 'undefined', ne_binary(), wh_timeout()) ->
-                            {'ok', whapps_call:call()} |
-                            {'error', _, whapps_call:call()}.
+-spec say_loop(whapps_call:call(), ne_binary(), ne_binary(), ne_binary(), ne_binary(), wh_timeout()) ->
+                      {'ok', whapps_call:call()} |
+                      {'error', _, whapps_call:call()}.
+-spec say_loop(whapps_call:call(), ne_binary(), ne_binary(), ne_binary(), list() | 'undefined', ne_binary(), wh_timeout()) ->
+                      {'ok', whapps_call:call()} |
+                      {'error', _, whapps_call:call()}.
 say_loop(Call, SayMe, Voice, Lang, Engine, N) ->
-    say_loop(Call, SayMe, Voice, Lang, undefined, Engine, N).
+    say_loop(Call, SayMe, Voice, Lang, 'undefined', Engine, N).
 
 say_loop(Call, _SayMe, _Voice, _Lang, _Terminators, _Engine, N) when N =< 0 -> {ok, Call};
 say_loop(Call, SayMe, Voice, Lang, Terminators, Engine, N) ->
     NoopId = whapps_call_command:tts(SayMe, Voice, Lang, Terminators, Engine, Call),
     case wait_for_noop(Call, NoopId) of
-        {ok, C} -> say_loop(C, SayMe, Voice, Lang, Terminators, Engine, decr_loop_counter(N));
-        {error, _, _}=ERR -> ERR
+        {'ok', C} -> say_loop(C, SayMe, Voice, Lang, Terminators, Engine, decr_loop_counter(N));
+        {'error', _, _}=ERR -> ERR
     end.
 
--spec play_loop/3 :: (whapps_call:call(), ne_binary(), wh_timeout()) ->
+-spec play_loop(whapps_call:call(), ne_binary(), wh_timeout()) ->
+                       {'ok', whapps_call:call()} |
+                       {'error', _, whapps_call:call()}.
+-spec play_loop(whapps_call:call(), ne_binary(), list() | 'undefined', wh_timeout()) ->
                              {'ok', whapps_call:call()} |
                              {'error', _, whapps_call:call()}.
--spec play_loop/4 :: (whapps_call:call(), ne_binary(), list() | 'undefined', wh_timeout()) ->
-                             {'ok', whapps_call:call()} |
-                             {'error', _, whapps_call:call()}.
-play_loop(Call, PlayMe, N) ->
-    play_loop(Call, PlayMe, 'undefined', N).
-play_loop(Call, _, _, 0) ->
-    {ok, Call};
+play_loop(Call, PlayMe, N) -> play_loop(Call, PlayMe, 'undefined', N).
+play_loop(Call, _, _, 0) -> {'ok', Call};
 play_loop(Call, PlayMe, Terminators, N) ->
-    lager:debug("terminators: ~p loop: ~p", [Terminators, N]),
     NoopId = whapps_call_command:play(PlayMe, Terminators, Call),
+    lager:debug("terminators: ~p loop: ~p noop: ~s", [Terminators, N, NoopId]),
     case wait_for_noop(Call, NoopId) of
-        {ok, C} -> play_loop(C, PlayMe, Terminators, decr_loop_counter(N));
-        {error, _, _}=ERR -> ERR
+        {'ok', C} -> lager:debug("noop recv"), play_loop(C, PlayMe, Terminators, decr_loop_counter(N));
+        {'error', _, _}=ERR -> lager:debug("err: ~p", [ERR]), ERR
     end.
 
--spec decr_loop_counter/1 :: (wh_timeout()) -> wh_timeout().
+-spec decr_loop_counter(wh_timeout()) -> wh_timeout().
 decr_loop_counter(infinity) -> infinity;
 decr_loop_counter(N) when is_integer(N), N > 0 -> N-1;
 decr_loop_counter(_) -> 0.
 
--spec wait_for_noop/2 :: (whapps_call:call(), ne_binary()) ->
+-spec wait_for_noop(whapps_call:call(), ne_binary()) ->
                                  {'ok', whapps_call:call()} |
                                  {'error', noop_error(), whapps_call:call()}.
 wait_for_noop(Call, NoopId) ->
@@ -109,7 +107,7 @@ wait_for_noop(Call, NoopId) ->
     end.
 
 -type noop_error() :: 'channel_destroy' | 'channel_hungup'.
--spec process_noop_event/3 :: (whapps_call:call(), ne_binary(), wh_json:object()) ->
+-spec process_noop_event(whapps_call:call(), ne_binary(), wh_json:object()) ->
                                       {'ok', whapps_call:call()} |
                                       {'error', noop_error(), whapps_call:call()}.
 process_noop_event(Call, NoopId, JObj) ->
@@ -128,7 +126,7 @@ process_noop_event(Call, NoopId, JObj) ->
         _ -> wait_for_noop(Call, NoopId)
     end.
 
--spec wait_for_offnet/1 :: (whapps_call:call()) -> {'ok', whapps_call:call()}.
+-spec wait_for_offnet(whapps_call:call()) -> {'ok', whapps_call:call()}.
 wait_for_offnet(Call) ->
     HangupDTMF = kzt_util:get_hangup_dtmf(Call),
     RecordCall = kzt_util:get_record_call(Call),
@@ -146,7 +144,7 @@ wait_for_offnet(Call) ->
                              }
                           ).
 
--spec wait_for_hangup/1 :: (whapps_call:call()) -> {'ok', whapps_call:call()}.
+-spec wait_for_hangup(whapps_call:call()) -> {'ok', whapps_call:call()}.
 wait_for_hangup(Call) ->
     case whapps_call_command:receive_event(?DEFAULT_EVENT_WAIT) of
         {ok, JObj} ->
@@ -176,7 +174,7 @@ call_status(_Status) ->
     lager:debug("unhandled call status: ~p", [_Status]),
     ?STATUS_FAILED.
 
--spec wait_for_offnet_events/1 :: (offnet_req()) -> {'ok', whapps_call:call()}.
+-spec wait_for_offnet_events(offnet_req()) -> {'ok', whapps_call:call()}.
 wait_for_offnet_events(#offnet_req{
                           call_timeout=CallTimeout
                           ,call_time_limit=CallTimeLimit
@@ -323,7 +321,7 @@ recording_name(ALeg, BLeg) ->
     DateTime = wh_util:pretty_print_datetime(calendar:universal_time()),
     iolist_to_binary([DateTime, "_", ALeg, "_to_", BLeg, ".mp3"]).
 
--spec handle_offnet_timeout/1 :: (offnet_req()) -> {'ok', whapps_call:call()}.
+-spec handle_offnet_timeout(offnet_req()) -> {'ok', whapps_call:call()}.
 handle_offnet_timeout(#offnet_req{
                          call=Call
                          ,call_timeout=undefined
@@ -334,12 +332,12 @@ handle_offnet_timeout(#offnet_req{
 handle_offnet_timeout(#offnet_req{call=Call}) ->
     {ok, kzt_util:update_call_status(?STATUS_NOANSWER, Call)}.
 
--spec which_time/2 :: (api_integer(), pos_integer()) -> non_neg_integer().
+-spec which_time(api_integer(), pos_integer()) -> non_neg_integer().
 which_time(undefined, Timelimit) when is_integer(Timelimit), Timelimit > 0 -> Timelimit;
 which_time(Timeout, _) when is_integer(Timeout), Timeout > 0 -> Timeout;
 which_time(_, _) -> 0.
 
--spec update_offnet_timers/1 :: (offnet_req()) -> offnet_req().
+-spec update_offnet_timers(offnet_req()) -> offnet_req().
 update_offnet_timers(#offnet_req{
                         call_timeout=undefined
                         ,call_time_limit=CallTimeLimit
