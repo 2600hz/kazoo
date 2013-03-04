6e3d400f5f02c7191558d277d14ccf9c3f3a8b96
James Aimonetti <james@2600hz.org>
2013-03-04 13:47:11 -0800

    KAZOO-240: fix gather sample

--- whistle_apps/apps/pivot/priv/samples/gather.2.process.php
+++ whistle_apps/apps/pivot/priv/samples/gather.2.process.php
@@ -5,6 +5,6 @@ echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
 if($_REQUEST['Digits'] == "TIMEOUT") {
   echo "<Response><Say>Time out, goodbye</Say><Hangup /></Response>";
 } else {
-  echo "<Response><Say>You entered " .  . "</Say></Response>";
+  echo "<Response><Say>You entered " . $_REQUEST['Digits'] . "</Say></Response>";
 }
 ?>
