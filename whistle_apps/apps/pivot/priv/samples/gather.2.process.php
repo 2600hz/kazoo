0fb3d035b415efd03665c092a1f1dfd43989a4b2
James Aimonetti <james@2600hz.org>
2013-03-03 22:46:17 -0800

    KAZOO-240: update some gather samples

--- whistle_apps/apps/pivot/priv/samples/gather.2.process.php
+++ whistle_apps/apps/pivot/priv/samples/gather.2.process.php
@@ -1,5 +1,10 @@
 <?php
     header("content-type: text/xml");
+
 echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
-echo "<Response><Say>You entered " . $_REQUEST['Digits'] . "</Say></Response>";
+if($_REQUEST['Digits'] == "TIMEOUT") {
+  echo "<Response><Say>Time out, goodbye</Say><Hangup /></Response>";
+} else {
+  echo "<Response><Say>You entered " .  . "</Say></Response>";
+}
 ?>
