5e13417900a2b168c673571dbc6ba696a040f874
James Aimonetti <james@2600hz.org>
2013-03-12 18:03:13 -0700

    KAZOO-240: add simple dial sample

---
+++ whistle_apps/apps/pivot/priv/samples/dial.1.php
@@ -0,0 +1,8 @@
+<?php
+    header("content-type: text/xml");
+    echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
+?>
+<Response>
+    <Dial>415-123-4567</Dial>
+    <Say>Goodbye</Say>
+</Response>
