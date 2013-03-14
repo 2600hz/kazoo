9497ac21ddc1624512b740ed3a77538eebabad10
karl anderson <karl@2600hz.com>
2013-03-14 19:59:10 +0000

    resolve KAZOO-706: I see your fancy ANSI coloring and raise you one

--- whistle_apps/apps/crossbar/src/crossbar_default_handler.erl
+++ whistle_apps/apps/crossbar/src/crossbar_default_handler.erl
@@ -17,15 +17,28 @@
 -include("crossbar.hrl").
 
 -spec init({atom(), 'http'}, #http_req{}, wh_proplist()) -> {'ok', #http_req{}, 'undefined'}.
-init({_Any, 'http'}, Req, []) ->    
+init({_Any, 'http'}, Req, []) ->
     {'ok', Req, 'undefined'}.
 
 -spec handle(#http_req{}, State) -> {'ok', #http_req{}, State}.
 handle(Req, State) ->
-    {'ok', Req1} = cowboy_http_req:reply(200, [], <<"Howdy, new world!">>, Req),
+    Headers = [{'Content-Type', "text/plain; charset=UTF-8"}],
+    {'ok', Req1} = cowboy_http_req:chunked_reply(200, Headers, Req),
+    Path = code:priv_dir(crossbar) ++ "/kazoo.txt",
+    {'ok', Fd} = file:open(Path, 'read'),
+    _ = readme_lines(Fd, Req1),
     {'ok', Req1, State}.
 
 -spec terminate(#http_req{}, term()) -> 'ok'.
 terminate(_Req, _State) -> 'ok'.
 
-
+-spec readme_lines(file:fd(), #http_req{}) -> 'ok' | {'error', _}.
+readme_lines(Fd, Req) ->
+    case file:read_line(Fd) of
+        {'ok', Data} ->
+            _ = cowboy_http_req:chunk(Data, Req),
+            timer:sleep(crypto:rand_uniform(15,250)),
+            readme_lines(Fd, Req);
+        _Else ->
+            file:close(Fd)
+    end.
