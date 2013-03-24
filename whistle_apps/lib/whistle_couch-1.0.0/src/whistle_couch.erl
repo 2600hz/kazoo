46780e4a6cf9c9a89bba386c8f77b134e98f39cd
James Aimonetti <james@2600hz.org>
2013-03-23 19:20:42 -0700

    KAZOO-543: formatting

--- whistle_apps/lib/whistle_couch-1.0.0/src/whistle_couch.erl
+++ whistle_apps/lib/whistle_couch-1.0.0/src/whistle_couch.erl
@@ -16,7 +16,7 @@
          ,stop/0, stop/1
         ]).
 
--define(DEPS, [sasl, crypto, public_key, ibrowse, couchbeam]).
+-define(DEPS, ['sasl', 'crypto', 'public_key', 'ibrowse', 'couchbeam']).
 
 %%
 start(_Type, _Args) ->
@@ -33,17 +33,15 @@ start_link() ->
 %% @doc Start the couch server.
 start() ->
     _ = start_deps(),
-    application:start(whistle_couch).
+    application:start('whistle_couch').
 
--spec start_deps/0 :: () -> any().
+-spec start_deps() -> any().
 start_deps() ->
     whistle_couch_deps:ensure(?MODULE),
     [wh_util:ensure_started(Dep) || Dep <- ?DEPS].
 
 %% @spec stop() -> ok
 %% @doc Stop the couch server.
-stop() ->
-    application:stop(whistle_couch).
+stop() -> application:stop('whistle_couch').
 
-stop(_) ->
-    ok.
+stop(_) -> 'ok'.
