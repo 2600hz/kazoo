19279cf429d975e078b95c5b391617b3c6b451e6
James Aimonetti <james@2600hz.org>
2013-03-25 11:40:38 -0700

    KAZOO-543: retry init steps if errors while loading agents/queues

--- whistle_apps/apps/acdc/src/acdc_init.erl
+++ whistle_apps/apps/acdc/src/acdc_init.erl
@@ -14,19 +14,18 @@
 -include("acdc.hrl").
 
 -spec start_link() -> 'ignore'.
-start_link() ->
-    spawn(?MODULE, init_acdc, []),
-    ignore.
+start_link() -> spawn(?MODULE, 'init_acdc', []), 'ignore'.
 
 -spec init_acdc() -> any().
 init_acdc() ->
-    [init_account(Acct) || Acct <- whapps_util:get_all_accounts(encoded)].
+    put('callid', ?MODULE),
+    [init_account(Acct) || Acct <- whapps_util:get_all_accounts('encoded')].
 
 -spec init_account(ne_binary()) -> 'ok'.
 init_account(AcctDb) ->
     lager:debug("init account: ~s", [AcctDb]),
 
-    init_queues((AcctId = wh_util:format_account_id(AcctDb, raw))
+    init_queues((AcctId = wh_util:format_account_id(AcctDb, 'raw'))
                 ,couch_mgr:get_results(AcctDb, <<"queues/crossbar_listing">>, [])
                ),
     init_agents(AcctId
@@ -34,13 +33,45 @@ init_account(AcctDb) ->
                ).
 
 -spec init_queues(ne_binary(), {'ok', wh_json:objects()} | {'error', _}) -> any().
-init_queues(_, {ok, []}) -> ok;
-init_queues(_, {error, _E}) -> lager:debug("error fetching queues: ~p", [_E]);
-init_queues(AcctId, {ok, Qs}) ->
+init_queues(_, {'ok', []}) -> 'ok';
+init_queues(AcctId, {'error', 'gateway_timeout'}) ->
+    lager:debug("gateway timed out loading queues in account ~s, trying again in a moment", [AcctId]),
+    try_queues_again(AcctId),
+    wait_a_bit(),
+    'ok';
+init_queues(AcctId, {'error', _E}) ->
+    lager:debug("error fetching queues: ~p", [_E]),
+    try_queues_again(AcctId),
+    wait_a_bit(),
+    'ok';
+init_queues(AcctId, {'ok', Qs}) ->
     acdc_stats:init_db(AcctId),
     [acdc_queues_sup:new(AcctId, wh_json:get_value(<<"id">>, Q)) || Q <- Qs].
 
-init_agents(_, {ok, []}) -> ok;
-init_agents(_, {error, _E}) -> lager:debug("error fetching agents: ~p", [_E]);
-init_agents(AcctId, {ok, As}) ->
+init_agents(_, {'ok', []}) -> 'ok';
+init_agents(AcctId, {'error', 'gateway_timeout'}) ->
+    lager:debug("gateway timed out loading agents in account ~s, trying again in a moment", [AcctId]),
+    try_agents_again(AcctId),
+    wait_a_bit(),
+    'ok';
+init_agents(AcctId, {'error', _E}) ->
+    lager:debug("error fetching agents: ~p", [_E]),
+    try_agents_again(AcctId),
+    wait_a_bit(),
+    'ok';
+init_agents(AcctId, {'ok', As}) ->
     [acdc_agents_sup:new(AcctId, wh_json:get_value(<<"id">>, A)) || A <- As].
+
+wait_a_bit() -> timer:sleep(1000 + random:uniform(500)).
+
+try_queues_again(AcctId) -> try_again(AcctId, <<"queues/crossbar_listing">>).
+try_agents_again(AcctId) -> try_again(AcctId, <<"agents/crossbar_listing">>).
+
+try_again(AcctId, View) ->
+    spawn(fun() ->
+                  put('callid', ?MODULE),
+                  wait_a_bit(),
+                  init_queues(AcctId, couch_mgr:get_results(wh_util:format_accuont_id(AcctId, 'encoded')
+                                                            ,View, []
+                                                           ))
+          end).
