6ea80115ef6f52604db1c17760808312688e9d52
Peter Defebvre <peter@2600hz.com>
2013-03-25 19:34:14 +0000

    KAZOO-590: Setting cookies in init

--- ecallmgr/src/ecallmgr_init.erl
+++ ecallmgr/src/ecallmgr_init.erl
@@ -15,14 +15,21 @@
 -include("ecallmgr.hrl").
 
 start_link() ->
-    spawn(?MODULE, init, []),
-    ignore.
+    spawn(?MODULE, 'init', []),
+    'ignore'.
 
 init() ->
-    put(callid, ?MODULE),
-    [set_loglevel(K, F, D) || {K, F, D} <- [{<<"console_log_level">>, fun wh_util:change_console_log_level/1, notice}
-                                            ,{<<"error_log_level">>, fun wh_util:change_error_log_level/1, error}
-                                            ,{<<"syslog_log_level">>, fun wh_util:change_syslog_log_level/1, info}
+    put('callid', ?MODULE),
+    case wh_config:get_atom('bigcouch', 'cookie') of
+        [Cookie|_] ->
+            erlang:set_cookie(erlang:node(), Cookie),
+            lager:info("setting ecallmgr cookie to ~p~n", [Cookie]);
+        [] ->
+            lager:warning("failed to set ecallmgr cookie ~n", [])
+    end,
+    [set_loglevel(K, F, D) || {K, F, D} <- [{<<"console_log_level">>, fun wh_util:change_console_log_level/1, 'notice'}
+                                            ,{<<"error_log_level">>, fun wh_util:change_error_log_level/1, 'error'}
+                                            ,{<<"syslog_log_level">>, fun wh_util:change_syslog_log_level/1, 'info'}
                                            ]].
 
 set_loglevel(K, F, D) ->
