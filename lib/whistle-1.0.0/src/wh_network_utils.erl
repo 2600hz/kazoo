46f6867dbb56e1df78699db06a5d866914a51620
James Aimonetti <james@2600hz.org>
2013-04-30 17:19:19 +0200

    KAZOO-1: whistle dialyzer specs, tickies, formatting

--- lib/whistle-1.0.0/src/wh_network_utils.erl
+++ lib/whistle-1.0.0/src/wh_network_utils.erl
@@ -25,30 +25,29 @@
 
 -spec get_hostname() -> string().
 get_hostname() ->
-    {ok, Host} = inet:gethostname(),
-    {ok, #hostent{h_name=Hostname}} = inet:gethostbyname(Host),
+    {'ok', Host} = inet:gethostname(),
+    {'ok', #hostent{h_name=Hostname}} = inet:gethostbyname(Host),
     Hostname.
 
--spec is_ipv4(nonempty_string() | ne_binary()) -> boolean().
+-spec is_ipv4(text()) -> boolean().
 is_ipv4(Address) when is_binary(Address) ->
     is_ipv4(wh_util:to_list(Address));
 is_ipv4(Address) when is_list(Address) ->
     case inet_parse:ipv4_address(Address) of
-        {ok, _} ->
-            true;
-        {error, _} -> false
+        {'ok', _} -> 'true';
+        {'error', _} -> 'false'
     end.
 
--spec is_ipv6(nonempty_string() | ne_binary()) -> boolean().
+-spec is_ipv6(text()) -> boolean().
 is_ipv6(Address) when is_binary(Address) ->
     is_ipv6(wh_util:to_list(Address));
 is_ipv6(Address) when is_list(Address) ->
     case inet_parse:ipv6_address(Address) of
-        {ok, _} -> true;
-        {error, _} -> false
+        {'ok', _} -> 'true';
+        {'error', _} -> 'false'
     end.
 
--spec verify_cidr(string() | ne_binary(), string() | ne_binary()) -> boolean().
+-spec verify_cidr(text(), text()) -> boolean().
 verify_cidr(IP, CIDR) when is_binary(IP) ->
     verify_cidr(wh_util:to_list(IP), CIDR);
 verify_cidr(IP, CIDR) when is_binary(CIDR) ->
@@ -58,33 +57,33 @@ verify_cidr(IP, CIDR) ->
     %% so, ummm ya, but probably cheaper then my expand bellow followed by a list
     %% test.  Just be aware this should only be used where performance is not
     %% critical
-    case orber_acl:verify(IP, CIDR, inet) of
-        true -> true;
-        {false, _, _} -> false;
-        {error, _} -> false
+    case orber_acl:verify(IP, CIDR, 'inet') of
+        'true' -> 'true';
+        {'false', _, _} -> 'false';
+        {'error', _} -> 'false'
     end.
 
--spec expand_cidr(string() | ne_binary()) -> [ne_binary(),...] | [].
+-spec expand_cidr(text()) -> ne_binaries().
 expand_cidr(CIDR) when is_binary(CIDR) ->
     expand_cidr(wh_util:to_list(CIDR));
 expand_cidr(CIDR) ->
     %% EXTREMELY wastefull/naive approach, should never be used, but if you
     %% must we keep it in a class C
-    case orber_acl:range(CIDR, inet) of
-        {error, _} -> [];
-        {ok, Start, End} ->
+    case orber_acl:range(CIDR, 'inet') of
+        {'error', _} -> [];
+        {'ok', Start, End} ->
             [A1, B1, C1, D1] = lists:map(fun wh_util:to_integer/1, string:tokens(Start, ".")),
             [A2, B2, C2, D2] = lists:map(fun wh_util:to_integer/1, string:tokens(End, ".")),
-            true = ((A2 + B2 + C2 + D2) - (A1 + B1 + C1 + D1)) =< 510,
+            'true' = ((A2 + B2 + C2 + D2) - (A1 + B1 + C1 + D1)) =< 510,
             [iptuple_to_binary({A,B,C,D})
-             || A <- lists:seq(A1, A2)
-                    ,B <- lists:seq(B1, B2)
-                    ,C <- lists:seq(C1, C2)
-                    ,D <- lists:seq(D1, D2)
+             || A <- lists:seq(A1, A2),
+                B <- lists:seq(B1, B2),
+                C <- lists:seq(C1, C2),
+                D <- lists:seq(D1, D2)
             ]
     end.
 
--spec is_rfc1918_ip(string() | ne_binary()) -> boolean().
+-spec is_rfc1918_ip(text()) -> boolean().
 is_rfc1918_ip(IP) ->
     verify_cidr(IP, "192.168.0.0/16")
         orelse verify_cidr(IP, "10.0.0.0/8")
@@ -93,51 +92,47 @@ is_rfc1918_ip(IP) ->
 -spec resolve(ne_binary()) -> wh_ip_list().
 resolve(Address) ->
     case binary:split(Address, <<":">>) of
-        [Addr|_Port] ->
-            maybe_is_ip(Addr);
-        _ ->
-            maybe_is_ip(Address)
+        [Addr|_Port] -> maybe_is_ip(Addr);
+        _ -> maybe_is_ip(Address)
     end.
 
 maybe_is_ip(Address) ->
     case is_ipv4(Address) of
-        true -> [Address];
-        false ->
-            maybe_resolve_srv_records(Address)
+        'true' -> [Address];
+        'false' -> maybe_resolve_srv_records(Address)
     end.
 
 -spec maybe_resolve_srv_records(ne_binary()) -> wh_ip_list().
 maybe_resolve_srv_records(Address) ->
     Domain = <<"_sip._udp.", Address/binary>>,
-    case inet_res:lookup(wh_util:to_list(Domain), in, srv) of
+    case inet_res:lookup(wh_util:to_list(Domain), 'in', 'srv') of
         [] -> maybe_resolve_a_records([Address]);
         SRVs -> maybe_resolve_a_records([D || {_, _, _, D} <- SRVs])
     end.
 
 -spec maybe_resolve_a_records(ne_binary()) -> wh_ip_list().
 maybe_resolve_a_records(Domains) ->
-    lists:foldr(fun(Domain, IPs) ->
-                        case is_ipv4(Domain) of
-                            true -> [Domain];
-                            false ->
-                                D = wh_util:to_list(Domain),
-                                resolve_a_record(D, IPs)
-                        end
-                end, [], Domains).
+    lists:foldr(fun maybe_resolve_fold/2, [], Domains).
+
+maybe_resolve_fold(Domain, IPs) ->
+    case is_ipv4(Domain) of
+        'true' -> [Domain];
+        'false' -> resolve_a_record(wh_util:to_list(Domain), IPs)
+    end.
 
 -spec resolve_a_record(ne_binary(), wh_ip_list()) -> wh_ip_list().
 resolve_a_record(Domain, IPs) ->
-    case inet:getaddrs(Domain, inet) of
-        {error, _R} ->
+    case inet:getaddrs(Domain, 'inet') of
+        {'error', _R} ->
             lager:info("unable to resolve ~s: ~p", [Domain, _R]),
             IPs;
-        {ok, Addresses} ->
+        {'ok', Addresses} ->
             lists:foldr(fun(IPTuple, I) ->
                                 [iptuple_to_binary(IPTuple)|I]
                         end, IPs, Addresses)
     end.
 
--spec iptuple_to_binary({1..255, 1..255, 1..255, 1..255}) -> ne_binary().
+-spec iptuple_to_binary(inet:ip4_address()) -> ne_binary().
 iptuple_to_binary({A,B,C,D}) ->
     <<(wh_util:to_binary(A))/binary, "."
       ,(wh_util:to_binary(B))/binary, "."
