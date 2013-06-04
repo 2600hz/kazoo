9325af306c68eb006d89094b4aa34f21a02b2347
Peter Defebvre <peter@2600hz.com>
2013-06-04 18:51:16 +0000

    KAZOO-966: Fix bash scripts conn to ecallmg and to apps

--- whistle_apps/conn-to-apps.sh
+++ whistle_apps/conn-to-apps.sh
@@ -3,25 +3,28 @@
 cd `dirname $0`
 
 if [ -z "$1" ]; then
-    REMOTE_SHELL="whistle_apps@`hostname`"
+    REMOTE_SHELL="whistle_apps@`hostname -f`"
 else
     REMOTE_SHELL="$1"
 fi
 
 if [ -z "$2" ]; then
-    SHELL_NAME="whistle_con_$(date +%s)@`hostname`"
+    SHELL_NAME="whistle_con_$(date +%s)@`hostname -f`"
 else
     SHELL_NAME="$2"
 fi
 
 if [[ ! "$SHELL_NAME" == *@*  ]]; then
-    SHELL_NAME="${SHELL_NAME}@`hostname`"
+    SHELL_NAME="${SHELL_NAME}@`hostname -f`"
 fi
 
 if [[ ! "$REMOTE_SHELL" == *@*  ]]; then
-    REMOTE_SHELL="${REMOTE_SHELL}@`hostname`"
+    REMOTE_SHELL="${REMOTE_SHELL}@`hostname -f`"
 fi
 
-ERL_COOKIE=`grep "setcookie" conf/vm.args`
+ERL_COOKIE=`../utils/sup/sup erlang get_cookie`
 
-exec erl $ERL_COOKIE -name ${SHELL_NAME} -remsh ${REMOTE_SHELL}
+echo $ERL_COOKIE
+
+exec erl -setcookie $ERL_COOKIE -name ${SHELL_NAME} -remsh ${REMOTE_SHELL}
+
