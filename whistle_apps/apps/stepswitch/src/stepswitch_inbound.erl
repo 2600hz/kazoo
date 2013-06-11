889c02c30962d59379f080839f5134a777a94b98
James Aimonetti <james@2600hz.org>
2013-06-11 11:01:19 -0700

    v2.14: update formatting, specs, tickies

--- whistle_apps/apps/stepswitch/src/stepswitch_inbound.erl
+++ whistle_apps/apps/stepswitch/src/stepswitch_inbound.erl
@@ -1,5 +1,5 @@
 %%%-------------------------------------------------------------------
-%%% @copyright (C) 2011, VoIP INC
+%%% @copyright (C) 2011-2013, 2600Hz
 %%% @doc
 %%% Handle route requests
 %%% @end
@@ -11,18 +11,16 @@
 -include("stepswitch.hrl").
 
 -spec init() -> 'ok'.
-init() ->
-    'ok'.
+init() -> 'ok'.
 
--spec handle_req(wh_json:json_object(), proplist()) -> 'ok'.
+-spec handle_req(wh_json:object(), wh_proplist()) -> 'ok'.
 handle_req(JObj, _Prop) ->
     _ = whapps_util:put_callid(JObj),
     case wh_json:get_ne_value([<<"Custom-Channel-Vars">>, <<"Account-ID">>], JObj) of
-        undefined ->
+        'undefined' ->
             lager:debug("received new inbound dialplan route request"),
             _ =  inbound_handler(JObj);
-        _AcctID ->
-            ok
+        _AcctID -> 'ok'
     end.
 
 %%--------------------------------------------------------------------
@@ -31,16 +29,16 @@ handle_req(JObj, _Prop) ->
 %% handle a request inbound from offnet
 %% @end
 %%--------------------------------------------------------------------
--spec inbound_handler(wh_json:json_object()) -> 'ok'.
--spec inbound_handler(wh_json:json_object(), ne_binary()) -> 'ok'.
+-spec inbound_handler(wh_json:object()) -> 'ok'.
+-spec inbound_handler(wh_json:object(), ne_binary()) -> 'ok'.
 inbound_handler(JObj) ->
     inbound_handler(JObj, get_dest_number(JObj)).
 inbound_handler(JObj, Number) ->
     case stepswitch_util:lookup_number(Number) of
-        {ok, AccountId, Props} ->
+        {'ok', AccountId, Props} ->
             lager:debug("number associated with account ~s", [AccountId]),
             relay_route_req(AccountId, Props, JObj);
-        {error, _R} ->
+        {'error', _R} ->
             lager:debug("failed to find account for number ~s: ~p", [Number, _R])
     end.
 
@@ -50,11 +48,11 @@ inbound_handler(JObj, Number) ->
 %% determine the e164 format of the inbound number
 %% @end
 %%--------------------------------------------------------------------
--spec get_dest_number(wh_json:json_object()) -> ne_binary().
+-spec get_dest_number(wh_json:object()) -> ne_binary().
 get_dest_number(JObj) ->
     {User, _} = whapps_util:get_destination(JObj, ?APP_NAME, <<"inbound_user_field">>),
     case whapps_config:get_is_true(<<"stepswitch">>, <<"assume_inbound_e164">>) of
-        true ->
+        'true' ->
             Number = assume_e164(User),
             lager:debug("assuming number is e164, normalizing to ~s", [Number]),
             Number;
@@ -71,10 +69,8 @@ get_dest_number(JObj) ->
 %% @end
 %%--------------------------------------------------------------------
 -spec assume_e164(ne_binary()) -> ne_binary().
-assume_e164(<<$+, _/binary>> = Number) ->
-    Number;
-assume_e164(Number) ->
-    <<$+, Number/binary>>.
+assume_e164(<<$+, _/binary>> = Number) -> Number;
+assume_e164(Number) -> <<$+, Number/binary>>.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -83,12 +79,12 @@ assume_e164(Number) ->
 %% account and authorizing  ID
 %% @end
 %%--------------------------------------------------------------------
--spec custom_channel_vars(ne_binary(), wh_json:json_object()) -> wh_json:json_object().
+-spec custom_channel_vars(ne_binary(), wh_json:object()) -> wh_json:object().
 custom_channel_vars(AccountId, JObj) ->
     CCVs = wh_json:get_value(<<"Custom-Channel-Vars">>, JObj, wh_json:new()),
     RemoveKeys = [<<"Account-ID">>
-                      ,<<"Inception">>
-                      ,<<"Authorizing-ID">>
+                  ,<<"Inception">>
+                  ,<<"Authorizing-ID">>
                  ],
     Props = [{<<"Account-ID">>, AccountId}
              ,{<<"Inception">>, <<"off-net">>}
@@ -102,25 +98,25 @@ custom_channel_vars(AccountId, JObj) ->
 %% relay a route request once populated with the new properties
 %% @end
 %%--------------------------------------------------------------------
--spec relay_route_req(ne_binary(), proplist(), wh_json:json_object()) -> 'ok'.
+-spec relay_route_req(ne_binary(), wh_proplist(), wh_json:object()) -> 'ok'.
 relay_route_req(AccountId, Props, JObj) ->
     Routines = [fun(J) -> custom_channel_vars(AccountId, J) end
                 ,fun(J) ->
-                         case props:get_value(inbound_cnam, Props) of
-                             false -> J;
-                             true -> stepswitch_cnam:lookup(J)
+                         case props:get_value('inbound_cnam', Props) of
+                             'false' -> J;
+                             'true' -> stepswitch_cnam:lookup(J)
                          end
                  end
                 ,fun(J) ->
-                         case props:get_value(ringback_media, Props) of
-                             undefined -> J;
+                         case props:get_value('ringback_media', Props) of
+                             'undefined' -> J;
                              MediaId ->
                                  wh_json:set_value(<<"Ringback-Media">>, MediaId, J)
                          end
                  end
                 ,fun(J) ->
-                         case props:get_value(transfer_media, Props) of
-                             undefined -> J;
+                         case props:get_value('transfer_media', Props) of
+                             'undefined' -> J;
                              MediaId ->
                                  wh_json:set_value(<<"Transfer-Media">>, MediaId, J)
                          end
