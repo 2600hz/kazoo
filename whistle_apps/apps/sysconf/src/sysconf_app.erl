7971db3086b2169f38a0d702839a3d671dbb6600
James Aimonetti <james@2600hz.org>
2013-06-17 14:40:47 -0700

    whapps_as_apps: update sysconf

--- whistle_apps/apps/sysconf/src/sysconf_app.erl
+++ whistle_apps/apps/sysconf/src/sysconf_app.erl
@@ -1,8 +1,10 @@
-%%% @author Edouard Swiac <edouard@2600hz.org>
-%%% @copyright (C) 2011 Edouard Swiac
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2011-2013, 2600Hz
 %%% @doc
-%%% 
+%%%
 %%% @end
+%%% @contributors
+%%%-------------------------------------------------------------------
 -module(sysconf_app).
 
 -behaviour(application).
@@ -14,13 +16,9 @@
 %% Application callbacks
 %% ===================================================================
 
--spec(start(StartType :: term(), StartArgs :: term()) -> tuple(ok, pid()) | tuple(error, term())).
-start(_StartType, _StartArgs) ->
-    case sysconf:start_link() of
-	{ok, P} -> {ok, P};
-	{error, {already_started, P} } -> {ok, P};
-	{error, _}=E -> E
-    end.
+-spec start(term(), term()) ->
+                   {'ok', pid()} |
+                   {'error', term()}.
+start(_StartType, _StartArgs) -> sysconf:start_link().
 
-stop(_State) ->
-    ok.
+stop(_State) -> sysconf:stop().
