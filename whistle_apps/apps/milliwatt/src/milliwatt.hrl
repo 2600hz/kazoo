593bf724fe0e117100cbb66f5af19faf7472daae
Peter Defebvre <peter@2600hz.com>
2013-06-17 22:08:03 +0000

    KAZOO-415: milliwatt whapp skel

---
+++ whistle_apps/apps/milliwatt/src/milliwatt.hrl
@@ -0,0 +1,10 @@
+-ifndef(milliwatt_HRL).
+
+%% Typical includes needed
+-include_lib("whistle/include/wh_amqp.hrl").
+-include_lib("whistle/include/wh_types.hrl").
+-include_lib("whistle/include/wh_log.hrl").
+-include_lib("whistle/include/wh_databases.hrl").
+
+-define(SKEL_HRL, true).
+-endif.
