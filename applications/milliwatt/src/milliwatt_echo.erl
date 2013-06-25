5e2459dfe9a34dfe8e13bf8159e9613debd6055a
Peter Defebvre <peter@2600hz.com>
2013-06-25 19:39:49 +0000

    KAZOO-1081: create echo action

---
+++ applications/milliwatt/src/milliwatt_echo.erl
@@ -0,0 +1,19 @@
+-module(milliwatt_echo).
+
+-export([exec/1]).
+
+-include("milliwatt.hrl").
+
+-define(DURATION, 10000).
+
+exec(Call) ->
+	lager:info("milliwatt execute action echo", []),
+	whapps_call_command:answer(Call),
+	whapps_call_command:echo(Call),
+	timer:sleep(get_duration()),
+	whapps_call_command:hangup(Call).
+
+-spec get_duration() -> integer().
+get_duration() ->
+	JObj = whapps_config:get_non_empty(<<"milliwatt">>, <<"echo">>),
+	wh_json:get_integer_value(<<"duration">>, JObj, ?DURATION).
