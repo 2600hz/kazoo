7e77ca5ede0269722505c513a6add668cce890f2
karl anderson <karl@2600hz.com>
2013-06-25 18:28:47 -0700

    either move rabbit_common out, update the make file of for a bunch of apps, or mangle the client includes... I went with the first... debate! also updated some unecessary includes

--- applications/registrar/src/reg.hrl
+++ applications/registrar/src/reg.hrl
@@ -1,5 +1,4 @@
 -ifndef(REG_HRL).
--include_lib("whistle/include/wh_amqp.hrl").
 -include_lib("whistle/include/wh_types.hrl").
 -include_lib("whistle/include/wh_log.hrl").
 -include_lib("whistle/include/wh_databases.hrl").
--- applications/spyvsspy/src/spyvsspy.hrl
+++ applications/spyvsspy/src/spyvsspy.hrl
@@ -1,7 +1,6 @@
 -ifndef(SPYVSSPY_HRL).
 
 %% Typical includes needed
--include_lib("whistle/include/wh_amqp.hrl").
 -include_lib("whistle/include/wh_types.hrl").
 -include_lib("whistle/include/wh_log.hrl").
 -include_lib("whistle/include/wh_databases.hrl").
