a2fee5b8d4600770f3bedec21be36f6d11232ed1
karl anderson <karl@2600hz.com>
2013-07-15 16:17:34 -0700

    support apps that include local libs

--- scripts/dev-start-apps.sh
+++ scripts/dev-start-apps.sh
@@ -4,4 +4,4 @@ cd `dirname $0`
 
 export ERL_CRASH_DUMP=$(date +%s)_apps_erl_crash.dump
 export ERL_LIBS=$PWD/../deps:$PWD/../core
-exec erl -name whistle_apps -args_file /etc/kazoo/vm.args -pa $PWD/../applications/*/ebin -pa $PWD/../core/*/ebin -pa $PWD/../deps/*/ebin -s reloader -s whistle_apps
+exec erl -name whistle_apps -args_file /etc/kazoo/vm.args -pa $PWD/../applications/*/ebin -pa $PWD/../core/*/ebin -pa $PWD/../deps/*/ebin $PWD/../deps/*/ebin $PWD/applications/*/lib/*/ebin -s reloader -s whistle_apps
