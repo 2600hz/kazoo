058ebe9fd5865cee553c18b20160c2dbe86b252f
karl anderson <karl@2600hz.com>
2013-07-15 16:46:29 -0700

    include -pa

--- scripts/start-apps.sh
+++ scripts/start-apps.sh
@@ -3,4 +3,4 @@
 cd `dirname $0`
 export ERL_CRASH_DUMP=$(date +%s)_apps_erl_crash.dump
 export ERL_LIBS=$PWD/../deps:$PWD/../core
-exec erl -name whistle_apps -args_file /etc/kazoo/vm.args -pa $PWD/../applications/*/ebin -pa $PWD/../core/*/ebin -pa $PWD/../deps/*/ebin $PWD/applications/*/lib/*/ebin -detached -s whistle_apps 
+exec erl -name whistle_apps -args_file /etc/kazoo/vm.args -pa $PWD/../applications/*/ebin -pa $PWD/../core/*/ebin -pa $PWD/../deps/*/ebin -pa $PWD/applications/*/lib/*/ebin -detached -s whistle_apps 
