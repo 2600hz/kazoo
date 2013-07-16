4eb1f99330bd422a5050dd46d3793be56dd493e6
Ben Wann <ben.wann@gmail.com>
2013-07-16 01:17:30 +0000

    Setting up the migration scaffolding for migrating CDR records

---
+++ applications/cdr/src/cdr_v3_migrate_server.erl
@@ -0,0 +1,161 @@
+%%%-------------------------------------------------------------------
+%%% @author Ben Wann <bwann@tickbook.local>
+%%% @copyright (C) 2013, Ben Wann
+%%% @doc
+%%%
+%%% @end
+%%% Created : 15 Jul 2013 by Ben Wann <bwann@tickbook.local>
+%%%-------------------------------------------------------------------
+-module(cdr_v3_migrate_server).
+
+-behaviour(gen_server).
+
+-include("cdr.hrl").
+
+%% API
+-export([start_link/0]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+	 terminate/2, code_change/3]).
+
+-define(SERVER, ?MODULE). 
+
+-record(state, {current_account_id :: api_binary(),
+		account_list :: objects(),
+		current_from_date :: api_binary(),
+		current_to_date :: api_binary(),
+		response_pid :: pid(), %% pid of the processing of the response
+		response_ref :: reference() %% monitor ref for the pid
+         }).
+-type state() :: #state{}.
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    gen_server:start_link({local, ?SERVER}, ?MODULE, [], []).
+
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    lager:debug("Starting to migrate accounts to the new sharded db format"),
+    lager:debug("Execute Couch View to get all Accounts"),
+    case couch_mgr:get_results(<<"accounts">>, "listing_by_id", []) of
+	{'ok', Accounts} -> init_migrate(Accounts);
+	{'error', _}=E -> {'stop', E}
+    end.
+
+init_migrate(Accounts) ->    
+    lager:debug("Account List: ~p", [Accounts]),
+    {'ok', #state{account_list=Accounts}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+handle_cast({'start_migrate'}, #state{account_list=Accounts}=State) ->
+    lager:debug("Call to Handle Cast made"),
+    [CurrentAccount | RestAccounts] = Accounts,
+    FromDate = 63541090800,
+    ToDate = 63541177200,
+    {'ok', PID} = spawn_monitor('cdr_v3_migrate_worker', 'migrate_cdr_records', [CurrentAccount]),
+    {'reply', State#state{account_list=RestAccounts, current_account_id=CurrentAccount, current_to_date=ToDate, current_from_date=FromDate}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {'noreply', State}.
+
+handle_info({'DOWN', Ref, 'process', Pid, Reason}, #state{response_pid=Pid
+                                                          ,response_ref=Ref
+                                                         }=State) ->
+    lager:debug("response pid ~p(~p) down: ~p", [Pid, Ref, Reason]),
+    lager:debug("Placeholder for next call"),
+    {'noreply', State#state{response_pid='undefined'}, 'hibernate'};
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
