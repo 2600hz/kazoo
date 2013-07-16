60db2ed67fe89ebc564172ce1249ac3468776005
Ben Wann <ben.wann@gmail.com>
2013-07-16 01:17:30 +0000

    Setting up the migration scaffolding for migrating CDR records

--- applications/cdr/src/cdr_sup.erl
+++ applications/cdr/src/cdr_sup.erl
@@ -11,7 +11,7 @@
 -behaviour(supervisor).
 
 %% API
--export([start_link/0]).
+-export([start_link/0, start_v3_migrate/0]).
 
 %% Supervisor callbacks
 -export([init/1]).
@@ -24,6 +24,14 @@
 
 start_link() ->
     supervisor:start_link({'local', ?MODULE}, ?MODULE, []).
+
+start_v3_migrate() ->
+    case cdr_v3_migrate_server:start_link() of
+	{'ok', PID} -> 
+	    lager:debug("Migrate Started"),
+	    gen_server:cast(PID, {'start_migrate'});
+	{'error', E} -> lager:debug("Error when starting cdr migrate process: ~p", [E])
+    end.
 
 %% ===================================================================
 %% Supervisor callbacks
