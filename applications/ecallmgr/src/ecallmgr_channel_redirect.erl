7f422e9ba06c515d93b457b2490a56fcbad9be35
James Aimonetti <james@2600hz.org>
2013-09-18 16:44:12 -0700

    KAZOO-243: send redirect command directly to FreeSWITCH

--- applications/ecallmgr/src/ecallmgr_channel_redirect.erl
+++ applications/ecallmgr/src/ecallmgr_channel_redirect.erl
@@ -12,32 +12,33 @@
 
 -include("ecallmgr.hrl").
 
--spec redirect(ne_binary(), atom() | ne_binary()) ->
-                      {ne_binary(), ne_binary()}.
+-spec redirect(ne_binary(), atom() | ne_binary()) -> ecallmgr_util:send_cmd_ret().
 redirect(UUID, DestinationNode) ->
-    NodeURL = ecallmgr_fs_node:sip_url(DestinationNode),
+    DestNodeURL = ecallmgr_fs_node:sip_url(DestinationNode),
     case ecallmgr_config:get_boolean(<<"redirect_via_proxy">>, 'true') of
-        'true' -> redirect_via_proxy(NodeURL, UUID);
-        'false' -> redirect_via_endpoint(NodeURL, UUID)
+        'true' -> redirect_via_proxy(DestNodeURL, UUID);
+        'false' -> redirect_via_endpoint(DestNodeURL, UUID)
     end.
 
-redirect_via_proxy(NodeURL, UUID) ->
+redirect_via_proxy(DestNodeURL, UUID) ->
     {'ok', #channel{destination=ToUser
                     ,realm=Realm
                    }=Channel} = ecallmgr_fs_channel:fetch(UUID, 'record'),
     Contact = <<"sip:", ToUser/binary, "@", Realm/binary>>,
-    RedirectURL = binary:replace(NodeURL, <<"mod_sofia@">>, <<>>),
+    RedirectURL = binary:replace(DestNodeURL, <<"mod_sofia@">>, <<>>),
     send_redirect(RedirectURL, Contact, Channel).
 
-redirect_via_endpoint(NodeURL, UUID) ->
+redirect_via_endpoint(DestNodeURL, UUID) ->
     {'ok', #channel{destination=ToUser}=Channel} = ecallmgr_fs_channel:fetch(UUID, 'record'),
-    Contact = binary:replace(NodeURL, <<"mod_sofia">>, ToUser),
+    Contact = binary:replace(DestNodeURL, <<"mod_sofia">>, ToUser),
     send_redirect('undefined', Contact, Channel).
 
-send_redirect('undefined', Contact, _) ->
-    {<<"redirect">>, Contact};
+send_redirect('undefined', Contact, #channel{node=Node
+                                            ,uuid=UUID
+                                            }) ->
+    ecallmgr_util:send_cmd(Node, UUID, <<"redirect">>, Contact);
 send_redirect(RedirectUrl, Contact, #channel{node=Node
                                              ,uuid=UUID
                                             }) ->
     ecallmgr_util:set(Node, UUID, [{<<"sip_rh_X-Redirect-Server">>, RedirectUrl}]),
-    {<<"redirect">>, Contact}.
+    ecallmgr_util:send_cmd(Node, UUID, <<"redirect">>, Contact).
