8705a7c8326d1f38c7fd69129e71c85dccd3611c
Stephen Gibberd <stephen@finder.com.kh>
2013-10-08 07:51:13 +1100

    Move most of the work to ipdevice application

--- applications/ipdevice/src/ipdevice_listener.erl
+++ applications/ipdevice/src/ipdevice_listener.erl
@@ -1,6 +1,7 @@
 -module(ipdevice_listener).
 -behaviour(gen_listener).
 
+-include_lib("whistle/include/wh_types.hrl").
 %% API
 -export([start_link/0,start/0,init/1,handle_call/3,handle_cast/2,handle_info/2
          ,terminate/2,code_change/3,handle_req/2]).
@@ -15,10 +16,11 @@
 %-define(QUEUE_OPTIONS, [{exclusive, false}]).
 %-define(CONSUME_OPTIONS, [{exclusive, false}]).
 
-
+-spec start() -> 'ok' | {'error', _}.
 start() ->
     application:start(ipdevice).
 
+-spec start_link() -> startlink_ret().
 start_link() ->
     lager:debug("starting new ipdevice proc"),
     [wh_util:ensure_started(A) || A <- [ 'sasl'
@@ -54,42 +56,100 @@ code_change(_OldVsn, State, _Extra) ->
 terminate(_Reason, _State) ->
     lager:debug("listener terminating: ~p", [_Reason]).
 
+-spec handle_req(wh_json:object(), wh_proplist()) -> any().
 handle_req(JObj,_Props) ->
     lager:debug("request ~p~n",[JObj]),
-    case wh_json:get_value(<<"IP">>,JObj) of
+    MsgId = wh_json:get_value(<<"Msg-ID">>,JObj),
+    ServerId = wh_json:get_value(<<"Server-ID">>,JObj),
+    case wh_json:get_value(<<"Caller-Network-Addr">>,JObj) of
 	'undefined' ->
-	    'nothing';
+	    reply_not_ip_device(ServerId,MsgId,<<"no_ip_address">>);
 	IP ->
 	    case couch_mgr:get_results(<<"sip_auth">>
 					   ,<<"credentials/lookup_by_ip">>
 					   ,[{key,IP}]) of
-		{error, _R} ->
-		    lager:debug("unable to get view results for sip_auth resources: ~p", [_R]);
 		{ok, [SipAuth]} ->
-		    get_domain_and_send_reply(SipAuth
-					      ,wh_json:get_value(<<"Server-ID">>,JObj)
-					      ,wh_json:get_value(<<"Msg-ID">>,JObj)
-					     )
+		    check_domain(SipAuth
+				 ,ServerId
+				 ,MsgId
+				 ,JObj
+				);
+		Err ->
+		    lager:debug("no results for sip_auth resources: ~p", [Err]),
+		    reply_not_ip_device(MsgId,ServerId,<<"is_carrier">>)
 	    end
     end.
 
-get_domain_and_send_reply(SipAuth,ServerId,MsgId) ->
-    lager:debug("sipauth ~p",[SipAuth]),
+-spec check_domain(ne_binary(),ne_binary(),ne_binary(),wh_json:object()) -> 
+			  any().
+check_domain(SipAuth,ServerId,MsgId,JObj) ->
     AccountId = wh_json:get_value([<<"value">>, <<"account_id">>], SipAuth),
-    AuthorizingId = wh_json:get_value(<<"id">>, SipAuth),
-    OwnerId = wh_json:get_value([<<"value">>, <<"owner_id">>],SipAuth),
     AcctDB = wh_util:format_account_id(AccountId, encoded),
+    Domain = wh_json:get_value(<<"variable_sip_from_host">>,JObj),
     case couch_mgr:get_results(AcctDB,<<"account/listing_by_realm">>,[]) of
-	{ok, [JObj]} ->
-	    Domain = wh_json:get_value(<<"key">>, JObj),
-	    SendJObj = wh_json:from_list([{'account_id',AccountId}
-					  ,{'id',AuthorizingId}
-					  ,{'owner_id',OwnerId}
-					  ,{'domain',Domain}
-					  ,{<<"Msg-ID">>,MsgId}
-					 ]),
-	    lager:debug("sending ~p",[SendJObj]),
-	    amqp_util:targeted_publish(ServerId,wh_json:encode(SendJObj));
+	{ok, [AcctJObj]} ->
+	    case wh_json:get_value(<<"key">>, AcctJObj) of
+		Domain ->
+		    DeviceId = wh_json:get_value(<<"id">>, SipAuth),
+		    OwnerId = wh_json:get_value([<<"value">>, <<"owner_id">>]
+						,SipAuth),
+		    NewJObj = update_device_params(JObj
+						   ,Domain
+						   ,DeviceId
+						   ,OwnerId
+						   ,AccountId
+						  ),
+%		    SendJObj = wh_json:from_list([{'account_id',AccountId}
+%						  ,{'id',AuthorizingId}
+%						  ,{'owner_id',OwnerId}
+%						  ,{'domain',Domain}
+%						  ,{<<"Msg-ID">>,MsgId}
+%						 ]),
+		    lager:debug("sending ~p",[NewJObj]),
+		    amqp_util:targeted_publish(ServerId,wh_json:encode(NewJObj));
+		_ ->
+		    reply_not_ip_device(MsgId,ServerId,<<"wrong_domain">>)
+	    end;
 	_ ->
-	    lager:debug("cannot fetch account ~s",[AccountId])
+	    reply_not_ip_device(MsgId,ServerId,<<"db_failure">>)
     end.
+
+-spec update_device_params(wh_json:object(),ne_binary(),ne_binary(),
+			   ne_binary(),ne_binary()) ->  wh_json:object().
+update_device_params(JObj,Domain,DeviceId,OwnerId, AccountId) ->
+    User = wh_json:get_value(<<"variable_sip_from_user">>,JObj),
+    JObj2 = remove_trusted_acl_params(JObj),
+    wh_json:set_values([{<<"variable_sip_number_alias">>,User}
+			,{<<"variable_sip_auth_username">>,User}
+			,{<<"variable_sip_auth_realm">>,Domain}
+			,{<<"variable_number_alias">>,User}
+			,{<<"variable_user_name">>,User}
+			,{<<"variable_domain_name">>,Domain}
+			,{<<"variable_ecallmgr_Inception">>,<<"on-net">>}
+			,{<<"variable_ecallmgr_Owner-ID">>,OwnerId}
+			,{<<"variable_ecallmgr_Authorizing-Type">>,<<"device">>}
+			,{<<"variable_ecallmgr_Authorizing-ID">>,DeviceId}
+			,{<<"variable_ecallmgr_Account-ID">>,AccountId}
+			,{<<"variable_ecallmgr_Realm">>,Domain}
+			,{<<"variable_ecallmgr_Username">>,User}
+		       ],JObj2).
+
+-spec remove_trusted_acl_params(wh_json:object()) -> wh_json:object().
+remove_trusted_acl_params(JObj) ->
+    wh_json:delete_keys( [ <<"variable_sip_acl_authed_by">>
+			       , <<"variable_sip_from_epid">>
+			       ,<<"variable_sip_req_params">>
+			       ,<<"variable_sip_to_params">>
+			       ,<<"variable_sip_contact_params">>
+			       ,<<"variable_sip_acl_authed_by">>
+			       ,<<"variable_sip_from_epid">>
+			 ],JObj).
+
+-spec reply_not_ip_device(ne_binary(),ne_binary(),ne_binary()) -> any().
+reply_not_ip_device(MsgId,ServerId,Msg) ->
+    lager:debug("sending not ip device: ~s",[Msg]),
+    SendJObj = wh_json:from_list([{<<"Msg-ID">>,MsgId}
+				  ,{<<"result">>,Msg}
+				 ]),
+    amqp_util:targeted_publish(ServerId,wh_json:encode(SendJObj)).
+    
