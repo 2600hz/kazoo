063e2222daed426b21577c3fc21882da9e1e7e0b
Peter Defebvre <peter@2600hz.com>
2013-10-08 17:37:55 -0400

    KAZOO-14: Add attachment reading (not done yet)

--- applications/crossbar/src/modules/cb_apps_store.erl
+++ applications/crossbar/src/modules/cb_apps_store.erl
@@ -13,10 +13,11 @@
 -export([init/0
          ,authenticate/1
          ,authorize/1
-         ,allowed_methods/0, allowed_methods/1
-         ,resource_exists/0, resource_exists/1
-         ,validate/1, validate/2
-         ,get/1, get/2
+         ,allowed_methods/0, allowed_methods/1, allowed_methods/2
+         ,resource_exists/0, resource_exists/1, resource_exists/2
+         ,validate/1, validate/2, validate/3
+         ,content_types_provided/3
+         ,get/1, get/2, get/3
          ,post/2
         ]).
 
@@ -36,6 +37,7 @@
 %%--------------------------------------------------------------------
 -spec init() -> 'ok'.
 init() ->
+    _ = crossbar_bindings:bind(<<"v1_resource.content_types_provided.apps_store">>, ?MODULE, 'content_types_provided'),
     _ = crossbar_bindings:bind(<<"v1_resource.authenticate">>, ?MODULE, 'authenticate'),
     _ = crossbar_bindings:bind(<<"v1_resource.authorize">>, ?MODULE, 'authorize'),
     _ = crossbar_bindings:bind(<<"v1_resource.allowed_methods.apps_store">>, ?MODULE, 'allowed_methods'),
@@ -73,10 +75,13 @@ authorize(_) -> 'false'.
 %%--------------------------------------------------------------------
 -spec allowed_methods() -> http_methods() | [].
 -spec allowed_methods(path_token()) -> http_methods() | [].
+-spec allowed_methods(path_token(), path_token()) -> http_methods() | [].
 allowed_methods() ->
     [?HTTP_GET].
 allowed_methods(_) ->
     [?HTTP_GET, ?HTTP_POST].
+allowed_methods(_, _) ->
+    [?HTTP_GET].
 
 %%--------------------------------------------------------------------
 %% @public
@@ -89,9 +94,27 @@ allowed_methods(_) ->
 %%--------------------------------------------------------------------
 -spec resource_exists() -> 'true'.
 -spec resource_exists(path_token()) -> 'true'.
+-spec resource_exists(path_token(), path_token()) -> 'true'.
 resource_exists() -> 'true'.
 resource_exists(_) -> 'true'.
+resource_exists(_, _) -> 'true'.
 
+
+-spec content_types_provided(cb_context:context(), path_token(), path_token()) ->
+                                    cb_context:context().
+content_types_provided(#cb_context{req_verb = ?HTTP_GET}=Context, Id, <<"icon">>) ->
+    case read(Id, Context) of
+        #cb_context{resp_status='success', doc=JObj} ->
+            Icon = wh_json:get_value(<<"icon">>, JObj),
+            case wh_json:get_value([<<"_attachments">>, Icon], JObj) of
+                'undefined' -> Context;
+                Attachment ->
+                    CT = wh_json:get_value(<<"content_type">>, Attachment),
+                    [Type, SubType] = binary:split(CT, <<"/">>),
+                    Context#cb_context{content_types_provided=[{'to_binary', [{Type, SubType}]}]}
+            end
+    end;
+content_types_provided(Context, _, _) -> Context.
 
 %%--------------------------------------------------------------------
 %% @public
@@ -105,6 +128,7 @@ resource_exists(_) -> 'true'.
 %%--------------------------------------------------------------------
 -spec validate(cb_context:context()) -> cb_context:context().
 -spec validate(cb_context:context(), path_token()) -> cb_context:context().
+-spec validate(cb_context:context(), path_token(), path_token()) -> cb_context:context().
 validate(#cb_context{req_verb = ?HTTP_GET}=Context) ->
     summary(Context).
 
@@ -112,6 +136,31 @@ validate(#cb_context{req_verb = ?HTTP_GET}=Context, Id) ->
     read(Id, Context);
 validate(#cb_context{req_verb = ?HTTP_POST}=Context, Id) ->
     update(Id, Context).
+
+validate(#cb_context{req_verb = ?HTTP_GET}=Context, Id, <<"icon">>) ->
+    get_icon(Id, Context);
+validate(#cb_context{req_verb = ?HTTP_GET}=Context, Id, <<"screenshots">>) ->
+    get_icon(Id, Context).
+
+-spec get_icon(ne_binary(), cb_context:context()) -> cb_context:context().
+get_icon(Id, Context) ->
+    case read(Id, Context) of
+        #cb_context{resp_status='success', doc=JObj}=Con ->
+            Icon = wh_json:get_value(<<"icon">>, JObj),
+            case wh_json:get_value([<<"_attachments">>, Icon], JObj) of
+                'undefined' -> crossbar_util:response_bad_identifier(Id, Context);
+                Attachment ->
+                    lists:foldl(fun({K, V}, C) ->
+                                    cb_context:add_resp_header(K, V, C)
+                                end
+                                ,crossbar_doc:load_attachment(Id, Icon, Con)
+                                ,[{<<"Content-Disposition">>, <<"attachment; filename=", Icon/binary>>}
+                                  ,{<<"Content-Type">>, wh_json:get_value(<<"content_type">>, Attachment)}
+                                  ,{<<"Content-Length">>, wh_json:get_value(<<"length">>, Attachment)}
+                                 ])
+            end;
+        Context1 -> Context1
+    end.
 
 
 %%--------------------------------------------------------------------
@@ -123,10 +172,12 @@ validate(#cb_context{req_verb = ?HTTP_POST}=Context, Id) ->
 %% @end
 %%--------------------------------------------------------------------
 -spec get(cb_context:context()) -> cb_context:context().
--spec get(cb_context:context(), path_token()) -> cb_context:context().
+-spec get(cb_context:context(), path_token(), path_token()) -> cb_context:context().
 get(#cb_context{}=Context) ->
     Context.
 get(#cb_context{}=Context, _) ->
+    Context.
+get(#cb_context{}=Context, _, _) ->
     Context.
 
 %%--------------------------------------------------------------------
