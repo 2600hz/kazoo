919cfc3e2f8ab049b8f96fd9b345c80cc471f639
karl anderson <karl@2600hz.com>
2013-10-10 18:40:40 -0400

    KAZOO-1535: remove delay and fix source file

--- applications/crossbar/src/crossbar_default_handler.erl
+++ applications/crossbar/src/crossbar_default_handler.erl
@@ -24,30 +24,12 @@ init({_Any, 'http'}, Req, []) ->
 
 -spec handle(cowboy_req:req(), State) -> {'ok', cowboy_req:req(), State}.
 handle(Req, State) ->
-    Headers = [{'Content-Type', "text/plain; charset=UTF-8"}],
-    try cowboy_req:chunked_reply(200, Headers, Req) of
-        {'ok', Req1} ->
-            Path = code:priv_dir('crossbar') ++ "/kazoo.txt",
-            {'ok', Fd} = file:open(Path, ['read']),
-            _ = readme_lines(Fd, Req1),
-            {'ok', Req1, State}
-    catch
-        _C:_E ->
-            ST = erlang:get_stacktrace(),
-            lager:debug("failed to load resource: ~s: ~p", [_C, _E]),
-            wh_util:log_stacktrace(ST)
-    end.
+    Headers = [{<<"Content-Type">>, <<"text/plain; charset=UTF-8">>}],
+    Path = code:priv_dir('crossbar') ++ "/kazoo.txt",
+    {'ok', Bytes} = file:read_file(Path),
+    io:format("~p~n", [Bytes]),
+    {'ok', Req1} = cowboy_req:reply(200, Headers, Bytes, Req),
+    {'ok', Req1, State}.
 
 -spec terminate(term(), cowboy_req:req(), term()) -> 'ok'.
 terminate(_Reason, _Req, _State) -> 'ok'.
-
--spec readme_lines(file:fd(), cowboy_req:req()) -> 'ok' | {'error', _}.
-readme_lines(Fd, Req) ->
-    case file:read_line(Fd) of
-        {'ok', Data} ->
-            _ = cowboy_req:chunk(Data, Req),
-            timer:sleep(crypto:rand_uniform(15,250)),
-            readme_lines(Fd, Req);
-        _Else ->
-            file:close(Fd)
-    end.
