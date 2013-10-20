b24678d4c94e811ab4c76960d50428d35cd58601
James Aimonetti <james@2600hz.org>
2013-10-20 14:39:03 -0700

    KAZOO-243: more specs

--- applications/ecallmgr/src/ecallmgr_channel_redirect.erl
+++ applications/ecallmgr/src/ecallmgr_channel_redirect.erl
@@ -20,6 +20,7 @@ redirect(UUID, DestinationNode) ->
         'false' -> redirect_via_endpoint(DestNodeURL, UUID)
     end.
 
+-spec redirect_via_proxy(ne_binary(), ne_binary()) -> ecallmgr_util:send_cmd_ret().
 redirect_via_proxy(DestNodeURL, UUID) ->
     {'ok', #channel{destination=ToUser
                     ,realm=Realm
@@ -28,11 +29,14 @@ redirect_via_proxy(DestNodeURL, UUID) ->
     RedirectURL = binary:replace(DestNodeURL, <<"mod_sofia@">>, <<>>),
     send_redirect(RedirectURL, Contact, Channel).
 
+-spec redirect_via_endpoint(ne_binary(), ne_binary()) -> ecallmgr_util:send_cmd_ret().
 redirect_via_endpoint(DestNodeURL, UUID) ->
     {'ok', #channel{destination=ToUser}=Channel} = ecallmgr_fs_channel:fetch(UUID, 'record'),
     Contact = binary:replace(DestNodeURL, <<"mod_sofia">>, ToUser),
     send_redirect('undefined', Contact, Channel).
 
+-spec send_redirect(api_binary(), ne_binary(), channel()) ->
+                           ecallmgr_util:send_cmd_ret().
 send_redirect('undefined', Contact, #channel{node=Node
                                              ,uuid=UUID
                                              ,answered=IsAnswered
@@ -45,6 +49,7 @@ send_redirect(RedirectUrl, Contact, #channel{node=Node
     ecallmgr_util:set(Node, UUID, [{<<"sip_rh_X-Redirect-Server">>, RedirectUrl}]),
     ecallmgr_util:send_cmd(Node, UUID, redirect_app(IsAnswered), Contact).
 
+-spec redirect_app(boolean()) -> ne_binary().
 redirect_app('true') ->
     <<"deflect">>;
 redirect_app('false') ->
