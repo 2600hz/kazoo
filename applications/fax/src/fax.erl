ad0998257166fa62f242d5c9cd9e2e43a21f4a55
karl anderson <karl@2600hz.com>
2013-10-23 19:39:00 -0400

    KAZOO-1574: cowboy requires ranch, if crossbar was not started prior to fax then the fax whapps would fail

--- applications/fax/src/fax.erl
+++ applications/fax/src/fax.erl
@@ -73,6 +73,7 @@ start_deps() ->
                                                 ,'lager'
                                                 ,'whistle_amqp'
                                                 ,'whistle_couch'
+                                                ,'ranch'
                                                 ,'cowboy'
                                                ]],
     'ok'.
