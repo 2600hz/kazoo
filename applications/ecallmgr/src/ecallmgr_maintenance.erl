a75ef73a86c45cf72d4f13d343a2e92c4c4b5ac5
James Aimonetti <james@2600hz.org>
2013-10-24 15:35:19 -0700

    KAZOO-1213: add {add,remove}_fs_node/2 to set whether to update default list of node's list
    {add,remove}_fs_node(FSNode, AsDefault) where AsDefault is true/false, and updates the default list if true.

--- applications/ecallmgr/src/ecallmgr_maintenance.erl
+++ applications/ecallmgr/src/ecallmgr_maintenance.erl
@@ -8,9 +8,10 @@
 %%%-------------------------------------------------------------------
 -module(ecallmgr_maintenance).
 
--export([add_fs_node/1]).
--export([list_fs_nodes/0]).
--export([remove_fs_node/1]).
+-export([add_fs_node/1, add_fs_node/2
+         ,remove_fs_node/1, remove_fs_node/2
+         ,list_fs_nodes/0
+        ]).
 -export([node_summary/0]).
 -export([node_details/0
          ,node_details/1
@@ -56,34 +57,49 @@
 -include("ecallmgr.hrl").
 
 -spec add_fs_node(string() | binary() | atom()) -> 'ok'.
-add_fs_node(Node) when not is_atom(Node) ->
-    add_fs_node(wh_util:to_atom(Node, 'true'));
-add_fs_node(Node) ->
-    Nodes = ecallmgr_config:get(<<"fs_nodes">>, []),
-    NodeBin = wh_util:to_binary(Node),
-    case lists:member(NodeBin, Nodes) of
+-spec add_fs_node(string() | binary() | atom(), text() | boolean()) -> 'ok'.
+add_fs_node(FSNode) ->
+    add_fs_node(FSNode, 'false').
+
+add_fs_node(FSNode, AsDefault) when not is_atom(FSNode) ->
+    add_fs_node(wh_util:to_atom(FSNode, 'true'), AsDefault);
+add_fs_node(FSNode, AsDefault) ->
+    Node = use_default(AsDefault),
+    FSNodes = ecallmgr_config:get(<<"fs_nodes">>, [], Node),
+    FSNodeBin = wh_util:to_binary(FSNode),
+    case lists:member(FSNodeBin, FSNodes) of
         'true' -> 'ok';
         'false' ->
-            io:format("adding ~s to ecallmgr.~s.fs_nodes~n", [Node, node()]),
-            ecallmgr_config:set(<<"fs_nodes">>, [NodeBin | Nodes])
+            io:format("adding ~s to ecallmgr.~s.fs_nodes~n", [FSNode, Node]),
+            ecallmgr_config:set(<<"fs_nodes">>, [FSNodeBin | FSNodes], Node)
     end,
-    ecallmgr_fs_nodes:add(Node).
+    ecallmgr_fs_nodes:add(FSNode).
+
+-spec use_default(boolean() | ne_binary()) -> ne_binary().
+use_default('true') -> <<"default">>;
+use_default('false') -> wh_util:to_binary(node());
+use_default(AsDefault) -> use_default(wh_util:is_true(AsDefault)).
 
 -spec remove_fs_node(string() | binary() | atom()) -> 'ok'.
-remove_fs_node(Node) when not is_atom(Node) ->
-    remove_fs_node(wh_util:to_atom(Node, 'true'));
-remove_fs_node(Node) ->
-    Nodes = ecallmgr_config:get(<<"fs_nodes">>, []),
-    NodeBin = wh_util:to_binary(Node),
-    case lists:member(NodeBin, Nodes) of
+-spec remove_fs_node(string() | binary() | atom(), boolean() | text()) -> 'ok'.
+remove_fs_node(FSNode) ->
+    remove_fs_node(FSNode, 'false').
+
+remove_fs_node(FSNode, AsDefault) when not is_atom(FSNode) ->
+    remove_fs_node(wh_util:to_atom(FSNode, 'true'), AsDefault);
+remove_fs_node(FSNode, AsDefault) ->
+    Node = use_default(AsDefault),
+    FSNodes = ecallmgr_config:get(<<"fs_nodes">>, [], Node),
+    FSNodeBin = wh_util:to_binary(FSNode),
+    case lists:member(FSNodeBin, FSNodes) of
         'false' -> 'ok';
         'true' ->
-            io:format("removing ~s from ecallmgr.~s.fs_nodes~n", [Node, node()]),
-            ecallmgr_config:set(<<"fs_nodes">>, lists:delete(NodeBin, Nodes))
+            io:format("removing ~s from ecallmgr.~s.fs_nodes~n", [FSNode, Node]),
+            ecallmgr_config:set(<<"fs_nodes">>, lists:delete(FSNodeBin, FSNodes), Node)
     end,
-    ecallmgr_fs_nodes:remove(Node).
+    ecallmgr_fs_nodes:remove(FSNode).
 
--spec list_fs_nodes() -> [atom(),...] | [].
+-spec list_fs_nodes() -> atoms().
 list_fs_nodes() -> ecallmgr_fs_nodes:connected().
 
 -spec node_summary() -> 'no_return'.
