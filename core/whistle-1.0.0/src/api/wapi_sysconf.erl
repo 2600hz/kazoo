bb5da3c1bf54295c23dee08fb72447f644a9ee42
James Aimonetti <james@2600hz.org>
2013-10-24 15:36:19 -0700

    KAZOO-1213: remove Node from sysconf APIs, since its a default header

--- core/whistle-1.0.0/src/api/wapi_sysconf.erl
+++ core/whistle-1.0.0/src/api/wapi_sysconf.erl
@@ -3,7 +3,7 @@
 %%% @doc
 %%% Expose system configuration data.
 %%% System configuration data is stored as key/values in a namespace
-%%% (a doc) in system_config DB. 
+%%% (a doc) in system_config DB.
 %%% @end
 %%% @contributors
 %%%   Edouard Swiac
@@ -35,28 +35,28 @@
 -define(KEY_SYSCONF_FLUSH_REQ, <<"sysconf.flush">>).
 
 %% Configuration Document Update
-%% request to read 
--define(SYSCONF_GET_REQ_HEADERS, [<<"Category">>, <<"Key">>, <<"Node">>]).
+%% request to read
+-define(SYSCONF_GET_REQ_HEADERS, [<<"Category">>, <<"Key">>]).
 -define(OPTIONAL_SYSCONF_GET_REQ_HEADERS, [<<"Default">>]).
 -define(SYSCONF_GET_REQ_VALUES, [{<<"Event-Name">>, <<"get_req">>} | ?SYSCONF_VALUES]).
 
 %% answer to a read request
 -define(SYSCONF_GET_RESP_HEADERS, [<<"Category">>, <<"Key">>, <<"Value">>]).
--define(OPTIONAL_SYSCONF_GET_RESP_HEADERS, [<<"Node">>]).
+-define(OPTIONAL_SYSCONF_GET_RESP_HEADERS, []).
 -define(SYSCONF_GET_RESP_VALUES, [{<<"Event-Name">>, <<"get_resp">>} | ?SYSCONF_VALUES]).
 
 %% request a write
--define(SYSCONF_SET_REQ_HEADERS, [<<"Category">>, <<"Key">>, <<"Value">>, <<"Node">>]).
+-define(SYSCONF_SET_REQ_HEADERS, [<<"Category">>, <<"Key">>, <<"Value">>]).
 -define(OPTIONAL_SYSCONF_SET_REQ_HEADERS, []).
 -define(SYSCONF_SET_REQ_VALUES, [{<<"Event-Name">>, <<"set_req">>} | ?SYSCONF_VALUES]).
 
--define(SYSCONF_FLUSH_REQ_HEADERS, [<<"Category">>, <<"Key">>, <<"Node">>]).
+-define(SYSCONF_FLUSH_REQ_HEADERS, [<<"Category">>, <<"Key">>]).
 -define(OPTIONAL_SYSCONF_FLUSH_REQ_HEADERS, []).
 -define(SYSCONF_FLUSH_REQ_VALUES, [{<<"Event-Name">>, <<"flush_req">>} | ?SYSCONF_VALUES]).
 
 %% answer to a write request
 -define(SYSCONF_SET_RESP_HEADERS, [<<"Category">>, <<"Key">>, <<"Value">>]).
--define(OPTIONAL_SYSCONF_SET_RESP_HEADERS, [<<"Node">>, <<"Status">>]).
+-define(OPTIONAL_SYSCONF_SET_RESP_HEADERS, [<<"Status">>]).
 -define(SYSCONF_SET_RESP_VALUES, [{<<"Event-Name">>, <<"set_resp">>} | ?SYSCONF_VALUES]).
 
 -define(SYSCONF_TYPES, [{<<"Category">>, fun is_binary/1}
@@ -69,7 +69,7 @@
 %% READ
 %% @end
 %%--------------------------------------------------------------------
--spec get_req/1 :: (api_terms()) -> {'ok', iolist()} | {'error', string()}.
+-spec get_req(api_terms()) -> {'ok', iolist()} | {'error', string()}.
 get_req(Prop) when is_list(Prop) ->
     case get_req_v(Prop) of
         true -> wh_api:build_message(Prop, ?SYSCONF_GET_REQ_HEADERS, ?OPTIONAL_SYSCONF_GET_REQ_HEADERS);
@@ -78,13 +78,13 @@ get_req(Prop) when is_list(Prop) ->
 get_req(JObj) ->
     get_req(wh_json:to_proplist(JObj)).
 
--spec get_req_v/1 :: (api_terms()) -> boolean().
+-spec get_req_v(api_terms()) -> boolean().
 get_req_v(Prop) when is_list(Prop) ->
     wh_api:validate(Prop, ?SYSCONF_GET_REQ_HEADERS, ?SYSCONF_GET_REQ_VALUES, ?SYSCONF_TYPES);
 get_req_v(JObj) ->
     get_req_v(wh_json:to_proplist(JObj)).
 
--spec get_resp/1 :: (api_terms()) -> {'ok', iolist()} | {'error', string()}.
+-spec get_resp(api_terms()) -> {'ok', iolist()} | {'error', string()}.
 get_resp(Prop) when is_list(Prop) ->
     case get_resp_v(Prop) of
         true -> wh_api:build_message(Prop, ?SYSCONF_GET_RESP_HEADERS, ?OPTIONAL_SYSCONF_GET_RESP_HEADERS);
@@ -93,7 +93,7 @@ get_resp(Prop) when is_list(Prop) ->
 get_resp(JObj) ->
     get_resp(wh_json:to_proplist(JObj)).
 
--spec get_resp_v/1 :: (api_terms()) -> boolean().
+-spec get_resp_v(api_terms()) -> boolean().
 get_resp_v(Prop) when is_list(Prop) ->
     wh_api:validate(Prop, ?SYSCONF_GET_RESP_HEADERS, ?SYSCONF_GET_RESP_VALUES, ?SYSCONF_TYPES);
 get_resp_v(JObj) ->
@@ -104,31 +104,31 @@ get_resp_v(JObj) ->
 %% WRITE
 %% @end
 %%--------------------------------------------------------------------
--spec set_req/1 :: (api_terms()) -> {'ok', iolist()} | {'error', string()}.
+-spec set_req(api_terms()) -> {'ok', iolist()} | {'error', string()}.
 set_req(Prop) when is_list(Prop) ->
     case set_req_v(Prop) of
-        true -> wh_api:build_message(Prop, ?SYSCONF_SET_REQ_HEADERS, ?OPTIONAL_SYSCONF_SET_REQ_HEADERS);
-        false -> {error, "Proplist failed validation for sysconf write"}
+        'true' -> wh_api:build_message(Prop, ?SYSCONF_SET_REQ_HEADERS, ?OPTIONAL_SYSCONF_SET_REQ_HEADERS);
+        'false' -> {'error', "Proplist failed validation for sysconf write"}
     end;
 set_req(JObj) ->
     set_req(wh_json:to_proplist(JObj)).
 
--spec set_req_v/1 :: (api_terms()) -> boolean().
+-spec set_req_v(api_terms()) -> boolean().
 set_req_v(Prop) when is_list(Prop) ->
     wh_api:validate(Prop, ?SYSCONF_SET_REQ_HEADERS, ?SYSCONF_SET_REQ_VALUES, ?SYSCONF_TYPES);
 set_req_v(JObj) ->
     set_req_v(wh_json:to_proplist(JObj)).
 
--spec set_resp/1 :: (api_terms()) -> {'ok', iolist()} | {'error', string()}.
+-spec set_resp(api_terms()) -> {'ok', iolist()} | {'error', string()}.
 set_resp(Prop) when is_list(Prop) ->
     case set_resp_v(Prop) of
-        true -> wh_api:build_message(Prop, ?SYSCONF_SET_RESP_HEADERS, ?OPTIONAL_SYSCONF_SET_RESP_HEADERS);
-        false -> {error, "Proplist failed validation for sysconf write"}
+        'true' -> wh_api:build_message(Prop, ?SYSCONF_SET_RESP_HEADERS, ?OPTIONAL_SYSCONF_SET_RESP_HEADERS);
+        'false' -> {'error', "Proplist failed validation for sysconf write"}
     end;
 set_resp(JObj) ->
     set_resp(wh_json:to_proplist(JObj)).
 
--spec set_resp_v/1 :: (api_terms()) -> boolean().
+-spec set_resp_v(api_terms()) -> boolean().
 set_resp_v(Prop) when is_list(Prop) ->
     wh_api:validate(Prop, ?SYSCONF_SET_RESP_HEADERS, ?SYSCONF_SET_RESP_VALUES, ?SYSCONF_TYPES);
 set_resp_v(JObj) ->
@@ -139,56 +139,56 @@ set_resp_v(JObj) ->
 %% Flush a given key
 %% @end
 %%--------------------------------------------------------------------
--spec flush_req/1 :: (api_terms()) -> {'ok', iolist()} | {'error', string()}.
+-spec flush_req(api_terms()) -> {'ok', iolist()} | {'error', string()}.
 flush_req(Prop) when is_list(Prop) ->
     case flush_req_v(Prop) of
-        true -> wh_api:build_message(Prop, ?SYSCONF_FLUSH_REQ_HEADERS, ?OPTIONAL_SYSCONF_FLUSH_REQ_HEADERS);
-        false -> {error, "Proplist failed validation for sysconf read"}
+        'true' -> wh_api:build_message(Prop, ?SYSCONF_FLUSH_REQ_HEADERS, ?OPTIONAL_SYSCONF_FLUSH_REQ_HEADERS);
+        'false' -> {'error', "Proplist failed validation for sysconf read"}
     end;
 flush_req(JObj) ->
     flush_req(wh_json:to_proplist(JObj)).
 
--spec flush_req_v/1 :: (api_terms()) -> boolean().
+-spec flush_req_v(api_terms()) -> boolean().
 flush_req_v(Prop) when is_list(Prop) ->
     wh_api:validate(Prop, ?SYSCONF_FLUSH_REQ_HEADERS, ?SYSCONF_FLUSH_REQ_VALUES, ?SYSCONF_TYPES);
 flush_req_v(JObj) ->
     flush_req_v(wh_json:to_proplist(JObj)).
 
--spec bind_q/2 :: (ne_binary(), proplist()) -> 'ok'.
+-spec bind_q(ne_binary(), proplist()) -> 'ok'.
 bind_q(Q, Prop) ->
-    add_bindings(Q, props:get_value(restrict_to, Prop)).
+    add_bindings(Q, props:get_value('restrict_to', Prop)).
 
-add_bindings(Q, undefined) ->
+add_bindings(Q, 'undefined') ->
     _ = amqp_util:bind_q_to_sysconf(Q, routing_key_get()),
     amqp_util:bind_q_to_sysconf(Q, routing_key_set());
-add_bindings(Q, [get|T]) ->
+add_bindings(Q, ['get'|T]) ->
     _ = amqp_util:bind_q_to_sysconf(Q, routing_key_get()),
     add_bindings(Q, T);
-add_bindings(Q, [set|T]) ->
+add_bindings(Q, ['set'|T]) ->
     _ = amqp_util:bind_q_to_sysconf(Q, routing_key_set()),
     add_bindings(Q, T);
 add_bindings(Q, [_|T]) ->
     add_bindings(Q, T);
 add_bindings(_, []) ->
-    ok.
+    'ok'.
 
--spec unbind_q/2 :: (ne_binary(), proplist()) -> 'ok'.
+-spec unbind_q(ne_binary(), proplist()) -> 'ok'.
 unbind_q(Q, Prop) ->
-    rm_bindings(Q, props:get_value(restrict_to, Prop)).
+    rm_bindings(Q, props:get_value('restrict_to', Prop)).
 
-rm_bindings(Q, undefined) ->
+rm_bindings(Q, 'undefined') ->
     _ = amqp_util:unbind_q_from_sysconf(Q, routing_key_get()),
     amqp_util:unbind_q_from_sysconf(Q, routing_key_set());
-rm_bindings(Q, [get|T]) ->
+rm_bindings(Q, ['get'|T]) ->
     _ = amqp_util:unbind_q_from_sysconf(Q, routing_key_get()),
     rm_bindings(Q, T);
-rm_bindings(Q, [set|T]) ->
+rm_bindings(Q, ['set'|T]) ->
     _ = amqp_util:unbind_q_from_sysconf(Q, routing_key_set()),
     rm_bindings(Q, T);
 rm_bindings(Q, [_|T]) ->
     rm_bindings(Q, T);
 rm_bindings(_, []) ->
-    ok.
+    'ok'.
 
 %%--------------------------------------------------------------------
 %% @doc
@@ -198,45 +198,45 @@ rm_bindings(_, []) ->
 -spec declare_exchanges() -> 'ok'.
 declare_exchanges() ->
     amqp_util:sysconf_exchange().
-    
--spec publish_get_req/1 :: (api_terms()) -> 'ok'.
--spec publish_get_req/2 :: (api_terms(), ne_binary()) -> 'ok'.
+
+-spec publish_get_req(api_terms()) -> 'ok'.
+-spec publish_get_req(api_terms(), ne_binary()) -> 'ok'.
 publish_get_req(JObj) ->
     publish_get_req(JObj, ?DEFAULT_CONTENT_TYPE).
 publish_get_req(Api, ContentType) ->
-    {ok, Payload} = wh_api:prepare_api_payload(Api, ?SYSCONF_GET_REQ_VALUES, fun ?MODULE:get_req/1),
+    {'ok', Payload} = wh_api:prepare_api_payload(Api, ?SYSCONF_GET_REQ_VALUES, fun ?MODULE:get_req/1),
     amqp_util:sysconf_publish(routing_key_get(), Payload, ContentType).
 
--spec publish_get_resp/2 :: (ne_binary(), api_terms()) -> 'ok'.
--spec publish_get_resp/3 :: (ne_binary(), api_terms(), ne_binary()) -> 'ok'.
+-spec publish_get_resp(ne_binary(), api_terms()) -> 'ok'.
+-spec publish_get_resp(ne_binary(), api_terms(), ne_binary()) -> 'ok'.
 publish_get_resp(RespQ, JObj) ->
     publish_get_resp(RespQ, JObj, ?DEFAULT_CONTENT_TYPE).
 publish_get_resp(RespQ, Api, ContentType) ->
-    {ok, Payload} = wh_api:prepare_api_payload(Api, ?SYSCONF_GET_RESP_VALUES, fun ?MODULE:get_resp/1),
+    {'ok', Payload} = wh_api:prepare_api_payload(Api, ?SYSCONF_GET_RESP_VALUES, fun ?MODULE:get_resp/1),
     amqp_util:targeted_publish(RespQ, Payload, ContentType).
 
--spec publish_set_req/1 :: (api_terms()) -> 'ok'.
--spec publish_set_req/2 :: (api_terms(), ne_binary()) -> 'ok'.
+-spec publish_set_req(api_terms()) -> 'ok'.
+-spec publish_set_req(api_terms(), ne_binary()) -> 'ok'.
 publish_set_req(JObj) ->
     publish_set_req(JObj, ?DEFAULT_CONTENT_TYPE).
 publish_set_req(Api, ContentType) ->
-    {ok, Payload} = wh_api:prepare_api_payload(Api, ?SYSCONF_SET_REQ_VALUES, fun ?MODULE:set_req/1),
+    {'ok', Payload} = wh_api:prepare_api_payload(Api, ?SYSCONF_SET_REQ_VALUES, fun ?MODULE:set_req/1),
     amqp_util:sysconf_publish(routing_key_set(), Payload, ContentType).
 
--spec publish_set_resp/2 :: (ne_binary(), api_terms()) -> 'ok'.
--spec publish_set_resp/3 :: (ne_binary(), api_terms(), ne_binary()) -> 'ok'.
+-spec publish_set_resp(ne_binary(), api_terms()) -> 'ok'.
+-spec publish_set_resp(ne_binary(), api_terms(), ne_binary()) -> 'ok'.
 publish_set_resp(RespQ, JObj) ->
     publish_set_resp(RespQ, JObj, ?DEFAULT_CONTENT_TYPE).
 publish_set_resp(RespQ, Api, ContentType) ->
-    {ok, Payload} = wh_api:prepare_api_payload(Api, ?SYSCONF_SET_RESP_VALUES, fun ?MODULE:set_resp/1),
+    {'ok', Payload} = wh_api:prepare_api_payload(Api, ?SYSCONF_SET_RESP_VALUES, fun ?MODULE:set_resp/1),
     amqp_util:targeted_publish(RespQ, Payload, ContentType).
 
--spec publish_flush_req/1 :: (api_terms()) -> 'ok'.
--spec publish_flush_req/2 :: (api_terms(), ne_binary()) -> 'ok'.
+-spec publish_flush_req(api_terms()) -> 'ok'.
+-spec publish_flush_req(api_terms(), ne_binary()) -> 'ok'.
 publish_flush_req(JObj) ->
     publish_flush_req(JObj, ?DEFAULT_CONTENT_TYPE).
 publish_flush_req(Api, ContentType) ->
-    {ok, Payload} = wh_api:prepare_api_payload(Api, ?SYSCONF_FLUSH_REQ_VALUES, fun ?MODULE:flush_req/1),
+    {'ok', Payload} = wh_api:prepare_api_payload(Api, ?SYSCONF_FLUSH_REQ_VALUES, fun ?MODULE:flush_req/1),
     amqp_util:sysconf_publish(routing_key_flush(), Payload, ContentType).
 
 
