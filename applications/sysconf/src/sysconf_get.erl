1944fbc21c5d5fbb549986f53304a6443c09f755
karl anderson <karl@2600hz.com>
2013-10-30 23:25:53 -0400

    KAZOO-1433: ignore undefined values

--- applications/sysconf/src/sysconf_get.erl
+++ applications/sysconf/src/sysconf_get.erl
@@ -26,13 +26,19 @@ handle_req(ApiJObj, _Props) ->
     lager:debug("received sysconf get for ~s:~s from ~s", [Category, Key, Node]),
     MsgID = wh_json:get_value(<<"Msg-ID">>, ApiJObj),
     RespQ =  wh_json:get_value(<<"Server-ID">>, ApiJObj),
-    Resp = [{<<"Category">>, Category}
-            ,{<<"Key">>, Key}
-            ,{<<"Value">>, get_value(Category, Key, Default, Node)}
-            ,{<<"Msg-ID">>, MsgID}
-            | wh_api:default_headers(?APP_NAME, ?APP_VERSION)
-           ],
-    wapi_sysconf:publish_get_resp(RespQ, Resp).
+    case get_value(Category, Key, Default, Node) of
+        'undefined' -> 'ok';
+        Value ->
+            lager:debug("sending reply for ~s.~s(~s): ~p"
+                        ,[Category, Key, Node, Value]),
+            Resp = [{<<"Category">>, Category}
+                    ,{<<"Key">>, Key}
+                    ,{<<"Value">>, Value}
+                    ,{<<"Msg-ID">>, MsgID}
+                    | wh_api:default_headers(?APP_NAME, ?APP_VERSION)
+                   ],
+            wapi_sysconf:publish_get_resp(RespQ, Resp)
+    end.
 
 -spec get_value(ne_binary(), ne_binary(), term(), ne_binary()) -> term().
 get_value(_, <<"acls">>, _, Node) ->
