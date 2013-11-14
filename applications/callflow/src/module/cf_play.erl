d04e3abf8c43cd97ead6b4d3e8d75880c501fd19
Ben Wann <ben.wann@gmail.com>
2013-11-14 09:45:56 -0600

    Fix: cf_play can handle a silence_stream now as a valid media stream

--- applications/callflow/src/module/cf_play.erl
+++ applications/callflow/src/module/cf_play.erl
@@ -26,6 +26,7 @@ handle(Data, Call) ->
         <<"/system_media", _/binary>> = Path -> play(Data, Call, Path);
         <<"system_media", _/binary>> = Path -> play(Data, Call, Path);
         <<"local_stream://",_/binary>> = Path -> play(Data, Call, Path);
+        <<"silence_stream://",_/binary>> = Path -> play(Data, Call, Path);
         Path when AccountId =/= 'undefined' ->
             lager:info("prepending media ID with /~s/", [AccountId]),
             Path1 = <<$/, (wh_util:to_binary(AccountId))/binary, $/, Path/binary>>,
