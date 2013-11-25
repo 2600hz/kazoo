6fce18150ede810931ee1ecc16612723e464646f
James Aimonetti <james@2600hz.org>
2013-11-25 11:37:08 -0800

    KAZOO-1631: process sub actions serially, and kill the sub process on first DTMF recvd

--- core/kazoo_translator-1.0.0/src/convertors/kzt_twiml.erl
+++ core/kazoo_translator-1.0.0/src/convertors/kzt_twiml.erl
@@ -269,9 +269,19 @@ redirect(Call, XmlText, Attrs) ->
               ],
     {'request', lists:foldl(fun({F, V}, C) -> F(V, C) end, Call1, Setters)}.
 
-exec_gather_els(_Parent, Call, SubActions) ->
-    lager:debug("subacts: ~p", [SubActions]),
-    exec_elements(Call, SubActions).
+exec_gather_els(_Parent, _Call, []) ->
+    lager:debug("finished gather sub elements");
+exec_gather_els(Parent, Call, [SubAction|SubActions]) ->
+    whapps_call:put_callid(Call),
+    lager:debug("subact: ~p", [SubAction]),
+
+    case exec_element(Call, SubAction) of
+        {'stop', _} -> lager:debug("sub els stopping");
+        {'error', _} -> lager:debug("sub els erroring");
+        {'ok', Call1} ->
+            lager:debug("exec done"),
+            exec_gather_els(Parent, Call1, SubActions)
+    end.
 
 exec_gather_els(Call, SubActions) ->
     {_Pid, _Ref}=PidRef =
@@ -298,12 +308,22 @@ gather(Call, Attrs) ->
     gather(Call, FinishKey, Timeout, Props, num_digits(Props)).
 
 gather(Call, FinishKey, Timeout, Props, N) ->
-    case kzt_receiver:collect_dtmfs(Call, FinishKey, Timeout, N) of
+    case kzt_receiver:collect_dtmfs(Call, FinishKey, Timeout, N, fun on_first_dtmf/1) of
         {'ok', 'timeout', C} -> gather_finished(C, Props);
         {'ok', 'dtmf_finish', C} -> gather_finished(C, Props);
         {'ok', C} -> gather_finished(C, Props);
         {'error', _E, _C}=ERR -> ERR;
         {'stop', _C}=STOP -> STOP
+    end.
+
+-spec on_first_dtmf(whapps_call:call()) -> 'ok' | 'stop'.
+on_first_dtmf(Call) ->
+    case kzt_util:get_gather_pidref(Call) of
+        'undefined' -> 'ok';
+        {Pid, Ref} ->
+            erlang:demonitor(Ref, ['flush']),
+            lager:debug("first dtmf recv, stopping ~p(~p)", [Pid, Ref]),
+            exit(Pid, 'kill')
     end.
 
 gather_finished(Call, Props) ->
