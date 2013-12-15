b6945ec99c221a1b1879eee3b3f159bc3b858732
James Aimonetti <james@2600hz.org>
2013-12-14 18:23:21 -0800

    KAZOO-1003: load up the ETS table, and handle config changes in webhooks

--- applications/webhooks/src/webhooks_listener.erl
+++ applications/webhooks/src/webhooks_listener.erl
@@ -9,7 +9,10 @@
 
 -behaviour(gen_listener).
 
--export([start_link/0]).
+-export([start_link/0
+         ,handle_config/2
+         ,handle_call_event/2
+        ]).
 -export([init/1
          ,handle_call/3
          ,handle_cast/2
@@ -33,11 +36,35 @@
 
 -record(state, {}).
 
-%% By convention, we put the options here in macros, but not required.
--define(BINDINGS, [{'call', []}
-                   ,{'self', []}
+-type http_verb() :: 'get' | 'post'.
+
+-record(webhook, {
+          id :: ne_binary()
+          ,uri :: ne_binary()
+          ,http_verb :: http_verb()
+          ,hook_event :: ne_binary()
+          ,retries = 3 :: 1..5
+          ,account_id :: ne_binary()
+         }).
+
+%% Three main call events
+-define(BINDINGS, [{'call', [{'restrict_to', ['new_channel'
+                                              ,'answered_channel'
+                                              ,'destroy_channel'
+                                             ]}]}
+                   ,{'conf', [{'action', <<"*">>}
+                              ,{'db', ?KZ_WEBHOOKS_DB}
+                              ,{'type', <<"webhook">>}
+                              ,{'id', <<"*">>}
+                             ]}
                   ]).
--define(RESPONDERS, []).
+-define(RESPONDERS, [{{?MODULE, 'handle_config'}
+                      ,[{<<"configuration">>, <<"*">>}]
+                     }
+                     ,{{?MODULE, 'handle_call_event'}
+                       ,[{<<"channel">>, <<"*">>}]
+                      }
+                    ]).
 -define(QUEUE_NAME, <<"webhooks_listener">>).
 -define(QUEUE_OPTIONS, [{'exclusive', 'false'}]).
 -define(CONSUME_OPTIONS, [{'exclusive', 'false'}]).
@@ -54,33 +81,92 @@
 %% @end
 %%--------------------------------------------------------------------
 start_link() ->
-    gen_listener:start_link(?MODULE, [
-                                      {'bindings', ?BINDINGS}
+    gen_listener:start_link(?MODULE, [{'bindings', ?BINDINGS}
                                       ,{'responders', ?RESPONDERS}
-                                      ,{'queue_name', ?QUEUE_NAME}       % optional to include
-                                      ,{'queue_options', ?QUEUE_OPTIONS} % optional to include
-                                      ,{'consume_options', ?CONSUME_OPTIONS} % optional to include
+                                      ,{'queue_name', ?QUEUE_NAME}
+                                      ,{'queue_options', ?QUEUE_OPTIONS}
+                                      ,{'consume_options', ?CONSUME_OPTIONS}
                                      ], []).
-
--record(hook, {id :: api_binary()
-               ,account_id :: ne_binary()
-               ,hook :: ne_binary()
-               ,uri :: ne_binary()
-               ,http_verb = 'get' :: 'get' | 'post'
-               ,retries = 3 :: pos_integer()
-              }).
 
 -spec table_id() -> ?MODULE.
 table_id() -> ?MODULE.
 
 -spec table_options() -> list().
-table_options() -> ['set', 'protected', {'keypos', #hook.id}].
+table_options() -> ['set', 'protected', {'keypos', #webhook.id}].
 
 -spec find_me_function() -> api_pid().
 find_me_function() -> whereis(?MODULE).
 
 -spec gift_data() -> 'ok'.
 gift_data() -> 'ok'.
+
+-spec handle_config(wh_json:object(), wh_proplist()) -> 'ok'.
+-spec handle_config(wh_json:object(), pid(), ne_binary()) -> 'ok'.
+handle_config(JObj, Props) ->
+    handle_config(JObj
+                  ,props:get_value('server', Props)
+                  ,wh_json:get_value(<<"Event-Type">>, JObj)
+                 ).
+handle_config(JObj, Srv, <<"doc_created">>) ->
+    case wapi_conf:get_doc(JObj) of
+        'undefined' -> find_and_add_hook(JObj, Srv);
+        Hook -> load_hook(Srv, Hook)
+    end;
+handle_config(JObj, Srv, <<"doc_edited">>) ->
+    case wapi_conf:get_doc(JObj) of
+        'undefined' -> find_and_update_hook(JObj, Srv);
+        Hook ->
+            gen_listener:cast(Srv, {'update_hook', jobj_to_rec(Hook)})
+    end;
+handle_config(JObj, Srv, <<"doc_deleted">>) ->
+    case wapi_conf:get_doc(JObj) of
+        'undefined' -> find_and_remove_hook(JObj, Srv);
+        Hook ->
+            gen_listener:cast(Srv, {'remove_hook', jobj_to_rec(Hook)})
+    end.
+
+-spec find_and_add_hook(wh_json:object(), pid()) -> 'ok'.
+find_and_add_hook(JObj, Srv) ->
+    case find_hook(JObj) of
+        {'ok', Hook} ->
+            gen_listener:cast(Srv, {'add_hook', jobj_to_rec(Hook)});
+        {'error', _E} ->
+            lager:debug("failed to add hook ~s: ~p", [hook_id(JObj), _E])
+    end.
+
+-spec find_and_update_hook(wh_json:object(), pid()) -> 'ok'.
+find_and_update_hook(JObj, Srv) ->
+    case find_hook(JObj) of
+        {'ok', Hook} ->
+            gen_listener:cast(Srv, {'update_hook', jobj_to_rec(Hook)});
+        {'error', _E} ->
+            lager:debug("failed to update hook ~s: ~p", [hook_id(JObj), _E])
+    end.
+
+-spec find_and_remove_hook(wh_json:object(), pid()) -> 'ok'.
+find_and_remove_hook(JObj, Srv) ->
+    case find_hook(JObj) of
+        {'ok', Hook} ->
+            gen_listener:cast(Srv, {'remove_hook', jobj_to_rec(Hook)});
+        {'error', _E} ->
+            lager:debug("failed to remove hook ~s: ~p", [hook_id(JObj), _E])
+    end.
+
+-spec find_hook(wh_json:object()) ->
+                       {'ok', wh_json:object()} |
+                       {'error', _}.
+find_hook(JObj) ->
+    couch_mgr:open_doc(wapi_conf:get_account_db(JObj)
+                       ,wapi_conf:get_id(JObj)
+                      ).
+
+-spec handle_call_event(wh_json:object(), wh_proplist()) -> 'ok'.
+handle_call_event(JObj, _Props) ->
+    HookEvent = wh_json:get_value(<<"Event-Name">>, JObj),
+    AccountId = wh_json:get_value([<<"Custom-Channel-Vars">>, <<"Account-ID">>]
+                                  ,JObj
+                                 ),
+    lager:debug("handle event ~s: ~s", [HookEvent, AccountId]).
 
 %%%===================================================================
 %%% gen_server callbacks
@@ -127,6 +213,18 @@ handle_call(_Request, _From, State) ->
 %%                                  {stop, Reason, State}
 %% @end
 %%--------------------------------------------------------------------
+handle_cast({'add_hook', #webhook{id=_Id}=Hook}, State) ->
+    lager:debug("adding hook ~s", [_Id]),
+    ets:insert_new(?MODULE, Hook),
+    {'noreply', State};
+handle_cast({'update_hook', #webhook{id=_Id}=Hook}, State) ->
+    lager:debug("updating hook ~s", [_Id]),
+    ets:insert(?MODULE, Hook),
+    {'noreply', State};
+handle_cast({'remove_hook', #webhook{id=Id}}, State) ->
+    lager:debug("removing hook ~s", [Id]),
+    ets:delete(?MODULE, Id),
+    {'noreply', State};
 handle_cast({'wh_amqp_channel', {'new_channel', _IsNew}}, State) ->
     {'noreply', State};
 handle_cast({'gen_listener', {'created_queue', _Q}}, State) ->
@@ -134,6 +232,7 @@ handle_cast({'gen_listener', {'created_queue', _Q}}, State) ->
 handle_cast({'gen_listener', {'is_consuming', _IsConsuming}}, State) ->
     {'noreply', State};
 handle_cast(_Msg, State) ->
+    lager:debug("unhandled cast: ~p", [_Msg]),
     {'noreply', State}.
 
 %%--------------------------------------------------------------------
@@ -151,6 +250,7 @@ handle_info({'ETS-TRANSFER', _TblId, _From, _Data}, State) ->
     spawn(?MODULE, 'load_hooks', [self()]),
     {'noreply', State};
 handle_info(_Info, State) ->
+    lager:debug("unhandled msg: ~p", [_Info]),
     {'noreply', State}.
 
 %%--------------------------------------------------------------------
@@ -192,7 +292,85 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+-spec load_hooks(pid()) -> 'ok'.
 load_hooks(Srv) ->
     wh_util:put_callid(?MODULE),
     lager:debug("loading hooks into memory"),
-    'ok'.
+    case couch_mgr:get_results(?KZ_WEBHOOKS_DB
+                               ,<<"webhooks/crossbar_listing">>
+                               ,['include_docs']
+                              )
+    of
+        {'ok', []} ->
+            lager:debug("no configured webhooks");
+        {'ok', WebHooks} ->
+            load_hooks(Srv, WebHooks);
+        {'error', _E} ->
+            lager:debug("failed to load webhooks: ~p", [_E])
+    end.
+
+-spec load_hooks(pid(), wh_json:objects()) -> 'ok'.
+load_hooks(Srv, WebHooks) ->
+    [load_hook(Srv, wh_json:get_value(<<"doc">>, Hook)) || Hook <- WebHooks],
+    lager:debug("sent hooks into server ~p", [Srv]).
+
+-spec load_hook(pid(), wh_json:object()) -> 'ok'.
+load_hook(Srv, WebHook) ->
+    try jobj_to_rec(WebHook) of
+        Hook -> gen_listener:cast(Srv, {'add_hook', Hook})
+    catch
+        'throw':{'bad_hook', HookEvent} ->
+            lager:debug("failed to load hook ~s.~s: bad_hook: ~s"
+                        ,[wh_json:get_value(<<"pvt_account_id">>, WebHook)
+                          ,wh_json:get_value(<<"_id">>, WebHook)
+                          ,HookEvent
+                         ])
+    end.
+
+-spec jobj_to_rec(wh_json:object()) -> #webhook{}.
+jobj_to_rec(Hook) ->
+    #webhook{id = hook_id(Hook)
+             ,uri = wh_json:get_value(<<"uri">>, Hook)
+             ,http_verb = http_verb(wh_json:get_value(<<"http_verb">>, Hook))
+             ,hook_event = hook_event(wh_json:get_value(<<"hook">>, Hook))
+             ,retries = retries(wh_json:get_integer(<<"retries">>, Hook, 3))
+             ,account_id = wh_json:get_value(<<"pvt_account_id">>, Hook)
+            }.
+
+-spec hook_id(wh_json:object()) -> ne_binary().
+-spec hook_id(ne_binary(), ne_binary()) -> ne_binary().
+hook_id(JObj) ->
+    hook_id(wh_json:get_first_defined([<<"pvt_account_id">>
+                                       ,<<"Account-ID">>
+                                      ], JObj)
+            ,wh_json:get_first_defined([<<"_id">>, <<"ID">>], JObj)
+           ).
+hook_id(AccountId, Id) ->
+    <<AccountId/binary, ".", Id/binary>>.
+
+-spec http_verb(atom() | binary()) -> http_verb().
+http_verb('get') -> 'get';
+http_verb('post') -> 'post';
+http_verb(Bin) when is_binary(Bin) ->
+    try wh_util:to_atom(wh_util:to_lower_binary(Bin)) of
+        Atom -> http_verb(Atom)
+    catch
+        'error':'badarg' -> 'get'
+    end;
+http_verb(_) -> 'get'.
+
+-spec hook_event(ne_binary()) -> ne_binary().
+-spec hook_event_lowered(ne_binary()) -> ne_binary().
+hook_event(Bin) -> hook_event_lowered(wh_util:to_lower_binary(Bin)).
+
+hook_event_lowered(<<"new_channel">>) -> <<"new_channel">>;
+hook_event_lowered(<<"answered_channel">>) -> <<"answered_channel">>;
+hook_event_lowered(<<"destroy_channel">>) -> <<"destroy_channel">>;
+hook_event_lowered(<<"all">>) -> <<"all">>;
+hook_event_lowered(Bin) -> throw({'bad_hook', Bin}).
+
+-spec retries(integer()) -> 1..5.
+retries(N) when N > 0, N < 5 -> N;
+retries(N) when N < 1 -> 1;
+retries(_) -> 5.
+
