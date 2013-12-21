b37887ae61152cdcce3afa16511ee71022601528
James Aimonetti <james@2600hz.org>
2013-12-20 16:01:08 -0800

    KAZOO-1802: create common HRL

---
+++ applications/hangups/src/hangups.hrl
@@ -0,0 +1,8 @@
+-ifndef(HANGUPS_HRL).
+
+-include_lib("whistle/include/wh_log.hrl").
+-include_lib("whistle/include/wh_types.hrl").
+-include_lib("whistle/include/wh_databases.hrl").
+
+-define(HANGUPS_HRL, 'true').
+-endif.
