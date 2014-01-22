e6ec1b0817be35d684d3d373a9de2f10616046df
James Aimonetti <james@2600hz.org>
2014-01-21 16:20:41 -0800

    KAZOO-658: flush call stat from queue if possible

--- applications/acdc/src/acdc_maintenance.erl
+++ applications/acdc/src/acdc_maintenance.erl
@@ -12,6 +12,7 @@
          ,current_statuses/1
          ,agent_presence_id/2
          ,migrate_to_acdc_db/0, migrate/0
+         ,flush_call_stat/1
         ]).
 
 -include("acdc.hrl").
@@ -23,10 +24,10 @@ current_statuses(AcctId) ->
     {'ok', Agents} = acdc_agent_util:most_recent_statuses(AcctId),
     case wh_json:get_values(Agents) of
         {[], []} ->
-            lager:debug("No agent statuses found for ~s", [AcctId]);
+            lager:info("No agent statuses found for ~s", [AcctId]);
         {As, _} ->
-            lager:debug("Agent Statuses for ~s", [AcctId]),
-            lager:debug("~4s | ~35s | ~12s | ~20s |", [<<>>, <<"Agent-ID">>, <<"Status">>, <<"Timestamp">>]),
+            lager:info("Agent Statuses for ~s", [AcctId]),
+            lager:info("~4s | ~35s | ~12s | ~20s |", [<<>>, <<"Agent-ID">>, <<"Status">>, <<"Timestamp">>]),
             log_current_statuses(As, 1)
     end,
     'ok'.
@@ -37,7 +38,7 @@ log_current_statuses([A|As], N) ->
     log_current_statuses(As, N+1).
 
 log_current_status(A, N) ->
-    lager:debug("~4b | ~35s | ~12s | ~20s |", [N, wh_json:get_value(<<"agent_id">>, A)
+    lager:info("~4b | ~35s | ~12s | ~20s |", [N, wh_json:get_value(<<"agent_id">>, A)
                                                ,wh_json:get_value(<<"status">>, A)
                                                ,wh_util:pretty_print_datetime(wh_json:get_integer_value(<<"timestamp">>, A))
                                               ]).
@@ -62,7 +63,11 @@ current_calls(AcctId, Props) ->
 
 get_and_show(AcctId, QueueId, Req) ->
     put('callid', <<"acdc_maint.", AcctId/binary, ".", QueueId/binary>>),
-    case whapps_util:amqp_pool_collect(Req, fun wapi_acdc_stats:publish_current_calls_req/1) of
+    case whapps_util:amqp_pool_collect(Req
+                                       ,fun wapi_acdc_stats:publish_current_calls_req/1
+                                       ,'acdc'
+                                      )
+    of
         {_, []} ->
             lager:info("no call stats returned for account ~s (queue ~s)", [AcctId, QueueId]);
         {'ok', JObjs} ->
@@ -85,7 +90,7 @@ show_call_stat_cat([K|Ks], Resp) ->
     case wh_json:get_value(K, Resp) of
         'undefined' -> show_call_stat_cat(Ks, Resp);
         V ->
-            lager:debug("call stats in ~s", [K]),
+            lager:info("call stats in ~s", [K]),
             show_stats(V),
             show_call_stat_cat(Ks, Resp)
     end.
@@ -106,7 +111,7 @@ migrate_to_acdc_db(AccountId) ->
     migrate_to_acdc_db(AccountId, 3).
 
 migrate_to_acdc_db(AccountId, 0) ->
-    lager:debug("retries exceeded, skipping account ~s", [AccountId]);
+    lager:info("retries exceeded, skipping account ~s", [AccountId]);
 migrate_to_acdc_db(AccountId, Retries) ->
     case couch_mgr:get_results(?KZ_ACDC_DB
                                ,<<"acdc/accounts_listing">>
@@ -116,14 +121,14 @@ migrate_to_acdc_db(AccountId, Retries) ->
         {'ok', []} ->
             maybe_migrate(AccountId);
         {'ok', [_|_]} ->
-            lager:debug("account ~s already in acdc db", [AccountId]);
+            lager:info("account ~s already in acdc db", [AccountId]);
         {'error', 'not_found'} ->
-            lager:debug("acdc db not found (or view is missing, restoring then trying again"),
+            lager:info("acdc db not found (or view is missing, restoring then trying again"),
             acdc_init:init_db(),
             timer:sleep(250),
             migrate_to_acdc_db(AccountId, Retries-1);
         {'error', _E} ->
-            lager:debug("failed to check acdc db for account: ~p", [_E]),
+            lager:info("failed to check acdc db for account: ~p", [_E]),
             timer:sleep(250),
             migrate_to_acdc_db(AccountId, Retries-1)
     end.
@@ -132,9 +137,9 @@ maybe_migrate(AccountId) ->
     AccountDb = wh_util:format_account_id(AccountId, 'encoded'),
     case couch_mgr:get_results(AccountDb, <<"queues/crossbar_listing">>, [{'limit', 1}]) of
         {'ok', []} ->
-            lager:debug("account ~s has no queues, skipping", [AccountId]);
+            lager:info("account ~s has no queues, skipping", [AccountId]);
         {'ok', [_|_]} ->
-            lager:debug("account ~s has queues, adding to acdc db", [AccountId]),
+            lager:info("account ~s has queues, adding to acdc db", [AccountId]),
             Doc = wh_doc:update_pvt_parameters(wh_json:new()
                                                ,?KZ_ACDC_DB
                                                ,[{'account_id', AccountId}
@@ -142,7 +147,7 @@ maybe_migrate(AccountId) ->
                                                 ]),
             couch_mgr:ensure_saved(?KZ_ACDC_DB, Doc);
         {'error', _E} ->
-            lager:debug("failed to query queue listing for account ~s: ~p", [AccountId, _E])
+            lager:info("failed to query queue listing for account ~s: ~p", [AccountId, _E])
     end.
 
 -spec agent_presence_id(ne_binary(), ne_binary()) -> 'ok'.
@@ -153,5 +158,17 @@ agent_presence_id(AccountId, AgentId) ->
         SupPid ->
             PresenceId = acdc_agent_listener:presence_id(acdc_agent_sup:listener(SupPid)),
             io:format("agent ~s(~s) is using presence ID ~s~n", [AgentId, AccountId, PresenceId])
+    end.
+
+flush_call_stat(CallId) ->
+    case acdc_stats:find_call(CallId) of
+        'undefined' -> io:format("nothing found for call ~s~n", [CallId]);
+        Call ->
+            whapps_util:amqp_pool_send(
+              wh_json:set_values(wh_api:default_headers(?APP_NAME, ?APP_VERSION), Call)
+              ,fun wapi_acdc_stats:publish_call_flush/1
+             ),
+            io:format("sending flush request for call ~s~n", [CallId]),
+            io:format("call data: ~s~n", [wh_json:encode(Call)])
     end.
 
