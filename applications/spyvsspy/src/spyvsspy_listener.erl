2e1be9f7a1b14318f7d45314eab3fc4d4a1b2d8d
bitbashing <bitbashing@gmail.com>
2014-01-30 14:33:33 -0800

    KAZOO-655: this is consumed by the gen_listener

--- applications/spyvsspy/src/spyvsspy_listener.erl
+++ applications/spyvsspy/src/spyvsspy_listener.erl
@@ -104,9 +104,7 @@ handle_call(_Request, _From, State) ->
 %%--------------------------------------------------------------------
 handle_cast({'gen_listener', {'created_queue', _QueueNAme}}, State) ->
     {'noreply', State};
-handle_cast({'gen_listener',{'is_consuming',_IsConsuming}}, State) ->
-    {'noreply', State};
-handle_cast({'wh_amqp_channel',{'new_channel',_IsNew}}, State) ->
+handle_cast({'gen_listener', {'is_consuming', _IsConsuming}}, State) ->
     {'noreply', State};
 handle_cast(_Msg, State) ->
     lager:debug("unhandled cast: ~p", [_Msg]),
