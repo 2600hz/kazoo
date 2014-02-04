14b0d8f8c995c4f505a598522884deb3dd1d323f
Alan Evans <alan@kagesys.com>
2014-02-04 23:59:13 +0000

    use Kazoo registrar to determine contact endpoint

--- applications/ecallmgr/src/ecallmgr_message.erl
+++ applications/ecallmgr/src/ecallmgr_message.erl
@@ -18,13 +18,20 @@ process_msg(Props, Node) ->
     From = props:get_value(<<"from">>, Props),
     To = props:get_value(<<"to">>, Props),
     Body = props:get_value(<<"body">>, Props),
-    Header = [
-           {"profile", ?DEFAULT_FS_PROFILE}
-          ,{"content-length", wh_util:to_list(size(Body))}
-          ,{"content-type", "text/plain"}
-          ,{"to", To}
-          ,{"from", From}
-          ,{"body", Body}
-         ],
-    Resp = freeswitch:sendevent(Node, 'SEND_MESSAGE', Header),
-    lager:debug("sent SIP/SIMPLE Msg to '~s' via ~s: ~p", [To, Node, Resp]).
+    [User, Realm] = binary:split(To, <<"@">>),
+    case  ecallmgr_registrar:lookup_contact(Realm,User) of
+        {ok, Contact} ->
+            Header = [
+               {"profile", ?DEFAULT_FS_PROFILE}
+              ,{"content-length", wh_util:to_list(size(Body))}
+              ,{"content-type", "text/plain"}
+              ,{"to", <<"sip:", To/binary>>}
+              ,{"from", <<"sip:", From/binary>>}
+              ,{"contact", Contact}
+              ,{"body", Body}
+            ],
+            Resp = freeswitch:sendevent(Node, 'SEND_MESSAGE', Header),
+            lager:debug("sent SIP/SIMPLE Msg to '~s' via ~s: ~p", [To, Node, Resp]);
+        _Else ->
+            lager:debug("drop SIP/SIMPLE Msg '~s' not registered", [To])
+    end. 
