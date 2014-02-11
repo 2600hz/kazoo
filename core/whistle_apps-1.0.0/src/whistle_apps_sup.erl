ca48347637a1073a8b90457a53f15f691b626e51
James Aimonetti <james@2600hz.org>
2014-02-11 13:38:01 -0800

    KAZOO-2048: add shared listener to supervisor

--- core/whistle_apps-1.0.0/src/whistle_apps_sup.erl
+++ core/whistle_apps-1.0.0/src/whistle_apps_sup.erl
@@ -46,6 +46,7 @@
 
 -define(CHILDREN, [?WORKER('wh_nodes')
                    ,?WORKER('wh_hooks_listener')
+                   ,?WORKER('wh_hooks_shared_listener')
                    ,?WORKER('wh_cache')
                    ,?CACHE_ARGS(?WHAPPS_CONFIG_CACHE, ?CACHE_PROPS)
                    ,?WORKER('whistle_apps_init')
