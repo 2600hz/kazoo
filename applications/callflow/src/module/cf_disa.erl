24f479f6119b2feef3141ff678df278317568d95
James Aimonetti <james@2600hz.org>
2014-02-12 13:24:49 -0800

    KAZOO-624: look for interdigit timeout in data
    refactor the call out into sub functions too

--- applications/callflow/src/module/cf_disa.erl
+++ applications/callflow/src/module/cf_disa.erl
@@ -1,7 +1,12 @@
 %%%%-------------------------------------------------------------------
 %%% @copyright (C) 2012-2013, 2600Hz INC
 %%% @doc
-%%%
+%%% "data":{
+%%%   "pin":"1234"
+%%%   ,"retries":3
+%%%   // optional after here
+%%%   ,"interdigit":2000
+%%% }
 %%% @end
 %%% @contributors
 %%%   James Aimonetti
@@ -21,11 +26,14 @@
 handle(Data, Call) ->
     lager:info("starting DISA handler"),
     whapps_call_command:answer(Call),
+
     Pin = wh_json:get_value(<<"pin">>, Data),
     Retries = wh_json:get_integer_value(<<"retries">>, Data, 3),
-    case try_collect_pin(Call, Pin, Retries) of
-        'allow' -> allow_dial(Data, Call, Retries);
-        _ -> cf_exe:stop(Call)
+    Interdigit = wh_json:get_integer_value(<<"interdigit">>, Data, whapps_call_command:default_interdigit_timeout()),
+
+    case try_collect_pin(Call, Pin, Retries, Interdigit) of
+        'allow' -> allow_dial(Data, Call, Retries, Interdigit);
+        'fail' -> cf_exe:stop(Call)
     end.
 
 %%--------------------------------------------------------------------
@@ -33,24 +41,32 @@ handle(Data, Call) ->
 %% @doc
 %% @end
 %%--------------------------------------------------------------------
--spec try_collect_pin(whapps_call:call(), binary(), non_neg_integer()) -> 'allow' | 'fail'.
-try_collect_pin(_Call, <<>>, _) ->
-    lager:info("no pin set on DISA object, permitting"),
+-spec try_collect_pin(whapps_call:call(), binary(), non_neg_integer(), pos_integer()) -> 'allow' | 'fail'.
+try_collect_pin(_Call, <<>>, _Retries, _Interdigit) ->
+    lager:warning("no pin set on DISA object, permitting"),
     'allow';
-try_collect_pin(Call, _, 0) ->
+try_collect_pin(Call, _Pin, 0, _Interdigit) ->
     lager:info("retries for DISA pin exceeded"),
     _ = whapps_call_command:b_prompt(<<"disa-retries_exceeded">>, Call),
     'fail';
-try_collect_pin(Call, Pin, Retries) ->
+try_collect_pin(Call, Pin, Retries, Interdigit) ->
     Prompt = <<"disa-enter_pin">>,
-    case whapps_call_command:b_prompt_and_collect_digits(1, 6, Prompt, Call) of
+    NoopId = whapps_call_command:prompt(Prompt, Call),
+
+    case whapps_call_command:collect_digits(6
+                                            ,whapps_call_command:default_collect_timeout()
+                                            ,Interdigit
+                                            ,NoopId
+                                            ,Call
+                                           )
+    of
         {'ok', Pin} ->
             lager:info("pin matches, permitting"),
             'allow';
         {'ok', _Digits} ->
-            lager:info("caller entered ~s for pin", [_Digits]),
+            lager:info("caller entered bad pin: '~s'", [_Digits]),
             _ = whapps_call_command:b_prompt(<<"disa-invalid_pin">>, Call),
-            try_collect_pin(Call, Pin, Retries - 1)
+            try_collect_pin(Call, Pin, Retries - 1, Interdigit)
     end.
 
 %%--------------------------------------------------------------------
@@ -58,32 +74,34 @@ try_collect_pin(Call, Pin, Retries) ->
 %% @doc
 %% @end
 %%--------------------------------------------------------------------
--spec allow_dial(wh_json:object(), whapps_call:call(), non_neg_integer()) -> 'ok'.
-allow_dial(_, Call, 0) ->
+-spec allow_dial(wh_json:object(), whapps_call:call(), non_neg_integer(), pos_integer()) -> 'ok'.
+allow_dial(_, Call, 0, _Interdigit) ->
     lager:info("retries exceeded for finding a callflow"),
     cf_exe:continue(Call);
-allow_dial(Data, Call, Retries) ->
-    case wh_json:get_ne_value(<<"preconnect_audio">>, Data, <<"dialtone">>) of
-        <<"dialtone">> ->
-            lager:debug("playing dialtone..."),
-            play_dialtone(Call);
-        <<"ringing">> ->
-            lager:debug("playing ringing..."),
-            play_ringing(Data, Call);
-        _Else -> lager:debug("unknown preconnect audio type: ~p", [_Else])
-    end,
-    {'ok', Digits} = whapps_call_command:collect_digits(15, Call),
+allow_dial(Data, Call, Retries, Interdigit) ->
+    _ = start_preconnect_audio(Data, Call),
+
+    {'ok', Digits} = whapps_call_command:collect_digits(15
+                                                        ,whapps_call_command:default_collect_timeout()
+                                                        ,Interdigit
+                                                        ,Call
+                                                       ),
+
     Number = wnm_util:to_e164(Digits),
-    lager:info("caller is trying to call ~s", [Number]),
-    case wh_json:is_true(<<"use_account_caller_id">>, Data, 'false') of
-        'true' -> set_caller_id(Call);
-        'false' -> 'ok'
-    end,
+    lager:info("caller is trying to call '~s'", [Number]),
+
+    _ = maybe_update_caller_id(Data, Call),
+
+    maybe_route_to_callflow(Data, Call, Retries, Interdigit, Number).
+
+maybe_route_to_callflow(Data, Call, Retries, Interdigit, Number) ->
     case cf_util:lookup_callflow(Number, whapps_call:account_id(Call)) of
         {'ok', Flow, NoMatch} ->
             lager:info("callflow ~s satisfies request", [wh_json:get_value(<<"_id">>, Flow)]),
-            Updates = [fun(C) -> whapps_call:set_request(list_to_binary([Number, "@", whapps_call:request_realm(C)]), C) end
-                       ,fun(C) -> whapps_call:set_to(list_to_binary([Number, "@", whapps_call:to_realm(C)]), C) end
+            Updates = [{fun whapps_call:set_request/2
+                        ,list_to_binary([Number, "@", whapps_call:request_realm(Call)])
+                       }
+                       ,{fun whapps_call:set_to/2, list_to_binary([Number, "@", whapps_call:to_realm(Call)])}
                        ,fun(C) when NoMatch ->
                                 {CIDNum, CIDName} = cf_attributes:caller_id(<<"external">>, C),
                                 C1 = whapps_call:set_caller_id_number(CIDNum, C),
@@ -97,7 +115,26 @@ allow_dial(Data, Call, Retries) ->
         _ ->
             lager:info("failed to find a callflow to satisfy ~s", [Number]),
             _ = whapps_call_command:b_prompt(<<"disa-invalid_extension">>, Call),
-            allow_dial(Data, Call, Retries - 1)
+            allow_dial(Data, Call, Retries - 1, Interdigit)
+    end.
+
+-spec maybe_update_caller_id(wh_json:object(), whapps_call:call()) -> 'ok'.
+maybe_update_caller_id(Data, Call) ->
+    case wh_json:is_true(<<"use_account_caller_id">>, Data, 'false') of
+        'true' -> set_caller_id(Call);
+        'false' -> 'ok'
+    end.
+
+-spec start_preconnect_audio(wh_json:object(), whapps_call:call()) -> 'ok'.
+start_preconnect_audio(Data, Call) ->
+    case wh_json:get_ne_value(<<"preconnect_audio">>, Data, <<"dialtone">>) of
+        <<"dialtone">> ->
+            lager:debug("playing dialtone..."),
+            play_dialtone(Call);
+        <<"ringing">> ->
+            lager:debug("playing ringing..."),
+            play_ringing(Data, Call);
+        _Else -> lager:debug("unknown preconnect audio type: ~p", [_Else])
     end.
 
 %%--------------------------------------------------------------------
