137245d0bc403dbccf958d54be91da76f4d1e14e
lazedo <luis.azedo@factorlusitano.com>
2014-02-13 14:13:14 +0000

    IN PROGRESS - issue KAZOO-2105: smtp to fax https://2600hz.atlassian.net/browse/KAZOO-2105 start smtp listener

--- applications/fax/src/fax_sup.erl
+++ applications/fax/src/fax_sup.erl
@@ -29,6 +29,7 @@
                    ,?WORKER('fax_jobs')
                    ,?SUPER('fax_requests_sup')
                    ,?WORKER('fax_listener')
+                   ,?WORKER_ARGS('gen_smtp_server',['fax_smtp', [[{port,whapps_config:get_integer(?CONFIG_CAT, <<"smtp_port">>, 19025)}]]])
                   ]).
 
 %% ===================================================================
