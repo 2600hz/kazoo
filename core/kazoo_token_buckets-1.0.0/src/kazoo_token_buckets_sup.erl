50bcf53ad2358ad29ef504a0bc5615fb4ed8e263
James Aimonetti <james@2600hz.org>
2014-02-14 12:34:31 -0800

    KAZOO-1862: kazoo token buckets application

---
+++ core/kazoo_token_buckets-1.0.0/src/kazoo_token_buckets_sup.erl
@@ -0,0 +1,56 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2014, 2600Hz
+%%% @doc
+%%%
+%%% @end
+%%% @contributors
+%%%-------------------------------------------------------------------
+-module(kazoo_token_buckets_sup).
+
+-behaviour(supervisor).
+
+-export([start_link/0]).
+-export([init/1]).
+
+-include("kz_buckets.hrl").
+
+%% Helper macro for declaring children of supervisor
+-define(CHILDREN, [?CACHE('kazoo_token_buckets_cache')
+                  ]).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Starts the supervisor
+%% @end
+%%--------------------------------------------------------------------
+-spec start_link() -> startlink_ret().
+start_link() ->
+    supervisor:start_link({'local', ?MODULE}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Whenever a supervisor is started using supervisor:start_link/[2,3],
+%% this function is called by the new process to find out about
+%% restart strategy, maximum restart frequency and child
+%% specifications.
+%% @end
+%%--------------------------------------------------------------------
+-spec init([]) -> sup_init_ret().
+init([]) ->
+    RestartStrategy = 'one_for_one',
+    MaxRestarts = 5,
+    MaxSecondsBetweenRestarts = 10,
+
+    SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+
+    {'ok', {SupFlags, ?CHILDREN}}.
---
+++ core/kazoo_token_buckets-1.0.0/src/kz_buckets.hrl
@@ -0,0 +1,7 @@
+-ifndef(KZ_BUCKETS_HRL).
+-include_lib("whistle/include/wh_types.hrl").
+-include_lib("whistle/include/wh_log.hrl").
+-include_lib("whistle/include/wh_databases.hrl").
+
+-define(KZ_BUCKETS_HRL, 'true').
+-endif.
