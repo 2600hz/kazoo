f57de7e8f6c1a60b7ec68aa5159874936f370e36
lazedo <luis.azedo@factorlusitano.com>
2014-02-20 12:54:33 +0000

    kazoo_oauth_service specs and helpers

--- core/kazoo_oauth-1.0.0/src/kazoo_oauth_service.erl
+++ core/kazoo_oauth-1.0.0/src/kazoo_oauth_service.erl
@@ -7,8 +7,21 @@
 -include("kazoo_oauth.hrl").
 
 -export([service_token/2]).
-	
-service_token(#oauth_service_app{provider=#oauth_provider{auth_url=URL}}=ServiceApp, Scopes) ->
+
+
+-spec service_token(api_binary() | oauth_service_app(), api_binary()) -> api_object().
+service_token('undefined', _) ->
+    'undefined';
+service_token(_, 'undefined') ->
+    'undefined';
+service_token(AppId, Scopes) when is_binary(AppId) ->
+    lager:debug("getting service app"),
+    case kazoo_oauth_util:get_oauth_service_app(AppId) of
+        {'ok', App } -> service_token(App, Scopes);
+        {'error', Error} ->
+            lager:debug("service token ~p",[Error]),'undefined'
+    end;   
+service_token(#oauth_service_app{private_key=PrivateKey,provider=#oauth_provider{auth_url=URL}}=ServiceApp, Scopes) ->
     Assertion = kazoo_oauth_util:jwt(ServiceApp, Scopes),
     GrantType = wh_util:to_list(wh_util:uri_encode(?OAUTH_GRANT_TYPE)),	
     Headers = [{"Content-Type","application/x-www-form-urlencoded"}
