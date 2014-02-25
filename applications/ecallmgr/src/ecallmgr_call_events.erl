c4e1fbe90aec8c169b7fb036ae1ddf1e4a917f1c
lazedo <luis.azedo@factorlusitano.com>
2014-02-25 20:06:51 +0000

    KAZOO-2106 fax specific events

--- applications/ecallmgr/src/ecallmgr_call_events.erl
+++ applications/ecallmgr/src/ecallmgr_call_events.erl
@@ -483,8 +483,14 @@ generic_call_event_props(Props) ->
      ,{<<"Caller-ID-Name">>, props:get_first_defined([<<"variable_effective_caller_id_name">>
                                                       ,<<"Caller-Caller-ID-Name">>
                                                      ], Props)}
-     ,{<<"Callee-ID-Number">>, props:get_value(<<"Caller-Callee-ID-Number">>, Props)}
-     ,{<<"Callee-ID-Name">>, props:get_value(<<"Caller-Callee-ID-Name">>, Props)}
+     ,{<<"Callee-ID-Number">>, props:get_first_defined([<<"variable_effective_callee_id_number">>
+                                                        ,<<"Caller-Callee-ID-Number">>
+                                                       ], Props)}
+     ,{<<"Callee-ID-Name">>, props:get_first_defined([<<"variable_effective_callee_id_name">>
+                                                      ,<<"Caller-Callee-ID-Name">>
+                                                     ], Props)}
+%     ,{<<"Callee-ID-Number">>, props:get_value(<<"Caller-Callee-ID-Number">>, Props)}
+%     ,{<<"Callee-ID-Name">>, props:get_value(<<"Caller-Callee-ID-Name">>, Props)}
      ,{<<"Other-Leg-Direction">>, props:get_value(<<"Other-Leg-Direction">>, Props)}
      ,{<<"Other-Leg-Caller-ID-Name">>, props:get_value(<<"Other-Leg-Caller-ID-Name">>, Props)}
      ,{<<"Other-Leg-Caller-ID-Number">>, props:get_value(<<"Other-Leg-Caller-ID-Number">>, Props)}
@@ -660,6 +666,7 @@ conference_specific(Props) ->
 
 -spec fax_specific(wh_proplist()) -> wh_proplist().
 fax_specific(Props) ->
+    props:to_log(Props,<<"FAX EVENT">>),
     props:filter_undefined(      
       [{<<"Fax-Success">>, get_fax_success(Props)}
        ,{<<"Fax-ECM-Used">>, get_fax_ecm_used(Props)}
@@ -681,6 +688,9 @@ fax_specific(Props) ->
        ,{<<"Fax-Longest-Bad-Row-Run">>, props:get_value(<<"variable_fax_longest_bad_row_run">>, Props)}
        ,{<<"Fax-Encoding">>, props:get_value(<<"variable_fax_encoding">>, Props)}
        ,{<<"Fax-Encoding-Name">>, props:get_value(<<"variable_fax_encoding_name">>, Props)}
+       ,{<<"Fax-Timezone">>, props:get_value(<<"variable_fax_timezone">>, Props)}
+       ,{<<"Fax-Identity-Number">>, props:get_value(<<"variable_fax_ident">>, Props)}
+       ,{<<"Fax-Identity-Name">>, props:get_value(<<"variable_fax_header">>, Props)}
        ]).
 
 -spec should_publish(ne_binary(), ne_binary(), boolean()) -> boolean().
