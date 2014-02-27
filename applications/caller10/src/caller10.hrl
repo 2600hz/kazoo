a02257e7b42e7d0aa150470d2ce4414dde3a0226
James Aimonetti <james@2600hz.org>
2014-02-26 16:56:06 -0800

    caller10: initial commit of caller10 app

---
+++ applications/caller10/src/caller10.hrl
@@ -0,0 +1,7 @@
+-ifndef(SKEL_HRL).
+-include_lib("whistle/include/wh_types.hrl").
+-include_lib("whistle/include/wh_log.hrl").
+-include_lib("whistle/include/wh_databases.hrl").
+
+-define(SKEL_HRL, 'true').
+-endif.
