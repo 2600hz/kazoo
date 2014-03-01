7d7990246f245d1624e244cf0b4ac341ec85732b
James Aimonetti <james@2600hz.org>
2014-02-28 17:53:39 -0800

    caller10: formatting

--- core/whistle-1.0.0/src/api/wapi_nodes.erl
+++ core/whistle-1.0.0/src/api/wapi_nodes.erl
@@ -1,5 +1,5 @@
 %%%-------------------------------------------------------------------
-%%% @copyright (C) 2011-2013, 2600Hz
+%%% @copyright (C) 2011-2014, 2600Hz
 %%% @doc
 %%%
 %%% @end
@@ -33,27 +33,27 @@
 %% Takes proplist, creates JSON string or error
 %% @end
 %%--------------------------------------------------------------------
--spec advertise(api_terms()) -> {'ok', iolist()} | {'error', string()}.
+-spec advertise(api_terms()) ->
+                       {'ok', iolist()} |
+                       {'error', string()}.
 advertise(Prop) when is_list(Prop) ->
     case advertise_v(Prop) of
-        true -> wh_api:build_message(Prop, ?ADVERTISE_HEADERS, ?OPTIONAL_ADVERTISE_HEADERS);
-        false -> {error, "Proplist failed validation for advertise"}
+        'true' -> wh_api:build_message(Prop, ?ADVERTISE_HEADERS, ?OPTIONAL_ADVERTISE_HEADERS);
+        'false' -> {'error', "Proplist failed validation for advertise"}
     end;
-advertise(JObj) ->
-    advertise(wh_json:to_proplist(JObj)).
+advertise(JObj) -> advertise(wh_json:to_proplist(JObj)).
 
 -spec advertise_v(api_terms()) -> boolean().
 advertise_v(Prop) when is_list(Prop) ->
     wh_api:validate(Prop, ?ADVERTISE_HEADERS, ?ADVERTISE_VALUES, ?ADVERTISE_TYPES);
-advertise_v(JObj) ->
-    advertise_v(wh_json:to_proplist(JObj)).
+advertise_v(JObj) -> advertise_v(wh_json:to_proplist(JObj)).
 
 %%--------------------------------------------------------------------
 %% @doc
 %% bind to a queue to the asr exchange and events
 %% @end
 %%--------------------------------------------------------------------
--spec bind_q(binary(), proplist()) -> 'ok'.
+-spec bind_q(ne_binary(), wh_proplist()) -> 'ok'.
 bind_q(Queue, _Props) ->
     amqp_util:bind_q_to_nodes(Queue).
 
@@ -62,7 +62,7 @@ bind_q(Queue, _Props) ->
 %% unbind to a queue to the asr exchange and events
 %% @end
 %%--------------------------------------------------------------------
--spec unbind_q(binary()) -> 'ok'.
+-spec unbind_q(ne_binary()) -> 'ok'.
 unbind_q(Queue) ->
     amqp_util:unbind_q_from_nodes(Queue).
 
@@ -85,5 +85,5 @@ declare_exchanges() ->
 publish_advertise(JObj) ->
     publish_advertise(JObj, ?DEFAULT_CONTENT_TYPE).
 publish_advertise(Advertise, ContentType) ->
-    {ok, Payload} = wh_api:prepare_api_payload(Advertise, ?ADVERTISE_VALUES, fun ?MODULE:advertise/1),
+    {'ok', Payload} = wh_api:prepare_api_payload(Advertise, ?ADVERTISE_VALUES, fun ?MODULE:advertise/1),
     amqp_util:nodes_publish(Payload, ContentType).
