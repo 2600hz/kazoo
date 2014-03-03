ea80f66ccedfc7453a92f90160bd35bfcd17a51a
James Aimonetti <james@2600hz.org>
2014-03-02 21:26:10 -0800

    caller10: bootstrap loading of contests

--- applications/caller10/src/caller10_contests.erl
+++ applications/caller10/src/caller10_contests.erl
@@ -22,10 +22,13 @@
          ,code_change/3
         ]).
 
+%% ETS related
 -export([table_id/0
          ,table_options/0
          ,find_me_function/0
          ,gift_data/0
+
+         ,load_contests/0
         ]).
 
 -include("caller10.hrl").
@@ -36,15 +39,16 @@
                }).
 
 -record(contest, {id :: ne_binary()
-                  ,prior_time :: pos_integer()
+                  ,prior_time :: pos_integer() | 'undefined'
                   ,start_time :: pos_integer()
-                  ,end_time :: pos_integer()
-                  ,after_time :: pos_integer()
+                  ,end_time :: pos_integer() | 'undefined'
+                  ,after_time :: pos_integer() | 'undefined'
                   ,doc :: api_object()
                   ,handling_app :: api_binary()
                   ,numbers :: ne_binaries()
                   ,account_id :: ne_binary()
                  }).
+-type contest() :: #contest{}.
 
 %%%===================================================================
 %%% API
@@ -122,6 +126,12 @@ handle_call(_Request, _From, State) ->
 %%                                  {stop, Reason, State}
 %% @end
 %%--------------------------------------------------------------------
+handle_cast({'load_contest', #contest{id=Id}=Contest}, State) ->
+    case ets:insert_new(table_id(), Contest) of
+        'false' -> lager:debug("contest ~s already loaded", [Id]);
+        'true' -> lager:debug("loaded contest ~s", [Id])
+    end,
+    {'noreply', State};
 handle_cast(_Msg, State) ->
     lager:debug("unhandled cast: ~p", [_Msg]),
     {'noreply', State}.
@@ -138,6 +148,8 @@ handle_cast(_Msg, State) ->
 %%--------------------------------------------------------------------
 handle_info({'ETS-TRANSFER', _TableId, _From, _GiftData}, State) ->
     lager:debug("recv ETS table ~p from ~p", [_TableId, _From]),
+    _Pid = spawn(?MODULE, 'load_contests', []),
+    lager:debug("loading contests in ~p", [_Pid]),
     {'noreply', State#state{is_writable='true'}};
 handle_info(_Info, State) ->
     lager:debug("unhandled msg: ~p", [_Info]),
@@ -171,3 +183,58 @@ code_change(_OldVsn, State, _Extra) ->
 %%%===================================================================
 %%% Internal functions
 %%%===================================================================
+-spec load_contests() -> 'ok'.
+load_contests() ->
+    wh_util:put_callid(?MODULE),
+    case couch_mgr:get_results(<<"contests">>, <<"contests/accounts_listing">>, [{'reduce', 'false'}]) of
+        {'ok', []} ->
+            lager:debug("no accounts found in the aggregate");
+        {'ok', Accounts} ->
+            load_contests(Accounts),
+            lager:debug("loaded ~p contest accounts", [length(Accounts)]);
+        {'error', 'not_found'} ->
+            lager:debug("aggregate DB not found or view is missing"),
+            caller10_maintenance:refresh_contests_db(),
+            load_contests();
+        {'error', _E} ->
+            lager:debug("unable to load contest accounts: ~p", [_E])
+    end.
+
+-spec load_contests(wh_json:objects()) -> 'ok'.
+load_contests(Accounts) ->
+    [load_account_contests(wh_json:get_value(<<"key">>, Account)) || Account <- Accounts],
+    'ok'.
+
+-spec load_account_contests(ne_binary()) -> 'ok'.
+load_account_contests(AccountId) ->
+    AccountDb = wh_util:format_account_id(AccountId, 'encoded'),
+    Options = ['include_docs'],
+    case couch_mgr:get_results(AccountDb, <<"contests/crossbar_listing">>, Options) of
+        {'ok', []} ->
+            lager:debug("account ~s is in aggregate, but has no contests; removing from aggregate", [AccountId]),
+            couch_mgr:del_doc(<<"contests">>, AccountId),
+            'ok';
+        {'ok', Contests} ->
+            [load_account_contest(wh_json:get_value(<<"doc">>, Contest))
+             || Contest <- Contests
+            ];
+        {'error', _E} ->
+            lager:debug("failed to load contests from account ~s: ~p", [AccountId, _E])
+    end.
+
+-spec load_account_contest(wh_json:object()) -> 'ok'.
+load_account_contest(ContestJObj) ->
+    gen_server:cast(?MODULE, {'load_contest', jobj_to_record(ContestJObj)}).
+
+-spec jobj_to_record(wh_json:object()) -> contest().
+jobj_to_record(JObj) ->
+    #contest{id = wh_json:get_value(<<"_id">>, JObj)
+             ,prior_time = wh_json:get_integer_value(<<"prior_time">>, JObj)
+             ,start_time = wh_json:get_integer_value(<<"start_time">>, JObj)
+             ,end_time = wh_json:get_integer_value(<<"end_time">>, JObj)
+             ,after_time = wh_json:get_integer_value(<<"after_time">>, JObj)
+             ,doc = JObj
+             ,handling_app = 'undefined'
+             ,numbers = []
+             ,account_id = wh_json:get_value(<<"pvt_account_id">>, JObj)
+            }.
--- applications/caller10/src/caller10_maintenance.erl
+++ applications/caller10/src/caller10_maintenance.erl
@@ -7,7 +7,9 @@
 %%%-------------------------------------------------------------------
 -module(caller10_maintenance).
 
--export([is_running/0]).
+-export([is_running/0
+         ,refresh_contests_db/0
+        ]).
 
 is_running() ->
     case lists:keyfind('caller10', 1, application:which_applications()) of
@@ -16,3 +18,23 @@ is_running() ->
             io:format("Caller10 (~s) is running~n", [_Vsn])
     end.
 
+-spec refresh_contests_db() -> 'ok'.
+refresh_contests_db() ->
+    case couch_mgr:db_exists(<<"contests">>) of
+        'true' -> refresh_view();
+        'false' ->
+            init_db(),
+            refresh_view()
+    end,
+    'ok'.
+
+-spec init_db() -> boolean().
+init_db() ->
+    io:format("creating 'contests' aggregate database~n", []),
+    couch_mgr:db_create(<<"contests">>).
+
+-spec refresh_view() -> {'ok', wh_json:object()} |
+                        {'error', _}.
+refresh_view() ->
+    io:format("refreshing contests design doc~n", []),
+    couch_mgr:revise_doc_from_file(<<"contests">>, 'crossbar', <<"views/contests.json">>).
