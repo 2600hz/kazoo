85bb93f1b4e518e91487d79cb41485924c43e7f7
James Aimonetti <james@2600hz.org>
2014-03-05 17:34:35 -0800

    caller10: supervisor to manage contest handler supervisors

---
+++ applications/caller10/src/caller10_contests_sup.erl
@@ -0,0 +1,64 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2014, 2600Hz INC
+%%% @doc
+%%%
+%%% @end
+%%% @contributors
+%%%   James Aimonetti
+%%%-------------------------------------------------------------------
+-module(caller10_contests_sup).
+
+-behaviour(supervisor).
+
+-include("caller10.hrl").
+
+%% API
+-export([start_link/0
+         ,new/1
+         ,workers/0
+        ]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%%%===================================================================
+%%% API functions
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the supervisor
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+-spec start_link() -> startlink_ret().
+start_link() -> supervisor:start_link({'local', ?MODULE}, ?MODULE, []).
+
+-spec new(caller10_contests:contest()) -> sup_startchild_ret().
+new(Contest) ->
+    supervisor:start_child(?MODULE, [Contest]).
+
+-spec workers() -> pids().
+workers() ->
+    [Pid
+     || {_, Pid, 'supervisor', [_]} <- supervisor:which_children(?MODULE)
+    ].
+
+%%%===================================================================
+%%% Supervisor callbacks
+%%%===================================================================
+
+-spec init(list()) -> sup_init_ret().
+init([]) ->
+    RestartStrategy = 'simple_one_for_one',
+    MaxRestarts = 1,
+    MaxSecondsBetweenRestarts = 1,
+
+    SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+
+    {'ok', {SupFlags, [?SUPER('caller10_contest_sup', 'temporary')]}}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
