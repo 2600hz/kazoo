46d6ad92bda57575c821ee2aca2bb6e16d8d4c7f
James Aimonetti <james@2600hz.org>
2014-03-14 13:28:04 -0700

    KAZOO-2122: add specs

--- core/kazoo_translator-1.0.0/src/kazoo_translator_app.erl
+++ core/kazoo_translator-1.0.0/src/kazoo_translator_app.erl
@@ -5,12 +5,14 @@
 %% Application callbacks
 -export([start/2, stop/1]).
 
+-include("kzt.hrl").
+
 %% ===================================================================
 %% Application callbacks
 %% ===================================================================
-
+-spec start(term(), term()) -> startlink_ret().
 start(_StartType, _StartArgs) ->
     kazoo_translator_sup:start_link().
 
-stop(_State) ->
-    ok.
+-spec stop(term()) -> 'ok'.
+stop(_State) -> 'ok'.
--- core/kazoo_translator-1.0.0/src/kazoo_translator_sup.erl
+++ core/kazoo_translator-1.0.0/src/kazoo_translator_sup.erl
@@ -1,4 +1,11 @@
-
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2012-2014, 2600Hz
+%%% @doc
+%%% Receive call events for various scenarios
+%%% @end
+%%% @contributors
+%%%   James Aimonetti
+%%%-------------------------------------------------------------------
 -module(kazoo_translator_sup).
 
 -behaviour(supervisor).
@@ -9,20 +16,19 @@
 %% Supervisor callbacks
 -export([init/1]).
 
-%% Helper macro for declaring children of supervisor
--define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+-include("kzt.hrl").
 
 %% ===================================================================
 %% API functions
 %% ===================================================================
-
+-spec start_link() -> startlink_ret().
 start_link() ->
-    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+    supervisor:start_link({'local', ?MODULE}, ?MODULE, []).
 
 %% ===================================================================
 %% Supervisor callbacks
 %% ===================================================================
-
+-spec init([]) -> sup_init_ret().
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
+    {'ok', { {'one_for_one', 5, 10}, []} }.
 
