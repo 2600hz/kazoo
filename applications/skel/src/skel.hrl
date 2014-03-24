463953522a73f0a99df5dcac7f8868421808a3ed
karl anderson <karl@2600hz.com>
2014-03-24 11:52:07 -0700

    KAZOO-2180: add an app_name/version

--- applications/skel/src/skel.hrl
+++ applications/skel/src/skel.hrl
@@ -3,5 +3,8 @@
 -include_lib("whistle/include/wh_log.hrl").
 -include_lib("whistle/include/wh_databases.hrl").
 
+-define(APP_NAME, <<"skel">>).
+-define(APP_VERSION, <<"0.0.1">> ).
+
 -define(SKEL_HRL, 'true').
 -endif.
