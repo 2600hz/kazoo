358d36eb99bd32d7ab215b4719c441b495a872e0
James Aimonetti <james@2600hz.org>
2014-03-28 09:05:22 -0700

    KAZOO-216: add branching support to konami_code_exe

--- applications/konami/src/konami_code_exe.erl
+++ applications/konami/src/konami_code_exe.erl
@@ -12,14 +12,29 @@
 
 -include("konami.hrl").
 
+-spec handle(api_object(), whapps_call:call()) -> 'ok'.
+handle('undefined', _Call) ->
+    lager:debug("no metaflow to execute");
 handle(Metaflow, Call) ->
     whapps_call:put_callid(Call),
     M = wh_json:get_value(<<"module">>, Metaflow),
     Data = wh_json:get_value(<<"data">>, Metaflow, wh_json:new()),
 
     try (wh_util:to_atom(<<"konami_", M/binary>>)):handle(Data, Call) of
-        _ -> lager:debug("finished handling metaflow for ~s", [M])
+        {'branch', ChildBranch, Call1} ->
+            lager:debug("continuing to child metaflow from konami_~s", [M]),
+            handle(find_child_metaflow(ChildBranch, Metaflow), Call1);
+        _ -> lager:debug("finished handling metaflow for konami_~s", [M])
     catch
         _E:_R ->
             lager:debug("failed to exe metaflow 'konami_~s': ~s: ~p", [M, _E, _R])
+    end.
+
+-spec find_child_metaflow(api_binary(), wh_json:object()) -> api_object().
+find_child_metaflow('undefined', Metaflow) ->
+    wh_json:get_value([<<"children">>, <<"_">>], Metaflow);
+find_child_metaflow(Child, Metaflow) ->
+    case wh_json:get_value([<<"children">>, Child], Metaflow) of
+        'undefined' -> find_child_metaflow('undefined', Metaflow);
+        ChildFlow -> ChildFlow
     end.
