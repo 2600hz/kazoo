f7cdb7e7beac874ad56041a0ee03a7156c17adac
James Aimonetti <james@2600hz.org>
2014-03-28 16:23:48 -0700

    KAZOO-216: send say inserted now

--- applications/konami/src/module/konami_say.erl
+++ applications/konami/src/module/konami_say.erl
@@ -2,6 +2,12 @@
 %%% @copyright (C) 2014, 2600Hz
 %%% @doc
 %%% Say something
+%%% Data = {
+%%%   "text":"text to say"
+%%%   ,"type":"name_spelled"
+%%%   ,"method":"pronounced"
+%%%   ,"language":"en"
+%%% }
 %%% @end
 %%% @contributors
 %%%   James Aimonetti
@@ -16,5 +22,15 @@
 handle(Data, Call) ->
     Say = wh_json:get_value(<<"text">>, Data),
     lager:debug("saying '~s'", [Say]),
-    whapps_call_command:b_say(Say, Call),
+
+    SayCommand = whapps_call_command:say_command(Say
+                                                 ,wh_json:get_value(<<"type">>, Data)
+                                                 ,wh_json:get_value(<<"method">>, Data)
+                                                 ,wh_json:get_value(<<"language">>, Data)
+                                                 ,Call
+                                                ),
+    whapps_call_command:send_command(
+      wh_json:set_value(<<"Insert-At">>, <<"now">>, SayCommand)
+      ,Call
+     ),
     {'continue', Call}.
