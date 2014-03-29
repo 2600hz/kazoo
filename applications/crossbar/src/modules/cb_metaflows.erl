ff9280a42f6222453c5f2867cdcefe1365f2678f
James Aimonetti <james@2600hz.org>
2014-03-28 17:11:52 -0700

    KAZOO-216: CRUD for crossbar metaflow endpoint

---
+++ applications/crossbar/src/modules/cb_metaflows.erl
@@ -0,0 +1,200 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2011-2014, 2600Hz INC
+%%% @doc
+%%%
+%%% Metaflows execute on top of a call
+%%%
+%%% @end
+%%% @contributors:
+%%%   Karl Anderson
+%%%   James Aimonetti
+%%%-------------------------------------------------------------------
+-module(cb_metaflows).
+
+-export([init/0
+         ,allowed_methods/0, allowed_methods/1
+         ,resource_exists/0, resource_exists/1
+         ,validate/1, validate/2
+         ,put/1
+         ,post/2
+         ,delete/2
+        ]).
+
+-include("../crossbar.hrl").
+
+-define(CB_LIST, <<"metaflows/crossbar_listing">>).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Initializes the bindings this module will respond to.
+%% @end
+%%--------------------------------------------------------------------
+-spec init() -> 'ok'.
+init() ->
+    _ = crossbar_bindings:bind(<<"*.allowed_methods.metaflows">>, ?MODULE, 'allowed_methods'),
+    _ = crossbar_bindings:bind(<<"*.resource_exists.metaflows">>, ?MODULE, 'resource_exists'),
+    _ = crossbar_bindings:bind(<<"*.validate.metaflows">>, ?MODULE, 'validate'),
+    _ = crossbar_bindings:bind(<<"*.execute.put.metaflows">>, ?MODULE, 'put'),
+    _ = crossbar_bindings:bind(<<"*.execute.post.metaflows">>, ?MODULE, 'post'),
+    _ = crossbar_bindings:bind(<<"*.execute.delete.metaflows">>, ?MODULE, 'delete').
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Given the path tokens related to this module, what HTTP methods are
+%% going to be responded to.
+%% @end
+%%--------------------------------------------------------------------
+-spec allowed_methods() -> http_methods().
+-spec allowed_methods(path_token()) -> http_methods().
+allowed_methods() ->
+    [?HTTP_GET, ?HTTP_PUT].
+allowed_methods(_) ->
+    [?HTTP_GET, ?HTTP_POST, ?HTTP_DELETE].
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Does the path point to a valid resource
+%% So /metaflows => []
+%%    /metaflows/foo => [<<"foo">>]
+%%    /metaflows/foo/bar => [<<"foo">>, <<"bar">>]
+%% @end
+%%--------------------------------------------------------------------
+-spec resource_exists() -> 'true'.
+-spec resource_exists(path_token()) -> 'true'.
+resource_exists() -> 'true'.
+resource_exists(_) -> 'true'.
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Check the request (request body, query string params, path tokens, etc)
+%% and load necessary information.
+%% /metaflows mights load a list of metaflow objects
+%% /metaflows/123 might load the metaflow object 123
+%% Generally, use crossbar_doc to manipulate the cb_context{} record
+%% @end
+%%--------------------------------------------------------------------
+-spec validate(cb_context:context()) -> cb_context:context().
+-spec validate(cb_context:context(), path_token()) -> cb_context:context().
+validate(Context) ->
+    validate_metaflows(Context, cb_context:req_verb(Context)).
+validate(Context, Id) ->
+    validate_metaflow(Context, Id, cb_context:req_verb(Context)).
+
+-spec validate_metaflows(cb_context:context(), http_method()) -> cb_context:context().
+validate_metaflows(Context, ?HTTP_GET) ->
+    summary(Context);
+validate_metaflows(Context, ?HTTP_PUT) ->
+    create(Context).
+
+-spec validate_metaflow(cb_context:context(), path_token(), http_method()) -> cb_context:context().
+validate_metaflow(Context, Id, ?HTTP_GET) ->
+    read(Id, Context);
+validate_metaflow(Context, Id, ?HTTP_POST) ->
+    update(Id, Context);
+validate_metaflow(Context, Id, ?HTTP_DELETE) ->
+    read(Id, Context).
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% If the HTTP verb is PUT, execute the actual action, usually a db save.
+%% @end
+%%--------------------------------------------------------------------
+-spec put(cb_context:context()) -> cb_context:context().
+put(Context) ->
+    crossbar_doc:save(Context).
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% If the HTTP verb is POST, execute the actual action, usually a db save
+%% (after a merge perhaps).
+%% @end
+%%--------------------------------------------------------------------
+-spec post(cb_context:context(), path_token()) -> cb_context:context().
+post(Context, _) ->
+    crossbar_doc:save(Context).
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% If the HTTP verb is DELETE, execute the actual action, usually a db delete
+%% @end
+%%--------------------------------------------------------------------
+-spec delete(cb_context:context(), path_token()) -> cb_context:context().
+delete(Context, _) ->
+    crossbar_doc:delete(Context).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Create a new instance with the data provided, if it is valid
+%% @end
+%%--------------------------------------------------------------------
+-spec create(cb_context:context()) -> cb_context:context().
+create(Context) ->
+    OnSuccess = fun(C) -> on_successful_validation('undefined', C) end,
+    cb_context:validate_request_data(<<"metaflows">>, Context, OnSuccess).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Load an instance from the database
+%% @end
+%%--------------------------------------------------------------------
+-spec read(ne_binary(), cb_context:context()) -> cb_context:context().
+read(Id, Context) ->
+    crossbar_doc:load(Id, Context).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Update an existing menu document with the data provided, if it is
+%% valid
+%% @end
+%%--------------------------------------------------------------------
+-spec update(ne_binary(), cb_context:context()) -> cb_context:context().
+update(Id, Context) ->
+    OnSuccess = fun(C) -> on_successful_validation(Id, C) end,
+    cb_context:validate_request_data(<<"metaflows">>, Context, OnSuccess).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Attempt to load a summarized listing of all instances of this
+%% resource.
+%% @end
+%%--------------------------------------------------------------------
+-spec summary(cb_context:context()) -> cb_context:context().
+summary(Context) ->
+    crossbar_doc:load_view(?CB_LIST, [], Context, fun normalize_view_results/2).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec on_successful_validation(api_binary(), cb_context:context()) -> cb_context:context().
+on_successful_validation('undefined', Context) ->
+    cb_context:set_doc(Context, wh_json:set_value(<<"pvt_type">>, <<"metaflow">>, cb_context:doc(Context)));
+on_successful_validation(Id, Context) ->
+    crossbar_doc:load_merge(Id, Context).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Normalizes the resuts of a view
+%% @end
+%%--------------------------------------------------------------------
+-spec normalize_view_results(wh_json:object(), wh_json:objects()) -> wh_json:objects().
+normalize_view_results(JObj, Acc) ->
+    [wh_json:get_value(<<"value">>, JObj)|Acc].
