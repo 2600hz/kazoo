e85405015725c2c923f34e642dc7aaf9991c2649
James Aimonetti <james@2600hz.org>
2014-03-31 16:08:44 -0700

    KAZOO-216: CRUD metaflows for account doc, and other "things" like users/devices

--- applications/crossbar/src/modules/cb_metaflows.erl
+++ applications/crossbar/src/modules/cb_metaflows.erl
@@ -3,6 +3,9 @@
 %%% @doc
 %%%
 %%% Metaflows execute on top of a call
+%%% /accounts/{account_id}/metaflows - manip account metaflows
+%%% /accounts/{account_id}/users/{user_id}/metaflows - manip user's metaflows
+%%% /accounts/{account_id}/devices/{device_id}/metaflows - manip user's metaflows
 %%%
 %%% @end
 %%% @contributors:
@@ -12,12 +15,11 @@
 -module(cb_metaflows).
 
 -export([init/0
-         ,allowed_methods/0, allowed_methods/1
-         ,resource_exists/0, resource_exists/1
-         ,validate/1, validate/2
-         ,put/1
-         ,post/2
-         ,delete/2
+         ,allowed_methods/0
+         ,resource_exists/0
+         ,validate/1
+         ,post/1
+         ,delete/1
         ]).
 
 -include("../crossbar.hrl").
@@ -39,7 +41,6 @@ init() ->
     _ = crossbar_bindings:bind(<<"*.allowed_methods.metaflows">>, ?MODULE, 'allowed_methods'),
     _ = crossbar_bindings:bind(<<"*.resource_exists.metaflows">>, ?MODULE, 'resource_exists'),
     _ = crossbar_bindings:bind(<<"*.validate.metaflows">>, ?MODULE, 'validate'),
-    _ = crossbar_bindings:bind(<<"*.execute.put.metaflows">>, ?MODULE, 'put'),
     _ = crossbar_bindings:bind(<<"*.execute.post.metaflows">>, ?MODULE, 'post'),
     _ = crossbar_bindings:bind(<<"*.execute.delete.metaflows">>, ?MODULE, 'delete').
 
@@ -51,10 +52,7 @@ init() ->
 %% @end
 %%--------------------------------------------------------------------
 -spec allowed_methods() -> http_methods().
--spec allowed_methods(path_token()) -> http_methods().
 allowed_methods() ->
-    [?HTTP_GET, ?HTTP_PUT].
-allowed_methods(_) ->
     [?HTTP_GET, ?HTTP_POST, ?HTTP_DELETE].
 
 %%--------------------------------------------------------------------
@@ -67,9 +65,7 @@ allowed_methods(_) ->
 %% @end
 %%--------------------------------------------------------------------
 -spec resource_exists() -> 'true'.
--spec resource_exists(path_token()) -> 'true'.
 resource_exists() -> 'true'.
-resource_exists(_) -> 'true'.
 
 %%--------------------------------------------------------------------
 %% @public
@@ -82,46 +78,80 @@ resource_exists(_) -> 'true'.
 %% @end
 %%--------------------------------------------------------------------
 -spec validate(cb_context:context()) -> cb_context:context().
--spec validate(cb_context:context(), path_token()) -> cb_context:context().
 validate(Context) ->
     validate_metaflows(Context, cb_context:req_verb(Context)).
-validate(Context, Id) ->
-    validate_metaflow(Context, Id, cb_context:req_verb(Context)).
 
 -spec validate_metaflows(cb_context:context(), http_method()) -> cb_context:context().
 validate_metaflows(Context, ?HTTP_GET) ->
-    summary(Context);
-validate_metaflows(Context, ?HTTP_PUT) ->
-    create(Context).
+    validate_get_metaflows(Context, cb_context:doc(Context));
+validate_metaflows(Context, ?HTTP_POST) ->
+    Metaflows = cb_context:req_data(Context),
 
--spec validate_metaflow(cb_context:context(), path_token(), http_method()) -> cb_context:context().
-validate_metaflow(Context, Id, ?HTTP_GET) ->
-    read(Id, Context);
-validate_metaflow(Context, Id, ?HTTP_POST) ->
-    update(Id, Context);
-validate_metaflow(Context, Id, ?HTTP_DELETE) ->
-    read(Id, Context).
+    validate_set_metaflows(Context, Metaflows, cb_context:doc(Context));
+validate_metaflows(Context, ?HTTP_DELETE) ->
+    validate_delete_metaflows(Context, cb_context:doc(Context)).
+
+validate_get_metaflows(Context, 'undefined') ->
+    {'ok', AccountDoc} =
+        couch_mgr:open_cache_doc(
+          cb_context:account_db(Context)
+          ,cb_context:account_id(Context)
+         ),
+    validate_get_metaflows(Context, AccountDoc);
+validate_get_metaflows(Context, Doc) ->
+    Metaflows = wh_json:get_value(<<"metaflows">>, Doc, wh_json:new()),
+    OwnerId = wh_json:get_first_defined([<<"_id">>, <<"pvt_account_id">>], Doc),
+    crossbar_util:response(wh_json:set_value(<<"owner_id">>, OwnerId, Metaflows), Context).
+
+validate_delete_metaflows(Context, 'undefined') ->
+    {'ok', AccountDoc} =
+        couch_mgr:open_cache_doc(
+          cb_context:account_db(Context)
+          ,cb_context:account_id(Context)
+         ),
+    validate_delete_metaflows(Context, AccountDoc);
+validate_delete_metaflows(Context, Doc) ->
+    crossbar_util:response(wh_json:new()
+                           ,cb_context:set_doc(Context
+                                               ,wh_json:delete_key(<<"metaflows">>, Doc)
+                                              )).
+
+validate_set_metaflows(Context, Metaflows, 'undefined') ->
+    lager:debug("no doc found, using account doc"),
+    {'ok', AccountDoc} =
+        couch_mgr:open_cache_doc(
+          cb_context:account_db(Context)
+          ,cb_context:account_id(Context)
+         ),
+    validate_set_metaflows(Context, Metaflows, AccountDoc);
+validate_set_metaflows(Context, Metaflows, Doc) ->
+    OwnerId = wh_json:get_first_defined([<<"_id">>, <<"pvt_account_id">>], Doc),
+    Doc1 = wh_json:set_value(<<"metaflows">>
+                             ,wh_json:set_value(<<"owner_id">>, OwnerId, Metaflows)
+                             ,Doc
+                            ),
+    crossbar_util:response(Metaflows
+                           ,cb_context:set_doc(Context, Doc1)
+                          ).
 
 %%--------------------------------------------------------------------
 %% @public
 %% @doc
-%% If the HTTP verb is PUT, execute the actual action, usually a db save.
+%% If the HTTP verb is POST, execute the actual action, usually a db save.
 %% @end
 %%--------------------------------------------------------------------
--spec put(cb_context:context()) -> cb_context:context().
-put(Context) ->
-    crossbar_doc:save(Context).
+-spec post(cb_context:context()) -> cb_context:context().
+post(Context) ->
+    after_post(crossbar_doc:save(Context)).
 
-%%--------------------------------------------------------------------
-%% @public
-%% @doc
-%% If the HTTP verb is POST, execute the actual action, usually a db save
-%% (after a merge perhaps).
-%% @end
-%%--------------------------------------------------------------------
--spec post(cb_context:context(), path_token()) -> cb_context:context().
-post(Context, _) ->
-    crossbar_doc:save(Context).
+after_post(Context) ->
+    after_post(Context, cb_context:resp_status(Context)).
+
+after_post(Context, 'success') ->
+    crossbar_util:response(wh_json:get_value(<<"metaflows">>, cb_context:doc(Context))
+                           ,Context);
+after_post(Context, _RespStatus) ->
+    Context.
 
 %%--------------------------------------------------------------------
 %% @public
@@ -129,72 +159,14 @@ post(Context, _) ->
 %% If the HTTP verb is DELETE, execute the actual action, usually a db delete
 %% @end
 %%--------------------------------------------------------------------
--spec delete(cb_context:context(), path_token()) -> cb_context:context().
-delete(Context, _) ->
-    crossbar_doc:delete(Context).
+-spec delete(cb_context:context()) -> cb_context:context().
+delete(Context) ->
+    after_delete(crossbar_doc:save(Context)).
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Create a new instance with the data provided, if it is valid
-%% @end
-%%--------------------------------------------------------------------
--spec create(cb_context:context()) -> cb_context:context().
-create(Context) ->
-    OnSuccess = fun(C) -> on_successful_validation('undefined', C) end,
-    cb_context:validate_request_data(<<"metaflows">>, Context, OnSuccess).
+after_delete(Context) ->
+    after_delete(Context, cb_context:resp_status(Context)).
 
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Load an instance from the database
-%% @end
-%%--------------------------------------------------------------------
--spec read(ne_binary(), cb_context:context()) -> cb_context:context().
-read(Id, Context) ->
-    crossbar_doc:load(Id, Context).
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Update an existing menu document with the data provided, if it is
-%% valid
-%% @end
-%%--------------------------------------------------------------------
--spec update(ne_binary(), cb_context:context()) -> cb_context:context().
-update(Id, Context) ->
-    OnSuccess = fun(C) -> on_successful_validation(Id, C) end,
-    cb_context:validate_request_data(<<"metaflows">>, Context, OnSuccess).
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Attempt to load a summarized listing of all instances of this
-%% resource.
-%% @end
-%%--------------------------------------------------------------------
--spec summary(cb_context:context()) -> cb_context:context().
-summary(Context) ->
-    crossbar_doc:load_view(?CB_LIST, [], Context, fun normalize_view_results/2).
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%%
-%% @end
-%%--------------------------------------------------------------------
--spec on_successful_validation(api_binary(), cb_context:context()) -> cb_context:context().
-on_successful_validation('undefined', Context) ->
-    cb_context:set_doc(Context, wh_json:set_value(<<"pvt_type">>, <<"metaflow">>, cb_context:doc(Context)));
-on_successful_validation(Id, Context) ->
-    crossbar_doc:load_merge(Id, Context).
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Normalizes the resuts of a view
-%% @end
-%%--------------------------------------------------------------------
--spec normalize_view_results(wh_json:object(), wh_json:objects()) -> wh_json:objects().
-normalize_view_results(JObj, Acc) ->
-    [wh_json:get_value(<<"value">>, JObj)|Acc].
+after_delete(Context, 'success') ->
+    crossbar_util:response(wh_json:new(), Context);
+after_delete(Context, _RespStatus) ->
+    Context.
