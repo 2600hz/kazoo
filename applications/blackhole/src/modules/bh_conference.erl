8bd52b8fdaf79165d41f0a365f4703c53fc0d178
James Aimonetti <james@thinky64.2600hz.com>
2014-05-15 13:24:26 -0700

    KAZOO-433: remove unused functions, cleanup formatting

--- applications/blackhole/src/modules/bh_conference.erl
+++ applications/blackhole/src/modules/bh_conference.erl
@@ -4,14 +4,14 @@
 %%%
 %%% @end
 %%% @contributors
-%%% James Aimonetti
-%%% Peter Defebvre
-%%% Ben Wann
+%%%   James Aimonetti
+%%%   Peter Defebvre
+%%%   Ben Wann
 %%%-------------------------------------------------------------------
 -module(bh_conference).
 
 -export([handle_event/2
-        ,add_amqp_binding/2, rm_amqp_binding/2
+         ,add_amqp_binding/2, rm_amqp_binding/2
         ]).
 
 %%%-------------------------------------------------------------------
@@ -27,8 +27,9 @@
 handle_event(Context, EventJObj) ->
     lager:debug("handling conference event ~s", [get_response_key(EventJObj)]),
     blackhole_data_emitter:emit(bh_context:websocket_pid(Context)
-                               ,get_response_key(EventJObj)
-                               ,EventJObj).
+                                ,get_response_key(EventJObj)
+                                ,EventJObj
+                               ).
 
 -spec add_amqp_binding(ne_binary(), bh_context:context()) -> 'ok'.
 add_amqp_binding(<<"conference.event.", ConfId/binary>>, _Context) ->
@@ -49,53 +50,3 @@ rm_amqp_binding(Binding, _Context) ->
 %%%==================================================================
 get_response_key(JObj) ->
     wh_json:get_first_defined([<<"Application-Name">>, <<"Event-Name">>], JObj).
-
-fw_participant_event(JObj, Pids) ->
-    Event = cleanup_binary(wh_json:get_value(<<"Event">>, JObj)),
-    fw_participant_event(Event, JObj, Pids).
-
-fw_participant_event(Event, JObj, Pids) ->
-    CleanJObj = clean_participant_event(JObj),
-    Id = wh_json:get_value(<<"Call-ID">>, JObj),
-    blackhole_sockets:send_event(Pids
-                                 ,Event
-                                 ,[Id, CleanJObj]
-                                ).
-
-fw_conference_event(JObj, Pids) ->
-    ConfId = wh_json:get_value(<<"Conference-ID">>, JObj),
-    Event = cleanup_binary(wh_json:get_value(<<"Event">>, JObj)),
-    blackhole_sockets:send_event(Pids
-                                 ,Event
-                                 ,[ConfId]
-                                ).
-
-clean_participant_event(JObj) ->
-    RemoveKeys = [<<"Focus">>
-                  ,<<"App-Version">>
-                  ,<<"App-Name">>
-                  ,<<"Event-Category">>
-                  ,<<"Event-Name">>
-                  ,<<"Msg-ID">>
-                  ,<<"Node">>
-                  ,<<"Server-ID">>
-                  ,<<"Switch-Hostname">>
-                  ,<<"Mute-Detect">>
-                  ,<<"Custom-Channel-Vars">>
-                 ],
-    CleanKeys = [{<<"Energy-Level">>, <<"energy_level">>, fun wh_util:to_integer/1}
-                 ,{<<"Current-Energy">>, <<"current_energy">>, fun wh_util:to_integer/1}
-                 ,{<<"Talking">>, <<"talking">>, fun wh_util:to_boolean/1}
-                 ,{<<"Speak">>, <<"mute">>, fun(X) -> not wh_util:to_boolean(X) end}
-                 ,{<<"Hear">>, <<"hear">>, fun wh_util:to_boolean/1}
-                 ,{<<"Video">>, <<"video">>, fun wh_util:to_boolean/1}
-                 ,{<<"Floor">>, <<"floor">>, fun wh_util:to_boolean/1}
-                 ,{<<"Event">>, <<"event">>, fun cleanup_binary/1}
-                 ,{<<"Custom-Channel-Vars">>, <<"pin">>, fun(X) -> wh_json:get_value(<<"pin">>, X) end}
-                ],
-    wh_json:normalize_jobj(JObj, RemoveKeys, CleanKeys).
-
-cleanup_binary(Binary) ->
-    String = binary:bin_to_list(Binary),
-    Binary1 = binary:list_to_bin(string:to_lower(String)),
-    binary:replace(Binary1, <<"-">>, <<"_">>, ['global']).
