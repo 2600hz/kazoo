2e32163a257fc2b5d0847e746b34281fb5a2dc8e
James Aimonetti <james@thinky64.2600hz.com>
2014-05-19 14:29:31 -0700

    KAZOO-2454: better determination of which leg to intercept
    also capture the responses from the originate so we can start a metaflow
    processor for the originated leg (in the case where the b-leg is
    intercepted by the originating leg, thus killing the original
    a-leg). With the control queue and call id it can be possible to start
    another FSM to handle the new a-leg

--- applications/konami/src/module/konami_move.erl
+++ applications/konami/src/module/konami_move.erl
@@ -14,12 +14,44 @@
 -include("../konami.hrl").
 
 -spec handle(wh_json:object(), whapps_call:call()) ->
-                    {'continue', whapps_call:call()}.
+                    {'continue', whapps_call:call()} |
+                    {'stop', whapps_call:call()}.
 handle(Data, Call) ->
     Req = get_originate_req(Data, Call),
-    Resp = send_originate_req(Req, Call),
-    lager:debug("resp to orig: ~p", [Resp]),
-    {'continue', Call}.
+    case send_originate_req(Req, Call) of
+        {'ok', Result} ->
+            lager:debug("resp to orig: ~p", [Result]),
+            maybe_update_metaflow(Result, Call);
+        {'error', _E} ->
+            lager:debug("failed to originate: ~p", [_E]),
+            {'stop', Call};
+        {'timeout', Result} ->
+            lager:debug("timed out, here's what we got: ~p", [Result]),
+            maybe_update_metaflow(Result, Call)
+    end.
+
+maybe_update_metaflow(Results, Call) ->
+    case [Result || Result <- Results, is_resp(Result)] of
+        [] ->
+            lager:debug("no useful responses"),
+            {'stop', Call};
+        [Resp] ->
+            lager:debug("resp: ~p", [Resp]),
+            CallId = wh_json:get_value(<<"Call-ID">>, Resp),
+            maybe_update_metaflow(Results, Call, CallId)
+    end.
+
+maybe_update_metaflow(Results, Call, CallId) ->
+    case [Result || Result <- Results, is_originate_uuid(Result, CallId)] of
+        [] ->
+            lager:debug("no matching originate_uuid"),
+            {'stop', Call};
+        [OriginateUUID|_] ->
+            lager:debug("found originate for ~s: ~p", [CallId, OriginateUUID]),
+            ControlQueue = wh_json:get_value(<<"Outbound-Call-Control-Queue">>, OriginateUUID),
+            lager:debug("should use ~s for control", [ControlQueue]),
+            {'stop', Call}
+    end.
 
 -spec get_originate_req(wh_json:object(), whapps_call:call()) -> wh_json:objects().
 get_originate_req(Data, Call) ->
@@ -30,21 +62,14 @@ get_originate_req(Data, Call) ->
                                 ], Data),
 
     OwnerId = wh_json:get_value(<<"owner_id">>, Data),
-    DeviceId = case whapps_call:call_id(Call) =:= SourceOfDTMF of
-                   'true' -> whapps_call:authorizing_id(Call);
-                   'false' -> find_device_id_for_leg(SourceOfDTMF)
-               end,
+    {DeviceId, TargetCallId} =
+        case whapps_call:call_id(Call) =:= SourceOfDTMF of
+            'true' -> {whapps_call:authorizing_id(Call), whapps_call:other_leg_call_id(Call)};
+            'false' -> {find_device_id_for_leg(SourceOfDTMF), whapps_call:call_id(Call)}
+        end,
 
-    DeviceIds = cf_attributes:owned_by(OwnerId, <<"device">>, Call),
     Endpoints = build_endpoints(DeviceId, OwnerId, Params, Call),
-    case lists:member(DeviceId, DeviceIds) of
-        'true' ->
-            lager:debug("filter a-leg devices, intercept b-leg"),
-            build_originate(Endpoints, whapps_call:other_leg_call_id(Call), Call);
-        'false' ->
-            lager:debug("filter b-leg devices, intercept a-leg"),
-            build_originate(Endpoints, whapps_call:call_id(Call), Call)
-    end.
+    build_originate(Endpoints, TargetCallId, Call).
 
 -spec build_endpoints(ne_binary(), ne_binary(), wh_json:object(), whapps_call:call()) -> wh_json:objects().
 build_endpoints(DeviceId, OwnerId, Params, Call) ->
@@ -66,12 +91,23 @@ build_endpoints(DeviceId, OwnerId, Params, Call) ->
 
 -spec build_originate(wh_json:objects(), ne_binary(), whapps_call:call()) -> wh_proplist().
 build_originate([], _CallId, _Call) -> [];
-build_originate(Endpoints, CallId, _Call) ->
+build_originate(Endpoints, CallId, Call) ->
+    lager:debug("targeting ~s for intercept", [CallId]),
     props:filter_undefined(
         [{<<"Application-Name">>, <<"bridge">>}
          ,{<<"Endpoints">>, Endpoints}
          ,{<<"Existing-Call-ID">>, CallId}
          ,{<<"Intercept-Unbridged-Only">>, 'false'}
+         ,{<<"Outbound-Caller-ID-Name">>, whapps_call:caller_id_name(Call)}
+         ,{<<"Outbound-Caller-ID-Number">>, whapps_call:caller_id_number(Call)}
+         ,{<<"Caller-ID-Name">>, whapps_call:caller_id_name(Call)}
+         ,{<<"Caller-ID-Number">>, whapps_call:caller_id_number(Call)}
+
+         ,{<<"Outbound-Callee-ID-Name">>, whapps_call:callee_id_name(Call)}
+         ,{<<"Outbound-Callee-ID-Number">>, whapps_call:callee_id_number(Call)}
+         ,{<<"Callee-ID-Name">>, whapps_call:callee_id_name(Call)}
+         ,{<<"Callee-ID-Number">>, whapps_call:callee_id_number(Call)}
+
          | wh_api:default_headers(?APP_NAME, ?APP_VERSION)
         ]
     ).
@@ -86,10 +122,20 @@ send_originate_req([], _Call) ->
 send_originate_req(OriginateProps, _Call) ->
     whapps_util:amqp_pool_collect(OriginateProps, fun wapi_resource:publish_originate_req/1, fun is_resp/1, 20000).
 
--spec is_resp(wh_json:objects()) -> {'ok', iolist()} |
-                                    {'error', string()}.
+-spec is_resp(wh_json:objects() | wh_json:object()) ->
+                     {'ok', iolist()} |
+                     {'error', string()}.
 is_resp([JObj|_]) ->
+    is_resp(JObj);
+is_resp(JObj) ->
     wapi_resource:originate_resp_v(JObj).
+
+-spec is_originate_uuid(wh_json:object(), api_binary()) -> boolean().
+is_originate_uuid(JObj, CallId) ->
+    wapi_resource:originate_uuid_v(JObj)
+        andalso (CallId =:= 'undefined'
+                 orelse CallId =:= wh_json:get_value(<<"Outbound-Call-ID">>, JObj)
+                ).
 
 -spec find_device_id_for_leg(ne_binary()) -> api_binary().
 find_device_id_for_leg(CallId) ->
