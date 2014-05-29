287fc95a3ba8fad46f8b7b34f0ce6f217436f86c
James Aimonetti <james@thinky64.2600hz.com>
2014-05-29 15:33:35 -0700

    KAZOO-2475: add Debug flag to API

--- applications/pivot/src/wapi_pivot.erl
+++ applications/pivot/src/wapi_pivot.erl
@@ -1,5 +1,5 @@
 %%%-------------------------------------------------------------------
-%%% @copyright (C) 2012, VoIP INC
+%%% @copyright (C) 2012-2014, 2600Hz INC
 %%% @doc
 %%% Pivot API
 %%% @end
@@ -20,19 +20,24 @@
 -define(KEY_PIVOT_REQ, <<"pivot.req">>).
 
 -define(PIVOT_REQ_HEADERS, [<<"Call">>, <<"Voice-URI">>]).
--define(OPTIONAL_PIVOT_REQ_HEADERS, [<<"CDR-URI">>, <<"Request-Format">>, <<"HTTP-Method">>
+-define(OPTIONAL_PIVOT_REQ_HEADERS, [<<"CDR-URI">>
+                                     ,<<"Request-Format">>
+                                     ,<<"HTTP-Method">>
+                                     ,<<"Debug">>
                                     ]).
 -define(PIVOT_REQ_VALUES, [{<<"Event-Category">>,<<"dialplan">>}
                            ,{<<"Event-Name">>, <<"pivot_req">>}
                           ]).
--define(PIVOT_REQ_TYPES, [{<<"Call">>, fun wh_json:is_json_object/1}]).
+-define(PIVOT_REQ_TYPES, [{<<"Call">>, fun wh_json:is_json_object/1}
+                          ,{<<"Debug">>, fun wh_util:is_boolean/1}
+                         ]).
 
 -spec req(api_terms()) -> {'ok', iolist()} |
-                                {'error', string()}.
+                          {'error', string()}.
 req(Prop) when is_list(Prop) ->
     case req_v(Prop) of
-        false -> {error, "Proplist failed validation for pivot_req"};
-        true -> wh_api:build_message(Prop, ?PIVOT_REQ_HEADERS, ?OPTIONAL_PIVOT_REQ_HEADERS)
+        'false' -> {'error', "Proplist failed validation for pivot_req"};
+        'true' -> wh_api:build_message(Prop, ?PIVOT_REQ_HEADERS, ?OPTIONAL_PIVOT_REQ_HEADERS)
     end;
 req(JObj) ->
     req(wh_json:to_proplist(JObj)).
@@ -43,14 +48,14 @@ req_v(Prop) when is_list(Prop) ->
 req_v(JObj) ->
     req_v(wh_json:to_proplist(JObj)).
 
--spec bind_q(ne_binary(), proplist()) -> 'ok'.
+-spec bind_q(ne_binary(), wh_proplist()) -> 'ok'.
 bind_q(Queue, Props) ->
-    Realm = props:get_value(realm, Props, <<"*">>),
+    Realm = props:get_value('realm', Props, <<"*">>),
     amqp_util:bind_q_to_callmgr(Queue, get_pivot_req_routing(Realm)).
 
--spec unbind_q(ne_binary(), proplist()) -> 'ok'.
+-spec unbind_q(ne_binary(), wh_proplist()) -> 'ok'.
 unbind_q(Queue, Props) ->
-    Realm = props:get_value(realm, Props, <<"*">>),
+    Realm = props:get_value('realm', Props, <<"*">>),
     amqp_util:unbind_q_from_callmgr(Queue, get_pivot_req_routing(Realm)).
 
 %%--------------------------------------------------------------------
@@ -61,18 +66,18 @@ unbind_q(Queue, Props) ->
 -spec declare_exchanges() -> 'ok'.
 declare_exchanges() ->
     amqp_util:callmgr_exchange().
-    
+
 get_pivot_req_routing(Realm) when is_binary(Realm) ->
     list_to_binary([?KEY_PIVOT_REQ, ".", amqp_util:encode(Realm)]);
 get_pivot_req_routing(Api) ->
     get_pivot_req_routing(get_from_realm(Api)).
 
 -spec publish_req(api_terms()) -> 'ok'.
--spec publish_req(api_terms(), binary()) -> 'ok'.
+-spec publish_req(api_terms(), ne_binary()) -> 'ok'.
 publish_req(JObj) ->
     publish_req(JObj, ?DEFAULT_CONTENT_TYPE).
 publish_req(Req, ContentType) ->
-    {ok, Payload} = wh_api:prepare_api_payload(Req, ?PIVOT_REQ_VALUES, fun req/1),
+    {'ok', Payload} = wh_api:prepare_api_payload(Req, ?PIVOT_REQ_VALUES, fun req/1),
     amqp_util:callmgr_publish(Payload, ContentType, get_pivot_req_routing(Req)).
 
 -spec get_from_realm(api_terms()) -> ne_binary().
