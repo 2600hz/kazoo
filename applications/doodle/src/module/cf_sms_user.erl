b8d760184ab956445c562a01b71a0cbb4e797534
lazedo <luis.azedo@factorlusitano.com>
2014-06-03 23:53:16 +0100

    cf_sms_resources cleaned up cf_sms_user

--- applications/doodle/src/module/cf_sms_user.erl
+++ applications/doodle/src/module/cf_sms_user.erl
@@ -26,12 +26,28 @@
 handle(Data, Call) ->
     UserId = wh_json:get_ne_value(<<"id">>, Data),
     Endpoints = get_endpoints(UserId, Data, Call),
-    
-    R = whapps_sms_command:b_send_sms(Endpoints, Call),
 
-    
-    doodle_exe:continue(Call).
+    case length(Endpoints) > 0
+        andalso whapps_sms_command:b_send_sms(Endpoints, Call)
+    of
+        'false' ->
+            lager:notice("user ~s has no endpoints", [UserId]),
+            doodle_exe:continue(Call);
+        {'ok', _} ->
+            lager:info("completed successful bridge to user"),
+            doodle_exe:stop(Call);
+        {'fail', _}=Reason -> maybe_handle_bridge_failure(Reason, Call);
+        {'error', _R} ->
+            lager:info("error bridging to user: ~p", [_R]),
+            doodle_exe:continue(Call)
+    end.
 
+-spec maybe_handle_bridge_failure(_, whapps_call:call()) -> 'ok'.
+maybe_handle_bridge_failure(Reason, Call) ->
+    case cf_util:handle_bridge_failure(Reason, Call) of
+        'not_found' -> cf_exe:continue(Call);
+        'ok' -> 'ok'
+    end.
 
 %%--------------------------------------------------------------------
 %% @private
@@ -40,11 +56,6 @@ handle(Data, Call) ->
 %% json object used in the bridge API
 %% @end
 %%--------------------------------------------------------------------
--spec is_resp(wh_json:objects()) -> {'ok', iolist()} |
-                                    {'error', string()}.
-is_resp([JObj|_Y]) ->
-    wapi_sms:delivery_v(JObj).
-
 
 -spec get_endpoints(api_binary(), wh_json:object(), whapps_call:call()) ->
                            wh_json:objects().
@@ -57,4 +68,3 @@ get_endpoints(UserId, Data, Call) ->
                             {'error', _E} -> Acc
                         end
                 end, [], cf_attributes:owned_by(UserId, <<"device">>, Call)).
-
