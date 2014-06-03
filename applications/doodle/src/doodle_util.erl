c1b727327fefeaa55da32178662daff7df36736d
lazedo <luis.azedo@factorlusitano.com>
2014-06-03 10:37:19 +0100

    utils

--- applications/doodle/src/doodle_util.erl
+++ applications/doodle/src/doodle_util.erl
@@ -26,26 +26,26 @@ create_sms(Call, Endpoints) ->
                    ]),
     
     [             
-               {<<"Message-ID">>, whapps_call:kvs_fetch(<<"Message-ID">>, Call)}
-               ,{<<"Call-ID">>, whapps_call:call_id(Call)}
-               ,{<<"Body">>, whapps_call:kvs_fetch(<<"Body">>, Call)}
-               ,{<<"From">>, whapps_call:from(Call)}
-               ,{<<"Caller-ID-Number">>, whapps_call:caller_id_number(Call)}
-               ,{<<"To">>, whapps_call:to(Call)}
-               ,{<<"Request">>, whapps_call:request(Call) }
-               ,{<<"Endpoints">>, Endpoints}
-               ,{<<"Application-Name">>, <<"send">>}
-               ,{<<"Custom-Channel-Vars">>, wh_json:set_values(CCVUpdates, wh_json:new())}
-                   | wh_api:default_headers(?APP_NAME, ?APP_VERSION)
-              ].    
+     {<<"Message-ID">>, whapps_call:kvs_fetch(<<"Message-ID">>, Call)}
+     ,{<<"Call-ID">>, whapps_call:call_id(Call)}
+     ,{<<"Body">>, whapps_call:kvs_fetch(<<"Body">>, Call)}
+     ,{<<"From">>, whapps_call:from(Call)}
+     ,{<<"Caller-ID-Number">>, whapps_call:caller_id_number(Call)}
+     ,{<<"To">>, whapps_call:to(Call)}
+     ,{<<"Request">>, whapps_call:request(Call) }
+     ,{<<"Endpoints">>, Endpoints}
+     ,{<<"Application-Name">>, <<"send">>}
+     ,{<<"Custom-Channel-Vars">>, wh_json:set_values(CCVUpdates, wh_json:new())}
+         | wh_api:default_headers(?APP_NAME, ?APP_VERSION)
+    ].    
 
 -spec save_sms(wh_json:object(), whapps_call:call()) -> 'ok'.
 save_sms(JObj, Call) ->
     AccountId = whapps_call:account_id(Call),
     CCVs = wh_json:get_value(<<"Custom-Channel-Vars">>, JObj),
-    OwnerId = wh_json:get_value(<<"Owner-ID">>, CCVs),
-    AuthType = wh_json:get_value(<<"Authorizing-Type">>, CCVs),
-    AuthId = wh_json:get_value(<<"Authorizing-ID">>, CCVs),
+    OwnerId = whapps_call:owner_id(Call),
+    AuthType = whapps_call:authorizing_type(Call),
+    AuthId = whapps_call:authorizing_id(Call),
     Body = wh_json:get_value(<<"Body">>, JObj),
     To = whapps_call:to(Call),
     From = whapps_call:from(Call),
@@ -55,29 +55,30 @@ save_sms(JObj, Call) ->
     [RequestUser, RequestRealm] = binary:split(Request, <<"@">>),
     MessageId = wh_json:get_value(<<"Message-ID">>, JObj),    
     
-    Doc = props:filter_undefined([
-           {<<"_id">>, whapps_call:call_id(Call)}
-          ,{<<"pvt_type">>, <<"sms">> }
-          ,{<<"account_id">>, AccountId }
-          ,{<<"owner_id">>, OwnerId }
-          ,{<<"authorization_type">>, AuthType }
-          ,{<<"authorization_id">>, AuthId }
-          ,{<<"to">>, To }
-          ,{<<"to_user">>, ToUser }
-          ,{<<"to_realm">>, ToRealm }
-          ,{<<"from">>, From }
-          ,{<<"from_user">>, FromUser }
-          ,{<<"from_realm">>, FromRealm }
-          ,{<<"request">>, Request }
-          ,{<<"request_user">>, RequestUser }
-          ,{<<"request_realm">>, RequestRealm }
-          ,{<<"body">>, Body }
-          ,{<<"message_id">>, MessageId}
-          ,{<<"pvt_created">>, wh_util:current_tstamp()}
-          ,{<<"status">>, <<"pending">>}
-          ,{<<"call_id">>, whapps_call:call_id(Call)}
-          ,{<<"pvt_call">>, whapps_call:to_json(Call)}                                 
-          ,{<<"pvt_json">>, JObj}                                 
-           ]),
+    Doc = props:filter_undefined(
+            [
+             {<<"_id">>, whapps_call:call_id(Call)}
+             ,{<<"pvt_type">>, <<"sms">> }
+             ,{<<"account_id">>, AccountId }
+             ,{<<"owner_id">>, OwnerId }
+             ,{<<"authorization_type">>, AuthType }
+             ,{<<"authorization_id">>, AuthId }
+             ,{<<"to">>, To }
+             ,{<<"to_user">>, ToUser }
+             ,{<<"to_realm">>, ToRealm }
+             ,{<<"from">>, From }
+             ,{<<"from_user">>, FromUser }
+             ,{<<"from_realm">>, FromRealm }
+             ,{<<"request">>, Request }
+             ,{<<"request_user">>, RequestUser }
+             ,{<<"request_realm">>, RequestRealm }
+             ,{<<"body">>, Body }
+             ,{<<"message_id">>, MessageId}
+             ,{<<"pvt_created">>, wh_util:current_tstamp()}
+             ,{<<"pvt_status">>, <<"pending">>}
+             ,{<<"call_id">>, whapps_call:call_id(Call)}
+             ,{<<"pvt_call">>, whapps_call:to_json(Call)}                                 
+             ,{<<"pvt_json">>, JObj}                                 
+            ]),
     {'ok', JObjSaved} = kazoo_modb:save_doc(AccountId, wh_json:from_list(Doc), []).
 
