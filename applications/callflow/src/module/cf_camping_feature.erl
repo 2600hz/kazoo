331321a4b2430a5292e21ff6ba5356d599050edf
SIPLABS, LLC <info@siplabs.ru>
2014-06-04 19:26:45 +0700

    make a call if target available now

--- applications/callflow/src/module/cf_camping_feature.erl
+++ applications/callflow/src/module/cf_camping_feature.erl
@@ -36,21 +36,23 @@
 handle(_Data, Call) ->
     lager:info("Camping feature started"),
     Number = whapps_call:kvs_fetch('cf_capture_group', Call),
-    Targets = get_sip_usernames_for_extension(Number, Call),
-    camper_channel_tracker:add_request(whapps_call:account_db(Call)
-                                       ,{whapps_call:authorizing_id(Call)
-                                         ,whapps_call:authorizing_type(Call)
-                                        }
-                                       ,Number
-                                       ,Targets
-                                      ),
-    cf_exe:stop(Call).
-
--spec get_sip_usernames_for_extension(ne_binary(), whapps_call:call()) -> wh_json:object().
-get_sip_usernames_for_extension(Number, Call) ->
     {'ok', Callflow, _} = cf_util:lookup_callflow(Number, whapps_call:account_id(Call)),
     TargetId = wh_json:get_ne_value([<<"flow">>, <<"data">>, <<"id">>], Callflow),
     TargetType = wh_json:get_ne_value([<<"flow">>, <<"module">>], Callflow),
+    Usernames = case TargetType of
+                   <<"device">> -> cf_util:sip_users_from_device_ids([TargetId], Call);
+                   <<"user">> ->
+                       EPs = cf_util:find_user_endpoints([TargetId], [], Call),
+                       cf_util:sip_users_from_device_ids(EPs, Call)
+               end,
+    Channels = cf_util:find_channels(Usernames, Call),
+    case Channels of
+        [] -> no_channels(TargetId, TargetType, Call);
+        _ -> has_channels(TargetId, TargetType, Number, Call)
+    end.
+
+-spec get_sip_usernames_for_target(ne_binary(), ne_binary(), whapps_call:call()) -> wh_json:object().
+get_sip_usernames_for_target(TargetId, TargetType, Call) ->
     Targets = case TargetType of
                   <<"user">> -> cf_attributes:owned_by(TargetId, <<"device">>, Call);
                   <<"device">> -> [TargetId];
@@ -65,3 +67,28 @@ get_sip_usernames_for_extension(Number, Call) ->
         end
         ,Targets
     ).
+
+-spec no_channels(ne_binary(), ne_binary(), whapps_call:call()) -> 'ok'.
+no_channels(TargetId, <<"device">>, Call) ->
+    Data = wh_json:from_list([{<<"id">>, TargetId}]),
+    cf_device:handle(Data, Call);
+no_channels(TargetId, <<"user">>, Call) ->
+    Data = wh_json:from_list([{<<"id">>, TargetId}]),
+    cf_user:handle(Data, Call);
+no_channels(_, Type, Call) ->
+    lager:info("Unknown target's type: ~s", [Type]),
+    cf_exe:stop(Call).
+
+-spec has_channels(ne_binary(), ne_binary(), ne_binary(), whapps_call:call()) -> 'ok'.
+has_channels(TargetId, TargetType, Number, Call) ->
+    Targets = get_sip_usernames_for_target(TargetId, TargetType, Call),
+    camper_channel_tracker:add_request(whapps_call:account_db(Call)
+        ,{whapps_call:authorizing_id(Call)
+            ,whapps_call:authorizing_type(Call)
+        }
+        ,Number
+        ,Targets
+    ),
+    whapps_call_command:answer(Call),
+    whapps_call_command:b_say(<<"request accepted">>, Call),
+    cf_exe:stop(Call).
