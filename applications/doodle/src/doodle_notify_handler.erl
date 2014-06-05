6abd89f689ccbe6624521cfa8b873e110e617ea3
lazedo <luis.azedo@factorlusitano.com>
2014-06-05 02:15:08 +0100

    handler revised

--- applications/doodle/src/doodle_notify_handler.erl
+++ applications/doodle/src/doodle_notify_handler.erl
@@ -13,8 +13,9 @@
 -include("doodle.hrl").
 
 -export([replay_sms/2]).
+-export([check_sms_by_device_id/2, check_sms_by_owner_id/2]).
 
--spec handle_req(wh_json:object(), wh_proplist()) -> 'ok'.
+-spec handle_req(wh_json:object(), wh_proplist()) -> any().
 handle_req(JObj, Props) ->
     'true' = wapi_notifications:register_v(JObj),
     _ = wh_util:put_callid(JObj),
@@ -22,43 +23,47 @@ handle_req(JObj, Props) ->
     Realm = wh_json:get_value(<<"Realm">>, JObj),    
     AccountId = wh_json:get_value(<<"Account-ID">>, JObj),
     DeviceId = wh_json:get_value(<<"Authorizing-ID">>, JObj),
-    check_sms_by_device(AccountId, DeviceId).
+    OwnerId = wh_json:get_value(<<"Owner-ID">>, JObj),
+    spawn(?MODULE, 'check_sms_by_device_id', [AccountId, DeviceId]),
+    spawn(?MODULE, 'check_sms_by_owner_id', [AccountId, OwnerId]).
     
-check_sms_by_device(AccountId, DeviceId) ->
-    ViewOptions = [{'key', [DeviceId]}],
+-spec check_sms_by_device_id(ne_binary(), ne_binary()) -> any().
+check_sms_by_device_id(AccountId, DeviceId) ->
+    ViewOptions = [{'key', DeviceId}],
     case kazoo_modb:get_results(AccountId, <<"sms/deliver_to_device">>, ViewOptions) of
-        {'ok', []} -> check_sms_by_owner(AccountId, cf_attributes:owner_id(DeviceId));
+        {'ok', []} ->
+            lager:debug("no sms for device ~s in account ~s", [DeviceId, AccountId]);
         {'ok', JObjs} ->
-            [ replay_sms(AccountId, wh_json:get_value(<<"_id">>, JObj) )
-             || JObj <- JObjs
-            ];
+            [replay_sms(AccountId, wh_json:get_value(<<"id">>, JObj) ) || JObj <- JObjs];
         {'error', _R} ->
-            lager:debug("unable to get sms by device for ~s/~s: ~p", [AccountId, DeviceId, _R]),
-            check_sms_by_owner(AccountId, cf_attributes:owner_id(DeviceId))
+            lager:debug("unable to get sms by device for ~s/~s: ~p", [AccountId, DeviceId, _R])
     end.
 
-check_sms_by_owner(AccountId, OwnerId) ->
-    ViewOptions = [{'key', [OwnerId]}],
+
+-spec check_sms_by_owner_id(ne_binary(), ne_binary()) -> any().
+check_sms_by_owner_id(AccountId, OwnerId) ->
+    ViewOptions = [{'key', OwnerId}],
     case kazoo_modb:get_results(AccountId, <<"sms/deliver_to_owner">>, ViewOptions) of
         {'ok', []} ->
-            lager:debug("no sms for owner_id ~s/~s",[AccountId, OwnerId]);
+            lager:debug("no sms for owner_id ~s in account ~s",[OwnerId, AccountId]);
         {'ok', JObjs} ->
-            [ replay_sms(AccountId, wh_json:get_value(<<"_id">>, JObj) )
-             || JObj <- JObjs
-            ];
+            [replay_sms(AccountId, wh_json:get_value(<<"id">>, JObj) ) || JObj <- JObjs];
         {'error', _R} ->
-            lager:debug("unable to get sms by owner_id for ~s/~s: ~p", [AccountId, OwnerId, _R])
+            lager:debug("unable to get sms by owner_id for owner_id ~s in account ~s: ~p", [AccountId, OwnerId, _R])
     end.
 
 -spec replay_sms(ne_binary(), ne_binary()) -> any().
 replay_sms(AccountId, DocId) ->
+    lager:debug("trying to replay sms ~s for account ~s",[DocId, AccountId]),
     {'ok', Doc} = kazoo_modb:open_doc(AccountId, DocId),
     Flow = wh_json:get_value(<<"pvt_call">>, Doc),
-    replay_sms_flow(Flow).
+    replay_sms_flow(AccountId, DocId, Flow).
 
-replay_sms_flow('undefined') -> 'ok';
-replay_sms_flow(JObj) ->
+replay_sms_flow(AccountId, DocId,'undefined') -> 'ok';
+replay_sms_flow(AccountId, DocId, JObj) ->
+    lager:debug("replaying sms ~s for account ~s",[DocId, AccountId]),
     Call = whapps_call:from_json(JObj),
+    whapps_call:set_account_id(AccountId, Call),
     whapps_call:put_callid(Call),
-    lager:info("received call resume, taking control"),
+    lager:info("doodle received sms resume for ~s of account ~s, taking control",[DocId, AccountId]),
     doodle_route_win:maybe_restrict_call(JObj, Call).
