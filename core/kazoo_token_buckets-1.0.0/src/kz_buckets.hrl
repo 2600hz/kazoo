51d7aa31bcd0040b5f4ab21ea911f0a4a33d59e3
James Aimonetti <james@thinky64.2600hz.com>
2014-06-05 15:18:29 -0700

    KAZOO-2538: add inactivity config value

--- core/kazoo_token_buckets-1.0.0/src/kz_buckets.hrl
+++ core/kazoo_token_buckets-1.0.0/src/kz_buckets.hrl
@@ -3,5 +3,11 @@
 -include_lib("whistle/include/wh_log.hrl").
 -include_lib("whistle/include/wh_databases.hrl").
 
+-define(INACTIVITY_TIMEOUT_MS
+        ,whapps_config:get_integer(<<"token_buckets">>, <<"inactivity_timeout_s">>, ?SECONDS_IN_MINUTE * 10)
+        * ?MILLISECONDS_IN_SECOND
+       ).
+-define(INACTIVITY_MSG, 'inactivity_timeout').
+
 -define(KZ_BUCKETS_HRL, 'true').
 -endif.
