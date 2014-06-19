3262d2416506c1712a81f7e496c8bb37c05c0bde
James Aimonetti <james@thinky64.2600hz.com>
2014-06-19 16:15:53 -0700

    3.12: upgrade cowboy to 0.10.0 and refresh Crossbar

---
+++ deps/ranch-0.10.0/guide/toc.md
@@ -0,0 +1,25 @@
+Ranch User Guide
+================
+
+The Ranch User Guide explores how to make best use of Ranch
+for writing powerful TCP applications.
+
+Introducing Ranch
+-----------------
+
+ *  [Introduction](introduction.md)
+
+Using Ranch
+-----------
+
+ *  [Listeners](listeners.md)
+ *  [Transports](transports.md)
+ *  [Protocols](protocols.md)
+ *  [Writing parsers](parsers.md)
+
+Advanced topics
+---------------
+
+ *  [SSL client authentication](ssl_auth.md)
+ *  [Embedded mode](embedded.md)
+ *  [Internals](internals.md)
---
+++ deps/ranch-0.10.0/src/ranch_acceptors_sup.erl
@@ -0,0 +1,42 @@
+%% Copyright (c) 2011-2014, Loïc Hoguin <essen@ninenines.eu>
+%%
+%% Permission to use, copy, modify, and/or distribute this software for any
+%% purpose with or without fee is hereby granted, provided that the above
+%% copyright notice and this permission notice appear in all copies.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+%% WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+%% MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+%% ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+%% WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+%% ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+%% OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+
+-module(ranch_acceptors_sup).
+-behaviour(supervisor).
+
+-export([start_link/4]).
+-export([init/1]).
+
+-spec start_link(ranch:ref(), non_neg_integer(), module(), any())
+	-> {ok, pid()}.
+start_link(Ref, NbAcceptors, Transport, TransOpts) ->
+	supervisor:start_link(?MODULE, [Ref, NbAcceptors, Transport, TransOpts]).
+
+init([Ref, NbAcceptors, Transport, TransOpts]) ->
+	ConnsSup = ranch_server:get_connections_sup(Ref),
+	LSocket = case proplists:get_value(socket, TransOpts) of
+		undefined ->
+			{ok, Socket} = Transport:listen(TransOpts),
+			Socket;
+		Socket ->
+			Socket
+	end,
+	{ok, {_, Port}} = Transport:sockname(LSocket),
+	ranch_server:set_port(Ref, Port),
+	Procs = [
+		{{acceptor, self(), N}, {ranch_acceptor, start_link, [
+			LSocket, Transport, ConnsSup
+		]}, permanent, brutal_kill, worker, []}
+			|| N <- lists:seq(1, NbAcceptors)],
+	{ok, {{one_for_one, 10, 10}, Procs}}.
