1feadf4e001a164a0950b0512f796f1ee1286572
Luis Azedo <luis.azedo@factorlusitano.com>
2014-06-20 18:21:57 +0100

    KAZOO-2646 - delete unwanted line

--- applications/callflow/src/module/cf_ring_group.erl
+++ applications/callflow/src/module/cf_ring_group.erl
@@ -36,7 +36,6 @@ attempt_endpoints(Endpoints, Data, Call) ->
     Strategy = wh_json:get_binary_value(<<"strategy">>, Data, <<"simultaneous">>),
     Ringback = wh_json:get_value(<<"ringback">>, Data),
     lager:info("attempting ring group of ~b members with strategy ~s", [length(Endpoints), Strategy]),
-    whapps_call_command:b_answer(Call),
     case whapps_call_command:b_bridge(Endpoints, Timeout, Strategy, <<"true">>, Ringback, Call) of
         {'ok', _} ->
             lager:info("completed successful bridge to the ring group - call finished normally"),
