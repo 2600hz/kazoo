1e4c8877bf5c82d7ffc07c10c3dfb8850344e45d
James Aimonetti <james@thinky64.2600hz.com>
2014-06-26 16:57:18 -0700

    KAZOO-2659: import prompts from a directory or import a specific file

--- core/whistle_media-1.0.0/src/whistle_media_maintenance.erl
+++ core/whistle_media-1.0.0/src/whistle_media_maintenance.erl
@@ -8,7 +8,9 @@
 -module(whistle_media_maintenance).
 
 -export([remove_empty_media_docs/1
-         ,migrate/0
+         ,migrate/0, migrate_prompts/0
+         ,import_prompts/1, import_prompts/2
+         ,import_prompt/1, import_prompt/2
          ,refresh/0
         ]).
 
@@ -24,18 +26,122 @@ migrate() ->
     migrate_system_config(<<"media_mgr">>),
     'no_return'.
 
+-spec migrate_prompts() -> 'no_return'.
+migrate_prompts() ->
+    io:format("Now updating existing system_media docs to be internationalizable!~n", []),
+    'no_return'.
+
+import_prompts(Path) ->
+    import_prompts(Path, wh_media_util:default_prompt_language()).
+
+import_prompts(Path, Lang) ->
+    case filelib:wildcard([Path, "/*.{wav,mp3}"]) of
+        [] ->
+            io:format("failed to find media files in '~s'~n", [Path]);
+        Files ->
+            io:format("importing prompts from '~s' with language '~s'~n", [Path, Lang]),
+            case [{F, Err}
+                  || F <- Files,
+                     (Err = (catch import_prompt(F, Lang))) =/= 'ok'
+                 ]
+            of
+                [] -> io:format("importing went successfully~n", []);
+                Errors ->
+                    io:format("errors encountered during import:~n"),
+                    [io:format("  '~s': ~p~n", [F, Err]) || {F, Err} <- Errors],
+                    'ok'
+            end
+    end.
+
+-spec import_prompt(text()) -> 'ok' | {'error', _}.
+-spec import_prompt(text(), text()) -> 'ok' | {'error', _}.
+-spec import_prompt(text(), text(), ne_binary()) -> 'ok' | {'error', _}.
+
+import_prompt(Path) ->
+    import_prompt(Path, wh_media_util:default_prompt_language()).
+
+import_prompt(Path, Lang) ->
+    case file:read_file(Path) of
+        {'ok', Contents} ->
+            io:format("importing prompt '~s' with language '~s'~n", [Path, Lang]),
+            import_prompt(Path, Lang, Contents);
+        {'error', E} ->
+            io:format("failed to open path '~s' for importing: ~p~n", [Path, E]),
+            {'error', E}
+    end.
+
+import_prompt(Path0, Lang0, Contents) ->
+    Lang = wh_util:to_binary(Lang0),
+    Path = wh_util:to_binary(Path0),
+
+    Extension = filename:extension(Path),
+    PromptName = wh_util:to_binary(filename:basename(Path, Extension)),
+
+    {Category, Type, _} = cow_mimetypes:all(Path),
+
+    ContentLength = byte_size(Contents),
+
+    ID = cow_qs:urlencode(<<Lang/binary, "/", PromptName/binary>>),
+
+    io:format("  importing as '~s'~n", [ID]),
+
+    Now = wh_util:current_tstamp(),
+
+    MetaJObj = wh_json:from_list(
+                 [{<<"_id">>, ID}
+                  ,{<<"name">>, PromptName}
+                  ,{<<"description">>, <<"System prompt in ", Lang/binary, " for ", PromptName/binary>>}
+                  ,{<<"content_length">>, ContentLength}
+                  ,{<<"language">>, wh_util:to_lower_binary(Lang)}
+                  ,{<<"content_type">>, <<Category/binary, "/", Type/binary>>}
+                  ,{<<"source_type">>, ?MODULE}
+                  ,{<<"streamable">>, 'true'}
+                  ,{<<"pvt_type">>, <<"media">>}
+                  ,{<<"pvt_created">>, Now}
+                  ,{<<"pvt_modified">>, Now}
+                 ]),
+
+    case couch_mgr:ensure_saved(?WH_MEDIA_DB, MetaJObj) of
+        {'ok', _MetaJObj1} ->
+            io:format("  saved metadata about '~s'~n", [Path]),
+            upload_prompt(ID, PromptName, Contents);
+        {'error', E} ->
+            io:format("  error saving metadata: ~p~n", [E]),
+            {'error', E}
+    end.
+
+-spec upload_prompt(ne_binary(), ne_binary(), ne_binary()) -> 'ok' | {'error', _}.
+upload_prompt(ID, AttachmentName, Contents) ->
+    case couch_mgr:put_attachment(?WH_MEDIA_DB, ID, AttachmentName, Contents) of
+        {'ok', _MetaJObj} ->
+            io:format("  uploaded prompt binary to ~s as ~s~n", [ID, AttachmentName]);
+        {'error', E} ->
+            io:format("  error uploading prompt binary: ~p~n", [E]),
+            io:format("  deleting metadata from ~s~n", [?WH_MEDIA_DB]),
+            case couch_mgr:del_doc(?WH_MEDIA_DB, ID) of
+                {'ok', _} ->
+                    io:format("  removed metadata for ~s~n", [ID]),
+                    {'error', E};
+                {'error', E1} ->
+                    io:format("  failed to remove metadata for ~s: ~p~n", [ID, E1]),
+                    {'error', E1}
+            end
+    end.
+
 -spec refresh() -> 'ok'.
 refresh() ->
     couch_mgr:revise_doc_from_file(?WH_MEDIA_DB, 'crossbar', "account/media.json"),
     'ok'.
 
+-spec migrate_system_config(ne_binary()) -> 'ok'.
 migrate_system_config(ConfigId) ->
     {'ok', ConfigJObj} = couch_mgr:open_doc(?WH_CONFIG_DB, ConfigId),
     {'ok', MediaJObj} = get_media_config_doc(),
 
     UpdatedMediaJObj = wh_json:foldl(fun migrate_system_config_fold/3, MediaJObj, ConfigJObj),
     io:format("saving updated media config~n", []),
-    couch_mgr:save_doc(?WH_CONFIG_DB, UpdatedMediaJObj).
+    {'ok', _} = couch_mgr:save_doc(?WH_CONFIG_DB, UpdatedMediaJObj),
+    'ok'.
 
 -spec get_media_config_doc() -> {'ok', wh_json:object()}.
 get_media_config_doc() ->
