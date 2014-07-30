f30d6feaadd14656d2fd3f9756461290c093f062
James Aimonetti <james@thinky64.2600hz.com>
2014-07-30 14:25:41 -0700

    KAZOO-2753: add endpoint for channels

---
+++ applications/crossbar/src/modules/cb_channels.erl
@@ -0,0 +1,152 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2011-2014, 2600Hz INC
+%%% @doc
+%%%
+%%% Listing of all expected v1 callbacks
+%%%
+%%% @end
+%%% @contributors:
+%%%   Karl Anderson
+%%%   James Aimonetti
+%%%-------------------------------------------------------------------
+-module(cb_channels).
+
+-export([init/0
+         ,allowed_methods/0, allowed_methods/1
+         ,resource_exists/0, resource_exists/1
+         ,content_types_provided/1
+         ,validate/1, validate/2
+         ,post/2
+        ]).
+
+-include("../crossbar.hrl").
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Initializes the bindings this module will respond to.
+%% @end
+%%--------------------------------------------------------------------
+-spec init() -> 'ok'.
+init() ->
+    cb_modules_util:bind(?MODULE
+                         ,[{<<"*.allowed_methods.channels">>, 'allowed_methods'}
+                           ,{<<"*.resource_exists.channels">>, 'resource_exists'}
+                           ,{<<"*.content_types_provided.channels">>, 'content_types_provided'}
+                           ,{<<"*.validate.channels">>, 'validate'}
+                           ,{<<"*.execute.post.channels">>, 'post'}
+                          ]).
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Given the path tokens related to this module, what HTTP methods are
+%% going to be responded to.
+%% @end
+%%--------------------------------------------------------------------
+-spec allowed_methods() -> http_methods().
+-spec allowed_methods(path_token()) -> http_methods().
+allowed_methods() ->
+    [?HTTP_GET].
+allowed_methods(_) ->
+    [?HTTP_GET, ?HTTP_POST].
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Does the path point to a valid resource
+%% So /channels => []
+%%    /channels/foo => [<<"foo">>]
+%%    /channels/foo/bar => [<<"foo">>, <<"bar">>]
+%% @end
+%%--------------------------------------------------------------------
+-spec resource_exists() -> 'true'.
+-spec resource_exists(path_token()) -> 'true'.
+resource_exists() -> 'true'.
+resource_exists(_) -> 'true'.
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% What content-types will the module be using to respond (matched against
+%% client's accept header)
+%% Of the form {atom, [{Type, SubType}]} :: {to_json, [{<<"application">>, <<"json">>}]}
+%% @end
+%%--------------------------------------------------------------------
+-spec content_types_provided(cb_context:context()) -> cb_context:context().
+content_types_provided(Context) ->
+    Context.
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Check the request (request body, query string params, path tokens, etc)
+%% and load necessary information.
+%% /channels mights load a list of channel objects
+%% /channels/123 might load the channel object 123
+%% Generally, use crossbar_doc to manipulate the cb_context{} record
+%% @end
+%%--------------------------------------------------------------------
+-spec validate(cb_context:context()) -> cb_context:context().
+-spec validate(cb_context:context(), path_token()) -> cb_context:context().
+validate(Context) ->
+    validate_channels(Context, cb_context:req_verb(Context)).
+validate(Context, Id) ->
+    validate_channel(Context, Id, cb_context:req_verb(Context)).
+
+-spec validate_channels(cb_context:context(), http_method()) -> cb_context:context().
+validate_channels(Context, ?HTTP_GET) ->
+    summary(Context).
+
+-spec validate_channel(cb_context:context(), path_token(), http_method()) -> cb_context:context().
+validate_channel(Context, Id, ?HTTP_GET) ->
+    read(Id, Context);
+validate_channel(Context, Id, ?HTTP_POST) ->
+    update(Id, Context).
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% If the HTTP verb is POST, execute the actual action, usually a db save
+%% (after a merge perhaps).
+%% @end
+%%--------------------------------------------------------------------
+-spec post(cb_context:context(), path_token()) -> cb_context:context().
+post(Context, _) ->
+    crossbar_doc:save(Context).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Load an instance from the database
+%% @end
+%%--------------------------------------------------------------------
+-spec read(ne_binary(), cb_context:context()) -> cb_context:context().
+read(Id, Context) ->
+    crossbar_doc:load(Id, Context).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Update an existing menu document with the data provided, if it is
+%% valid
+%% @end
+%%--------------------------------------------------------------------
+-spec update(ne_binary(), cb_context:context()) -> cb_context:context().
+update(_Id, Context) ->
+    Context.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Attempt to load a summarized listing of all instances of this
+%% resource.
+%% @end
+%%--------------------------------------------------------------------
+-spec summary(cb_context:context()) -> cb_context:context().
+summary(Context) ->
+    Context.
