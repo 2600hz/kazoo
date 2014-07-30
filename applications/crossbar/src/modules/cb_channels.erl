744eefe82381ff25309b669d552f5c3ec17f8915
James Aimonetti <james@thinky64.2600hz.com>
2014-07-30 14:46:29 -0700

    KAZOO-2753: query channels for account/user

--- applications/crossbar/src/modules/cb_channels.erl
+++ applications/crossbar/src/modules/cb_channels.erl
@@ -149,4 +149,94 @@ update(_Id, Context) ->
 %%--------------------------------------------------------------------
 -spec summary(cb_context:context()) -> cb_context:context().
 summary(Context) ->
-    Context.
+    case cb_context:req_nouns(Context) of
+        [{<<"channels">>, []}, {<<"users">>, [UserId]} | _] ->
+            lager:debug("getting user summary for ~s", [UserId]),
+            user_summary(Context, UserId);
+        [{<<"channels">>, []}, {<<"accounts">>, [_AccountId]} | _] ->
+            lager:debug("getting account summary"),
+            account_summary(Context);
+        _Nouns ->
+            lager:debug("unexpected nouns: ~p", [_Nouns]),
+            Context
+    end.
+
+user_summary(Context, UserId) ->
+    Options = [{'key', [UserId, <<"device">>]}
+               ,'include_docs'
+              ],
+    %% TODO: Using the cf_attributes from crossbar isn't exactly kosher
+    Context1 = crossbar_doc:load_view(<<"cf_attributes/owned">>, Options, Context),
+    case cb_context:has_errors(Context1) of
+        'true' -> Context1;
+        'false' -> get_channels(Context1, cb_context:doc(Context1), fun wapi_call:publish_query_user_channels_req/1)
+    end.
+
+account_summary(Context) ->
+    get_channels(Context, [], fun wapi_call:publish_query_account_channels_req/1).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
+-spec get_channels(cb_context:context(), wh_json:objects(), function()) -> cb_context:context().
+get_channels(Context, Devices, PublisherFun) ->
+    Realm = crossbar_util:get_account_realm(cb_context:account_id(Context)),
+    Usernames = [Username
+                 || JObj <- Devices,
+                    (Username = wh_json:get_value([<<"doc">>
+                                                   ,<<"sip">>
+                                                   ,<<"username">>
+                                                  ], JObj))
+                        =/= 'undefined'
+                ],
+    Req = [{<<"Realm">>, Realm}
+           ,{<<"Usernames">>, Usernames}
+           ,{<<"Account-ID">>, cb_context:account_id(Context)}
+           | wh_api:default_headers(?APP_NAME, ?APP_VERSION)
+          ],
+    case whapps_util:amqp_pool_collect(Req
+                                       ,PublisherFun
+                                       ,{'ecallmgr', 'true'}
+                                      )
+    of
+        {'error', _R} ->
+            lager:error("could not reach ecallmgr channels: ~p", [_R]),
+            crossbar_util:response('error', <<"could not reach ecallmgr channels">>, Context);
+        {_, Resp} ->
+            Channels = merge_user_channels_jobjs(Resp),
+            crossbar_util:response(Channels, Context)
+    end.
+
+-spec merge_user_channels_jobjs(wh_json:objects()) -> wh_json:objects().
+merge_user_channels_jobjs(JObjs) ->
+    merge_user_channels_jobjs(JObjs, dict:new()).
+
+-spec merge_user_channels_jobjs(wh_json:objects(), dict()) -> wh_json:objects().
+merge_user_channels_jobjs([], Dict) ->
+    [delete_keys(Channel) || {_, Channel} <- dict:to_list(Dict)];
+merge_user_channels_jobjs([JObj|JObjs], Dict) ->
+    merge_user_channels_jobjs(JObjs, merge_user_channels_jobj(JObj, Dict)).
+
+-spec merge_user_channels_jobj(wh_json:object(), dict()) -> dict().
+merge_user_channels_jobj(JObj, Dict) ->
+    lists:foldl(fun merge_user_channels_fold/2, Dict, wh_json:get_value(<<"Channels">>, JObj, [])).
+
+-spec merge_user_channels_fold(wh_json:object(), dict()) -> dict().
+merge_user_channels_fold(Channel, D) ->
+    UUID = wh_json:get_value(<<"uuid">>, Channel),
+    dict:store(UUID, Channel, D).
+
+-spec delete_keys(wh_json:object()) -> wh_json:object().
+delete_keys(JObj) ->
+    wh_json:delete_keys([<<"account_id">>
+                         ,<<"bridge_id">>
+                         ,<<"context">>
+                         ,<<"dialplan">>
+                         ,<<"handling_locally">>
+                         ,<<"node">>
+                         ,<<"precedence">>
+                         ,<<"profile">>
+                         ,<<"realm">>
+                        ], JObj).
