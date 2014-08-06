8cae5ab1f0fc446d5f5e84f1c78ebc42cc4f9f32
James Aimonetti <james@thinky64.2600hz.com>
2014-08-06 16:04:03 -0700

    KAZOO-2753: add number_builder for resume

--- applications/konami/src/module/konami_resume.erl
+++ applications/konami/src/module/konami_resume.erl
@@ -10,7 +10,9 @@
 %%%-------------------------------------------------------------------
 -module(konami_resume).
 
--export([handle/2]).
+-export([handle/2
+         ,number_builder/1
+        ]).
 
 -include("../konami.hrl").
 
@@ -22,3 +24,41 @@ handle(_Data, Call) ->
                                           ]),
     whapps_call_command:pickup(whapps_call:other_leg_call_id(Call), <<"now">>, Call),
     {'continue', Call}.
+
+-spec number_builder(wh_json:object()) -> wh_json:object().
+number_builder(DefaultJObj) ->
+    io:format("Let's configure a 'resume' metaflow~n", []),
+
+    {'ok', [Number]} = io:fread("What number should invoke 'resume'? ", "~d"),
+
+    K = [<<"numbers">>, wh_util:to_binary(Number)],
+
+    case number_builder_check(wh_json:get_value(K, DefaultJObj)) of
+        'undefined' -> wh_json:delete_key(K, DefaultJObj);
+        NumberJObj -> wh_json:set_value(K, NumberJObj, DefaultJObj)
+    end.
+
+-spec number_builder_check(api_object()) -> api_object().
+number_builder_check('undefined') ->
+    metaflow_jobj(wh_json:new());
+number_builder_check(NumberJObj) ->
+    io:format("  Existing config for this number: ~s~n", [wh_json:encode(NumberJObj)]),
+    io:format("  e. Edit Number~n", []),
+    io:format("  d. Delete Number~n", []),
+    {'ok', [Option]} = io:fread("What would you like to do: ", "~s"),
+    number_builder_check_option(NumberJObj, Option).
+
+-spec number_builder_check_option(wh_json:object(), string()) -> api_object().
+number_builder_check_option(NumberJObj, "e") ->
+    metaflow_jobj(NumberJObj);
+number_builder_check_option(_NumberJObj, "d") ->
+    'undefined';
+number_builder_check_option(NumberJObj, _Option) ->
+    io:format("invalid selection~n", []),
+    number_builder_check(NumberJObj).
+
+-spec metaflow_jobj(wh_json:object()) -> wh_json:object().
+metaflow_jobj(NumberJObj) ->
+    wh_json:set_values([{<<"module">>, <<"resume">>}
+                        ,{<<"data">>, wh_json:new()}
+                       ], NumberJObj).
