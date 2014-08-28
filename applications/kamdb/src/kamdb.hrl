7cf372524ba1d1795a30d553fe58fffba3aecfd3
SIPLABS, LLC <info@siplabs.ru>
2014-08-28 15:51:20 +0700

    empty kamdb app

---
+++ applications/kamdb/src/kamdb.hrl
@@ -0,0 +1,10 @@
+-ifndef(KAMDB_HRL).
+-include_lib("whistle/include/wh_types.hrl").
+-include_lib("whistle/include/wh_log.hrl").
+-include_lib("whistle/include/wh_databases.hrl").
+
+-define(APP_NAME, <<"kamdb">>).
+-define(APP_VERSION, <<"0.0.1">> ).
+
+-define(KAMDB_HRL, 'true').
+-endif.
