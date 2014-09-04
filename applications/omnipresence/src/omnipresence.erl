58aead868f3320b5a3f0f2e5d9aab1074a0775b6
bitbashing <bitbashing@gmail.com>
2014-09-04 15:41:11 -0700

    KAZOO-2845: remove dead code

--- applications/omnipresence/src/omnipresence.erl
+++ applications/omnipresence/src/omnipresence.erl
@@ -74,5 +74,4 @@ declare_exchanges() ->
     _ = wapi_call:declare_exchanges(),
     _ = wapi_notifications:declare_exchanges(),
     _ = wapi_presence:declare_exchanges(),
-%   _ = wapi_omnipresence:declare_exchanges(),
     wapi_self:declare_exchanges().
