3ff90273aef8345254164809c23ab8794cbff196
SIPLABS, LLC <info@siplabs.ru>
2014-09-19 19:37:06 +0700

    fix for re-DELETE case

--- applications/crossbar/src/modules/cb_faxboxes.erl
+++ applications/crossbar/src/modules/cb_faxboxes.erl
@@ -208,7 +208,11 @@ delete_faxbox(Id, Context) ->
 -spec read(ne_binary(), cb_context:context()) -> cb_context:context().
 read(Id, Context) ->
     Ctx1 = crossbar_doc:load(Id, Context),
-    cb_context:set_resp_data(Ctx1, wh_doc:public_fields(leak_private_fields(cb_context:doc(Ctx1)))).
+    Doc = cb_context:doc(Ctx1),
+    case Doc of
+        'undefined' -> Ctx1;
+        _ -> cb_context:set_resp_data(Ctx1, wh_doc:public_fields(leak_private_fields(Doc)))
+    end.
 
 -spec leak_private_fields(wh_json:object()) -> wh_json:object().
 leak_private_fields(JObj) ->
