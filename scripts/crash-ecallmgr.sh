a85c013119291d8c52e8c0e930fcab111db15091
karl anderson <karl@2600hz.com>
2014-09-23 19:57:59 -0400

    HELP-12896: scripts to monitor and generate core-dumps when necessary

---
+++ scripts/crash-ecallmgr.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+cd `dirname $0`
+
+NODE="ecallmgr"
+HOSTNAME=$(/bin/hostname)
+REMOTE_SHELL="${NODE}@${HOSTNAME}"
+SUP="../utils/sup/sup"
+
+if [ -z "${1}" ]; then
+    ERL_COOKIE=`${SUP} -n ${NODE} erlang get_cookie | sed "s/'//g"`
+else
+	ERL_COOKIE="${1}"
+fi
+
+erl_call -c ${ERL_COOKIE} -name ${REMOTE_SHELL} -a 'erlang halt ["admin-request"]'
