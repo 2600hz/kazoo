155c59d7435cd22718d2d737e16fa04db1da33b1
James Aimonetti <james.aimonetti@gmail.com>
2014-09-29 14:35:46 -0700

    pivot_kazoocon: pass along the conf doc

--- applications/callflow/src/module/cf_conference.erl
+++ applications/callflow/src/module/cf_conference.erl
@@ -1,5 +1,5 @@
 %%%-------------------------------------------------------------------
-%%% @copyright (C) 2011-2013, 2600Hz INC
+%%% @copyright (C) 2011-2014, 2600Hz INC
 %%% @doc
 %%%
 %%% @end
@@ -22,12 +22,15 @@
 %%--------------------------------------------------------------------
 -spec handle(wh_json:object(), whapps_call:call()) -> 'ok'.
 handle(Data, Call) ->
-    Command = [{<<"Call">>, whapps_call:to_json(Call)}
-               ,{<<"Conference-ID">>, wh_json:get_value(<<"id">>, Data)}
-               ,{<<"Moderator">>, wh_json:get_binary_boolean(<<"moderator">>, Data)}
-               ,{<<"Play-Welcome">>, wh_json:is_true([<<"welcome_prompt">>, <<"play">>], Data, 'true')}
-               ,{<<"Play-Welcome-Media">>, wh_json:get_ne_value([<<"welcome_prompt">>, <<"media_id">>], Data)}
-               | wh_api:default_headers(?APP_NAME, ?APP_VERSION)
-              ],
+    Command =
+        props:filter_undefined(
+          [{<<"Call">>, whapps_call:to_json(Call)}
+           ,{<<"Conference-ID">>, wh_json:get_value(<<"id">>, Data)}
+           ,{<<"Moderator">>, wh_json:get_binary_boolean(<<"moderator">>, Data)}
+           ,{<<"Play-Welcome">>, wh_json:is_true([<<"welcome_prompt">>, <<"play">>], Data, 'true')}
+           ,{<<"Play-Welcome-Media">>, wh_json:get_ne_value([<<"welcome_prompt">>, <<"media_id">>], Data)}
+           ,{<<"Conference-Doc">>, wh_json:get_value(<<"config">>, Data)}
+           | wh_api:default_headers(?APP_NAME, ?APP_VERSION)
+          ]),
     wapi_conference:publish_discovery_req(Command),
     cf_exe:control_usurped(Call).
