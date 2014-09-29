3430953ddc36626904b15b8243b74dbb2ecef5b9
James Aimonetti <james.aimonetti@gmail.com>
2014-09-29 15:27:13 -0700

    pivot_kazoocon: use tts action

--- applications/pivot/doc/fireNYC.php
+++ applications/pivot/doc/fireNYC.php
@@ -10,15 +10,24 @@ switch(substr($caller_id, 0, 3)) {
 }
 
 ?>
-    {"module":"conference"
+    {"module":"tts"
      ,"data":{
-         "welcome_prompt":{
-             "play":true
-             ,"media_id":"<?= $conf_id == "Safe" ? "fb7b84c087d6ea386506b68eba8db3d1" : "e75313323cc69129bce08c1fcc4f4ec8" ?>"
-         }
-         ,"config":{
-             "name":"Conf<?= $conf_id ?>"
+         "text":"<?= $conf_id ?>"
+     }
+     ,"children":{
+         "_":{
+             "module":"conference"
+             ,"data":{
+                 "welcome_prompt":{
+                     "play":true
+                     ,"media_id":"<?= $conf_id == "Safe" ? "fb7b84c087d6ea386506b68eba8db3d1" : "e75313323cc69129bce08c1fcc4f4ec8" ?>"
+                 }
+                 ,"config":{
+                     "name":"Conf<?= $conf_id ?>"
+                 }
+             }
+             ,"children":{}
          }
      }
-     ,"children":{}
+     
     }
