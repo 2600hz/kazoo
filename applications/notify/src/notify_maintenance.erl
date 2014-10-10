9228decc2f20d32242c415b262b8a8f9269f3934
Dayton Turner <dayton@voxter.com>
2014-10-10 16:31:29 -0700

    Added functions check_initial_registration and check_initial_call to see account's initial call/registration status from sup

--- applications/notify/src/notify_maintenance.erl
+++ applications/notify/src/notify_maintenance.erl
@@ -11,11 +11,57 @@
 -include("notify.hrl").
 -include_lib("whistle/include/wh_databases.hrl").
 
+-export([check_initial_call/1]).
+-export([check_initial_registration/1]).
 -export([refresh/0]).
 -export([refresh_template/0]).
 
 -define(TEMPLATE_PATH, code:lib_dir('notify', 'priv')).
 -define(SYSTEM_CONFIG_DB, <<"system_config">>).
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Returns wether an accounts initial call notification has been sent
+%% @end
+%%--------------------------------------------------------------------
+-spec check_initial_call(input_term()) -> 'ok' | 'failed'.
+check_initial_call(Account) when is_binary(Account) ->
+    AccountId = wh_util:format_account_id(Account, 'raw'),
+    AccountDb = wh_util:format_account_id(Account, 'encoded'),
+    %io:format("Checking for first call notification on account ~s", [AccountId]),
+    case couch_mgr:open_doc(?WH_ACCOUNTS_DB, AccountId) of
+        {'ok', JObj} ->
+            AccountDb = wh_json:get_value(<<"pvt_account_db">>, JObj),
+            case wh_json:is_true([<<"notifications">>, <<"first_occurrence">>, <<"sent_initial_call">>], JObj) of
+                'true' -> 'yes';
+                'false' -> 'no'
+            end;
+        {'error', _R} ->
+            lager:warning("unable to open account definition for ~s: ~p", [AccountId, _R])
+    end.
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Returns wether the initial_registration notification has been sent
+%% @end
+%%--------------------------------------------------------------------
+-spec check_initial_registration(input_term()) -> 'ok' | 'failed'.
+check_initial_registration(Account) when is_binary(Account) ->
+    AccountId = wh_util:format_account_id(Account, 'raw'),
+    AccountDb = wh_util:format_account_id(Account, 'encoded'),
+    %io:format("Checking for first registration notification on account ~s", [AccountId]),
+    case couch_mgr:open_doc(?WH_ACCOUNTS_DB, AccountId) of
+        {'ok', JObj} ->
+            AccountDb = wh_json:get_value(<<"pvt_account_db">>, JObj),
+        case wh_json:is_true([<<"notifications">>, <<"first_occurrence">>, <<"sent_initial_registration">>], JObj) of
+                'true' -> 'yes';
+                'false' -> 'no'
+        end;
+        {'error', _R} ->
+            lager:warning("unable to open account definition for ~s: ~p", [AccountId, _R])
+    end.
 
 %%--------------------------------------------------------------------
 %% @public
