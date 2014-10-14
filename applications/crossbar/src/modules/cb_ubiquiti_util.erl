dbc7acae9f24bdc525f6f00f08be2c39fa5d111a
James Aimonetti <james.aimonetti@gmail.com>
2014-10-14 10:34:07 -0700

    KAZOO-2885: continued to figure out how to generate the api token

--- applications/crossbar/src/modules/cb_ubiquiti_util.erl
+++ applications/crossbar/src/modules/cb_ubiquiti_util.erl
@@ -11,13 +11,14 @@
 -export([api_token/4]).
 
 -include("../crossbar.hrl").
+-include_lib("eunit/include/eunit.hrl").
 
 -spec api_token(ne_binary(), integer(), ne_binary(), ne_binary()) -> ne_binary().
 api_token(ProviderId, Timestamp, Salt, Secret) ->
     TimestampHex = wh_util:to_lower_binary(integer_to_list(Timestamp, 16)),
     wh_util:join_binary(
       [<<"1">> % Version
-       ,wh_util:to_binary(Salt)
+       ,Salt
        ,TimestampHex
        ,auth_hash(ProviderId, TimestampHex, Salt, Secret)
       ]
@@ -26,20 +27,26 @@ api_token(ProviderId, Timestamp, Salt, Secret) ->
 
 -spec auth_hash(ne_binary(), ne_binary(), ne_binary(), ne_binary()) -> ne_binary().
 auth_hash(ProviderId, TimestampHex, Salt, Secret) ->
-    ProviderLen = byte_size(ProviderId),
-    TimestampLen = byte_size(TimestampHex),
-    SaltLen = byte_size(Salt),
-    wh_util:to_hex_binary(
-      crypto:hash('sha'
-                  ,<<ProviderId:ProviderLen/binary
-                     ,TimestampHex:TimestampLen/binary
-                     ,(wh_util:to_hex_binary(crypto:hash('sha', <<Salt:SaltLen/binary, Secret/binary>>)))/binary
-                   >>
-                 )).
+    Auth = wh_util:to_lower_binary(
+             wh_util:hexencode_binary(
+               crypto:hash('sha', [Salt, Secret])
+              )
+            ),
+
+    PreHash = [ProviderId
+               ,TimestampHex
+               ,Auth
+              ],
+
+    ?debugFmt("prehash: ~s~n", [PreHash]),
+
+    Hash = crypto:hash('sha', PreHash),
+
+    ?debugFmt("hashe: ~s~n", [bstr:hexencode(Hash)]),
+
+    wh_util:hexencode_binary(Hash).
 
 -ifdef(TEST).
-
--include_lib("eunit/include/eunit.hrl").
 
 api_token_test() ->
     ProviderId = <<"2600hz">>,
@@ -48,16 +55,17 @@ api_token_test() ->
     Epoch      = 1400080558,
     Expiration = 7200,
     Timestamp = Epoch + Expiration,
+    TimestampHex = wh_util:to_lower_binary(integer_to_list(Timestamp, 16)),
 
     ApiToken = <<"1:966d1579e4c0c324c0d95c266adce307bd1e7e08:5373a4ce:61e46d33721f4de8c806b079cd1b6aecb210dd70">>,
     Generated = api_token(ProviderId, Timestamp, Salt, Secret),
 
-    ?debugFmt("tstamp hex: ~s~n", [wh_util:to_lower_binary(integer_to_list(Timestamp, 16))]),
+    Hash = auth_hash(ProviderId, TimestampHex, Salt, Secret),
+
+    ?debugFmt("hash: ~s~n", [Hash]),
     ?debugFmt("api: ~s~n", [ApiToken]),
     ?debugFmt("gen: ~s~n", [Generated]),
 
-    Hash = auth_hash(ProviderId, <<"5373f465">>, Salt, Secret),
-    ?debugFmt("hash: ~s~n", [Hash]),
-
     ?assertEqual(ApiToken, Generated).
+
 -endif.
