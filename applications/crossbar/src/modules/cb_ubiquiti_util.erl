b77f0354b8bd63568c42045635742e619a4c7802
James Aimonetti <james.aimonetti@gmail.com>
2014-10-15 15:42:12 -0700

    KAZOO-2885: create/split api tokens for SSO

--- applications/crossbar/src/modules/cb_ubiquiti_util.erl
+++ applications/crossbar/src/modules/cb_ubiquiti_util.erl
@@ -8,22 +8,33 @@
 %%%-------------------------------------------------------------------
 -module(cb_ubiquiti_util).
 
--export([api_token/4]).
+-export([make_api_token/4
+         ,split_api_token/1
+        ]).
 
 -include("../crossbar.hrl").
--include_lib("eunit/include/eunit.hrl").
 
--spec api_token(ne_binary(), integer(), ne_binary(), ne_binary()) -> ne_binary().
-api_token(ProviderId, Timestamp, Salt, Secret) ->
-    TimestampHex = wh_util:to_lower_binary(integer_to_list(Timestamp, 16)),
+-define(VERSION, <<"1">>).
+
+-spec make_api_token(ne_binary(), integer(), ne_binary(), ne_binary()) -> ne_binary().
+make_api_token(ProviderId, Timestamp, Salt, Secret) ->
+    TimestampHex = encode_timestamp(Timestamp),
     wh_util:join_binary(
-      [<<"1">> % Version
+      [?VERSION % Version
        ,Salt
        ,TimestampHex
        ,auth_hash(ProviderId, TimestampHex, Salt, Secret)
       ]
       ,<<":">>
      ).
+
+-spec encode_timestamp(integer()) -> ne_binary().
+encode_timestamp(Timestamp) when is_integer(Timestamp) ->
+    wh_util:to_lower_binary(integer_to_list(Timestamp, 16)).
+
+-spec decode_timestamp(ne_binary()) -> integer().
+decode_timestamp(<<_/binary>> = TimestampHex) ->
+    list_to_integer(binary_to_list(TimestampHex), 16).
 
 -spec auth_hash(ne_binary(), ne_binary(), ne_binary(), ne_binary()) -> ne_binary().
 auth_hash(ProviderId, TimestampHex, Salt, Secret) ->
@@ -38,34 +49,45 @@ auth_hash(ProviderId, TimestampHex, Salt, Secret) ->
                ,Auth
               ],
 
-    ?debugFmt("prehash: ~s~n", [PreHash]),
-
     Hash = crypto:hash('sha', PreHash),
-
-    ?debugFmt("hashe: ~s~n", [bstr:hexencode(Hash)]),
 
     wh_util:hexencode_binary(Hash).
 
--ifdef(TEST).
+-spec split_api_token(ne_binary()) -> {ne_binary(), integer(), ne_binary()}.
+split_api_token(Token) ->
+    [?VERSION, Salt, TimestampHex, Auth] = binary:split(wh_util:to_lower_binary(Token), <<":">>, ['global']),
+    {Salt, decode_timestamp(TimestampHex), Auth}.
 
-api_token_test() ->
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
+
+make_api_token_test() ->
     ProviderId = <<"2600hz">>,
     Secret     = <<"f1986434eb540c8c956eb0a21094c38c6f9f12bf">>,
     Salt       = <<"966d1579e4c0c324c0d95c266adce307bd1e7e08">>,
     Epoch      = 1400080558,
     Expiration = 7200,
     Timestamp = Epoch + Expiration,
-    TimestampHex = wh_util:to_lower_binary(integer_to_list(Timestamp, 16)),
 
-    ApiToken = <<"1:966d1579e4c0c324c0d95c266adce307bd1e7e08:5373a4ce:61e46d33721f4de8c806b079cd1b6aecb210dd70">>,
-    Generated = api_token(ProviderId, Timestamp, Salt, Secret),
-
-    Hash = auth_hash(ProviderId, TimestampHex, Salt, Secret),
-
-    ?debugFmt("hash: ~s~n", [Hash]),
-    ?debugFmt("api: ~s~n", [ApiToken]),
-    ?debugFmt("gen: ~s~n", [Generated]),
+    ApiToken = <<"1:966d1579e4c0c324c0d95c266adce307bd1e7e08:5373a4ce:edd438990c1fc47a8c780d11fb4bbb0c49b35731">>,
+    Generated = make_api_token(ProviderId, Timestamp, Salt, Secret),
 
     ?assertEqual(ApiToken, Generated).
+
+split_api_token_test() ->
+    ProviderId = <<"2600hz">>,
+    Secret     = <<"f1986434eb540c8c956eb0a21094c38c6f9f12bf">>,
+    Salt       = <<"966d1579e4c0c324c0d95c266adce307bd1e7e08">>,
+    Epoch      = 1400080558,
+    Expiration = 7200,
+    Timestamp = Epoch + Expiration,
+    Auth = auth_hash(ProviderId, encode_timestamp(Timestamp), Salt, Secret),
+
+    Generated = make_api_token(ProviderId, Timestamp, Salt, Secret),
+    {Salt1, Timestamp1, Auth1} = split_api_token(Generated),
+
+    ?assertEqual(Salt, Salt1),
+    ?assertEqual(Timestamp, Timestamp1),
+    ?assertEqual(Auth, Auth1).
 
 -endif.
