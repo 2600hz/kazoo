73e9b2b72f884371dde043d90d7d053c97e6fa5a
James Aimonetti <james.aimonetti@gmail.com>
2014-10-15 21:40:43 -0700

    KAZOO-2885: more tools for SSO api tokens

--- applications/crossbar/src/modules/cb_ubiquiti_util.erl
+++ applications/crossbar/src/modules/cb_ubiquiti_util.erl
@@ -8,13 +8,30 @@
 %%%-------------------------------------------------------------------
 -module(cb_ubiquiti_util).
 
--export([make_api_token/4
+-export([create_api_token/1, create_api_token/2
+         ,make_api_token/4
          ,split_api_token/1
         ]).
 
 -include("../crossbar.hrl").
+-define(U_CONFIG_CAT, <<"crossbar.ubiquiti">>).
 
 -define(VERSION, <<"1">>).
+-define(EXPIRES, whapps_config:get_integer(?U_CONFIG_CAT, <<"api_token_expires_s">>, 1800)).
+-define(SECRET, whapps_config:get(?U_CONFIG_CAT, <<"api_secret">>)).
+-define(SALT_LENGTH, whapps_config:get_integer(?U_CONFIG_CAT, <<"salt_length">>, 20)).
+
+-spec create_api_token(ne_binary()) -> ne_binary().
+-spec create_api_token(ne_binary(), ne_binary()) -> ne_binary().
+create_api_token(ProviderId) ->
+    create_api_token(ProviderId, ?SECRET).
+
+create_api_token(ProviderId, <<_/binary>> = Secret) ->
+    Salt = wh_util:rand_hex_binary(?SALT_LENGTH),
+    ExpireTime = wh_util:current_unix_timestamp() + ?EXPIRES,
+    make_api_token(ProviderId, ExpireTime, Salt, Secret);
+create_api_token(_ProviderId, 'undefined') ->
+    throw({'error', 'no_api_secret'}).
 
 -spec make_api_token(ne_binary(), integer(), ne_binary(), ne_binary()) -> ne_binary().
 make_api_token(ProviderId, Timestamp, Salt, Secret) ->
