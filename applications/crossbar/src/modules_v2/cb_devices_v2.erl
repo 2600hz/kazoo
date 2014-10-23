c9b3616d8fdadee21f60144e5040c6282aa64906
James Aimonetti <james.aimonetti@gmail.com>
2014-10-23 13:53:19 -0700

    KAZOO-2991: only flush when device credentials change

--- applications/crossbar/src/modules_v2/cb_devices_v2.erl
+++ applications/crossbar/src/modules_v2/cb_devices_v2.erl
@@ -249,11 +249,7 @@ delete(Context, DeviceId) ->
 %%%===================================================================
 -spec registration_update(cb_context:context()) -> 'ok'.
 registration_update(Context) ->
-    Device = cb_context:doc(Context),
-    crossbar_util:flush_registration(
-      wh_json:get_value([<<"sip">>, <<"username">>], Device)
-      ,crossbar_util:get_account_realm(Context)
-     ).
+    cb_devices_v1:registration_update(Context).
 
 %%--------------------------------------------------------------------
 %% @private
