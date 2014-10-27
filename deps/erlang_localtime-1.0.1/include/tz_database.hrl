e54c6d2483b276cd4209c78a5c99c78e229b3513
James Aimonetti <james.aimonetti@gmail.com>
2014-10-27 15:18:59 -0700

    3.16: add cleanup for old localtime lib

--- deps/erlang_localtime-1.0.1/include/tz_database.hrl
+++ deps/erlang_localtime-1.0.1/include/tz_database.hrl
@@ -1,5 +1,18 @@
-%% @author  Dmitry S. Melnikov (dmitryme@gmail.com)
-%% @copyright 2010 Dmitry S. Melnikov
+%% Copyright (C) 07/01/2010 Dmitry S. Melnikov (dmitryme@gmail.com)
+%%
+%% This program is free software; you can redistribute it and/or
+%% modify it under the terms of the GNU General Public License
+%% as published by the Free Software Foundation; either version 2
+%% of the License, or (at your option) any later version.
+%%
+%% This program is distributed in the hope that it will be useful,
+%% but WITHOUT ANY WARRANTY; without even the implied warranty of
+%% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%% GNU General Public License for more details.
+%%
+%% You should have received a copy of the GNU General Public License
+%% along with this program; if not, write to the Free Software
+%% Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 -author("Dmitry Melnikov <dmitryme@gmail.com>").
 
