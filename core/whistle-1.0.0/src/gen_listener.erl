8b7f94efa468d92b9c533233b97a8dc4d7261107
James Aimonetti <james.aimonetti@gmail.com>
2014-11-05 12:30:44 -0800

    KAZOO-3048: spec updates

--- core/whistle-1.0.0/src/gen_listener.erl
+++ core/whistle-1.0.0/src/gen_listener.erl
@@ -921,32 +921,32 @@ broker_connections(Listeners, Brokers) ->
                             props:get_value(Broker, Listeners) =/= 'undefined'
                     end, Brokers).
 
--spec start_new_listeners(ne_binary(), binding(), wh_proplist(), state()) -> {'ok', federator_listeners()}.
+-spec start_new_listeners(ne_binaries(), binding_module(), wh_proplist(), state()) -> {'ok', federator_listeners()}.
 start_new_listeners(Brokers, Binding, Props, State) ->
     {'ok', [start_new_listener(Broker, Binding, Props, State)
             || Broker <- Brokers
            ]}.
 
--spec start_new_listener(ne_binary(), binding(), wh_proplist(), state()) -> federator_listener().
+-spec start_new_listener(ne_binary(), binding_module(), wh_proplist(), state()) -> federator_listener().
 start_new_listener(Broker, Binding, Props, #state{params=Ps}) ->
     FederateParams = create_federated_params({Binding, Props}, Ps),
     {'ok', Pid} = listener_federator:start_link(self(), Broker, FederateParams),
     lager:debug("started federated listener on broker ~s: ~p", [Broker, Pid]),
     {Broker, Pid}.
 
--spec update_existing_listeners_bindings(federator_listeners(), binding(), wh_proplist()) -> 'ok'.
+-spec update_existing_listeners_bindings(federator_listeners(), binding_module(), wh_proplist()) -> 'ok'.
 update_existing_listeners_bindings(Listeners, Binding, Props) ->
     [update_existing_listener_bindings(Listener, Binding, Props)
      || Listener <- Listeners
     ],
     'ok'.
 
--spec update_existing_listener_bindings(federator_listener(), binding(), wh_proplist()) -> 'ok'.
+-spec update_existing_listener_bindings(federator_listener(), binding_module(), wh_proplist()) -> 'ok'.
 update_existing_listener_bindings({_Broker, Pid}, Binding, Props) ->
     lager:debug("updating listener ~p with ~s", [Pid, Binding]),
     gen_listener:add_binding(Pid, Binding, Props).
 
--spec create_federated_params({binding(), wh_proplist()}, wh_proplist()) -> wh_proplist().
+-spec create_federated_params({binding_module(), wh_proplist()}, wh_proplist()) -> wh_proplist().
 create_federated_params(FederateBindings, Params) ->
     [{'responders', []}
      ,{'bindings', [FederateBindings]}
