8c1074bdef4173f54a3403eb31efd2a0a74d2074
Brian Davis <brian@scratchwireless.com>
2014-11-10 12:49:40 -0500

    Fixed lookup of int value to get_integer_value and added default pin length

--- applications/callflow/src/module/cf_disa.erl
+++ applications/callflow/src/module/cf_disa.erl
@@ -53,10 +53,11 @@ try_collect_pin(Call, _Pin, 0, _Interdigit) ->
 try_collect_pin(Call, Pin, Retries, Interdigit) ->
     Prompt = <<"disa-enter_pin">>,
     NoopId = whapps_call_command:prompt(Prompt, Call),
-    PinSize = byte_size(Pin),
+    DefaultPinLength = whapps_config:get_integer(?CF_CONFIG_CAT, <<"default_pin_length">>, 10),
+    PinLength = erlang:max(DefaultPinLength, byte_size(Pin)),
 
-    lager:debug("collecting ~p digits for pin", [PinSize]),
-    case whapps_call_command:collect_digits(PinSize
+    lager:debug("collecting up to ~p digits for pin", [PinLength]),
+    case whapps_call_command:collect_digits(PinLength
                                             ,whapps_call_command:default_collect_timeout()
                                             ,Interdigit
                                             ,NoopId
@@ -83,7 +84,7 @@ allow_dial(_, Call, 0, _Interdigit) ->
     cf_exe:continue(Call);
 allow_dial(Data, Call, Retries, Interdigit) ->
     _ = start_preconnect_audio(Data, Call),
-    MaxDigits = wh_json:get_value(<<"max_digits">>, Data, 15),
+    MaxDigits = wh_json:get_integer_value(<<"max_digits">>, Data, 15),
 
     lager:debug("collecting max ~p digits for destination number", [MaxDigits]),
     {'ok', Digits} = whapps_call_command:collect_digits(MaxDigits
