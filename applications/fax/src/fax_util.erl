6e4c29b6e68f58facdf6501aa7954d148df78375
lazedo <luis.azedo@factorlusitano.com>
2014-11-24 04:55:56 +0000

    KAZOO-2630 remove extra blank line

--- applications/fax/src/fax_util.erl
+++ applications/fax/src/fax_util.erl
@@ -161,5 +161,3 @@ filter_numbers(Number) ->
     NumberList = wh_util:to_list(Number),
     Out = lists:filter(fun(X) -> lists:member(X, "1234567890") end, NumberList),
     wh_util:to_binary(Out).
-    
-    
