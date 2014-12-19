73852e20e756f91f0d1becc10fed6335a0c7d528
James Aimonetti <james.aimonetti@gmail.com>
2014-12-19 11:36:39 -0800

    KAZOO-3200: init template using list of properties

--- applications/teletype/src/teletype.hrl
+++ applications/teletype/src/teletype.hrl
@@ -13,8 +13,22 @@
 -type mime_tuples() :: [mimemail:mimetuple(),...] | [].
 
 %% {ContentType, Filename, Content}
--type attachment() :: {ne_binary(), ne_binary(), binary()}.
+-type attachment() :: {ne_binary(), ne_binary(), ne_binary()}.
 -type attachments() :: [attachment(),...] | [].
+
+-type init_param() :: {'macros', wh_json:object()} |
+                      {'text', ne_binary()} |
+                      {'html', ne_binary()} |
+                      {'subject', ne_binary()} |
+                      {'to', wh_json:object()} |
+                      {'cc', wh_json:object()} |
+                      {'bcc', wh_json:object()} |
+                      {'from', ne_binary()} |
+                      {'reply_to', ne_binary()}.
+-type init_params() :: [init_param(),...].
+
+-define(TEXT_PLAIN, <<"text/plain">>).
+-define(TEXT_HTML, <<"text/html">>).
 
 -define(TELETYPE_HRL, 'true').
 -endif.
