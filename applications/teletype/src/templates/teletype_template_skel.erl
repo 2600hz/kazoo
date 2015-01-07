fc84f2cd5901c53df3de07e492120ce936cc4cc4
James Aimonetti <james.aimonetti@gmail.com>
2015-01-07 14:33:24 -0800

    KAZOO-3089: rework skel to use new send_email api

--- applications/teletype/src/templates/teletype_template_skel.erl
+++ applications/teletype/src/templates/teletype_template_skel.erl
@@ -10,6 +10,7 @@
 
 -export([init/0
          ,handle_req/2
+         ,test/2
         ]).
 
 -include("../teletype.hrl").
@@ -30,11 +31,18 @@
 -define(TEMPLATE_CATEGORY, <<"skel">>).
 -define(TEMPLATE_NAME, <<"Skeleton">>).
 
--define(TEMPLATE_TO, ?CONFIGURED_EMAILS(<<"original">>)).
+-define(TEMPLATE_TO, ?CONFIGURED_EMAILS(?EMAIL_ORIGINAL)).
 -define(TEMPLATE_FROM, teletype_util:default_from_address(?MOD_CONFIG_CAT)).
--define(TEMPLATE_CC, ?CONFIGURED_EMAILS(<<"specified">>, [])).
--define(TEMPLATE_BCC, ?CONFIGURED_EMAILS(<<"specificed">>, [])).
+-define(TEMPLATE_CC, ?CONFIGURED_EMAILS(?EMAIL_SPECIFIED, [])).
+-define(TEMPLATE_BCC, ?CONFIGURED_EMAILS(?EMAIL_SPECIFIED, [])).
 -define(TEMPLATE_REPLY_TO, teletype_util:default_reply_to(?MOD_CONFIG_CAT)).
+
+test(AccountId, UserId) ->
+    Req = [{<<"Account-ID">>, AccountId}
+           ,{<<"User-ID">>, UserId}
+           | wh_api:default_headers(?APP_NAME, ?APP_VERSION)
+          ],
+    wh_amqp_worker:cast(Req, fun wapi_notifications:publish_skel/1).
 
 -spec init() -> 'ok'.
 init() ->
@@ -61,7 +69,7 @@ handle_req(JObj, _Props) ->
     %% Gather data for template
     DataJObj = wh_json:normalize(wh_api:remove_defaults(JObj)),
     ServiceData = teletype_util:service_params(DataJObj, ?MOD_CONFIG_CAT),
-    Macros = [{<<"service">>, ServiceData} | build_template_data(DataJObj)],
+    Macros = [{<<"service">>, ServiceData} | build_macro_data(DataJObj)],
 
     %% Load templates
     Templates = teletype_util:fetch_templates(?TEMPLATE_ID, DataJObj),
@@ -74,37 +82,67 @@ handle_req(JObj, _Props) ->
     {'ok', TemplateMetaJObj} = teletype_util:fetch_template_meta(?TEMPLATE_ID, wh_json:get_value(<<"Account-ID">>, JObj)),
 
     Subject = teletype_util:render_subject(
-                wh_json:find(<<"subject">>, [JObj, TemplateMetaJObj])
+                wh_json:find(<<"subject">>, [DataJObj, TemplateMetaJObj])
                 ,Macros
                ),
 
+    Emails = teletype_util:find_addresses(set_to_address(DataJObj), TemplateMetaJObj, ?MOD_CONFIG_CAT),
+
     %% Send email
-    teletype_util:send_email(?TEMPLATE_ID, DataJObj, ServiceData, Subject, RenderedTemplates).
+    teletype_util:send_email(Emails, Subject, ServiceData, RenderedTemplates).
 
--spec build_template_data(wh_json:object()) -> wh_proplist().
-build_template_data(DataJObj) ->
-    [{<<"user">>, build_user_data(DataJObj)}
-     | wh_json:to_proplist(wh_json:normalize(DataJObj))
-    ].
+-spec build_macro_data(wh_json:object()) -> wh_proplist().
+build_macro_data(DataJObj) ->
+    wh_json:foldl(fun(MacroKey, _V, Acc) ->
+                          maybe_add_macro_key(MacroKey, Acc, DataJObj)
+                  end
+                  ,[]
+                  ,?TEMPLATE_MACROS
+                 ).
 
--spec build_user_data(wh_json:object()) -> wh_proplist().
--spec build_user_data(wh_json:object(), wh_json:object()) -> wh_proplist().
-build_user_data(DataJObj) ->
+set_to_address(DataJObj) ->
+    AccountId = wh_json:get_value(<<"account_id">>, DataJObj),
+    UserId = wh_json:get_value(<<"user_id">>, DataJObj),
+    {'ok', UserJObj} = couch_mgr:open_cache_doc(wh_util:format_account_id(AccountId, 'encoded')
+                                                ,UserId
+                                               ),
+    wh_json:set_value(<<"to">>, [find_email(UserJObj)], DataJObj).
+
+find_email(UserJObj) ->
+    wh_json:get_first_defined([<<"email">>, <<"username">>], UserJObj).
+
+-spec maybe_add_macro_key(wh_json:key(), wh_proplist(), wh_json:object()) -> wh_proplist().
+maybe_add_macro_key(<<"user.", UserKey/binary>>, Acc, DataJObj) ->
+    maybe_add_user_data(UserKey, Acc, DataJObj);
+maybe_add_macro_key(_Key, Acc, _DataJObj) ->
+    lager:debug("unprocessed macro key ~s: ~p", [_Key, _DataJObj]),
+    Acc.
+
+-spec maybe_add_user_data(wh_json:key(), wh_proplist(), wh_json:object()) -> wh_proplist().
+maybe_add_user_data(Key, Acc, DataJObj) ->
+    User = get_user(DataJObj),
+
+    UserMacros = props:get_value(<<"user">>, Acc, []),
+
+    case wh_json:get_value(Key, User) of
+        'undefined' ->
+            lager:debug("unprocessed user macro key ~s: ~p", [Key, User]),
+            Acc;
+        V -> props:set_value(<<"user">>, [{Key, V} | UserMacros], Acc)
+    end.
+
+-spec get_user(wh_json:object()) -> wh_json:object().
+get_user(DataJObj) ->
     AccountId = wh_json:get_value(<<"account_id">>, DataJObj),
     AccountDb = wh_util:format_account_id(AccountId, 'encoded'),
     UserId = wh_json:get_value(<<"user_id">>, DataJObj),
 
     case couch_mgr:open_cache_doc(AccountDb, UserId) of
-        {'ok', UserJObj} ->
-            build_user_data(DataJObj, UserJObj);
+        {'ok', UserJObj} -> UserJObj;
         {'error', _E} ->
             lager:debug("failed to find user ~s in ~s: ~p", [UserId, AccountId, _E]),
-            [{<<"first_name">>, <<"First">>}
-             ,{<<"last_name">>, <<"Last">>}
-             ,{<<"email">>, <<"Email">>}
-            ]
+            case wh_json:is_true(<<"preview">>, DataJObj) of
+                'false' -> throw({'error', 'not_found'});
+                'true' -> wh_json:new()
+            end
     end.
-
-build_user_data(_DataJObj, UserJObj) ->
-    UserKeys = [<<"first_name">>, <<"last_name">>, <<"email">>],
-    [{Key, wh_json:get_value(Key, UserJObj)} || Key <- UserKeys].
