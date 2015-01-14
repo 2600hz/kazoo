221bf07218ac8188141d58a7b3b10c41db06be8d
James Aimonetti <james.aimonetti@gmail.com>
2015-01-14 15:52:32 -0800

    KAZOO-3075: add macros from before

--- deps/jesse-1.1.5/src/jesse_schema_validator.hrl
+++ deps/jesse-1.1.5/src/jesse_schema_validator.hrl
@@ -26,6 +26,7 @@
 -define(PROPERTIES,           <<"properties">>).
 -define(PATTERNPROPERTIES,    <<"patternProperties">>).
 -define(ADDITIONALPROPERTIES, <<"additionalProperties">>).
+-define(MINPROPERTIES,        <<"minProperties">>).
 -define(ITEMS,                <<"items">>).
 -define(ADDITIONALITEMS,      <<"additionalItems">>).
 -define(REQUIRED,             <<"required">>).
@@ -76,12 +77,20 @@
 -define(not_allowed,                 'not_allowed').
 -define(not_unique,                  'not_unique').
 -define(not_in_range,                'not_in_range').
+-define(not_minimum,                 'not_minimum').
+-define(not_maximum,                 'not_maximum').
+-define(not_in_enum,                 'not_in_enum').
 -define(not_divisible,               'not_divisible').
 -define(wrong_type,                  'wrong_type').
 -define(wrong_type_items,            'wrong_type_items').
 -define(wrong_type_dependency,       'wrong_type_dependency').
 -define(wrong_size,                  'wrong_size').
+-define(wrong_min_items,             'wrong_min_items').
+-define(wrong_max_items,             'wrong_max_items').
+-define(wrong_min_properties,        'wrong_min_properties').
 -define(wrong_length,                'wrong_length').
+-define(wrong_min_length,            'wrong_min_length').
+-define(wrong_max_length,            'wrong_max_length').
 -define(wrong_format,                'wrong_format').
 -define(schema_unsupported,          'schema_unsupported').
 
