ab67842d77a14cc315266556ce367fd57cce0201
James Aimonetti <james.aimonetti@gmail.com>
2015-01-15 14:50:46 -0800

    KAZOO-3229: add unit tests for contact strings

--- applications/ecallmgr/src/ecallmgr_registrar.erl
+++ applications/ecallmgr/src/ecallmgr_registrar.erl
@@ -1046,7 +1046,14 @@ print_property(<<"Expires">> =Key, Value, #registration{expires=Expires
 print_property(Key, Value, _) ->
     io:format("~-19s: ~s~n", [Key, wh_util:to_binary(Value)]).
 
--spec breakup_contact(text()) -> wh_proplist().
+-type contact_param() :: {'uri', ne_binary()} |
+                         {'hostport', ne_binary()} |
+                         {'transport', ne_binary()} |
+                         {'fs_path', ne_binary()} |
+                         {'received', ne_binary()}.
+-type contact_params() :: [contact_param(),...] | [].
+
+-spec breakup_contact(text()) -> contact_params().
 breakup_contact(Contact) when is_binary(Contact) ->
     C = binary:replace(Contact, [<<$'>>, <<$<>>, <<$>>>, <<"sip:">>], <<>>, ['global']),
     [Uri|Parameters] = binary:split(C, <<";">>, ['global']),
@@ -1058,7 +1065,7 @@ breakup_contact(Contact) ->
 -spec find_contact_parameters(ne_binaries(), wh_proplist()) -> wh_proplist().
 find_contact_parameters([], Props) -> Props;
 find_contact_parameters([<<"transport=", Transport/binary>>|Parameters], Props) ->
-    find_contact_parameters(Parameters, [{'transport', Transport}|Props]);
+    find_contact_parameters(Parameters, [{'transport', wh_util:to_lower_binary(Transport)}|Props]);
 find_contact_parameters([<<"fs_path=", FsPath/binary>>|Parameters], Props) ->
     find_contact_parameters(Parameters, [{'fs_path', FsPath}|Props]);
 find_contact_parameters([<<"received=", Received/binary>>|Parameters], Props) ->
@@ -1072,3 +1079,54 @@ get_contact_hostport(Uri) ->
         [_, Hostport] -> Hostport;
         _Else -> Uri
     end.
+
+-ifdef(TEST).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-define(CONTACTS
+        ,[{<<"sip:user_0tbp@1.1.1.1:55274;fs_path=sip:2.2.2.2:5060;lr;received='sip:1.1.1.1:55274;transport=udp'">>
+           ,[{'transport', <<"udp">>}
+             ,{'received', <<"1.1.1.1:55274">>}
+             ,{'fs_path', <<"2.2.2.2:5060">>}
+             ,{'uri', <<"user_0tbp@1.1.1.1:55274">>}
+             ,{'hostport', <<"1.1.1.1:55274">>}
+            ]
+          }
+          ,{<<"<sip:5519@2.2.2.2:55061;transport=TLS;ob;fs_path=<sip:1.1.1.1:5061;lr;received='sip:1.1.1.1:55061;transport=tls'>>">>
+            ,[{'transport', <<"tls">>}
+              ,{'received', <<"1.1.1.1:55061">>}
+              ,{'fs_path', <<"1.1.1.1:5061">>}
+              ,{'uri', <<"5519@2.2.2.2:55061">>}
+              ,{'hostport', <<"2.2.2.2:55061">>}
+             ]
+           }
+          ,{<<"<sip:User_2pnrza@3.3.3.3:64967;ob;fs_path=<sip:1.1.1.1:5060;lr;received='sip:3.3.3.3:64967;transport=udp'>>">>
+            ,[{'transport', <<"udp">>}
+              ,{'received', <<"3.3.3.3:64967">>}
+              ,{'fs_path', <<"1.1.1.1:5060">>}
+              ,{'uri', <<"User_2pnrza@3.3.3.3:64967">>}
+              ,{'hostport', <<"3.3.3.3:64967">>}
+             ]
+           }
+          ,{<<"<sip:5520@78.25.120.237:26070;transport=TLS;rinstance=e6b1e5bda0fcfd30;fs_path=<sip:94.125.5.31:5061;lr;received='sip:78.25.120.237:26070;transport=tls'>>">>
+            ,[{'transport', <<"tls">>}
+              ,{'received', <<"78.25.120.237:26070">>}
+              ,{'fs_path', <<"94.125.5.31:5061">>}
+              ,{'uri', <<"5520@78.25.120.237:26070">>}
+              ,{'hostport', <<"78.25.120.237:26070">>}
+             ]
+           }
+         ]).
+
+registrar_summary_test() ->
+    lists:foreach(fun contact_props/1, ?CONTACTS).
+
+contact_props({Contact, Ps}) ->
+    Props = breakup_contact(Contact),
+
+    lists:foreach(fun({K, V}) ->
+                          ?assertEqual(V, props:get_value(K, Props))
+                  end, Ps).
+
+-endif.
