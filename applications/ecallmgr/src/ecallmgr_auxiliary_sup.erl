ee1ea9aacbbe08f3308afe6ab773db73eb4341ea
Peter Defebvre <macpie59@gmail.com>
2015-01-20 19:47:13 +0000

    KAZOO-3218: Fix Media caching problem - playback is an old file after new file uploaded

--- applications/ecallmgr/src/ecallmgr_auxiliary_sup.erl
+++ applications/ecallmgr/src/ecallmgr_auxiliary_sup.erl
@@ -19,8 +19,12 @@
 
 -define(CACHE_AUTHN_PROPS, [{'origin_bindings', [[{'type', <<"account">>}]
                                                  ,[{'type', <<"device">>}]
+
                                                 ]}]).
--define(CACHE_UTIL_PROPS, [{'origin_bindings', [[{'db', ?WH_CONFIG_DB}]]}]).
+-define(CACHE_UTIL_PROPS, [{'origin_bindings', [[{'db', ?WH_CONFIG_DB}]
+                                                 ,[{'type', <<"media">>}]
+                                               ]}
+                          ]).
 
 -define(CHILDREN, [?CACHE_ARGS(?ECALLMGR_UTIL_CACHE, ?CACHE_UTIL_PROPS)
                    ,?CACHE_ARGS(?ECALLMGR_AUTH_CACHE, ?CACHE_AUTHN_PROPS)
