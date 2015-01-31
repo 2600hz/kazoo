458721b1d2f2e0665914ffbfbfd8e9460edb1ad2
James Aimonetti <james.aimonetti@gmail.com>
2015-01-30 16:14:09 -0800

    KAZOO-3212: get replaced-by id

--- core/kazoo_documents-1.0.0/src/kz_call_event.erl
+++ core/kazoo_documents-1.0.0/src/kz_call_event.erl
@@ -10,6 +10,7 @@
 
 -export([call_id/1
          ,other_leg_call_id/1
+         ,replaced_by/1
          ,custom_channel_vars/1
          ,custom_sip_headers/1
         ]).
@@ -23,6 +24,10 @@ call_id(JObj) ->
 -spec other_leg_call_id(wh_json:object()) -> api_binary().
 other_leg_call_id(JObj) ->
     wh_json:get_value(<<"Other-Leg-Call-ID">>, JObj).
+
+-spec replaced_by(wh_json:object()) -> api_binary().
+replaced_by(JObj) ->
+    wh_json:get_value(<<"Replaced-By">>, JObj).
 
 -spec custom_channel_vars(wh_json:object()) -> api_object().
 custom_channel_vars(JObj) ->
