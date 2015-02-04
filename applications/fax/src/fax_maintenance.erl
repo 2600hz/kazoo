1b91a2fcfb7b7ef305e2a9b577d91cfff672060f
lazedo <luis.azedo@factorlusitano.com>
2015-02-04 23:47:56 +0000

    KAZOO-3301 optionally use 'override_existing_document' in migrate_faxes to override existing docs in modb.
    sup fax_maintenance migrate override_existing_documents
    
    added migrate/2 for
    
    sup fax_maintenance migrate accountid override_existing_documents

--- applications/fax/src/fax_maintenance.erl
+++ applications/fax/src/fax_maintenance.erl
@@ -13,7 +13,7 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([migrate/0, migrate/1]).
+-export([migrate/0, migrate/1, migrate/2]).
 -export([flush/0]).
 
 -export([restart_job/1 , update_job/2]).
@@ -21,37 +21,57 @@
 -export([faxbox_jobs/1, faxbox_jobs/2]).
 -export([pending_jobs/0, active_jobs/0]).
 
+-define(DEFAULT_MIGRATE_OPTIONS, []).
+-define(OVERRIDE_DOCS, ['override_existing_document']).
+       
 -spec migrate() -> 'ok'.
 migrate() ->
     Accounts = whapps_util:get_all_accounts(),
     Total = length(Accounts),
-    lists:foldr(fun(A, C) -> migrate_faxes_fold(A, C, Total) end, 1, Accounts),
+    lists:foldr(fun(A, C) -> migrate_faxes_fold(A, C, Total,?DEFAULT_MIGRATE_OPTIONS) end, 1, Accounts),
     'ok'.
 
 -spec migrate(ne_binaries() | ne_binary()) -> 'ok'.
 migrate([]) -> 'ok';
+migrate(<<"override_existing_documents">>) ->
+    Accounts = whapps_util:get_all_accounts(),
+    Total = length(Accounts),
+    lists:foldr(fun(A, C) -> migrate_faxes_fold(A, C, Total, ?OVERRIDE_DOCS) end, 1, Accounts),
+    'ok';
 migrate([Account|Accounts]) ->
-    _ = migrate_faxes(Account),
+    _ = migrate_faxes(Account, ?DEFAULT_MIGRATE_OPTIONS),
     migrate(Accounts);
 migrate(Account) ->
-    migrate_faxes(Account).
+    migrate_faxes(Account, ?DEFAULT_MIGRATE_OPTIONS).
+
+-spec migrate(ne_binaries() | ne_binary(), ne_binary() | wh_proplist()) -> 'ok'.
+migrate([], _) -> 'ok';
+migrate(Accounts, <<"override_existing_documents">>) ->
+    migrate(Accounts, ?OVERRIDE_DOCS);
+migrate(Accounts, Option) when is_binary(Option)->
+    migrate(Accounts, ?DEFAULT_MIGRATE_OPTIONS);    
+migrate([Account|Accounts], Options) when is_list(Options) ->
+    _ = migrate_faxes(Account, Options),
+    migrate(Accounts, Options);
+migrate(Account, Options) when is_list(Options) ->
+    migrate_faxes(Account, Options).
 
 %% ====================================================================
 %% Internal functions
 %% ====================================================================
 
-migrate_faxes_fold(AccountDb, Current, Total) ->
+migrate_faxes_fold(AccountDb, Current, Total, Options) ->
     io:format("migrating faxes in database (~p/~p) '~s'~n", [Current, Total, AccountDb]),
-    _ = migrate_faxes(AccountDb),
+    _ = migrate_faxes(AccountDb, Options),
     Current + 1.
 
--spec migrate_faxes(atom() | string() | binary()) -> 'ok'.
-migrate_faxes(Account) when not is_binary(Account) ->
-    migrate_faxes(wh_util:to_binary(Account));
-migrate_faxes(Account) ->
+-spec migrate_faxes(atom() | string() | binary(),  wh_proplist()) -> 'ok'.
+migrate_faxes(Account, Options) when not is_binary(Account) ->
+    migrate_faxes(wh_util:to_binary(Account), Options);
+migrate_faxes(Account, Options) ->
     migrate_private_media(Account),
     recover_private_media(Account),
-    migrate_faxes_to_modb(Account).
+    migrate_faxes_to_modb(Account, Options).
 
 -spec migrate_private_media(ne_binary()) -> 'ok'.
 -spec migrate_private_media(ne_binary(), wh_json:object(), ne_binary()) -> 'ok'.
@@ -117,11 +137,11 @@ recover_private_media(AccountDb, Doc, _MediaType) ->
     _ = couch_mgr:save_doc(AccountDb, wh_json:set_value(<<"pvt_type">>, <<"private_media">>, Doc)),
     'ok'.
 
--spec migrate_faxes_to_modb(ne_binary()) -> 'ok'.
--spec maybe_migrate_fax_to_modb(ne_binary(), wh_json:object()) -> 'ok'.
--spec migrate_fax_to_modb(ne_binary(), ne_binary(), wh_json:object()) -> 'ok'.
+-spec migrate_faxes_to_modb(ne_binary(),  wh_proplist()) -> 'ok'.
+-spec maybe_migrate_fax_to_modb(ne_binary(), wh_json:object(),  wh_proplist()) -> 'ok'.
+-spec migrate_fax_to_modb(ne_binary(), ne_binary(), wh_json:object(),  wh_proplist()) -> 'ok'.
 
-migrate_faxes_to_modb(Account) ->
+migrate_faxes_to_modb(Account, Options) ->
     AccountDb = case couch_mgr:db_exists(Account) of
                     'true' -> Account;
                     'false' -> wh_util:format_account_id(Account, 'encoded')
@@ -130,13 +150,13 @@ migrate_faxes_to_modb(Account) ->
     case couch_mgr:get_results(AccountDb, <<"maintenance/listing_by_type">>, ViewOptions) of
         {'ok', []} -> io:format("no fax docs in db for fax migration ~s~n", [AccountDb]);
         {'ok', JObjs3}->
-            _ = [maybe_migrate_fax_to_modb(AccountDb, JObj) || JObj <- JObjs3],
+            _ = [maybe_migrate_fax_to_modb(AccountDb, JObj, Options) || JObj <- JObjs3],
             'ok';
         {'error', _}=E3 ->
             io:format("unable to fetch fax docs in db ~s: ~p~n", [AccountDb, E3])
     end.
 
-maybe_migrate_fax_to_modb(AccountDb, JObj) ->
+maybe_migrate_fax_to_modb(AccountDb, JObj, Options) ->
     DocId = wh_json:get_value(<<"id">>, JObj),
     case couch_mgr:open_doc(AccountDb, DocId) of
         {'ok', Doc} ->
@@ -149,13 +169,13 @@ maybe_migrate_fax_to_modb(AccountDb, JObj) ->
                          'false' -> 'ok'
                      end;
                 _Attachments ->
-                    migrate_fax_to_modb(AccountDb, DocId, Doc)
+                    migrate_fax_to_modb(AccountDb, DocId, Doc, Options)
             end;
         {'error', E} ->
             io:format("unable to get document ~s for fax migration : ~p",[DocId, E])
     end.
 
-migrate_fax_to_modb(AccountDb, DocId, JObj) ->
+migrate_fax_to_modb(AccountDb, DocId, JObj, Options) ->
     Timestamp = wh_json:get_integer_value(<<"pvt_created">>, JObj, wh_util:current_tstamp()),
     {{Year, Month, _}, _} = calendar:gregorian_seconds_to_datetime(Timestamp),
     AccountMODb = kazoo_modb:get_modb(AccountDb, Year, Month),
@@ -167,7 +187,7 @@ migrate_fax_to_modb(AccountDb, DocId, JObj) ->
             >>,
     io:format("moving doc ~s/~s to ~s/~s~n",[AccountDb, DocId, AccountMODb, FaxId]),
     kazoo_modb:create(AccountMODb),
-    case couch_mgr:move_doc(AccountDb, DocId, FaxMODb, FaxId, []) of
+    case couch_mgr:move_doc(AccountDb, DocId, FaxMODb, FaxId, Options) of
         {'ok', _JObj} -> io:format("document ~s moved to ~s~n",[DocId, FaxId]);
         {'error', Error} -> io:format("error ~p moving document ~s to ~s~n",[Error, DocId, FaxId])
     end.
