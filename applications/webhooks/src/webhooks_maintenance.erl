3559539842831059183d12ba853258deb2a1f5aa
James Aimonetti <james.aimonetti@gmail.com>
2015-02-10 15:06:04 -0800

    KAZOO-2570: cleanup maint module

--- applications/webhooks/src/webhooks_maintenance.erl
+++ applications/webhooks/src/webhooks_maintenance.erl
@@ -1,18 +1,19 @@
 %%%-------------------------------------------------------------------
-%%% @copyright (C) 2013, 2600Hz
+%%% @copyright (C) 2013-2015, 2600Hz
 %%% @doc
 %%%
 %%% @end
 %%% @contributors
+%%%   James Aimonetti
 %%%-------------------------------------------------------------------
 -module(webhooks_maintenance).
 
 -export([hooks_configured/0, hooks_configured/1]).
 
 hooks_configured() ->
-    webhooks_listener:hooks_configured(),
+    webhooks_shared_listener:hooks_configured(),
     'ok'.
 
 hooks_configured(AccountId) ->
-    webhooks_listener:hooks_configured(AccountId),
+    webhooks_shared_listener:hooks_configured(AccountId),
     'ok'.
