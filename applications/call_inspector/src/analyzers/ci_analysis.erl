2c027a27ba6edd5ea83f98f5e4da6c0d2cb774e6
karl anderson <karl@2600hz.com>
2015-02-28 00:31:11 -0500

    update the type

--- applications/call_inspector/src/analyzers/ci_analysis.erl
+++ applications/call_inspector/src/analyzers/ci_analysis.erl
@@ -33,61 +33,61 @@
                      ,failure_location
                      ,reason
                      }).
--type ci_analysis() :: #ci_analysis{}.
+-type analysis() :: #ci_analysis{}.
 
 -export_type([ci_analysis/0]).
 
 -include("../call_inspector.hrl").
 
--spec new() -> ci_analysis().
+-spec new() -> analysis().
 new() -> #ci_analysis{}.
 
--spec set_call_id(ci_analysis(), ne_binary()) -> ci_analysis().
+-spec set_call_id(analysis(), ne_binary()) -> analysis().
 set_call_id(Analysis, CallId) ->
     Analysis#ci_analysis{call_id=CallId}.
 
--spec call_id(ci_analysis()) -> api_binary().
+-spec call_id(analysis()) -> api_binary().
 call_id(#ci_analysis{call_id=CallId}) ->
     CallId.
 
--spec set_originiate_type(ci_analysis(), ne_binary()) -> ci_analysis().
+-spec set_originiate_type(analysis(), ne_binary()) -> analysis().
 set_originiate_type(Analysis, <<"phone">> = Type) ->
     Analysis#ci_analysis{originiate_type=Type};
 set_originiate_type(Analysis, <<"carrier">> = Type) ->
     Analysis#ci_analysis{originiate_type=Type}.
 
--spec originiate_type(ci_analysis()) -> api_binary().
+-spec originiate_type(analysis()) -> api_binary().
 originiate_type(#ci_analysis{originiate_type=Type}) -> Type.
 
--spec set_terminate_type(ci_analysis(), ne_binary()) -> ci_analysis().
+-spec set_terminate_type(analysis(), ne_binary()) -> analysis().
 set_terminate_type(Analysis, <<"phone">> = Type) ->
     Analysis#ci_analysis{terminate_type=Type};
 set_terminate_type(Analysis, <<"carrier">> = Type) ->
     Analysis#ci_analysis{terminate_type=Type}.
 
--spec terminate_type(ci_analysis()) -> api_binary().
+-spec terminate_type(analysis()) -> api_binary().
 terminate_type(#ci_analysis{terminate_type=Type}) ->
     Type.
 
--spec set_failure_location(ci_analysis(), ne_binary()) -> ci_analysis().
+-spec set_failure_location(analysis(), ne_binary()) -> analysis().
 set_failure_location(Analysis, <<"origination">> = Location) ->
     Analysis#ci_analysis{failure_location=Location};
 set_failure_location(Analysis, <<"termination">> = Location) ->
     Analysis#ci_analysis{failure_location=Location}.
 
--spec failure_location(ci_analysis()) -> api_binary().
+-spec failure_location(analysis()) -> api_binary().
 failure_location(#ci_analysis{failure_location=Location}) ->
     Location.
 
--spec set_reason(ci_analysis(), ne_binary()) -> ci_analysis().
+-spec set_reason(analysis(), ne_binary()) -> analysis().
 set_reason(Analysis, Reason) ->
     Analysis#ci_analysis{reason=Reason}.
 
--spec reason(ci_analysis()) -> api_binary().
+-spec reason(analysis()) -> api_binary().
 reason(#ci_analysis{reason=Reason}) ->
     Reason.
 
--spec to_json(ci_analysis()) -> wh_json:object().
+-spec to_json(analysis()) -> wh_json:object().
 to_json(#ci_analysis{}=Analysis) ->
     wh_json:from_list(
       props:filter_undefined(
