894a0bff62180597a06c7db54a6580651c2c355b
SIPLABS, LLC <info@siplabs.ru>
2015-03-11 15:50:46 +0700

    force `record_sample_rate` to be integer

--- applications/callflow/src/module/cf_record_call.erl
+++ applications/callflow/src/module/cf_record_call.erl
@@ -72,7 +72,7 @@ record_call(Data, Call) ->
              ,{<<"Media-Transfer-Destination">>, wh_json:get_value(<<"url">>, Data)}
              ,{<<"Additional-Headers">>, wh_json:get_value(<<"additional_headers">>, Data)}
              ,{<<"Time-Limit">>, wh_json:get_value(<<"time_limit">>, Data)}
-             ,{<<"Record-Sample-Rate">>, wh_json:get_value(<<"record_sample_rate">>, Data)}
+             ,{<<"Record-Sample-Rate">>, wh_json:get_integer_value(<<"record_sample_rate">>, Data)}
             ],
     _ = whapps_call_command:record_call(props:filter_undefined(Props), <<"start">>, Call),
     lager:debug("auto handling call recording").
