63e2a376d1efb2b9266de2a631f63c4911cd355c
James Aimonetti <james.aimonetti@gmail.com>
2015-03-12 13:50:05 -0700

    KAZOO-3267: move case to function clauses

--- applications/teletype/src/templates/teletype_voicemail_to_email.erl
+++ applications/teletype/src/templates/teletype_voicemail_to_email.erl
@@ -143,21 +143,23 @@ process_req(DataJObj) ->
     end.
 
 -spec email_attachments(wh_json:object(), wh_proplist()) -> attachments().
+-spec email_attachments(wh_json:object(), wh_proplist(), boolean()) -> attachments().
 email_attachments(DataJObj, Macros) ->
-    case wh_json:is_true(<<"preview">>, DataJObj, 'false') of
-        'true' -> [];
-        'false' ->
-            VMId = wh_json:get_value(<<"voicemail_box">>, DataJObj),
-            AccountDb = wh_json:get_value(<<"account_db">>, DataJObj),
-            {'ok', VMJObj} = couch_mgr:open_cache_doc(AccountDb, VMId),
-            {[AttachmentMeta], [AttachmentId]} = wh_json:get_values(wh_doc:attachments(VMJObj)),
-            {'ok', AttachmentBin} = couch_mgr:fetch_attachment(AccountDb, VMId, AttachmentId),
+    email_attachments(DataJObj, Macros, wh_json:is_true(<<"preview">>, DataJObj, 'false')).
 
-            [{wh_json:get_value(<<"content_type">>, AttachmentMeta)
-              ,get_file_name(VMJObj, Macros)
-              ,AttachmentBin
-             }]
-    end.
+email_attachments(_DataJObj, _Macros, 'true') -> [];
+email_attachments(DataJObj, Macros, 'false') ->
+    VMId = wh_json:get_value(<<"voicemail_box">>, DataJObj),
+    AccountDb = wh_json:get_value(<<"account_db">>, DataJObj),
+    {'ok', VMJObj} = couch_mgr:open_cache_doc(AccountDb, VMId),
+
+    {[AttachmentMeta], [AttachmentId]} = wh_json:get_values(wh_doc:attachments(VMJObj)),
+    {'ok', AttachmentBin} = couch_mgr:fetch_attachment(AccountDb, VMId, AttachmentId),
+
+    [{wh_json:get_value(<<"content_type">>, AttachmentMeta)
+      ,get_file_name(VMJObj, Macros)
+      ,AttachmentBin
+     }].
 
 -spec get_file_name(wh_json:object(), wh_proplist()) -> ne_binary().
 get_file_name(MediaJObj, Macros) ->
