50f7076bc0d7fa785bcf38253ab596a854b72e65
James Aimonetti <james.aimonetti@gmail.com>
2015-03-20 15:01:11 -0700

    KAZOO-3441: fix dialyer complaint

--- applications/callflow/src/module/cf_intercept_feature.erl
+++ applications/callflow/src/module/cf_intercept_feature.erl
@@ -62,7 +62,8 @@ handle(Data, Call) ->
             maybe_intercept(Data, Call, Params);
         {'error', _E} ->
             lager:info("Error <<~s>> processing intercept '~s' for number ~s"
-                       ,[_E, InterceptType, Number]),
+                       ,[_E, InterceptType, Number]
+                      ),
             _ = whapps_call_command:b_play(<<"park-no_caller">>, Call),
             cf_exe:stop(Call)
     end.
@@ -159,9 +160,6 @@ params_from_data(<<"user">>, Data, _Call) ->
 params_from_data(<<"device">>, Data, _Call) ->
     EndpointId = wh_json:get_value(<<"id">>, Data),
     {'ok', [{<<"device_id">>, EndpointId}]};
-
-params_from_data('undefined', _, _) ->
-    {'error',<<"module not defined in callflow">>};
 params_from_data(Other, _, _) ->
     {'error',<<"module ",Other/binary," not implemented">>}.
 
