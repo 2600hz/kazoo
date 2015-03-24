ce662491d5e0fa7e64579de4f0011619f691d76e
lazedo <luis.azedo@factorlusitano.com>
2015-03-24 10:02:01 +0000

    fix - cleanup contact when creating bridge string
    cleanup contact for sips
    (cherry picked from commit 7f7869db4b749ef60855ee68e135c62cd6bab425)

--- applications/ecallmgr/src/ecallmgr_util.erl
+++ applications/ecallmgr/src/ecallmgr_util.erl
@@ -717,6 +717,7 @@ get_sip_contact(#bridge_endpoint{ip_address=IPAddress}) -> IPAddress.
 
 -spec maybe_clean_contact(ne_binary(), bridge_endpoint()) -> ne_binary().
 maybe_clean_contact(<<"sip:", Contact/binary>>, _Endpoint) -> Contact;
+maybe_clean_contact(<<"sips:", Contact/binary>>, _Endpoint) -> Contact;
 maybe_clean_contact(Contact, #bridge_endpoint{invite_format = <<"route">>}) ->
     Contact;
 maybe_clean_contact(Contact, #bridge_endpoint{invite_format = <<"loopback">>}) ->
