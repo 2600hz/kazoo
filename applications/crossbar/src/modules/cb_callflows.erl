f13097a6b30cc63f28d2436fada348731cbddcda
SIPLABS, LLC <info@siplabs.ru>
2015-03-27 19:03:12 +0600

    Implemented HTTP PATCH method for the callflows

--- applications/crossbar/src/modules/cb_callflows.erl
+++ applications/crossbar/src/modules/cb_callflows.erl
@@ -16,6 +16,7 @@
          ,validate/1, validate/2
          ,put/1
          ,post/2
+         ,patch/2
          ,delete/2
         ]).
 
@@ -37,6 +38,7 @@ init() ->
     _ = crossbar_bindings:bind(<<"*.validate.callflows">>, ?MODULE, 'validate'),
     _ = crossbar_bindings:bind(<<"*.execute.put.callflows">>, ?MODULE, 'put'),
     _ = crossbar_bindings:bind(<<"*.execute.post.callflows">>, ?MODULE, 'post'),
+    _ = crossbar_bindings:bind(<<"*.execute.patch.callflows">>, ?MODULE, 'patch'),
     _ = crossbar_bindings:bind(<<"*.execute.delete.callflows">>, ?MODULE, 'delete').
 
 %%--------------------------------------------------------------------
@@ -94,6 +96,8 @@ validate_callflow(Context, DocId, ?HTTP_GET) ->
     load_callflow(DocId, Context);
 validate_callflow(Context, DocId, ?HTTP_POST) ->
     validate_request(DocId, Context);
+validate_callflow(Context, DocId, ?HTTP_PATCH) ->
+    validate_patch(DocId, Context);
 validate_callflow(Context, DocId, ?HTTP_DELETE) ->
     load_callflow(DocId, Context).
 
@@ -106,6 +110,11 @@ post(Context, _DocId) ->
             maybe_reconcile_numbers(Context1);
         _Status -> Context1
     end.
+
+-spec patch(cb_context:context(), path_token()) -> cb_context:context().
+patch(Context, _DocId) ->
+    'ok' = track_assignment('post', Context),
+    maybe_reconcile_numbers(crossbar_doc:save(Context)).
 
 -spec put(cb_context:context()) -> cb_context:context().
 put(Context) ->
@@ -193,6 +202,45 @@ validate_request(CallflowId, Context) ->
                 ,[]
                 ,cb_context:set_req_data(C, wh_json:set_value(<<"numbers">>, [], JObj))
             )
+    end.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%%
+%% @end
+%%--------------------------------------------------------------------
+-spec validate_patch(api_binary(), cb_context:context()) -> cb_context:context().
+validate_patch(CallflowId, Context) ->
+    Context1 = crossbar_doc:load(CallflowId, Context),
+    case cb_context:resp_status(Context1) of
+        'success' ->
+            PatchJObj = wh_doc:public_fields(cb_context:req_data(Context)),
+            CallflowsJObj = wh_json:merge_jobjs(PatchJObj, cb_context:doc(Context1)),
+            Context2 = cb_context:set_req_data(Context, CallflowsJObj),
+            JObj = cb_context:req_data(Context2),
+            try [wnm_util:to_e164(Number) || Number <- wh_json:get_ne_value(<<"numbers">>, JObj, [])] of
+                [] -> prepare_patterns(CallflowId, Context2);
+                Numbers ->
+                    C = cb_context:set_req_data(Context2
+                        ,wh_json:set_value(<<"numbers">>, Numbers, JObj)
+                    ),
+                    validate_unique_numbers(CallflowId, Numbers, C)
+            catch
+                _E:_R ->
+                    lager:debug("failed to convert all numbers to e164: ~s: ~p", [_E, _R]),
+                    C = cb_context:add_validation_error(<<"numbers">>
+                        ,<<"type">>
+                        ,<<"Value is not of type array">>
+                        ,Context2
+                    ),
+                    validate_unique_numbers(CallflowId
+                        ,[]
+                        ,cb_context:set_req_data(C, wh_json:set_value(<<"numbers">>, [], JObj))
+                    )
+            end;
+    _Status ->
+            Context1
     end.
 
 -spec prepare_patterns(api_binary(), cb_context:context()) -> cb_context:context().
