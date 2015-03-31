a86961c5262c325b9ebba958f3053e9c15220795
James Aimonetti <james.aimonetti@gmail.com>
2015-03-31 09:36:31 -0700

    KAZOO-2840: add unit tests, use cowboy for content type

--- core/kazoo_documents-1.0.0/src/kz_attachment.erl
+++ core/kazoo_documents-1.0.0/src/kz_attachment.erl
@@ -22,26 +22,58 @@ decode_base64(Base64) ->
     case binary:split(Base64, <<",">>) of
         %% http://tools.ietf.org/html/rfc4648
         [Bin] ->
-            lager:debug("not split on ','"),
             {'undefined', corrected_base64_decode(Bin)};
         %% http://tools.ietf.org/rfc/rfc2397.txt
         [<<"data:", CT/binary>>, Bin] ->
-            {ContentType, _Opts} = mochiweb_util:parse_header(wh_util:to_list(CT)),
-
-            {wh_util:to_binary(ContentType), corrected_base64_decode(Bin)};
+            {get_content_type(CT), corrected_base64_decode(Bin)};
         [_SplitLeft, _SplitRight] ->
-            lager:debug("split unexpectedly: ~p/~p", [byte_size(_SplitLeft), byte_size(_SplitRight)]),
-            lager:debug("l: ~s", [binary:part(_SplitLeft, byte_size(_SplitLeft), -20)]),
-            lager:debug("r: ~s", [binary:part(_SplitRight, byte_size(_SplitRight), -10)]),
             {'undefined', corrected_base64_decode(Base64)}
+    end.
+
+-spec get_content_type(ne_binary()) -> api_binary().
+get_content_type(MediaType) ->
+    get_content_type(MediaType, binary:part(MediaType, byte_size(MediaType), -6)).
+
+get_content_type(MediaType, <<"base64">>) ->
+    get_content_type(binary:part(MediaType, 0, byte_size(MediaType) - 7), 'undefined');
+get_content_type(MediaType, _) ->
+    case cowboy_http:nonempty_list(MediaType, fun cowboy_http:media_range/2) of
+        [{{Type, SubType, _Options}, _, _}] ->
+            wh_util:join_binary([Type, SubType], <<"/">>);
+        {'error', 'badarg'} ->
+            lager:warning("failed to extract content type from ~s", [MediaType]),
+            'undefined'
     end.
 
 -spec corrected_base64_decode(ne_binary()) -> ne_binary().
 corrected_base64_decode(Base64) when byte_size(Base64) rem 4 == 3 ->
-    base64:mime_decode(<<Base64/binary, "=">>);
+    base64:decode(<<Base64/binary, "=">>);
 corrected_base64_decode(Base64) when byte_size(Base64) rem 4 == 2 ->
-    base64:mime_decode(<<Base64/binary, "==">>);
+    base64:decode(<<Base64/binary, "==">>);
 corrected_base64_decode(Base64) ->
-    base64:mime_decode(Base64).
+    base64:decode(Base64).
 
+-ifdef(TEST).
+-include_lib("eunit/include/eunit.hrl").
 
+decode_plain_text_test() ->
+    Data = <<"foobar">>,
+    Base64Data = base64:encode(Data),
+    InlineData = <<"data:text/plain;base64,", Base64Data/binary>>,
+
+    ?assertEqual({'undefined', Data}, decode_base64(Base64Data)),
+    ?assertEqual({<<"text/plain">>, Data}, decode_base64(InlineData)).
+
+decode_image_png_test() ->
+    Data = <<137,80,78,71,13,10,26,10,0,0,0,13,73,72,68,82,0,0,0,5,0,0,0,5,8,6,0,0,0,141,
+             111,38,229,0,0,0,28,73,68,65,84,8,215,99,248,255,255,63,195,127,6,32,5,195,
+             32,18,132,208,49,241,130,88,205,4,0,14,245,53,203,209,142,14,31,0,0,0,0,73,
+             69,78,68,174,66,96,130
+           >>,
+    Base64Data = base64:encode(Data),
+    InlineData = <<"data:image/png;base64,", Base64Data/binary>>,
+
+    ?assertEqual({'undefined', Data}, decode_base64(Base64Data)),
+    ?assertEqual({<<"image/png">>, Data}, decode_base64(InlineData)).
+
+-endif.
