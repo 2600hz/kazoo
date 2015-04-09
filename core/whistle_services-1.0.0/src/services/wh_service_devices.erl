18a3e4dfc0c244edc61a2095b229e4642f66207d
James Aimonetti <james.aimonetti@gmail.com>
2015-04-08 21:07:41 -0700

    KAZOO-3434: share more code

--- core/whistle_services-1.0.0/src/services/wh_service_devices.erl
+++ core/whistle_services-1.0.0/src/services/wh_service_devices.erl
@@ -19,25 +19,9 @@
 %% @end
 %%--------------------------------------------------------------------
 -spec reconcile(wh_services:services()) -> wh_services:services().
--spec reconcile(wh_services:services(), ne_binary()) -> wh_services:services().
+-spec reconcile(wh_services:services(), api_binary()) -> wh_services:services().
 reconcile(Services) ->
-    AccountId = wh_services:account_id(Services),
-    AccountDb = wh_util:format_account_id(AccountId, 'encoded'),
-    ViewOptions = ['reduce'
-                   ,'group'
-                  ],
-    case couch_mgr:get_results(AccountDb, <<"services/devices">>, ViewOptions) of
-        {'error', _R} ->
-            lager:debug("unable to get current devices in service: ~p", [_R]),
-            Services;
-        {'ok', []} -> wh_services:reset_category(<<"devices">>, Services);
-        {'ok', JObjs} ->
-            lists:foldl(fun(JObj, S) ->
-                                Item = wh_json:get_value(<<"key">>, JObj),
-                                Quantity = wh_json:get_integer_value(<<"value">>, JObj, 0),
-                                wh_services:update(<<"devices">>, Item, Quantity, S)
-                        end, wh_services:reset_category(<<"devices">>, Services), JObjs)
-    end.
+    reconcile(Services, 'undefined').
 
 reconcile(Services, DeviceType) ->
     AccountId = wh_services:account_id(Services),
@@ -49,23 +33,31 @@ reconcile(Services, DeviceType) ->
         {'error', _R} ->
             lager:debug("unable to get current devices in service: ~p", [_R]),
             Services;
-        {'ok', []} -> wh_services:update(<<"devices">>, DeviceType, 1, Services);
+        {'ok', []} when DeviceType =:= 'undefined' -> wh_services:reset_category(<<"devices">>, Services);
+        {'ok', []} ->
+            wh_services:update(<<"devices">>, DeviceType, 1, Services);
         {'ok', JObjs} ->
-            S = lists:foldl(
-                  fun(JObj, S) ->
-                          Item = wh_json:get_value(<<"key">>, JObj),
-                          Quantity = wh_json:get_integer_value(<<"value">>, JObj, 0),
-                          case Item =:= DeviceType of
-                              'false' -> wh_services:update(<<"devices">>, Item, Quantity, S);
-                              'true' -> wh_services:update(<<"devices">>, Item, Quantity+1, S)
-                          end
-                  end
-                  ,wh_services:reset_category(<<"devices">>, Services)
-                  ,JObjs
-                 ),
-            Keys = couch_mgr:get_result_keys(JObjs),
-            case lists:member(DeviceType, Keys) of
-                'false' -> wh_services:update(<<"devices">>, DeviceType, 1, S);
-                'true' -> S
-            end
+            reconcile_devices(Services, DeviceType, JObjs)
+    end.
+
+-spec reconcile_devices(wh_services:services(), api_binary(), wh_json:objects()) ->
+                               wh_services:services().
+reconcile_devices(Services, DeviceType, JObjs) ->
+    S = lists:foldl(fun(JObj, S) -> reconcile_devices_fold(JObj, S, DeviceType) end
+                    ,wh_services:reset_category(<<"devices">>, Services)
+                    ,JObjs
+                   ),
+    Keys = couch_mgr:get_result_keys(JObjs),
+    case lists:member(DeviceType, Keys) of
+        'false' -> wh_services:update(<<"devices">>, DeviceType, 1, S);
+        'true' -> S
+    end.
+
+-spec reconcile_devices_fold(wh_json:object(), wh_services:services(), ne_binary()) ->
+                                    wh_services:services().
+reconcile_devices_fold(JObj, S, DeviceType) ->
+    Quantity = wh_json:get_integer_value(<<"value">>, JObj, 0),
+    case wh_json:get_value(<<"key">>, JObj) of
+        DeviceType -> wh_services:update(<<"devices">>, DeviceType, Quantity+1, S);
+        Item -> wh_services:update(<<"devices">>, Item, Quantity, S)
     end.
