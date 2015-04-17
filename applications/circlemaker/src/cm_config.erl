c465539a946b4e111aade16fd7b42bd981eb7926
SIPLABS, LLC <info@siplabs.ru>
2015-04-17 16:31:21 +0600

    Fix for correct generation of DocId for AAA configuration document for system_config.
    Change-Id: I3d53297c8a9f4ea22a4286f5418e63d4a1511748

--- applications/circlemaker/src/cm_config.erl
+++ applications/circlemaker/src/cm_config.erl
@@ -13,7 +13,7 @@
 -include_lib("whistle/src/wh_json.hrl").
 
 %% API
--export([get_aaa_mode/1, get_servers_list/1, init_doc/1]).
+-export([get_aaa_mode/1, get_servers_list/1, init_aaa_doc/1]).
 
 %% ===================================================================
 %% API functions
@@ -22,13 +22,11 @@
 %%--------------------------------------------------------------------
 %% @public
 %% @doc
-%% Returns default AAA configuration document 
+%% Returns default AAA configuration document.
 %% @end
 %%--------------------------------------------------------------------
--spec get_default_doc() -> wh_json:object().
--spec new_default_doc(account) -> wh_json:object(); (system) -> wh_json:object().
-
-get_default_doc() ->
+-spec new_default_aaa_doc() -> wh_json:object().
+new_default_aaa_doc() ->
     Servers = [
         {<<"enabled">>, false},
         {<<"name">>, <<"unique_server_name">>},
@@ -50,48 +48,62 @@ get_default_doc() ->
     ],
     wh_json:from_list(KV).
 
-new_default_doc(account) ->
-    wh_json:insert_value(<<"pvt_type">>, <<"aaa">>, get_default_doc());
-new_default_doc(system) ->
-    wh_json:insert_value(<<"pvt_type">>, <<"config">>, get_default_doc()).
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Returns AAA configuration document for system_config or an account.
+%% @end
+%%--------------------------------------------------------------------
+-spec new_default_aaa_doc(account) -> wh_json:object(); (system) -> wh_json:object().
+new_default_aaa_doc(account) ->
+    wh_json:insert_value(<<"pvt_type">>, <<"aaa">>, new_default_aaa_doc());
+new_default_aaa_doc(system) ->
+    Fields = [{<<"pvt_type">>, <<"config">>}, {<<"_id">>, <<"circlemaker">>}, {<<"id">>, <<"circlemaker">>}],
+    wh_json:set_values(Fields, new_default_aaa_doc()).
 
 %%--------------------------------------------------------------------
 %% @public
 %% @doc
-%% Initialize AAA configuration document in the database
-%% Returns resulted document
+%% Initialize AAA configuration document in the database.
+%% AccId should be something like <<"account%2Fd0%2Fb7%2Fcf8509c7f5f1c45c1c6f7bb2fc3a">>.
+%% Returns resulted document.
 %% @end
 %%--------------------------------------------------------------------
--spec init_doc
+-spec init_aaa_doc
     (ne_binary()) -> wh_json:object();
     (system_config) -> wh_json:object().
-init_doc(AccId) when is_binary(AccId) ->
-    case couch_mgr:get_results(AccId, <<"circlemaker/doc">>) of
-        {ok, [DocId]} ->
+init_aaa_doc(AccId) when is_binary(AccId) ->
+    io:format("Init account ~p circlemaker document...~n", [AccId]),
+    ok = couch_mgr:revise_views_from_folder(AccId, ?APP_NAME),
+    case couch_mgr:get_results(AccId, <<"aaa/fetch_doc">>) of
+        {ok, [{[{<<"id">>, DocId},
+                {<<"key">>, _},
+                {<<"value">>, _}]}]} ->
+            io:format("Open existing account ~p circlemaker document...~n", [AccId]),
             couch_mgr:open_cache_doc(AccId, DocId);
         {ok, []} ->
-            Doc = new_default_doc(account),
-            wh_json:insert_value(<<"account_id">>, <<"aaa">>, get_default_doc()),
+            Doc = wh_json:insert_value(<<"account_id">>, <<"aaa">>, new_default_aaa_doc(account)),
+            io:format("Create new account ~p circlemaker document...~n", [AccId]),
             couch_mgr:save_doc(?WH_CONFIG_DB, Doc);
         {error, Reason} ->
             {error, Reason}
     end;
-init_doc(system_config) ->
+init_aaa_doc(system_config) ->
     io:format("Init system_config circlemaker document...~n"),
     case couch_mgr:open_cache_doc(?WH_CONFIG_DB, ?APP_NAME) of
         {ok, Doc} ->
-            io:format("System_config circlemaker document already exist~n"),
+            io:format("Open existing system_config circlemaker document...~n"),
             {ok, Doc};
         {error, not_found} ->
-            io:format("Create new system_config circlemaker document~n"),
-            Doc = new_default_doc(system),
+            io:format("Create new system_config circlemaker document...~n"),
+            Doc = new_default_aaa_doc(system),
             couch_mgr:save_doc(?WH_CONFIG_DB, Doc)
     end.
 
 %%--------------------------------------------------------------------
 %% @public
 %% @doc
-%% Get AAA-mode from document
+%% Get AAA-mode from document.
 %% @end
 %%--------------------------------------------------------------------
 -spec get_aaa_mode(wh_json:object()) -> on|off|inherit.
@@ -101,7 +113,7 @@ get_aaa_mode(Doc) ->
 %%--------------------------------------------------------------------
 %% @public
 %% @doc
-%% Get servers list
+%% Get servers list.
 %% @end
 %%--------------------------------------------------------------------
 -spec get_servers_list(wh_json:object()) -> on|off|inherit.
