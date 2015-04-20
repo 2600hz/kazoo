020422acc7ac6fe44dd7d09bf54eaaecc9ae0d32
James Aimonetti <james.aimonetti@gmail.com>
2015-04-20 11:49:50 -0700

    KAZOO-645: formatting

--- core/braintree-1.0.0/src/braintree_request.erl
+++ core/braintree-1.0.0/src/braintree_request.erl
@@ -182,32 +182,38 @@ braintree_server_url(Env) ->
 verify_response(Xml) ->
     case xmerl_xpath:string("/api-error-response", Xml) of
         [] -> Xml;
-        _ ->
-            lager:debug("braintree api error response"),
-            Errors = [#bt_error{code = wh_util:get_xml_value("/error/code/text()", Error)
-                                ,message = wh_util:get_xml_value("/error/message/text()", Error)
-                                ,attribute = wh_util:get_xml_value("/error/attribute/text()", Error)}
-                      || Error <- xmerl_xpath:string("/api-error-response/errors//errors/error", Xml)
-                     ],
-            Verif = #bt_verification{verification_status =
-                                         wh_util:get_xml_value("/api-error-response/verification/status/text()", Xml)
-                                     ,processor_response_code =
-                                         wh_util:get_xml_value("/api-error-response/verification/processor-response-code/text()", Xml)
-                                     ,processor_response_text =
-                                         wh_util:get_xml_value("/api-error-response/verification/processor-response-text/text()", Xml)
-                                     ,cvv_response_code =
-                                         wh_util:get_xml_value("/api-error-response/verification/cvv-response-code/text()", Xml)
-                                     ,avs_response_code =
-                                         wh_util:get_xml_value("/api-error-response/verification/avs-error-response-code/text()", Xml)
-                                     ,postal_response_code =
-                                         wh_util:get_xml_value("/api-error-response/verification/avs-postal-code-response-code/text()", Xml)
-                                     ,street_response_code =
-                                         wh_util:get_xml_value("/api-error-response/verification/avs-street-address-response-code/text()", Xml)
-                                     ,gateway_rejection_reason =
-                                         wh_util:get_xml_value("/api-error-response/verification/gateway-rejection-reason/text()", Xml)
-                                    },
-            braintree_util:error_api(#bt_api_error{errors = Errors
-                                                   ,verification=Verif
-                                                   ,message = wh_util:get_xml_value("/api-error-response/message/text()", Xml)
-                                                  })
+        _ -> error_response(Xml)
     end.
+
+-spec error_response(bt_xml()) -> bt_xml().
+error_response(Xml) ->
+    lager:debug("braintree api error response"),
+    Errors = [#bt_error{code = wh_util:get_xml_value("/error/code/text()", Error)
+                        ,message = wh_util:get_xml_value("/error/message/text()", Error)
+                        ,attribute = wh_util:get_xml_value("/error/attribute/text()", Error)
+                       }
+              || Error <- xmerl_xpath:string("/api-error-response/errors//errors/error", Xml)
+             ],
+    Verif = #bt_verification{verification_status =
+                                 wh_util:get_xml_value("/api-error-response/verification/status/text()", Xml)
+                             ,processor_response_code =
+                                 wh_util:get_xml_value("/api-error-response/verification/processor-response-code/text()", Xml)
+                             ,processor_response_text =
+                                 wh_util:get_xml_value("/api-error-response/verification/processor-response-text/text()", Xml)
+                             ,cvv_response_code =
+                                 wh_util:get_xml_value("/api-error-response/verification/cvv-response-code/text()", Xml)
+                             ,avs_response_code =
+                                 wh_util:get_xml_value("/api-error-response/verification/avs-error-response-code/text()", Xml)
+                             ,postal_response_code =
+                                 wh_util:get_xml_value("/api-error-response/verification/avs-postal-code-response-code/text()", Xml)
+                             ,street_response_code =
+                                 wh_util:get_xml_value("/api-error-response/verification/avs-street-address-response-code/text()", Xml)
+                             ,gateway_rejection_reason =
+                                 wh_util:get_xml_value("/api-error-response/verification/gateway-rejection-reason/text()", Xml)
+                            },
+    braintree_util:error_api(
+      #bt_api_error{errors=Errors
+                    ,verification=Verif
+                    ,message=wh_util:get_xml_value("/api-error-response/message/text()", Xml)
+                   }
+     ).
