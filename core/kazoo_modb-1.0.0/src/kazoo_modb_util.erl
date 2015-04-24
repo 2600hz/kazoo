469d40bf039ff67d1d980fb455566bf6db274a76
James Aimonetti <james.aimonetti@gmail.com>
2015-04-24 14:58:02 -0700

    run-tests: move kazoo_modb_util tests to test/

--- core/kazoo_modb-1.0.0/src/kazoo_modb_util.erl
+++ core/kazoo_modb-1.0.0/src/kazoo_modb_util.erl
@@ -1,5 +1,5 @@
 %%%-------------------------------------------------------------------
-%%% @copyright (C) 2011-2014, 2600HZ, INC
+%%% @copyright (C) 2011-2015, 2600HZ, INC
 %%% @doc
 %%%
 %%% @end
@@ -46,37 +46,3 @@ split_account_mod(<<Account:48/binary, "-",  Year:4/binary, Month:2/binary>>) ->
      ,wh_util:to_integer(Year)
      ,wh_util:to_integer(Month)
     }.
-
--ifdef(TEST).
--include_lib("eunit/include/eunit.hrl").
-
-prev_year_month_test() ->
-    {Year3, Month3} = {1776, 3},
-    {Year2, Month2} = prev_year_month(Year3, Month3),
-    {Year1, Month1} = prev_year_month(Year2, Month2),
-    {Year12, Month12} = prev_year_month(Year1, Month1),
-    {Year11, Month11} = prev_year_month(Year12, Month12),
-
-    ?assertEqual({1776, 3}, {Year3, Month3}),
-    ?assertEqual({1776, 2}, {Year2, Month2}),
-    ?assertEqual({1776, 1}, {Year1, Month1}),
-    ?assertEqual({1775, 12}, {Year12, Month12}),
-    ?assertEqual({1775, 11}, {Year11, Month11}).
-
-split_account_mod_test() ->
-    DB = <<"account%2F0a%2F93%2F6fc79bdb4a8c38e6089ab44ad030-201405">>,
-    ID = <<"account/0a/93/6fc79bdb4a8c38e6089ab44ad030-201405">>,
-
-    {DbAccountId, DbYear, DbMonth} = split_account_mod(DB),
-    {IdAccountId, IdYear, IdMonth} = split_account_mod(ID),
-
-    ?assertEqual(2014, DbYear),
-    ?assertEqual(DbYear, IdYear),
-
-    ?assertEqual(5, DbMonth),
-    ?assertEqual(DbMonth, IdMonth),
-
-    ?assertEqual(<<"0a936fc79bdb4a8c38e6089ab44ad030">>, DbAccountId),
-    ?assertEqual(DbAccountId, IdAccountId).
-
--endif.
