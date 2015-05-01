7ae4f2cf0614afd7af46fed97fa68d21f47bcd3f
James Aimonetti <james.aimonetti@gmail.com>
2015-05-01 10:19:00 -0700

    xref: remove lager call

--- core/kazoo_documents-1.0.0/src/kz_documents.hrl
+++ core/kazoo_documents-1.0.0/src/kz_documents.hrl
@@ -1,7 +1,6 @@
 -ifndef(KZ_DOCUMENTS_HRL).
 
 -include_lib("whistle/include/wh_types.hrl").
--include_lib("whistle/include/wh_log.hrl").
 -include_lib("kazoo_documents/include/kazoo_documents.hrl").
 
 -define(KZ_DOCUMENTS_HRL, 'true').
