896e15a45ef61cac5eb01c679b5343c33533a447
James Aimonetti <james.aimonetti@gmail.com>
2015-05-08 12:51:20 -0700

    KAZOO-3627: let dev start script take node name as param

--- scripts/dev-start-apps.sh
+++ scripts/dev-start-apps.sh
@@ -2,6 +2,13 @@
 
 cd `dirname $0`
 
+if [ -z "$1"]
+then
+  NODE_NAME=whistle_apps
+else
+  NODE_NAME=$1
+fi
+
 export ERL_CRASH_DUMP=$PWD/../$(date +%s)_apps_erl_crash.dump
 export ERL_LIBS=$PWD/../deps:$PWD/../core:$PWD/../applications/
-exec erl -name whistle_apps -args_file /etc/kazoo/vm.args -s reloader -s whistle_apps
+exec erl -name $NODE_NAME -args_file /etc/kazoo/vm.args -s reloader -s whistle_apps
