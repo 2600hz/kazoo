b4cc60c7948921ab6a48c45021578f80094b77f0
lazedo <luis.azedo@factorlusitano.com>
2015-05-12 16:31:08 +0100

    KAZOO-3638 pass doc id to sms processor so it can be properly saved KAZOO-3636 set API-Call so sms resources knows if caller-id should be set

--- applications/doodle/src/doodle_api.erl
+++ applications/doodle/src/doodle_api.erl
@@ -80,5 +80,8 @@ route_req_ccvs(FetchId, JObj) ->
        ,{<<"Owner-ID">>, wh_json:get_value(<<"pvt_owner_id">>, JObj)}
        ,{<<"Channel-Authorized">>, 'true'}
        ,{<<"Doc-Revision">>, wh_json:get_value(<<"_rev">>, JObj)}
+       ,{<<"Doc-ID">>, wh_json:get_value(<<"_id">>, JObj)}
        ,{<<"Scheduled-Delivery">>, wh_json:get_value(<<"scheduled">>, JObj)}
+       ,{<<"API-Call">>, 'true'}
+       | wh_json:to_proplist(<<"pvt_address_options">>, JObj)
       ]).
