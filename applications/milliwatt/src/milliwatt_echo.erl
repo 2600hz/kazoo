f4f1a0cf0b4607cebe8abe61df97a3fa6ea8b145
karl anderson <karl@2600hz.com>
2015-05-20 14:47:34 -0400

    KAZOO-3752: queued_hangup can cause the tones to be skipped

--- applications/milliwatt/src/milliwatt_echo.erl
+++ applications/milliwatt/src/milliwatt_echo.erl
@@ -15,13 +15,13 @@
 -define(DURATION, 10000).
 
 exec(Call) ->
-	lager:info("milliwatt execute action echo", []),
-	whapps_call_command:answer(Call),
-	whapps_call_command:echo(Call),
-	timer:sleep(get_duration()),
-	whapps_call_command:hangup(Call).
+    lager:info("milliwatt execute action echo", []),
+    whapps_call_command:answer(Call),
+    whapps_call_command:echo(Call),
+    timer:sleep(get_duration()),
+    whapps_call_command:hangup(Call).
 
 -spec get_duration() -> integer().
 get_duration() ->
-	JObj = whapps_config:get_non_empty(<<"milliwatt">>, <<"echo">>),
-	wh_json:get_integer_value(<<"duration">>, JObj, ?DURATION).
+    JObj = whapps_config:get_non_empty(<<"milliwatt">>, <<"echo">>),
+    wh_json:get_integer_value(<<"duration">>, JObj, ?DURATION).
--- applications/milliwatt/src/milliwatt_tone.erl
+++ applications/milliwatt/src/milliwatt_tone.erl
@@ -16,18 +16,22 @@
 -define(DURATION, [<<"10000">>]).
 
 exec(Call) ->
-	Tone = get_tone(),
-	lager:info("milliwatt execute action tone", []),
-	whapps_call_command:answer(Call),
-	whapps_call_command:tones([Tone], Call),
-	whapps_call_command:queued_hangup(Call).
+    Tone = get_tone(),
+    Duration = wh_json:get_integer_value(<<"Duration-ON">>, Tone, ?DURATION),
+    lager:info("milliwatt execute action tone", []),
+    whapps_call_command:answer(Call),
+    whapps_call_command:tones([Tone], Call),
+    timer:sleep(Duration),
+    whapps_call_command:hangup(Call).
 
 -spec get_tone() -> wh_json:json().
 get_tone() ->
-	JObj = whapps_config:get_non_empty(<<"milliwatt">>, <<"tone">>),
-	Hz = wh_json:get_value(<<"frequencies">>, JObj, ?FREQUENCIES),
-	Duration = wh_json:get_value(<<"duration">>, JObj, ?DURATION),
-	wh_json:from_list([{<<"Frequencies">>, Hz}
-                      ,{<<"Duration-ON">>, Duration}
-                      ,{<<"Duration-OFF">>, <<"1000">>}
-                      ]).
+    JObj = whapps_config:get_non_empty(<<"milliwatt">>, <<"tone">>),
+    Hz = wh_json:get_value(<<"frequencies">>, JObj, ?FREQUENCIES),
+    Duration = wh_json:get_value(<<"duration">>, JObj, ?DURATION),
+    wh_json:from_list(
+      [{<<"Frequencies">>, Hz}
+      ,{<<"Duration-ON">>, Duration}
+      ,{<<"Duration-OFF">>, <<"1000">>}
+      ]
+     ).
