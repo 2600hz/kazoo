f8485e59dbd338839dd4d20def708f80b500d306
James Aimonetti <james.aimonetti@gmail.com>
2015-05-27 15:29:44 -0700

    KAZOO-3808: don't default langauge in schema for users/devices

--- applications/callflow/src/cf_route_win.erl
+++ applications/callflow/src/cf_route_win.erl
@@ -216,7 +216,7 @@ set_language(Call) ->
     Default = wh_media_util:prompt_language(whapps_call:account_id(Call)),
     case cf_endpoint:get(Call) of
         {'ok', Endpoint} ->
-            Language = wh_json:get_value(<<"language">>, Endpoint, Default),
+            Language = kz_device:language(Endpoint, Default),
             lager:debug("setting language '~s' for this call", [Language]),
             whapps_call:set_language(wh_util:to_lower_binary(Language), Call);
         {'error', _E} ->
