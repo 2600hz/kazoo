4f23e289d8bfb441e3595eab6980363808e8de87
Sergey Korobkov <skorobkov@dinkor.net>
2015-05-27 09:01:52 +0000

    Incorrect bindings

--- applications/trunkstore/src/trunkstore_sup.erl
+++ applications/trunkstore/src/trunkstore_sup.erl
@@ -18,11 +18,10 @@
 
 -include("ts.hrl").
 
--define(ORIGIN_BINDINGS, [[{'type', <<"account">>}
-                           ,{'type', <<"connectivity">>}
-                           ,{'type', <<"sys_info">>}
-                           ,{'type', <<"number">>}
-                          ]
+-define(ORIGIN_BINDINGS, [[{'type', <<"account">>}]
+                           ,[{'type', <<"connectivity">>}]
+                           ,[{'type', <<"sys_info">>}]
+                           ,[{'type', <<"number">>}]
                          ]).
 -define(CACHE_PROPS, [{'origin_bindings', ?ORIGIN_BINDINGS}]).
 
