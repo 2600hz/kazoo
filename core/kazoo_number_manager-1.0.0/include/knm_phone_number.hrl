5ec9df545a9c8f6950d25311682fe96bb8c9a6af
Peter Defebvre <macpie59@gmail.com>
2015-06-09 21:07:23 +0000

    KAZOO-3800: First draft for kazoo-phone_number

---
+++ core/kazoo_number_manager-1.0.0/include/knm_phone_number.hrl
@@ -0,0 +1,35 @@
+-ifndef(KNM_NUMBER_MANAGER_HRL).
+
+-include_lib("whistle/include/wh_log.hrl").
+-include_lib("whistle/include/wh_types.hrl").
+
+-record(number, {number :: ne_binary()
+                 ,number_db :: ne_binary()
+                 ,assigned_to :: api_binary()
+                 ,prev_assigned_to :: api_binary()
+                 ,used_by :: api_binary()
+                 ,features = wh_json:new() :: wh_json:object()
+                 ,state :: ne_binary()
+                 ,reserve_history = [] :: ne_binaries()
+                 ,ported_in = 'false' :: boolean()
+                 ,module_name :: ne_binary()
+                 ,region :: ne_binary()
+                 ,doc :: wh_json:object()
+                }).
+
+-define(PVT_DB_NAME, <<"pvt_db_name">>).
+-define(PVT_ASSIGNED_TO, <<"pvt_assigned_to">>).
+-define(PVT_PREVIOUSLY_ASSIGNED_TO, <<"pvt_previously_assigned_to">>).
+-define(PVT_USED_BY, <<"pvt_used_by">>).
+-define(PVT_FEATURES, <<"pvt_features">>).
+-define(PVT_STATE, <<"pvt_state">>).
+-define(PVT_RESERVE_HISTORY, <<"pvt_reserve_history">>).
+-define(PVT_PORTED_IN, <<"pvt_ported_in">>).
+-define(PVT_MODULE_NAME, <<"pvt_module_name">>).
+-define(PVT_REGION, <<"pvt_region">>).
+-define(PVT_MODIFIED, <<"pvt_modified">>).
+-define(PVT_CREATED, <<"pvt_created">>).
+-define(PVT_TYPE, <<"pvt_type">>).
+
+-define(KNM_NUMBER_MANAGER_HRL, 'true').
+-endif.
