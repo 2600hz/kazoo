16f04b76a560ab808578a72160c5fa060ed09cab
Peter Defebvre <macpie59@gmail.com>
2015-06-10 17:55:12 +0000

    KAZOO-3801: Add knm_converter

---
+++ core/kazoo_number_manager-1.0.0/src/carriers/knm_carrier_skel.erl
@@ -0,0 +1,11 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2015, 2600Hz INC
+%%% @doc
+%%%
+%%% Handle client requests for phone_number documents
+%%%
+%%% @end
+%%% @contributors
+%%%   Peter Defebvre
+%%%-------------------------------------------------------------------
+-module(knm_carrier_skel).
