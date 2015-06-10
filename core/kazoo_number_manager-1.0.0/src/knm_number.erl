294e057195fe0db73d7ae0f93a0d8aeef45a6258
Peter Defebvre <macpie59@gmail.com>
2015-06-10 22:00:04 +0000

    KAZOO-3804: Adding move function

--- core/kazoo_number_manager-1.0.0/src/knm_number.erl
+++ core/kazoo_number_manager-1.0.0/src/knm_number.erl
@@ -17,13 +17,18 @@
 %% @doc
 %% @end
 %%--------------------------------------------------------------------
+-spec move(ne_binary(), ne_binary()) -> {'ok', number()} | {'error', _}.
 move(Num, MoveTo) ->
-    Number = knm_phone_number:fetch(Num),
-    AccountId = wh_util:format_account_id(MoveTo, 'raw'),
-    AssignedTo = knm_phone_number:assigned_to(Number),
-    Props = [
-        {fun knm_phone_number:set_assigned_to/2, AccountId}
-        ,{fun knm_phone_number:set_prev_assigned_to/2, AssignedTo}
-    ],
-    UpdatedNumber = knm_phone_number:setters(Number, Props),
-    knm_phone_number:save(UpdatedNumber).
+    lager:debug("trying to move ~s to ~s", [Num, MoveTo]),
+    case knm_phone_number:fetch(Num) of
+        {'error', _R}=E -> E;
+        {'ok', Number} ->
+            AccountId = wh_util:format_account_id(MoveTo, 'raw'),
+            AssignedTo = knm_phone_number:assigned_to(Number),
+            Props = [
+                {fun knm_phone_number:set_assigned_to/2, AccountId}
+                ,{fun knm_phone_number:set_prev_assigned_to/2, AssignedTo}
+            ],
+            UpdatedNumber = knm_phone_number:setters(Number, Props),
+            knm_phone_number:save(UpdatedNumber)
+    end.
