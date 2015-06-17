1770fed37ed7199b1de135a061735108bd626c03
SIPLABS, LLC <info@siplabs.ru>
2015-06-17 17:46:26 +0600

    Added worker which process an AAA message and make corresponding request to an AAA server using eradius library. Also added pool manager to manage workers and redirect requests/responses to the right place
    Change-Id: I8dc57053e06ad0fc204a080095f27cbf84d26fcc

--- applications/circlemaker/src/cm_pool_mgr.erl
+++ applications/circlemaker/src/cm_pool_mgr.erl
@@ -11,7 +11,8 @@
 
 -include("circlemaker.hrl").
 
--record(state, {workers =[]}).
+-record(state, {workers = [] :: list(pid())}).
+-type pool_mgr_state() :: #state{}.
 
 % TODO: should be part of timeout
 -define(CM_TIMEOUT, 5000).
@@ -21,54 +22,78 @@
 
 -spec start_link() -> startlink_ret().
 start_link() ->
-    lager:debug([{'trace', 'true'}], "", []),
+    lager:debug(""),
     gen_server:start_link({'local', ?MODULE}, ?MODULE, [], []).
 
--spec do_request(api_object()) -> 'ok'.
+-spec do_request(wh_json:object()) -> 'ok'.
 do_request(Request) ->
-    lager:debug([{'trace', 'true'}], "Request=~p~n", [Request]),
+    lager:debug("Request=~p~n"),
     gen_server:cast(?MODULE, {'request', Request}).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Stores a new worker process into the Pool Manager's state
+%% @end
+%%--------------------------------------------------------------------
+-spec insert_worker(pid(), pool_mgr_state()) -> pool_mgr_state().
+insert_worker(Worker, State) ->
+    State#state{workers=[Worker | State#state.workers]}.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Removes a new worker process from the Pool Manager's state
+%% @end
+%%--------------------------------------------------------------------
+-spec remove_worker(pid(), pool_mgr_state()) -> pool_mgr_state().
+remove_worker(Worker, State) ->
+    State#state{workers=lists:delete(Worker, State#state.workers)}.
 
 %% Internal API
 
 -spec init([]) -> {'ok', tuple()}.
 init([]) ->
-    lager:debug([{'trace', 'true'}], "", []),
+    lager:debug(""),
     {'ok', #state{}}.
 
 handle_call(Request, _From, State) ->
-    lager:debug([{'trace', 'true'}], "Request=~p~nState=~p~n", [Request, State]),
+    lager:debug("Request=~p~nState=~p~n", [Request, State]),
     {'reply', {'error', 'not_implemented'}, State, ?CM_TIMEOUT}.
 
 handle_cast({'request', JObj}, State) ->
-    lager:debug([{'trace', 'true'}], "JObj:~n~p~nState:~n~p~n", [JObj, State]),
-    {'noreply', State#state{workers=dist_workers(JObj, State#state.workers)}, ?CM_TIMEOUT};
+    lager:debug("JObj:~n~p~nState:~n~p~n", [JObj, State]),
+    {'noreply', dist_workers(JObj, State), ?CM_TIMEOUT};
 handle_cast({'response', Response, Worker}, State) ->
-    lager:debug([{'trace', 'true'}], "Response=~p~nWorker=~p~nState=~p~n", [Response, Worker, State]),
+    lager:debug("Response=~p~nWorker=~p~nState=~p~n", [Response, Worker, State]),
     poolboy:checkin(?WORKER_POOL, Worker),
     % send response to queue, which is aaa_listener of the registrar app
     Queue = props:get_value(<<"Response-Queue">>, Response),
     wapi_aaa:publish_resp(Queue, Response),
-    {'noreply', State#state{workers=[W || W <- State#state.workers, Worker =/= W]}, ?CM_TIMEOUT};
+    {'noreply', remove_worker(Worker, State), ?CM_TIMEOUT};
 handle_cast({'error', Response, Worker}, State) ->
-    lager:debug([{'trace', 'true'}], "Response=~p~nWorker=~p~nState=~p~n", [Response, Worker, State]),
+    lager:debug("Response=~p~nWorker=~p~nState=~p~n", [Response, Worker, State]),
     poolboy:checkin(?WORKER_POOL, Worker),
-    {'noreply', State#state{workers=[W || W <- State#state.workers, Worker =/= W]}, ?CM_TIMEOUT};
+    {'noreply', remove_worker(Worker, State), ?CM_TIMEOUT};
 handle_cast(Message, State) ->
-    lager:debug([{'trace', 'true'}], "Message=~p~nState=~p~n", [Message, State]),
+    lager:debug("Message=~p~nState=~p~n", [Message, State]),
     {'noreply', State, ?CM_TIMEOUT}.
 
-dist_workers(JObj, Workers) ->
-    lager:debug([{'trace', 'true'}], "JObj=~p~nWorkers=~p~n", [JObj, Workers]),
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts new worker for processing a request
+%% @end
+%%--------------------------------------------------------------------
+-spec dist_workers(wh_json:object(), pool_mgr_state()) -> pool_mgr_state().
+dist_workers(JObj, State) ->
+    lager:debug("JObj=~p~nWorkers=~p~n", [JObj, State]),
     case catch poolboy:checkout(?WORKER_POOL) of
         Worker when is_pid(Worker) ->
-            gen_server:cast(Worker, {'authn_req', self(), JObj}),
-            [Worker | Workers];
-        _Else -> Workers
+            cm_worker:handle_authn(Worker, JObj, self()),
+            insert_worker(Worker, State);
+        _Else -> State
     end.
 
 handle_info(Info, State) ->
-    lager:debug([{'trace', 'true'}], "Info=~p~nState=~p~n", [Info, State]),
+    lager:debug("Info=~p~nState=~p~n", [Info, State]),
     {'reply', {'error', 'not_implemented'}, State, ?CM_TIMEOUT}.
 
 terminate(Reason, _State) ->
---
+++ applications/circlemaker/src/cm_worker.erl
@@ -0,0 +1,138 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2010-2015, 2600Hz
+%%% @doc
+%%%  The module is main application supervisor module
+%%% @end
+%%% @contributors
+%%%   SIPLABS, LLC (Vladimir Potapev)
+%%%-------------------------------------------------------------------
+-module(cm_worker).
+
+-behaviour(gen_server).
+
+-include("circlemaker.hrl").
+-include_lib("eradius/include/eradius_lib.hrl").
+
+%% API
+-export([init/1
+         ,handle_call/3
+         ,handle_cast/2
+         ,handle_info/2
+         ,terminate/2
+         ,code_change/3
+         ,start_link/1
+         ,handle_authn/3]).
+
+start_link(_) ->
+    gen_server:start_link(?MODULE, [], []).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Make a AuthN request to a worker
+%% @end
+%%--------------------------------------------------------------------
+-spec handle_authn(pid(), wh_json:object(), pid()) -> ok.
+handle_authn(Worker, JObj, Caller) ->
+    gen_server:cast(Worker, {'authn_req', Caller, JObj}).
+
+init(_) ->
+    {'ok', []}.
+
+handle_call(_Request, _From, State) ->
+    {'reply', {'error', 'not_implemented'}, State}.
+
+handle_cast({'authn_req', SenderPid, JObj}, State) ->
+    AccountId = wh_json:get_value(<<"Account-Id">>, JObj, <<"system_config">>),
+    lager:debug("JObj=~p~nAccountId=~p~n", [JObj, AccountId]),
+    case maybe_aaa_mode(JObj, AccountId) of
+        {'ok', Response} ->
+            gen_server:cast(SenderPid, {'response', Response, self()});
+        {'error', Reason} ->
+            gen_server:cast(SenderPid, {'error', Reason, self()})
+    end,
+    {'noreply', State};
+handle_cast(_Message, State) ->
+    lager:debug("Message=~p~n"),
+    {'noreply', State}.
+
+handle_info(_Info, State) ->
+    {'reply', {'error', 'not_implemented'}, State}.
+
+terminate(_Reason, _State) ->
+    [].
+
+code_change(_OldVsn, State, _Extra) ->
+    {'ok', State}.
+
+maybe_aaa_mode(_JObj, 'undefined') ->
+    {'error', 'no_respond'};
+maybe_aaa_mode(JObj, <<"system_config">> = AccountId) ->
+    lager:debug("AccountId=~p~n", [AccountId]),
+    % used system_config-related AAA settings
+    AAAProtocol = wh_json:get_value(<<"AAA-Protocol">>, JObj),
+    % add system_config as source account
+    JObj1 = wh_json:set_value(<<"Account-Id">>, AccountId, JObj),
+    {ok, AaaDoc} = couch_mgr:open_doc(?WH_CONFIG_DB, <<"circlemaker">>),
+    case wh_json:get_value(<<"aaa_mode">>, AaaDoc) of
+        <<"off">> ->
+            % AAA functionality is off for this account
+            {'ok', 'aaa_mode_off'};
+        <<"on">> ->
+            % 'on' mode
+            Result = maybe_suitable_servers(JObj1, AaaDoc, AAAProtocol, 'undefined'),
+            {'ok', Result}
+    end;
+maybe_aaa_mode(JObj, AccountId) when is_binary(AccountId) ->
+    AAAProtocol = wh_json:get_value(<<"AAA-Protocol">>, JObj),
+    {ok, Account} = couch_mgr:open_doc(?WH_ACCOUNTS_DB, AccountId),
+    Value = wh_json:get_value(<<"value">>, Account),
+    AccountDb = wh_json:get_value(<<"account_db">>, Value, Account),
+    {ok, AaaDoc} = couch_mgr:open_doc(AccountDb, <<"aaa">>),
+    case wh_json:get_value(<<"aaa_mode">>, AaaDoc) of
+        <<"off">> ->
+            % AAA functionality is off for this account
+            {'ok', 'aaa_mode_off'};
+        <<"on">> ->
+            % AAA functionality is on for this account
+            ParentAccountId = cm_util:parent_account_id(AccountDb),
+            Result = maybe_suitable_servers(JObj, AaaDoc, AAAProtocol, ParentAccountId),
+            {'ok', Result};
+        <<"inherit">> ->
+            % AAA functionality is in the 'inherit' mode for this account
+            ParentAccountId = cm_util:parent_account_id(AccountDb),
+            Result = maybe_suitable_servers(JObj, AaaDoc, AAAProtocol, ParentAccountId),
+            {'ok', Result}
+    end.
+
+maybe_suitable_servers(JObj, AaaDoc, AAAProtocol, ParentAccountId) ->
+    Servers = [S || S <- wh_json:get_value(<<"servers">>, AaaDoc),
+        wh_json:get_value(<<"enabled">>, S) == 'true', wh_json:get_value(<<"aaa_engine">>, S) == AAAProtocol],
+    lager:debug("AAAProtocol=~p~nServersBefore=~p~nServersAfter=~p~n",
+        [AAAProtocol, wh_json:get_value(<<"servers">>, AaaDoc), Servers]),
+    maybe_server_request(Servers, JObj, AaaDoc, ParentAccountId).
+
+maybe_server_request([], JObj, AaaDoc, ParentAccountId) ->
+    case wh_json:get_value(<<"aaa_mode">>, AaaDoc) of
+        <<"inherit">> ->
+            % we are in the "inherit" mode so we should get parent account and use its AAA settings
+            maybe_aaa_mode(JObj, ParentAccountId);
+        <<"on">> ->
+            % 'on' mode
+            {'error', 'no_respond'}
+    end;
+maybe_server_request([Server | Servers], JObj, AaaDoc, ParentAccountId) ->
+    AccountId = wh_json:get_value(<<"Account-Id">>, JObj),
+    Address = cm_util:binary_ip_to_tuple(wh_json:get_value(<<"address">>, Server)),
+    Port = wh_json:get_value(<<"port">>, Server),
+    Secret = wh_json:get_value(<<"secret">>, Server),
+    ParamList = [{eradius_dict:lookup_by_name(AccountId, 'attribute2', Key), Value} ||
+                    {Key, Value} <- wh_json:to_proplist(JObj)],
+    Request = eradius_lib:set_attributes(#radius_request{cmd = request}, ParamList),
+    lager:debug("Address=~p~nPort=~p~nSecret=~p~nRequest=~p~n", [Address, Port, Secret, Request]),
+    case eradius_client:send_request({Address, Port, Secret}, Request) of
+        {'ok', Result} ->
+            Response = eradius_lib:decode_request(Result, Secret),
+            {'ok', Response};
+        _Error ->
+            maybe_server_request(Servers, JObj, AaaDoc, ParentAccountId)
+    end.
