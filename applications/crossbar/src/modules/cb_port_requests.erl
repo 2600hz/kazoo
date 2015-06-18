b85495ba7b6e24ced7e4ff918db01fbf8fb9ccdd
James Aimonetti <james.aimonetti@gmail.com>
2015-06-18 13:50:27 -0700

    KAZOO-3503: specs and types, some refactoring

--- applications/crossbar/src/modules/cb_port_requests.erl
+++ applications/crossbar/src/modules/cb_port_requests.erl
@@ -325,16 +325,19 @@ validate(Context, Id, ?PATH_TOKEN_LOA) ->
 validate(Context, Id, ?PORT_ATTACHMENT, AttachmentId) ->
     validate_attachment(Context, Id, AttachmentId, cb_context:req_verb(Context)).
 
+-spec validate_port_requests(cb_context:context(), http_method()) ->
+                                    cb_context:context().
 validate_port_requests(Context, ?HTTP_GET) ->
     validate_get_port_requests(
-        Context
-        ,props:get_value(<<"accounts">>, cb_context:req_nouns(Context))
-        ,cb_context:req_value(Context, <<"by_number">>)
-    );
+      Context
+      ,props:get_value(<<"accounts">>, cb_context:req_nouns(Context))
+      ,cb_context:req_value(Context, <<"by_number">>)
+     );
 validate_port_requests(Context, ?HTTP_PUT) ->
     create(Context).
 
-
+-spec validate_get_port_requests(cb_context:context(), path_tokens(), api_binary()) ->
+                                        cb_context:context().
 validate_get_port_requests(Context, [_AccountId], 'undefined') ->
     summary(Context);
 validate_get_port_requests(Context, [_AccountId], ByNumber) ->
@@ -344,7 +347,8 @@ validate_get_port_requests(Context, [_AccountId, ?PORT_DESCENDANTS], 'undefined'
 validate_get_port_requests(Context, [_AccountId, ?PORT_DESCENDANTS], ByNumber) ->
     read_descendants_by_number(Context, ByNumber).
 
-
+-spec validate_port_request(cb_context:context(), ne_binary(), http_method()) ->
+                                   cb_context:context().
 validate_port_request(Context, Id, ?HTTP_GET) ->
     read(Context, Id);
 validate_port_request(Context, Id, ?HTTP_POST) ->
@@ -352,6 +356,8 @@ validate_port_request(Context, Id, ?HTTP_POST) ->
 validate_port_request(Context, Id, ?HTTP_DELETE) ->
     is_deletable(crossbar_doc:load(Id, cb_context:set_account_db(Context, ?KZ_PORT_REQUESTS_DB))).
 
+-spec validate_port_request(cb_context:context(), ne_binary(), ne_binary(), http_method()) ->
+                                   cb_context:context().
 validate_port_request(Context, Id, ?PORT_SUBMITTED, ?HTTP_POST) ->
     maybe_move_state(Context, Id, ?PORT_SUBMITTED);
 validate_port_request(Context, Id, ?PORT_PENDING, ?HTTP_POST) ->
@@ -730,64 +736,80 @@ summary(Context) ->
 %% @doc
 %% @end
 %%--------------------------------------------------------------------
--spec summary_by_number(cb_context:context(), ne_binary()) -> cb_context:context().
+-spec summary_by_number(cb_context:context(), ne_binary()) ->
+                               cb_context:context().
 summary_by_number(Context, Number) ->
-    ViewOptions = [{'keys', build_keys(Context, Number)}, 'include_docs'],
+    ViewOptions = [{'keys', build_keys(Context, Number)}
+                   ,'include_docs'
+                  ],
     crossbar_doc:load_view(
-        ?ALL_PORT_REQ_NUMBERS
-        ,ViewOptions
-        ,cb_context:set_account_db(Context, ?KZ_PORT_REQUESTS_DB)
-        ,fun normalize_view_results/2
-    ).
+      ?ALL_PORT_REQ_NUMBERS
+      ,ViewOptions
+      ,cb_context:set_account_db(Context, ?KZ_PORT_REQUESTS_DB)
+      ,fun normalize_view_results/2
+     ).
 
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
 %% @end
 %%--------------------------------------------------------------------
--spec read_descendants_by_number(cb_context:context(), ne_binary()) -> cb_context:context().
+-spec read_descendants_by_number(cb_context:context(), ne_binary()) ->
+                                        cb_context:context().
 read_descendants_by_number(Context, Number) ->
-    ViewOptions = [{'keys', build_keys(Context, Number)}, 'include_docs'],
+    ViewOptions = [{'keys', build_keys(Context, Number)}
+                   ,'include_docs'
+                  ],
     crossbar_doc:load_view(
-        ?ALL_PORT_REQ_NUMBERS
-        ,ViewOptions
-        ,cb_context:set_account_db(Context, ?KZ_PORT_REQUESTS_DB)
-        ,fun normalize_view_results/2
-    ).
+      ?ALL_PORT_REQ_NUMBERS
+      ,ViewOptions
+      ,cb_context:set_account_db(Context, ?KZ_PORT_REQUESTS_DB)
+      ,fun normalize_view_results/2
+     ).
 
--spec build_keys(cb_context:context() | ne_binary(), ne_binary()) -> [ne_binaries()] | [].
-build_keys(E164, [AccountId]) ->
+-type descendant_keys() :: [ne_binaries(),...] | [].
+
+-spec build_keys(cb_context:context(), ne_binary()) ->
+                        descendant_keys().
+build_keys(Context, Number) ->
+    build_keys_from_account(
+      wnm_util:to_e164(Number)
+      ,props:get_value(<<"accounts">>, cb_context:req_nouns(Context))
+     ).
+
+-spec build_keys_from_account(ne_binary(), ne_binaries()) ->
+                                     descendant_keys().
+build_keys_from_account(E164, [AccountId]) ->
     [[AccountId, E164]];
-build_keys(E164, [AccountId, ?PORT_DESCENDANTS]) ->
-    ViewOptions = [
-        {'startkey', [AccountId]}
-        ,{'endkey', [AccountId, wh_json:new()]}
-    ],
-    case
-        couch_mgr:get_results(
-            ?WH_ACCOUNTS_DB
-            ,?AGG_VIEW_DESCENDANTS
-            ,ViewOptions
-        )
+build_keys_from_account(E164, [AccountId, ?PORT_DESCENDANTS]) ->
+    ViewOptions = [{'startkey', [AccountId]}
+                   ,{'endkey', [AccountId, wh_json:new()]}
+                  ],
+    case couch_mgr:get_results(
+           ?WH_ACCOUNTS_DB
+           ,?AGG_VIEW_DESCENDANTS
+           ,ViewOptions
+          )
     of
         {'error', _R} ->
             lager:error("failed to query view ~p", [_R]),
             [];
         {'ok', JObjs} ->
             lists:foldl(
-                fun(JObj, Acc) ->
-                    Id = wh_json:get_value(<<"id">>, JObj),
-                    [[Id, E164]|Acc]
-                end
-                ,[[AccountId, E164]]
-                ,JObjs
-            )
-    end;
-build_keys(Context, Number) ->
-    build_keys(
-        wnm_util:to_e164(Number)
-        ,props:get_value(<<"accounts">>, cb_context:req_nouns(Context))
-    ).
+              fun(JObj, Acc) ->
+                      build_descendant_key(JObj, Acc, E164)
+              end
+              ,[[AccountId, E164]]
+              ,JObjs
+             )
+    end.
+
+-spec build_descendant_key(wh_json:object(), descendant_keys(), ne_binary()) ->
+                                  descendant_keys().
+build_descendant_key(JObj, Acc, E164) ->
+    [[wh_json:get_value(<<"id">>, JObj), E164]
+     |Acc
+    ].
 
 %%--------------------------------------------------------------------
 %% @private
