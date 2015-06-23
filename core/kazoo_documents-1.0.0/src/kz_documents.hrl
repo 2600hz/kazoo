a4b40e9291ee9ba89fc66879a0a20879d5a876c7
James Aimonetti <james.aimonetti@gmail.com>
2015-06-23 14:11:40 -0700

    KAZOO-3772: fetch default timezone from sysconfig

--- core/kazoo_documents-1.0.0/src/kz_documents.hrl
+++ core/kazoo_documents-1.0.0/src/kz_documents.hrl
@@ -3,7 +3,9 @@
 -include_lib("whistle/include/wh_types.hrl").
 -include_lib("kazoo_documents/include/kazoo_documents.hrl").
 
--define(DEFAULT_TIMEZONE, <<"America/Los_Angeles">>).
+-define(DEFAULT_TIMEZONE
+        ,whapps_config:get(<<"accounts">>, <<"default_timezone">>, <<"America/Los_Angeles">>)
+       ).
 
 -define(KZ_DOCUMENTS_HRL, 'true').
 -endif.
