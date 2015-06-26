432e1c1b099531c3a71ab686015c7f813f0f6f47
Peter Defebvre <macpie59@gmail.com>
2015-06-26 20:46:29 +0000

    KAZOO-3887: migrate port manager

---
+++ core/kazoo_number_manager-1.0.0/include/knm_port_request.hrl
@@ -0,0 +1,16 @@
+-ifndef(KNM_PORT_REQUEST_HRL).
+
+-define(PORT_WAITING, <<"unconfirmed">>).
+-define(PORT_SUBMITTED, <<"submitted">>).
+-define(PORT_PENDING, <<"pending">>).
+-define(PORT_SCHEDULED, <<"scheduled">>).
+-define(PORT_COMPLETE, <<"completed">>).
+-define(PORT_REJECT, <<"rejected">>).
+-define(PORT_CANCELED, <<"canceled">>).
+-define(PORT_ATTACHMENT, <<"attachments">>).
+-define(PORT_DESCENDANTS, <<"descendants">>).
+-define(PORT_PVT_STATE, <<"pvt_port_state">>).
+-define(PVT_SENT, <<"pvt_sent">>).
+
+-define(KNM_PORT_REQUEST_HRL, 'true').
+-endif.
---
+++ core/kazoo_number_manager-1.0.0/src/knm_port_request_crawler.erl
@@ -0,0 +1,152 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2015, 2600Hz INC
+%%% @doc
+%%%
+%%% @end
+%%% @contributors
+%%%   Peter Defebvre
+%%%-------------------------------------------------------------------
+-module(knm_port_request_crawler).
+
+-behaviour(gen_server).
+
+%% API
+-export([start_link/0
+         ,stop/0
+        ]).
+
+%% gen_server callbacks
+-export([init/1
+         ,handle_call/3
+         ,handle_cast/2
+         ,handle_info/2
+         ,terminate/2
+         ,code_change/3
+        ]).
+
+-include("knm.hrl").
+
+-define(MOD_CONFIG_CAT, <<(?KNM_CONFIG_CAT)/binary, ".port_request">>).
+
+-record(state, {cleanup_ref :: reference()}).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Starts the server
+%%
+%% @spec start_link() -> {ok, Pid} | ignore | {error, Error}
+%% @end
+%%--------------------------------------------------------------------
+start_link() ->
+    gen_server:start_link(?MODULE, [], []).
+
+stop() ->
+    gen_server:cast(?MODULE, 'stop').
+
+cleanup_timer() ->
+    Timeout = whapps_config:get_integer(?MOD_CONFIG_CAT, <<"crawler_delay_time_ms">>, ?MILLISECONDS_IN_MINUTE),
+    erlang:start_timer(Timeout, self(), 'ok').
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%%
+%% @spec init(Args) -> {ok, State} |
+%%                     {ok, State, Timeout} |
+%%                     ignore |
+%%                     {stop, Reason}
+%% @end
+%%--------------------------------------------------------------------
+init([]) ->
+    put('callid', ?MODULE),
+    lager:debug("started ~s", [?MODULE]),
+    {'ok', #state{cleanup_ref=cleanup_timer()}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_call(_Request, _From, State) ->
+    {'reply', {'error', 'not_implemented'}, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_cast('stop', State) ->
+    lager:debug("crawler has been stopped"),
+    {'stop', 'normal', State};
+handle_cast(_Msg, State) ->
+    lager:debug("unhandled cast: ~p", [_Msg]),
+    {'noreply', State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+handle_info({'timeout', Ref, _Msg}, #state{cleanup_ref=Ref}=State) ->
+    _P = wh_util:spawn('knm_port_request', 'send_submitted_requests', []),
+    {'noreply', State#state{cleanup_ref=cleanup_timer()}};
+handle_info(_Msg, State) ->
+    lager:debug("unhandled msg: ~p", [_Msg]),
+    {'noreply', State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    lager:debug("~s terminating: ~p", [?MODULE, _Reason]).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
+%% @end
+%%--------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {'ok', State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
