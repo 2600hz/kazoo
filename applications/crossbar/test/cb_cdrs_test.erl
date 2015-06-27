b7d60e3e0194026ccff8c1e3e1b931f44114fa8b
James Aimonetti <james.aimonetti@gmail.com>
2015-06-26 22:18:18 -0700

    KAZOO-3795: add blind transfer cdrs

--- applications/crossbar/test/cb_cdrs_test.erl
+++ applications/crossbar/test/cb_cdrs_test.erl
@@ -16,6 +16,10 @@
 -define(NORMAL_A_LEG, <<"{\"call_id\":\"normal_a\",\"timestamp\":1,\"other_leg_call_id\":\"normal_b\",\"call_direction\":\"inbound\"}">>).
 -define(NORMAL_B_LEG, <<"{\"call_id\":\"normal_b\",\"timestamp\":2,\"other_leg_call_id\":\"normal_a\",\"call_direction\":\"outbound\"}">>).
 
+-define(BLIND_A_LEG, <<"{\"call_id\":\"blind_a\",\"timestamp\":1,\"other_leg_call_id\":\"blind_c\",\"call_direction\":\"inbound\"}">>).
+-define(BLIND_B_LEG, <<"{\"call_id\":\"blind_b\",\"timestamp\":2,\"other_leg_call_id\":\"blind_a\",\"call_direction\":\"outbound\"}">>).
+-define(BLIND_C_LEG, <<"{\"call_id\":\"blind_c\",\"timestamp\":3,\"other_leg_call_id\":\"blind_a\",\"call_direction\":\"inbound\"}">>).
+
 -define(ATTX_A_LEG, <<"{\"call_id\":\"attx_a\",\"timestamp\":1,\"other_leg_call_id\":\"attx_d\",\"call_direction\":\"inbound\"}">>).
 -define(ATTX_B_LEG, <<"{\"call_id\":\"attx_b\",\"timestamp\":2,\"other_leg_call_id\":\"attx_a\",\"call_direction\":\"outbound\"}">>).
 -define(ATTX_C_LEG, <<"{\"call_id\":\"attx_c\",\"timestamp\":3,\"other_leg_call_id\":\"attx_d\",\"call_direction\":\"inbound\"}">>).
@@ -23,6 +27,7 @@
 
 -define(CDRS, [?SINGLE_LEG
                ,?NORMAL_B_LEG, ?NORMAL_A_LEG
+               ,?BLIND_A_LEG, ?BLIND_B_LEG, ?BLIND_C_LEG
                ,?ATTX_A_LEG, ?ATTX_B_LEG, ?ATTX_C_LEG, ?ATTX_D_LEG
               ]).
 
@@ -30,11 +35,9 @@ group_by_calls_test() ->
     Legs = wh_util:shuffle_list(
              [wh_json:decode(Leg) || Leg <- ?CDRS]
             ),
-
-    Calls = cb_cdrs:group_cdrs(Legs),
-    ?debugFmt("calls: ~p~n", [Calls]),
-
-    wh_json:foreach(fun grouped_call/1, Calls).
+    wh_json:foreach(fun grouped_call/1
+                    ,cb_cdrs:group_cdrs(Legs)
+                   ).
 
 grouped_call({<<"single_a">>, Legs}) ->
     ?assertEqual(1, length(Legs)),
@@ -48,6 +51,16 @@ grouped_call({<<"normal_a">>, Legs}) ->
     ?assertEqual(<<"normal_b">>, kzd_cdr:call_id(B)),
     ?assertEqual('true', wh_json:is_true(<<"is_a_leg">>, A)),
     ?assertEqual('false', wh_json:is_true(<<"is_a_leg">>, B));
+grouped_call({<<"blind_a">>, Legs}) ->
+    ?assertEqual(3, length(Legs)),
+    [A, B, C] = Legs,
+    ?assertEqual(<<"blind_a">>, kzd_cdr:call_id(A)),
+    ?assertEqual(<<"blind_b">>, kzd_cdr:call_id(B)),
+    ?assertEqual(<<"blind_c">>, kzd_cdr:call_id(C)),
+
+    ?assertEqual('true', wh_json:is_true(<<"is_a_leg">>, A)),
+    ?assertEqual('false', wh_json:is_true(<<"is_a_leg">>, B)),
+    ?assertEqual('false', wh_json:is_true(<<"is_a_leg">>, C));
 grouped_call({<<"attx_a">>, Legs}) ->
     ?assertEqual(4, length(Legs)),
     [A, B, C, D] = Legs,
