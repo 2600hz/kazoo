3b872d021dfb7b60e57689a377e5d9a86bd506be
SIPLABS, LLC <info@siplabs.ru>
2015-07-15 17:41:04 +0600

    cb_limits_v2.erl: using new resource_exists
    Change-Id: Ia994804c33d5c3af5b339f26d9c7818b65166feb

--- applications/crossbar/src/modules_v2/cb_limits_v2.erl
+++ applications/crossbar/src/modules_v2/cb_limits_v2.erl
@@ -9,8 +9,8 @@
 -module(cb_limits_v2).
 
 -export([init/0
-         ,allowed_methods/0
-         ,resource_exists/0
+         ,allowed_methods/1
+         ,resource_exists/1
          ,billing/1
          ,validate/1
          ,post/1
@@ -42,8 +42,8 @@ init() ->
 %% Failure here returns 405
 %% @end
 %%--------------------------------------------------------------------
--spec allowed_methods() -> http_methods().
-allowed_methods() ->
+-spec allowed_methods(cb_context:context()) -> http_methods().
+allowed_methods(_Context) ->
     [?HTTP_GET, ?HTTP_POST].
 
 %%--------------------------------------------------------------------
@@ -54,8 +54,8 @@ allowed_methods() ->
 %% Failure here returns 404
 %% @end
 %%--------------------------------------------------------------------
--spec resource_exists() -> 'true'.
-resource_exists() -> 'true'.
+-spec resource_exists(cb_context:context()) -> api_util:resource_existence().
+resource_exists(Context) -> {'true', Context}.
 
 %%--------------------------------------------------------------------
 %% @public
