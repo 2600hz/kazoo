3347ff0dee5c61a9e80976f95b0bc02c10b95f56
SIPLABS, LLC <info@siplabs.ru>
2015-07-15 17:41:04 +0600

    cb_token_restrictions.erl: using new resource_exists
    Change-Id: I4727982fc50de571d3f740eec2fbe540d965be6a

--- applications/crossbar/src/modules/cb_token_restrictions.erl
+++ applications/crossbar/src/modules/cb_token_restrictions.erl
@@ -14,8 +14,8 @@
 -module(cb_token_restrictions).
 
 -export([init/0
-         ,allowed_methods/0
-         ,resource_exists/0
+         ,allowed_methods/1
+         ,resource_exists/1
          ,validate/1
          ,post/1
          ,delete/1
@@ -60,12 +60,12 @@ init() ->
     _ = crossbar_bindings:bind(<<"*.execute.post.token_restrictions">>, ?MODULE, 'post'),
     _ = crossbar_bindings:bind(<<"*.execute.delete.token_restrictions">>, ?MODULE, 'delete').
 
--spec allowed_methods() -> http_methods().
-allowed_methods() ->
+-spec allowed_methods(cb_context:context()) -> http_methods().
+allowed_methods(_Context) ->
     [?HTTP_GET, ?HTTP_POST, ?HTTP_DELETE].
 
--spec resource_exists() -> 'true'.
-resource_exists() -> 'true'.
+-spec resource_exists(cb_context:context()) -> api_util:resource_existence().
+resource_exists(Context) -> {'true', Context}.
 
 -spec validate(cb_context:context()) -> cb_context:context().
 -spec validate(cb_context:context(), path_token()) -> cb_context:context().
