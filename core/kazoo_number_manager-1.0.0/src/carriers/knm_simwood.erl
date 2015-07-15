2d2ae14802e86cde0c039d8dc5db4ef40aa821d6
Peter Defebvre <macpie59@gmail.com>
2015-07-15 23:32:30 +0000

    kazoo-number_manager: format find number return

--- core/kazoo_number_manager-1.0.0/src/carriers/knm_simwood.erl
+++ core/kazoo_number_manager-1.0.0/src/carriers/knm_simwood.erl
@@ -87,15 +87,20 @@ disconnect_number(Number) ->
         {'ok', _Body} -> {'ok', Number};
         {'error', _R}=Error -> Error
     end.
+
 %%--------------------------------------------------------------------
 %% @public
 %% @doc
 %% @end
 %%--------------------------------------------------------------------
-
 -spec is_number_billable(number()) -> 'true'.
 is_number_billable(_Number) -> 'true'.
 
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
 -spec should_lookup_cnam() -> boolean().
 should_lookup_cnam() -> 'true'.
 
@@ -103,6 +108,11 @@ should_lookup_cnam() -> 'true'.
 %%% Internal functions
 %%%===================================================================
 
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
 -spec query_simwood(ne_binary(), 'get'|'put'|'post'|'delete') ->
                            {'ok', iolist()} |
                            {'error', 'not_available'}.
@@ -122,24 +132,38 @@ query_simwood(URL, Verb) ->
             {'error', 'not_available'}
     end.
 
-%%
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
 %%  Simwood number query supports only 1|10|100 search amount
-%%
+%% @end
+%%--------------------------------------------------------------------
 -spec sw_quantity(pos_integer()) -> ne_binary().
 sw_quantity(Quantity) when Quantity == 1 -> <<"1">>;
 sw_quantity(Quantity) when Quantity > 1, Quantity =< 10  -> <<"10">>;
 sw_quantity(_Quantity) -> <<"100">>.
 
--spec process_response(wh_json:objects()) -> {'ok', wh_json:object()}.
-process_response([]) -> {'ok', wh_json:new()};
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
+-spec process_response(wh_json:objects()) -> {'ok', numbers()}.
+-spec process_response(wh_json:objects(), numbers()) -> {'ok', numbers()}.
 process_response(JObjs) ->
-    process_response(JObjs, wh_json:new()).
+    process_response(JObjs, []).
 
--spec process_response(wh_json:objects(), wh_json:object()) -> {'ok', wh_json:object()}.
-process_response([], Acc) -> {'ok', Acc};
-process_response([JObj|JObjs], Acc) ->
-    CountryCode = wh_json:get_value(<<"country_code">>, JObj),
-    FoundNumber = wh_json:get_value(<<"number">>, JObj),
-    E164 = <<"+", CountryCode/binary, FoundNumber/binary>>,
-    NumberJObj = wh_json:from_list([{<<"number">>, E164}]),
-    process_response(JObjs, wh_json:set_value(E164, NumberJObj, Acc)).
+process_response([], Numbers) -> {'ok', Numbers};
+process_response([JObj|JObjs], Numbers) ->
+    Num = wh_json:get_value(<<"number">>, JObj),
+    NormalizedNum = knm_converters:normalize(Num),
+    NumberDb = knm_converters:to_db(NormalizedNum),
+    Updates = [
+        {fun knm_phone_number:set_number/2, NormalizedNum}
+        ,{fun knm_phone_number:set_number_db/2, NumberDb}
+        ,{fun knm_phone_number:set_module_name/2, wh_util:to_binary(?MODULE)}
+        ,{fun knm_phone_number:set_carrier_data/2, JObj}
+        ,{fun knm_phone_number:set_number_db/2, NumberDb}
+    ],
+    Number = knm_phone_number:setters(knm_phone_number:new(), Updates),
+    process_response(JObjs, [Number|Numbers]).
