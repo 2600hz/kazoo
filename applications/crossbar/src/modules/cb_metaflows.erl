8724931016c96b816c47a0f22a3cf96bba5b5441
SIPLABS, LLC <info@siplabs.ru>
2015-07-15 17:41:03 +0600

    cb_metaflows.erl: using new resource_exists
    Change-Id: Id4b2c533957848a82ff6ac858faba1f6bb7409a1

--- applications/crossbar/src/modules/cb_metaflows.erl
+++ applications/crossbar/src/modules/cb_metaflows.erl
@@ -15,8 +15,8 @@
 -module(cb_metaflows).
 
 -export([init/0
-         ,allowed_methods/0
-         ,resource_exists/0
+         ,allowed_methods/1
+         ,resource_exists/1
          ,validate/1
          ,post/1
          ,delete/1
@@ -51,8 +51,8 @@ init() ->
 %% going to be responded to.
 %% @end
 %%--------------------------------------------------------------------
--spec allowed_methods() -> http_methods().
-allowed_methods() ->
+-spec allowed_methods(cb_context:context()) -> http_methods().
+allowed_methods(_Context) ->
     [?HTTP_GET, ?HTTP_POST, ?HTTP_DELETE].
 
 %%--------------------------------------------------------------------
@@ -64,8 +64,8 @@ allowed_methods() ->
 %%    /metaflows/foo/bar => [<<"foo">>, <<"bar">>]
 %% @end
 %%--------------------------------------------------------------------
--spec resource_exists() -> 'true'.
-resource_exists() -> 'true'.
+-spec resource_exists(cb_context:context()) -> api_util:resource_existence().
+resource_exists(Context) -> {'true', Context}.
 
 %%--------------------------------------------------------------------
 %% @public
