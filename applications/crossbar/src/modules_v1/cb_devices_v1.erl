358dfd812d9286144d194692359763efa72bfdf7
SIPLABS, LLC <info@siplabs.ru>
2015-07-15 17:41:04 +0600

    cb_devices_v1.erl: using new resource_exists
    Change-Id: Ifb377111b94e63f4a11ab43be67490bb705870d5

--- applications/crossbar/src/modules_v1/cb_devices_v1.erl
+++ applications/crossbar/src/modules_v1/cb_devices_v1.erl
@@ -13,8 +13,8 @@
 -module(cb_devices_v1).
 
 -export([init/0
-         ,allowed_methods/0, allowed_methods/1, allowed_methods/3
-         ,resource_exists/0, resource_exists/1, resource_exists/3
+         ,allowed_methods/1, allowed_methods/2, allowed_methods/4
+         ,resource_exists/1, resource_exists/2, resource_exists/4
          ,billing/1
          ,authenticate/1
          ,authorize/1
@@ -62,19 +62,19 @@ init() ->
 %% Failure here returns 405
 %% @end
 %%--------------------------------------------------------------------
--spec allowed_methods() -> http_methods().
--spec allowed_methods(path_token()) -> http_methods().
--spec allowed_methods(path_token(), path_token(), path_token()) -> http_methods().
+-spec allowed_methods(cb_context:context()) -> http_methods().
+-spec allowed_methods(cb_context:context(), path_token()) -> http_methods().
+-spec allowed_methods(cb_context:context(), path_token(), path_token(), path_token()) -> http_methods().
 
-allowed_methods() ->
+allowed_methods(_Context) ->
     [?HTTP_GET, ?HTTP_PUT].
 
-allowed_methods(?STATUS_PATH_TOKEN) ->
+allowed_methods(_Context, ?STATUS_PATH_TOKEN) ->
     [?HTTP_GET];
-allowed_methods(_) ->
+allowed_methods(_Context, _) ->
     [?HTTP_GET, ?HTTP_POST, ?HTTP_DELETE].
 
-allowed_methods(_, ?QUICKCALL_PATH_TOKEN, _) ->
+allowed_methods(_Context, _, ?QUICKCALL_PATH_TOKEN, _) ->
     [?HTTP_GET].
 
 %%--------------------------------------------------------------------
@@ -85,13 +85,13 @@ allowed_methods(_, ?QUICKCALL_PATH_TOKEN, _) ->
 %% Failure here returns 404
 %% @end
 %%--------------------------------------------------------------------
--spec resource_exists() -> 'true'.
--spec resource_exists(path_token()) -> 'true'.
--spec resource_exists(path_token(), path_token(), path_token()) -> 'true'.
+-spec resource_exists(cb_context:context()) -> api_util:resource_existence().
+-spec resource_exists(cb_context:context(), path_token()) -> api_util:resource_existence().
+-spec resource_exists(cb_context:context(), path_token(), path_token(), path_token()) -> api_util:resource_existence().
 
-resource_exists() -> 'true'.
-resource_exists(_) -> 'true'.
-resource_exists(_, ?QUICKCALL_PATH_TOKEN, _) -> 'true'.
+resource_exists(Context) -> {'true', Context}.
+resource_exists(Context, _) -> {'true', Context}.
+resource_exists(Context, _, ?QUICKCALL_PATH_TOKEN, _) -> {'true', Context}.
 
 %%--------------------------------------------------------------------
 %% @public
