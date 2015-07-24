95d05484c8b901e0d908e6b4dc7c400d89112439
SIPLABS, LLC <info@siplabs.ru>
2015-07-24 16:48:30 +0700

    respect the dialed number
    Change-Id: I36b83ec06b89696a903a028930e4781e725e8354

--- applications/callflow/src/module/cf_branch_bnumber.erl
+++ applications/callflow/src/module/cf_branch_bnumber.erl
@@ -8,10 +8,17 @@
 %%%-------------------------------------------------------------------
 -module(cf_branch_bnumber).
 
+-include("../callflow.hrl").
+
 %% API
 -export([handle/2]).
 
 -spec handle(wh_json:object(), whapps_call:call()) -> 'ok'.
 handle(_Data, Call) ->
     Number = whapps_call:kvs_fetch('cf_capture_group', Call),
-    cf_exe:continue(Number, Call).
+    NumberToBranch = case wh_util:is_empty(Number) of
+                         'true' -> whapps_call:request_user(Call);
+                         'false' -> Number
+                     end,
+    lager:debug("Trying to branch to ~p", [NumberToBranch]),
+    cf_exe:continue(NumberToBranch, Call).
