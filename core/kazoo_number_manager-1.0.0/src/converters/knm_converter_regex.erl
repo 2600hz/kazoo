1c132170d60eed64588410d0f77338c2dbd26da1
James Aimonetti <james.aimonetti@gmail.com>
2015-08-21 15:28:49 -0700

    KAZOO-3891: add account e164 convertor

--- core/kazoo_number_manager-1.0.0/src/converters/knm_converter_regex.erl
+++ core/kazoo_number_manager-1.0.0/src/converters/knm_converter_regex.erl
@@ -10,7 +10,7 @@
 
 -include("../knm.hrl").
 
--export([normalize/1
+-export([normalize/1, normalize/2
          ,to_npan/1
          ,to_1npan/1
         ]).
@@ -26,6 +26,8 @@
                                    }
                                  ]).
 
+-define(KEY_E164_CONVERTERS, <<"e164_converters">>).
+
 %%--------------------------------------------------------------------
 %% @public
 %% @doc
@@ -34,6 +36,12 @@
 -spec normalize(ne_binary()) -> ne_binary().
 normalize(<<_/binary>> = Num) ->
     to_e164(Num).
+
+-spec normalize(ne_binary(), api_binary()) -> ne_binary().
+normalize(<<_/binary>> = Num, 'undefined') ->
+    to_e164(Num);
+normalize(<<_/binary>> = Num, AccountId) ->
+    to_e164(Num, AccountId).
 
 %%--------------------------------------------------------------------
 %% @public
@@ -75,6 +83,50 @@ to_e164(Number) ->
     Regexes = wh_json:get_keys(Converters),
     maybe_convert_to_e164(Regexes, Converters, Number).
 
+to_e164(<<$+, _/binary>> = N, _AccountId) -> N;
+to_e164(Number, Account) ->
+    AccountId = wh_util:format_account_id(Account, 'raw'),
+    case kz_account:fetch(AccountId) of
+        {'ok', JObj} ->
+            to_e164_from_account_dialplan(Number, AccountId, kz_account:dial_plan(JObj));
+        {'error', _E} ->
+            to_e164_from_account(Number, AccountId)
+    end.
+
+-spec to_e164_from_account(ne_binary(), ne_binary()) -> ne_binary().
+to_e164_from_account(Number, <<_/binary>> = AccountId) ->
+    Converters = get_e164_converters(AccountId),
+    Regexes = wh_json:get_keys(Converters),
+    maybe_convert_to_e164(Regexes, Converters, Number).
+
+to_e164_from_account_dialplan(Number, AccountId, 'undefined') ->
+    to_e164_from_account(Number, AccountId);
+to_e164_from_account_dialplan(Number, AccountId, DialPlan) ->
+    to_e164_from_account_dialplan_regexes(Number
+                                          ,AccountId
+                                          ,DialPlan
+                                          ,wh_json:get_keys(DialPlan)
+                                         ).
+
+to_e164_from_account_dialplan_regexes(Number, AccountId, _DialPlan, []) ->
+    to_e164_from_account(Number, AccountId);
+to_e164_from_account_dialplan_regexes(Number, AccountId, DialPlan, Regexes) ->
+    to_e164_from_account(apply_dialplan(Number, DialPlan, Regexes), AccountId).
+
+apply_dialplan(Number, _Dialplan, []) -> Number;
+apply_dialplan(Number, DialPlan, [Regex|Rs]) ->
+    case re:run(Number, Regex, [{'capture', 'all', 'binary'}]) of
+        'nomatch' ->
+            apply_dialplan(Number, DialPlan, Rs);
+        'match' ->
+            Number;
+        {'match', Captures} ->
+            Root = lists:last(Captures),
+            Prefix = wh_json:get_binary_value([Regex, <<"prefix">>], DialPlan, <<>>),
+            Suffix = wh_json:get_binary_value([Regex, <<"suffix">>], DialPlan, <<>>),
+            <<Prefix/binary, Root/binary, Suffix/binary>>
+    end.
+
 %%--------------------------------------------------------------------
 %% @private
 %% @doc
@@ -83,11 +135,28 @@ to_e164(Number) ->
 -spec get_e164_converters() -> wh_json:object().
 get_e164_converters() ->
     Default = wh_json:from_list(?DEFAULT_E164_CONVERTERS),
-    try whapps_config:get(?KNM_CONFIG_CAT, <<"e164_converters">>, Default) of
+    try whapps_config:get(?KNM_CONFIG_CAT
+                          ,?KEY_E164_CONVERTERS
+                          ,Default
+                         )
+    of
         Converters -> Converters
     catch
-        _:_ ->
-            Default
+        _:_ -> Default
+    end.
+
+-spec get_e164_converters(ne_binary()) -> wh_json:object().
+get_e164_converters(AccountId) ->
+    Default = wh_json:from_list(?DEFAULT_E164_CONVERTERS),
+    try whapps_account_config:get_global(AccountId
+                                         ,?KNM_CONFIG_CAT
+                                         ,?KEY_E164_CONVERTERS
+                                         ,Default
+                                        )
+    of
+        Converters -> Converters
+    catch
+        _:_ -> Default
     end.
 
 %%--------------------------------------------------------------------
