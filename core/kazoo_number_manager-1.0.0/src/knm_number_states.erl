d66893b7aefd42c3811456e6208222fe8179dba3
James Aimonetti <james.aimonetti@gmail.com>
2015-08-25 13:40:49 -0700

    KAZOO-3891: move number to reserved state

--- core/kazoo_number_manager-1.0.0/src/knm_number_states.erl
+++ core/kazoo_number_manager-1.0.0/src/knm_number_states.erl
@@ -23,6 +23,7 @@ to_reserved(Number, ?NUMBER_STATE_RESERVED) ->
     Routines = [fun not_assigning_to_self/1
                 ,fun is_auth_by_authorized/1
                 ,fun update_reserve_history/1
+                ,fun move_to_reserved_state/1
                ],
     apply_transitions(Number, Routines);
 to_reserved(Number, ?NUMBER_STATE_DISCOVERY) ->
@@ -67,6 +68,42 @@ update_reserve_history(Number) ->
       Number
       ,knm_phone_number:add_reserve_history(PhoneNumber, AssignTo)
      ).
+
+-spec move_to_reserved_state(kn()) -> kn().
+move_to_reserved_state(Number) ->
+    PhoneNumber = knm_number:phone_number(Number),
+    knm_number:set_phone_number(
+      Number
+      ,move_phone_number_to_reserved_state(
+         PhoneNumber
+         ,knm_phone_number:assigned_to(PhoneNumber)
+        )
+     ).
+
+-spec move_phone_number_to_reserved_state(knm_phone_number:knm_number(), api_binary()) ->
+                                     knm_phone_number:knm_number().
+move_phone_number_to_reserved_state(PhoneNumber, 'undefined') ->
+    Setters = [{fun knm_phone_number:set_assigned_to/2
+                ,knm_phone_number:assign_to(PhoneNumber)
+               }
+               ,{fun knm_phone_number:set_state/2, ?NUMBER_STATE_RESERVED}
+              ],
+    knm_phone_number:setters(PhoneNumber, Setters);
+move_phone_number_to_reserved_state(PhoneNumber, AssignedTo) ->
+    move_phone_number_to_reserved_state(
+      PhoneNumber
+      ,AssignedTo
+      ,knm_phone_number:assign_to(PhoneNumber)
+     ).
+
+move_phone_number_to_reserved_state(PhoneNumber, AssignTo, AssignTo) ->
+    knm_phone_number:set_state(PhoneNumber, ?NUMBER_STATE_RESERVED);
+move_phone_number_to_reserved_state(PhoneNumber, AssignedTo, AssignTo) ->
+    Setters = [{fun knm_phone_number:set_prev_assigned_to/2, AssignedTo}
+               ,{fun knm_phone_number:set_assigned_to/2, AssignTo}
+               ,{fun knm_phone_number:set_state/2, ?NUMBER_STATE_RESERVED}
+              ],
+    knm_phone_number:setters(PhoneNumber, Setters).
 
 -type transition() :: fun((kn()) -> kn()).
 -type transitions() :: [transition()].
