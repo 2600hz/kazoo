14ef3cb66a548e124dc79682823b2215b7afe9a2
James Aimonetti <james.aimonetti@gmail.com>
2015-08-25 16:58:50 -0700

    KAZOO-3891: add local carrier module

---
+++ core/kazoo_number_manager-1.0.0/src/carriers/knm_local.erl
@@ -0,0 +1,73 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2011-2015, 2600Hz INC
+%%% @doc
+%%%
+%%% Handle client requests for phone_number documents
+%%%
+%%% @end
+%%% @contributors
+%%%   Karl Anderson
+%%%-------------------------------------------------------------------
+-module(knm_local).
+
+-export([find_numbers/3]).
+-export([acquire_number/1]).
+-export([disconnect_number/1]).
+-export([is_number_billable/1]).
+-export([should_lookup_cnam/0]).
+
+-include("../knm.hrl").
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Query the local system for a quanity of available numbers
+%% in a rate center
+%% @end
+%%--------------------------------------------------------------------
+-spec find_numbers(ne_binary(), pos_integer(), wh_proplist()) ->
+                          {'error', 'non_available'}.
+find_numbers(Number, Quanity, Opts) when size(Number) < 5 ->
+    find_numbers(<<"+1", Number/binary>>, Quanity, Opts);
+find_numbers(_Number, _Quanity, _Opts) ->
+    %% TODO: given the requestors account number discovery wnm_local numbers that are
+    %%       available but managed by accendants of the account.
+    {'error', 'non_available'}.
+
+-spec is_number_billable(knm_number:knm_number()) -> 'false'.
+is_number_billable(_Number) -> 'false'.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Acquire a given number from the carrier
+%% @end
+%%--------------------------------------------------------------------
+-spec acquire_number(knm_number:knm_number()) -> knm_number:knm_number().
+acquire_number(Number) -> {'ok', Number}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Release a number from the routing table
+%% @end
+%%--------------------------------------------------------------------
+
+-spec disconnect_number(knm_number:knm_number()) ->
+                               knm_number:knm_number().
+disconnect_number(Number) ->
+    knm_number:set_phone_number(
+      Number
+      ,knm_phone_number:set_state(
+         knm_number:phone_number(Number)
+         ,?NUMBER_STATE_RELEASED
+        )
+     ).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% @end
+%%--------------------------------------------------------------------
+-spec should_lookup_cnam() -> 'true'.
+should_lookup_cnam() -> 'true'.
