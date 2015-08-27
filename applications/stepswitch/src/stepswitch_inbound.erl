ee1771167317fa5099bf59a5b39329f85e19ffaa
lazedo <luis.azedo@factorlusitano.com>
2015-08-27 01:01:43 +0100

    KAZOO-1243 set Authorizing-Type

--- applications/stepswitch/src/stepswitch_inbound.erl
+++ applications/stepswitch/src/stepswitch_inbound.erl
@@ -118,11 +118,11 @@ add_resource_id(JObj, ResourceProps) ->
     ResourceId = props:get_value('resource_id', ResourceProps),
     wh_json:set_values([{?CCV(<<"Resource-ID">>), ResourceId}
                         ,{?CCV(<<"Global-Resource">>), props:get_is_true('global', ResourceProps)}
+                        ,{?CCV(<<"Authorizing-Type">>), <<"resource">>}
                        %% TODO
                        %% we need to make sure that Authorizing-ID is used
                        %% with Authorizing-Type in ALL whapps
-                       %% when this is done remove the comments
-                       %% ,{?CCV(<<"Authorizing-Type">>), <<"resource">>}
+                       %% when this is done remove the comment below
                        %% ,{?CCV(<<"Authorizing-ID">>), ResourceId}
                        ]
                        ,JObj
