2c85ef325814784c19a7393750d8ebbdab927980
James Aimonetti <james.aimonetti@gmail.com>
2015-08-31 10:04:47 -0700

    KAZOO-3891: add assertions of properties on created number

--- core/kazoo_number_manager-1.0.0/test/knm_create_new_number_test.erl
+++ core/kazoo_number_manager-1.0.0/test/knm_create_new_number_test.erl
@@ -18,4 +18,12 @@ create_new_number_test() ->
                ,kz_account:set_allow_number_additions(?RESELLER_ACCOUNT_DOC, 'true')
               }
             ],
-    knm_number:create_or_load(?TEST_CREATE_NUM, Props, {'error', 'not_found'}).
+    N = knm_number:create_or_load(?TEST_CREATE_NUM, Props, {'error', 'not_found'}),
+    PN = knm_number:phone_number(N),
+    ?debugFmt("pn: ~p~n", [PN]),
+    ?assertEqual(?RESELLER_ACCOUNT_ID, knm_phone_number:assigned_to(PN)),
+    ?assertEqual(?MASTER_ACCOUNT_ID, knm_phone_number:auth_by(PN)),
+    ?assertEqual(<<"numbers%2F%2B1555">>, knm_phone_number:number_db(PN)),
+    ?assertEqual(?NUMBER_STATE_RESERVED, knm_phone_number:state(PN)),
+    ?assertEqual([?RESELLER_ACCOUNT_ID], knm_phone_number:reserve_history(PN)),
+    ?assertEqual(?CARRIER_LOCAL, knm_phone_number:module_name(PN)).
