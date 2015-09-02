a605bf60700a100938537fad0d8ae243d5fbbe2e
Mark Magnusson <mark@2600hz.com>
2015-09-02 16:19:38 +0000

    KAZOO-2206: correctly set original callerid if the ringback fails

--- applications/callflow/src/module/cf_park.erl
+++ applications/callflow/src/module/cf_park.erl
@@ -646,7 +646,10 @@ ringback_parker(EndpointId, SlotNumber, TmpCID, Call) ->
                          end,
             Call1 = whapps_call:set_caller_id_name(TmpCID, Call),
             whapps_call_command:bridge(Endpoints, ?DEFAULT_TIMEOUT_S, Call1),
-            wait_for_ringback(CleanUpFun, Call1);
+            case wait_for_ringback(CleanUpFun, Call1) of
+                'failed' -> whapps_call:set_caller_id_name(OriginalCID, Call), 'failed';
+                Result   -> Result
+            end;
         _ -> 'failed'
     end.
 
