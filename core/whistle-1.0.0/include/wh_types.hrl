05e56d0e3debc3c9ff44529e0bf1be62d5a02adc
lazedo <luis.azedo@factorlusitano.com>
2015-09-04 21:55:58 +0100

    KAZOO-4139 startchild_ret

--- core/whistle-1.0.0/include/wh_types.hrl
+++ core/whistle-1.0.0/include/wh_types.hrl
@@ -146,6 +146,7 @@
 -type startlink_err() :: {'already_started', pid()} | 'shutdown' | term().
 -type startlink_ret() :: {'ok', pid()} | 'ignore' | {'error', startlink_err()}.
 -type startapp_ret() :: {'ok', pid()} | {'ok', pid(), term()} | {'error', startlink_err()}.
+-type startchild_ret() :: {'ok', pid()} | {'ok', pid(), term()} | {'error', startlink_err()}.
 
 -type call_from() :: pid_ref().
 -type gen_server_timeout() :: 'hibernate' | non_neg_integer().
