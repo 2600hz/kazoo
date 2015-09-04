df6c3a44c50169df377b05e067c1eba980c5388c
lazedo <luis.azedo@factorlusitano.com>
2015-09-04 02:38:42 +0100

    HELP-18800 mail message size restricted with HELO

--- applications/fax/src/fax_smtp.erl
+++ applications/fax/src/fax_smtp.erl
@@ -78,22 +78,13 @@ init(Hostname, SessionCount, Address, Options) ->
                          {'ok', pos_integer(), state()} |
                          {'ok', state()} |
                          error_message().
-handle_HELO(<<"invalid">>, State) ->
-    %% contrived example
-    {'error', "554 invalid hostname", State};
-handle_HELO(<<"trusted_host">>, State) ->
-    {'ok', State}; %% no size limit because we trust them.
 handle_HELO(Hostname, State) ->
     lager:debug("HELO from ~s", [Hostname]),
-    {'ok', 655360, State}. % 640kb of HELO should be enough for anyone.
-%% If {ok, State} was returned here, we'd use the default 10mb limit
+    {'ok', State}.
 
 -spec handle_EHLO(binary(), list(), state()) ->
                          {'ok', list(), state()} |
                          error_message().
-handle_EHLO(<<"invalid">>, _Extensions, State) ->
-    %% contrived example
-    {'error', "554 invalid hostname", State};
 handle_EHLO(Hostname, Extensions, #state{options=Options}=State) ->
     lager:debug("EHLO from ~s", [Hostname]),
     %% You can advertise additional extensions, or remove some defaults
