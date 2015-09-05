2c63ec70987a64327f8547323de71a60e4c9ae31
lazedo <luis.azedo@factorlusitano.com>
2015-09-05 02:54:15 +0100

    KAZOO-4139 spec test

--- applications/omnipresence/src/omnip_sup.erl
+++ applications/omnipresence/src/omnip_sup.erl
@@ -69,7 +69,13 @@ start_link() ->
 %% specifications.
 %% @end
 %%--------------------------------------------------------------------
--spec init([]) -> sup_init_ret().
+-spec init([]) -> 
+    {ok, {{RestartStrategy :: supervisor:strategy(),
+           MaxR            :: non_neg_integer(),
+           MaxT            :: non_neg_integer()},
+           [ChildSpec :: supervisor:child_spec()]}}
+    | ignore.
+%          sup_init_ret().
 init([]) ->
     RestartStrategy = 'one_for_one',
     MaxRestarts = 5,
