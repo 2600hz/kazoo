c69a2a038eddc8b4c47d59c2a7853dd371267be1
James Aimonetti <james.aimonetti@gmail.com>
2015-09-14 10:46:56 -0700

    KAZOO-3891: add labels to tests

--- core/kazoo_number_manager-1.0.0/test/knm_create_new_number_test.erl
+++ core/kazoo_number_manager-1.0.0/test/knm_create_new_number_test.erl
@@ -10,7 +10,7 @@
 -include_lib("eunit/include/eunit.hrl").
 -include("../src/knm.hrl").
 
-create_new_number_test() ->
+create_new_number_test_() ->
     Props = [{<<"auth_by">>, ?MASTER_ACCOUNT_ID}
              ,{<<"assign_to">>, ?RESELLER_ACCOUNT_ID}
              ,{<<"dry_run">>, 'false'}
@@ -21,9 +21,22 @@ create_new_number_test() ->
     N = knm_number:create_or_load(?TEST_CREATE_NUM, Props, {'error', 'not_found'}),
     PN = knm_number:phone_number(N),
 
-    ?assertEqual(?RESELLER_ACCOUNT_ID, knm_phone_number:assigned_to(PN)),
-    ?assertEqual(?MASTER_ACCOUNT_ID, knm_phone_number:auth_by(PN)),
-    ?assertEqual(<<"numbers%2F%2B1555">>, knm_phone_number:number_db(PN)),
-    ?assertEqual(?NUMBER_STATE_RESERVED, knm_phone_number:state(PN)),
-    ?assertEqual([?RESELLER_ACCOUNT_ID], knm_phone_number:reserve_history(PN)),
-    ?assertEqual(?CARRIER_LOCAL, knm_phone_number:module_name(PN)).
+    [{"Verify phone number is assigned to reseller account"
+      ,?_assertEqual(?RESELLER_ACCOUNT_ID, knm_phone_number:assigned_to(PN))
+     }
+     ,{"Verify phone number was authorized by master account"
+       ,?_assertEqual(?MASTER_ACCOUNT_ID, knm_phone_number:auth_by(PN))
+      }
+     ,{"Verify phone number database is properly set"
+       ,?_assertEqual(<<"numbers%2F%2B1555">>, knm_phone_number:number_db(PN))
+      }
+     ,{"Verify phone number is in RESERVED state"
+       ,?_assertEqual(?NUMBER_STATE_RESERVED, knm_phone_number:state(PN))
+      }
+     ,{"Verify the reseller account is listed in reserve history"
+       ,?_assertEqual([?RESELLER_ACCOUNT_ID], knm_phone_number:reserve_history(PN))
+      }
+     ,{"Verify the local carrier module is being used"
+       ,?_assertEqual(?CARRIER_LOCAL, knm_phone_number:module_name(PN))
+      }
+    ].
