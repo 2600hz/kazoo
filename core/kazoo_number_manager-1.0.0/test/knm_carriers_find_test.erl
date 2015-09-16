450562e679ca3e5a9d00521d4113f46ed5542971
James Aimonetti <james.aimonetti@gmail.com>
2015-09-16 16:19:09 -0700

    KAZOO-3891: tests for finding numbers

---
+++ core/kazoo_number_manager-1.0.0/test/knm_carriers_find_test.erl
@@ -0,0 +1,11 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2015, 2600Hz
+%%% @doc
+%%% @end
+%%% @contributors
+%%%   James Aimonetti
+%%%-------------------------------------------------------------------
+-module(knm_carriers_find_test).
+
+-include_lib("eunit/include/eunit.hrl").
+-include("../src/knm.hrl").
