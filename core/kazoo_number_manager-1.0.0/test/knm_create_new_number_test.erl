a0351cdc00dc5714d28201df45c9b4615a6e00bb
James Aimonetti <james.aimonetti@gmail.com>
2015-09-16 10:27:36 -0700

    KAZOO-3891: add tests for error json

--- core/kazoo_number_manager-1.0.0/test/knm_create_new_number_test.erl
+++ core/kazoo_number_manager-1.0.0/test/knm_create_new_number_test.erl
@@ -87,12 +87,14 @@ create_existing_in_service_test_() ->
               }
             ],
 
+    Resp = knm_number:attempt(fun knm_number:create_or_load/3
+                              ,[?TEST_EXISTING_NUM, Props, {'ok', InServicePN}]
+                             ),
+
     [{"Verifying that IN SERVICE numbers can't be created"
-      ,?_assertException('throw'
-                         ,{'error', 'number_exists', ?TEST_EXISTING_NUM}
-                         ,knm_number:create_or_load(?TEST_EXISTING_NUM, Props, {'ok', InServicePN})
-                        )
+      ,?_assertMatch({'error', _}, Resp)
      }
+     | check_error_response(Resp, 409, <<"number_exists">>, ?TEST_EXISTING_NUM)
     ].
 
 create_dry_run_test_() ->
@@ -104,7 +106,7 @@ create_dry_run_test_() ->
               }
             ],
     {'dry_run', Services, Charges} =
-        knm_number:create_or_load(?TEST_CREATE_NUM, Props, {'error', 'not_found'}),
+        knm_number:create(?TEST_CREATE_NUM, Props),
 
     %% Eventually make a stub service plan to test this
     [{"Verify charges for dry_run"
@@ -140,18 +142,21 @@ load_existing_checks() ->
 existing_in_state(PN, 'false') ->
     State = wh_util:to_list(knm_phone_number:state(PN)),
 
-    {lists:flatten(["Ensure number in ", State, " cannot be 'created'"])
-     ,?_assertException('throw'
-                        ,{'error', 'number_exists', ?TEST_EXISTING_NUM}
-                        ,knm_number:ensure_can_load_to_create(PN)
-                       )
-    };
+    Resp = knm_number:attempt(fun knm_number:ensure_can_load_to_create/1
+                              ,[PN]
+                             ),
+
+    [{lists:flatten(["Ensure number in ", State, " cannot be 'created'"])
+      ,?_assertMatch({'error', _}, Resp)
+     }
+     | check_error_response(Resp, 409, <<"number_exists">>, ?TEST_EXISTING_NUM)
+    ];
 existing_in_state(PN, 'true') ->
     State = wh_util:to_list(knm_phone_number:state(PN)),
 
-    {lists:flatten(["Ensure number in ", State, " can be 'created'"])
+    [{lists:flatten(["Ensure number in ", State, " can be 'created'"])
      ,?_assert(knm_number:ensure_can_load_to_create(PN))
-    }.
+    }].
 
 create_available_checks() ->
     [create_with_no_auth_by()
@@ -161,42 +166,74 @@ create_available_checks() ->
     ].
 
 create_with_no_auth_by() ->
-    {"Ensure unauthorized error thrown when no auth_by supplied"
-     ,?_assertException('throw'
-                        ,{'error', 'unauthorized'}
-                        ,knm_number:ensure_can_create(?TEST_CREATE_NUM, [])
-                       )
-    }.
+    Resp = knm_number:attempt(fun knm_number:ensure_can_create/2
+                              ,[?TEST_CREATE_NUM, []]
+                             ),
+    [{"Ensure unauthorized error thrown when no auth_by supplied"
+      ,?_assertMatch({'error', _}, Resp)
+     }
+     | check_error_response(Resp, 403, <<"forbidden">>)
+    ].
 
 create_with_disallowed_account() ->
-    {"Ensure unauthorized error when auth_by account isn't allowed to create numbers"
-     ,?_assertException('throw'
-                        ,{'error', 'unauthorized'}
-                        ,knm_number:ensure_can_create(
-                           ?TEST_CREATE_NUM
-                           ,[{<<"auth_by">>, ?RESELLER_ACCOUNT_ID}
-                             ,{<<"auth_by_account">>
-                               ,kz_account:set_allow_number_additions(?RESELLER_ACCOUNT_DOC, 'false')
-                              }
-                            ]
-                          )
-                       )
-    }.
+    Resp = knm_number:attempt(fun knm_number:ensure_can_create/2
+                              ,[?TEST_CREATE_NUM
+                                ,[{<<"auth_by">>, ?RESELLER_ACCOUNT_ID}
+                                  ,{<<"auth_by_account">>
+                                    ,kz_account:set_allow_number_additions(?RESELLER_ACCOUNT_DOC, 'false')
+                                   }
+                                 ]
+                               ]
+                             ),
+
+    [{"Ensure unauthorized error when auth_by account isn't allowed to create numbers"
+      ,?_assertMatch({'error', _}, Resp)
+     }
+     | check_error_response(Resp, 403, <<"forbidden">>)
+    ].
 
 create_with_number_porting() ->
-    {"Ensure number_is_porting error when auth_by account isn't allowed to create numbers"
-     ,?_assertException('throw'
-                        ,{'error', 'number_is_porting', ?TEST_EXISTING_NUM}
-                        ,knm_number:ensure_can_create(
-                           ?TEST_EXISTING_NUM %% pretend it is porting
-                           ,[{<<"auth_by">>, ?RESELLER_ACCOUNT_ID}
-                             ,{<<"auth_by_account">>
-                               ,kz_account:set_allow_number_additions(?RESELLER_ACCOUNT_DOC, 'true')
-                              }
-                            ]
-                          )
-                       )
-    }.
+    Resp = knm_number:attempt(fun knm_number:ensure_can_create/2
+                              ,[?TEST_EXISTING_NUM %% pretend it is porting
+                                ,[{<<"auth_by">>, ?RESELLER_ACCOUNT_ID}
+                                  ,{<<"auth_by_account">>
+                                    ,kz_account:set_allow_number_additions(?RESELLER_ACCOUNT_DOC, 'true')
+                                   }
+                                 ]
+                               ]),
+
+    [{"Ensure number_is_porting error when auth_by account isn't allowed to create numbers"
+      ,?_assertMatch({'error', _}, Resp)
+     }
+     | check_error_response(Resp, 400, <<"number_is_porting">>, ?TEST_EXISTING_NUM)
+    ].
+
+%% check_error_response(E, Code) ->
+%%     check_error_response(E, Code, 'undefined').
+check_error_response(E, Code, Error) ->
+    check_error_response(E, Code, Error, 'undefined').
+check_error_response(E, Code, Error, Cause) ->
+    check_error_response(E, Code, Error, Cause, 'undefined').
+check_error_response({'error', JObj}, Code, Error, Cause, Message) ->
+    validate_errors(JObj
+                    ,[{Code, fun knm_errors:code/1, "Verify 'code' is set properly"}
+                      ,{Error, fun knm_errors:error/1, "Verify 'error' is set properly"}
+                      ,{Cause, fun knm_errors:cause/1, "Verify 'cause' is set properly"}
+                      ,{Message, fun knm_errors:message/1, "Verify 'message' is set properly"}
+                     ]).
+
+validate_errors(JObj, Vs) ->
+    validate_errors(JObj, Vs, []).
+
+validate_errors(JObj, [{'undefined', _, _}|Vs], Tests) ->
+    validate_errors(JObj, Vs, Tests);
+validate_errors(_JObj, [], Tests) ->
+    Tests;
+validate_errors(JObj, [{V, F, L}|Vs], Tests) ->
+    validate_errors(JObj
+                    ,Vs
+                    ,[{L, ?_assertEqual(V, F(JObj))} | Tests]
+                   ).
 
 create_new_number() ->
     {"Ensure success when auth_by account is allowed to create numbers"
