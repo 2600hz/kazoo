23c0ad48f5a22367c99bcf21feb26461a7001d83
James Aimonetti <james.aimonetti@gmail.com>
2015-09-18 13:42:44 -0700

    KAZOO-3891: start testing find functionality for knm_local

--- core/kazoo_number_manager-1.0.0/test/knm_carriers_find_test.erl
+++ core/kazoo_number_manager-1.0.0/test/knm_carriers_find_test.erl
@@ -9,3 +9,14 @@
 
 -include_lib("eunit/include/eunit.hrl").
 -include("../src/knm.hrl").
+
+find_local_test_() ->
+    [{"Finding local numbers not supported"
+      ,?_assertMatch({'error', 'non_available'}
+                     ,knm_local:find_numbers(<<"415">>, 1, [])
+                    )
+     }
+     ,{"Finding local numbers returns empty list"
+       ,?_assertEqual([], knm_carriers:find(<<"415">>, 1))
+      }
+    ].
