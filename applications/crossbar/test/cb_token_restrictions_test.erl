62f20542c8c2dad8b5f37ac908d766e92e873d49
James Aimonetti <james.aimonetti@gmail.com>
2015-09-25 17:23:28 -0700

    token_restrictions_fixes: add tests file

---
+++ applications/crossbar/test/cb_token_restrictions_test.erl
@@ -0,0 +1,9 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2011-2015, 2600Hz INC
+%%% @doc
+%%% @contributors
+%%%   James Aimonetti
+%%%-------------------------------------------------------------------
+-module(cb_token_restrictions_test).
+
+-include_lib("eunit/include/eunit.hrl").
