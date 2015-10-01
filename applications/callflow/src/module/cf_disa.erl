ac10a417847bf6ee4b8a5f99364eaffe521da7f1
SIPLABS, LLC <info@siplabs.ru>
2015-10-01 18:29:08 +0600

    fixed typo in cf_disa
    Change-Id: Ifdffc25b331f9b4c686ac2f0a8bd88d9401b428e

--- applications/callflow/src/module/cf_disa.erl
+++ applications/callflow/src/module/cf_disa.erl
@@ -18,7 +18,7 @@
 
 -export([handle/2]).
 
--define(DEFAULT_USE_ACCOUNT_CALLER_ID, whapps_config:get(?CF_CONFIG_CAT, <<"refault_use_account_caller_id">>, 'true')).
+-define(DEFAULT_USE_ACCOUNT_CALLER_ID, whapps_config:get(?CF_CONFIG_CAT, <<"default_use_account_caller_id">>, 'true')).
 -define(DEFAULT_PIN_LENGTH, whapps_config:get_integer(?CF_CONFIG_CAT, <<"default_pin_length">>, 10)).
 
 %%--------------------------------------------------------------------
