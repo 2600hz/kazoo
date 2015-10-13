5473964185bd36913127f8e705d61015b1846d86
James Aimonetti <james.aimonetti@gmail.com>
2015-10-13 15:56:53 -0700

    KAZOO-3891: provide default

--- core/kazoo_number_manager-1.0.0/src/knm_config.erl
+++ core/kazoo_number_manager-1.0.0/src/knm_config.erl
@@ -1,12 +1,16 @@
 -module(knm_config).
 
--export([should_permanently_delete/1]).
+-export([should_permanently_delete/0, should_permanently_delete/1
+        ]).
 
 -include("knm.hrl").
 
+-spec should_permanently_delete() -> boolean().
 -spec should_permanently_delete(boolean()) -> boolean().
+should_permanently_delete() ->
+    should_permanently_delete('false').
 should_permanently_delete(Default) ->
-    whapps_config:get_is_true(?CONFIG_CAT
+    whapps_config:get_is_true(?KNM_CONFIG_CAT
                               ,<<"should_permanently_delete">>
-                              ,'false'
+                              ,Default
                              ).
