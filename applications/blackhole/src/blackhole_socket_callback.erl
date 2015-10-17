4bb195d8facd5ff44117e0c0098f6eb1618095fa
Kirill Sysoev <kirill.sysoev@gmail.com>
2015-10-18 01:16:45 +0300

    particular account binding removal added

--- applications/blackhole/src/blackhole_socket_callback.erl
+++ applications/blackhole/src/blackhole_socket_callback.erl
@@ -39,11 +39,28 @@ recv(_SessionPid, _SessionId, {'event', _Ignore, <<"subscribe">>, SubscriptionJO
     end,
     {'ok', Context1};
 
-recv(SessionPid, SessionId, {'event', _Ignore, <<"clear_subscriptions">>, _Data}, Context) ->
+recv(SessionPid, SessionId, {'event', _Ignore, <<"unsubscribe">>, {[]}}, Context) ->
     lager:debug("remove all bindings for session: ~p", [SessionId]),
     Filter = fun (_1, _2, _3, _4) -> filter_bindings(SessionPid, _1, _2, _3, _4) end,
     blackhole_bindings:filter(Filter),
     {'ok', Context};
+
+recv(_SessionPid, SessionId, {'event', _Ignore, <<"unsubscribe">>, SubscriptionJObj}, Context) ->
+    lager:debug("Remove binding for session: ~p. Data: ~p", [SessionId, SubscriptionJObj]),
+    Context1 = bh_context:from_subscription(Context, SubscriptionJObj),
+    case blackhole_util:is_authorized(Context1) of
+        'true' ->
+            Binding = bh_context:binding(Context1),
+            case blackhole_util:get_callback_module(Binding) of
+                'undefined' -> blackhole_util:respond_with_error(Context1);
+                Module ->
+                    blackhole_bindings:unbind(Binding, Module, 'handle_event', Context1),
+                    blackhole_util:maybe_rm_binding_from_listener(Module, Binding, Context1)
+            end;
+        'false' ->
+            blackhole_util:respond_with_authn_failure(Context1)
+    end,
+    {'ok', Context1};
 
 recv(_SessionPid, _SessionId, {'event', _Ignore, _Event, _Data}, Context) ->
     lager:debug("received event: ~p on socket ~p with data payload", [_Event, _SessionId]),
