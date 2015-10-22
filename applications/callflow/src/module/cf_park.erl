3c925263ddfb67e5dfec8d698b02b51934c3b7a8
lazedo <luis.azedo@factorlusitano.com>
2015-10-22 15:19:26 +0100

    KAZOO-4224 specs

--- applications/callflow/src/module/cf_park.erl
+++ applications/callflow/src/module/cf_park.erl
@@ -616,18 +616,22 @@ wait_for_pickup(SlotNumber, Slot, Call) ->
             cf_exe:transfer(Call)
     end.
 
+-spec ringback_timeout(ne_binary(), integer()) -> integer().
 ringback_timeout(AccountId, SlotNumber) ->
     JObj = slot_configuration(AccountId, SlotNumber),
-    wh_json:get_value(<<"ringback_time">>, JObj, ?ACCOUNT_RINGBACK_TM(AccountId)).
+    wh_json:get_integer_value(<<"ringback_time">>, JObj, ?ACCOUNT_RINGBACK_TM(AccountId)).
 
+-spec unanswered_action(integer(), wh_json:object(), whapps_call:call()) -> 'ok'.
 unanswered_action(SlotNumber, Slot, Call) ->
     JObj = slot_configuration(whapps_call:account_id(Call), SlotNumber),
     unanswered_action(SlotNumber, Slot, Call, JObj).
 
+-spec unanswered_action(integer(), wh_json:object(), whapps_call:call(), wh_json:object()) -> 'ok'.
 unanswered_action(SlotNumber, Slot, Call, JObj) ->
     Action = wh_json:get_string_value(<<"type">>, JObj, <<"repark">>),
     unanswered_action(Action, SlotNumber, Slot, Call, JObj).
 
+-spec unanswered_action(ne_binary(), integer(), wh_json:object(), whapps_call:call(), wh_json:object()) -> 'ok'.
 unanswered_action(<<"repark">>, SlotNumber, Slot, Call, _JObj) ->
     lager:info("ringback was not answered, continuing to hold parked call"),
     wait_for_pickup(SlotNumber, Slot, Call);
@@ -649,9 +653,11 @@ get_flow(Id, AccountId) ->
         _ -> 'undefined'
     end.
 
+-spec slots_configuration(ne_binary()) -> wh_json:object().
 slots_configuration(AccountId) ->
     whapps_account_config:get(AccountId, ?MOD_CONFIG_CAT, <<"slots">>, wh_json:new()).
 
+-spec slot_configuration(ne_binary(), integer()) -> wh_json:object().
 slot_configuration(AccountId, SlotNumber) ->
     wh_json:get_value(SlotNumber, slots_configuration(AccountId), wh_json:new()).
 
