3a4f1778b6f9b7759e9a89ff7085cbab5ddbf1cb
James Aimonetti <james.aimonetti@gmail.com>
2015-10-30 14:34:09 -0700

    KAZOO-3566: get out what you put in
    when asking for the previous modb, get it in the format of the modb you
    passed into the function.

--- core/kazoo_modb-1.0.0/src/kazoo_modb_util.erl
+++ core/kazoo_modb-1.0.0/src/kazoo_modb_util.erl
@@ -16,19 +16,36 @@
         ]).
 
 -spec prev_year_month(ne_binary()) -> {wh_year(), wh_month()}.
--spec prev_year_month(wh_year(), wh_month()) -> {wh_year(), wh_month()}.
+-spec prev_year_month(ne_binary() | wh_year(), ne_binary() | wh_month()) ->
+                             {wh_year(), wh_month()}.
 prev_year_month(AccountMod) ->
     {_AccountId, Year, Month} = ?MODULE:split_account_mod(AccountMod),
     prev_year_month(Year, Month).
 
+prev_year_month(<<_/binary>> = Year, Month) ->
+    prev_year_month(wh_util:to_integer(Year), Month);
+prev_year_month(Year, <<_/binary>> = Month) ->
+    prev_year_month(Year, wh_util:to_integer(Month));
 prev_year_month(Year, 1) -> {Year-1, 12};
 prev_year_month(Year, Month) -> {Year, Month-1}.
 
+prev_year_month_bin(Y, M) ->
+    {Year, Month} = prev_year_month(Y, M),
+    {wh_util:to_binary(Year), wh_util:pad_month(Month)}.
+
 -spec prev_year_month_mod(ne_binary()) -> ne_binary().
-prev_year_month_mod(AccountModb) ->
-    {AccountId, Year, Month} = split_account_mod(AccountModb),
-    {PrevYear, PrevMonth} = prev_year_month(Year, Month),
-    wh_util:format_account_id(AccountId, PrevYear, PrevMonth).
+prev_year_month_mod(?MATCH_MODB_SUFFIX_RAW(AccountId, Year, Month)) ->
+    {PrevYear, PrevMonth} = prev_year_month_bin(Year, Month),
+    ?MATCH_MODB_SUFFIX_RAW(AccountId, PrevYear, PrevMonth);
+prev_year_month_mod(?MATCH_MODB_SUFFIX_UNENCODED(A, B, Rest, Year, Month)) ->
+    {PrevYear, PrevMonth} = prev_year_month_bin(Year, Month),
+    ?MATCH_MODB_SUFFIX_UNENCODED(A, B, Rest, PrevYear, PrevMonth);
+prev_year_month_mod(?MATCH_MODB_SUFFIX_ENCODED(A, B, Rest, Year, Month)) ->
+    {PrevYear, PrevMonth} = prev_year_month_bin(Year, Month),
+    ?MATCH_MODB_SUFFIX_ENCODED(A, B, Rest, PrevYear, PrevMonth);
+prev_year_month_mod(?MATCH_MODB_SUFFIX_encoded(A, B, Rest, Year, Month)) ->
+    {PrevYear, PrevMonth} = prev_year_month_bin(Year, Month),
+    ?MATCH_MODB_SUFFIX_encoded(A, B, Rest, PrevYear, PrevMonth).
 
 -spec split_account_mod(ne_binary()) -> {ne_binary(), wh_year(), wh_month()}.
 split_account_mod(?MATCH_MODB_SUFFIX_RAW(Account,Year,Month)) ->
