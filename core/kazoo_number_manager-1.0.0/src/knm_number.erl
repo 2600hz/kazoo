4d2b8bcfa50838203c1e06bdab94045740ee4883
James Aimonetti <james.aimonetti@gmail.com>
2015-11-20 13:08:55 -0800

    KAZOO-3891: update function called

--- core/kazoo_number_manager-1.0.0/src/knm_number.erl
+++ core/kazoo_number_manager-1.0.0/src/knm_number.erl
@@ -566,7 +566,7 @@ buy(Num, Account) ->
 buy(Num, Account, Options) ->
     Updates = [{fun knm_phone_number:set_assigned_to/2, wh_util:format_account_id(Account, 'raw')}
                ,{fun knm_phone_number:set_state/2, ?NUMBER_STATE_IN_SERVICE}
-               ,fun knm_carriers:maybe_acquire/1
+               ,fun knm_carriers:acquire/1
               ],
     update(Num, Updates, Options).
 
