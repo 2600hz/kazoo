8514e3486fb0382f6618f09c0955a88a4ffea7f6
James Aimonetti <james.aimonetti@gmail.com>
2015-11-23 09:08:51 -0800

    KAZOO-3891: tests for fetching numbers

--- core/kazoo_number_manager-1.0.0/test/knm_number_test.erl
+++ core/kazoo_number_manager-1.0.0/test/knm_number_test.erl
@@ -8,3 +8,56 @@
 -module(knm_number_test).
 
 -include_lib("eunit/include/eunit.hrl").
+-include("../src/knm.hrl").
+
+get_available_test_() ->
+    [fun available_as_owner/0
+     ,fun available_as_parent/0
+     ,fun available_as_rando/0
+    ].
+
+available_as_owner() ->
+    available_as(?RESELLER_ACCOUNT_ID).
+
+available_as_parent() ->
+    available_as(?MASTER_ACCOUNT_ID).
+
+available_as_rando() ->
+    available_as(wh_util:rand_hex_binary(16)).
+
+available_as(AuthAccountId) ->
+    case knm_number:get(?TEST_AVAILABLE_NUM
+                       ,[{'auth_by', AuthAccountId}]
+                       )
+    of
+        {'ok', Number} -> available_tests(Number);
+        {'error', 'unauthorized'} ->
+            [{io_lib:format("Account ~s not authorized", [AuthAccountId])
+             ,?_assert('true')
+             }
+            ]
+    end.
+
+available_tests(Number) ->
+    ?debugFmt("n: ~p~n", [Number]),
+    PhoneNumber = knm_number:phone_number(Number),
+    ?debugFmt("pn: ~p~n", [PhoneNumber]),
+
+    [{"Verify available phone number"
+      ,?_assertEqual(?TEST_AVAILABLE_NUM, knm_phone_number:number(PhoneNumber))
+      }
+     ,{"Verify available number module"
+       ,?_assertEqual(?CARRIER_LOCAL, knm_phone_number:module_name(PhoneNumber))
+      }
+     ,{"Verify available number state"
+       ,?_assertEqual(?NUMBER_STATE_AVAILABLE, knm_phone_number:state(PhoneNumber))
+      }
+    ].
+
+get_unreconcilable_number_test_() ->
+    [{"Verify non-reconcilable numbers result in errors"
+      ,?_assertMatch({'error', 'not_reconcilable'}
+                     ,knm_number:get(<<"1000">>)
+                    )
+     }
+    ].
