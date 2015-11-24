c02a534d8aa17e2120ae45a37dd6f609713dbd82
James Aimonetti <james.aimonetti@gmail.com>
2015-11-24 09:21:12 -0800

    KAZOO-3891: update locality and config

--- core/kazoo_number_manager-1.0.0/src/knm_config.erl
+++ core/kazoo_number_manager-1.0.0/src/knm_config.erl
@@ -2,9 +2,13 @@
 
 -export([should_permanently_delete/0, should_permanently_delete/1
          ,released_state/0, released_state/1
+         ,default_locality_url/0, default_locality_url/1
+         ,locality_url/0, locality_url/1
         ]).
 
 -include("knm.hrl").
+
+-define(LOCALITY_CONFIG_CAT, <<"number_manager.locality">>).
 
 -spec should_permanently_delete() -> boolean().
 -spec should_permanently_delete(boolean()) -> boolean().
@@ -30,3 +34,17 @@ released_state(Default) ->
                              ,Default
                             ).
 -endif.
+
+-spec default_locality_url() -> api_binary().
+-spec default_locality_url(api_binary()) -> api_binary().
+default_locality_url() ->
+    default_locality_url('undefined').
+default_locality_url(Default) ->
+    whapps_config:get(<<"number_manager.other">>, <<"phonebook_url">>, Default).
+
+-spec locality_url() -> api_binary().
+-spec locality_url(api_binary()) -> api_binary().
+locality_url() ->
+    locality_url(default_locality_url()).
+locality_url(Default) ->
+    whapps_config:get(?LOCALITY_CONFIG_CAT, <<"url">>, Default).
