a682f678df02fa9119cc880f8da79ad47d79e863
Mark Magnusson <mark@2600hz.com>
2015-11-30 15:52:20 +0000

    KAZOO-4344, KAZOO-4361: add basic session interface and basic queue json schema

---
+++ scripts/dev-start-node.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+cd `dirname $0`
+
+if [ -z "$1"]
+then
+  NODE_NAME=kazoo-dev
+else
+  NODE_NAME=$1
+fi
+
+export ERL_CRASH_DUMP=$PWD/../$(date +%s)_dev_erl_crash.dump
+export ERL_LIBS="$ERL_LIBS":$PWD/../deps:$PWD/../core:$PWD/../applications/
+exec erl -name $NODE_NAME -args_file /etc/kazoo/vm.args -s reloader -s lager
