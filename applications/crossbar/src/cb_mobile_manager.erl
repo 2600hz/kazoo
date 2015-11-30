81bdaaa546f11f7fb943d149cd69a35e32d5a7ce
Peter Defebvre <macpie59@gmail.com>
2015-11-30 23:40:51 +0000

    KAZOO-4350: Fix spec

--- applications/crossbar/src/cb_mobile_manager.erl
+++ applications/crossbar/src/cb_mobile_manager.erl
@@ -63,7 +63,7 @@ handle_resp(_Error) ->
 %%
 %% @end
 %%--------------------------------------------------------------------
--spec req_uri(ne_binaries()) -> list() |  'undefined'.
+-spec req_uri(ne_binaries()) -> api_list().
 req_uri(ExplodedPath) ->
     case whapps_config:get_binary(?MOD_CONFIG_CAT, <<"url">>) of
         'undefined' -> 'undefined';
