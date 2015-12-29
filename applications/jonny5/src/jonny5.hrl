ac66e84f4c3c1a970a8ddac4268669adbf8ba702
karl anderson <karl@2600hz.com>
2015-12-28 18:09:47 -0800

    KAZOO-4473: handle when devices are authd by IP

--- applications/jonny5/src/jonny5.hrl
+++ applications/jonny5/src/jonny5.hrl
@@ -7,6 +7,12 @@
 
 -define(DEFAULT_RATE, 0.5).
 
+-define(INBOUND_ACCOUNT_TYPES,
+        [<<"account">>
+         ,<<"device">>
+         ,<<"sys_info">>
+        ]).
+
 -define(APP_VERSION, <<"2.0.0">>).
 -define(APP_NAME, <<"jonny5">>).
 
