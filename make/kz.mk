3685a25af688a8ed1f0037b8447c469b298197d8
Pierre Fenoll <pierrefenoll@gmail.com>
2016-01-13 20:44:15 +0100

    KAZOO-4362: tiny tiny single quote breaking the whole build system. Cute

--- make/kz.mk
+++ make/kz.mk
@@ -58,7 +58,7 @@ test/$(PROJECT).app: ERLC_OPTS += -DTEST
 test/$(PROJECT).app: $(wildcard $(TEST_SOURCES))
 	@mkdir -p test/
 	ERL_LIBS=$(ERL_LIBS) erlc -v $(ERLC_OPTS) $(TEST_PA) -o test/ $(TEST_SOURCES)
-	@sed "s/{modules, \[\]}/{modules, \[`echo ebin/*.beam | sed 's%\.beam ebin/%, %g;s%ebin/%%;s/\.beam//''`\]}/" src/$(PROJECT).app.src > $@
+	@sed "s/{modules, \[\]}/{modules, \[`echo ebin/*.beam | sed 's%\.beam ebin/%, %g;s%ebin/%%;s/\.beam//'`\]}/" src/$(PROJECT).app.src > $@
 
 
 clean: clean-test
