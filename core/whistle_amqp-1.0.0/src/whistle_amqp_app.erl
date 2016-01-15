52880bbaffb0557ce8dba9ad70c374de44fe4fe7
James Aimonetti <james@2600hz.com>
2016-01-14 16:02:11 -0800

    KAZOO-4362: update whistle_amqp app stuff

--- core/whistle_amqp-1.0.0/src/whistle_amqp_app.erl
+++ core/whistle_amqp-1.0.0/src/whistle_amqp_app.erl
@@ -10,7 +10,7 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    whistle_amqp:start_link().
+    wh_amqp_sup:start_link().
 
 stop(_State) ->
-    'ok'.
+    exit(whereis('wh_amqp_sup'), 'shutdown').
