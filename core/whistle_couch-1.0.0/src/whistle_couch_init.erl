cfd6558812af4538e3602ae8dd62723051dba019
James Aimonetti <james@2600hz.com>
2016-01-14 16:21:53 -0800

    KAZOO-4362: init whistle_couch

---
+++ core/whistle_couch-1.0.0/src/whistle_couch_init.erl
@@ -0,0 +1,35 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2010-2016, 2600Hz INC
+%%% @doc
+%%%
+%%% @end
+%%% @contributors
+%%%   James Aimonetti
+%%%-------------------------------------------------------------------
+-module(whistle_couch_init).
+
+-include_lib("whistle/include/wh_types.hrl").
+
+-export([start_link/0]).
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Starts the app for inclusion in a supervisor tree
+%% @end
+%%--------------------------------------------------------------------
+-spec start_link() -> startlink_ret().
+start_link() ->
+    _ = declare_exchanges(),
+    'ignore'.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Ensures that all exchanges used are declared
+%% @end
+%%--------------------------------------------------------------------
+-spec declare_exchanges() -> 'ok'.
+declare_exchanges() ->
+    _ = wapi_conf:declare_exchanges(),
+    wapi_self:declare_exchanges().
