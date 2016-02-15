e3060f1ff154772b89395ec854289750f532d921
lazedo <luis.azedo@factorlusitano.com>
2016-02-15 11:49:24 +0000

    add loopback control to wapi_resource

--- core/whistle/src/api/wapi_resource.erl
+++ core/whistle/src/api/wapi_resource.erl
@@ -83,6 +83,8 @@
                                          ,<<"Fax-Timezone">>
                                          ,<<"Intercept-Unbridged-Only">>
                                          ,<<"Simplify-Loopback">> %% loopback_bowout flag
+                                         ,<<"Loopback-Bowout">>
+                                         ,<<"Start-Control-Process">>
                                          | wapi_dialplan:optional_bridge_req_headers()
                                         ]).
 -define(ORIGINATE_REQ_VALUES, [{<<"Event-Category">>, <<"resource">>}
