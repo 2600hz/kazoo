5b589989aa13d0e7fbd3bb13d8022267a2791cd3
Pierre Fenoll <pierrefenoll@gmail.com>
2016-02-16 15:10:49 +0100

    KAZOO-KNM: bump app version

--- core/kazoo_number_manager/src/knm.hrl
+++ core/kazoo_number_manager/src/knm.hrl
@@ -2,7 +2,7 @@
 -include_lib("whistle/include/wh_databases.hrl").
 -include_lib("kazoo_number_manager/include/knm_phone_number.hrl").
 
--define(APP_VERSION, <<"1.0.0">>).
+-define(APP_VERSION, <<"4.0.0">>).
 -define(APP_NAME, <<"kazoo_number_manager">>).
 
 -define(KNM_CACHE, 'knm_cache').
@@ -15,7 +15,8 @@
 -define(KEY_DISPLAY_NAME, <<"display_name">>).
 -define(KEY_INBOUND_LOOKUP, <<"inbound_lookup">>).
 
--define(KNM_USER_AGENT, "Kazoo Number Manager 1.0.0").
+-define(KNM_USER_AGENT, "Kazoo Number Manager " ++ binary_to_list(?APP_VERSION)).
+
 
 -ifdef(TEST).
 -include_lib("eunit/include/eunit.hrl").
