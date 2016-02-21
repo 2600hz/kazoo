168ed06826d8608432a385aecc2f1853cda45147
lazedo <luis.azedo@factorlusitano.com>
2016-02-21 13:11:01 +0000

    add kazoo_data_cache
    this is because of the way deps start

---
+++ core/kazoo_data_cache/src/kazoo_data_cache.erl
@@ -0,0 +1,27 @@
+-module(kazoo_data_cache).
+
+-behaviour(application).
+
+-include_lib("whistle/include/wh_types.hrl").
+
+%% Application callbacks
+-export([start/2, stop/1]).
+
+%% ===================================================================
+%% Application callbacks
+%% ===================================================================
+%%--------------------------------------------------------------------
+%% @public
+%% @doc Implement the application start behaviour
+%%--------------------------------------------------------------------
+-spec start(application:start_type(), any()) -> startapp_ret().
+start(_StartType, _StartArgs) ->
+    kazoo_data_cache_sup:start_link().
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc Implement the application stop behaviour
+%%--------------------------------------------------------------------
+-spec stop(any()) -> any().
+stop(_State) ->
+    'ok'.
---
+++ core/kazoo_data_cache/src/kz_data_cache.hrl
@@ -0,0 +1,12 @@
+-ifndef(KZ_DATA_CACHE_HRL).
+-include_lib("whistle/include/wh_types.hrl"). % get the whistle types
+-include_lib("whistle/include/wh_log.hrl").
+-include_lib("whistle/include/wh_databases.hrl").
+-include_lib("whistle/include/kz_system_config.hrl").
+
+-define(KZ_DATA_CACHE, 'kazoo_data_cache').
+-define(CONFIG_CAT, <<"datamgr">>).
+
+
+-define(KZ_DATA_CACHE_HRL, 'true').
+-endif.
