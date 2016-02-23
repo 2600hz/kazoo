98a40d5bf7090e4c5357838d7549f14b56c80d5c
lazedo <luis.azedo@factorlusitano.com>
2016-02-23 10:21:38 +0000

    KAZOO-4574 typo

--- core/kazoo_sip/include/kzsip_uri.hrl
+++ core/kazoo_sip/include/kzsip_uri.hrl
@@ -2,8 +2,8 @@
 
 -include_lib("nklib/include/nklib.hrl").
 
-type uri() :: nklib:uri().
-type uris() :: nklib_parse_uri:uris().
+-type uri() :: nklib:uri().
+-type uris() :: nklib_parse_uri:uris().
 
 -define(KZSIP_URI_HRL, 'true').
 -endif.
