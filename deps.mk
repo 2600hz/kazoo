af15e015887fb8feb1373573393cefdd7de82151
lazedo <luis.azedo@factorlusitano.com>
2016-02-23 14:28:18 +0000

    KAZOO-4574 dependencies

--- deps.mk
+++ deps.mk
@@ -14,11 +14,13 @@ dep_amqp_client_commit = rabbitmq_v3_6_0
 
 dep_eflame = git https://github.com/slfritchie/eflame 7b0bb1a7e8c8482a59421a3a50ae69d49af59d52
 
-dep_socketio = git https://github.com/lazedo/socket.io-cowboy 0.2
-
 dep_detergent = git https://github.com/pap/detergent e86dfeded3e4f9f3f9278c6a1aea802079d38b54
+
+
+### need to update upstream ###
+dep_socketio = git https://github.com/lazedo/socket.io-cowboy 0.2
 
 dep_hep = git https://github.com/lazedo/hep 1.5.4
 
-dep_ecsv = git https://github.com/rcouch/ecsv 0.2
+dep_ecsv = git https://github.com/lazedo/ecsv 0.2.1
 
