c6f40acf0518a5d8d5a4fc637af94f7d7dc7e7fd
James Aimonetti <james@2600hz.com>
2016-02-29 23:05:10 +0000

    HELP-20785: add specs

--- core/whistle_media/src/wh_media_map.erl
+++ core/whistle_media/src/wh_media_map.erl
@@ -356,6 +356,8 @@ maybe_add_prompt(AccountId, JObj, PromptId) ->
 
     insert_map(UpdatedMap).
 
+-spec insert_map(media_map()) -> 'ok' | 'true'.
+-spec insert_map(media_map(), pid()) -> 'ok' | 'true'.
 insert_map(Map) ->
     insert_map(Map, whereis(?MODULE)).
 insert_map(Map, Srv) when Srv =:= self() ->
@@ -399,6 +401,8 @@ init_account_map(AccountId, PromptId) ->
                                     },
     new_map(AccountMap).
 
+-spec new_map(media_map()) -> 'true'.
+-spec new_map(media_map(), pid()) -> 'true'.
 new_map(Map) ->
     new_map(Map, whereis(?MODULE)).
 
