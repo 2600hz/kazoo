96e1422ba876619fe583aa04acd0bfcb5250796c
James Aimonetti <james@2600hz.com>
2016-02-29 22:55:15 +0000

    HELP-20785: better tombstones
    Also check for hard-deleted docs when publishing changes

--- core/whistle_couch/src/couch_util.erl
+++ core/whistle_couch/src/couch_util.erl
@@ -81,10 +81,12 @@
 -define(MAX_BULK_INSERT, 2000).
 -define(RETRY_504(F), retry504s(fun() -> F end)).
 
--define(PUBLISH_FIELDS, [<<"pvt_type">>
+-define(PUBLISH_FIELDS, [<<"_deleted">>
                          ,<<"pvt_account_id">>
                          ,<<"pvt_created">>
+                         ,<<"pvt_deleted">>
                          ,<<"pvt_modified">>
+                         ,<<"pvt_type">>
                         ]).
 
 -type db_create_options() :: [{'q',integer()} | {'n',integer()}].
@@ -767,7 +769,14 @@ maybe_tombstone(JObj) ->
     maybe_tombstone(JObj, wh_json:is_true(<<"_deleted">>, JObj, 'false')).
 
 maybe_tombstone(JObj, 'true') ->
-    wh_json:delete_keys(?PUBLISH_FIELDS, JObj);
+    wh_json:from_list(
+      props:filter_undefined(
+        [{<<"_id">>, wh_doc:id(JObj)}
+        ,{<<"_rev">>, wh_doc:revision(JObj)}
+        ,{<<"_deleted">>, 'true'}
+        ]
+       )
+     );
 maybe_tombstone(JObj, 'false') -> JObj.
 
 -spec maybe_set_docid(wh_json:object()) -> wh_json:object().
@@ -1059,21 +1068,22 @@ publish(Action, Db, Doc) ->
     Id = wh_doc:id(Doc),
 
     IsSoftDeleted = wh_doc:is_soft_deleted(Doc),
-    EventName = doc_change_event_name(Action, IsSoftDeleted),
+    IsHardDeleted = wh_doc:is_deleted(Doc),
+    EventName = doc_change_event_name(Action, IsSoftDeleted orelse IsHardDeleted),
 
     Props =
         [{<<"ID">>, Id}
-         ,{<<"Type">>, Type}
-         ,{<<"Database">>, Db}
-         ,{<<"Rev">>, wh_doc:revision(Doc)}
-         ,{<<"Account-ID">>, doc_acct_id(Db, Doc)}
-         ,{<<"Date-Modified">>, wh_doc:created(Doc)}
-         ,{<<"Date-Created">>, wh_doc:modified(Doc)}
-         ,{<<"Is-Soft-Deleted">>, IsSoftDeleted}
+        ,{<<"Type">>, Type}
+        ,{<<"Database">>, Db}
+        ,{<<"Rev">>, wh_doc:revision(Doc)}
+        ,{<<"Account-ID">>, doc_acct_id(Db, Doc)}
+        ,{<<"Date-Modified">>, wh_doc:created(Doc)}
+        ,{<<"Date-Created">>, wh_doc:modified(Doc)}
+        ,{<<"Is-Soft-Deleted">>, IsSoftDeleted}
          | wh_api:default_headers(<<"configuration">>
-                                  ,EventName
-                                  ,?CONFIG_CAT
-                                  ,<<"1.0.0">>
+                                 ,EventName
+                                 ,?CONFIG_CAT
+                                 ,<<"1.0.0">>
                                  )
         ],
     Fun = fun(P) -> wapi_conf:publish_doc_update(Action, Db, Type, Id, P) end,
