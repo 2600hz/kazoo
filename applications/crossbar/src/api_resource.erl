1cc3659f612509aca8f1276d87f4a54f3822a5dd
Max Lay <kalda341@gmail.com>
2016-03-04 12:26:30 +1300

    Let Cowboy determine content length of a CSV response

--- applications/crossbar/src/api_resource.erl
+++ applications/crossbar/src/api_resource.erl
@@ -738,7 +738,6 @@ to_csv(Req, Context) ->
         _ ->
             RespBody = maybe_flatten_jobj(Context1),
             RespHeaders1 = [{<<"Content-Type">>, <<"application/octet-stream">>}
-                            ,{<<"Content-Length">>, iolist_size(RespBody)}
                             ,{<<"Content-Disposition">>, <<"attachment; filename=\"data.csv\"">>}
                             | cb_context:resp_headers(Context1)
                            ],
