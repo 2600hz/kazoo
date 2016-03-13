08d8765ff0b17fab1e10d2b001891e41fe2f08fb
karl anderson <karl@2600hz.com>
2016-03-13 03:27:20 +0000

    KAZOO-4588: address comments

--- applications/crossbar/src/cb_apps_util.erl
+++ applications/crossbar/src/cb_apps_util.erl
@@ -63,10 +63,11 @@ is_authorized(AccountId, UserId, AppId) ->
             'false';
         {'ok', Doc} ->
             AppJObj = wh_json:get_value(AppId, kzd_apps_store:apps(Doc)),
-            Allowed = wh_json:get_value(<<"allowed_users">>, AppJObj, <<"specific">>),
-            Users = wh_json:get_value(<<"users">>, AppJObj, []),
-            UserIds = [wh_json:get_value(<<"id">>, U, []) || U <- Users],
-            case {Allowed, UserIds} of
+            AllowedType = wh_json:get_value(<<"allowed_users">>, AppJObj, <<"specific">>),
+            SpecificIds = get_specific_ids(
+                            wh_json:get_value(<<"users">>, AppJObj, [])
+                           ),
+            case {AllowedType, SpecificIds} of
                 {<<"all">>, _} -> 'true';
                 {<<"specific">>, []} -> 'false';
                 {<<"specific">>, UserIds} ->
@@ -77,6 +78,18 @@ is_authorized(AccountId, UserId, AppId) ->
                     lager:error("unknown data ~p : ~p", [_A, _U]),
                     'false'
             end
+    end.
+
+-spec get_specific_ids(wh_json:objects()) -> ne_binaries().
+get_specific_ids(Users) ->
+    get_specific_ids(Users, []).
+
+-spec get_specific_ids(wh_json:objects(), ne_binaries()) -> ne_binaries().
+get_specific_ids([], UserIds) -> UserIds;
+get_specific_ids([User|Users], UserIds) ->
+    case wh_json:get_ne_value(<<"id">>, User) of
+        'undefined' -> get_specific_ids(Users, UserIds);
+        UserId -> get_specific_ids(Users, [UserId|UserIds])
     end.
 
 %%--------------------------------------------------------------------
