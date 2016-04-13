68c0c9be41e3b99156d71cafffa942148694b792
Pierre Fenoll <pierrefenoll@gmail.com>
2016-04-13 15:12:36 +0200

    knm-REST-fix: move summary/2 into its own function

--- applications/crossbar/src/modules_v2/cb_phone_numbers_v2.erl
+++ applications/crossbar/src/modules_v2/cb_phone_numbers_v2.erl
@@ -297,13 +297,7 @@ validate(Context, Number) ->
 
 -spec validate_number(cb_context:context(), path_token(), http_method()) -> cb_context:context().
 validate_number(Context, Number, ?HTTP_GET) ->
-    Options = [{'auth_by', cb_context:auth_account_id(Context)}
-              ],
-    case knm_number:get(Number, Options) of
-        {'ok', KNMNumber} ->
-            crossbar_util:response(knm_number:to_public_json(KNMNumber), Context);
-        {'error', _JObj} -> reply_number_not_found(Context)
-    end;
+    summary(Context, Number);
 validate_number(Context, _Number, ?HTTP_POST) ->
     validate_request(Context);
 validate_number(Context, _Number, ?HTTP_PUT) ->
@@ -448,7 +442,21 @@ delete(Context, Number) ->
 
 %%--------------------------------------------------------------------
 %% @private
-%% @doc Lists numbers on GET /v2/accounts/{{ACCOUNT_ID}}/phone_numbers[/{{DID}}]
+%% @doc Lists number on GET /v2/accounts/{{ACCOUNT_ID}}/phone_numbers/{{DID}}
+%%--------------------------------------------------------------------
+-spec summary(cb_context:context(), ne_binary()) -> cb_context:context().
+summary(Context, Number) ->
+    Options = [{'auth_by', cb_context:auth_account_id(Context)}
+              ],
+    case knm_number:get(Number, Options) of
+        {'ok', KNMNumber} ->
+            crossbar_util:response(knm_number:to_public_json(KNMNumber), Context);
+        {'error', _JObj} -> reply_number_not_found(Context)
+    end.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc Lists numbers on GET /v2/accounts/{{ACCOUNT_ID}}/phone_numbers
 %%--------------------------------------------------------------------
 -spec summary(cb_context:context()) -> cb_context:context().
 summary(Context) ->
