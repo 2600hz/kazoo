caf1098144e1f865e2f3380dbcf8095045700ad0
lazedo <luis.azedo@factorlusitano.com>
2016-04-20 13:59:59 +0100

    allow document update for backwards compatibility

--- core/kazoo_data/src/kzs_attachments.erl
+++ core/kazoo_data/src/kzs_attachments.erl
@@ -63,7 +63,7 @@ put_attachment(#{att_handler := {Handler, Params}}=Map
             CT = props:get_value('content_type', Options, kz_mime:from_filename(AName)),
             Size = size(Contents),
             Att = attachment_from_handler(AName, attachment_handler_jobj(Handler, Props), Size, CT),
-            handle_put_attachment(Map, Att, DbName, DocId, AName, Contents, Options);
+            handle_put_attachment(Map, Att, DbName, DocId, AName, Contents, Options, Props);
         {'error', _} = E -> E
     end;
 put_attachment(#{server := {App, Conn}}, DbName, DocId, AName, Contents, Options) ->
@@ -80,27 +80,31 @@ attachment_from_handler(AName, AttHandler, Size, CT) ->
     wh_json:set_value(AName, wh_json:from_list(Props), wh_json:new()).
 
 attachment_handler_jobj(Handler, Props) ->
-    wh_json:set_value(wh_util:to_binary(Handler), wh_json:from_list(Props), wh_json:new()).
+    JObj = wh_json:from_list(props:get_value('attachment', Props, [])),
+    wh_json:set_value(wh_util:to_binary(Handler), JObj, wh_json:new()).
 
--spec handle_put_attachment(map(), wh_json:object(), ne_binary(), ne_binary(), ne_binary(), ne_binary(), wh_proplist()) ->
+-spec handle_put_attachment(map(), wh_json:object(), ne_binary(), ne_binary(), ne_binary(), ne_binary()
+                           , wh_proplist(), wh_proplist()) ->
                             {'ok', wh_json:object()} |
                             data_error().
 
 handle_put_attachment(#{att_post_handler := 'stub'
                        ,server := {App, Conn}
-                       }, _Att, DbName, DocId, AName, Contents, Options) ->
+                       }, _Att, DbName, DocId, AName, Contents, Options, _Props) ->
     kzs_cache:flush_cache_doc(DbName, DocId),
     App:put_attachment(Conn, DbName, DocId, AName, Contents, Options);
 
-handle_put_attachment(#{att_post_handler := 'external'}, Att, DbName, DocId, _AName, _Contents, Options) ->
+handle_put_attachment(#{att_post_handler := 'external'}, Att, DbName, DocId, _AName, _Contents, Options, Props) ->
     case kz_datamgr:open_cache_doc(DbName, DocId, Options) of
-        {'ok', JObj} -> external_attachment(DbName, JObj, Att);
+        {'ok', JObj} -> external_attachment(DbName, JObj, Att, Props);
         {'error', _}=E -> E
     end.
 
-external_attachment(DbName, JObj, Att) ->
+external_attachment(DbName, JObj, Att, Props) ->
     Atts = wh_json:merge_jobjs(Att, wh_json:get_value(?KEY_STUB_ATTACHMENTS, JObj, wh_json:new())),
-    kz_datamgr:save_doc(DbName, wh_json:set_value(?KEY_STUB_ATTACHMENTS, Atts, JObj)).
+    kz_datamgr:save_doc(DbName, wh_json:set_values([{?KEY_STUB_ATTACHMENTS, Atts}
+                                                    | props:get_value('document', Props, [])
+                                                   ], JObj)).
 
 -spec delete_attachment(map(), ne_binary(), ne_binary(), ne_binary(), wh_proplist()) ->
                                {'ok', wh_json:object()} |
