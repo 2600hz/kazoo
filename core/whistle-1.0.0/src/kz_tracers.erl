ac521077f382e2f639c5de53aedeecb99fefda8c
James Aimonetti <james@2600hz.com>
2016-04-28 06:43:01 +0000

    HELP-22210: clear trace and stop later

--- core/whistle-1.0.0/src/kz_tracers.erl
+++ core/whistle-1.0.0/src/kz_tracers.erl
@@ -14,6 +14,7 @@ add_trace(Pid) ->
 add_trace(Pid, CollectFor) ->
     spawn(fun() ->
                   io:format("started trace for ~p in ~p~n", [Pid, self()]),
+                  dbg:stop_clear(),
                   BinFile = wh_util:to_list(<<"/home/james/eflame.trace">>),
                   eflame2:write_trace('global_calls_plus_new_procs'
                                      ,BinFile
@@ -32,7 +33,12 @@ gen_load(N) ->
     wait_for_refs(Start, {0, 'ok'}, PidRefs).
 
 wait_for_refs(Start, MaxMailbox, []) ->
-    io:format("finished test after ~pms: ~p~n", [wh_util:elapsed_ms(Start), MaxMailbox]);
+    case cache_data() of
+        [{message_queue_len, 0} | _] ->
+            io:format("finished test after ~pms: ~p~n", [wh_util:elapsed_ms(Start), MaxMailbox]);
+        _ -> timer:sleep(1000),
+             wait_for_refs(Start, MaxMailbox, [])
+    end;
 wait_for_refs(Start, {M, G}, [{Pid, Ref}|R]=PRs) ->
     receive
         {'DOWN', Ref, 'process', Pid, _Reason} ->
@@ -42,7 +48,7 @@ wait_for_refs(Start, {M, G}, [{Pid, Ref}|R]=PRs) ->
                 [{message_queue_len, N}
                  ,{current_function, F}
                 ] when N > M ->
-                    cache_status(Start),
+                    io:format("new max message queue size ~p (~p)~n", [N, F]),
                     wait_for_refs(Start, {N, F}, PRs);
                 _ ->
                     wait_for_refs(Start, {M, G}, PRs)
@@ -50,13 +56,12 @@ wait_for_refs(Start, {M, G}, [{Pid, Ref}|R]=PRs) ->
     end.
 
 do_load_gen(1) ->
-    add_trace(whereis(whistle_couch_cache)),
+    %% add_trace(whereis(whistle_couch_cache)),
     do_load_gen();
 do_load_gen(_) ->
     do_load_gen().
 
 do_load_gen() ->
-    Start = os:timestamp(),
     AccountId = wh_util:rand_hex_binary(16),
     AccountDb = wh_util:format_account_db(AccountId),
     'true' = couch_mgr:db_create(AccountDb),
@@ -64,31 +69,35 @@ do_load_gen() ->
     io:format("building ~p~n", [AccountDb]),
 
     Docs = [new_doc(AccountDb, Doc) || Doc <- lists:seq(1,1000)],
-    {'ok', Saved} = couch_mgr:save_docs(AccountDb, Docs),
-    _Result = (catch do_stuff_to_docs(Start, AccountDb, Saved)),
+
+    Start = os:timestamp(),
+
+    _Result = (catch do_stuff_to_docs(Start, AccountDb, Docs)),
     couch_mgr:db_delete(AccountDb),
     io:format("deleted ~s~nafter ~p~n", [AccountDb, _Result]).
 
 new_doc(AccountDb, Ref) ->
-    wh_doc:update_pvt_parameters(
-      wh_json:from_list([{<<"_id">>, wh_util:rand_hex_binary(16)}
-                        ,{<<"ref">>, Ref}
-                        ,{<<"pvt_type">>, <<"load_test">>}
-                         | [{wh_util:rand_hex_binary(8)
-                             ,wh_util:rand_hex_binary(8)
-                            }
-                            || _ <- lists:seq(1, 12)
-                           ]
-                        ])
-                                ,AccountDb
-     ).
+    Doc = wh_doc:update_pvt_parameters(
+            wh_json:from_list([{<<"_id">>, wh_util:rand_hex_binary(16)}
+                              ,{<<"ref">>, Ref}
+                              ,{<<"pvt_type">>, <<"load_test">>}
+                               | [{wh_util:rand_hex_binary(8)
+                                  ,wh_util:rand_hex_binary(8)
+                                  }
+                                  || _ <- lists:seq(1, 12)
+                                 ]
+                              ])
+                                      ,AccountDb
+           ),
+    {'ok', Saved} = couch_mgr:save_doc(AccountDb, Doc),
+    {'ok', _Loaded} = couch_mgr:open_cache_doc(AccountDb, wh_doc:id(Saved)),
+    Saved.
 
 do_stuff_to_docs(Start, _AccountDb, []) ->
     wait_for_cache(Start);
 do_stuff_to_docs(Start, AccountDb, Docs) ->
     cache_status(Start),
     Ops = [{op(), Doc} || Doc <- Docs],
-    io:format("doing stuff to ~p docs~n", [length(Docs)]),
     perform_ops(Start, AccountDb, Ops).
 
 perform_ops(Start, AccountDb, Ops) ->
@@ -113,23 +122,29 @@ cache_data() ->
                        ).
 
 wait_for_cache(Start) ->
+    wait_for_cache(Start, {0, 'ok'}).
+
+wait_for_cache(Start, {N, F}) ->
     case cache_data() of
+        [{message_queue_len, M}
+         ,{current_function, G}
+        ] when M > N ->
+            io:format("~p new max msg queue size ~p (~p)~n", [Start, M, G]),
+            timer:sleep(1000),
+            wait_for_cache(Start, {M, G});
         [{message_queue_len, 0}
-         ,{current_function, _F}
+        ,{current_function, _F}
         ] ->
             io:format("~pms done (in ~p)~n", [wh_util:elapsed_ms(Start), _F]);
-        [{message_queue_len, Len}
-         ,{current_function, F}
-        ] ->
-            io:format("~pms msg left ~p in ~p~n", [wh_util:elapsed_ms(Start), Len, F]),
+        _ ->
             timer:sleep(1000),
-            wait_for_cache(Start)
+            wait_for_cache(Start, {N, F})
     end.
 
 perform_op({'edit', Doc}, Acc, AccountDb) ->
     Inc = wh_json:get_integer_value(<<"inc">>, Doc, 0),
     Edited = wh_json:set_value(<<"inc">>, Inc+1, Doc),
-    {'ok', Saved} = couch_mgr:ensure_saved(AccountDb, Edited),
+    {'ok', Saved} = couch_mgr:save_doc(AccountDb, Edited),
     {'ok', _Loaded} = couch_mgr:open_cache_doc(AccountDb, wh_doc:id(Saved)),
     [Saved | Acc];
 perform_op({'delete', Doc}, Acc, AccountDb) ->
