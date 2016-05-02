ed600555ea02fd08d540dcd57e157778b0bef697
Pierre Fenoll <pierrefenoll@gmail.com>
2016-05-02 20:15:24 +0200

    ci-docs: check how many API endpoints are documented (120 / 475)

---
+++ scripts/state-of-docs.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env python2
+
+# Find undocumented API endpoints
+
+from glob import glob
+import re
+import sys
+
+print 'Undocumented API endpoints:'
+
+APIs = []
+for fn in glob('applications/crossbar/doc/ref/*.md'):
+    with open(fn, 'r') as fd:
+        ref = fd.read()
+        endpoints = re.findall('> [^\n]+', ref)
+        APIs.extend(endpoints)
+
+MDs = []
+for fn in glob('applications/crossbar/doc/*.md'):
+    with open(fn, 'r') as fd:
+        MDs.append(fd.read())
+
+documented, undocumented = 0, 0
+for endpoint in APIs:
+    found = False
+    for MD in MDs:
+        if '\n'+endpoint+'\n' in MD:
+            found = True
+            break
+    if found:
+        documented += 1
+    else:
+        print endpoint
+        undocumented += 1
+
+print documented, '/', documented + undocumented
+sys.exit(undocumented)
