9b63041e283729b06c31fd98b1b9c6cb1ba12c17
Pierre Fenoll <pierrefenoll@gmail.com>
2016-05-04 18:41:29 +0200

    sup-cmd: fix symlink creation

--- scripts/ecallmgr-process-count.sh
+++ scripts/ecallmgr-process-count.sh
@@ -2,15 +2,15 @@
 
 cd $(dirname $0)
 
-NODE="ecallmgr"
+NODE=ecallmgr
 HOSTNAME=$(/bin/hostname)
 REMOTE_SHELL="${NODE}@${HOSTNAME}"
-SUP="../core/sup/priv/sup"
+SUP=../core/sup/priv/sup
 
 if [ -z "${1}" ]; then
     ERL_COOKIE=$(${SUP} -n ${NODE} erlang get_cookie | sed "s/'//g")
 else
-	ERL_COOKIE="${1}"
+    ERL_COOKIE="${1}"
 fi
 
 echo $(erl_call -c ${ERL_COOKIE} -name ${REMOTE_SHELL} -a "erlang system_info ['process_count']")
