94c4cd8bffc118dd0957bb1ce25dff25965e6be8
James Aimonetti <james@2600hz.com>
2016-05-05 23:16:04 +0000

    KAZOO-4739: migrate endpoint/attributes to kazoo_endpoint

---
+++ core/kazoo_endpoint/src/kazoo_endpoint.hrl
@@ -0,0 +1,17 @@
+-ifndef(KAZOO_ENDPOINT_HRL).
+
+-include_lib("kazoo/include/kz_types.hrl").
+-include_lib("kazoo/include/kz_log.hrl").
+-include_lib("kazoo_number_manager/include/knm_phone_number.hrl").
+
+-define(APP_NAME, <<"kazoo_endpoint">>).
+-define(APP_VERSION, kz_util:application_version('kazoo_endpoint')).
+-define(CONFIG_CAT, <<"kazoo_endpoint">>).
+
+-define(CACHE_NAME, 'kazoo_endpoint_cache').
+
+-define(ATTR_LOWER_KEY, <<109,108,112,112>>).
+-define(ATTR_UPPER_KEY, <<109,097,120,095,112,114,101,099,101,100,101,110,099,101>>).
+
+-define(KAZOO_ENDPOINT_HRL, 'true').
+-endif.
---
+++ core/kazoo_endpoint/src/kazoo_endpoint_sup.erl
@@ -0,0 +1,74 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2016, 2600Hz
+%%% @doc
+%%%
+%%% @end
+%%% @contributors
+%%%   Karl Anderson
+%%%   James Aimonetti
+%%%-------------------------------------------------------------------
+-module(kazoo_endpoint_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+-export([listener_proc/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+-include("kazoo_endpoint.hrl").
+
+-define(SERVER, ?MODULE).
+
+-define(ORIGIN_BINDINGS, [[{'type', <<"account">>}]
+                         ,[{'type', <<"user">>}]
+                         ,[{'type', <<"device">>}]
+                         ]).
+
+-define(CACHE_PROPS, [{'origin_bindings', ?ORIGIN_BINDINGS}]).
+
+-define(CHILDREN, [?CACHE_ARGS(?CACHE_NAME, ?CACHE_PROPS)]).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc Starts the supervisor
+%%--------------------------------------------------------------------
+-spec start_link() -> startlink_ret().
+start_link() ->
+    supervisor:start_link({'local', ?SERVER}, ?MODULE, []).
+
+-spec listener_proc() -> {'ok', pid()}.
+listener_proc() ->
+    [P] = [P || {Mod, P, _, _} <- supervisor:which_children(?SERVER),
+                Mod =:= 'cf_listener'
+          ],
+    {'ok', P}.
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Whenever a supervisor is started using supervisor:start_link/[2,3],
+%% this function is called by the new process to find out about
+%% restart strategy, maximum restart frequency and child
+%% specifications.
+%% @end
+%%--------------------------------------------------------------------
+-spec init(any()) -> sup_init_ret().
+init([]) ->
+    kz_util:set_startup(),
+    RestartStrategy = 'one_for_one',
+    MaxRestarts = 5,
+    MaxSecondsBetweenRestarts = 10,
+
+    SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+    {'ok', {SupFlags, ?CHILDREN}}.
