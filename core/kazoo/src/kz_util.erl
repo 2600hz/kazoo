19ed0baadb6ec344460b8712ded0f32d78af279e
Pierre Fenoll <pierrefenoll@gmail.com>
2016-05-09 15:27:17 +0200

    split-kz_util: introduce kz_time

--- core/kazoo/src/kz_util.erl
+++ core/kazoo/src/kz_util.erl
@@ -32,10 +32,6 @@
 
 -export([try_load_module/1]).
 
--export([to_date/1
-        ,to_datetime/1
-        ]).
-
 -export([uri_encode/1
          ,uri_decode/1
          ,resolve_uri/2
@@ -45,26 +41,11 @@
 
 -export([uri/2]).
 
--export([pad_month/1]).
 -export([error_to_binary/1]).
 -export([a1hash/3]).
 
--export([current_tstamp/0, current_unix_tstamp/0
-         ,gregorian_seconds_to_unix_seconds/1, unix_seconds_to_gregorian_seconds/1
-         ,unix_timestamp_to_gregorian_seconds/1
-         ,pretty_print_datetime/1
-         ,rfc1036/1, rfc1036/2
-         ,iso8601/1
-         ,pretty_print_elapsed_s/1
-         ,decr_timeout/2
-         ,pretty_print_bytes/1
+-export([pretty_print_bytes/1
          ,bin_usage/0, mem_usage/0
-        ]).
--export([microseconds_to_seconds/1
-         ,milliseconds_to_seconds/1
-         ,elapsed_s/1, elapsed_ms/1, elapsed_us/1
-         ,elapsed_s/2, elapsed_ms/2, elapsed_us/2
-         ,now/0, now_s/1, now_ms/1, now_us/1
         ]).
 
 -export([put_callid/1, get_callid/0
@@ -82,10 +63,6 @@
          ,change_error_log_level/1
          ,change_syslog_log_level/1
         ]).
-
--export([format_date/0, format_date/1]).
--export([format_time/0, format_time/1]).
--export([format_datetime/0, format_datetime/1]).
 
 -export([node_name/0, node_hostname/0]).
 
@@ -274,7 +251,7 @@ format_account_id(AccountId, Year, Month) when not is_integer(Month) ->
 format_account_id(Account, Year, Month) when is_integer(Year),
                                              is_integer(Month) ->
     ?MATCH_ACCOUNT_RAW(A,B,Rest) = raw_account_id(Account),
-    ?MATCH_MODB_SUFFIX_ENCODED(A, B, Rest, kz_term:to_binary(Year), pad_month(Month)).
+    ?MATCH_MODB_SUFFIX_ENCODED(A, B, Rest, kz_term:to_binary(Year), kz_time:pad_month(Month)).
 
 %%--------------------------------------------------------------------
 %% @public
@@ -330,14 +307,6 @@ format_account_modb(AccountId, 'unencoded') ->
 format_account_modb(AccountId, 'encoded') ->
     ?MATCH_ACCOUNT_RAW(A,B,Rest) = raw_account_modb(AccountId),
     kz_term:to_binary(["account%2F", A, "%2F", B, "%2F", Rest]).
-
--spec pad_month(kz_month() | ne_binary()) -> ne_binary().
-pad_month(<<_/binary>> = Month) ->
-    pad_month(kz_term:to_integer(Month));
-pad_month(Month) when Month < 10 ->
-    <<"0", (kz_term:to_binary(Month))/binary>>;
-pad_month(Month) ->
-    kz_term:to_binary(Month).
 
 %%--------------------------------------------------------------------
 %% @public
@@ -639,7 +608,7 @@ spawn_monitor(Fun, Arguments) ->
 
 -spec set_startup() -> api_seconds().
 set_startup() ->
-    put('$startup', current_tstamp()).
+    put('$startup', kz_time:current_tstamp()).
 
 -spec startup() -> api_seconds().
 startup() ->
@@ -771,16 +740,6 @@ safe_urlencode(V) when is_binary(V)
     kz_http_util:urlencode(kz_term:to_binary(V)).
 
 
--spec to_date(binary() | string() | integer()) -> kz_date().
-to_date(X) ->
-    {Date, _ } = to_datetime(X),
-    Date.
-
--spec to_datetime(binary() | string() | integer()) -> kz_datetime().
-to_datetime(X) when is_integer(X) -> calendar:gregorian_seconds_to_datetime(X);
-to_datetime(X) when is_binary(X) -> to_datetime(kz_term:to_integer(X));
-to_datetime(X) when is_list(X) -> to_datetime(kz_term:to_integer(X)).
-
 -spec error_to_binary({'error', binary()} | binary()) -> binary().
 error_to_binary({'error', Reason}) ->
     error_to_binary(Reason);
@@ -795,15 +754,6 @@ error_to_binary(Reason) ->
 a1hash(User, Realm, Password) ->
     kz_term:to_hex(erlang:md5(list_to_binary([User,":",Realm,":",Password]))).
 
-%% returns current seconds
--spec current_tstamp() -> gregorian_seconds().
-current_tstamp() ->
-    calendar:datetime_to_gregorian_seconds(calendar:universal_time()).
-
--spec current_unix_tstamp() -> unix_seconds().
-current_unix_tstamp() ->
-    gregorian_seconds_to_unix_seconds(current_tstamp()).
-
 %% fetch and cache the kazoo version from the VERSION file in kazoo's root folder
 -spec kazoo_version() -> ne_binary().
 kazoo_version() ->
@@ -813,97 +763,6 @@ kazoo_version() ->
 -spec write_pid(ne_binary() | nonempty_string() | iolist()) -> 'ok' | {'error', atom()}.
 write_pid(FileName) ->
     file:write_file(FileName, io_lib:format("~s", [os:getpid()]), ['write', 'binary']).
-
--spec gregorian_seconds_to_unix_seconds(integer() | string() | binary()) -> integer().
-gregorian_seconds_to_unix_seconds(GregorianSeconds) ->
-    kz_term:to_integer(GregorianSeconds) - ?UNIX_EPOCH_IN_GREGORIAN.
-
--spec unix_seconds_to_gregorian_seconds(integer() | string() | binary()) -> integer().
-unix_seconds_to_gregorian_seconds(UnixSeconds) ->
-    kz_term:to_integer(UnixSeconds) + ?UNIX_EPOCH_IN_GREGORIAN.
-
--spec unix_timestamp_to_gregorian_seconds(integer() | string() | binary()) -> integer().
-unix_timestamp_to_gregorian_seconds(UnixTimestamp) ->
-    ?UNIX_EPOCH_IN_GREGORIAN + (kz_term:to_integer(UnixTimestamp) div 1000).
-
--spec pretty_print_datetime(kz_datetime() | integer()) -> ne_binary().
-pretty_print_datetime(Timestamp) when is_integer(Timestamp) ->
-    pretty_print_datetime(calendar:gregorian_seconds_to_datetime(Timestamp));
-pretty_print_datetime({{Y,Mo,D},{H,Mi,S}}) ->
-    iolist_to_binary(io_lib:format("~4..0w-~2..0w-~2..0w_~2..0w-~2..0w-~2..0w"
-                                   ,[Y, Mo, D, H, Mi, S]
-                                  )).
-
--spec rfc1036(calendar:datetime() | gregorian_seconds()) -> ne_binary().
--spec rfc1036(calendar:datetime() | gregorian_seconds(), ne_binary()) -> ne_binary().
-rfc1036(DateTime) ->
-    rfc1036(DateTime, <<"GMT">>).
-
-rfc1036({Date = {Y, Mo, D}, {H, Mi, S}}, TZ) ->
-    Wday = calendar:day_of_the_week(Date),
-    <<(weekday(Wday))/binary, ", ",
-      (kz_term:pad_binary_left(kz_term:to_binary(D), 2, <<"0">>))/binary, " ",
-      (month(Mo))/binary, " ",
-      (kz_term:to_binary(Y))/binary, " ",
-      (kz_term:pad_binary_left(kz_term:to_binary(H), 2, <<"0">>))/binary, ":",
-      (kz_term:pad_binary_left(kz_term:to_binary(Mi), 2, <<"0">>))/binary, ":",
-      (kz_term:pad_binary_left(kz_term:to_binary(S), 2, <<"0">>))/binary,
-      " ", TZ/binary
-    >>;
-rfc1036(Timestamp, TZ) when is_integer(Timestamp) ->
-    rfc1036(calendar:gregorian_seconds_to_datetime(Timestamp), TZ).
-
--spec iso8601(calendar:datetime() | gregorian_seconds()) -> ne_binary().
-iso8601({{Y,M,D},_}) ->
-    <<(kz_term:to_binary(Y))/binary, "-"
-      ,(kz_term:pad_binary_left(kz_term:to_binary(M), 2, <<"0">>))/binary, "-"
-      ,(kz_term:pad_binary_left(kz_term:to_binary(D), 2, <<"0">>))/binary
-    >>;
-iso8601(Timestamp) when is_integer(Timestamp) ->
-    iso8601(calendar:gregorian_seconds_to_datetime(Timestamp)).
-
-%% borrowed from cow_date.erl
--spec weekday(1..7) -> <<_:24>>.
-weekday(1) -> <<"Mon">>;
-weekday(2) -> <<"Tue">>;
-weekday(3) -> <<"Wed">>;
-weekday(4) -> <<"Thu">>;
-weekday(5) -> <<"Fri">>;
-weekday(6) -> <<"Sat">>;
-weekday(7) -> <<"Sun">>.
-
--spec month(1..12) -> <<_:24>>.
-month( 1) -> <<"Jan">>;
-month( 2) -> <<"Feb">>;
-month( 3) -> <<"Mar">>;
-month( 4) -> <<"Apr">>;
-month( 5) -> <<"May">>;
-month( 6) -> <<"Jun">>;
-month( 7) -> <<"Jul">>;
-month( 8) -> <<"Aug">>;
-month( 9) -> <<"Sep">>;
-month(10) -> <<"Oct">>;
-month(11) -> <<"Nov">>;
-month(12) -> <<"Dec">>.
-
--spec pretty_print_elapsed_s(non_neg_integer()) -> ne_binary().
-pretty_print_elapsed_s(0) -> <<"0s">>;
-pretty_print_elapsed_s(Seconds) ->
-    iolist_to_binary(unitfy_seconds(Seconds)).
-
--spec unitfy_seconds(non_neg_integer()) -> iolist().
-unitfy_seconds(0) -> "";
-unitfy_seconds(Seconds) when Seconds < ?SECONDS_IN_MINUTE ->
-    [kz_term:to_binary(Seconds), "s"];
-unitfy_seconds(Seconds) when Seconds < ?SECONDS_IN_HOUR ->
-    M = Seconds div ?SECONDS_IN_MINUTE,
-    [kz_term:to_binary(M), "m", unitfy_seconds(Seconds - (M * ?SECONDS_IN_MINUTE))];
-unitfy_seconds(Seconds) when Seconds < ?SECONDS_IN_DAY ->
-    H = Seconds div ?SECONDS_IN_HOUR,
-    [kz_term:to_binary(H), "h", unitfy_seconds(Seconds - (H * ?SECONDS_IN_HOUR))];
-unitfy_seconds(Seconds) ->
-    D = Seconds div ?SECONDS_IN_DAY,
-    [kz_term:to_binary(D), "d", unitfy_seconds(Seconds - (D * ?SECONDS_IN_DAY))].
 
 -spec pretty_print_bytes(non_neg_integer()) -> ne_binary().
 pretty_print_bytes(0) -> <<"0B">>;
@@ -926,91 +785,6 @@ unitfy_bytes(Bytes) when Bytes < ?BYTES_T ->
 unitfy_bytes(Bytes) ->
     T = Bytes div ?BYTES_T,
     [kz_term:to_binary(T), "T", unitfy_bytes(Bytes rem ?BYTES_T)].
-
--spec decr_timeout(kz_timeout(), non_neg_integer() | kz_now()) -> kz_timeout().
-decr_timeout('infinity', _) -> 'infinity';
-decr_timeout(Timeout, Elapsed) when is_integer(Elapsed) ->
-    Diff = Timeout - Elapsed,
-    case Diff < 0 of
-        'true' -> 0;
-        'false' -> Diff
-    end;
-decr_timeout(Timeout, Start) ->
-    decr_timeout(Timeout, ?MODULE:elapsed_ms(Start)).
-
--spec microseconds_to_seconds(float() | integer() | string() | binary()) -> non_neg_integer().
-microseconds_to_seconds(Microseconds) -> kz_term:to_integer(Microseconds) div 1000000.
-milliseconds_to_seconds(Milliseconds) -> kz_term:to_integer(Milliseconds) div ?MILLISECONDS_IN_SECOND.
-
--spec elapsed_s(kz_now() | pos_integer()) -> pos_integer().
--spec elapsed_ms(kz_now() | pos_integer()) -> pos_integer().
--spec elapsed_us(kz_now() | pos_integer()) -> pos_integer().
-elapsed_s({_,_,_}=Start) -> elapsed_s(Start, os:timestamp());
-elapsed_s(Start) when is_integer(Start) -> elapsed_s(Start, current_tstamp()).
-
-elapsed_ms({_,_,_}=Start) -> elapsed_ms(Start, os:timestamp());
-elapsed_ms(Start) when is_integer(Start) -> elapsed_ms(Start, current_tstamp()).
-
-elapsed_us({_,_,_}=Start) -> elapsed_us(Start, os:timestamp());
-elapsed_us(Start) when is_integer(Start) -> elapsed_us(Start, current_tstamp()).
-
--spec elapsed_s(kz_now() | pos_integer(), kz_now() | pos_integer()) -> pos_integer().
--spec elapsed_ms(kz_now() | pos_integer(), kz_now() | pos_integer()) -> pos_integer().
--spec elapsed_us(kz_now() | pos_integer(), kz_now() | pos_integer()) -> pos_integer().
-elapsed_s({_,_,_}=Start, {_,_,_}=Now) -> timer:now_diff(Now, Start) div 1000000;
-elapsed_s({_,_,_}=Start, Now) -> elapsed_s(now_s(Start), Now);
-elapsed_s(Start, {_,_,_}=Now) -> elapsed_s(Start, now_s(Now));
-elapsed_s(Start, Now) when is_integer(Start), is_integer(Now) -> Now - Start.
-
-elapsed_ms({_,_,_}=Start, {_,_,_}=Now) -> timer:now_diff(Now, Start) div ?MILLISECONDS_IN_SECOND;
-elapsed_ms({_,_,_}=Start, Now) -> elapsed_ms(now_s(Start), Now);
-elapsed_ms(Start, {_,_,_}=Now) -> elapsed_ms(Start, now_s(Now));
-elapsed_ms(Start, Now)
-  when is_integer(Start),
-       is_integer(Now) ->
-    (Now - Start) * ?MILLISECONDS_IN_SECOND.
-
-elapsed_us({_,_,_}=Start, {_,_,_}=Now) -> timer:now_diff(Now, Start);
-elapsed_us({_,_,_}=Start, Now) -> elapsed_us(now_s(Start), Now);
-elapsed_us(Start, {_,_,_}=Now) -> elapsed_us(Start, now_s(Now));
-elapsed_us(Start, Now) when is_integer(Start), is_integer(Now) -> (Now - Start) * 1000000.
-
--spec now() -> kz_now().
-now() -> erlang:timestamp().
-
--spec now_s(any()) -> gregorian_seconds().
--spec now_ms(any()) -> pos_integer().
--spec now_us(any()) -> pos_integer().
-now_s(_) ->  erlang:system_time('seconds').
-now_ms(_) -> erlang:system_time('milli_seconds').
-now_us(_) -> erlang:system_time('micro_seconds').
-
--spec format_date() -> binary().
--spec format_date(gregorian_seconds()) -> binary().
--spec format_time() -> binary().
--spec format_time(gregorian_seconds()) -> binary().
--spec format_datetime() -> binary().
--spec format_datetime(gregorian_seconds()) -> binary().
-
-format_date() ->
-    format_date(current_tstamp()).
-
-format_date(Timestamp) ->
-    {{Y,M,D}, _ } = calendar:gregorian_seconds_to_datetime(Timestamp),
-    list_to_binary([kz_term:to_binary(Y), "-", kz_term:to_binary(M), "-", kz_term:to_binary(D)]).
-
-format_time() ->
-    format_time(current_tstamp()).
-
-format_time(Timestamp) ->
-    { _, {H,I,S}} = calendar:gregorian_seconds_to_datetime(Timestamp),
-    list_to_binary([kz_term:to_binary(H), ":", kz_term:to_binary(I), ":", kz_term:to_binary(S)]).
-
-format_datetime() ->
-    format_datetime(current_tstamp()).
-
-format_datetime(Timestamp) ->
-    list_to_binary([format_date(Timestamp), " ", format_time(Timestamp)]).
 
 -spec bin_usage() -> integer().
 bin_usage() ->
--- scripts/wh_to_kz.sh
+++ scripts/wh_to_kz.sh
@@ -30,3 +30,7 @@ function util_split() {
 
 kz_term_exports='shuffle_list to_integer to_float to_number to_hex to_hex_binary rand_hex_binary hexencode_binary from_hex_binary from_hex_string to_list to_binary to_atom to_boolean is_boolean is_true is_false is_empty is_not_empty is_proplist identity always_true always_false to_lower_binary to_upper_binary to_lower_string to_upper_string ucfirst_binary lcfirst_binary strip_binary strip_left_binary strip_right_binary suffix_binary truncate_binary truncate_left_binary truncate_right_binary clean_binary remove_white_spaces binary_md5 pad_binary pad_binary_left join_binary floor ceiling'
 util_split 'kz_term' $kz_term_exports
+
+
+kz_time_exports='to_date to_datetime pad_month current_tstamp current_unix_tstamp gregorian_seconds_to_unix_seconds unix_seconds_to_gregorian_seconds unix_timestamp_to_gregorian_seconds pretty_print_datetime rfc1036 rfc1036 iso8601 pretty_print_elapsed_s decr_timeout microseconds_to_seconds milliseconds_to_seconds elapsed_s elapsed_ms elapsed_us elapsed_s elapsed_ms elapsed_us now now_s now_ms now_us format_date format_date format_time format_time format_datetime format_datetime'
+util_split 'kz_time' $kz_time_exports
