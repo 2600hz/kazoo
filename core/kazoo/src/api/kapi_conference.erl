a98296033a147c415bde0463df814f7a5e1825a3
Roman Galeev <roman@jamhed.tk>
2016-06-08 22:32:59 +0200

    KAZOO-4462 participant events
    * generate amqp events and send them to` conference.event.$conf_id.$call_id

--- core/kazoo/src/api/kapi_conference.erl
+++ core/kazoo/src/api/kapi_conference.erl
@@ -34,7 +34,7 @@
 -export([unmute_participant/1, unmute_participant_v/1]).
 -export([participant_volume_in/1, participant_volume_in_v/1]).
 -export([participant_volume_out/1, participant_volume_out_v/1]).
--export([participants_event/1, participants_event_v/1]).
+-export([participant_event/1, participant_event_v/1]).
 -export([conference_error/1, conference_error_v/1]).
 -export([config_req/1, config_req_v/1
          ,config_resp/1, config_resp_v/1
@@ -67,7 +67,7 @@
 -export([publish_participant_volume_in/2, publish_participant_volume_in/3]).
 -export([publish_participant_volume_out/2, publish_participant_volume_out/3]).
 -export([publish_error/2, publish_error/3]).
--export([publish_participants_event/2, publish_participants_event/3]).
+-export([publish_participant_event/3, publish_participant_event/4]).
 -export([publish_command/2, publish_command/3]).
 -export([publish_targeted_command/2, publish_targeted_command/3]).
 -export([publish_config_req/1, publish_config_req/2
@@ -305,13 +305,28 @@
                                       ]).
 
 %% Conference Participants Event
--define(PARTICIPANTS_EVENT_HEADERS, [<<"Conference-ID">>, <<"Focus">>]).
--define(OPTIONAL_PARTICIPANTS_EVENT_HEADERS, [<<"Participants">>, <<"Switch-Hostname">>
-                                              ,<<"Switch-URL">>, <<"Switch-External-IP">>]).
--define(PARTICIPANTS_EVENT_VALUES, [{<<"Event-Category">>, <<"conference">>}
-                                    ,{<<"Event-Name">>, <<"participants_event">>}
-                                   ]).
--define(PARTICIPANTS_EVENT_TYPES, [{<<"Conference-ID">>, fun is_binary/1}]).
+-define(PARTICIPANT_EVENT_HEADERS, [<<"Event">>
+     ,<<"Call-ID">>
+     ,<<"Focus">>
+     ,<<"Conference-ID">>
+     ,<<"Instance-ID">>
+     ,<<"Participant-ID">>
+     ,<<"Floor">>
+     ,<<"Hear">>
+     ,<<"Speak">>
+     ,<<"Talking">>
+     ,<<"Current-Energy">>
+     ,<<"Energy-Level">>
+     ,<<"Video">>
+     ,<<"Mute-Detect">>
+     ,<<"Caller-ID-Name">>
+     ,<<"Caller-ID-Number">>
+     ,<<"Channel-Presence-ID">>
+     ,<<"Custom-Channel-Vars">>
+        ]).
+-define(OPTIONAL_PARTICIPANT_EVENT_HEADERS, []).
+-define(PARTICIPANT_EVENT_VALUES, [{<<"Event-Category">>, <<"conference">>}, {<<"Event-Name">>, <<"participant_event">>}]).
+-define(PARTICIPANT_EVENT_TYPES, []).
 
 %% Conference Error
 -define(CONFERENCE_ERROR_HEADERS, [<<"Error-Message">>, <<"Request">>]).
@@ -842,18 +857,18 @@ participant_volume_out_v(JObj) -> participant_volume_out_v(kz_json:to_proplist(J
 %% Takes proplist, creates JSON string or error
 %% @end
 %%--------------------------------------------------------------------
--spec participants_event(api_terms()) -> {'ok', iolist()} | {'error', string()}.
-participants_event(Prop) when is_list(Prop) ->
-    case participants_event_v(Prop) of
-        'true' -> kz_api:build_message(Prop, ?PARTICIPANTS_EVENT_HEADERS, ?OPTIONAL_PARTICIPANTS_EVENT_HEADERS);
-        'false' -> {'error', "Proplist failed validation for participants_event response"}
+-spec participant_event(api_terms()) -> {'ok', iolist()} | {'error', string()}.
+participant_event(Prop) when is_list(Prop) ->
+    case participant_event_v(Prop) of
+        'true' -> kz_api:build_message(Prop, ?PARTICIPANT_EVENT_HEADERS, ?OPTIONAL_PARTICIPANT_EVENT_HEADERS);
+        'false' -> {'error', "Proplist failed validation for participant_event response"}
     end;
-participants_event(JObj) -> participants_event(kz_json:to_proplist(JObj)).
+participant_event(JObj) -> participant_event(kz_json:to_proplist(JObj)).
 
--spec participants_event_v(api_terms()) -> boolean().
-participants_event_v(Prop) when is_list(Prop) ->
-    kz_api:validate(Prop, ?PARTICIPANTS_EVENT_HEADERS, ?PARTICIPANTS_EVENT_VALUES, ?PARTICIPANTS_EVENT_TYPES);
-participants_event_v(JObj) -> participants_event_v(kz_json:to_proplist(JObj)).
+-spec participant_event_v(api_terms()) -> boolean().
+participant_event_v(Prop) when is_list(Prop) ->
+    kz_api:validate(Prop, ?PARTICIPANT_EVENT_HEADERS, ?PARTICIPANT_EVENT_VALUES, ?PARTICIPANT_EVENT_TYPES);
+participant_event_v(JObj) -> participant_event_v(kz_json:to_proplist(JObj)).
 
 %%--------------------------------------------------------------------
 %% @doc
@@ -934,6 +949,9 @@ bind_to_q(Q, ['event'|T], Props) ->
 bind_to_q(Q, ['config'|T], Props) ->
     Profile = props:get_value('profile', Props, <<"*">>),
     'ok' = amqp_util:bind_q_to_conference(Q, 'config', Profile),
+    bind_to_q(Q, T, Props);
+bind_to_q(Q, [{'conference', {ConfId,CallId}}|T], Props) ->
+    'ok' = amqp_util:bind_q_to_conference(Q, 'event', ConfId, CallId),
     bind_to_q(Q, T, Props);
 bind_to_q(Q, [{'conference', ConfId}|T], Props) ->
     'ok' = amqp_util:bind_q_to_conference(Q, 'event', ConfId),
@@ -1271,13 +1289,13 @@ publish_participant_volume_out(ConferenceId, Req, ContentType) ->
 %% Publish to the conference exchange
 %% @end
 %%--------------------------------------------------------------------
--spec publish_participants_event(ne_binary(), api_terms()) -> 'ok'.
--spec publish_participants_event(ne_binary(), api_terms(), ne_binary()) -> 'ok'.
-publish_participants_event(ConferenceId, JObj) ->
-    publish_participants_event(ConferenceId, JObj, ?DEFAULT_CONTENT_TYPE).
-publish_participants_event(ConferenceId, Event, ContentType) ->
-    {'ok', Payload} = kz_api:prepare_api_payload(Event, ?PARTICIPANTS_EVENT_VALUES, fun ?MODULE:participants_event/1),
-    amqp_util:conference_publish(Payload, 'event', ConferenceId, [], ContentType).
+-spec publish_participant_event(ne_binary(), ne_binary(), api_terms()) -> 'ok'.
+-spec publish_participant_event(ne_binary(), ne_binary(), api_terms(), ne_binary()) -> 'ok'.
+publish_participant_event(ConferenceId, CallId, JObj) ->
+    publish_participant_event(ConferenceId, CallId, JObj, ?DEFAULT_CONTENT_TYPE).
+publish_participant_event(ConferenceId, CallId, Event, ContentType) ->
+    {'ok', Payload} = kz_api:prepare_api_payload(Event, ?PARTICIPANT_EVENT_VALUES, fun ?MODULE:participant_event/1),
+    amqp_util:conference_publish(Payload, 'event', ConferenceId, CallId, [], ContentType).
 
 %%--------------------------------------------------------------------
 %% @doc
