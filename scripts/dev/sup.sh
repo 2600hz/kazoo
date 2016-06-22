c0cc6cf84122e2a37fce1c86dacb6842bb13b055
Roman Galeev <roman@jamhed.tk>
2016-06-22 15:04:26 +0200

    sup fix: return code, allow to pass timeout in command-line

--- scripts/dev/sup.sh
+++ scripts/dev/sup.sh
@@ -6,11 +6,8 @@ IFS='@' read -r -a NH <<< "$KAZOO_NODE"
 NAME="${NH[0]}"
 HOST="${NH[1]}"
 
-# request can execute longer than 5 seconds, so timeout 10 is required
-
 RELX_REPLACE_OS_VARS=true KZname="-name $KAZOO_NODE" \
-	_rel/kazoo/bin/kazoo escript lib/sup-*/priv/sup.escript \
+	exec _rel/kazoo/bin/kazoo escript lib/sup-*/priv/sup.escript \
 	-n $NAME \
 	-h $HOST \
-	-c change_me \
-	-t 10 "$*"
+	"$*"
