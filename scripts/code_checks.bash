bf9b9b940a0b2729663c4b9682116c2abc69f475
James Aimonetti <james.aimonetti@gmail.com>
2016-07-10 05:18:58 +0000

    KAZOO-4786: check for missing callflow schema docs

--- scripts/code_checks.bash
+++ scripts/code_checks.bash
@@ -10,10 +10,41 @@ function check_andalso_orelse {
     BOOLS=$(grep -Elr '[a-zA-Z\)] (andalso|orelse)' $ROOT/{applications,core})
 
     if [ ${#BOOLS[@]} -ge 0 ]; then
+        echo "check for andalso/orelse formatting issues in:"
         grep -Elr '[a-zA-Z\)] (andalso|orelse)' $ROOT/{applications,core}
     fi
 }
 
+function create_stub_schema {
+    CF=$1
+    SCHEMA=$2
+
+    SKEL="$ROOT/applications/crossbar/priv/couchdb/schemas/callflows.skel.json"
+    $(cp -a $SKEL $SCHEMA)
+    $(sed -i "s/skel/$CF/g" $SCHEMA)
+    echo "  created stub for $SCHEMA"
+}
+
+function check_callflow_schema {
+    CF=$(basename $1)
+    CF=${CF#cf_}
+    CF=${CF%.erl}
+    SCHEMA="$ROOT/applications/crossbar/priv/couchdb/schemas/callflows.$CF.json"
+
+    if [ ! -f $SCHEMA ]; then
+        create_stub_schema $CF $SCHEMA
+    fi
+}
+
+function check_callflow_schemas {
+    CALLFLOWS=$(ls $ROOT/applications/callflow/src/module/*.erl | grep -v skel)
+    for CALLFLOW in $CALLFLOWS; do
+        check_callflow_schema $CALLFLOW
+    done
+}
+
 check_andalso_orelse
+
+check_callflow_schemas
 
 popd > /dev/null
