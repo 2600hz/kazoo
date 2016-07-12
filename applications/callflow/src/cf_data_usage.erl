3b9e890ece2c82a17dd0434936a2fa480895b4a1
James Aimonetti <james.aimonetti@gmail.com>
2016-07-12 21:23:32 +0000

    KAZOO-4786: only ignore kz_json

--- applications/callflow/src/cf_data_usage.erl
+++ applications/callflow/src/cf_data_usage.erl
@@ -231,12 +231,12 @@ process_mfa(#usage{data_var_name=DataName
              };
 
 process_mfa(#usage{data_var_name=DataName}=Acc
-           ,_M, _F, [?VAR(_Name), ?VAR(DataName)]=_Args
+           ,'kz_json'=_M, _F, [?VAR(_Name), ?VAR(DataName)]=_Args
            ) ->
     ?DEBUG("  skipping usage ~p:~p(~p)~n", [_M, _F, _Args]),
     Acc;
 process_mfa(#usage{data_var_name=DataName}=Acc
-           ,_M, _F, [?VAR(_Name), ?VAR(DataName), _Default]=_Args
+           ,'kz_json'=_M, _F, [?VAR(_Name), ?VAR(DataName), _Default]=_Args
            ) ->
     ?DEBUG("  skipping usage ~p:~p(~p)~n", [_M, _F, _Args]),
     Acc;
