360c56af79bf8da24f801737714ad03b10333443
Roman Galeev <roman@jamhed.tk>
2016-07-20 12:27:18 +0200

    KAZOO-4890: crossbar returns parked_calls document

---
+++ applications/crossbar/src/modules/cb_parked_calls.erl
@@ -0,0 +1,34 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2011-2016, 2600Hz INC
+%%% @contributors:
+%%%   Roman Galeev
+%%%-------------------------------------------------------------------
+-module(cb_parked_calls).
+
+-include("crossbar.hrl").
+
+-export([init/0
+        ,allowed_methods/0
+        ,resource_exists/0
+        ,validate/1
+        ]).
+
+-define(MOD_CONFIG_CAT, <<"callflow.park">>).
+-define(DB_DOC_NAME, kapps_config:get(?MOD_CONFIG_CAT, <<"db_doc_name">>, <<"parked_calls">>)).
+
+-spec init() -> 'ok'.
+init() ->
+    _ = crossbar_bindings:bind(<<"*.allowed_methods.parked_calls">>, ?MODULE, 'allowed_methods'),
+    _ = crossbar_bindings:bind(<<"*.resource_exists.parked_calls">>, ?MODULE, 'resource_exists'),
+    crossbar_bindings:bind(<<"*.validate.parked_calls">>, ?MODULE, 'validate').
+
+-spec allowed_methods() -> http_methods().
+allowed_methods() -> [?HTTP_GET].
+
+-spec resource_exists() -> 'true'.
+resource_exists() -> 'true'.
+
+-spec validate(cb_context:context()) -> cb_context:context().
+validate(Context) ->
+    Ctx1 = crossbar_doc:load(?DB_DOC_NAME, Context, [{'expected_type', ?DB_DOC_NAME}]),
+    cb_context:set_resp_data(Ctx1, kz_json:public_fields(kz_json:normalize_jobj(cb_context:doc(Ctx1)))).
