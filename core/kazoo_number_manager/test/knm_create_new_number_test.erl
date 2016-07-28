c586c545f6f87fc86e125d6342345fb11bc37e65
Pierre Fenoll <pierrefenoll@gmail.com>
2016-07-28 18:53:45 +0200

    knm-add-test: test new available number creation (#2365)

--- core/kazoo_number_manager/test/knm_create_new_number_test.erl
+++ core/kazoo_number_manager/test/knm_create_new_number_test.erl
@@ -41,6 +41,38 @@ create_new_number_test_() ->
      }
     ].
 
+create_new_available_number_test_() ->
+    Props = [{'auth_by', ?MASTER_ACCOUNT_ID}
+            ,{'assign_to', ?RESELLER_ACCOUNT_ID}
+            ,{'dry_run', 'false'}
+            ,{<<"auth_by_account">>
+             ,kz_account:set_allow_number_additions(?RESELLER_ACCOUNT_DOC, 'true')
+             }
+            ,{'state', ?NUMBER_STATE_AVAILABLE}
+            ],
+    {'ok', N} = knm_number:create(?TEST_CREATE_NUM, Props),
+    PN = knm_number:phone_number(N),
+
+    [{"Verify phone number is assigned to reseller account"
+     ,?_assertEqual(?RESELLER_ACCOUNT_ID, knm_phone_number:assigned_to(PN))
+     }
+    ,{"Verify new phone number was authorized by master account"
+     ,?_assertEqual(?MASTER_ACCOUNT_ID, knm_phone_number:auth_by(PN))
+     }
+    ,{"Verify new phone number database is properly set"
+     ,?_assertEqual(<<"numbers%2F%2B1555">>, knm_phone_number:number_db(PN))
+     }
+    ,{"Verify new phone number is in RESERVED state"
+     ,?_assertEqual(?NUMBER_STATE_AVAILABLE, knm_phone_number:state(PN))
+     }
+    ,{"Verify the reseller account is listed in reserve history"
+     ,?_assertEqual([?RESELLER_ACCOUNT_ID], knm_phone_number:reserve_history(PN))
+     }
+    ,{"Verify the local carrier module is being used"
+     ,?_assertEqual(?CARRIER_LOCAL, knm_phone_number:module_name(PN))
+     }
+    ].
+
 create_existing_number_test_() ->
     Props = [{'auth_by', ?MASTER_ACCOUNT_ID}
             ,{'assign_to', ?RESELLER_ACCOUNT_ID}
