48764adb7c92e1146889a78d8fae06c012609639
lazedo <luis.azedo@factorlusitano.com>
2016-07-29 11:01:26 +0100

    qf-knm_local revert previous changes and simplify

--- core/kazoo_number_manager/src/carriers/knm_local.erl
+++ core/kazoo_number_manager/src/carriers/knm_local.erl
@@ -45,15 +45,7 @@ is_local() -> 'true'.
 find_numbers(Number, Quantity, Options) ->
     case props:get_value(?KNM_ACCOUNTID_CARRIER, Options) of
         'undefined' -> {'error', 'not_available'};
-        AccountId ->
-            ResellerId = props:get_value(?KNM_RESELLERID_CARRIER, Options),
-            case ResellerId == AccountId of
-                'false' -> {'error', 'not_available'};
-                'true' ->
-                    do_find_numbers(Number, Quantity, ResellerId)
-                    %% TODO: given the requestor's account, discover knm_local numbers
-                    %%        that are available but managed by accendants of the account.
-            end
+        AccountId -> do_find_numbers(Number, Quantity, AccountId)
     end.
 
 -spec do_find_numbers(ne_binary(), pos_integer(), ne_binary()) ->
@@ -61,12 +53,14 @@ find_numbers(Number, Quantity, Options) ->
                              {'error', any()}.
 do_find_numbers(<<"+",_/binary>>=Number, Quantity, AccountId)
   when is_integer(Quantity), Quantity > 0 ->
-    DB = kz_util:format_account_db(AccountId),
     ViewOptions = [{'startkey', [?NUMBER_STATE_AVAILABLE, Number]}
                   ,{'endkey', [?NUMBER_STATE_AVAILABLE, <<"\ufff0">>]}
                   ,{'limit', Quantity}
                   ],
-    case kz_datamgr:get_results(DB, <<"numbers_local/status">>, ViewOptions) of
+    case
+        'undefined' /= (DB = knm_converters:to_db(Number))
+        andalso kz_datamgr:get_results(DB, <<"numbers_local/status">>, ViewOptions)
+    of
         'false' -> {'error', 'not_available'};
         {'ok', []} ->
             lager:debug("found no available local numbers for account ~s", [AccountId]),
