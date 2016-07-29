fd9617d4071ca5338777a51c0b12b70bac1f9d90
Roman Galeev <jamhed@2600hz.com>
2016-07-29 15:36:33 +0200

    check for account_id removed

--- applications/blackhole/src/modules/bh_fax.erl
+++ applications/blackhole/src/modules/bh_fax.erl
@@ -18,19 +18,10 @@
 handle_event(Context, EventJObj) ->
     kz_util:put_callid(EventJObj),
     lager:debug("handle_event fired for ~s ~s", [bh_context:account_id(Context), bh_context:websocket_session_id(Context)]),
-    'true' = kapi_fax:status_v(EventJObj)
-        andalso is_account_event(Context, EventJObj),
+    'true' = kapi_fax:status_v(EventJObj),
     lager:debug("valid event and emitting to ~p: ~s", [bh_context:websocket_pid(Context), event_name(EventJObj)]),
     J = kz_json:normalize_jobj(EventJObj),
     blackhole_data_emitter:emit(bh_context:websocket_pid(Context), event_name(EventJObj), J).
-
--spec is_account_event(bh_context:context(), kz_json:object()) -> any().
-is_account_event(Context, EventJObj) ->
-    kz_json:get_first_defined([<<"Account-ID">>
-                              ,[<<"Custom-Channel-Vars">>, <<"Account-ID">>]
-                              ], EventJObj
-                             )
-        =:= bh_context:account_id(Context).
 
 -spec event_name(kz_json:object()) -> ne_binary().
 event_name(_JObj) -> <<"fax.status">>.
