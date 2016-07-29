b214b3bd8cd0bd1140df75c69fa6bf31ce0e8a3a
Roman Galeev <jamhed@2600hz.com>
2016-07-29 12:17:31 +0200

    KAZOO-4201: adapt bh_call to ws protocol

--- applications/blackhole/src/modules/bh_call.erl
+++ applications/blackhole/src/modules/bh_call.erl
@@ -22,13 +22,14 @@
                    ]).
 
 -spec handle_event(bh_context:context(), kz_json:object()) -> 'ok'.
-handle_event(Context, EventJObj) ->
+handle_event(#bh_context{binding=Binding} = Context, EventJObj) ->
     kz_util:put_callid(EventJObj),
     lager:debug("handle_event fired for ~s ~s", [bh_context:account_id(Context), bh_context:websocket_session_id(Context)]),
     'true' = kapi_call:event_v(EventJObj)
         andalso is_account_event(Context, EventJObj),
     lager:debug("valid event and emitting to ~p: ~s", [bh_context:websocket_pid(Context), event_name(EventJObj)]),
-    blackhole_data_emitter:emit(bh_context:websocket_pid(Context), event_name(EventJObj), EventJObj).
+    NormJObj = kz_json:normalize_jobj(kz_json:set_value(<<"Binding">>, Binding, EventJObj)),
+    blackhole_data_emitter:emit(bh_context:websocket_pid(Context), event_name(EventJObj), NormJObj).
 
 is_account_event(Context, EventJObj) ->
     kz_json:get_first_defined([<<"Account-ID">>
@@ -44,13 +45,13 @@ event_name(JObj) ->
 -spec subscribe(bh_context:context(), ne_binary()) -> 'ok'.
 subscribe(Context, <<"call.*.*">>) ->
     AccountId = bh_context:account_id(Context),
-    add_call_binding(AccountId, ?LISTEN_TO),
+    add_call_binding(AccountId, Context, ?LISTEN_TO),
     {'ok', Context};
 subscribe(Context, <<"call.", Binding/binary>>) ->
     case binary:split(Binding, <<".">>, ['global']) of
         [Event, <<"*">>] ->
             AccountId = bh_context:account_id(Context),
-            add_call_binding(AccountId, [Event]);
+            add_call_binding(AccountId, Context, [Event]);
         _ ->
             blackhole_util:send_error_message(Context, <<"unmatched binding">>, Binding)
     end,
@@ -62,13 +63,13 @@ subscribe(Context, Binding) ->
 -spec unsubscribe(bh_context:context(), ne_binary()) -> 'ok'.
 unsubscribe(Context, <<"call.*.*">>) ->
     AccountId = bh_context:account_id(Context),
-    rm_call_binding(AccountId, ?LISTEN_TO),
+    rm_call_binding(AccountId, Context, ?LISTEN_TO),
     {'ok', Context};
 unsubscribe(Context, <<"call.", Binding/binary>>) ->
     case binary:split(Binding, <<".">>, ['global']) of
         [Event, <<"*">>] ->
             AccountId = bh_context:account_id(Context),
-            rm_call_binding(AccountId, [Event]);
+            rm_call_binding(AccountId, Context, [Event]);
         _ ->
             blackhole_util:send_error_message(Context, <<"unmatched binding">>, Binding)
     end,
@@ -77,14 +78,16 @@ unsubscribe(Context, Binding) ->
     blackhole_util:send_error_message(Context, <<"unmatched binding">>, Binding),
     {'ok', Context}.
 
--spec add_call_binding(ne_binary(), [ne_binary()]) -> ok.
-add_call_binding(_AccountId, []) -> ok;
-add_call_binding(AccountId, [Event | Events]) ->
+-spec add_call_binding(ne_binary(), bh_context:context(), [ne_binary()]) -> ok.
+add_call_binding(_AccountId, _Context, []) -> ok;
+add_call_binding(AccountId, Context, [Event | Events]) ->
+    blackhole_bindings:bind(<<"call.", Event/binary, ".*">>, ?MODULE, 'handle_event', Context),
     blackhole_listener:add_call_binding(AccountId, Event),
-    add_call_binding(AccountId, Events).
+    add_call_binding(AccountId, Context, Events).
 
--spec rm_call_binding(ne_binary(), [ne_binary()]) -> ok.
-rm_call_binding(_AccountId, []) -> ok;
-rm_call_binding(AccountId, [Event | Events]) ->
+-spec rm_call_binding(ne_binary(), bh_context:context(), [ne_binary()]) -> ok.
+rm_call_binding(_AccountId, _Context, []) -> ok;
+rm_call_binding(AccountId, Context, [Event | Events]) ->
+    blackhole_bindings:unbind(<<"call.", Event/binary, ".*">>, ?MODULE, 'handle_event', Context),
     blackhole_listener:remove_call_binding(AccountId, Event),
-    rm_call_binding(AccountId, Events).
+    rm_call_binding(AccountId, Context, Events).
