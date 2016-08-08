67fa5f1c83cafff85e6b642d54cccdee8cd89ba6
Pierre Fenoll <pierrefenoll@gmail.com>
2016-08-08 15:38:27 +0200

    check_MODULE: add check_MODULE to scripts/code_checks.bash

--- scripts/code_checks.bash
+++ scripts/code_checks.bash
@@ -7,4 +7,22 @@ function check_andalso_orelse {
     ! grep -Ern '[a-zA-Z\)] +(andalso|orelse)' -- $@
 }
 
+function check_MODULE {
+    # Check for uses of module in lieu of ?MODULE
+    local errors=0
+    for f in "$@"; do
+        m0=$(grep -E module'\(' "$f"  2>/dev/null)
+        [[ $? -ne 0 ]] && continue
+        local err=0
+        m=$(echo $m0 | cut -d'(' -f2 | cut -d')' -f1)
+        grep -nF $m: "$f"
+        [[ $? -ne 1 ]] && ((err++))
+        grep -nF "'$m'" "$f"
+        [[ $? -ne 1 ]] && ((err++))
+        [[ $err -ne 0 ]] && echo "^ $f" && echo && ((errors++))
+    done
+    return $errors
+}
+
 check_andalso_orelse "$@"
+check_MODULE "$@"
