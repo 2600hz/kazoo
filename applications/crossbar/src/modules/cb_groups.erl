ab8ea96026733a8d0498acf654f9ece13d35e2b4
Roman Galeev <jamhed@2600hz.com>
2016-08-09 15:26:39 +0200

    use groups.json schema to validate cb_groups create/update

--- applications/crossbar/src/modules/cb_groups.erl
+++ applications/crossbar/src/modules/cb_groups.erl
@@ -154,7 +154,7 @@ delete(Context, _) ->
 -spec create(cb_context:context()) -> cb_context:context().
 create(Context) ->
     OnSuccess = fun(C) -> on_successful_validation('undefined', C) end,
-    cb_context:validate_request_data(<<"callflows.ring_group">>, Context, OnSuccess).
+    cb_context:validate_request_data(<<"groups">>, Context, OnSuccess).
 
 %%--------------------------------------------------------------------
 %% @private
@@ -176,7 +176,7 @@ read(Id, Context) ->
 -spec update(ne_binary(), cb_context:context()) -> cb_context:context().
 update(Id, Context) ->
     OnSuccess = fun(C) -> on_successful_validation(Id, C) end,
-    cb_context:validate_request_data(<<"callflows.ring_group">>, Context, OnSuccess).
+    cb_context:validate_request_data(<<"groups">>, Context, OnSuccess).
 
 %%--------------------------------------------------------------------
 %% @private
