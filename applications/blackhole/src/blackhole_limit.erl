87ac1f83304f8f92bc9826c66589883337f84e78
Roman Galeev <jamhed@2600hz.com>
2016-08-19 17:56:56 +0200

    limit message rate both for connection and account

--- applications/blackhole/src/blackhole_limit.erl
+++ applications/blackhole/src/blackhole_limit.erl
@@ -10,12 +10,12 @@
 -include("blackhole.hrl").
 -behaviour(gen_server).
 
--export([start_link/0, stop/0, reload/0, ip/1, account/1, message/1, release/1]).
+-export([start_link/0, stop/0, reload/0, ip/1, account/1, conn_rate/1, acc_rate/1, release/1]).
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
 
 -record(state, {buckets :: #{}
-               ,message_bucket_size
-               ,message_bucket_rate
+               ,conn_bs, conn_br
+               ,acc_bs, acc_br
                ,connections_per_source_ip
                ,connections_per_account
                }).
@@ -27,15 +27,20 @@ reload() -> gen_server:cast(?MODULE, {'reload'}).
 release(Context) -> gen_server:call(?MODULE, {'release', Context}).
 ip(Ip) -> die(gen_server:call(?MODULE, {ip, Ip}), 'too_many_connections_per_ip').
 account(Account) -> die(gen_server:call(?MODULE, {'account', Account}), 'too_many_connections_per_account').
-message(Pid) -> die(gen_server:call(?MODULE, {'message', Pid}), 'too_fast').
+conn_rate(Pid) -> die(gen_server:call(?MODULE, {'conn_rate', Pid}), 'connection_too_fast').
+acc_rate(Pid) -> die(gen_server:call(?MODULE, {'acc_rate', Pid}), 'account_too_fast').
 
 fetch_config(#state{}=S) ->
-    BucketSize = kapps_config:get(?BLACKHOLE_CONFIG_CAT, <<"message_bucket_size">>, 100),
-    BucketRate = kapps_config:get(?BLACKHOLE_CONFIG_CAT, <<"message_bucket_rate">>, 10),
+    ConnBs = kapps_config:get(?BLACKHOLE_CONFIG_CAT, <<"connection_bucket_size">>, 100),
+    ConnBr = kapps_config:get(?BLACKHOLE_CONFIG_CAT, <<"connecttion_bucket_rate">>, 10),
+    AccBs = kapps_config:get(?BLACKHOLE_CONFIG_CAT, <<"account_bucket_size">>, 100),
+    AccBr = kapps_config:get(?BLACKHOLE_CONFIG_CAT, <<"account_bucket_rate">>, 10),
     SourceIpLimit = kapps_config:get(?BLACKHOLE_CONFIG_CAT, <<"connections_per_source_ip">>, 100),
     AccountLimit = kapps_config:get(?BLACKHOLE_CONFIG_CAT, <<"connections_per_account">>, 100),
-    S#state{message_bucket_size=BucketSize
-           ,message_bucket_rate=BucketRate
+    S#state{conn_bs=ConnBs
+           ,conn_br=ConnBr
+           ,acc_bs=AccBs
+           ,acc_br=AccBr
            ,connections_per_source_ip=SourceIpLimit
            ,connections_per_account=AccountLimit
            }.
@@ -43,20 +48,16 @@ fetch_config(#state{}=S) ->
 init([]) -> 
     {'ok', fetch_config(#state{ buckets = #{} })}.
 
-handle_call({'message', Pid}, _From, S=#state{buckets=Buckets}) ->
-    Bucket = 
-        case maps:is_key(Pid, Buckets) of
-            'true' ->
-                maps:get(Pid, Buckets);
-            'false' ->
-                {'ok', NewBucket} = kz_token_bucket:start_link(100, 10),
-                NewBucket
-        end,
-    {reply, kz_token_bucket:consume(Bucket, 1), S#state{buckets=Buckets#{ Pid => Bucket }}};
+handle_call({'conn_rate', Key}, _From, S=#state{buckets=Buckets, conn_bs=Bs, conn_br=Br}) ->
+    Bucket = get_bucket(Key, Buckets, Bs, Br),
+    {reply, kz_token_bucket:consume(Bucket, 1), S#state{buckets=Buckets#{ Key => Bucket }}};
+handle_call({'acc_rate', Key}, _From, S=#state{buckets=Buckets, acc_bs=Bs, acc_br=Br}) ->
+    Bucket = get_bucket(Key, Buckets, Bs, Br),
+    {reply, kz_token_bucket:consume(Bucket, 1), S#state{buckets=Buckets#{ Key => Bucket }}};
 handle_call({'release', #bh_context{source=SrcIp, auth_account_id=AuthAccountId, websocket_pid=Pid}}, _From, S=#state{buckets=Buckets}) ->
     blackhole_counters:dec({?APP_NAME, 'source_ip', SrcIp}),
     blackhole_counters:dec({?APP_NAME, 'account', AuthAccountId}),
-    {'reply', 'ok', S#state{buckets=maps:remove(Pid, Buckets)}};
+    {'reply', 'ok', S#state{buckets=maps:remove(Pid, maps:remove(AuthAccountId, Buckets))}};
 handle_call({'ip', Ip}, _From, #state{connections_per_source_ip=Limit} = S) ->
     {'reply', blackhole_counters:inc({?APP_NAME, 'source_ip', Ip}) < Limit, S};
 handle_call({'account', AccountId}, _From, #state{connections_per_source_ip=Limit} = S) ->
@@ -75,3 +76,12 @@ code_change(_OldVsn, S=#state{}, _Extra) -> {'ok', S}.
 
 die('true', _Reason) -> 'true';
 die('false', Reason) -> erlang:error(Reason).
+
+get_bucket(Key, Buckets, Bs, Br) ->
+    case maps:is_key(Key, Buckets) of
+        'true' ->
+            maps:get(Key, Buckets);
+        'false' ->
+            {'ok', NewBucket} = kz_token_bucket:start_link(Bs, Br),
+            NewBucket
+    end.
