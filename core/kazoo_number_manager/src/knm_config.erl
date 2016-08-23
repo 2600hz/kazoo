aa65d5c74e435f68065a69f99698b6a8ad7a912d
Pierre Fenoll <pierrefenoll@gmail.com>
2016-08-23 13:59:24 +0200

    KAZOO-4173: remove superfluous test code

--- core/kazoo_number_manager/src/knm_config.erl
+++ core/kazoo_number_manager/src/knm_config.erl
@@ -12,27 +12,17 @@
 
 -spec should_permanently_delete() -> boolean().
 -spec should_permanently_delete(boolean()) -> boolean().
--ifdef(TEST).
-should_permanently_delete() -> 'false'.
-should_permanently_delete(Default) -> Default.
--else.
 should_permanently_delete() ->
     should_permanently_delete('false').
 should_permanently_delete(Default) ->
     kapps_config:get_is_true(?KNM_CONFIG_CAT, <<"should_permanently_delete">>, Default).
--endif.
 
 -spec released_state() -> ne_binary().
 -spec released_state(ne_binary()) -> ne_binary().
 released_state() ->
     released_state(?NUMBER_STATE_AVAILABLE).
-
--ifdef(TEST).
-released_state(Default) -> Default.
--else.
 released_state(Default) ->
     kapps_config:get_binary(?KNM_CONFIG_CAT, <<"released_state">>, Default).
--endif.
 
 -spec default_locality_url() -> api_binary().
 -spec default_locality_url(api_binary()) -> api_binary().
