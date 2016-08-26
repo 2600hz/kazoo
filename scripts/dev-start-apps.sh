c49e139868df1e7fbede0656ac4537879eaba2e4
Pierre Fenoll <pierrefenoll@gmail.com>
2016-08-26 18:48:15 +0200

    qf-dev: use long name when host is short

--- scripts/dev-start-apps.sh
+++ scripts/dev-start-apps.sh
@@ -8,6 +8,8 @@ export ERL_CRASH_DUMP=$ROOT/$(date +%s)_ecallmgr_erl_crash.dump
 export ERL_LIBS="$ERL_LIBS":$ROOT/deps:$ROOT/core:$ROOT/applications:$(echo $ROOT/deps/rabbitmq_erlang_client-*/deps)
 
 NODE_NAME=${1:-kazoo_apps}
+hostname -f | grep '\.'
+[ $? -eq 0 ] || NODE_NAME="$NODE_NAME@127.0.0.1"
 
 exec erl \
      -name $NODE_NAME \
