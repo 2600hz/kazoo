a7aafa4ae5b3437085b10a6c94686906b3e92400
Pierre Fenoll <pierrefenoll@gmail.com>
2016-08-27 12:28:53 +0200

    qf-swagger: handle success in validate-swagger.sh

--- scripts/validate-swagger.sh
+++ scripts/validate-swagger.sh
@@ -8,10 +8,9 @@ URL='http://online.swagger.io/validator/debug?url=https://raw.githubusercontent.
 
 tmp=$RANDOM.json
 curl -o $tmp "$URL" || exit 2
-"$(dirname "$0")"/format-json.sh $tmp || rm $tmp && exit 3
-echo Swagger file validation errors:
-cat $tmp
-errors=$(wc -l $tmp | cut -d' ' -f1)
+"$(dirname "$0")"/format-json.sh $tmp || (rm $tmp && exit 3)
+errors=$(cat $tmp | python2 -c 'import sys, json; print len(json.load(sys.stdin))')
+[[ $errors -ne 0 ]] && echo Swagger file validation errors: $errors && cat $tmp
 rm $tmp
 
 exit $errors
