c02035da26745aea09650d97c3c7de0651eabdb5
lazedo <luis.azedo@factorlusitano.com>
2016-09-09 15:56:28 +0100

    wip

--- applications/crossbar/src/cb_context.erl
+++ applications/crossbar/src/cb_context.erl
@@ -29,7 +29,7 @@
         ,is_account_admin/1
 
         ,system_error/2
-        
+
          %% Getters / Setters
         ,setters/2
         ,new/0
@@ -943,16 +943,16 @@ maybe_fix_index(Key) ->
 
 -spec system_error_props(context()) -> kz_proplist().
 system_error_props(Context) ->
-    Extract = [{<<"account_id">>, fun cb_context:account_id/1}
-              ,{<<"account_name">>, fun cb_context:account_name/1}
-              ,{<<"auth_account_id">>, fun cb_context:auth_account_id/1}
-              ,{<<"req_headers">>, fun(C) -> kz_json:from_list(req_headers(C)) end}
-              ,{<<"req_json">>, fun cb_context:req_json/1}
-              ,{<<"req_data">>, fun cb_context:req_data/1}
-              ,{<<"query_json">>, fun cb_context:query_string/1}
-              ,{<<"req_id">>, fun cb_context:req_id/1}
+    Extract = [{fun cb_context:account_id/1, <<"account_id">>}
+              ,{fun cb_context:account_name/1, <<"account_name">>}
+              ,{fun cb_context:auth_account_id/1, <<"auth_account_id">>}
+              ,{fun(C) -> kz_json:from_list(req_headers(C)) end, <<"req_headers">>}
+              ,{fun cb_context:req_json/1, <<"req_json">>}
+              ,{fun cb_context:req_data/1, <<"req_data">>}
+              ,{fun cb_context:query_string/1, <<"query_json">>}
+              ,{fun cb_context:req_id/1, <<"req_id">>}
               ],
-    Fun = fun({K, Fun}, KVs) -> [{K, Fun(Context)} | KVs] end,
+    Fun = fun({Fun, K}, KVs) -> [{K, Fun(Context)} | KVs] end,
     Props = lists:foldl(Fun, [], Extract),
     props:filter_undefined(Props).
 
