f92254ba595f447fed3850266469df7725d650ec
Roman Galeev <jamhed@2600hz.com>
2016-09-15 17:42:00 +0200

    KAZOO-4767 handle account and system config value uniformly

--- applications/crossbar/src/modules/cb_dialplans.erl
+++ applications/crossbar/src/modules/cb_dialplans.erl
@@ -64,15 +64,16 @@ resource_exists() -> 'true'.
 %%--------------------------------------------------------------------
 -spec validate(cb_context:context()) -> cb_context:context().
 validate(Context) ->
-    Doc = get_dialplans(cb_context:account_id(Context)),
+    Doc = maybe_add_name(get_dialplans(cb_context:account_id(Context))),
     cb_context:setters(Context, [{fun cb_context:set_resp_data/2, Doc}
                                 ,{fun cb_context:set_resp_status/2, 'success'}
                                 ]).
 
+-spec get_dialplans(api_binary()) -> [kz_json:object()].
 get_dialplans('undefined') ->
-    maybe_add_name(kapps_config:get_all_kvs(<<"dialplans">>));
+    kapps_config:get_all_kvs(<<"dialplans">>);
 get_dialplans(AccountId) ->
-    kapps_account_config:get_global(AccountId, ?ACCOUNT_CONFIG, <<"dialplans">>).
+    kapps_account_config:get_global(AccountId, ?ACCOUNT_CONFIG, <<"dialplans">>, []).
 
 -spec maybe_add_name(kz_proplist()) -> kz_json:object().
 maybe_add_name(KVs) ->
