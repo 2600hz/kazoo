c287afa03ea475dfcd79f00f269c7e3c11eb98da
Roman Galeev <jamhed@2600hz.com>
2016-09-23 21:36:32 +0200

    KAZOO-5039 rename list() to kz_json:objects()

--- applications/crossbar/src/cb_pager.erl
+++ applications/crossbar/src/cb_pager.erl
@@ -9,7 +9,7 @@
 -module(cb_pager).
 -export([descending/7, ascending/7]).
 
--spec descending(binary(), binary(), integer(), integer(), integer(), fun(), []) -> {integer(), list()}.
+-spec descending(binary(), binary(), integer(), integer(), integer(), fun(), []) -> {integer(), kz_json:objects()}.
 descending(AccountId, View, Start, End, Limit, Filter, Options) when
       is_binary(AccountId), is_binary(View), is_integer(Start), is_integer(End), is_integer(Limit), Start > End ->
     MODbs = lists:reverse(kazoo_modb:get_range(AccountId, End, Start)),
@@ -18,7 +18,7 @@ descending(AccountId, View, Start, End, Limit, Filter, Options) when
         lists:foldl(fun fold_query/2, {Limit, 'undefined', []}, [ {Db, View, CouchOpts, Filter} || Db <- MODbs ]),
     {LastKey, JObjs}.
 
--spec ascending(binary(), binary(), integer(), integer(), integer(), fun(), []) -> {integer(), list()}.
+-spec ascending(binary(), binary(), integer(), integer(), integer(), fun(), []) -> {integer(), kz_json:objects()}.
 ascending(AccountId, View, Start, End, Limit, Filter, Options) when
       is_binary(AccountId), is_binary(View), is_integer(Start), is_integer(End), is_integer(Limit), Start < End ->
     MODbs = kazoo_modb:get_range(AccountId, Start, End),
@@ -27,7 +27,7 @@ ascending(AccountId, View, Start, End, Limit, Filter, Options) when
         lists:foldl(fun fold_query/2, {Limit, 'undefined', []}, [ {Db, View, CouchOpts, Filter} || Db <- MODbs ]),
     {LastKey, JObjs}.
 
--spec fold_query({binary(), binary(), list(), fun()}, {integer(), integer(), list()}) -> {integer(), integer(), list()}.
+-spec fold_query({binary(), binary(), list(), fun()}, {integer(), integer(), kz_json:objects()}) -> {integer(), integer(), kz_json:objects()}.
 fold_query({Db, View, CouchOpts, Filter}, {Limit, _LastKey, Res}) when is_integer(Limit), Limit > 0 ->
     Queried = erlang:length(Res),
     LimitWithLast = 1 + Limit - Queried,
@@ -44,13 +44,13 @@ limited_query(Limit, Db, View, CouchOpts) ->
         {'error', Error} -> throw(Error)
     end.
 
--spec apply_filter(fun(), list()) -> list().
+-spec apply_filter(fun(), kz_json:objects()) -> kz_json:objects().
 apply_filter(Map, Objects) when is_function(Map, 1) ->
     lists:foldl(fun(Obj, Acc) -> [ Map(Obj) | Acc ] end, [], Objects);
 apply_filter(Filter, Objects) when is_function(Filter, 2) ->
     lists:foldl(Filter, [], Objects).
 
--spec last_key(list(), integer(), integer()) -> {integer()|undefined, list()}.
+-spec last_key(kz_json:objects(), integer(), integer()) -> {integer()|undefined, kz_json:objects()}.
 last_key([], _, _) -> {'undefined', []};
 last_key(JObjs, Limit, Returned) when Returned < Limit -> {'undefined', JObjs};
 last_key([Last|JObjs], Limit, Returned) when Returned == Limit -> {kz_json:get_value(<<"key">>, Last), JObjs}.
