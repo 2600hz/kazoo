f31f21785029ae5adf88f9ad4fe32d5f3add8507
Roman Galeev <jamhed@2600hz.com>
2016-09-27 20:34:34 +0200

    KAZOO-4904 filter fax errors by spandsp code

--- applications/ecallmgr/src/fs_event_filters.hrl
+++ applications/ecallmgr/src/fs_event_filters.hrl
@@ -135,6 +135,7 @@
         ,<<"variable_fax_remote_station_id">>
         ,<<"variable_fax_remote_vendor">>
         ,<<"variable_fax_result_text">>
+        ,<<"variable_fax_result_code">>
         ,<<"variable_fax_success">>
         ,<<"variable_fax_timezone">>
         ,<<"variable_fax_transfer_rate">>
