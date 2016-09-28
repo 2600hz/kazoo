ca16662d495de0c20af6ef3b24dfcd06ed24d0d4
Pierre Fenoll <pierrefenoll@gmail.com>
2016-09-28 17:54:58 +0200

    KAZOO-5072: formatting

--- core/kazoo_globals/src/kz_nodes.erl
+++ core/kazoo_globals/src/kz_nodes.erl
@@ -202,11 +202,7 @@ determine_whapp_zones_fold({Zone, Whapps}, {Whapp, Zones, C}=Acc) ->
 -spec status() -> 'no_return'.
 status() ->
     try
-        Nodes = lists:sort(fun(#kz_node{node=N1}, #kz_node{node=N2}) ->
-                                   N1 > N2
-                           end
-                          ,ets:tab2list(?MODULE)
-                          ),
+        Nodes = lists:sort(fun compare_nodes/2, ets:tab2list(?MODULE)),
         print_status(Nodes, gen_listener:call(?SERVER, 'zone'))
     catch
         {'EXIT', {'badarg', _}} ->
@@ -214,9 +210,13 @@ status() ->
             'no_return'
     end.
 
+-spec compare_nodes(kz_node(), kz_node()) -> boolean().
+compare_nodes(#kz_node{node = N1}, #kz_node{node = N2}) -> N1 > N2.
+
 -spec print_status(kz_nodes(), atom()) -> 'no_return'.
 print_status(Nodes, Zone) ->
-    _ = [print_node_status(Node, Zone) || Node <- Nodes],
+    F = fun (Node) -> print_node_status(Node, Zone) end,
+    lists:foreach(F, Nodes),
     'no_return'.
 
 -spec print_node_status(kz_node(), atom()) -> 'ok'.
@@ -260,7 +260,7 @@ maybe_print_kapps(Whapps) ->
     case lists:sort(fun compare_apps/2, Whapps) of
         []-> 'ok';
         SortedWhapps ->
-            io:format("WhApps        : ", []),
+            io:format("WhApps        : "),
             status_list(SortedWhapps, 0)
     end.
 
