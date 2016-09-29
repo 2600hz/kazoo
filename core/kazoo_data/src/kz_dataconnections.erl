bf3933df2a62823c43659cf4eebd9d35ec548abc
Pierre Fenoll <pierrefenoll@gmail.com>
2016-09-29 10:24:53 +0200

    qf-ll: nicer log line on Couch connection (#2618)

--- core/kazoo_data/src/kz_dataconnections.erl
+++ core/kazoo_data/src/kz_dataconnections.erl
@@ -71,7 +71,8 @@ wait_for_connection(Tag, Timeout) ->
         {'error', _E} ->
             timer:sleep(rand:uniform(?MILLISECONDS_IN_SECOND) + 100),
             wait_for_connection(Tag, kz_util:decr_timeout(Timeout, Start));
-        {'ok', Info} -> lager:info("connected to ~s : ~p", [Tag, Info])
+        {'ok', Info} ->
+            lager:info("connected to ~s: ~s", [Tag, kz_json:encode(Info)])
     catch
         'error':{'badmatch','$end_of_table'} ->
             timer:sleep(rand:uniform(?MILLISECONDS_IN_SECOND) + 100),
