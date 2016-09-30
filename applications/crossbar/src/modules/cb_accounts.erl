81cec5acc0a3e1c7dfeb8ff03682db6eddcff27b
Roman Galeev <jamhed@2600hz.com>
2016-09-30 15:33:11 +0200

    KAZOO-5092 github notes

--- applications/crossbar/src/modules/cb_accounts.erl
+++ applications/crossbar/src/modules/cb_accounts.erl
@@ -1541,7 +1541,7 @@ delete_remove_sip_aggregates(Context) ->
 
 -spec delete_remove_db(cb_context:context()) -> cb_context:context() | boolean().
 delete_remove_db(Context) ->
-    _ = delete_remove_from_accounts(Context),
+    _ = provisioner_util:maybe_delete_account(Context),
     Removed = case kz_datamgr:open_doc(cb_context:account_db(Context), cb_context:account_id(Context)) of
                   {'ok', _} ->
                       kz_datamgr:db_delete(cb_context:account_db(Context)),
@@ -1553,7 +1553,8 @@ delete_remove_db(Context) ->
               end,
     case Removed of
         'true' ->
-            lager:debug("deleted db ~s", [cb_context:account_db(Context)]);
+            lager:debug("deleted db ~s", [cb_context:account_db(Context)]),
+            _ = delete_remove_from_accounts(Context);
         'false' ->
             lager:debug("failed to remove database ~s", [cb_context:account_db(Context)]),
             crossbar_util:response('error', <<"unable to remove database">>, 500, Context)
@@ -1582,7 +1583,6 @@ delete_mod_dbs(AccountId, Year, Month) ->
 delete_remove_from_accounts(Context) ->
     case kz_datamgr:open_doc(?KZ_ACCOUNTS_DB, cb_context:account_id(Context)) of
         {'ok', JObj} ->
-            _ = provisioner_util:maybe_delete_account(Context),
             _ = cb_mobile_manager:delete_account(Context),
             crossbar_doc:delete(
               cb_context:setters(Context
