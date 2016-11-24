28eb71b7a5d72fc86060c9715990cba8d134f9cb
Pierre Fenoll <pierrefenoll@gmail.com>
2016-11-25 00:43:30 +0100

    KAZOO-305: add upper bound to number of DID search results (#2839)

--- applications/crossbar/src/modules_v2/cb_phone_numbers_v2.erl
+++ applications/crossbar/src/modules_v2/cb_phone_numbers_v2.erl
@@ -609,8 +609,7 @@ find_numbers(Context, AccountId, ResellerId) ->
         fun(C) ->
                 lager:debug("carriers find: ~p", [Options]),
                 Prefix = knm_carriers:prefix(Options),
-                Quantity = knm_carriers:quantity(Options),
-                Found = knm_carriers:find(Prefix, Quantity, Options),
+                Found = knm_carriers:find(Prefix, knm_carriers:quantity(Options), Options),
                 cb_context:setters(C
                                   ,[{fun cb_context:set_resp_data/2, Found}
                                    ,{fun cb_context:set_resp_status/2, 'success'}
