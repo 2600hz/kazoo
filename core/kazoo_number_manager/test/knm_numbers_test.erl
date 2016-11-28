caed3d03cebd44a36b32d90bf53f49f3fff67c0b
Pierre Fenoll <pierrefenoll@gmail.com>
2016-11-28 17:18:28 +0100

    KAZOO-5180: add tests for knm_numbers:get/1,2

---
+++ core/kazoo_number_manager/test/knm_numbers_test.erl
@@ -0,0 +1,36 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2016, 2600Hz
+%%% @doc
+%%% @end
+%%% @contributors
+%%%   Pierre Fenoll
+%%%-------------------------------------------------------------------
+-module(knm_numbers_test).
+
+-include_lib("eunit/include/eunit.hrl").
+-include("knm.hrl").
+
+get_test_() ->
+    Ret = knm_numbers:get([?TEST_AVAILABLE_NUM]),
+    NotNum = <<"NOT a number">>,
+    [?_assertMatches(#{ko := #{}
+                      ,ok := [_]
+                      }, Ret)
+    ,?_assertEqual(?TEST_AVAILABLE_NUM
+                  ,knm_phone_number:number(knm_number:phone_number(hd(maps:get(ok, Ret))))
+                  )
+    ,?_assertMatches(#{ko := #{NotNum => not_reconcilable}}
+                    ,km_numbers:get([NotNum], [])
+                    )
+    ,?_assertMatches(#{ko := #{NotNum => not_reconcilable}
+                      ,ok := [_]
+                      }
+                    ,knm_numbers:get([?TEST_AVAILABLE_NUM, NotNum])
+                    )
+    ,?_assertMatches(#{ko := #{NotNum => not_reconcilable}
+                      ,ok := [_]
+                      }
+                    ,knm_numbers:get([?TEST_AVAILABLE_NUM, NotNum
+                                     ,?TEST_AVAILABLE_NUM, NotNum])
+                    )
+    ].
