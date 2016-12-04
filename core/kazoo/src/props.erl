815c76c3a3016de7b52cf82d328b6ea6f276a91c
Pierre Fenoll <pierrefenoll@gmail.com>
2016-12-04 12:03:26 +0100

    KAZOO-5180: remove dead code in props.erl

--- core/kazoo/src/props.erl
+++ core/kazoo/src/props.erl
@@ -101,11 +101,9 @@ get_value(Key, Props) ->
     get_value(Key, Props, 'undefined').
 
 get_value(_Key, [], Default) -> Default;
-get_value([Key], Props, Default) when is_binary(Key)
-                                      orelse is_atom(Key) ->
+get_value([Key], Props, Default) when is_binary(Key); is_atom(Key) ->
     get_value(Key, Props, Default);
-get_value([Key|Keys], Props, Default) when is_binary(Key)
-                                           orelse is_atom(Key) ->
+get_value([Key|Keys], Props, Default) when is_binary(Key); is_atom(Key) ->
     case get_value(Key, Props) of
         'undefined' -> Default;
         SubProps -> get_value(Keys, SubProps, Default)
@@ -117,8 +115,7 @@ get_value(Key, Props, Default) when is_list(Props) ->
                 'true' -> 'true';
                 'false' -> Default
             end;
-        {Key, V} -> V; % only return V if a two-tuple is found
-        Other when is_tuple(Other) -> Default % otherwise return the default
+        {Key, V} -> V
     end.
 
 %% Given a list of keys, find the first one defined
