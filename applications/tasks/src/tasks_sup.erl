22566ae943e3f72073141a48c0afa44298011e09
Pierre Fenoll <pierrefenoll@gmail.com>
2016-12-06 17:52:22 +0100

    qf-tasks-uptime: keep track of tasks app uptime

--- applications/tasks/src/tasks_sup.erl
+++ applications/tasks/src/tasks_sup.erl
@@ -55,6 +55,7 @@ start_link() ->
 %%--------------------------------------------------------------------
 -spec init(any()) -> sup_init_ret().
 init([]) ->
+    kz_util:set_startup(),
     RestartStrategy = 'one_for_one',
     MaxRestarts = 5,
     MaxSecondsBetweenRestarts = 10,
