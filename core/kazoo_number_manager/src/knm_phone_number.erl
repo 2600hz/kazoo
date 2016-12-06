d469df3eece69aa39ae40369af0d1e2232feff63
Pierre Fenoll <pierrefenoll@gmail.com>
2016-12-06 13:49:04 +0100

    KAZOO-5225: move clause to the /3 version

--- core/kazoo_number_manager/src/knm_phone_number.erl
+++ core/kazoo_number_manager/src/knm_phone_number.erl
@@ -742,8 +742,6 @@ set_ported_in(N, Ported) when is_boolean(Ported) ->
 module_name(#knm_phone_number{module_name = Name}) -> Name.
 
 -spec set_module_name(knm_phone_number(), ne_binary()) -> knm_phone_number().
-set_module_name(N, <<"undefined">>) ->
-    set_module_name(N, ?CARRIER_LOCAL);
 set_module_name(N0, ?CARRIER_LOCAL=Name) ->
     Feature =
         case feature(N0, ?FEATURE_LOCAL) of
@@ -774,11 +772,14 @@ set_module_name(N, Name=?NE_BINARY) ->
                               }
     end.
 
-%% Do not override is_billable when field is already set on doc.
 -spec set_module_name(knm_phone_number(), ne_binary(), api_boolean()) -> knm_phone_number().
+%% Some old docs have these as module name
+set_module_name(N, <<"undefined">>, IsBillable) ->
+    set_module_name(N, ?CARRIER_LOCAL, IsBillable);
 set_module_name(N0, Name, IsBillable)
   when is_boolean(IsBillable) ->
     N = set_module_name(N0, Name),
+    %% Do not override is_billable when field is already set on doc.
     case N#knm_phone_number.is_billable of
         IsBillable -> N;
         _ ->
