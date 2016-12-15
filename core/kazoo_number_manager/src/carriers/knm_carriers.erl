cbca43f6bf5d28e48e901844092202714dc8c3c6
karl anderson <karl@2600hz.com>
2016-12-15 13:19:11 -0800

    HELP-26693: bill for billable

--- core/kazoo_number_manager/src/carriers/knm_carriers.erl
+++ core/kazoo_number_manager/src/carriers/knm_carriers.erl
@@ -382,7 +382,7 @@ is_number_billable(PhoneNumber) ->
     Carrier = knm_phone_number:module_name(PhoneNumber),
     try apply(Carrier, is_number_billable, [PhoneNumber])
     catch
-        'error':_R -> 'false'
+        'error':_R -> 'true'
     end.
 
 %%%===================================================================
