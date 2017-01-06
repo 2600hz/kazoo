4d18cd3e42a1e88dfb00b5be8a3c995916c3930d
Pierre Fenoll <pierrefenoll@gmail.com>
2017-01-06 20:26:17 +0100

    KAZOO-5271: remove superfluous module name

--- core/kazoo_data/src/kz_data.erl
+++ core/kazoo_data/src/kz_data.erl
@@ -37,7 +37,7 @@
                   {'url_parsing_failed', _} |
                   {integer(), _}. % {error_code, body}
 
--type error() :: {'error', kz_data:errors()}.
+-type error() :: {'error', errors()}.
 
 -export_type([connection/0
              ,option/0, options/0
