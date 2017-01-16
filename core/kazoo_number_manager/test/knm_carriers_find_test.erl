cf35586eb3646ed1852c0aa9714f2f3fde3a7946
Pierre Fenoll <pierrefenoll@gmail.com>
2017-01-16 18:33:15 +0100

    KAZOO-5276: remove dead code

--- core/kazoo_number_manager/test/knm_carriers_find_test.erl
+++ core/kazoo_number_manager/test/knm_carriers_find_test.erl
@@ -40,12 +40,7 @@ check_test_() ->
 
 find_local_test_() ->
     [{"Finding local numbers not supported"
-     ,?_assertMatch({'error', 'not_available'}
-                   ,knm_local:find_numbers(<<"415">>, 1, [])
-                   )
-     }
-    ,{"Finding local numbers returns empty list"
-     ,?_assertEqual([], knm_carriers:find(<<"415">>))
+     ,?_assertMatch({'error', 'not_available'}, knm_local:find_numbers(<<"415">>, 1, []))
      }
     ].
 
