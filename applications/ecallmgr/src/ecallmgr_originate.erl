469823d7319aa78e4576d93d7247d917457d8927
Roman Galeev <jamhedd@gmail.com>
2017-01-16 14:58:26 +0100

    KAZOO-5118: forcefully set caller-id on transfered calls if specified (#3091)
    * KAZOO-5118: forcefully set caller-id on transfered calls if specified
    
    * make fmt
    
    * use set instead of export

--- applications/ecallmgr/src/ecallmgr_originate.erl
+++ applications/ecallmgr/src/ecallmgr_originate.erl
@@ -662,6 +662,7 @@ get_unset_vars(JObj) ->
             ,maybe_fix_ignore_early_media(Export)
             ,maybe_fix_group_confirm(Export)
             ,maybe_fix_fs_auto_answer_bug(Export)
+            ,maybe_fix_caller_id(Export, JObj)
             ,"^"
             ]
     end.
@@ -687,6 +688,14 @@ maybe_fix_fs_auto_answer_bug(Export) ->
         'false' ->
             "^unset:sip_h_Call-Info^unset:sip_h_Alert-Info^unset:alert_info^unset:sip_invite_params^set:sip_auto_answer=false"
     end.
+
+-spec maybe_fix_caller_id(strings(), kz_json:object()) -> string().
+maybe_fix_caller_id(Export, JObj) ->
+    Fix = [
+           {lists:member("origination_callee_id_name", Export), kz_json:get_value(<<"Outbound-Callee-ID-Name">>, JObj), "effective_caller_id_name"}
+          ,{lists:member("origination_callee_id_number", Export), kz_json:get_value(<<"Outbound-Callee-ID-Number">>, JObj), "effective_caller_id_number"}
+          ],
+    string:join([ "^set:" ++ Key ++ "=" ++ erlang:binary_to_list(Value) || {IsTrue, Value, Key} <- Fix, IsTrue ], ":").
 
 -spec publish_error(ne_binary(), created_uuid() | api_binary(), kz_json:object(), api_binary()) -> 'ok'.
 publish_error(_, _, _, 'undefined') -> 'ok';
