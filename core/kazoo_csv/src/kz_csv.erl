53caad98fd978fc90a5e769d60e00a149e6b6203
Pierre Fenoll <pierrefenoll@gmail.com>
2017-02-01 18:24:05 +0100

    KAZOO-5307: move kz_csv tests under test/

--- core/kazoo_csv/src/kz_csv.erl
+++ core/kazoo_csv/src/kz_csv.erl
@@ -8,6 +8,8 @@
 %%%-------------------------------------------------------------------
 -module(kz_csv).
 
+-include("csv.hrl").
+
 %% Public API
 -export([count_rows/1
         ,fold/3
@@ -20,10 +22,6 @@
         ]).
 
 -include_lib("kazoo/include/kz_types.hrl").
-
--ifndef(ZILCH).
--define(ZILCH, 'undefined').
--endif.
 
 -type cell() :: ne_binary() | ?ZILCH.
 -type row() :: [cell(), ...].
@@ -214,89 +212,4 @@ cell_to_binary(?ZILCH) -> <<>>;
 cell_to_binary(Cell=?NE_BINARY) ->
     binary:replace(Cell, <<$,>>, <<$;>>, ['global']).
 
-
 %%% End of Module.
-
-
--ifdef(TEST).
--include_lib("eunit/include/eunit.hrl").
-
-associator_test() ->
-    OrderedFields = [<<"A">>, <<"B">>, <<"C">>, <<"D">>, <<"E">>],
-    CSVHeader = [<<"A">>, <<"E">>, <<"C">>, <<"B">>],
-    CSVRow    = [<<"1">>, <<"5">>, <<"3">>, <<"2">>],
-    Verify = fun (_Cell) -> 'true' end,
-    Verifier = fun (_Field, Cell) -> Verify(Cell) end,
-    FAssoc = associator(CSVHeader, OrderedFields, Verifier),
-    ?assertEqual({'true', [<<"1">>, <<"2">>, <<"3">>, 'undefined', <<"5">>]}, FAssoc(CSVRow)).
-
-associator_verify_test() ->
-    OrderedFields = [<<"A">>, <<"B">>, <<"C">>, <<"D">>, <<"E">>],
-    CSVHeader = [<<"A">>, <<"E">>, <<"C">>, <<"B">>],
-    CSVRow    = [<<"1">>, <<"5">>, <<"3">>, <<"2">>],
-    Verify = fun (_Cell) -> 'false' end,
-    Verifier = fun (<<"B">>, Cell) -> Verify(Cell); (_Field, _Cell) -> 'true' end,
-    FAssoc = associator(CSVHeader, OrderedFields, Verifier),
-    ?assertEqual('false', FAssoc(CSVRow)).
-
-take_row_test_() ->
-    CSV1 = <<"a\r\nb\nc\nd\n\re\r\r">>,
-    CSV2 = <<"b\nc\nd\n\re\r\r">>,
-    CSV3 = <<"c\nd\n\re\r\r">>,
-    CSV4 = <<"d\n\re\r\r">>,
-    CSV5 = <<"e\r\r">>,
-    CSV6 = <<>>,
-    CSV7 = <<"\r\r">>,
-    [?_assertEqual({[<<"a">>], CSV2}, take_row(CSV1))
-    ,?_assertEqual({[<<"b">>], CSV3}, take_row(CSV2))
-    ,?_assertEqual({[<<"c">>], CSV4}, take_row(CSV3))
-    ,?_assertEqual({[<<"d">>], CSV5}, take_row(CSV4))
-    ,?_assertEqual({[<<"e">>], CSV6}, take_row(CSV5))
-    ,?_assertEqual('eof', take_row(CSV6))
-    ,?_assertEqual('eof', take_row(CSV7))
-    ,?_assertEqual({[<<"1">>,<<"B">>], <<>>}, take_row(<<"1,B">>))
-    ].
-
-pad_row_to_test_() ->
-    [?_assertEqual([?ZILCH], pad_row_to(1, []))
-    ,?_assertEqual([?ZILCH], pad_row_to(1, [?ZILCH]))
-    ,?_assertEqual([?ZILCH, ?ZILCH, ?ZILCH], pad_row_to(3, [?ZILCH]))
-    ].
-
-count_rows_test_() ->
-    [?_assertEqual(0, count_rows(<<"a,b,\n,1,2,">>))
-    ,?_assertEqual(0, count_rows(<<"abc">>))
-    ,?_assertEqual(0, count_rows(<<>>))
-    ,?_assertEqual(0, count_rows(<<"a,b,c\n1\n2\n3">>))
-    ,?_assertEqual(1, count_rows(<<"a,b,c\n1,2,3">>))
-    ,?_assertEqual(3, count_rows(<<"a,b,c\n1,2,3\r\n4,5,6\n7,8,9\n">>))
-    ].
-
-row_to_iolist_test_() ->
-    [?_assertException('error', 'function_clause', row_to_iolist([]))] ++
-        [?_assertEqual(Expected, iolist_to_binary(row_to_iolist(Input)))
-         || {Expected, Input} <- [{<<"a,b">>, [<<"a">>, <<"b">>]}
-                                 ,{<<"a,,b">>, [<<"a">>, ?ZILCH, <<"b">>]}
-                                 ,{<<",,b">>, [?ZILCH, ?ZILCH, <<"b">>]}
-                                 ,{<<"a,b,">>, [<<"a">>, <<"b">>, ?ZILCH]}
-                                 ,{<<"a,b,,,c">>, [<<"a">>, <<"b">>, ?ZILCH, ?ZILCH, <<"c">>]}
-                                 ]
-        ].
-
-json_to_iolist_test_() ->
-    Records1 = [kz_json:from_list([{<<"A">>, <<"a1">>}])
-               ,kz_json:from_list([{<<"A">>, <<"42">>}])
-               ],
-    Records2 = [kz_json:from_list([{<<"field1">>,?ZILCH}, {<<"field deux">>,<<"QUUX">>}])
-               ,kz_json:from_list([{<<"field deux">>, ?ZILCH}])
-               ,kz_json:from_list([{<<"field1">>, <<"r'bla.+\\n'">>}])
-               ],
-    Records3 = [kz_json:from_list([{<<"account_id">>,<<"009afc511c97b2ae693c6cc4920988e8">>}, {<<"e164">>,<<"+14157215234">>}, {<<"cnam.outbound">>,<<"me">>}])
-               ,kz_json:from_list([{<<"account_id">>,<<>>}, {<<"e164">>,<<"+14157215235">>}, {<<"cnam.outbound">>,<<>>}])
-               ],
-    [?_assertEqual(<<"A\na1\n42\n">>, json_to_iolist(Records1))
-    ,?_assertEqual(<<"field1,field deux\n,QUUX\n,\nr'bla.+\\n',\n">>, json_to_iolist(Records2))
-    ,?_assertEqual(<<"account_id,e164,cnam.outbound\n009afc511c97b2ae693c6cc4920988e8,+14157215234,me\n,+14157215235,\n">>, json_to_iolist(Records3))
-    ].
-
--endif.
---
+++ core/kazoo_csv/test/kz_csv_test.erl
@@ -0,0 +1,91 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2016-2017, 2600Hz
+%%% @doc
+%%% @end
+%%% @contributors
+%%%   Pierre Fenoll
+%%%-------------------------------------------------------------------
+-module(kz_csv_test).
+
+-include_lib("kazoo_csv/include/kazoo_csv.hrl").
+
+-include_lib("eunit/include/eunit.hrl").
+
+
+associator_test() ->
+    OrderedFields = [<<"A">>, <<"B">>, <<"C">>, <<"D">>, <<"E">>],
+    CSVHeader = [<<"A">>, <<"E">>, <<"C">>, <<"B">>],
+    CSVRow    = [<<"1">>, <<"5">>, <<"3">>, <<"2">>],
+    Verify = fun (_Cell) -> 'true' end,
+    Verifier = fun (_Field, Cell) -> Verify(Cell) end,
+    FAssoc = kz_csv:associator(CSVHeader, OrderedFields, Verifier),
+    ?assertEqual({'true', [<<"1">>, <<"2">>, <<"3">>, 'undefined', <<"5">>]}, FAssoc(CSVRow)).
+
+associator_verify_test() ->
+    OrderedFields = [<<"A">>, <<"B">>, <<"C">>, <<"D">>, <<"E">>],
+    CSVHeader = [<<"A">>, <<"E">>, <<"C">>, <<"B">>],
+    CSVRow    = [<<"1">>, <<"5">>, <<"3">>, <<"2">>],
+    Verify = fun (_Cell) -> 'false' end,
+    Verifier = fun (<<"B">>, Cell) -> Verify(Cell); (_Field, _Cell) -> 'true' end,
+    FAssoc = kz_csv:associator(CSVHeader, OrderedFields, Verifier),
+    ?assertEqual('false', FAssoc(CSVRow)).
+
+take_row_test_() ->
+    CSV1 = <<"a\r\nb\nc\nd\n\re\r\r">>,
+    CSV2 = <<"b\nc\nd\n\re\r\r">>,
+    CSV3 = <<"c\nd\n\re\r\r">>,
+    CSV4 = <<"d\n\re\r\r">>,
+    CSV5 = <<"e\r\r">>,
+    CSV6 = <<>>,
+    CSV7 = <<"\r\r">>,
+    [?_assertEqual({[<<"a">>], CSV2}, kz_csv:take_row(CSV1))
+    ,?_assertEqual({[<<"b">>], CSV3}, kz_csv:take_row(CSV2))
+    ,?_assertEqual({[<<"c">>], CSV4}, kz_csv:take_row(CSV3))
+    ,?_assertEqual({[<<"d">>], CSV5}, kz_csv:take_row(CSV4))
+    ,?_assertEqual({[<<"e">>], CSV6}, kz_csv:take_row(CSV5))
+    ,?_assertEqual('eof', kz_csv:take_row(CSV6))
+    ,?_assertEqual('eof', kz_csv:take_row(CSV7))
+    ,?_assertEqual({[<<"1">>,<<"B">>], <<>>}, kz_csv:take_row(<<"1,B">>))
+    ].
+
+pad_row_to_test_() ->
+    [?_assertEqual([?ZILCH], kz_csv:pad_row_to(1, []))
+    ,?_assertEqual([?ZILCH], kz_csv:pad_row_to(1, [?ZILCH]))
+    ,?_assertEqual([?ZILCH, ?ZILCH, ?ZILCH], kz_csv:pad_row_to(3, [?ZILCH]))
+    ].
+
+count_rows_test_() ->
+    [?_assertEqual(0, kz_csv:count_rows(<<"a,b,\n,1,2,">>))
+    ,?_assertEqual(0, kz_csv:count_rows(<<"abc">>))
+    ,?_assertEqual(0, kz_csv:count_rows(<<>>))
+    ,?_assertEqual(0, kz_csv:count_rows(<<"a,b,c\n1\n2\n3">>))
+    ,?_assertEqual(1, kz_csv:count_rows(<<"a,b,c\n1,2,3">>))
+    ,?_assertEqual(3, kz_csv:count_rows(<<"a,b,c\n1,2,3\r\n4,5,6\n7,8,9\n">>))
+    ].
+
+row_to_iolist_test_() ->
+    [?_assertException('error', 'function_clause', kz_csv:row_to_iolist([]))] ++
+        [?_assertEqual(Expected, iolist_to_binary(kz_csv:row_to_iolist(Input)))
+         || {Expected, Input} <- [{<<"a,b">>, [<<"a">>, <<"b">>]}
+                                 ,{<<"a,,b">>, [<<"a">>, ?ZILCH, <<"b">>]}
+                                 ,{<<",,b">>, [?ZILCH, ?ZILCH, <<"b">>]}
+                                 ,{<<"a,b,">>, [<<"a">>, <<"b">>, ?ZILCH]}
+                                 ,{<<"a,b,,,c">>, [<<"a">>, <<"b">>, ?ZILCH, ?ZILCH, <<"c">>]}
+                                 ]
+        ].
+
+json_to_iolist_test_() ->
+    Records1 = [kz_json:from_list([{<<"A">>, <<"a1">>}])
+               ,kz_json:from_list([{<<"A">>, <<"42">>}])
+               ],
+    Records2 = [kz_json:from_list([{<<"field1">>,?ZILCH}, {<<"field deux">>,<<"QUUX">>}])
+               ,kz_json:from_list([{<<"field deux">>, ?ZILCH}])
+               ,kz_json:from_list([{<<"field1">>, <<"r'bla.+\\n'">>}])
+               ],
+    Records3 = [kz_json:from_list([{<<"account_id">>,<<"009afc511c97b2ae693c6cc4920988e8">>}, {<<"e164">>,<<"+14157215234">>}, {<<"cnam.outbound">>,<<"me">>}])
+               ,kz_json:from_list([{<<"account_id">>,<<>>}, {<<"e164">>,<<"+14157215235">>}, {<<"cnam.outbound">>,<<>>}])
+               ],
+    [?_assertEqual(<<"A\na1\n42\n">>, kz_csv:json_to_iolist(Records1))
+    ,?_assertEqual(<<"field1,field deux\n,QUUX\n,\nr'bla.+\\n',\n">>, kz_csv:json_to_iolist(Records2))
+    ,?_assertEqual(<<"account_id,e164,cnam.outbound\n009afc511c97b2ae693c6cc4920988e8,+14157215234,me\n,+14157215235,\n">>, kz_csv:json_to_iolist(Records3))
+    ].
