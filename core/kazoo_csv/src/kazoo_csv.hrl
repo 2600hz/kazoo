29a45fa09c5394c5826c9fe8945ef7abd6bac383
Pierre Fenoll <pierrefenoll@gmail.com>
2017-02-01 18:15:20 +0100

    KAZOO-5307: move kz_csv to its own app

---
+++ core/kazoo_csv/src/kazoo_csv.hrl
@@ -0,0 +1,12 @@
+-ifndef(KZ_KAZOO_CSV_HRL).
+
+-include_lib("kazoo/include/kz_types.hrl").
+-include_lib("kazoo/include/kz_log.hrl").
+-include_lib("kazoo/include/kz_databases.hrl").
+
+-define(APP, 'kzaoo_csv').
+-define(APP_NAME, <<"kazoo_csv">>).
+-define(APP_VERSION, <<"4.0.0">> ).
+
+-define(KZ_KAZOO_CSV_HRL, 'true').
+-endif.
