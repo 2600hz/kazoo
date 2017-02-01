2c229ac7777813dc11e385248e8abaa01b3cb41a
Pierre Fenoll <pierrefenoll@gmail.com>
2017-02-01 18:24:05 +0100

    KAZOO-5307: move kz_csv tests under test/

--- core/kazoo_csv/src/csv.hrl
+++ core/kazoo_csv/src/csv.hrl
@@ -1,8 +1,6 @@
 -ifndef(KZ_KAZOO_CSV_HRL).
 
--include_lib("kazoo/include/kz_types.hrl").
--include_lib("kazoo/include/kz_log.hrl").
--include_lib("kazoo/include/kz_databases.hrl").
+-include("kazoo_csv.hrl").
 
 -define(APP, 'kzaoo_csv').
 -define(APP_NAME, <<"kazoo_csv">>).
