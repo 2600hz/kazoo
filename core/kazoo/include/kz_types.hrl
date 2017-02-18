1dce304c4a77de4d509b16c327c5bcb84809ac4b
Mark Magnusson <mark@2600hz.com>
2017-02-18 11:11:51 -0500

    add some encoded cases

--- core/kazoo/include/kz_types.hrl
+++ core/kazoo/include/kz_types.hrl
@@ -287,9 +287,16 @@
         <<"account%2f", (A):2/binary, "%2f", (B):2/binary, "%2f", (Rest):28/binary>>
        ).
 
--define(MATCH_PROVISIONER(Account),
+-define(MATCH_PROVISIONER_RAW(Account),
         <<"account/", (Account):34/binary, "-provisioner">>
        ).
+-define(MATCH_PROVISIONER_ENCODED(Account),
+        <<"account%2F", (Account):38/binary, "-provisioner">>
+       ).
+-define(MATCH_PROVISIONER_encoded(Account),
+        <<"account%2f", (Account):38/binary, "-provisioner">>
+       ).
+
 
 -define(MATCH_MODB_SUFFIX_RAW(A, B, Rest, Year, Month),
         <<(A):2/binary, (B):2/binary, (Rest):28/binary
