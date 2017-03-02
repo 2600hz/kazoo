efc875b83e1a81d9e4200bc41643fd84c412de00
Pierre Fenoll <pierrefenoll@gmail.com>
2017-03-02 19:23:21 +0100

    HELP-28353: test that the local feature gets indeed reset on carrier rename
    Conflicts:
    	core/kazoo_number_manager/test/knm_telnyx_test.erl

--- core/kazoo_number_manager/test/knm_telnyx_test.erl
+++ core/kazoo_number_manager/test/knm_telnyx_test.erl
@@ -187,7 +187,12 @@ rename_carrier_test_() ->
     ,{"Verify carrier name is changed"
      ,?_assertEqual(?CARRIER_LOCAL, knm_phone_number:module_name(PN2))
      }
-    ,?_assertEqual(undefined, kz_json:get_value(?FEATURE_RENAME_CARRIER, knm_phone_number:doc(PN2)))
+    ,{"Verify feature is now removed"
+     ,?_assertEqual(undefined, kz_json:get_value(?FEATURE_RENAME_CARRIER, knm_phone_number:doc(PN2)))
+     }
+    ,{"Verify local feature is now set"
+     ,?_assertEqual(true, lists:member(?FEATURE_LOCAL, knm_phone_number:features_list(PN4)))
+     }
     ,{"Verify setting wrong carrier is forbidden"
      ,?_assertThrow({error, invalid, _N, <<"'wrong_carrier' is not known by the system">>}
                    ,knm_number:update_phone_number(N2, [{fun knm_phone_number:reset_doc/2, JObj2}])
@@ -212,5 +217,28 @@ rename_carrier_test_() ->
                                                   ,[{auth_by, ?MASTER_ACCOUNT_ID}]
                                                   )
                    )
+     }
+    ].
+
+rename_from_local_test_() ->
+    Options = [{auth_by, ?MASTER_ACCOUNT_ID}
+              ],
+    {ok, N1} = knm_number:get(?TEST_IN_SERVICE_NUM, Options),
+    PN1 = knm_number:phone_number(N1),
+    JObj1 = kz_json:from_list([{?FEATURE_RENAME_CARRIER, <<"telnyx">>}]),
+    {ok, N2} = knm_number:update_phone_number(N1, [{fun knm_phone_number:reset_doc/2, JObj1}], Options),
+    PN2 = knm_number:phone_number(N2),
+    [{"Verify carrier name is right"
+     ,?_assertEqual(?CARRIER_LOCAL, knm_phone_number:module_name(PN1))
+     }
+    ,?_assertEqual(undefined, kz_json:get_value(?FEATURE_RENAME_CARRIER, knm_phone_number:doc(PN1)))
+    ,{"Verify carrier name is changed"
+     ,?_assertEqual(<<"knm_telnyx">>, knm_phone_number:module_name(PN2))
+     }
+    ,{"Verify feature is now removed"
+     ,?_assertEqual(undefined, kz_json:get_value(?FEATURE_RENAME_CARRIER, knm_phone_number:doc(PN2)))
+     }
+    ,{"Verify local feature is now set"
+     ,?_assertEqual(false, lists:member(?FEATURE_LOCAL, knm_phone_number:features_list(PN2)))
      }
     ].
