37ff6fe15584a089bc04a791e2869cf95b85d4db
Hesaam Farhang <hesaam@2600hz.com>
2017-03-02 11:22:06 -0800

    HELP-27320: add wht_util:modb to kazoo modb add routin

--- core/kazoo_services/src/kz_services_modb.erl
+++ core/kazoo_services/src/kz_services_modb.erl
@@ -29,8 +29,7 @@ modb(?MATCH_MODB_SUFFIX_ENCODED(_AccountId, _Year, _Month) = AccountMODb) ->
 modb(?MATCH_MODB_SUFFIX_RAW(AccountId, _Year, _Month) = AccountMODb) ->
     ServicesJObj = kz_services:to_json(kz_services:fetch(AccountId)),
     save_services_to_modb(AccountMODb, ServicesJObj, ?SERVICES_BOM),
-    maybe_save_to_previous_modb(AccountMODb, ServicesJObj),
-    wht_util:modb(AccountMODb).
+    maybe_save_to_previous_modb(AccountMODb, ServicesJObj).
 
 -spec save_services_to_modb(ne_binary(), kz_json:object(), ne_binary()) -> 'ok'.
 save_services_to_modb(AccountMODb, ServicesJObj, Id) ->
