d8f0475666a2107124774ab23492ade28a6a0020
Pierre Fenoll <pierrefenoll@gmail.com>
2017-03-07 12:58:27 +0100

    qf-release_4oh: remove dead code

--- core/kazoo_number_manager/src/knm_config.erl
+++ core/kazoo_number_manager/src/knm_config.erl
@@ -1,15 +1,18 @@
 -module(knm_config).
 
 -export([should_permanently_delete/0, should_permanently_delete/1
-        ,released_state/0, released_state/1
-        ,default_locality_url/0, default_locality_url/1
-        ,locality_url/0, locality_url/1
+        ,released_state/0
+        ,locality_url/0
         ,should_age/0
         ]).
 
 -include("knm.hrl").
 
 -define(LOCALITY_CONFIG_CAT, <<"number_manager.locality">>).
+
+-define(DEFAULT_LOCALITY_URL
+       ,kapps_config:get_ne_binary(<<"number_manager.other">>, <<"phonebook_url">>)).
+
 
 -spec should_age() -> boolean().
 should_age() ->
@@ -23,22 +26,9 @@ should_permanently_delete(Default) ->
     kapps_config:get_is_true(?KNM_CONFIG_CAT, <<"should_permanently_delete">>, Default).
 
 -spec released_state() -> ne_binary().
--spec released_state(ne_binary()) -> ne_binary().
 released_state() ->
-    released_state(?NUMBER_STATE_AVAILABLE).
-released_state(Default) ->
-    kapps_config:get_binary(?KNM_CONFIG_CAT, <<"released_state">>, Default).
+    kapps_config:get_ne_binary(?KNM_CONFIG_CAT, <<"released_state">>, ?NUMBER_STATE_AVAILABLE).
 
--spec default_locality_url() -> api_binary().
--spec default_locality_url(api_binary()) -> api_binary().
-default_locality_url() ->
-    default_locality_url('undefined').
-default_locality_url(Default) ->
-    kapps_config:get(<<"number_manager.other">>, <<"phonebook_url">>, Default).
-
--spec locality_url() -> api_binary().
--spec locality_url(api_binary()) -> api_binary().
+-spec locality_url() -> api_ne_binary().
 locality_url() ->
-    locality_url(default_locality_url()).
-locality_url(Default) ->
-    kapps_config:get(?LOCALITY_CONFIG_CAT, <<"url">>, Default).
+    kapps_config:get_ne_binary(?LOCALITY_CONFIG_CAT, <<"url">>, ?DEFAULT_LOCALITY_URL).
