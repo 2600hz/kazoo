d5909a06b72e4da307db2d648d0a8332a6db8f36
Pierre Fenoll <pierrefenoll@gmail.com>
2017-03-21 17:46:14 -0700

    KAZOO-5429: remove pvt CNAM features if they were manually stripped out
    Conflicts:
    	core/kazoo_number_manager/src/knm_phone_number.erl
    	core/kazoo_number_manager/src/providers/knm_providers.erl

--- core/kazoo_number_manager/src/knm_util.erl
+++ core/kazoo_number_manager/src/knm_util.erl
@@ -122,8 +122,7 @@ get_classifier_regex(JObj) ->
 
 -spec fixture(file:filename()) -> string().
 fixture(Filename) ->
-    Priv = code:priv_dir('kazoo_number_manager'),
-    Fixture = filename:join([Priv, "fixtures", Filename]),
+    Fixture = filename:join([code:priv_dir(?APP), "fixtures", Filename]),
     read_fixture(file:read_file(Fixture), Fixture).
 
 read_fixture({'ok', Contents}, _F) ->
