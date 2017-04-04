30deeaf532e0bd6aac7c32e7f272010479479074
Pierre Fenoll <pierrefenoll@gmail.com>
2017-04-03 17:09:12 -0700

    qf-mvcar_4oh: part trois

--- core/kazoo_number_manager/test/knm_number_test.erl
+++ core/kazoo_number_manager/test/knm_number_test.erl
@@ -1,5 +1,5 @@
 %%%-------------------------------------------------------------------
-%%% @copyright (C) 2017, 2600Hz
+%%% @copyright (C) 2016-2017, 2600Hz
 %%% @doc
 %%% @end
 %%% @contributors
@@ -34,16 +34,17 @@ unavailable_tests(ErrorJObj) ->
      }
     ].
 
-available_tests(Number) ->
-    PhoneNumber = knm_number:phone_number(Number),
-    [{"Verify available phone number"
-     ,?_assertEqual(?TEST_AVAILABLE_NUM, knm_phone_number:number(PhoneNumber))
+available_tests(N) ->
+    PN = knm_number:phone_number(N),
+    [?_assertEqual(false, knm_phone_number:is_dirty(PN))
+    ,{"Verify available phone number"
+     ,?_assertEqual(?TEST_AVAILABLE_NUM, knm_phone_number:number(PN))
      }
     ,{"Verify available number module"
-     ,?_assertEqual(?CARRIER_LOCAL, knm_phone_number:module_name(PhoneNumber))
+     ,?_assertEqual(?CARRIER_LOCAL, knm_phone_number:module_name(PN))
      }
     ,{"Verify available number state"
-     ,?_assertEqual(?NUMBER_STATE_AVAILABLE, knm_phone_number:state(PhoneNumber))
+     ,?_assertEqual(?NUMBER_STATE_AVAILABLE, knm_phone_number:state(PN))
      }
     ].
 
@@ -64,27 +65,31 @@ mdn_transitions_test_() ->
     {ok, N2} = knm_number:release(Num, DefaultOptions),
     {ok, N3} = knm_number:reconcile(Num, DefaultOptions),
     {ok, N4} = knm_number:create(?TEST_CREATE_NUM, [{module_name,?CARRIER_MDN}|DefaultOptions]),
-    [{"Verify MDN can move from in_service to in_service"
-     ,?_assertEqual(?NUMBER_STATE_IN_SERVICE, knm_phone_number:state(knm_number:phone_number(N1)))
+    PN1 = knm_number:phone_number(N1),
+    PN2 = knm_number:phone_number(N2),
+    PN3 = knm_number:phone_number(N3),
+    PN4 = knm_number:phone_number(N4),
+    [?_assert(knm_phone_number:is_dirty(PN1))
+    ,{"Verify MDN can move from in_service to in_service"
+     ,?_assertEqual(?NUMBER_STATE_IN_SERVICE, knm_phone_number:state(PN1))
      }
-    ,?_assert(knm_phone_number:is_dirty(knm_number:phone_number(N1)))
+    ,?_assert(knm_phone_number:is_dirty(PN2))
     ,{"Verify releasing MDN results in deletion"
-     ,?_assertEqual(?NUMBER_STATE_DELETED, knm_phone_number:state(knm_number:phone_number(N2)))
+     ,?_assertEqual(?NUMBER_STATE_DELETED, knm_phone_number:state(PN2))
      }
-    ,?_assert(knm_phone_number:is_dirty(knm_number:phone_number(N2)))
+    ,?_assert(knm_phone_number:is_dirty(PN3))
     ,{"Verify MDN can reconcile from in_service to in_service"
-     ,?_assertEqual(?NUMBER_STATE_IN_SERVICE, knm_phone_number:state(knm_number:phone_number(N3)))
+     ,?_assertEqual(?NUMBER_STATE_IN_SERVICE, knm_phone_number:state(PN3))
      }
-    ,?_assert(knm_phone_number:is_dirty(knm_number:phone_number(N3)))
     ,{"Verify MDN cannot be reserved"
      ,?_assertMatch({error,_}, knm_number:reserve(Num, knm_number_options:default()))
      }
+    ,?_assert(knm_phone_number:is_dirty(PN4))
     ,{"Verify MDN creation forces state to in_service"
-     ,?_assertEqual(?NUMBER_STATE_IN_SERVICE, knm_phone_number:state(knm_number:phone_number(N4)))
+     ,?_assertEqual(?NUMBER_STATE_IN_SERVICE, knm_phone_number:state(PN4))
      }
-    ,?_assert(knm_phone_number:is_dirty(knm_number:phone_number(N4)))
     ,{"Verify MDN creation creates local feature"
-     ,?_assertEqual([?FEATURE_LOCAL], knm_phone_number:features_list(knm_number:phone_number(N4)))
+     ,?_assertEqual([?FEATURE_LOCAL], knm_phone_number:features_list(PN4))
      }
     ].
 
@@ -94,28 +99,28 @@ is_mdn_for_mdn_run_test_() ->
     Sudo = knm_number_options:default(),
     Fs = [{fun knm_phone_number:update_doc/2, kz_json:from_list([{<<"*">>,42}])}],
     [{"Verify an mdn_run && knm_mdn number can be updated"
-     ,?_assertMatch({ok,_}, knm_number:update(?TEST_IN_SERVICE_MDN, Fs, [Run|Base]))
+     ,?_assertMatch({ok,_}, knm_number:update_phone_number(?TEST_IN_SERVICE_MDN, Fs, [Run|Base]))
      }
     ,{"Verify an mdn_run && !knm_mdn number cannot be updated"
-     ,?_assertMatch({error,_}, knm_number:update(?TEST_IN_SERVICE_NUM, Fs, [Run|Base]))
+     ,?_assertMatch({error,_}, knm_number:update_phone_number(?TEST_IN_SERVICE_NUM, Fs, [Run|Base]))
      }
     ,{"Verify a !mdn_run && knm_mdn number cannot be updated"
-     ,?_assertMatch({error,_}, knm_number:update(?TEST_IN_SERVICE_MDN, Fs, Base))
+     ,?_assertMatch({error,_}, knm_number:update_phone_number(?TEST_IN_SERVICE_MDN, Fs, Base))
      }
     ,{"Verify a !mdn_run && !knm_mdn number can be updated"
-     ,?_assertMatch({ok,_}, knm_number:update(?TEST_IN_SERVICE_NUM, Fs, Base))
+     ,?_assertMatch({ok,_}, knm_number:update_phone_number(?TEST_IN_SERVICE_NUM, Fs, Base))
      }
     ,{"Verify sudo can update mdn_run && knm_mdn number"
-     ,?_assertMatch({ok,_}, knm_number:update(?TEST_IN_SERVICE_MDN, Fs, [Run|Sudo]))
+     ,?_assertMatch({ok,_}, knm_number:update_phone_number(?TEST_IN_SERVICE_MDN, Fs, [Run|Sudo]))
      }
     ,{"Verify sudo can update mdn_run && !knm_mdn number"
-     ,?_assertMatch({ok,_}, knm_number:update(?TEST_IN_SERVICE_NUM, Fs, [Run|Sudo]))
+     ,?_assertMatch({ok,_}, knm_number:update_phone_number(?TEST_IN_SERVICE_NUM, Fs, [Run|Sudo]))
      }
     ,{"Verify sudo can update !mdn_run && knm_mdn number"
-     ,?_assertMatch({ok,_}, knm_number:update(?TEST_IN_SERVICE_MDN, Fs, Sudo))
+     ,?_assertMatch({ok,_}, knm_number:update_phone_number(?TEST_IN_SERVICE_MDN, Fs, Sudo))
      }
     ,{"Verify sudo can update !mdn_run && !knm_mdn number"
-     ,?_assertMatch({ok,_}, knm_number:update(?TEST_IN_SERVICE_NUM, Fs, Sudo))
+     ,?_assertMatch({ok,_}, knm_number:update_phone_number(?TEST_IN_SERVICE_NUM, Fs, Sudo))
      }
     ].
 
@@ -235,3 +240,93 @@ update_used_by_from_undefined_test_() ->
     ,?_assertEqual(undefined, knm_phone_number:used_by(PN2))
     ,?_assert(knm_phone_number:is_dirty(PN2))
     ].
+
+fix_number_test_() ->
+    Num = ?TEST_OLD5_1_NUM,
+    {ok, N1} = knm_number:get(Num),
+    PN1 = knm_number:phone_number(N1),
+    Doc1 = knm_phone_number:doc(PN1),
+    FeaturesList1 = knm_phone_number:features_list(PN1),
+    [{ok, {Num, N2}}] = fix_number(N1),
+    PN2 = knm_number:phone_number(N2),
+    Doc2 = knm_phone_number:doc(PN2),
+    FeaturesList2 = knm_phone_number:features_list(PN2),
+    [?_assert(not knm_phone_number:is_dirty(PN1))
+    ,?_assertEqual(Num, knm_phone_number:number(PN1))
+    ,?_assertEqual(?RESELLER_ACCOUNT_ID, knm_phone_number:assigned_to(PN1))
+    ,?_assertEqual(<<"knm_bandwidth2">>, knm_phone_number:module_name(PN1))
+    ,?_assertEqual(?NUMBER_STATE_IN_SERVICE, knm_phone_number:state(PN1))
+    ,?_assertNotEqual(undefined, kz_json:get_value(?FEATURE_CNAM, Doc1))
+    ,?_assertNotEqual(undefined, kz_json:get_value(?FEATURE_E911, Doc1))
+    ,?_assertNotEqual(undefined, kz_json:get_value(?LEGACY_DASH_E911, Doc1))
+    ,?_assert(not lists:member(?FEATURE_LOCAL, FeaturesList1))
+    ,?_assert(lists:member(?FEATURE_CNAM_INBOUND, FeaturesList1))
+    ,?_assert(lists:member(?FEATURE_CNAM_OUTBOUND, FeaturesList1))
+    ,?_assert(lists:member(?FEATURE_E911, FeaturesList1))
+    ,?_assert(not lists:member(?LEGACY_DASH_E911, FeaturesList1))
+    ,?_assertEqual(undefined, knm_phone_number:feature(PN1, ?LEGACY_DASH_E911))
+    ,?_assert(kz_json:are_equal(knm_phone_number:feature(PN1, ?FEATURE_E911), kz_json:get_value(?FEATURE_E911, Doc1)))
+    ,?_assert(kz_json:are_equal(knm_phone_number:feature(PN1, ?FEATURE_E911), kz_json:get_value(?LEGACY_DASH_E911, Doc1)))
+    ,?_assertEqual(undefined, knm_phone_number:used_by(PN1))
+    ,?_assert(not knm_phone_number:is_dirty(PN2))
+    ,?_assertEqual(Num, knm_phone_number:number(PN2))
+    ,?_assertEqual(?RESELLER_ACCOUNT_ID, knm_phone_number:assigned_to(PN2))
+    ,?_assertEqual(<<"knm_bandwidth2">>, knm_phone_number:module_name(PN2))
+    ,?_assertEqual(?NUMBER_STATE_IN_SERVICE, knm_phone_number:state(PN2))
+    ,?_assertNotEqual(undefined, kz_json:get_value(?FEATURE_CNAM, Doc2))
+    ,?_assertNotEqual(undefined, kz_json:get_value(?FEATURE_E911, Doc2))
+    ,?_assertNotEqual(undefined, kz_json:get_value(?LEGACY_DASH_E911, Doc2))
+    ,?_assert(not lists:member(?FEATURE_LOCAL, FeaturesList2))
+    ,?_assert(lists:member(?FEATURE_CNAM_INBOUND, FeaturesList2))
+    ,?_assert(lists:member(?FEATURE_CNAM_OUTBOUND, FeaturesList2))
+    ,?_assert(lists:member(?FEATURE_E911, FeaturesList2))
+    ,?_assert(not lists:member(?LEGACY_DASH_E911, FeaturesList2))
+    ,?_assertEqual(undefined, knm_phone_number:feature(PN2, ?LEGACY_DASH_E911))
+    ,?_assert(kz_json:are_equal(knm_phone_number:feature(PN2, ?FEATURE_E911), kz_json:get_value(?FEATURE_E911, Doc2)))
+    ,?_assert(kz_json:are_equal(knm_phone_number:feature(PN2, ?FEATURE_E911), kz_json:get_value(?LEGACY_DASH_E911, Doc2)))
+    ,?_assertEqual(undefined, knm_phone_number:used_by(PN2))
+    ].
+
+fix_number_wrong_used_by_and_dangling_pvt_features_test_() ->
+    {ok, N1} = knm_number:get(?TEST_OLD7_NUM),
+    PN1 = knm_number:phone_number(N1),
+    [{ok, {Num, N2}}] = fix_number(N1),
+    PN2 = knm_number:phone_number(N2),
+    [?_assert(not knm_phone_number:is_dirty(PN1))
+    ,?_assertEqual(?TEST_OLD7_NUM, knm_phone_number:number(PN1))
+    ,?_assertEqual(?CHILD_ACCOUNT_ID, knm_phone_number:assigned_to(PN1))
+    ,?_assertEqual(?CARRIER_LOCAL, knm_phone_number:module_name(PN1))
+    ,?_assertEqual(?NUMBER_STATE_IN_SERVICE, knm_phone_number:state(PN1))
+    ,?_assertEqual(undefined, kz_json:get_value(?FEATURE_CNAM, knm_phone_number:doc(PN1)))
+    ,?_assert(lists:member(?FEATURE_LOCAL, knm_phone_number:features_list(PN1)))
+    ,?_assert(lists:member(?FEATURE_CNAM_INBOUND, knm_phone_number:features_list(PN1)))
+    ,?_assert(lists:member(?FEATURE_CNAM_OUTBOUND, knm_phone_number:features_list(PN1)))
+    ,?_assertEqual(<<"callflow">>, knm_phone_number:used_by(PN1))
+    ,?_assert(knm_phone_number:is_dirty(PN2))
+    ,?_assertEqual(?TEST_OLD7_NUM, knm_phone_number:number(PN2))
+    ,?_assertEqual(?CHILD_ACCOUNT_ID, knm_phone_number:assigned_to(PN2))
+    ,?_assertEqual(?CARRIER_LOCAL, knm_phone_number:module_name(PN2))
+    ,?_assertEqual(?NUMBER_STATE_IN_SERVICE, knm_phone_number:state(PN2))
+    ,?_assertEqual(undefined, kz_json:get_value(?FEATURE_CNAM, knm_phone_number:doc(PN2)))
+    ,?_assert(lists:member(?FEATURE_LOCAL, knm_phone_number:features_list(PN2)))
+    ,?_assert(not lists:member(?FEATURE_CNAM_INBOUND, knm_phone_number:features_list(PN2)))
+    ,?_assert(not lists:member(?FEATURE_CNAM_OUTBOUND, knm_phone_number:features_list(PN2)))
+    ,?_assertEqual(<<"trunkstore">>, knm_phone_number:used_by(PN2))
+    ].
+
+fix_number(N) ->
+    PN = knm_number:phone_number(N),
+    Num = knm_phone_number:number(PN),
+    AuthBy = knm_phone_number:assigned_to(PN),
+    AccountDb = kz_util:format_account_db(AuthBy),
+    %% -- below is verbatim from maintenance module --
+    UsedBy = kazoo_number_manager_maintenance:app_using(knm_converters:normalize(Num), AccountDb),
+    Routines = [{fun knm_phone_number:set_used_by/2, UsedBy}
+               ,fun knm_phone_number:remove_denied_features/1
+               ],
+    Options = [{auth_by, AuthBy}
+              ,{dry_run, false}
+              ,{batch_run, false}
+              ],
+    %% -- above is verbatim from maintenance module --
+    knm_numbers:update_phone_number(N, Routines, Options).
--- core/kazoo_number_manager/test/knm_rename_carrier_test.erl
+++ core/kazoo_number_manager/test/knm_rename_carrier_test.erl
@@ -20,13 +20,16 @@ create_new_number_rename_carrier_test_() ->
     {ok, N1} = knm_number:create(?TEST_CREATE_NUM, Options),
     PN1 = knm_number:phone_number(N1),
     WrongCarrier = kz_json:from_list([{?FEATURE_RENAME_CARRIER, <<"wrong_carrier">>}]),
-    {error, Error2} = knm_number:create(?TEST_CREATE_NUM, props:set_value(public_fields, WrongCarrier, Options)),
+    {ok, N2} = knm_number:create(?TEST_CREATE_NUM, props:set_value(public_fields, WrongCarrier, Options)),
+    PN2 = knm_number:phone_number(N2),
     {error, Error3} = knm_number:create(?TEST_CREATE_NUM, props:set_value(auth_by, ?RESELLER_ACCOUNT_ID, Options)),
-    [{"Verify only admin can create and set carrier"
+    [?_assert(knm_phone_number:is_dirty(PN1))
+    ,{"Verify only admin can create and set carrier"
      ,?_assertEqual(<<"knm_telnyx">>, knm_phone_number:module_name(PN1))
      }
-    ,{"Verify creating number with wrong carrier is invalid"
-     ,?_assertEqual(<<"invalid">>, knm_errors:message(Error2))
+    ,?_assert(knm_phone_number:is_dirty(PN2))
+    ,{"Verify creating number with wrong carrier is actually ok"
+     ,?_assertEqual(<<"knm_wrong_carrier">>, knm_phone_number:module_name(PN2))
      }
     ,{"Verify creating number and setting carrier as non-admin is forbidden"
      ,?_assertEqual(<<"forbidden">>, knm_errors:error(Error3))
@@ -43,11 +46,15 @@ rename_carrier_test_() ->
     JObj1 = kz_json:from_list([{?FEATURE_RENAME_CARRIER, <<"local">>}]),
     {ok, N2} = knm_number:update_phone_number(N1, [{fun knm_phone_number:reset_doc/2, JObj1}]),
     PN2 = knm_number:phone_number(N2),
-    JObj2 = kz_json:from_list([{?FEATURE_RENAME_CARRIER, <<"wrong_carrier">>}]),
+    JObj2 = kz_json:from_list([{?FEATURE_RENAME_CARRIER, <<"blabla">>}]),
+    {ok, N3} = knm_number:update_phone_number(N1, [{fun knm_phone_number:update_doc/2, JObj2}]),
+    PN3 = knm_number:phone_number(N3),
     {ok, N4} = knm_number:update_phone_number(N2, [{fun knm_phone_number:reset_doc/2, kz_json:new()}]),
     PN4 = knm_number:phone_number(N4),
     JObj3 = kz_json:from_list([{?FEATURE_RENAME_CARRIER, <<"telnyx">>}]),
     JObj4 = kz_json:from_list([{?FEATURE_RENAME_CARRIER, <<"gen_carrier">>}]),
+    {ok, N6} = knm_number:update_phone_number(N3, [{fun knm_phone_number:reset_doc/2, JObj4}]),
+    PN6 = knm_number:phone_number(N6),
     [?_assert(knm_phone_number:is_dirty(PN1))
     ,{"Verify carrier name is right"
      ,?_assertEqual(<<"knm_telnyx">>, knm_phone_number:module_name(PN1))
@@ -63,14 +70,19 @@ rename_carrier_test_() ->
     ,{"Verify feature is now removed"
      ,?_assertEqual(undefined, kz_json:get_value(?FEATURE_RENAME_CARRIER, knm_phone_number:doc(PN2)))
      }
+    ,?_assert(knm_phone_number:is_dirty(PN3))
+    ,{"Verify carrier name is changed"
+     ,?_assertEqual(<<"knm_blabla">>, knm_phone_number:module_name(PN3))
+     }
+    ,{"Verify local feature is not set"
+     ,?_assert(not lists:member(?FEATURE_LOCAL, knm_phone_number:features_list(PN3)))
+     }
+    ,{"Verify feature is now removed"
+     ,?_assertEqual(undefined, kz_json:get_value(?FEATURE_RENAME_CARRIER, knm_phone_number:doc(PN3)))
+     }
     ,?_assert(not knm_phone_number:is_dirty(PN4))
     ,{"Verify local feature is still set"
      ,?_assertEqual(true, lists:member(?FEATURE_LOCAL, knm_phone_number:features_list(PN4)))
-     }
-    ,{"Verify setting wrong carrier is forbidden"
-     ,?_assertThrow({error, invalid, _N, <<"'wrong_carrier' is not known by the system">>}
-                   ,knm_number:update_phone_number(N2, [{fun knm_phone_number:reset_doc/2, JObj2}])
-                   )
      }
     ,{"Verify carrier name is still changed"
      ,?_assertEqual(?CARRIER_LOCAL, knm_phone_number:module_name(PN4))
@@ -84,13 +96,15 @@ rename_carrier_test_() ->
                                                   )
                    )
      }
-    ,{"Verify setting not a carrier is invalid"
-     ,?_assertThrow({error, invalid, _, _}
-                   ,knm_number:update_phone_number(N2
-                                                  ,[{fun knm_phone_number:reset_doc/2, JObj4}]
-                                                  ,[{auth_by, ?MASTER_ACCOUNT_ID}]
-                                                  )
-                   )
+    ,?_assert(knm_phone_number:is_dirty(PN6))
+    ,{"Verify carrier name is changed"
+     ,?_assertEqual(<<"knm_gen_carrier">>, knm_phone_number:module_name(PN6))
+     }
+    ,{"Verify local feature is still not set"
+     ,?_assert(not lists:member(?FEATURE_LOCAL, knm_phone_number:features_list(PN6)))
+     }
+    ,{"Verify feature is now removed"
+     ,?_assertEqual(undefined, kz_json:get_value(?FEATURE_RENAME_CARRIER, knm_phone_number:doc(PN6)))
      }
     ].
 
@@ -113,9 +127,82 @@ rename_from_local_test_() ->
     ,{"Verify feature is now removed"
      ,?_assertEqual(undefined, kz_json:get_value(?FEATURE_RENAME_CARRIER, knm_phone_number:doc(PN2)))
      }
-    ,{"Verify local feature is now set"
+    ,{"Verify local feature is now unset"
      ,?_assertEqual(false, lists:member(?FEATURE_LOCAL, knm_phone_number:features_list(PN2)))
      }
+    ].
+
+rename_to_local_with_external_features_test_() ->
+    Options = [{auth_by, ?MASTER_ACCOUNT_ID}
+              ],
+    {ok, N1} = knm_number:get(?TEST_VITELITY_NUM, Options),
+    PN1 = knm_number:phone_number(N1),
+    JObj1 = kz_json:from_list([{?FEATURE_RENAME_CARRIER, <<"local">>}]),
+    #{ok := [N2]} = knm_numbers:update([N1], [{fun knm_phone_number:update_doc/2, JObj1}], Options),
+    PN2 = knm_number:phone_number(N2),
+    [?_assert(not knm_phone_number:is_dirty(PN1))
+    ,{"Verify carrier name is right"
+     ,?_assertEqual(<<"knm_vitelity">>, knm_phone_number:module_name(PN1))
+     }
+    ,?_assertEqual(undefined, kz_json:get_value(?FEATURE_RENAME_CARRIER, knm_phone_number:doc(PN1)))
+    ,{"Verify local feature is not set"
+     ,?_assert(not lists:member(?FEATURE_LOCAL, knm_phone_number:features_list(PN1)))
+     }
+    ,?_assertNotEqual(undefined, kz_json:get_value(?FEATURE_CNAM, knm_phone_number:doc(PN1)))
+    ,?_assert(lists:member(?FEATURE_CNAM_INBOUND, knm_phone_number:features_list(PN1)))
+    ,?_assert(lists:member(?FEATURE_CNAM_OUTBOUND, knm_phone_number:features_list(PN1)))
+    ,?_assert(lists:member(?FEATURE_PREPEND, knm_phone_number:features_list(PN1)))
+    ,?_assert(knm_phone_number:is_dirty(PN2))
+    ,{"Verify carrier name is changed"
+     ,?_assertEqual(?CARRIER_LOCAL, knm_phone_number:module_name(PN2))
+     }
+    ,{"Verify local feature is now set"
+     ,?_assert(lists:member(?FEATURE_LOCAL, knm_phone_number:features_list(PN2)))
+     }
+    ,{"Verify feature is now removed"
+     ,?_assertEqual(undefined, kz_json:get_value(?FEATURE_RENAME_CARRIER, knm_phone_number:doc(PN2)))
+     }
+    ,?_assertEqual(undefined, kz_json:get_value(?FEATURE_CNAM, knm_phone_number:doc(PN2)))
+    ,?_assert(not lists:member(?FEATURE_CNAM, knm_phone_number:features_list(PN2)))
+    ,?_assert(not lists:member(?FEATURE_CNAM_INBOUND, knm_phone_number:features_list(PN2)))
+    ,?_assert(not lists:member(?FEATURE_CNAM_OUTBOUND, knm_phone_number:features_list(PN2)))
+    ,?_assert(lists:member(?FEATURE_PREPEND, knm_phone_number:features_list(PN2)))
+    ].
+
+rename_to_nonlocal_with_external_features_test_() ->
+    Options = [{auth_by, ?MASTER_ACCOUNT_ID}
+              ],
+    {ok, N1} = knm_number:get(?TEST_VITELITY_NUM, Options),
+    PN1 = knm_number:phone_number(N1),
+    JObj1 = kz_json:from_list([{?FEATURE_RENAME_CARRIER, <<"bandwidth">>}]),
+    #{ok := [N2]} = knm_numbers:update([N1], [{fun knm_phone_number:update_doc/2, JObj1}], Options),
+    PN2 = knm_number:phone_number(N2),
+    [?_assert(not knm_phone_number:is_dirty(PN1))
+    ,{"Verify carrier name is right"
+     ,?_assertEqual(<<"knm_vitelity">>, knm_phone_number:module_name(PN1))
+     }
+    ,{"Verify local feature is not set"
+     ,?_assert(not lists:member(?FEATURE_LOCAL, knm_phone_number:features_list(PN1)))
+     }
+    ,?_assertEqual(undefined, kz_json:get_value(?FEATURE_RENAME_CARRIER, knm_phone_number:doc(PN1)))
+    ,?_assertNotEqual(undefined, kz_json:get_value(?FEATURE_CNAM, knm_phone_number:doc(PN1)))
+    ,?_assert(lists:member(?FEATURE_CNAM_INBOUND, knm_phone_number:features_list(PN1)))
+    ,?_assert(lists:member(?FEATURE_CNAM_OUTBOUND, knm_phone_number:features_list(PN1)))
+    ,?_assert(lists:member(?FEATURE_PREPEND, knm_phone_number:features_list(PN1)))
+    ,?_assert(knm_phone_number:is_dirty(PN2))
+    ,{"Verify carrier name is changed"
+     ,?_assertEqual(<<"knm_bandwidth2">>, knm_phone_number:module_name(PN2))
+     }
+    ,{"Verify local feature is not set"
+     ,?_assert(not lists:member(?FEATURE_LOCAL, knm_phone_number:features_list(PN2)))
+     }
+    ,{"Verify feature is now removed"
+     ,?_assertEqual(undefined, kz_json:get_value(?FEATURE_RENAME_CARRIER, knm_phone_number:doc(PN2)))
+     }
+    ,?_assertNotEqual(undefined, kz_json:get_value(?FEATURE_CNAM, knm_phone_number:doc(PN2)))
+    ,?_assert(lists:member(?FEATURE_CNAM_INBOUND, knm_phone_number:features_list(PN2)))
+    ,?_assert(lists:member(?FEATURE_CNAM_OUTBOUND, knm_phone_number:features_list(PN2)))
+    ,?_assert(lists:member(?FEATURE_PREPEND, knm_phone_number:features_list(PN2)))
     ].
 
 available_features_test_() ->
@@ -123,10 +210,18 @@ available_features_test_() ->
     ,?_assert(is_feature_available(?TEST_IN_SERVICE_NUM, [{auth_by, ?KNM_DEFAULT_AUTH_BY}]))
     ,?_assert(is_feature_available(?TEST_IN_SERVICE_NUM, [{auth_by, ?MASTER_ACCOUNT_ID}]))
     ,?_assert(not is_feature_available(?TEST_IN_SERVICE_NUM, [{auth_by, ?RESELLER_ACCOUNT_ID}]))
-    ,?_assert(not is_feature_available(?BW_EXISTING_DID, []))
-    ,?_assert(not is_feature_available(?BW_EXISTING_DID, [{auth_by, ?KNM_DEFAULT_AUTH_BY}]))
-    ,?_assert(not is_feature_available(?BW_EXISTING_DID, [{auth_by, ?MASTER_ACCOUNT_ID}]))
+    ,?_assert(is_feature_available(?BW_EXISTING_DID, []))
+    ,?_assert(is_feature_available(?BW_EXISTING_DID, [{auth_by, ?KNM_DEFAULT_AUTH_BY}]))
+    ,?_assert(is_feature_available(?BW_EXISTING_DID, [{auth_by, ?MASTER_ACCOUNT_ID}]))
     ,?_assert(not is_feature_available(?BW_EXISTING_DID, [{auth_by, ?RESELLER_ACCOUNT_ID}]))
+    ,?_assert(is_feature_available(?TEST_OLD5_1_NUM, []))
+    ,?_assert(is_feature_available(?TEST_OLD5_1_NUM, [{auth_by, ?KNM_DEFAULT_AUTH_BY}]))
+    ,?_assert(is_feature_available(?TEST_OLD5_1_NUM, [{auth_by, ?MASTER_ACCOUNT_ID}]))
+    ,?_assert(not is_feature_available(?TEST_OLD5_1_NUM, [{auth_by, ?RESELLER_ACCOUNT_ID}]))
+    ,?_assert(is_feature_available(?TEST_VITELITY_NUM, []))
+    ,?_assert(is_feature_available(?TEST_VITELITY_NUM, [{auth_by, ?KNM_DEFAULT_AUTH_BY}]))
+    ,?_assert(is_feature_available(?TEST_VITELITY_NUM, [{auth_by, ?MASTER_ACCOUNT_ID}]))
+    ,?_assert(not is_feature_available(?TEST_VITELITY_NUM, [{auth_by, ?RESELLER_ACCOUNT_ID}]))
     ].
 
 is_feature_available(Num, Options) ->
