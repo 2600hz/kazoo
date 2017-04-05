f39567dac6daff1e0091f9935daef240f3c6cb81
Pierre Fenoll <pierrefenoll@gmail.com>
2017-04-05 15:44:42 -0700

    HELP-29314: formatting

--- core/kazoo_number_manager/test/knm_move_number_test.erl
+++ core/kazoo_number_manager/test/knm_move_number_test.erl
@@ -32,23 +32,15 @@ move_changing_public_fields_test_() ->
     {ok, N} = knm_number:move(?TEST_AVAILABLE_NUM, ?RESELLER_ACCOUNT_ID, Options),
     [?_assert(knm_phone_number:is_dirty(knm_number:phone_number(N)))
     ,{"verify a public key is set"
-     ,?_assertEqual(<<"my string">>
-                   ,kz_json:get_value(Key, knm_number:to_public_json(N0))
-                   )
+     ,?_assertEqual(<<"my string">>, public_value(Key, N0))
      }
     ,{"verify that key got updated"
-     ,?_assertEqual(42
-                   ,kz_json:get_value(Key, knm_number:to_public_json(N))
-                   )
+     ,?_assertEqual(42, public_value(Key, N))
      }
     ,{"verify another key was added"
-     ,?_assertEqual(<<"bla">>
-                   ,kz_json:get_value(<<"a">>, knm_number:to_public_json(N))
-                   )
+     ,?_assertEqual(<<"bla">>, public_value(<<"a">>, N))
      }
     ,{"verify that that other key is really new"
-     ,?_assertEqual(undefined
-                   ,kz_json:get_value(<<"a">>, knm_number:to_public_json(N0))
-                   )
+     ,?_assertEqual(undefined, public_value(<<"a">>, N0))
      }
     ].
