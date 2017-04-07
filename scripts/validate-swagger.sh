3ad40012892e472770495c50ea835c3c4f8aa9fc
Pierre Fenoll <pierrefenoll@gmail.com>
2017-04-07 14:40:25 -0700

    qf-swag_validation: properly count Swagger errors (#3543)

--- scripts/validate-swagger.sh
+++ scripts/validate-swagger.sh
@@ -9,7 +9,7 @@ URL='http://online.swagger.io/validator/debug?url=https://raw.githubusercontent.
 tmp=$RANDOM.json
 curl -o $tmp "$URL" || exit 2
 "$(dirname "$0")"/format-json.sh $tmp || (rm $tmp && exit 3)
-errors=$(cat $tmp | python2 -c 'import sys, json; print len(json.load(sys.stdin))')
+errors=$(cat $tmp | python2 -c 'import sys, json; print len(json.load(sys.stdin)["schemaValidationMessages"])')
 [[ $errors -ne 0 ]] && echo Swagger file validation errors: $errors && cat $tmp
 rm $tmp
 
