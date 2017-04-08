0e06c89bd0aaeb6bce98d0db595435fa6c74007c
bitbashing <bitbashing@gmail.com>
2017-04-07 17:33:27 -0700

    Kazoo 5470 master (#3549)
    * KAZOO-5470: make the archive optional when purging modbs
    
    Conflicts:
    	core/kazoo_modb/src/kazoo_modb_maintenance.erl
    
    * KAZOO-5470: formating tweak

--- core/kazoo_modb/src/kazoo_modb_maintenance.erl
+++ core/kazoo_modb/src/kazoo_modb_maintenance.erl
@@ -7,7 +7,9 @@
 -module(kazoo_modb_maintenance).
 
 -export([delete_modbs/1
-        ,archive_modbs/0
+        ,delete_modbs/2
+        ]).
+-export([archive_modbs/0
         ,archive_modbs/1
         ]).
 -export([current_rollups/0]).
@@ -41,40 +43,44 @@
                     ]).
 
 -spec delete_modbs(ne_binary()) -> 'ok' | 'no_return'.
--spec delete_modbs(ne_binary() | kz_year(), ne_binary() | kz_month()) -> 'ok' | 'no_return'.
+-spec delete_modbs(ne_binary(), boolean() | ne_binary()) -> 'ok' | 'no_return'.
+-spec delete_modbs(ne_binary() | kz_year(), ne_binary() | kz_month(), boolean()) -> 'ok' | 'no_return'.
 delete_modbs(Period) ->
+    delete_modbs(Period, 'true').
+
+delete_modbs(Period, ShouldArchive) ->
     Regex = <<"(2[0-9]{3})(0[1-9]|1[0-2])">>,
     case re:run(Period, Regex, [{'capture', 'all', 'binary'}]) of
         {'match', [_Full, Year, Month]} ->
-            delete_modbs(Year, Month);
+            delete_modbs(Year, Month, kz_term:is_true(ShouldArchive));
         'nomatch' ->
             io:format("period '~s' does not match YYYYMM format~n", [Period])
     end.
 
-delete_modbs(?NE_BINARY=Year, Month) ->
-    delete_modbs(kz_term:to_integer(Year), Month);
-delete_modbs(Year, ?NE_BINARY=Month) ->
-    delete_modbs(Year, kz_term:to_integer(Month));
-delete_modbs(Year, Month) when is_integer(Year),
-                               is_integer(Month),
-                               Year > 2000,
-                               Year < 2999,
-                               Month > 0,
-                               Month < 13 ->
+delete_modbs(<<_/binary>> = Year, Month, ShouldArchive) ->
+    delete_modbs(kz_term:to_integer(Year), Month, ShouldArchive);
+delete_modbs(Year, <<_/binary>> = Month, ShouldArchive) ->
+    delete_modbs(Year, kz_term:to_integer(Month), ShouldArchive);
+delete_modbs(Year, Month, ShouldArchive) when is_integer(Year),
+                                              is_integer(Month),
+                                              Year > 2000,
+                                              Year < 2999,
+                                              Month > 0,
+                                              Month < 13 ->
     case erlang:date() of
         {Year, Month, _} ->
             io:format("request to delete the current MODB (~p~p) denied~n", [Year, Month]);
         {CurrYear, CurrMonth, _} when (CurrYear * 12 + CurrMonth) > (Year * 12 + Month) ->
             io:format("deleting all MODBs equal to or older than ~p/~p~n", [Year, Month]),
-            delete_older_modbs(Year, Month, kapps_util:get_all_account_mods());
+            delete_older_modbs(Year, Month, kapps_util:get_all_account_mods(), ShouldArchive);
         {_CurrYear, _CurrMonth, _} ->
             io:format("request to delete future MODBs (~p~p) denied~n", [Year, Month])
     end.
 
--spec delete_older_modbs(kz_year(), kz_month(), ne_binaries()) -> 'no_return'.
-delete_older_modbs(Year, Month, AccountModbs) ->
+-spec delete_older_modbs(kz_year(), kz_month(), ne_binaries(), boolean()) -> 'no_return'.
+delete_older_modbs(Year, Month, AccountModbs, ShouldArchive) ->
     Months = (Year * 12) + Month,
-    _ = [delete_modb(AccountModb) || AccountModb <- AccountModbs, should_delete(AccountModb, Months)],
+    _ = [delete_modb(AccountModb, ShouldArchive) || AccountModb <- AccountModbs, should_delete(AccountModb, Months)],
     'no_return'.
 
 -spec should_delete(ne_binary(), pos_integer()) -> boolean().
@@ -82,11 +88,14 @@ should_delete(AccountModb, Months) ->
     {_AccountId, ModYear, ModMonth} = kazoo_modb_util:split_account_mod(AccountModb),
     ((ModYear * 12) + ModMonth) =< Months.
 
--spec delete_modb(ne_binary()) -> 'ok'.
-delete_modb(?MATCH_MODB_SUFFIX_UNENCODED(_,_,_) = AccountModb) ->
-    delete_modb(kz_util:format_account_db(AccountModb));
-delete_modb(?MATCH_MODB_SUFFIX_ENCODED(_,_,_) = AccountModb) ->
-    'ok' = kz_datamgr:db_archive(AccountModb),
+-spec delete_modb(ne_binary(), boolean()) -> 'ok'.
+delete_modb(?MATCH_MODB_SUFFIX_UNENCODED(_,_,_) = AccountModb, ShouldArchive) ->
+    delete_modb(kz_util:format_account_db(AccountModb), ShouldArchive);
+delete_modb(?MATCH_MODB_SUFFIX_ENCODED(_,_,_) = AccountModb, ShouldArchive) ->
+    'ok' = case ShouldArchive of
+               'true' -> kz_datamgr:db_archive(AccountModb);
+               'false' -> io:format("deleting database ~s~n", [AccountModb])
+           end,
     _Deleted = kz_datamgr:db_delete(AccountModb),
     io:format("    deleted: ~p~n", [_Deleted]),
     timer:sleep(5 * ?MILLISECONDS_IN_SECOND).
