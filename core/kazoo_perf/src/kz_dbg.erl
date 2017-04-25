07926f67ecd39d8401b94044a18b81ee50e7821b
James Aimonetti <james.aimonetti@gmail.com>
2017-04-25 18:03:26 +0000

    add dbg helper

---
+++ core/kazoo_perf/src/kz_dbg.erl
@@ -0,0 +1,18 @@
+-module(kz_dbg).
+
+-export([debug_call/3]).
+
+-spec debug_call(atom(), atom(), [any()]) -> any().
+debug_call(M, F, Args) ->
+    dbg:start(),
+
+    dbg:tracer(),
+
+    dbg:tpl(M, [{'_', [], [$_]}]),
+    dbg:p(all, c),
+
+    Result = apply(M, F, Args),
+
+    dbg:stop_clear(),
+    dbg:stop(),
+    Result.
