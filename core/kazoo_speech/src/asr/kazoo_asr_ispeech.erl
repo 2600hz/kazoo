5e8ddb04d7f4c5e2e21b873507e30776b34ee6c8
James Aimonetti <james.aimonetti@gmail.com>
2017-04-25 00:04:50 +0000

    add behaviour

--- core/kazoo_speech/src/asr/kazoo_asr_ispeech.erl
+++ core/kazoo_speech/src/asr/kazoo_asr_ispeech.erl
@@ -1,4 +1,5 @@
 -module(kazoo_asr_ispeech).
+-behaviour(gen_asr_provider).
 
 -export([freeform/4
         ,commands/5
@@ -6,10 +7,11 @@
 
 -include("kazoo_speech.hrl").
 
-
+-spec default_url() -> ne_binary().
 default_url() ->
     kapps_config:get_ne_binary(?MOD_CONFIG_CAT, <<"asr_url">>, <<"http://api.ispeech.org/api/json">>).
 
+-spec default_api_key() -> binary().
 default_api_key() ->
     kapps_config:get_binary(?MOD_CONFIG_CAT, <<"asr_api_key">>, <<>>).
 
@@ -30,31 +32,6 @@ freeform(Content, ContentType, Locale, Options) ->
     lager:debug("asr req body: ~s", [Body]),
 
     handle_response(make_request(BaseUrl, Headers, Body, Options)).
-
-make_request(BaseUrl, Headers, Body, Opts) ->
-    case props:get_value('receiver', Opts) of
-        Pid when is_pid(Pid) ->
-            HTTPOptions = props:delete('receiver', Opts),
-            lager:debug("streaming response to ~p", [Pid]),
-            kz_http:async_req(Pid, 'post', kz_term:to_list(BaseUrl), Headers, Body, HTTPOptions);
-        _ ->
-            HTTPOptions = props:delete('receiver', Opts),
-            kz_http:post(kz_term:to_list(BaseUrl), Headers, Body, HTTPOptions)
-    end.
-
-handle_response({'error', _R}=E) ->
-    lager:debug("asr failed with error ~p", [_R]),
-    E;
-handle_response({'http_req_id', ReqID}) ->
-    lager:debug("streaming response ~p to provided receiver", [ReqID]),
-    {'ok', ReqID};
-handle_response({'ok', 200, _Headers, Content2}) ->
-    lager:debug("ASR of media succeeded: ~s", [Content2]),
-    {'ok', kz_json:decode(Content2)};
-handle_response({'ok', _Code, _Hdrs, Content2}) ->
-    lager:debug("asr of media failed with code ~p", [_Code]),
-    lager:debug("resp: ~s", [Content2]),
-    {'error', 'asr_provider_failure', kz_json:decode(Content2)}.
 
 -spec commands(ne_binary(), ne_binaries(), ne_binary(), ne_binary(), kz_proplist()) -> provider_return().
 commands(Bin, Commands, ContentType, Locale, Opts) ->
@@ -80,3 +57,30 @@ commands(Bin, Commands, ContentType, Locale, Opts) ->
     lager:debug("req body: ~s", [Body]),
 
     handle_response(make_request(BaseUrl, Headers, Body, Opts)).
+
+-spec make_request(ne_binary(), kz_proplist(), iolist(), kz_proplist()) -> kz_http:ret().
+make_request(BaseUrl, Headers, Body, Opts) ->
+    case props:get_value('receiver', Opts) of
+        Pid when is_pid(Pid) ->
+            HTTPOptions = props:delete('receiver', Opts),
+            lager:debug("streaming response to ~p", [Pid]),
+            kz_http:async_req(Pid, 'post', kz_term:to_list(BaseUrl), Headers, Body, HTTPOptions);
+        _ ->
+            HTTPOptions = props:delete('receiver', Opts),
+            kz_http:post(kz_term:to_list(BaseUrl), Headers, Body, HTTPOptions)
+    end.
+
+-spec handle_response(kz_http:ret()) -> asr_resp().
+handle_response({'error', _R}=E) ->
+    lager:debug("asr failed with error ~p", [_R]),
+    E;
+handle_response({'http_req_id', ReqID}) ->
+    lager:debug("streaming response ~p to provided receiver", [ReqID]),
+    {'ok', ReqID};
+handle_response({'ok', 200, _Headers, Content2}) ->
+    lager:debug("ASR of media succeeded: ~s", [Content2]),
+    {'ok', kz_json:decode(Content2)};
+handle_response({'ok', _Code, _Hdrs, Content2}) ->
+    lager:debug("asr of media failed with code ~p", [_Code]),
+    lager:debug("resp: ~s", [Content2]),
+    {'error', 'asr_provider_failure', kz_json:decode(Content2)}.
