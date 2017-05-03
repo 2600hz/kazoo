a840bde14b98008e1194c05c3ba2d15442c5b58c
Pierre Fenoll <pierrefenoll@gmail.com>
2017-05-03 15:47:59 -0700

    rel-migrate: noexec

--- scripts/check-release-startup.sh
+++ scripts/check-release-startup.sh
@@ -9,7 +9,7 @@ rel=${REL:-kazoo_apps}  # kazoo_apps | ecallmgr | ...
 echo "Checking release startup with node $rel..."
 
 sup_() {
-    RELX_REPLACE_OS_VARS=true KZname="-name $rel" exec $PWD/_rel/kazoo/bin/kazoo escript lib/sup-*/priv/sup.escript "$*"
+    RELX_REPLACE_OS_VARS=true KZname="-name $rel" $PWD/_rel/kazoo/bin/kazoo escript lib/sup-*/priv/sup.escript "$*"
 }
 
 script() {
