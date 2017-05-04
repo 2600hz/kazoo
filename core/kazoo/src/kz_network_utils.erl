01d2e331e121ba2c23395b9e2f7487f7cb7cae14
Hesaam Farhang <hesaam@2600hz.com>
2017-05-04 12:15:07 -0700

    make fmt

--- core/kazoo/src/kz_network_utils.erl
+++ core/kazoo/src/kz_network_utils.erl
@@ -19,7 +19,7 @@
 -export([to_cidr/1
         ,to_cidr/2
         ,verify_cidr/2
-%%        ,expand_cidr/1
+         %%        ,expand_cidr/1
         ]).
 -export([find_nameservers/1
         ,find_nameservers/2
