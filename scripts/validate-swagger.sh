57b10e27a4aed546ff22a5e27d8bc8ca5b87dbcf
Pierre Fenoll <pierrefenoll@gmail.com>
2017-05-08 16:37:50 -0700

    KAZOO-5507: validate file even locally

--- scripts/validate-swagger.sh
+++ scripts/validate-swagger.sh
@@ -2,14 +2,11 @@
 
 # Validate Swagger file using online validator
 
-branch=${1:-$CIRCLE_BRANCH}
-[[ "$branch" == '' ]] && branch=master
-echo "Checking Swagger file in branch $branch"
-
-URL='http://online.swagger.io/validator/debug?url=https://raw.githubusercontent.com/2600hz/kazoo/'$branch'/applications/crossbar/priv/api/swagger.json'
+URL='http://online.swagger.io/validator/debug'
+SWAGGER=applications/crossbar/priv/api/swagger.json
 
 tmp=$RANDOM.json
-curl -o $tmp "$URL" || exit 2
+curl -o $tmp "$URL" -X POST -d @$SWAGGER -H 'Content-Type:application/json' || exit 2
 "$(dirname "$0")"/format-json.sh $tmp || (rm $tmp && exit 3)
 errors=$(cat $tmp | python2 -c 'import sys, json; print len(json.load(sys.stdin)["schemaValidationMessages"])')
 [[ $errors -ne 0 ]] && echo Swagger file validation errors: $errors && cat $tmp
