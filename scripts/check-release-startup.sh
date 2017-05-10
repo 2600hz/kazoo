b5e4b92083b5405d83325d1c687a5df4cd340ab7
Pierre Fenoll <pierrefenoll@gmail.com>
2017-05-10 09:11:33 -0700

    rel-migrate_4oh: test release startup better + log sup usage (#3673)
    * rel-migrate: test release migration function (#3666)
    
    * rel-migrate: check return values
    
    * show sup command as it is run
    
    * error when error.log is non-empty
    
    * rel-migrate: introduce sup:in_kazoo

--- scripts/check-release-startup.sh
+++ scripts/check-release-startup.sh
@@ -1,25 +1,43 @@
-#!/bin/bash
+#!/bin/bash -e
 
 [[ ! -d _rel ]] && echo 'Cannot find _rel/ Is the release built?' && exit -1
 
-echo 'Checking the startup of the release...'
-
 rel=${REL:-kazoo_apps}  # kazoo_apps | ecallmgr | ...
-[[ $rel != *@* ]] && rel=$rel@127.0.0.1
-
+[[ $rel != *@* ]] && rel=$rel@$(hostname -f)
 [[ $rel != kazoo_apps* ]] && export KAZOO_APPS='ecallmgr'
 
-function sup_() {
-    local M=$1; shift
-    local F=$1; shift
-    declare -a a=()
-    for arg in "$@"; do a+=( '<<"'"$arg"'">>' ); done
-    IFS=, eval 'A=${a[*]}'
-    erl -noshell -setcookie change_me -name sup_$RANDOM@${rel##*@} -eval "ok = rpc:call('$rel', $M, $F, [$A])." -s init stop
+echo "Checking release startup with node $rel..."
+
+sup_() {
+    RELX_REPLACE_OS_VARS=true KZname="-name $rel" $PWD/_rel/kazoo/bin/kazoo escript lib/sup-*/priv/sup.escript "$*"
 }
 
-sleep 360 && sup_ crossbar_maintenance create_account 'compte_maitre' 'royaume' 'superduperuser' 'pwd!' &
-sleep 440 && sup_ init stop &
+script() {
+    sup_ crossbar_maintenance create_account 'compte_maitre' 'royaume' 'superduperuser' 'pwd!'
+    sleep 3
+    sup_ kapps_maintenance migrate
+    sleep 3
+    sup_ kapps_maintenance migrate_to_4_0
+    sleep 3
+    sup_ init stop
+}
 
+sleep 240 && script &
 export KAZOO_CONFIG=$PWD/rel/ci-config.ini
 REL=$rel make release
+code=$?
+
+if [[ -f erl_crash.dump ]]; then
+    echo A crash dump was generated!
+    code=3
+fi
+error_log=$PWD/_rel/kazoo/log/error.log
+echo
+echo Error log:
+cat $error_log
+if [[ $(grep -c -v -F 'exit with reason shutdown' $error_log) -gt 0 ]]; then
+    echo
+    echo "Found errors in $error_log"
+    code=4
+fi
+exit $code
