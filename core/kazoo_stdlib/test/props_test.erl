14b24f1b16c84c0cfab6d2d6a27d66386e126246
James Aimonetti <james.aimonetti@gmail.com>
2017-05-12 16:55:17 +0000

    constrained testing of proplists

--- core/kazoo_stdlib/test/props_test.erl
+++ core/kazoo_stdlib/test/props_test.erl
@@ -90,18 +90,73 @@ is_defined_test_() ->
 
 run_proper_test_() ->
     {"Runs props PropEr tests"
-    ,{'timeout'
-     ,10000
-     ,[{atom_to_list(F)
+    ,[{'timeout'
+      ,10000
+      ,{atom_to_list(F)
        ,fun() ->
-                ?assert(proper:quickcheck(?MODULE:F(), [{'to_file', 'user'}]))
+                ?assert(proper:quickcheck(?MODULE:F(), [{'to_file', 'user'}, 500]))
         end
        }
-       || {F, 0} <- ?MODULE:module_info('exports'),
-          F > 'prop_',
-          F < 'prop`'
-      ]
-     }
+      }
+      || {F, 0} <- ?MODULE:module_info('exports'),
+         F > 'prop_',
+         F < 'prop`'
+     ]
     }.
+
+prop_set_value() ->
+    ?FORALL({KV, Before, After}
+           ,{test_property(), test_proplist(), test_proplist()}
+           ,?WHENFAIL(?debugFmt("failed: props:is_defined(~p, ~p ++ props:set_value(~p, ~p)).~n", [KV, Before, KV, After])
+                     ,props:is_defined(KV, Before ++ props:set_value(KV, After))
+                     )
+           ).
+
+prop_set_values() ->
+    ?FORALL({KVs, Before, After}
+           ,{list(test_property()), test_proplist(), test_proplist()}
+           ,?WHENFAIL(?debugFmt("Props = ~p ++ props:set_values(~p, ~p)~n", [Before, KVs, After])
+                     ,begin
+                          Props = Before ++ props:set_values(KVs, After),
+                          lists:all(fun(KV) -> props:is_defined(KV, Props) end
+                                   ,KVs
+                                   )
+                      end
+                     )
+           ).
+
+prop_get_value() ->
+    ?FORALL({Props, KV}
+           ,test_proplist_and_kv()
+           ,begin
+                K = case is_tuple(KV) of 'true' -> element(1, KV); 'false' -> KV end,
+                V = case is_tuple(KV) of 'true' -> element(2, KV); 'false' -> 'true' end,
+                ?WHENFAIL(?debugFmt("~p = props:get_value(~p, ~p).~n"
+                                   ,[V, K, Props]
+                                   )
+                         ,V =:= props:get_value(K, Props)
+                         )
+            end
+           ).
+
+test_proplist() ->
+    list(test_property()).
+
+test_property() ->
+    oneof([test_key()
+          ,{test_key(), any()}
+          ]).
+
+test_key() ->
+    oneof([atom(), binary()]).
+
+test_proplist_and_kv() ->
+    ?LET(Props
+        ,?SUCHTHAT(UniqueProps
+                  ,?LET(GenProps, non_empty(test_proplist()), props:unique(GenProps))
+                  ,is_list(UniqueProps)
+                  )
+        ,{Props, elements(Props)}
+        ).
 
 -endif.
