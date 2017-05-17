2002991c9c8a4e929f82899ae60629db184845e3
Pierre Fenoll <pierrefenoll@gmail.com>
2017-05-17 16:16:08 -0700

    KAZOO-5492: crossbar does not directly rely on this exchange

--- applications/crossbar/src/crossbar_app.erl
+++ applications/crossbar/src/crossbar_app.erl
@@ -52,7 +52,6 @@ declare_exchanges() ->
     _ = kapi_presence:declare_exchanges(),
     _ = kapi_registration:declare_exchanges(),
     _ = kapi_resource:declare_exchanges(),
-    _ = kapi_switch:declare_exchanges(),
     _ = kapi_sysconf:declare_exchanges(),
     _ = kapi_call:declare_exchanges(),
     _ = kapi_dialplan:declare_exchanges(),
