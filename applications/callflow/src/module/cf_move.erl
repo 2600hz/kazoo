670f7158d08c03f75e1b360bdecf96cf86eecb46
Pierre Fenoll <pierrefenoll@gmail.com>
2017-05-31 21:34:48 +0200

    HELP-29745: fix MOVEing call to a user's other device (#3772)
    * HELP-29745: more logging
    
    * HELP-29745: have pickup be blocking so that hangup cannot race with it
    
    * HELP-29745: fix typo

--- applications/callflow/src/module/cf_move.erl
+++ applications/callflow/src/module/cf_move.erl
@@ -24,26 +24,23 @@
 handle(_Data, Call) ->
     case kapps_call:owner_id(Call) of
         'undefined' ->
-            lager:warning('call has no owner_id', []),
-            kapps_call_command:b_prompt(<<"cf-move-no_owner">>, Call),
-            cf_exe:stop(Call);
+            lager:warning('call has no owner_id'),
+            kapps_call_command:b_prompt(<<"cf-move-no_owner">>, Call);
         OwnerId ->
             Channels = get_channels(OwnerId, Call),
             case filter_channels(Channels, Call) of
                 {'error', 'no_channel'} ->
-                    lager:warning('cannot move call no channel up', []),
-                    kapps_call_command:b_prompt(<<"cf-move-no_channel">>, Call),
-                    cf_exe:stop(Call);
+                    lager:warning('cannot move call no channel up'),
+                    kapps_call_command:b_prompt(<<"cf-move-no_channel">>, Call);
                 {'error', 'too_many_channels'} ->
-                    lager:warning('cannot decide which channel to move to, too many channels', []),
-                    kapps_call_command:b_prompt(<<"cf-move-too_many_channels">>, Call),
-                    cf_exe:stop(Call);
+                    lager:warning('cannot decide which channel to move to, too many channels'),
+                    kapps_call_command:b_prompt(<<"cf-move-too_many_channels">>, Call);
                 {'ok', Channel} ->
-                    OtherLegId = kz_json:get_value(<<"other_leg">>, Channel),
-                    kapps_call_command:pickup(OtherLegId, Call),
-                    cf_exe:stop(Call)
+                    OtherLegId = kz_json:get_ne_binary_value(<<"other_leg">>, Channel),
+                    kapps_call_command:b_pickup(OtherLegId, Call)
             end
-    end.
+    end,
+    cf_exe:stop(Call).
 
 -spec get_channels(ne_binary(), kapps_call:call()) -> dict:dict().
 get_channels(OwnerId, Call) ->
@@ -53,7 +50,8 @@ get_channels(OwnerId, Call) ->
           ],
     case kapps_util:amqp_pool_collect(Req
                                      ,fun kapi_call:publish_query_user_channels_req/1
-                                     ,{'ecallmgr', 'true'})
+                                     ,{'ecallmgr', 'true'}
+                                     )
     of
         {'error', _E} ->
             lager:error("could not reach ecallmgr channels: ~p", [_E]),
@@ -66,24 +64,22 @@ get_channels(OwnerId, Call) ->
 clean_channels(JObjs) ->
     clean_channels(JObjs, dict:new()).
 
-clean_channels([], Dict) ->
-    Dict;
+clean_channels([], Dict) -> Dict;
 clean_channels([JObj|JObjs], Dict) ->
     clean_channels(JObjs, clean_channel(JObj, Dict)).
 
 clean_channel(JObj, Dict) ->
-    lists:foldl(
-      fun(Channel, D) ->
-              UUID = kz_json:get_value(<<"uuid">>, Channel),
-              dict:store(UUID, Channel, D)
-      end
+    lists:foldl(fun(Channel, D) ->
+                        UUID = kz_json:get_ne_binary_value(<<"uuid">>, Channel),
+                        dict:store(UUID, Channel, D)
+                end
                ,Dict
-               ,kz_json:get_value(<<"Channels">>, JObj, [])
-     ).
+               ,kz_json:get_list_value(<<"Channels">>, JObj, [])
+               ).
 
 -spec filter_channels(dict:dict(), kapps_call:call()) ->
                              {'ok', kz_json:object()} |
-                             {'error', atom()}.
+                             {'error', no_channel | too_many_channels}.
 filter_channels(Channels, Call) ->
     CallId = kapps_call:call_id(Call),
     NChannels = dict:erase(CallId, Channels),
@@ -91,5 +87,11 @@ filter_channels(Channels, Call) ->
         [] -> {'error', 'no_channel'};
         [{_, Channel}] ->
             {'ok', Channel};
-        [_|_] -> {'error', 'too_many_channels'}
+        [_|_]=_Channels ->
+            _ = [lager:debug("found channel ~s with other_leg: ~s"
+                            ,[_UUID, kz_json:get_ne_binary_value(<<"other_leg">>, _Channel)]
+                            )
+                 || {_UUID, _Channel} <- _Channels
+                ],
+            {'error', 'too_many_channels'}
     end.
