3a3f1a91bd25a86a35a14c692c76cbf19d2bcbb9
James Aimonetti <james@2600hz.com>
2017-06-08 21:27:29 +0000

    add some helpful debug logging

--- applications/crossbar/src/modules/cb_token_restrictions.erl
+++ applications/crossbar/src/modules/cb_token_restrictions.erl
@@ -160,12 +160,14 @@ get_auth_restrictions(AuthDoc) ->
     kz_json:get_json_value(<<"restrictions">>, AuthDoc).
 -else.
 -spec get_auth_restrictions(api_object()) -> api_object().
-get_auth_restrictions('undefined') -> 'undefined';
+get_auth_restrictions('undefined') ->
+    ?LOG_DEBUG("no auth doc to check"),
+    'undefined';
 get_auth_restrictions(AuthDoc) ->
     AuthModule = kz_json:get_atom_value(<<"method">>, AuthDoc),
     AccountId = kz_json:get_ne_binary_value(<<"account_id">>, AuthDoc),
     OwnerId = kz_json:get_ne_binary_value(<<"owner_id">>, AuthDoc),
-
+    ?LOG_DEBUG("checking for restrictions for ~s in ~s using ~s method", [OwnerId, AccountId, AuthModule]),
     crossbar_util:get_token_restrictions(AuthModule, AccountId, OwnerId).
 -endif.
 
