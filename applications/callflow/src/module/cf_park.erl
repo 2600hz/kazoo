4cecf969b63af331a95bc04525d4a46218f359cb
lazedo <luis.azedo@factorlusitano.com>
2017-06-12 14:47:26 +0100

    remove duplicated line

--- applications/callflow/src/module/cf_park.erl
+++ applications/callflow/src/module/cf_park.erl
@@ -658,7 +658,6 @@ ringback_parker(EndpointId, SlotNumber, Slot, Data, Call0) ->
                 ,{fun kapps_call:kvs_store/3, 'force_dynamic_cid', 'true'}
                 ,{fun kapps_call:set_callee_id_number/2, CalleeNumber}
                 ,{fun kapps_call:set_callee_id_name/2, CalleeName}
-                ,{fun kapps_call:set_callee_id_name/2, CalleeName}
                ],
     Call = kapps_call:exec(Routines, Call0),
     Timeout = callback_timeout(Data, SlotNumber),
