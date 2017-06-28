34caa744b17422a6717de628dad219740aaa870f
Hesaam Farhang <hesaam@2600hz.com>
2017-06-28 10:06:38 -0700

    KAZOO-5259: use federate to reload acls/send command

--- applications/ecallmgr/src/ecallmgr_fs_node.erl
+++ applications/ecallmgr/src/ecallmgr_fs_node.erl
@@ -121,6 +121,7 @@
                                                      ,'command'
                                                      ]
                                      }
+                                    ,'federate'
                                     ]
                          }
                         ]).
