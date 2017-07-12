7defc0459e93ddddb1e584a399a08597c091b679
Hesaam Farhang <hesaam@2600hz.com>
2017-07-12 11:55:47 -0700

    KAZOO-5451: reset api key on put

--- applications/crossbar/src/modules/cb_accounts.erl
+++ applications/crossbar/src/modules/cb_accounts.erl
@@ -110,7 +110,7 @@ allowed_methods(_AccountId, ?RESELLER) ->
 allowed_methods(_AccountId, ?CHILDREN) -> [?HTTP_GET];
 allowed_methods(_AccountId, ?DESCENDANTS) -> [?HTTP_GET];
 allowed_methods(_AccountId, ?SIBLINGS) -> [?HTTP_GET];
-allowed_methods(_AccountId, ?API_KEY) -> [?HTTP_GET];
+allowed_methods(_AccountId, ?API_KEY) -> [?HTTP_GET, ?HTTP_PUT];
 allowed_methods(_AccountId, ?TREE) -> [?HTTP_GET];
 allowed_methods(_AccountId, ?PARENTS) -> [?HTTP_GET].
 
@@ -229,6 +229,12 @@ validate_account_path(Context, AccountId, ?API_KEY, ?HTTP_GET) ->
             cb_context:set_resp_data(Context1, RespJObj);
         _Else -> Context1
     end;
+validate_account_path(Context, AccountId, ?API_KEY, ?HTTP_PUT) ->
+    Context1 = crossbar_doc:load(AccountId, prepare_context('undefined', Context), ?TYPE_CHECK_OPTION(?PVT_TYPE)),
+    case cb_context:resp_status(Context1) of
+        'success' -> add_pvt_api_key(Context1);
+        _Else -> Context1
+    end;
 validate_account_path(Context, AccountId, ?MOVE, ?HTTP_POST) ->
     Data = cb_context:req_data(Context),
     case kz_json:get_binary_value(<<"to">>, Data) of
@@ -334,6 +340,16 @@ put(Context, PathAccountId) ->
             cb_context:add_system_error('unspecified_fault', <<"internal error, unable to create the account">>, Context)
     end.
 
+put(Context, _AccountId, ?API_KEY) ->
+    C1 = crossbar_doc:save(Context),
+    case cb_context:resp_status(C1) of
+        'success' ->
+            JObj = cb_context:doc(C1),
+            ApiKey = kz_account:api_key(JObj),
+            RespJObj = kz_json:from_list([{<<"api_key">>, ApiKey}]),
+            cb_context:set_resp_data(C1, RespJObj);
+        _ -> C1
+    end;
 put(Context, AccountId, ?RESELLER) ->
     case whs_account_conversion:promote(AccountId) of
         {'error', 'master_account'} -> cb_context:add_system_error('forbidden', Context);
@@ -1187,11 +1203,15 @@ add_pvt_enabled(Context) ->
 maybe_add_pvt_api_key(Context) ->
     JObj = cb_context:doc(Context),
     case kz_account:api_key(JObj) of
-        'undefined' ->
-            APIKey = kz_term:to_hex_binary(crypto:strong_rand_bytes(32)),
-            cb_context:set_doc(Context, kz_account:set_api_key(JObj, APIKey));
+        'undefined' -> add_pvt_api_key(Context);
         _Else -> Context
     end.
+
+-spec add_pvt_api_key(cb_context:context()) -> cb_context:context().
+add_pvt_api_key(Context) ->
+    JObj = cb_context:doc(Context),
+    APIKey = kz_term:to_hex_binary(crypto:strong_rand_bytes(32)),
+    cb_context:set_doc(Context, kz_account:set_api_key(JObj, APIKey)).
 
 -spec maybe_add_pvt_tree(cb_context:context()) -> cb_context:context().
 maybe_add_pvt_tree(Context) ->
