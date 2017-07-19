f12749572e84d1ae2fcc3b9bf35f793d6f8d0553
karl anderson <karl@2600hz.com>
2017-07-19 15:17:30 -0700

    HELP-31085: do not handle redirects for callflow devices in trunkstore (#3969)

--- applications/trunkstore/src/ts_route_req.erl
+++ applications/trunkstore/src/ts_route_req.erl
@@ -19,24 +19,36 @@ init() -> 'ok'.
 
 -spec handle_req(kz_json:object(), kz_proplist()) -> any().
 handle_req(ApiJObj, _Options) ->
-    kz_util:put_callid(ApiJObj),
     'true' = kapi_route:req_v(ApiJObj),
-    CallID = kz_json:get_value(<<"Call-ID">>, ApiJObj),
-    CCVs = kz_json:get_value(<<"Custom-Channel-Vars">>, ApiJObj, kz_json:new()),
+    kz_util:put_callid(ApiJObj),
     lager:info("received request ~s asking if trunkstore can route this call", [kapi_route:fetch_id(ApiJObj)]),
-    case {kz_json:get_value(<<"Account-ID">>, CCVs)
-         ,kz_json:get_first_defined([<<"Authorizing-ID">>, <<"Referred-By">>, <<"Redirected-By">>], CCVs)
-         }
-    of
-        {AcctID, 'undefined'} when is_binary(AcctID) ->
-            %% Coming from carrier (off-net)
-            lager:info("call with fetch-id ~s began from outside the network", [kapi_route:fetch_id(ApiJObj)]),
-            ts_offnet_sup:start_handler(<<"offnet-", CallID/binary>>, ApiJObj);
-        {AcctID, AuthID} when is_binary(AcctID)
-                              andalso is_binary(AuthID) ->
-            %% Coming from PBX (on-net); authed by Registrar or ts_auth
-            lager:info("call with fetch-id ~s began on the network", [kapi_route:fetch_id(ApiJObj)]),
-            ts_onnet_sup:start_handler(<<"onnet-", CallID/binary>>, ApiJObj);
-        {_AcctID, _AuthID} ->
-            lager:debug("insufficient information available to lookup routing for ~s, ignoring", [kapi_route:fetch_id(ApiJObj)])
+    CCVs = kz_json:get_value(<<"Custom-Channel-Vars">>, ApiJObj),
+    ApplicationName = kz_json:get_ne_binary_value(<<"Application-Name">>, CCVs),
+    case kz_json:get_first_defined([<<"Referred-By">>, <<"Redirected-By">>], CCVs) =/= 'undefined' of
+        'true' when ApplicationName =:= ?APP_NAME -> handle_onnet_req(ApiJObj);
+        'true' ->
+            lager:info("request is the result of a transfer by another application, ~s, ignoring"
+                      ,[ApplicationName]
+                      );
+        'false' ->
+            case kz_json:get_ne_binary_value(<<"Authorizing-Type">>, CCVs) =:= <<"sys_info">>
+                andalso kz_json:get_ne_binary_value(<<"Authorizing-ID">>, CCVs) =/= 'undefined'
+            of
+                'true' -> handle_onnet_req(ApiJObj);
+                'false' -> handle_offnet_req(ApiJObj)
+            end
     end.
+
+-spec handle_onnet_req(kz_json:object()) -> any().
+handle_onnet_req(ApiJObj) ->
+    %% Coming from PBX (on-net); authed by Registrar or ts_auth
+    CallId = kz_json:get_ne_binary_value(<<"Call-ID">>, ApiJObj),
+    lager:info("call with fetch-id ~s began on the network", [kapi_route:fetch_id(ApiJObj)]),
+    ts_onnet_sup:start_handler(<<"onnet-", CallId/binary>>, ApiJObj).
+
+-spec handle_offnet_req(kz_json:object()) -> any().
+handle_offnet_req(ApiJObj) ->
+    %% Coming from carrier (off-net)
+    CallId = kz_json:get_ne_binary_value(<<"Call-ID">>, ApiJObj),
+    lager:info("call with fetch-id ~s began from outside the network", [kapi_route:fetch_id(ApiJObj)]),
+    ts_offnet_sup:start_handler(<<"offnet-", CallId/binary>>, ApiJObj).
