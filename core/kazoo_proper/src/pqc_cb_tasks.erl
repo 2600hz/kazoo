eb64a615c81c4c6f338f30aa986026c8ae7e3a66
James Aimonetti <james.aimonetti@gmail.com>
2017-07-21 00:01:13 +0000

    api for creating and executing tasks

---
+++ core/kazoo_proper/src/pqc_cb_tasks.erl
@@ -0,0 +1,37 @@
+-module(pqc_cb_tasks).
+
+-export([create/3
+        ,execute/2
+        ]).
+
+-include("kazoo_proper.hrl").
+
+-spec create(pqc_cb_api:state(), api_ne_binary(), ne_binary()) -> pqc_cb_api:response().
+create(API, QueryString, CSV) ->
+    TaskURL = tasks_url(QueryString),
+    RequestHeaders = pqc_cb_api:request_headers(API, [{<<"content-type">>, <<"text/csv">>}]),
+    io:format('user', "create task for ~s: ~s~n", [TaskURL, CSV]),
+    pqc_cb_api:make_request([201, 404, 409]
+                           ,fun kz_http:put/3
+                           ,TaskURL
+                           ,RequestHeaders
+                           ,CSV
+                           ).
+
+-spec execute(pqc_cb_api:state(), ne_binary()) -> pqc_cb_api:response().
+execute(API, TaskId) ->
+    pqc_cb_api:make_request([200]
+                           ,fun kz_http:patch/3
+                           ,task_url(TaskId)
+                           ,pqc_cb_api:request_headers(API)
+                           ,<<>>
+                           ).
+
+-spec task_url(string()) -> string().
+
+task_url(TaskId) ->
+    string:join([pqc_cb_api:v2_base_url(), "tasks", kz_term:to_list(TaskId)], "/").
+
+-spec tasks_url(string()) -> string().
+tasks_url(QueryString) ->
+    string:join([pqc_cb_api:v2_base_url(), "tasks"], "/") ++ [$? | QueryString].
