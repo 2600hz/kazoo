729df6541512063278944d0e549f1c6ede9a7767
James Aimonetti <james.aimonetti@gmail.com>
2017-07-23 21:37:34 +0000

    re-run a counter-example

--- core/kazoo_proper/src/pqc_util.erl
+++ core/kazoo_proper/src/pqc_util.erl
@@ -1,6 +1,8 @@
 -module(pqc_util).
 
--export([transition_if/2]).
+-export([transition_if/2
+        ,run_counterexample/2
+        ]).
 
 -include("kazoo_proper.hrl").
 
@@ -21,3 +23,19 @@ transition_if_fold({Fun, Args}, {'true', Model}) ->
         {'true', _NewState}=True -> True;
         NewModel -> {'true', NewModel}
     end.
+
+-spec run_counterexample(module(), list()) -> {module(), any()}.
+run_counterexample(PQC, Steps) ->
+    run_counterexample(PQC, Steps, PQC:initial_state()).
+run_counterexample(PQC, Steps, State) ->
+    lists:foldl(fun run_step/2, {PQC, State}, Steps).
+
+run_step({'set', Var, Call}, {PQC, State}) ->
+    run_call(Var, Call, {PQC, State}).
+
+run_call(_Var, {'call', M, F, Args}=Call, {PQC, State}) ->
+    io:format('user', "~p:~p(~p) -> ", [M, F, Args]),
+    Resp = erlang:apply(M, F, Args),
+    io:format('user', "~p~n", [Resp]),
+    'true' = PQC:postcondition(State, Call, Resp),
+    {PQC, PQC:next_state(State, Resp, Call)}.
