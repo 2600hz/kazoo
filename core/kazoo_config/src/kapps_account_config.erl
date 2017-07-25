3665aa1b5b50b3c97e9e88af600789fcbae72b16
Hesaam Farhang <hesaam@2600hz.com>
2017-07-25 13:59:01 -0700

    cleanup code

--- core/kazoo_config/src/kapps_account_config.erl
+++ core/kazoo_config/src/kapps_account_config.erl
@@ -18,13 +18,36 @@
         ,get_from_reseller/3, get_from_reseller/4
         ,set/4
         ,set_global/4
-        ,flush/1, flush/2
+        ,flush/2
         ,migrate/1
         ,config_doc_id/1
         ]).
 
--type account() :: ne_binary() | kapps_call:call() | kz_json:object().
 -type api_account() :: api_ne_binary() | kapps_call:call() | kz_json:object().
+-type account_or_not() :: ne_binary() | 'no_account_id'.
+
+
+-spec get_ne_binary(api_account(), ne_binary(), kz_json:path()) -> api_ne_binary().
+-spec get_ne_binary(api_account(), ne_binary(), kz_json:path(), Default) -> ne_binary() | Default.
+get_ne_binary(Account, Category, Path) ->
+    get_ne_binary(Account, Category, Path, undefined).
+get_ne_binary(Account, Category, Path, Default) ->
+    Value = get(Account, Category, Path, Default),
+    case kz_term:is_empty(Value) of
+        true -> Default;
+        false -> kz_term:to_binary(Value)
+    end.
+
+-spec get_ne_binaries(api_account(), ne_binary(), kz_json:path()) -> ne_binaries().
+-spec get_ne_binaries(api_account(), ne_binary(), kz_json:path(), Default) -> ne_binaries() | Default.
+get_ne_binaries(Account, Category, Path) ->
+    get_ne_binaries(Account, Category, Path, undefined).
+get_ne_binaries(Account, Category, Path, Default) ->
+    Values = get(Account, Category, Path, Default),
+    case kz_term:is_ne_binaries(Values) of
+        false -> Default;
+        true -> Values
+    end.
 
 %%--------------------------------------------------------------------
 %% @public
@@ -40,9 +63,9 @@ get_global(Account, Category, Key) ->
     get_global(Account, Category, Key, 'undefined').
 
 get_global(Account, Category, Key, Default) ->
-    case get_global_from_account(Account, Category, Key, Default) of
+    case fetch_from_account(Account, Category) of
         {'ok', JObj} -> get_global_from_doc(Category, Key, Default, JObj);
-        {'error', 'unknown_object'} -> kapps_config:get(Category, Key, Default);
+        {'error', 'no_account_id'} -> kapps_config:get(Category, Key, Default);
         {'error', _} -> get_from_reseller(Account, Category, Key, Default)
     end.
 
@@ -53,196 +76,208 @@ get_global(Account, Category, Key, Default) ->
 %% i.e. makes sure to skip reading from Account (i.e. sub-account of reseller).
 %% @end
 %%--------------------------------------------------------------------
--spec get_from_reseller(api_account(), ne_binary(), kz_json:path()) ->
-                               kz_json:api_json_term().
+-spec get_from_reseller(api_account(), ne_binary(), kz_json:path()) -> kz_json:api_json_term().
 get_from_reseller(Account, Category, Key) ->
     get_from_reseller(Account, Category, Key, 'undefined').
 
--spec get_from_reseller(api_account(), ne_binary(), kz_json:path(), kz_json:api_json_term()) ->
-                               kz_json:api_json_term().
+-spec get_from_reseller(api_account(), ne_binary(), kz_json:path(), kz_json:api_json_term()) -> kz_json:api_json_term().
 -ifdef(TEST).
 get_from_reseller(_, _, _, Default) -> Default.
 -else.
-get_from_reseller('undefined', Category, Key, Default) ->
-    kapps_config:get(Category, Key, Default);
 get_from_reseller(Account, Category, Key, Default) ->
-    try account_id(Account) of
-        AccountId ->
-            ResellerId = kz_services:find_reseller_id(AccountId),
-            maybe_get_global_from_reseller(AccountId, ResellerId, Category, Key, Default)
-    catch
-        _T:_E ->
-            kapps_config:get(Category, Key, Default)
-    end.
+    maybe_get_global_from_reseller(account_id(Account), Category, Key, Default).
+-endif.
 
--spec maybe_get_global_from_reseller(api_account(), account(), ne_binary(), kz_json:path(), kz_json:api_json_term()) ->
+-spec maybe_get_global_from_reseller(account_or_not(), ne_binary(), kz_json:path(), kz_json:api_json_term()) ->
                                             kz_json:api_json_term().
+maybe_get_global_from_reseller('no_account_id', Category, Key, Default) ->
+    kapps_config:get(Category, Key, Default);
+maybe_get_global_from_reseller(AccountId, Category, Key, Default) ->
+    ResellerId = kz_services:find_reseller_id(AccountId),
+    maybe_get_global_from_reseller(AccountId, ResellerId, Category, Key, Default).
+
+-spec maybe_get_global_from_reseller(ne_binary(), api_binary(), ne_binary(), kz_json:path(), kz_json:api_json_term()) ->
+                                            kz_json:api_json_term().
+maybe_get_global_from_reseller(_AccountId, 'undefined', Category, Key, Default) ->
+    kapps_config:get(Category, Key, Default);
 maybe_get_global_from_reseller(AccountId, AccountId, Category, Key, Default) ->
     kapps_config:get(Category, Key, Default);
 maybe_get_global_from_reseller(_AccountId, ResellerId, Category, Key, Default) ->
-    case get_global_from_account(ResellerId, Category, Key, Default) of
+    case fetch_from_account(ResellerId, Category) of
         {'ok', JObj} -> get_global_from_doc(Category, Key, Default, JObj);
         {'error', _} -> kapps_config:get(Category, Key, Default)
     end.
--endif.
 
--spec get_global_from_account(api_account(), ne_binary(), kz_json:path(), kz_json:api_json_term()) ->
-                                     {'ok', kz_json:object()} |
-                                     {'error', any()}.
-get_global_from_account('undefined', _Category, _Key, _Default) ->
-    {'error', 'unknown_object'};
-get_global_from_account(Account, Category, _Key, _Default) ->
-    try account_id(Account) of
-        AccountId ->
-            AccountDb = kz_util:format_account_id(AccountId, 'encoded'),
-            kz_datamgr:open_cache_doc(AccountDb, config_doc_id(Category), [{'cache_failures', ['not_found']}])
-    catch
-        _T:_E -> {'error', 'unknown_object'}
-    end.
-
--spec get_global_from_doc(ne_binary(), kz_json:path(), kz_json:api_json_term(), kz_json:object()) ->
-                                 kz_json:object().
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Get Key's value from account's db document if defined, otherwise get
+%% from system_config
+%% @end
+%%--------------------------------------------------------------------
+-spec get_global_from_doc(ne_binary(), kz_json:path(), kz_json:api_json_term(), kz_json:object()) -> kz_json:object().
 get_global_from_doc(Category, Key, Default, JObj) ->
     case kz_json:get_value(Key, JObj) of
         'undefined' -> kapps_config:get(Category, Key, Default);
         V -> V
     end.
 
--spec flush(account()) -> 'ok'.
--spec flush(account(), ne_binary()) -> 'ok'.
-flush(Account) ->
-    AccountDb = kz_util:format_account_id(Account, 'encoded'),
-    kz_datamgr:flush_cache_docs(AccountDb).
+-spec get(api_account(), ne_binary()) -> kz_json:object().
+-spec get(api_account(), ne_binary(), kz_json:path()) -> kz_json:api_json_term().
+-spec get(api_account(), ne_binary(), kz_json:path(), Default) -> kz_json:json_term() | Default.
 
-flush(Account, Config) ->
-    AccountDb = kz_util:format_account_id(Account, 'encoded'),
-    kz_datamgr:flush_cache_doc(AccountDb, config_doc_id(Config)).
+get(Account, Category, Key) ->
+    get(Account, Category, Key, 'undefined').
 
--spec get(account(), ne_binary()) -> kz_json:object().
--spec get(account(), ne_binary(), kz_json:path()) -> kz_json:api_json_term().
--spec get(account(), ne_binary(), kz_json:path(), Default) -> kz_json:json_term() | Default.
+get(Account, Category, Key, Default) ->
+    kz_json:get_value(Key, get(Account, Category), Default).
 
-get(Account, Config, Key) ->
-    get(Account, Config, Key, 'undefined').
+get(Account, Category) ->
+    case fetch_from_account(account_id(Account), Category) of
+        {'ok', JObj} -> JObj;
+        {'error', _} -> kz_doc:set_id(kz_json:new(), config_doc_id(Category))
+    end.
+
 -ifdef(TEST).
-get(_, _, _, Default) -> Default.
+-spec fetch_from_account(account_or_not(), ne_binary()) ->  kazoo_data:get_results_return().
 -else.
-get(Account, Config, Key, Default) ->
-    kz_json:get_value(Key, get(Account, Config), Default).
+fetch_from_account('no_account_id', _Category) ->
+    {'error', 'no_account_id'};
+fetch_from_account(AccountId, Category) ->
+    DocId = config_doc_id(Category),
+    AccountDb = kz_util:format_account_db(AccountId),
+    kz_datamgr:open_cache_doc(AccountDb, DocId, [{'cache_failures', ['not_found']}]).
 -endif.
 
-get(Account, Config) ->
-    AccountId = account_id(Account),
-    AccountDb = kz_util:format_account_id(AccountId, 'encoded'),
-    DocId = config_doc_id(Config),
-    case kz_datamgr:open_cache_doc(AccountDb, DocId, [{'cache_failures', ['not_found']}]) of
-        {'error', _} -> kz_doc:set_id(kz_json:new(), DocId);
-        {'ok', JObj} -> JObj
-    end.
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Set the Value for the Key in account db
+%% @end
+%%--------------------------------------------------------------------
+-spec set(api_account(), ne_binary(), kz_json:path(), kz_json:json_term()) -> kz_json:object().
+-ifdef(TEST).
+set(_, _, Key, Value) ->
+    kz_json:set_value(Key, Value, kz_json:new()).
+-else.
+set(Account, Category, Key, Value) ->
+    maybe_set_account(account_id(Account), Category, Key, Value).
+-endif.
 
--spec get_ne_binary(account(), ne_binary(), kz_json:path()) -> api_ne_binary().
--spec get_ne_binary(account(), ne_binary(), kz_json:path(), Default) -> ne_binary() | Default.
-get_ne_binary(Account, Config, Path) ->
-    get_ne_binary(Account, Config, Path, undefined).
-get_ne_binary(Account, Config, Path, Default) ->
-    Value = get(Account, Config, Path, Default),
-    case kz_term:is_empty(Value) of
-        true -> Default;
-        false -> kz_term:to_binary(Value)
-    end.
-
--spec get_ne_binaries(account(), ne_binary(), kz_json:path()) -> ne_binaries().
--spec get_ne_binaries(account(), ne_binary(), kz_json:path(), Default) -> ne_binaries() | Default.
-get_ne_binaries(Account, Config, Path) ->
-    get_ne_binaries(Account, Config, Path, undefined).
-get_ne_binaries(Account, Config, Path, Default) ->
-    Values = get(Account, Config, Path, Default),
-    case kz_term:is_ne_binaries(Values) of
-        false -> Default;
-        true -> Values
-    end.
-
--spec set(account(), ne_binary(), kz_json:path(), kz_json:json_term()) -> kz_json:object().
-set(Account, Config, Key, Value) ->
-    JObj = kz_json:set_value(Key, Value, get(Account, Config)),
-    AccountDb = account_db(Account),
-    JObj1 = update_config_for_saving(AccountDb, JObj),
+-spec maybe_set_account(account_or_not(), ne_binary(), kz_json:path(), kz_json:json_term()) -> kz_json:object().
+maybe_set_account('no_account_id', _, Key, Value) ->
+    kz_json:set_value(Key, Value, kz_json:new());
+maybe_set_account(AccountId, Category, Key, Value) ->
+    JObj = kz_json:set_value(Key, Value, get(AccountId, Category)),
+    JObj1 = update_config_for_saving(AccountId, JObj),
+    AccountDb = kz_util:format_account_db(AccountId),
     {'ok', JObj2} = kz_datamgr:ensure_saved(AccountDb, JObj1),
     JObj2.
 
--spec update_config_for_saving(ne_binary(), kz_json:object()) -> kz_json:object().
-update_config_for_saving(AccountDb, JObj) ->
-    kz_doc:update_pvt_parameters(JObj
-                                ,AccountDb
-                                ,[{'type', <<"account_config">>}
-                                 ,{'account_id', account_id(AccountDb)}
-                                 ]).
-
--spec set_global(account(), ne_binary(), kz_json:path(), kz_json:json_term()) ->
-                        kz_json:object().
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Set the Value for the Key in account db if found, otherwise get
+%% system_config value then save in account db.
+%% @end
+%%--------------------------------------------------------------------
+-ifdef(TEST).
+set_global(_, _, Key, Value) ->
+    kz_json:set_value(Key, Value, kz_json:new()).
+-else.
+-spec set_global(api_account(), ne_binary(), kz_json:path(), kz_json:json_term()) -> kz_json:object().
 set_global(Account, Category, Key, Value) ->
-    AccountId = account_id(Account),
-    AccountDb = account_db(Account),
+    set_account_or_merge_global(account_id(Account), Category, Key, Value).
+-endif.
 
-    Doc = case kz_datamgr:open_cache_doc(AccountDb, Category, [{'cache_failures', ['not_found']}]) of
+-spec set_account_or_merge_global(account_or_not(), ne_binary(), kz_json:path(), kz_json:json_term()) ->
+                                         kz_json:object().
+set_account_or_merge_global('no_account_id', _, Key, Value) ->
+    kz_json:set_value(Key, Value, kz_json:new());
+set_account_or_merge_global(AccountId, Category, Key, Value) ->
+    Doc = case fetch_from_account(AccountId, Category) of
               {'ok', JObj} -> JObj;
               {'error', _} -> kz_json:set_value(Key, kapps_config:get(Category, Key), kz_json:new())
           end,
+    Doc1 = kz_json:set_value(Key, Value, update_config_for_saving(AccountId, Doc)),
 
-    Doc1 = kz_json:set_value(Key
-                            ,Value
-                            ,update_config_for_saving(AccountDb, Doc)
-                            ),
-
+    AccountDb = kz_util:format_account_db(AccountId),
     {'ok', JObj1} = kz_datamgr:ensure_saved(AccountDb, Doc1),
-    kz_cache:erase_local(?KAPPS_CONFIG_CACHE, cache_key(AccountId, Category)),
     JObj1.
 
+-spec update_config_for_saving(ne_binary(), kz_json:object()) -> kz_json:object().
+update_config_for_saving(AccountId, JObj) ->
+    AccountDb = kz_util:format_account_db(AccountId),
+    kz_doc:update_pvt_parameters(JObj
+                                ,AccountDb
+                                ,[{'type', <<"account_config">>}
+                                 ,{'account_id', AccountId}
+                                 ]).
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Flush account's specific config cache
+%% @end
+%%--------------------------------------------------------------------
+-spec flush(ne_binary(), ne_binary()) -> 'ok'.
+flush(Account, Category) ->
+    AccountDb = kz_util:format_account_id(Account, 'encoded'),
+    kz_datamgr:flush_cache_doc(AccountDb, config_doc_id(Category)).
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Give document Id of config's document in account db
+%% @end
+%%--------------------------------------------------------------------
 -spec config_doc_id(ne_binary()) -> ne_binary().
-config_doc_id(Config) -> <<(?KZ_ACCOUNT_CONFIGS)/binary, Config/binary>>.
+config_doc_id(Category) -> <<(?KZ_ACCOUNT_CONFIGS)/binary, Category/binary>>.
 
--spec cache_key(ne_binary(), ne_binary()) -> {?MODULE, ne_binary(), ne_binary()}.
-cache_key(AccountId, Config) -> {?MODULE, Config, AccountId}.
 
--spec account_id(account()) -> ne_binary().
-account_id(Account) when is_binary(Account) ->
-    kz_util:format_account_id(Account, 'raw');
-account_id(Obj) ->
-    account_id_from_call(Obj, kapps_call:is_call(Obj)).
+%% ====================================================================
+%% Internal functions
+%% ====================================================================
 
--spec account_id_from_call(kapps_call:call() | kz_json:object(), boolean()) -> ne_binary().
-account_id_from_call(Call, 'true') ->
-    kapps_call:account_id(Call);
-account_id_from_call(Obj, 'false') ->
-    account_id_from_jobj(Obj, kz_json:is_json_object(Obj)).
 
--spec account_id_from_jobj(kz_json:object(), 'true') -> ne_binary().
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Find AccountId from binary, Call object or JObj.
+%% @end
+%%--------------------------------------------------------------------
+-spec account_id(api_account()) -> account_or_not().
+account_id(?NE_BINARY=Account) -> kz_util:format_account_id(Account);
+account_id('undefined') -> 'no_account_id';
+account_id(Obj) -> account_id_from_call(Obj, kapps_call:is_call(Obj)).
+
+-spec account_id_from_call(kapps_call:call() | kz_json:object(), boolean()) -> account_or_not().
+account_id_from_call(Call, 'true') -> maybe_format_account_id(kapps_call:account_id(Call));
+account_id_from_call(Obj, 'false') -> account_id_from_jobj(Obj, kz_json:is_json_object(Obj)).
+
+-spec account_id_from_jobj(kz_json:object(), 'true') -> account_or_not().
 account_id_from_jobj(JObj, 'true') ->
-    kz_json:get_first_defined([<<"Account-ID">>, <<"account_id">>], JObj);
+    Paths = [<<"Account-ID">>
+            ,<<"account_id">>
+            ,<<"Account-DB">>
+            ,<<"account_db">>
+            ],
+    maybe_format_account_id(kz_json:get_first_defined(Paths, JObj));
 account_id_from_jobj(_Obj, 'false') ->
     lager:debug("unable to find account id from ~p", [_Obj]),
-    throw({'error', 'unknown_object'}).
+    'no_account_id'.
 
--spec account_db(account()) -> ne_binary().
-account_db(Account) when is_binary(Account) ->
-    kz_util:format_account_id(Account, 'encoded');
-account_db(Obj) ->
-    account_db_from_call(Obj, kapps_call:is_call(Obj)).
 
--spec account_db_from_call(kapps_call:call() | kz_json:object(), boolean()) -> ne_binary().
-account_db_from_call(Call, 'true') ->
-    kapps_call:account_db(Call);
-account_db_from_call(Obj, 'false') ->
-    account_db_from_jobj(Obj, kz_json:is_json_object(Obj)).
+-spec maybe_format_account_id(api_ne_binary()) -> account_or_not().
+maybe_format_account_id('undefined') -> 'no_account_id';
+maybe_format_account_id(Account) -> kz_util:format_account_id(Account).
 
--spec account_db_from_jobj(kz_json:object(), 'true') -> ne_binary().
-account_db_from_jobj(JObj, 'true') ->
-    kz_json:get_first_defined([<<"Account-DB">>, <<"account_db">>], JObj);
-account_db_from_jobj(_Obj, 'false') ->
-    lager:debug("unable to find account db from ~p", [_Obj]),
-    throw({'error', 'unknown_object'}).
 
+%% ====================================================================
 %% Migrates config settings
+%% ====================================================================
+
+
 -type migrate_setting() :: {ne_binary(), kz_json:path()}.
 -type migrate_value() :: {ne_binary(), ne_binary(), kz_json:path(), _}.
 -type migrate_values() :: [migrate_value()].
