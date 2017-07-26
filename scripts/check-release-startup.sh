e353fb3dd43266234f2df701b9274a716260c022
James Aimonetti <james.aimonetti@gmail.com>
2017-07-26 00:10:12 +0000

    maybe capture exits

--- scripts/check-release-startup.sh
+++ scripts/check-release-startup.sh
@@ -9,21 +9,26 @@ rel=${REL:-kazoo_apps}  # kazoo_apps | ecallmgr | ...
 echo "Checking release startup with node $rel..."
 
 sup() {
-    "$PWD"/core/sup/priv/sup "$*"
+    result=$(bash -c "$PWD"/core/sup/priv/sup "$*" 2>&1)
+    echo $result
+}
+
+shutdown() {
+    sup init stop
 }
 
 script() {
-    sup crossbar_maintenance create_account 'compte_maitre' 'royaume' 'superduperuser' 'pwd!'
+    sup crossbar_maintenance create_account 'compte_maitre' 'royaume' 'superduperuser' 'pwd!' || shutdown
     sleep 3
 #    sup kazoo_perf_maintenance json_metrics | python -m json.tool
     sleep 1
 #    sup kazoo_perf_maintenance graphite_metrics 'compte_maitre' 'clu1' 'royaume'
     sleep 1
-    sup kapps_maintenance migrate
+    sup kapps_maintenance migrate || shutdown
     sleep 3
-    sup kapps_maintenance migrate_to_4_0
+    sup kapps_maintenance migrate_to_4_0 || shutdown
     sleep 9
-    sup init stop
+    shutdown
 }
 
 sleep 240 && script &
