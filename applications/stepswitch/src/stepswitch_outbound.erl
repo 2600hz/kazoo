0d26bd8441c4f561d608704c77120e3b7c30829b
karl anderson <karl@2600hz.com>
2017-07-27 13:04:22 -0700

    KAZOO-5602: added resource-type to ccvs
    Conflicts:
    	applications/stepswitch/src/stepswitch_resources.erl

--- applications/stepswitch/src/stepswitch_outbound.erl
+++ applications/stepswitch/src/stepswitch_outbound.erl
@@ -249,8 +249,10 @@ create_loopback_endpoint(Props, JObj) ->
              [{<<?CHANNEL_LOOPBACK_HEADER_PREFIX, "Inception">>, <<Number/binary, "@", Realm/binary>>}
              ,{<<?CHANNEL_LOOPBACK_HEADER_PREFIX, "Account-ID">>, AccountId}
              ,{<<?CHANNEL_LOOPBACK_HEADER_PREFIX, "Retain-CID">>, "true"}
+             ,{<<?CHANNEL_LOOPBACK_HEADER_PREFIX, "Resource-Type">>, <<"onnet-origination">>}
              ,{<<"Resource-ID">>, AccountId}
              ,{<<"Loopback-Request-URI">>, <<Number/binary, "@", Realm/binary>>}
+             ,{<<"Resource-Type">>, <<"onnet-termination">>}
              ]),
     Endpoint = kz_json:from_list(
                  props:filter_undefined(
