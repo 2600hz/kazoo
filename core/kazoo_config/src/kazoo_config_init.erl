46105506a9a80a75d596ae05f66f2f4f1b821d5d
icehess <icehess@users.noreply.github.com>
2017-08-01 10:32:34 -0700

    get merge result of account, it's parents and system config (#4008)

--- core/kazoo_config/src/kazoo_config_init.erl
+++ core/kazoo_config/src/kazoo_config_init.erl
@@ -60,14 +60,14 @@ set_env() ->
     AppEnv = load_file(),
     lager:notice("loaded settings : ~p", [AppEnv]),
     set_zone(AppEnv),
-    application:set_env(?APP_NAME_ATOM, 'kz_config', AppEnv).
+    application:set_env(?APP, 'kz_config', AppEnv).
 
 set_zone(AppEnv) ->
     erlang:put(?SETTINGS_KEY, AppEnv),
     [Local] = kz_config:get(kz_config:get_node_section_name(), 'zone', ['local']),
     Zone = kz_term:to_atom(Local, 'true'),
     lager:notice("setting zone to ~p", [Zone]),
-    application:set_env(?APP_NAME_ATOM, 'zone', Zone).
+    application:set_env(?APP, 'zone', Zone).
 
 
 -spec reload() -> 'ok'.
