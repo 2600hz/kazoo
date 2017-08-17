110fb636d143b4f6039628a6cad0bb83b46d8416
lazedo <luis.azedo@factorlusitano.com>
2017-08-17 14:47:59 +0100

    update check MODULE script

--- scripts/code_checks.bash
+++ scripts/code_checks.bash
@@ -21,9 +21,9 @@ function check_MODULE {
         [[ 'erl' != ${base##*.} ]] && continue
         local err=0
         m=$(grep -Fe '-module(' "$f"  2>/dev/null | cut -d'(' -f2 | cut -d')' -f1)
-        grep -nE '^[^%]*[^a-zA-Z0-9_]'$m: "$f"
+        grep -nE '^[^%]*[^a-zA-Z0-9_]'$m[[:space:]]*: "$f"
         [[ $? -ne 1 ]] && ((err++))
-        grep -nE "^[^%]*'$m'" "$f"
+        grep -nE "^[^%]*'$m[[:space:]]*:'" "$f"
         [[ $? -ne 1 ]] && ((err++))
         [[ $err -ne 0 ]] && echo "^ $f" && echo && ((errors++))
     done
