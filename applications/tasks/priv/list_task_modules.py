3175c13310146d133343e395965063d190b0caeb
Hesaam Farhang <hesaam@2600hz.com>
2017-08-29 18:10:41 -0700

    do not add kt_compactor_worker to list of tasks module

--- applications/tasks/priv/list_task_modules.py
+++ applications/tasks/priv/list_task_modules.py
@@ -5,6 +5,7 @@ import sys, os, glob
 
 header_path = "src/task_modules.hrl"
 task_modules_path = "src/modules/*.erl"
+ignore_files = ['src/modules/kt_compactor_worker.erl']
 
 def fname(path):
     (name, ext) = (os.path.splitext(os.path.basename(path)))
@@ -14,7 +15,7 @@ with open(header_path, 'w') as header_file:
     name = fname(header_path)
     header_name = name.upper()
 
-    keys = glob.glob(task_modules_path)
+    keys = [key for key in glob.glob(task_modules_path) if key not in ignore_files]
     keys.sort()
     first, rest = keys[0], keys[1:]
 
