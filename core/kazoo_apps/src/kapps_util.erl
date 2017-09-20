f1f748caf59eeb6e7f1f4b7e180a0d84c8e54f67
karl anderson <karl@2600hz.com>
2017-09-20 13:46:27 -0700

    per-minute-data: trailing whitespace

--- core/kazoo_apps/src/kapps_util.erl
+++ core/kazoo_apps/src/kapps_util.erl
@@ -382,7 +382,7 @@ do_get_ccvs_by_ip(IP) ->
     case kapps_config:get_is_true(<<"registrar">>, <<"use_aggregate">>, 'true')
         andalso kz_datamgr:get_results(?KZ_SIP_DB, ?AGG_LIST_BY_IP, [{'key', IP}])
     of
-        'false' -> 
+        'false' ->
             NotF = {'error', 'not_found'},
             lager:debug("aggregate SIP auth db disabled, skipping CCV lookup by IP ~s", [IP]),
             kz_cache:store_local(?KAPPS_GETBY_CACHE, ?ACCT_BY_IP_CACHE(IP), NotF),
