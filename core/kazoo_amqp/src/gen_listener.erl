3557d45db741c32b2482d80af49af159f33e63a2
Mark Magnusson <mark@2600hz.com>
2017-09-29 14:29:08 -0400

    master: log crashes in handle_event for gen_listener (#4227)
    Conflicts:
    	core/kazoo_amqp/src/gen_listener.erl

--- core/kazoo_amqp/src/gen_listener.erl
+++ core/kazoo_amqp/src/gen_listener.erl
@@ -915,11 +915,14 @@ callback_handle_event(JObj
                                    handle_event_return() |
                                    {'EXIT', any()}.
 callback_handle_event(JObj, _, {Module, Fun, 2}, ModuleState) ->
-    catch Module:Fun(JObj, ModuleState);
+    try Module:Fun(JObj, ModuleState)
+    catch E:O -> lager:error("CRASH in handle_event ~p:~p", [E, O]) end;
 callback_handle_event(JObj, {BasicDeliver, _}, {Module, Fun, 3}, ModuleState) ->
-    catch Module:Fun(JObj, BasicDeliver, ModuleState);
+    try Module:Fun(JObj, BasicDeliver, ModuleState)
+    catch E:O -> lager:error("CRASH in handle_event ~p:~p", [E, O]) end;
 callback_handle_event(JObj, {BasicDeliver, Basic}, {Module, Fun, 4}, ModuleState) ->
-    catch Module:Fun(JObj, BasicDeliver, Basic, ModuleState);
+    try Module:Fun(JObj, BasicDeliver, Basic, ModuleState)
+    catch E:O -> lager:error("CRASH in handle_event ~p:~p", [E, O]) end;
 callback_handle_event(_, _, _, _) ->
     {'EXIT', 'not_exported'}.
 
