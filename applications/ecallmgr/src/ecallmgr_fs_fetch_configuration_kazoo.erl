948048df063798af2c22f40330f3606168c67870
lazedo <luis.azedo@factorlusitano.com>
2017-10-09 11:14:22 +0100

    split configuration handlers

---
+++ applications/ecallmgr/src/ecallmgr_fs_fetch_configuration_kazoo.erl
@@ -0,0 +1,69 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2012-2017, 2600Hz INC
+%%% @doc
+%%% Send config commands to FS
+%%% @end
+%%% @contributors
+%%%   Edouard Swiac
+%%%   James Aimonetti
+%%%-------------------------------------------------------------------
+-module(ecallmgr_fs_fetch_configuration_kazoo).
+
+%% API
+-export([init/0]).
+
+-export([kazoo/1]).
+
+-include("ecallmgr.hrl").
+
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the bindings
+%% @end
+%%--------------------------------------------------------------------
+-spec init() -> 'ok'.
+init() ->
+    kazoo_bindings:bind(<<"fetch.configuration.kazoo.conf">>, ?MODULE, 'kazoo'),
+    'ok'.
+
+-spec kazoo(tuple()) -> fs_sendmsg_ret().
+kazoo({Node, Id, JObj}) ->
+    kz_util:put_callid(Id),
+    lager:debug("received configuration request for kazoo configuration ~p , ~p", [Node, Id]),
+    fetch_mod_kazoo_config(Node, Id, kz_api:event_name(JObj), JObj).
+
+
+-spec fetch_mod_kazoo_config(atom(), ne_binary(), ne_binary(), kz_json:object()) -> fs_sendmsg_ret().
+fetch_mod_kazoo_config(Node, Id, <<"COMMAND">>, _JObj) ->
+    lager:debug_unsafe("kazoo conf request : ~s", [kz_json:encode(_JObj, ['pretty'])]),
+    config_req_not_handled(Node, Id, <<"kazoo.conf">>);
+fetch_mod_kazoo_config(Node, Id, <<"REQUEST_PARAMS">>, JObj) ->
+    Action = kz_json:get_ne_binary_value(<<"Action">>, JObj),
+    fetch_mod_kazoo_config_action(Node, Id, Action, JObj);
+fetch_mod_kazoo_config(Node, Id, Event, _JObj) ->
+    lager:debug("unhandled mod kazoo config event : ~p : ~p", [Node, Event]),
+    config_req_not_handled(Node, Id, <<"kazoo.conf">>).
+
+-spec config_req_not_handled(atom(), ne_binary(), ne_binary()) -> fs_sendmsg_ret().
+config_req_not_handled(Node, Id, Conf) ->
+    {'ok', NotHandled} = ecallmgr_fs_xml:not_found(),
+    lager:debug("ignoring conf ~s: ~s", [Conf, Id]),
+    freeswitch:fetch_reply(Node, Id, 'configuration', iolist_to_binary(NotHandled)).
+
+-spec fetch_mod_kazoo_config_action(atom(), ne_binary(), api_ne_binary(), kz_proplist()) -> fs_sendmsg_ret().
+fetch_mod_kazoo_config_action(Node, Id, <<"request-filter">>, _Data) ->
+    {'ok', Xml} = ecallmgr_fs_xml:event_filters_resp_xml(?FS_EVENT_FILTERS),
+    lager:debug("replying with xml response for request-filter params request"),
+    freeswitch:fetch_reply(Node, Id, 'configuration', iolist_to_binary(Xml));
+fetch_mod_kazoo_config_action(Node, Id, 'undefined', _Data) ->
+    config_req_not_handled(Node, Id, <<"kazoo.conf">>);
+fetch_mod_kazoo_config_action(Node, Id, Action, _Data) ->
+    lager:debug("unhandled mod kazoo config action : ~p : ~p", [Node, Action]),
+    config_req_not_handled(Node, Id, <<"kazoo.conf">>).
---
+++ applications/ecallmgr/src/ecallmgr_fs_fetch_configuration_sofia.erl
@@ -0,0 +1,203 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2012-2017, 2600Hz INC
+%%% @doc
+%%% Send config commands to FS
+%%% @end
+%%% @contributors
+%%%   Edouard Swiac
+%%%   James Aimonetti
+%%%-------------------------------------------------------------------
+-module(ecallmgr_fs_fetch_configuration_sofia).
+
+%% API
+-export([init/0]).
+
+-export([sofia/1]).
+
+-include("ecallmgr.hrl").
+
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the bindings
+%% @end
+%%--------------------------------------------------------------------
+-spec init() -> 'ok'.
+init() ->
+    kazoo_bindings:bind(<<"fetch.configuration.sofia.conf">>, ?MODULE, 'sofia'),
+    'ok'.
+
+-spec sofia(tuple()) -> fs_sendmsg_ret().
+sofia({Node, Id, _JObj}) ->
+    kz_util:put_callid(Id),
+    case ecallmgr_config:is_true(<<"sofia_conf">>) of
+        'false' ->
+            lager:info("sofia conf disabled"),
+            {'ok', Resp} = ecallmgr_fs_xml:not_found(),
+            freeswitch:fetch_reply(Node, Id, 'configuration', iolist_to_binary(Resp));
+        'true' ->
+            Profiles = ecallmgr_config:fetch(<<"fs_profiles">>, kz_json:new()),
+            DefaultProfiles = default_sip_profiles(Node),
+            try ecallmgr_fs_xml:sip_profiles_xml(kz_json:merge(DefaultProfiles, Profiles)) of
+                {'ok', ConfigXml} ->
+                    lager:debug("sending sofia XML to ~s: ~s", [Node, ConfigXml]),
+                    freeswitch:fetch_reply(Node, Id, 'configuration', erlang:iolist_to_binary(ConfigXml))
+            catch
+                _E:_R ->
+                    lager:info("sofia profile resp failed to convert to XML (~s): ~p", [_E, _R]),
+                    {'ok', Resp} = ecallmgr_fs_xml:not_found(),
+                    freeswitch:fetch_reply(Node, Id, 'configuration', iolist_to_binary(Resp))
+            end
+    end.
+
+
+-spec default_sip_profiles(atom()) -> kz_json:object().
+default_sip_profiles(Node) ->
+    Gateways = case ecallmgr_config:is_true(<<"process_gateways">>) of
+                   'false' -> kz_json:new();
+                   'true' ->
+                       SysconfResp = ecallmgr_config:fetch(<<"gateways">>, kz_json:new()),
+                       _ = maybe_kill_node_gateways(SysconfResp, Node),
+                       SysconfResp
+               end,
+    JObj = kz_json:from_list([{kz_term:to_binary(?DEFAULT_FS_PROFILE)
+                              ,kz_json:from_list(default_sip_profile())}
+                             ]),
+    kz_json:set_value([kz_term:to_binary(?DEFAULT_FS_PROFILE), <<"Gateways">>]
+                     ,Gateways
+                     ,JObj
+                     ).
+
+-spec default_sip_profile() -> kz_proplist().
+default_sip_profile() ->
+    [{<<"Settings">>, kz_json:from_list(default_sip_settings())}
+    ,{<<"Gateways">>, kz_json:from_list(default_sip_gateways())}
+    ].
+
+-spec default_sip_settings() -> kz_proplist().
+default_sip_settings() ->
+    [{<<"message-threads">>, <<"10">>}
+    ,{<<"auth-calls">>, <<"true">>}
+    ,{<<"apply-nat-acl">>, <<"rfc1918.auto">>}
+    ,{<<"apply-inbound-acl">>, <<"trusted">>}
+    ,{<<"apply-proxy-acl">>, <<"authoritative">>}
+    ,{<<"local-network-acl">>, <<"localnet.auto">>}
+    ,{<<"challenge-realm">>, <<"auto_from">>}
+    ,{<<"multiple-registrations">>, <<"false">>}
+    ,{<<"accept-blind-reg">>, <<"false">>}
+    ,{<<"accept-blind-auth">>, <<"false">>}
+    ,{<<"nonce-ttl">>, <<"86400">>}
+    ,{<<"disable-register">>, <<"false">>}
+    ,{<<"inbound-reg-force-matching-username">>, <<"true">>}
+    ,{<<"auth-all-packets">>, <<"false">>}
+    ,{<<"context">>, <<"context_2">>}
+    ,{<<"dialplan">>, <<"XML">>}
+    ,{<<"manual-redirect">>, <<"false">>}
+    ,{<<"disable-transfer">>, <<"false">>}
+    ,{<<"sip-ip">>, <<"$${local_ip_v4}">>}
+    ,{<<"ext-sip-ip">>, <<"auto">>}
+    ,{<<"sip-port">>, <<"5060">>}
+    ,{<<"user-agent-string">>, <<"2600hz">>}
+    ,{<<"enable-100rel">>, <<"false">>}
+    ,{<<"max-proceeding">>, <<"1000">>}
+    ,{<<"inbound-use-callid-as-uuid">>, <<"true">>}
+    ,{<<"outbound-use-uuid-as-callid">>, <<"true">>}
+    ,{<<"rtp-ip">>, <<"$${local_ip_v4}">>}
+    ,{<<"ext-rtp-ip">>, <<"auto">>}
+    ,{<<"rtp-timer-name">>, <<"soft">>}
+    ,{<<"rtp-autoflush-during-bridge">>, <<"true">>}
+    ,{<<"rtp-rewrite-timestamps">>, <<"false">>}
+    ,{<<"hold-music">>, <<"local_stream://default">>}
+    ,{<<"record-path">>, <<"$${recordings_dir}">>}
+    ,{<<"record-template">>, <<"${caller_id_number}.${target_domain}.${strftime(%Y-%m-%d-%H-%M-%S)}.wav">>}
+    ,{<<"dtmf-duration">>, <<"960">>}
+    ,{<<"rfc2833-pt">>, <<"101">>}
+    ,{<<"dtmf-type">>, <<"rfc2833">>}
+    ,{<<"pass-rfc2833">>, <<"false">>}
+    ,{<<"inbound-codec-prefs">>, <<"$${codecs}">>}
+    ,{<<"outbound-codec-prefs">>, <<"$${codecs}">>}
+    ,{<<"inbound-codec-negotiation">>, <<"generous">>}
+    ,{<<"inbound-late-negotiation">>, <<"false">>}
+    ,{<<"disable-transcoding">>, <<"false">>}
+    ,{<<"t38-passthru">>, <<"true">>}
+    ,{<<"all-reg-options-ping">>, <<"true">>}
+    ,{<<"enable-timer">>, <<"false">>}
+    ,{<<"rtp-timeout-sec">>, <<"3600">>}
+    ,{<<"rtp-hold-timeout-sec">>, <<"3600">>}
+    ,{<<"minimum-session-expires">>, <<"90">>}
+    ,{<<"manage-presence">>, <<"true">>}
+    ,{<<"send-message-query-on-register">>, <<"false">>}
+    ,{<<"watchdog-enabled">>, <<"false">>}
+    ,{<<"debug">>, <<"info">>}
+    ,{<<"sip-trace">>, <<"true">>}
+    ,{<<"log-auth-failures">>, <<"true">>}
+    ,{<<"log-level">>, <<"info">>}
+    ,{<<"tracelevel">>, <<"debug">>}
+    ,{<<"debug-presence">>, <<"0">>}
+    ,{<<"debug-sla">>, <<"0">>}
+    ,{<<"auto-restart">>, <<"false">>}
+    ,{<<"rtp-enable-zrtp">>, <<"true">>}
+    ,{<<"liberal-dtmf">>, <<"true">>}
+    ].
+
+default_sip_gateways() -> [].
+
+maybe_kill_node_gateways(JObj, Node) ->
+    try get_node_gateways(Node) of
+        Gateways ->
+            NewNames = kz_json:get_keys(JObj),
+            _ = maybe_kill_changed_gateways(NewNames, Gateways, JObj, Node),
+            RunningNames = kz_json:get_keys(Gateways),
+            _ = maybe_kill_removed_gateways(RunningNames, JObj, Node)
+    catch
+        _:_ -> 'ok'
+    end.
+
+maybe_kill_removed_gateways([], _, _) -> 'ok';
+maybe_kill_removed_gateways([GatewayName|Names], JObj, Node) ->
+    _ = case kz_json:get_value(GatewayName, JObj) of
+            'undefined' -> kill_gateway(GatewayName, Node);
+            _Else -> 'ok'
+        end,
+    maybe_kill_removed_gateways(Names, JObj, Node).
+
+maybe_kill_changed_gateways([], _, _, _) -> 'ok';
+maybe_kill_changed_gateways([GatewayName|Names], Gateways, JObj, Node) ->
+    Running =  kz_json:get_value(GatewayName, Gateways),
+    New = kz_json:get_value(GatewayName, JObj),
+    _ = maybe_kill_changed_gateway(GatewayName, Running, New, Node),
+    maybe_kill_changed_gateways(Names, Gateways, JObj, Node).
+
+maybe_kill_changed_gateway(_, 'undefined', _, _) -> 'ok';
+maybe_kill_changed_gateway(GatewayName, Running, New, Node) ->
+    case compare_node_gateways(Running, New) of
+        'false' -> kill_gateway(GatewayName, Node);
+        'true' -> 'ok'
+    end.
+
+compare_node_gateways(Running, New) ->
+    NewVersion = kz_json:get_value([<<"Variables">>, <<"Gateway-Version">>], New),
+    case kz_json:get_value([<<"Inbound-Variables">>, <<"Gateway-Version">>], Running) of
+        'undefined' -> 'true';
+        NewVersion -> 'true';
+        _Else -> 'false'
+    end.
+
+kill_gateway(GatewayName, Node) ->
+    Args = ["profile "
+           ,?DEFAULT_FS_PROFILE
+           ," killgw "
+           ,kz_term:to_list(GatewayName)
+           ],
+    freeswitch:api(Node, 'sofia', lists:flatten(Args)).
+
+get_node_gateways(Node) ->
+    {'ok', Response} = freeswitch:api(Node, 'sofia', "xmlstatus gateway"),
+    {Xml, _} = xmerl_scan:string(kz_term:to_list(Response)),
+    ecallmgr_fs_xml:sofia_gateways_xml_to_json(Xml).
