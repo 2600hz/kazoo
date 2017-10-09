0f998d1af344eb814c99b6276fc9726090607f46
lazedo <luis.azedo@factorlusitano.com>
2017-10-09 12:01:35 +0100

    use multiple listeners in callflow for route_req

---
+++ applications/callflow/src/cf_listener_sup.erl
@@ -0,0 +1,64 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2012-2015, 2600Hz, INC
+%%% @doc
+%%%
+%%% @end
+%%% @contributors
+%%%-------------------------------------------------------------------
+-module(cf_listener_sup).
+
+-behaviour(supervisor).
+
+-include("callflow.hrl").
+
+-define(SERVER, ?MODULE).
+
+-export([start_link/0]).
+-export([init/1]).
+
+-define(CHILDREN(I), [?WORKER_ARGS_TYPE('cf_listener', [I], 'temporary')]).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Starts the supervisor
+%% @end
+%%--------------------------------------------------------------------
+-spec start_link() -> startlink_ret().
+start_link() ->
+    {'ok', Pid} = supervisor:start_link({'local', ?SERVER}, ?MODULE, []),
+    Workers = kapps_config:get_integer(?CF_CONFIG_CAT, <<"callflow_listeners">>, 10),
+    kz_util:spawn(fun() -> [begin
+                                supervisor:start_child(Pid, []),
+                                timer:sleep(500)
+                            end
+                              || _N <- lists:seq(1, Workers)
+                           ]
+                  end),
+    {'ok', Pid}.
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+%%--------------------------------------------------------------------
+%% @public
+%% @doc
+%% Whenever a supervisor is started using supervisor:start_link/[2,3],
+%% this function is called by the new process to find out about
+%% restart strategy, maximum restart frequency and child
+%% specifications.
+%% @end
+%%--------------------------------------------------------------------
+-spec init(list()) -> sup_init_ret().
+init([]) ->
+    RestartStrategy = 'simple_one_for_one',
+    MaxRestarts = 0,
+    MaxSecondsBetweenRestarts = 1,
+    SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+    Instance = kz_binary:rand_hex(16),
+    {'ok', {SupFlags, ?CHILDREN(Instance)}}.
