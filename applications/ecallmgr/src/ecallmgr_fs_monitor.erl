c31e939045e30aac5d388400b1025da832c140b5
lazedo <luis.azedo@factorlusitano.com>
2017-10-09 10:19:50 +0100

    monitor fs node in a separate process

---
+++ applications/ecallmgr/src/ecallmgr_fs_monitor.erl
@@ -0,0 +1,140 @@
+%%%-----------------------------------------------------------------------------
+%%% @copyright (C) 2010-2017, 2600Hz INC
+%%% @doc
+%%%
+%%% When connecting to a FreeSWITCH node, we create this process
+%%% to monitor the node.
+%%%
+%%% @end
+%%% @contributors
+%%%-----------------------------------------------------------------------------
+-module(ecallmgr_fs_monitor).
+-behaviour(gen_server).
+
+-export([start_link/1, start_link/2]).
+
+
+-export([init/1
+        ,handle_call/3
+        ,handle_cast/2
+        ,handle_info/2
+        ,terminate/2
+        ,code_change/3
+        ]).
+
+-include("ecallmgr.hrl").
+
+-define(SERVER, ?MODULE).
+
+-type state() :: #{}.
+
+%%%===================================================================
+%% @doc Starts the server
+%%%===================================================================
+
+-spec start_link(atom()) -> startlink_ret().
+-spec start_link(atom(), kz_proplist()) -> startlink_ret().
+start_link(Node) -> start_link(Node, []).
+start_link(Node, Options) ->
+    gen_server:start_link(?SERVER, [Node, Options], []).
+
+
+%%%===================================================================
+%%% gen_server callbacks
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Initializes the server
+%% @end
+%%--------------------------------------------------------------------
+-spec init([]) -> {'ok', state()}.
+init([Node, Options]) ->
+    process_flag('trap_exit', 'true'),
+    kz_util:put_callid(Node),
+    erlang:monitor_node(Node, 'true'),
+    {'ok', #{node => Node, options => Options}}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling call messages
+%%
+%% @spec handle_call(Request, From, State) ->
+%%                                   {reply, Reply, State} |
+%%                                   {reply, Reply, State, Timeout} |
+%%                                   {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, Reply, State} |
+%%                                   {stop, Reason, State}
+%% @end
+%% #state{nodes=[{FSNode, HandlerPid}]}
+%%--------------------------------------------------------------------
+-spec handle_call(any(), pid_ref(), state()) -> handle_call_ret_state(state()).
+handle_call(_Request, _From, State) ->
+    {'reply', {'error', 'not_implemented'}, State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling cast messages
+%%
+%% @spec handle_cast(Msg, State) -> {noreply, State} |
+%%                                  {noreply, State, Timeout} |
+%%                                  {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+-spec handle_cast(any(), state()) -> handle_cast_ret_state(state()).
+handle_cast(_Cast, State) ->
+    lager:debug("unhandled cast: ~p", [_Cast]),
+    {'noreply', State, 'hibernate'}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Handling all non call/cast messages
+%%
+%% @spec handle_info(Info, State) -> {noreply, State} |
+%%                                   {noreply, State, Timeout} |
+%%                                   {stop, Reason, State}
+%% @end
+%%--------------------------------------------------------------------
+-spec handle_info(any(), state()) -> handle_info_ret_state(state()).
+handle_info({'nodedown', Node}, State) ->
+    ecallmgr_fs_nodes:nodedown(Node),
+    {'noreply', State};
+handle_info(_Info, State) ->
+    lager:debug("unhandled message: ~p", [_Info]),
+    {'noreply', State}.
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% This function is called by a gen_server when it is about to
+%% terminate. It should be the opposite of Module:init/1 and do any
+%% necessary cleaning up. When it returns, the gen_server terminates
+%% with Reason. The return value is ignored.
+%%
+%% @spec terminate(Reason, State) -> void()
+%% @end
+%%--------------------------------------------------------------------
+-spec terminate(any(), state()) -> 'ok'.
+terminate(_Reason, #{node := Node}) ->
+    lager:debug("fs monitor termination for ~s: ~p", [Node, _Reason]).
+
+%%--------------------------------------------------------------------
+%% @private
+%% @doc
+%% Convert process state when code is changed
+%%
+%% @spec code_change(OldVsn, State, Extra) -> {'ok', NewState}
+%% @end
+%%--------------------------------------------------------------------
+-spec code_change(any(), state(), any()) -> {'ok', state()}.
+code_change(_OldVsn, State, _Extra) ->
+    {'ok', State}.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
