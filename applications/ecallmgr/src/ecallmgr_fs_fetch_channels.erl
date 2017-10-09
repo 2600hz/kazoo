53b691cf40774809274f01f04d1f40481d277629
lazedo <luis.azedo@factorlusitano.com>
2017-10-09 10:34:52 +0100

    plugable fetch handlers

---
+++ applications/ecallmgr/src/ecallmgr_fs_fetch_channels.erl
@@ -0,0 +1,93 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2013-2017, 2600Hz
+%%% @doc
+%%% Track the FreeSWITCH channel information, and provide accessors
+%%% @end
+%%% @contributors
+%%%   James Aimonetti
+%%%   Karl Anderson
+%%%-------------------------------------------------------------------
+-module(ecallmgr_fs_fetch_channels).
+
+-export([channel_req/1]).
+-export([init/0]).
+
+-include("ecallmgr.hrl").
+-include_lib("kazoo_sip/include/kzsip_uri.hrl").
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+-spec init() -> 'ok'.
+init() ->
+    kazoo_bindings:bind(<<"fetch.channels.*">>, ?MODULE, 'channel_req'),
+    'ok'.
+
+
+-spec channel_req(tuple()) -> 'ok'.
+channel_req(#{node := Node, fetch_id := FetchId, jobj := JObj}) ->
+    UUID = kzd_fetch:call_id(JObj),
+    ForUUID = kz_json:get_value(<<"refer-for-channel-id">>, JObj),
+    {'ok', ForChannel} = ecallmgr_fs_channel:fetch(ForUUID, 'proplist'),
+    case ecallmgr_fs_channel:fetch_channel(UUID) of
+        'undefined' -> channel_not_found(Node, FetchId);
+        Channel ->
+            [Uri] = kzsip_uri:uris(props:get_value(<<"switch_url">>, Channel)),
+            URL = kzsip_uri:ruri(
+                    #uri{user= kz_json:get_value(<<"refer-to-user">>, JObj)
+                        ,domain= props:get_value(<<"realm">>, Channel)
+                        ,opts=[{<<"fs_path">>, kzsip_uri:ruri(Uri#uri{user= <<>>})}]
+                        }),
+            build_channel_resp(FetchId, JObj, Node, URL, ForChannel, ecallmgr_fs_channel:channel_ccvs(Channel))
+    end.
+
+-spec build_channel_resp(ne_binary(), kz_json:object(), atom(), ne_binary(), kz_proplist(), kz_proplist()) -> 'ok'.
+build_channel_resp(FetchId, JObj, Node, URL, Channel, ChannelVars) ->
+    %% NOTE
+    %% valid properties to return are
+    %% sip-url , dial-prefix, absolute-dial-string, sip-profile (defaulted to current channel profile)
+    %% freeswitch formats the dial string with the following logic
+    %% if absolute-dial-string => %s%s [dial-prefix, absolute-dial-string]
+    %% else => %ssofia/%s/%s [dial-prefix, sip-profile, sip-url]
+    Resp = props:filter_undefined(
+             [{<<"sip-url">>, URL}
+             ,{<<"dial-prefix">>, channel_resp_dialprefix(JObj, Channel, ChannelVars)}
+             ]),
+    try_channel_resp(FetchId, Node, Resp).
+
+-spec channel_resp_dialprefix(kz_json:object(), kz_proplist(), kz_proplist()) -> ne_binary().
+channel_resp_dialprefix(JObj, Channel, ChannelVars) ->
+    Props = props:filter_undefined(
+              [{<<"sip_invite_domain">>, props:get_value(<<"Realm">>, ChannelVars)}
+              ,{<<"sip_h_X-Core-UUID">>, kz_json:get_value(<<"Core-UUID">>, JObj)}
+              ,{<<"sip_h_X-ecallmgr_", ?CALL_INTERACTION_ID>>, props:get_value(<<"interaction_id">>, Channel)}
+              ,{<<"sip_h_X-ecallmgr_replaces-call-id">>, kz_json:get_value(<<"replaces-call-id">>, JObj)}
+              ,{<<"sip_h_X-ecallmgr_refer-from-channel-id">>, kz_json:get_value(<<"refer-from-channel-id">>, JObj)}
+              ,{<<"sip_h_X-ecallmgr_refer-for-channel-id">>, kz_json:get_value(<<"refer-for-channel-id">>, JObj)}
+              ,{<<"sip_h_X-ecallmgr_Account-ID">>, props:get_value(<<"Account-ID">>, ChannelVars)}
+              ,{<<"sip_h_X-ecallmgr_Realm">>, props:get_value(<<"Realm">>, ChannelVars)}
+              ]),
+    fs_props_to_binary(Props).
+
+-spec fs_props_to_binary(kz_proplist()) -> ne_binary().
+fs_props_to_binary([{Hk,Hv}|T]) ->
+    Rest = << <<",", K/binary, "='", (kz_term:to_binary(V))/binary, "'">> || {K,V} <- T >>,
+    <<"[", Hk/binary, "='", (kz_term:to_binary(Hv))/binary, "'", Rest/binary, "]">>.
+
+-spec try_channel_resp(ne_binary(), atom(), kz_proplist()) -> 'ok'.
+try_channel_resp(FetchId, Node, Props) ->
+    try ecallmgr_fs_xml:sip_channel_xml(Props) of
+        {'ok', ConfigXml} ->
+            lager:debug("sending sofia XML to ~s: ~s", [Node, ConfigXml]),
+            freeswitch:fetch_reply(Node, FetchId, 'channels', erlang:iolist_to_binary(ConfigXml))
+    catch
+        _E:_R ->
+            lager:info("sofia profile resp failed to convert to XML (~s): ~p", [_E, _R]),
+            channel_not_found(Node, FetchId)
+    end.
+
+-spec channel_not_found(atom(), ne_binary()) -> 'ok'.
+channel_not_found(Node, FetchId) ->
+    {'ok', Resp} = ecallmgr_fs_xml:not_found(),
+    freeswitch:fetch_reply(Node, FetchId, 'channels', iolist_to_binary(Resp)).
---
+++ applications/ecallmgr/src/ecallmgr_fs_fetch_dialplan.erl
@@ -0,0 +1,181 @@
+%%%-------------------------------------------------------------------
+%%% @copyright (C) 2011-2017, 2600Hz INC
+%%% @doc
+%%% Receive route(dialplan) requests from FS, request routes and respond
+%%% @end
+%%% @contributors
+%%%   James Aimonetti
+%%%-------------------------------------------------------------------
+-module(ecallmgr_fs_fetch_dialplan).
+
+-export([init/0]).
+-export([dialplan/1]).
+-export([route_winner/1]).
+
+-include("ecallmgr.hrl").
+
+
+-define(ROUTE_WINNER_TIMEOUT, 60 * ?MILLISECONDS_IN_SECOND).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Initializes the server
+%% @end
+%%--------------------------------------------------------------------
+-spec init() -> 'ok'.
+init() ->
+    kazoo_bindings:bind(<<"fetch.dialplan.context_2">>, ?MODULE, 'dialplan'),
+    kazoo_bindings:bind(<<"event_stream.event.dialplan.ROUTE_WINNER">>, ?MODULE, 'route_winner'),
+    'ok'.
+
+-spec dialplan(map()) -> fs_sendmsg_ret().
+dialplan(#{}=Map) ->
+    Routines = [fun control_q/1
+               ,fun call_id/1
+               ,fun timeout/1
+               ,{fun add_time_marker/2, start_processing}
+               ,fun(M) -> M#{callback => fun process/1} end
+               ],
+    ecallmgr_call_control_sup:start_proc(kz_maps:exec(Routines, Map)).
+
+process(#{}=Map) ->
+    Routines = [{fun add_time_marker/2, request_ready}
+               ,fun request/1
+               ,fun control_p/1
+               ,fun timeout_reply/1
+               ],
+    maybe_expired(kz_maps:exec(Routines, Map)).
+
+request(#{request := _Request}=Map) -> Map;
+request(#{node := Node, fetch_id := FetchId, call_id := UUID, control_q := ControlQ, jobj := JObj}=Map) ->
+    Map#{request => ecallmgr_fs_router_util:route_req(ControlQ, UUID, FetchId, kz_json:to_proplist(JObj), Node)}.
+
+control_p(#{control_p := _Pid}=Map) -> Map;
+control_p(#{request := Request}=Map) ->
+    Map#{request => [{<<"Request-From-PID">>, kz_term:to_binary(self())} | Request], control_p => self()}.
+
+control_q(#{control_q := _Queue}= Map) -> Map;
+control_q(#{node := Node}= Map) ->
+    SupPid = whereis(?CALLCTL_SUPERVISOR_NAME(Node)),
+    Listeners = supervisor:which_children(SupPid),
+    Size = length(Listeners),
+    Selected = rand:uniform(Size),
+    {_, ControlP, _, _} = lists:nth(Selected, Listeners),
+    Map#{control_q => gen_listener:queue_name(ControlP)}.
+    
+timeout(#{timeout := _Timeout}=Map) -> Map;
+timeout(#{jobj := JObj}=Map) ->
+    T0 = kz_json:get_integer_value(<<"Fetch-Timestamp-Micro">>, JObj),
+    T1 = kz_json:get_integer_value(<<"Fetch-Timeout">>, JObj),
+    T3 = kz_time:now_us(),
+    T4 = T3 - T0,
+    T5 = T1 - T4,
+    T6 = T5 div 1000,
+    Map#{timeout => T6 - 100}.
+              
+call_id(#{call_id := _CallId}=Map) -> Map;
+call_id(#{jobj := JObj}=Map) ->
+    Map#{call_id => kzd_fetch:call_id(JObj)}.
+
+add_time_marker(Name, #{timer := Timer}= Map) ->
+    Map#{timer => Timer#{Name => kz_time:now_us()}};
+add_time_marker(Name, #{}= Map) ->
+    add_time_marker(Name, Map#{timer => #{}}).
+
+%% add_time_marker(Name, Value, #{timer := Timer}= Map) ->
+%%     Map#{timer => Timer#{Name => Value}};
+%% add_time_marker(Name, Value, #{}= Map) ->
+%%     add_time_marker(Name, Value, Map#{timer => #{}}).
+
+maybe_expired(#{timeout := Timeout}=Map)
+  when Timeout =< 0 ->
+    lager:warning("timeout before sending route request"),
+    send_reply(Map);
+maybe_expired(#{request := Request}=Map) ->
+    kapi_route:publish_req(Request),
+    wait_for_route_resp(add_time_marker(request_sent, Map)).
+
+wait_for_route_resp(#{timeout := Timeout}=Map) ->
+    Now = kz_time:current_tstamp(),
+    receive
+        {'route_resp', Resp, Props} ->
+            case kz_api:defer_response(Resp) of
+                true ->
+                    lager:notice("received deferred reply"),
+                    NewTimeout = Timeout - kz_time:elapsed_ms(Now),
+                    wait_for_route_resp(Map#{timeout => NewTimeout, reply => #{payload => Resp, props => Props}});
+                false ->
+                    lager:info("received route reply"),
+                    send_reply(Map#{reply => #{payload => Resp, props => Props}})
+            end
+    after Timeout ->
+            lager:warning("timeout after ~B receiving route response", [Timeout]),
+            send_reply(Map)
+    end.
+
+send_reply(#{node := Node, fetch_id := FetchId, reply := #{payload := Reply}}=Map) ->
+    Props = maps:to_list(Map),
+    {'ok', XML} = ecallmgr_fs_xml:route_resp_xml('dialplan', Reply, Props),
+    lager:debug("sending xml dialplan reply for request ~s",[FetchId]),
+    freeswitch:fetch_reply(Node, FetchId, 'dialplan', iolist_to_binary(XML)),
+    case kz_api:defer_response(Reply)
+        orelse kz_json:get_ne_binary_value(<<"Method">>, Reply) /= <<"park">>
+    of
+        true -> ok;
+        false -> wait_for_route_win(Map)
+    end.
+
+wait_for_route_win(Map) ->
+    receive
+        {'route_winner', JObj, Props} ->
+            start_call_control(Map#{winner => #{payload => JObj, props => Props}})
+    after ?ROUTE_WINNER_TIMEOUT ->
+            lager:warning("timeout after ~B receiving route winner", [?ROUTE_WINNER_TIMEOUT])
+    end.
+
+        
+
+
+-spec start_call_control(map()) -> 'ok'.
+start_call_control(#{call_id := CallId, winner := #{payload := JObj}} = Map) ->
+    kz_util:put_callid(CallId),
+    CCVs = kzd_fetch:ccvs(JObj),
+    ControllerQ = kzd_fetch:controller_queue(JObj),
+    ControllerP = kzd_fetch:controller_pid(JObj),
+    ecallmgr_fs_channels:deferred_update(CallId, #channel.handling_locally, 'true'),
+    Args = Map#{controller_q => ControllerQ
+               ,controller_p => ControllerP
+               ,initial_ccvs => CCVs
+               },
+    {'ok', Args}.
+
+%% error_message() ->
+%%     error_message(<<"not enough resources">>).
+
+error_message(ErrorMsg) ->
+    error_message(<<"604">>, ErrorMsg).
+
+error_message(ErrorCode, ErrorMsg) ->
+    kz_json:from_list([{<<"Method">>, <<"error">>}
+              ,{<<"Route-Error-Code">>, ErrorCode}
+              ,{<<"Route-Error-Message">>, ErrorMsg}
+              ]).
+
+timeout_reply(Map) ->
+    Map#{reply => #{payload => error_message(<<"no available handlers">>), props => []}}.
+
+
+-spec route_winner(map()) -> fs_sendmsg_ret().
+route_winner({_Node, _UUID, _Category, _Event, JObj}) ->
+    NodeWinner = kzd_fetch:ccv(JObj, <<"Ecallmgr-Node">>),
+    case NodeWinner =:= kz_term:to_binary(node()) of
+        true ->
+            Pid = kz_term:to_pid(kz_api:reply_to(JObj)),
+            Pid ! {'route_winner', JObj, []};
+        false ->
+            lager:debug("route winner handled by other node : ~s", [NodeWinner])
+    end.
