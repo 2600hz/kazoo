689d0873155855069f7fd7826112dabe9d4a7014
lazedo <luis.azedo@factorlusitano.com>
2017-10-13 09:11:33 +0100

    move event_stream processing to map

--- applications/ecallmgr/src/ecallmgr_fs_presence.erl
+++ applications/ecallmgr/src/ecallmgr_fs_presence.erl
@@ -6,262 +6,97 @@
 %%% @contributors
 %%%-------------------------------------------------------------------
 -module(ecallmgr_fs_presence).
--behaviour(gen_server).
 
--export([start_link/1, start_link/2]).
-
--export([init/1
-        ,handle_call/3
-        ,handle_cast/2
-        ,handle_info/2
-        ,terminate/2
-        ,code_change/3
-
-        ]).
+-export([init/0]).
+-export([publish_presence/1]).
 
 -include("ecallmgr.hrl").
-
--define(SERVER, ?MODULE).
-
--record(state, {node = 'undefined' :: atom()
-               ,event :: ne_binary()
-               ,options = [] :: kz_proplist()
-               }).
--type state() :: #state{}.
 
 %%%===================================================================
 %%% API
 %%%===================================================================
 
-%%--------------------------------------------------------------------
-%% @doc Starts the server
-%%--------------------------------------------------------------------
--spec start_link(atom()) -> startlink_ret().
--spec start_link(atom(), kz_proplist()) -> startlink_ret().
-start_link(Node) -> start_link(Node, []).
-start_link(Node, Options) ->
-    gen_server:start_link(?SERVER, [Node, Options], []).
+-spec init() -> 'ok'.
+init() ->
+    kazoo_bindings:bind(<<"event_stream.publish.call_event.PRESENCE_IN">>, ?MODULE, 'publish_presence'),
+    'ok'.
 
-%%%===================================================================
-%%% gen_server callbacks
-%%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Initializes the server
-%%
-%% @spec init(Args) -> {ok, State} |
-%%                     {ok, State, Timeout} |
-%%                     ignore |
-%%                     {stop, Reason}
-%% @end
-%%--------------------------------------------------------------------
--spec init([atom() | kz_proplist()]) -> {'ok', state()}.
-init([Node, Options]) ->
-    kz_util:put_callid(Node),
-    Event = application:get_env(?APP, 'presence_event', <<"PRESENCE_IN">>),
-    gen_server:cast(self(), 'bind_to_record'),
-    {'ok', #state{node=Node, event = Event, options=Options}}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling call messages
-%%
-%% @spec handle_call(Request, From, State) ->
-%%                                   {reply, Reply, State} |
-%%                                   {reply, Reply, State, Timeout} |
-%%                                   {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, Reply, State} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
--spec handle_call(any(), pid_ref(), state()) -> handle_call_ret_state(state()).
-handle_call(_Request, _From, State) ->
-    {'reply', {'error', 'not_implemented'}, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling cast messages
-%%
-%% @spec handle_cast(Msg, State) -> {noreply, State} |
-%%                                  {noreply, State, Timeout} |
-%%                                  {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
--spec handle_cast(any(), state()) -> handle_cast_ret_state(state()).
-handle_cast('bind_to_record', #state{node=Node, event=Event}=State) ->
-    case gproc:reg({'p', 'l', ?FS_EVENT_REG_MSG(Node, Event)}) =:= 'true'
-        andalso gproc:reg({'p', 'l', ?FS_OPTION_MSG(Node)}) =:= 'true'
-    of
-        'true' -> {'noreply', State};
-        'false' -> {'stop', 'gproc_badarg', State}
-    end;
-handle_cast(_Msg, State) ->
-    lager:debug("unhandled cast: ~p", [_Msg]),
-    {'noreply', State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%%
-%% @spec handle_info(Info, State) -> {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
--spec handle_info(any(), state()) -> handle_info_ret_state(state()).
-handle_info({'event', [UUID | Props]}, #state{node=Node
-                                             ,options=Options
-                                             ,event=Event
-                                             }=State) ->
-    _ = kz_util:spawn(fun handle_presence_event/5, [Event, UUID, Props, Node, Options]),
-    {'noreply', State};
-handle_info({'option', K, V}, #state{options=Options}=State) ->
-    {'noreply', State#state{options=props:set_value(K, V, Options)}};
-handle_info(_Other, State) ->
-    lager:debug("unhandled msg: ~p", [_Other]),
-    {'noreply', State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
-%%--------------------------------------------------------------------
--spec terminate(any(), state()) -> ok.
-terminate(_Reason, #state{node=Node}) ->
-    lager:info("presence listener for ~s terminating: ~p", [Node, _Reason]).
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% @end
-%%--------------------------------------------------------------------
--spec code_change(any(), state(), any()) -> {ok, state()}.
-code_change(_OldVsn, State, _Extra) ->
-    {'ok', State}.
-
-%%%===================================================================
-%%% Internal functions
-%%%===================================================================
-
--spec init_props(kz_proplist(), kz_proplist()) -> kz_proplist().
-init_props(Props, Options) ->
-    case props:get_is_true(<<"Publish-Channel-State">>, Props) of
-        'undefined' ->
-            case props:is_false(<<"Publish-Channel-State">>, Options, 'false') of
-                'true' -> props:set_value(<<"Publish-Channel-State">>, 'false', Props);
-                _ -> Props
-            end;
-        _Value -> Props
-    end.
-
--spec handle_presence_event(ne_binary(), api_binary(), kz_proplist(), atom(), kz_proplist()) -> any().
-handle_presence_event(BindingEvent, UUID, FSProps, Node, Options) ->
+-spec publish_presence(map()) -> any().
+publish_presence(#{call_id := UUID} = Ctx) ->
     kz_util:put_callid(UUID),
-    Props = init_props(FSProps, Options),
-    EventName = props:get_value(<<"Event-Subclass">>, Props, props:get_value(<<"Event-Name">>, Props)),
-    process_specific_event(BindingEvent, EventName, UUID, Props, Node).
-
--spec process_specific_event(ne_binary(), ne_binary(), api_binary(), kz_proplist(), atom()) -> any().
-process_specific_event(Event, Event, UUID, Props, Node) ->
-    maybe_build_presence_event(Node, UUID, Props).
-
--spec maybe_build_presence_event(atom(), api_binary(), kz_proplist()) -> any().
-maybe_build_presence_event(Node, UUID, Props) ->
-    Routines = [fun check_proto/3
-               ,fun check_publish_state/3
+    Routines = [fun check_proto/1
                ],
-    case lists:all(fun(F) -> F(Node, UUID, Props) end, Routines) of
-        'true' -> build_presence_event(Node, UUID, Props);
+    case lists:all(fun(F) -> F(Ctx) end, Routines) of
+        'true' -> build_presence_event(UUID, Ctx);
         'false' -> 'ok'
     end.
 
--spec check_proto(atom(), api_binary(), kz_proplist()) -> boolean().
-check_proto(_Node, _UUID, Props) ->
-    Proto = props:get_value(<<"proto">>, Props),
-    check_proto(Proto).
+-spec check_proto(map()) -> boolean().
+check_proto(#{payload := JObj}) ->
+    is_proto_handled(kz_json:get_value(<<"proto">>, JObj)).
 
--spec check_proto(api_binary()) -> boolean().
-check_proto(<<"any">>) -> 'true';
-check_proto(_Proto) ->
+-spec is_proto_handled(api_binary()) -> boolean().
+is_proto_handled(<<"any">>) -> 'true';
+is_proto_handled(_Proto) ->
     lager:debug("presence proto ~p not handled", [_Proto]),
     'false'.
 
--spec check_publish_state(atom(), api_binary(), kz_proplist()) -> boolean().
-check_publish_state(_Node, _UUID, Props) ->
-    props:is_true(<<"Force-Publish-Event-State">>, Props, 'false')
-        orelse props:is_true(<<"Publish-Channel-State">>, Props, 'true').
+-spec realm(kz_json:object()) -> ne_binary().
+realm(JObj) ->
+    kz_json:get_first_defined([?GET_CCV(<<"Realm">>)
+                              ,<<"variable_sip_invite_domain">>
+                              ,<<"variable_sip_auth_realm">>
+                              ,<<"variable_sip_to_host">>
+                              ], JObj, ?DEFAULT_REALM).
 
--spec realm(kz_proplist()) -> ne_binary().
-realm(Props) ->
-    props:get_first_defined([?GET_CCV(<<"Realm">>)
-                            ,<<"variable_sip_invite_domain">>
-                            ,<<"variable_sip_auth_realm">>
-                            ,<<"variable_sip_to_host">>
-                            ], Props, ?DEFAULT_REALM).
-
--spec get_user_realm(kz_proplist()) -> {ne_binary(), ne_binary()}.
-get_user_realm(Props) ->
-    case binary:split(from(Props), <<"@">>, ['global']) of
+-spec get_user_realm(kz_json:object()) -> {ne_binary(), ne_binary()}.
+get_user_realm(JObj) ->
+    case binary:split(from(JObj), <<"@">>, ['global']) of
         [Username, Realm | _] -> {Username, Realm};
-        [From] -> {From, realm(Props)}
+        [From] -> {From, realm(JObj)}
     end.
 
--spec from(kz_proplist()) -> ne_binary().
-from(Props) ->
-    props:get_first_defined([<<"from">>
-                            ,<<"variable_presence_id">>
-                            ,<<"Channel-Presence-ID">>
-                            ], Props).
+-spec from(kz_json:object()) -> ne_binary().
+from(JObj) ->
+    kz_json:get_first_defined([<<"from">>
+                              ,<<"variable_presence_id">>
+                              ,<<"Channel-Presence-ID">>
+                              ], JObj).
 
--spec to_user(kz_proplist()) -> ne_binary().
-to_user(Props) ->
-    to_user(direction(Props), Props).
+-spec to_user(kz_json:object()) -> ne_binary().
+to_user(JObj) ->
+    to_user(direction(JObj), JObj).
 
-to_user(<<"initiator">>, Props) ->
-    props:get_first_defined([<<"Caller-Destination-Number">>
-                            ,<<"variable_sip_to_user">>
-                            ], Props);
-to_user(<<"recipient">>, Props) ->
-    props:get_first_defined([<<"Caller-Caller-ID-Number">>
-                            ,<<"variable_sip_from_user">>
-                            ], Props).
+to_user(<<"initiator">>, JObj) ->
+    kz_json:get_first_defined([<<"Caller-Destination-Number">>
+                              ,<<"variable_sip_to_user">>
+                              ], JObj);
+to_user(<<"recipient">>, JObj) ->
+    kz_json:get_first_defined([<<"Caller-Caller-ID-Number">>
+                              ,<<"variable_sip_from_user">>
+                              ], JObj).
 
 -spec expires(ne_binary()) -> integer().
 expires(<<"early">>) -> 0;
 expires(<<"confirmed">>) -> 0;
 expires(<<"terminated">>) -> 20.
 
--spec build_presence_event(atom(), api_binary(), kz_proplist()) -> any().
-build_presence_event(_Node, UUID, Props) ->
-    ToTag = kzd_freeswitch:to_tag(Props),
-    FromTag = kzd_freeswitch:from_tag(Props),
+-spec build_presence_event(api_binary(), map()) -> any().
+build_presence_event(UUID, #{payload := JObj}) ->
+    ToTag = kzd_freeswitch:to_tag(JObj),
+    FromTag = kzd_freeswitch:from_tag(JObj),
 
-    {FromUser, Realm} = get_user_realm(Props),
+    {FromUser, Realm} = get_user_realm(JObj),
     PresenceId = <<FromUser/binary, "@", Realm/binary>>,
     PresenceURI =  <<"sip:", PresenceId/binary>>,
 
-    ToUser =  to_user(Props),
+    ToUser =  to_user(JObj),
     To =  <<ToUser/binary, "@", Realm/binary>>,
     ToURI =  <<"sip:", To/binary>>,
 
-    State = presence_status(Props),
+    State = presence_status(JObj),
     Expires = expires(State),
-    SwitchURI = props:get_value(<<"Switch-URI">>, Props),
+    SwitchURI = kzd_freeswitch:switch_uri(JObj),
 
     Payload = props:filter_undefined(
                 [{<<"Presence-ID">>, PresenceId}
@@ -277,7 +112,7 @@ build_presence_event(_Node, UUID, Props) ->
                 ,{<<"To-Tag">>, ToTag}
                 ,{<<"To-URI">>, PresenceURI}
 
-                ,{<<"Direction">>, direction(Props)}
+                ,{<<"Direction">>, direction(JObj)}
                 ,{<<"State">>, State}
                 ,{<<"Call-ID">>, UUID}
                 ,{<<"Switch-URI">>, SwitchURI}
@@ -288,26 +123,26 @@ build_presence_event(_Node, UUID, Props) ->
     lager:debug("sending presence ~s to ~s/~s in realm ~s", [State, FromUser, ToUser, Realm]),
     kz_amqp_worker:cast(Payload, fun kapi_presence:publish_dialog/1).
 
--spec direction(kz_proplist()) -> ne_binary().
-direction(Props) ->
-    case props:get_value(<<"Presence-Call-Direction">>, Props) of
+-spec direction(kz_json:object()) -> ne_binary().
+direction(JObj) ->
+    case kz_json:get_value(<<"Presence-Call-Direction">>, JObj) of
         <<"inbound">> -> <<"initiator">>;
         <<"outbound">> -> <<"recipient">>
     end.
 
--spec status(kz_proplist()) -> ne_binary().
-status(Props) ->
-    kz_term:to_lower_binary(props:get_binary_value(<<"status">>, Props)).
+-spec status(kz_json:object()) -> ne_binary().
+status(JObj) ->
+    kz_term:to_lower_binary(kz_json:get_binary_value(<<"status">>, JObj)).
 
--spec answer_state(kz_proplist()) -> ne_binary().
-answer_state(Props) ->
-    kz_term:to_lower_binary(props:get_value(<<"Answer-State">>, Props)).
+-spec answer_state(kz_json:object()) -> ne_binary().
+answer_state(JObj) ->
+    kz_term:to_lower_binary(props:get_value(<<"Answer-State">>, JObj)).
 
--spec presence_status(kz_proplist()) -> ne_binary().
-presence_status(Props) ->
-    Status = status(Props),
-    AnswerState = answer_state(Props),
-    Direction = direction(Props),
+-spec presence_status(kz_json:object()) -> ne_binary().
+presence_status(JObj) ->
+    Status = status(JObj),
+    AnswerState = answer_state(JObj),
+    Direction = direction(JObj),
     presence_status(Direction, Status, AnswerState).
 
 -spec presence_status(ne_binary(), ne_binary(), ne_binary()) -> ne_binary().
--- applications/ecallmgr/src/ecallmgr_fs_recordings.erl
+++ applications/ecallmgr/src/ecallmgr_fs_recordings.erl
@@ -1,228 +1,45 @@
 %%%-------------------------------------------------------------------
 %%% @copyright (C) 2011-2017, 2600Hz INC
 %%% @doc
-%%% Receives START/STOP RECORD event
+%%% Receives STOP RECORD event
 %%% @end
 %%% @contributors
 %%%-------------------------------------------------------------------
 -module(ecallmgr_fs_recordings).
--behaviour(gen_server).
 
--export([start_link/1, start_link/2]).
-
--export([init/1
-        ,handle_call/3
-        ,handle_cast/2
-        ,handle_info/2
-        ,terminate/2
-        ,code_change/3
-        ]).
+-export([init/0]).
+-export([handle_record_stop/1]).
 
 -include("ecallmgr.hrl").
 
--define(SERVER, ?MODULE).
+-spec init() -> 'ok'.
+init() ->
+    kazoo_bindings:bind(<<"event_stream.event.call_event.RECORD_STOP">>, ?MODULE, 'handle_record_stop'),
+    'ok'.
 
--record(state, {node = 'undefined' :: atom()
-               ,options = [] :: kz_proplist()
-               }).
--type state() :: #state{}.
-
-%%%===================================================================
-%%% API
-%%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @doc Starts the server
-%%--------------------------------------------------------------------
--spec start_link(atom()) -> startlink_ret().
--spec start_link(atom(), kz_proplist()) -> startlink_ret().
-start_link(Node) -> start_link(Node, []).
-start_link(Node, Options) ->
-    gen_server:start_link(?SERVER, [Node, Options], []).
-
-%%%===================================================================
-%%% gen_server callbacks
-%%%===================================================================
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Initializes the server
-%%
-%% @spec init(Args) -> {ok, State} |
-%%                     {ok, State, Timeout} |
-%%                     ignore |
-%%                     {stop, Reason}
-%% @end
-%%--------------------------------------------------------------------
--spec init(list()) -> {'ok', state()}.
-init([Node, Options]) ->
-    process_flag('trap_exit', 'true'),
-    kz_util:put_callid(Node),
-    gen_server:cast(self(), 'bind_to_record'),
-    {'ok', #state{node=Node, options=Options}}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling call messages
-%%
-%% @spec handle_call(Request, From, State) ->
-%%                                   {reply, Reply, State} |
-%%                                   {reply, Reply, State, Timeout} |
-%%                                   {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, Reply, State} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
--spec handle_call(any(), pid_ref(), state()) -> handle_call_ret_state(state()).
-handle_call(_Request, _From, State) ->
-    {'reply', {'error', 'not_implemented'}, State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling cast messages
-%%
-%% @spec handle_cast(Msg, State) -> {noreply, State} |
-%%                                  {noreply, State, Timeout} |
-%%                                  {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
--spec handle_cast(any(), state()) -> handle_cast_ret_state(state()).
-handle_cast('bind_to_record', #state{node=Node}=State) ->
-    case gproc:reg({'p', 'l', ?FS_EVENT_REG_MSG(Node, <<"RECORD_START">>)}) =:= 'true'
-        andalso gproc:reg({'p', 'l', ?FS_EVENT_REG_MSG(Node, <<"RECORD_STOP">>)}) =:= 'true'
-        andalso gproc:reg({'p', 'l', ?FS_OPTION_MSG(Node)}) =:= 'true'
-    of
-        'true' -> {'noreply', State};
-        'false' -> {'stop', 'gproc_badarg', State}
-    end;
-handle_cast(_Msg, State) ->
-    lager:debug("unhandled cast: ~p", [_Msg]),
-    {'noreply', State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Handling all non call/cast messages
-%%
-%% @spec handle_info(Info, State) -> {noreply, State} |
-%%                                   {noreply, State, Timeout} |
-%%                                   {stop, Reason, State}
-%% @end
-%%--------------------------------------------------------------------
--spec handle_info(any(), state()) -> handle_info_ret_state(state()).
-handle_info({'event', Props}, #state{node=Node
-                                    ,options=Options
-                                    }=State) ->
-    _ = kz_util:spawn(fun handle_record_event/3, [Node, Props, Options]),
-    {'noreply', State};
-handle_info({'EXIT', _, 'noconnection'}, State) ->
-    {stop, {'shutdown', 'noconnection'}, State};
-handle_info({'EXIT', _, Reason}, State) ->
-    {stop, Reason, State};
-handle_info({'option', K, V}, #state{options=Options}=State) ->
-    {'noreply', State#state{options=props:set_value(K, V, Options)}};
-handle_info(_Other, State) ->
-    lager:debug("unhandled msg: ~p", [_Other]),
-    {'noreply', State}.
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% This function is called by a gen_server when it is about to
-%% terminate. It should be the opposite of Module:init/1 and do any
-%% necessary cleaning up. When it returns, the gen_server terminates
-%% with Reason. The return value is ignored.
-%%
-%% @spec terminate(Reason, State) -> void()
-%% @end
-%%--------------------------------------------------------------------
--spec terminate(any(), state()) -> 'ok'.
-terminate(_Reason, #state{node=Node}) ->
-    lager:info("recording handler for ~s terminating: ~p", [Node, _Reason]).
-
-%%--------------------------------------------------------------------
-%% @private
-%% @doc
-%% Convert process state when code is changed
-%%
-%% @spec code_change(OldVsn, State, Extra) -> {ok, NewState}
-%% @end
-%%--------------------------------------------------------------------
--spec code_change(any(), state(), any()) -> {'ok', state()}.
-code_change(_OldVsn, State, _Extra) ->
-    {'ok', State}.
-
-%%%===================================================================
-%%% Internal functions
-%%%===================================================================
--spec init_props(kz_proplist(), kz_proplist()) -> kz_proplist().
-init_props(Props, Options) ->
-    case props:get_is_true(<<"Publish-Channel-State">>, Props) of
-        'undefined' ->
-            case props:is_false(<<"Publish-Channel-State">>, Options, 'false') of
-                'true' -> props:set_value(<<"Publish-Channel-State">>, 'false', Props);
-                _ -> Props
-            end;
-        _Value -> Props
-    end.
-
--spec handle_record_event(atom(), kz_proplist(), kz_proplist()) -> 'ok'.
-handle_record_event(Node, [UUID | FSProps], Options) ->
+-spec handle_record_stop(map()) -> any().
+handle_record_stop(#{node := Node, call_id := UUID, payload := JObj}) ->
     kz_util:put_callid(UUID),
-    Props = init_props(FSProps, Options),
-    process_event(UUID, Props, Node),
-    maybe_publish_record_event(Props).
-
--spec process_event(api_binary(), kz_proplist(), atom()) -> any().
-process_event(UUID, Props, Node) ->
-    kz_util:put_callid(UUID),
-    EventName = kzd_freeswitch:event_name(Props),
-    process_specific_event(EventName, UUID, Props, Node).
-
--spec process_specific_event(ne_binary(), api_binary(), kz_proplist(), atom()) -> any().
-process_specific_event(<<"RECORD_STOP">>, UUID, Props, Node) ->
-    IsLocal = props:is_true(<<"Force-Publish-Event-State">>, Props, 'true')
-        orelse (props:is_true(<<"Publish-Channel-State">>, Props, 'true')
-                andalso handling_locally(Props)
-               ),
-    Args = [IsLocal, kzd_freeswitch:media_recorder(Props), Props, UUID, Node],
-    kz_util:spawn(fun maybe_store_recording/5, Args);
-process_specific_event(<<"RECORD_START">>, _UUID, _Props, _Node) -> 'ok';
-process_specific_event(_Event, _UUID, _Props, _Node) ->
-    lager:debug("event ~s for callid ~s not handled in recordings (~s)", [_Event, _UUID, _Node]).
-
-
--spec maybe_publish_record_event(kz_proplist()) -> 'ok'.
-maybe_publish_record_event(Props) ->
-    case props:is_true(<<"Force-Publish-Event-State">>, Props, 'true')
-        orelse (props:is_true(<<"Publish-Channel-State">>, Props, 'true')
-                andalso handling_locally(Props)
-               )
-    of
-        'true' -> ecallmgr_call_events:process_channel_event(Props);
-        'false' -> lager:debug("not publishing record event ~s", [kzd_freeswitch:event_name(Props)])
-    end.
+    IsLocal = handling_locally(JObj),
+    MediaRecorder = kz_call_event:custom_channel_var(JObj, <<"Media-Recorder">>),
+    maybe_store_recording(IsLocal, MediaRecorder, JObj, UUID, Node).
 
 -spec maybe_store_recording(boolean(), api_binary(), kz_proplist(), ne_binary(), atom()) ->
                                    'ok' |
                                    'error' |
                                    ecallmgr_util:send_cmd_ret() |
                                    [ecallmgr_util:send_cmd_ret(),...].
-maybe_store_recording('false', _, _Props, _CallId, _Node) -> 'ok';
+maybe_store_recording('false', _, _JObj, _CallId, _Node) -> 'ok';
 maybe_store_recording('true', <<"kz_media_recording">>, _Props, _CallId, _Node) -> 'ok';
-maybe_store_recording('true', _, Props, CallId, Node) ->
-    case kzd_freeswitch:ccv(Props, <<"Media-Transfer-Destination">>) of
+maybe_store_recording('true', _, JObj, CallId, Node) ->
+    case kz_call_event:custom_channel_var(JObj, <<"Media-Transfer-Destination">>) of
         'undefined' -> 'ok';
         <<>> -> 'ok';
         <<_/binary>> = Destination ->
             kz_util:put_callid(CallId),
             lager:debug("no one is handling call recording, storing recording to ~s", [Destination]),
 
-            MediaName = kzd_freeswitch:ccv(Props, <<"Media-Name">>),
+            MediaName = kz_call_event:custom_channel_var(JObj, <<"Media-Name">>),
             %% TODO: if you change this logic be sure it matches kz_media_util as well!
             Url = kz_binary:join([kz_binary:strip_right(Destination, $/)
                                  ,MediaName
@@ -236,7 +53,7 @@ maybe_store_recording('true', _, Props, CallId, Node) ->
                      ,{<<"Media-Name">>, MediaName}
                      ,{<<"Media-Transfer-Destination">>, Url}
                      ,{<<"Insert-At">>, <<"now">>}
-                     ,{<<"Media-Transfer-Method">>, media_transfer_method(Props)}
+                     ,{<<"Media-Transfer-Method">>, media_transfer_method(JObj)}
                      ,{<<"Application-Name">>, <<"store">>}
                      ,{<<"Event-Category">>, <<"call">>}
                      ,{<<"Event-Name">>, <<"command">>}
@@ -246,20 +63,19 @@ maybe_store_recording('true', _, Props, CallId, Node) ->
     end.
 
 -spec media_transfer_method(kz_proplist()) -> ne_binary().
-media_transfer_method(Props) ->
-    kzd_freeswitch:ccv(Props, <<"Media-Transfer-Method">>, <<"put">>).
+media_transfer_method(JObj) ->
+    kz_call_event:custom_channel_var(JObj, <<"Media-Transfer-Method">>, <<"put">>).
 
 -spec handling_locally(kz_proplist()) -> boolean().
-handling_locally(Props) ->
-    handling_locally(Props, kzd_freeswitch:other_leg_call_id(Props)).
+handling_locally(JObj) ->
+    handling_locally(JObj, kz_call_event:other_leg_call_id(JObj)).
 
--spec handling_locally(kz_proplist(), api_binary()) -> boolean().
-handling_locally(Props, 'undefined') ->
-    props:get_value(?GET_CCV(<<"Ecallmgr-Node">>), Props)
-        =:= kz_term:to_binary(node());
-handling_locally(Props, OtherLeg) ->
+-spec handling_locally(kz_json:object(), api_binary()) -> boolean().
+handling_locally(JObj, 'undefined') ->
+    kz_call_event:custom_channel_var(JObj, <<"Ecallmgr-Node">>) =:= kz_term:to_binary(node());
+handling_locally(JObj, OtherLeg) ->
     Node = kz_term:to_binary(node()),
-    case props:get_value(?GET_CCV(<<"Ecallmgr-Node">>), Props) of
+    case kz_call_event:custom_channel_var(JObj, <<"Ecallmgr-Node">>) of
         Node -> 'true';
         _ -> other_leg_handling_locally(OtherLeg)
     end.
