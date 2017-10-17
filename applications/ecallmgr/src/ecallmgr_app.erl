cc197ad0fe33785beb384ecda46f6c3f7367844e
lazedo <luis.azedo@factorlusitano.com>
2017-10-17 02:50:42 +0100

    update event stream handlers

--- applications/ecallmgr/src/ecallmgr_app.erl
+++ applications/ecallmgr/src/ecallmgr_app.erl
@@ -80,6 +80,7 @@ node_bindings() ->
                           ,'ecallmgr_call_event_publisher'
                           ,'ecallmgr_fs_conference_stream'
                           ,'ecallmgr_fs_conference_publish'
+                          ,'ecallmgr_fs_presence'
                           ]).
 
 -define(EVENTSTREAM_PUBLISHERS_MODS, ['ecallmgr_call_event_publisher'
