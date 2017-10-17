3036bdfdca37d73a8b751b9f6ed2dac91a421049
lazedo <luis.azedo@factorlusitano.com>
2017-10-17 03:05:51 +0100

    update fs event filter

--- applications/ecallmgr/src/fs_event_filters.hrl
+++ applications/ecallmgr/src/fs_event_filters.hrl
@@ -2,44 +2,9 @@
 
 -define(FS_GENERATED_EVENT_FILTERS
        ,[<<"Acquired-UUID">>
-        ,<<"Auth-Realm">>
-        ,<<"Call-Direction">>
-        ,<<"Caller-Destination-Number">>
-        ,<<"Channel-Presence-ID">>
-        ,<<"Event-Name">>
         ,<<"Event-Subclass">>
-        ,<<"Hunt-Destination-Number">>
-        ,<<"Other-Leg-ANI">>
-        ,<<"Other-Leg-Callee-ID-Number">>
-        ,<<"Other-Leg-Caller-ID-Number">>
-        ,<<"Other-Leg-RDNIS">>
         ,<<"Resigning-UUID">>
-        ,<<"X-AUTH-IP">>
-        ,<<"X-AUTH-PORT">>
-        ,<<"action">>
-        ,<<"domain">>
-        ,<<"expires">>
-        ,<<"ip">>
-        ,<<"port">>
-        ,<<"sip_auth_nonce">>
-        ,<<"sip_auth_realm">>
-        ,<<"sip_auth_response">>
-        ,<<"sip_auth_uri">>
-        ,<<"sip_call_id">>
-        ,<<"sip_request_host">>
-        ,<<"sip_to_host">>
-        ,<<"sip_user_agent">>
-        ,<<"user">>
-        ,<<"variable_sip_auth_realm">>
-        ,<<"variable_sip_from_uri">>
-        ,<<"variable_sip_from_user">>
-        ,<<"variable_sip_loopback_req_uri">>
         ,<<"variable_sip_refer_to">>
-        ,<<"variable_sip_req_host">>
-        ,<<"variable_sip_req_uri">>
-        ,<<"variable_sip_to_host">>
-        ,<<"variable_sip_to_uri">>
-        ,<<"variable_sip_to_user">>
         ]).
 
 -define(FS_GENERATED_EVENT_FILTERS_HRL, 'true').
