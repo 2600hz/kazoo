a02a807eea4eb8c74b6f49fceb19777c403d8d6e
lazedo <luis.azedo@factorlusitano.com>
2017-10-18 12:08:04 +0100

    use proper function to clear helper

--- applications/callflow/src/cf_exe.erl
+++ applications/callflow/src/cf_exe.erl
@@ -691,7 +691,7 @@ terminate(_Reason, #state{cf_module_pid='undefined'
 terminate(_Reason, #state{call=Call
                          ,cf_module_pid='undefined'
                          }) ->
-    hangup_call(kapps_call:clear_control_helper(Call)),
+    hangup_call(kapps_call:clear_control_queue_helper(Call)),
     lager:info("callflow execution has been stopped: ~p", [_Reason]);
 terminate(_Reason, #state{cf_module_pid={Pid, _}
                          ,destroyed='true'
