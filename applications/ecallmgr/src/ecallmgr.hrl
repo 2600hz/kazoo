1e85ad5cb267e91c7d4e78b36cbae753579ed361
lazedo <luis.azedo@factorlusitano.com>
2017-10-19 20:39:36 +0100

    add call_pickup to list of fs apps

--- applications/ecallmgr/src/ecallmgr.hrl
+++ applications/ecallmgr/src/ecallmgr.hrl
@@ -371,6 +371,7 @@
                               ,{<<"privacy">>, <<"privacy">>}
                               ,{<<"conference">>, <<"page">>}
                               ,{<<"playback">>, <<"play_macro">>}
+                              ,{<<"intercept">>, <<"call_pickup">>}
                               ]).
 
 -define(FAX_EVENTS, ['spandsp::txfaxresult'
