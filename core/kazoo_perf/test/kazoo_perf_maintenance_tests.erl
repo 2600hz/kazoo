e049b34e1c00b05e62a407ec88faee4de6c9f49b
Hesaam Farhang <hesaam@2600hz.com>
2017-10-19 14:53:43 -0700

    please pass

--- core/kazoo_perf/test/kazoo_perf_maintenance_tests.erl
+++ core/kazoo_perf/test/kazoo_perf_maintenance_tests.erl
@@ -5,6 +5,10 @@
 metrics_test_() ->
     {A, C, Z} = {<<"my_account">>, <<"my_cluster">>, <<"my_zone">>},
     %% We only care about if this crashes
-    [?_assertEqual(no_return, kazoo_perf_maintenance:graphite_metrics(A, C, Z))
-    ,?_assertEqual(no_return, kazoo_perf_maintenance:json_metrics())
+    [{"Testing Graphite metrics"
+     ,?_assertEqual(no_return, kazoo_perf_maintenance:graphite_metrics(A, C, Z))
+     }
+    ,{"Testing JSON metrics"
+     ,?_assertEqual(no_return, kazoo_perf_maintenance:json_metrics())
+     }
     ].
