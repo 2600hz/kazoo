44f6cba4f3ecb1f82a3e3a4923ca32686d716c3b
James Aimonetti <james@2600hz.com>
2017-10-25 16:59:48 -0700

    use metaflows schema
    format
    
    default data object to empty json
    
    formatting
    
    remove old metaflow.*.json schemas in favor of auto-gen metaflows.*.json

--- core/kazoo_schemas/src/kz_json_schema_extensions.erl
+++ core/kazoo_schemas/src/kz_json_schema_extensions.erl
@@ -36,23 +36,24 @@ extra_validation(Value, State) ->
 extra_validation(<<"metaflow.data">>, Value, State) ->
     JObj = jesse_state:get_current_value(State),
     [_Data | Path] = jesse_state:get_current_path(State),
-    Module = jesse_json_path:path(lists:reverse([<<"module">> | Path]), JObj, undefined),
+    Module = jesse_json_path:path(lists:reverse([<<"module">> | Path]), JObj, 'undefined'),
     lager:debug("validating metaflow action '~s' with data ~p", [Module, Value]),
-    validate_module_data(<<"metaflow.", Module/binary>>, Value, State);
+    validate_module_data(<<"metaflows.", Module/binary>>, Value, State);
+
 extra_validation(<<"metaflow.module">>, Value, State) ->
     lager:debug("validating metaflow action '~s'", [Value]),
     Schema = <<"metaflows.", Value/binary>>,
     State1 = jesse_state:resolve_ref(State, Schema),
     State2 = case jesse_state:get_current_schema_id(State1) of
                  Schema -> State1;
-                 _OtherSchema -> jesse_error:handle_data_invalid(external_error, <<"unable to find metaflow schema for module ", Value/binary>>, State)
+                 _OtherSchema -> jesse_error:handle_data_invalid('external_error', <<"unable to find metaflow schema for module ", Value/binary>>, State)
              end,
     jesse_state:undo_resolve_ref(State2, State);
 extra_validation(<<"callflows.action.data">>, Value, State) ->
     JObj = jesse_state:get_current_value(State),
     [_Data | Path] = jesse_state:get_current_path(State),
     case jesse_json_path:path(lists:reverse([<<"module">> | Path]), JObj, undefined) of
-        undefined -> State;
+        'undefined' -> State;
         Module -> validate_module_data(<<"callflows.", Module/binary>>, Value, State)
     end;
 extra_validation(<<"callflows.action.module">>, Value, State) ->
@@ -70,9 +71,9 @@ extra_validation(<<"storage.plan.database.document.connection">>, Value, State) 
     case lists:member(Value, Keys) of
         'true' -> State;
         'false' -> jesse_error:handle_data_invalid('external_error'
-                                                  ,?INVALID_STORAGE_CONNECTION_REFERENCE(Value)
-                                                  ,State
-                                                  )
+                                                 ,?INVALID_STORAGE_CONNECTION_REFERENCE(Value)
+                                                 ,State
+                                                 )
     end;
 extra_validation(<<"storage.plan.database.attachment.handler">>, Value, State) ->
     JObj = jesse_state:get_current_value(State),
@@ -80,9 +81,9 @@ extra_validation(<<"storage.plan.database.attachment.handler">>, Value, State) -
     case lists:member(Value, Keys) of
         'true' -> State;
         'false' -> jesse_error:handle_data_invalid('external_error'
-                                                  ,?INVALID_STORAGE_ATTACHMENT_REFERENCE(Value)
-                                                  ,State
-                                                  )
+                                                 ,?INVALID_STORAGE_ATTACHMENT_REFERENCE(Value)
+                                                 ,State
+                                                 )
     end;
 extra_validation(_Key, _Value, State) ->
     lager:debug("extra validation of ~s not handled for value ~p", [_Key, _Value]),
