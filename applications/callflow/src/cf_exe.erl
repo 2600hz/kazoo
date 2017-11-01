0cbc5c0b5e261ee8f7ec3ab081c624a99f159470
James Aimonetti <james.aimonetti@gmail.com>
2017-11-01 22:36:17 +0000

    branch based on CHANNEL_PIVOT event
    pivot an in-progress call
    
    stop executing cf action if running
    
    doc the pivot "metaflow"
    
    remove pivot from channels api

--- applications/callflow/src/cf_exe.erl
+++ applications/callflow/src/cf_exe.erl
@@ -611,6 +611,8 @@ handle_event(JObj, #state{cf_module_pid=PidRef
             handle_channel_replaced(Call, JObj, Notify);
         {{<<"call_event">>, <<"CHANNEL_BRIDGE">>}, _} ->
             handle_channel_bridged(Self, Notify, JObj, Call);
+        {{<<"call_event">>, <<"CHANNEL_PIVOT">>}, CallId} ->
+            handle_channel_pivoted(Self, PidRef, JObj);
         {{<<"call_event">>, <<"usurp_control">>}, CallId} ->
             handle_usurp(Self, Call, JObj);
         {{<<"error">>, _}, _} ->
@@ -887,3 +889,18 @@ hangup_call(Call) ->
           ,{<<"Insert-At">>, <<"tail">>}
           ],
     send_command(Cmd, kapps_call:control_queue_direct(Call), kapps_call:call_id_direct(Call)).
+
+-spec handle_channel_pivoted(server_ref(), api_pid_ref(), kz_call_event:doc()) -> 'ok'.
+handle_channel_pivoted(Self, PidRef, JObj) ->
+    case kz_json:get_ne_binary_value(<<"Application-Data">>, JObj) of
+        'undefined' -> lager:info("no app data to pivot");
+        FlowBin ->
+            lager:debug("pivoting to ~s", [FlowBin]),
+            maybe_stop_action(PidRef),
+            branch(kz_json:decode(FlowBin), Self)
+    end.
+
+-spec maybe_stop_action(api_pid_ref()) -> 'ok'.
+maybe_stop_action({Pid, _Ref}) ->
+    exit(Pid, 'normal');
+maybe_stop_action('undefined') -> 'ok'.
