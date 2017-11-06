23f20a82e23e0afac8205f23128237baf7ce22eb
James Aimonetti <james@2600hz.com>
2017-11-06 14:10:43 -0800

    re-add json stuff

--- core/kazoo_amqp/src/api/kapi_conference.erl
+++ core/kazoo_amqp/src/api/kapi_conference.erl
@@ -340,6 +340,7 @@
 -define(DIAL_TYPES, [{<<"Caller-ID-Name">>, fun is_binary/1}
                     ,{<<"Caller-ID-Number">>, fun is_binary/1}
                     ,{<<"Endpoints">>, fun kz_term:is_ne_list/1}
+                    ,{<<"Custom-Channel-Vars">>, fun kz_json:is_json_object/1}
                     ]).
 
 %% Conference Participants Event
