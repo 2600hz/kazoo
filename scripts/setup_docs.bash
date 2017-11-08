dd5182a781d879f085609b43601ee49b9b9902b6
Hesaam Farhang <hesaam@2600hz.com>
2017-11-08 11:21:12 -0800

    fix setup_docs.bash DOCS_ROOT path

--- scripts/setup_docs.bash
+++ scripts/setup_docs.bash
@@ -4,10 +4,10 @@
 pushd $(dirname $0) > /dev/null
 
 cd $(pwd -P)/..
-DOCS_ROOT=`cd "$(dirname './doc/mkdocs')" && pwd -P`
+DOCS_ROOT=`cd ./doc/mkdocs && pwd -P`
 
 if [ -z `command -v cpio` ] ; then
-	echo "cpio command is not available, please install it" && exit 1
+    echo "cpio command is not available, please install it" && popd > /dev/null && exit 1
 fi
 
 find {scripts,doc,core,applications} -type f -path "doc/mkdocs*" -prune -o -regex ".+\.\(md\|png\|jpg\|svg|json\)$" -print | cpio -p -dum --quiet $DOCS_ROOT/docs
