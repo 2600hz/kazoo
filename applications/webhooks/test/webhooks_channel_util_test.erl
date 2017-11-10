8f33058d5da53f2df36f2dda6209dd0654fd20ed
James Aimonetti <james@2600hz.com>
2017-11-10 09:34:54 -0800

    remove check on loopback headers
    These exist on the "real" b-leg as well, filtering out events we
    actually want to send. Channel-Is-Loopback is only on CHANNEL_DESTROY
    so a new method of detection is needed for non-DESTROY
    events (probably Channel-Name having the "loopback/" prefix).

--- applications/webhooks/test/webhooks_channel_util_test.erl
+++ applications/webhooks/test/webhooks_channel_util_test.erl
@@ -4,10 +4,6 @@
 -include("webhooks.hrl").
 
 -define(IS_LOOPBACK_EVT, kz_json:from_list([{<<"Channel-Is-Loopback">>, 'true'}])).
--define(IS_LOOPBACK_ALT_EVT, [kz_json:from_list([{<<"Channel-Loopback-Leg">>, <<"A">>}])
-                             ,kz_json:from_list([{<<"Channel-Loopback-Bowout-Execute">>, 'true'}])
-                             ,kz_json:from_list([{<<"Channel-Loopback-Other-Leg-ID">>, <<"boo">>}])
-                             ]).
 -define(IS_NOT_LOOPBACK_EVT, kz_json:from_list([{<<"Hangup-Cause">>, <<"PEBCAK">>}])).
 
 -define(INC_LOOPBACK_HOOK, #webhook{include_loopback='true'}).
@@ -19,16 +15,6 @@ include_loopback_test_() ->
 
             ,{?IS_NOT_LOOPBACK_EVT, ?INC_LOOPBACK_HOOK, 'true'}
             ,{?IS_NOT_LOOPBACK_EVT, ?FILTER_LOOPBACK_HOOK, 'true'}
-
-             | lists:foldl(fun(Evt, Acc) ->
-                                   [{Evt, ?INC_LOOPBACK_HOOK, 'true'}
-                                   ,{Evt, ?FILTER_LOOPBACK_HOOK, 'false'}
-                                    | Acc
-                                   ]
-                           end
-                          ,[]
-                          ,?IS_LOOPBACK_ALT_EVT
-                          )
             ],
     [{hd(kz_json:get_keys(JObj))
      ,?_assertEqual(Include
