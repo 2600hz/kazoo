b289c5af1f3aa8707499eecdece6e9559d469517
James Aimonetti <james.aimonetti@gmail.com>
2017-11-21 19:35:39 +0000

    spawn the test workers, add timeout override

--- core/kazoo_caches/test/kz_cache_tests.erl
+++ core/kazoo_caches/test/kz_cache_tests.erl
@@ -1,17 +1,23 @@
 -module(kz_cache_tests).
 
 -include_lib("eunit/include/eunit.hrl").
+-include_lib("kazoo_stdlib/include/kz_types.hrl").
 
 cache_test_() ->
-    {'setup'
-    ,fun init/0
-    ,fun cleanup/1
-    ,fun(_CachePid) ->
-             [{"key exists", fun wait_for_key_local_existing/0}
-             ,{"key appears", fun wait_for_key_local_mid_stream/0}
-             ,{"key timeout", fun wait_for_key_local_timeout/0}
-             ]
-     end
+    {'timeout'
+    ,10
+    ,{'spawn'
+     ,{'setup'
+      ,fun init/0
+      ,fun cleanup/1
+      ,fun(_CachePid) ->
+               [{"key exists", fun wait_for_key_local_existing/0}
+               ,{"key appears", fun wait_for_key_local_mid_stream/0}
+               ,{"key timeout", fun wait_for_key_local_timeout/0}
+               ]
+       end
+      }
+     }
     }.
 
 init() ->
@@ -30,7 +36,7 @@ wait_for_key_local_existing() ->
 
     _WriterPid = spawn_monitor(fun() -> writer_job(Key, Value, Timeout) end),
 
-    ?_assertEqual({'ok', Value}, kz_cache:wait_for_key_local(?MODULE, Key, Timeout)).
+    ?assertEqual({'ok', Value}, kz_cache:wait_for_key_local(?MODULE, Key, Timeout)).
 
 wait_for_key_local_mid_stream() ->
     Timeout = rand:uniform(1000)+1000,
@@ -39,13 +45,13 @@ wait_for_key_local_mid_stream() ->
 
     _WriterPid = spawn_monitor(fun() -> writer_job(Key, Value, Timeout) end),
 
-    ?_assertEqual({'ok', Value}, kz_cache:wait_for_key_local(?MODULE, Key, Timeout)).
+    ?assertEqual({'ok', Value}, kz_cache:wait_for_key_local(?MODULE, Key, Timeout)).
 
 wait_for_key_local_timeout() ->
     Timeout = 1000,
     Key = kz_binary:rand_hex(5),
 
-    ?_assertEqual({'error', 'timeout'}, kz_cache:wait_for_key_local(?MODULE, Key, Timeout)).
+    ?assertEqual({'error', 'timeout'}, kz_cache:wait_for_key_local(?MODULE, Key, Timeout)).
 
 writer_job(Key, Value, Timeout) ->
     timer:sleep(Timeout div 2),
