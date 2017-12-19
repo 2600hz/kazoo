b436a88b6bdfbea8920899b26620232931e8a34f
Mark Magnusson <mark@2600hz.com>
2017-12-19 16:23:08 -0500

    clean up logs a bit
    Conflicts:
    	core/kazoo_stdlib/include/kz_log.hrl

--- core/kazoo_stdlib/include/kz_log.hrl
+++ core/kazoo_stdlib/include/kz_log.hrl
@@ -1,7 +1,7 @@
 -ifndef(KZ_LOG_HRL).
 
 -define(KEY_LOG_ID, <<"System-Log-ID">>).
--define(DEFAULT_LOG_SYSTEM_ID, <<"00000000000">>).
+-define(DEFAULT_LOG_SYSTEM_ID, <<"">>).
 
 -define(KZ_LOG_HRL, 'true').
 -endif.
