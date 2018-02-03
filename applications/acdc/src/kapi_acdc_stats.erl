a078a5fec7cec488138e77547753d43140f75032
Daniel Finke <danielfinke2011@gmail.com>
2018-02-03 11:48:52 +0000

    PISTON-534: allow Limit on status reqs

--- applications/acdc/src/kapi_acdc_stats.erl
+++ applications/acdc/src/kapi_acdc_stats.erl
@@ -285,7 +285,7 @@ current_calls_resp_v(JObj) ->
 
 -define(STATUS_REQ_HEADERS, [<<"Account-ID">>]).
 -define(OPTIONAL_STATUS_REQ_HEADERS, [<<"Agent-ID">>, <<"Start-Range">>, <<"End-Range">>
-                                     ,<<"Status">>
+                                     ,<<"Status">>, <<"Limit">>
                                      ]).
 -define(STATUS_REQ_VALUES, [{<<"Event-Category">>, <<"acdc_stat">>}
                            ,{<<"Event-Name">>, <<"status_req">>}
