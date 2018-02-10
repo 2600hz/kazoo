5478670884f9bf6c7cbcaa32acd7d011bb29257c
Hesaam Farhang <hesaam@2600hz.com>
2018-02-09 18:12:29 -0800

    hmm whattttt?! adding back macros

--- core/kazoo_tasks/src/kazoo_tasks.hrl
+++ core/kazoo_tasks/src/kazoo_tasks.hrl
@@ -6,5 +6,9 @@
 -include_lib("kazoo_tasks/include/task_fields.hrl").
 -include_lib("kazoo_tasks/include/tasks.hrl").
 
+-define(APP, 'kzaoo_tasks').
+-define(APP_NAME, <<"kazoo_tasks">>).
+-define(APP_VERSION, <<"4.0.0">> ).
+
 -define(KZ_KAZOO_TASKS_HRL, 'true').
 -endif.
