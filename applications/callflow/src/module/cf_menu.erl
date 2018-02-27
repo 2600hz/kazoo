ba9f6c4187a12952c54e9032effc08b613b75bf1
James Aimonetti <james.aimonetti@gmail.com>
2018-02-27 18:39:31 +0000

    drop end of list down

--- applications/callflow/src/module/cf_menu.erl
+++ applications/callflow/src/module/cf_menu.erl
@@ -433,7 +433,8 @@ recording_media_doc(Type, #cf_menu_data{name=MenuName
             ,{<<"source_type">>, <<"menu">>}
             ,{<<"source_id">>, Id}
             ,{<<"media_source">>, <<"recording">>}
-            ,{<<"streamable">>, 'true'}],
+            ,{<<"streamable">>, 'true'}
+            ],
     Doc = kz_doc:update_pvt_parameters(kz_json:from_list(Props), AccountDb, [{'type', <<"media">>}]),
     {'ok', JObj} = kz_datamgr:save_doc(AccountDb, Doc),
     kz_doc:id(JObj).
