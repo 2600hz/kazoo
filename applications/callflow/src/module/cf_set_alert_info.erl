607da3b0ef05e120e530b0b939e7a892c9065af5
Sergey Safarov <s.safarov@gmail.com>
2018-02-28 10:03:09 +0000

    applications/callflow: Added  set_alert_info

---
+++ applications/callflow/src/module/cf_set_alert_info.erl
@@ -0,0 +1,37 @@
+%%%-----------------------------------------------------------------------------
+%%% @copyright (C) 2013-2018, 2600Hz
+%%% @doc "data":{
+%%%   "alert_info":{{value}},
+%%% }
+%%%
+%%%
+%%% @author Sergey Safarov <s.safarov@gmail.com>, Sponsored by Audian
+%%% @end
+%%%-----------------------------------------------------------------------------
+-module(cf_set_alert_info).
+
+-behaviour(gen_cf_action).
+
+-include("callflow.hrl").
+
+-export([handle/2]).
+
+%%------------------------------------------------------------------------------
+%% @doc Entry point for this module
+%% @end
+%%------------------------------------------------------------------------------
+-spec handle(kz_json:object(), kapps_call:call()) -> 'ok'.
+handle(Data, Call) ->
+    Value = kz_json:get_ne_binary_value(<<"alert_info">>, Data),
+    set_variable(Value, Call),
+    Call1 = kapps_call:set_custom_channel_var(<<"Alert_info">>, Value, Call),
+    cf_exe:set_call(Call1),
+    cf_exe:continue(Call1).
+
+-spec set_variable(kz_term:api_ne_binary(), kapps_call:call()) -> 'ok'.
+set_variable('undefined', _Call) ->
+    lager:warning("can not set alert_info without value!");
+set_variable(Value, Call) ->
+    lager:debug("set Alert-Info to ~s", [Value]),
+    Var = kz_json:from_list([{<<"Alert-Info">>, Value}]),
+    kapps_call_command:set('undefined', Var, Call).
