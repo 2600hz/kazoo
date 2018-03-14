5c975b17fb75d278137ba6baf967d18cdf9e05e6
karl anderson <karl@2600hz.com>
2018-03-14 22:45:36 +0000

    fixture-macros: formatting

--- core/kazoo_apps/test/kapps_util_test.erl
+++ core/kazoo_apps/test/kapps_util_test.erl
@@ -9,11 +9,11 @@
 -include_lib("kazoo_fixturedb/include/kz_fixturedb.hrl").
 
 kapps_util_with_fixtures_test_() ->
-     {'setup'
-     ,fun kzd_test_fixtures:setup/0
-     ,fun kzd_test_fixtures:cleanup/1
-     ,fun(_) -> [test_get_master_account_id()] end
-     }.
+    {'setup'
+    ,fun kzd_test_fixtures:setup/0
+    ,fun kzd_test_fixtures:cleanup/1
+    ,fun(_) -> [test_get_master_account_id()] end
+    }.
 
 test_get_master_account_id() ->
     [{"Verify the fixture provides the master account"
