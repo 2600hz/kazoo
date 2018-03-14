9d5c1e3d584b6d5090fb0112143237ed71de6b3d
karl anderson <karl@2600hz.com>
2018-03-14 22:01:58 +0000

    fixture-macros: helps to run the test, and fixed broken test

--- core/kazoo_apps/test/kapps_util_test.erl
+++ core/kazoo_apps/test/kapps_util_test.erl
@@ -8,8 +8,15 @@
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("kazoo_fixturedb/include/kz_fixturedb.hrl").
 
-get_master_account_id_test() ->
+kapps_util_with_fixtures_test_() ->
+     {'setup'
+     ,fun kzd_test_fixtures:setup/0
+     ,fun kzd_test_fixtures:cleanup/1
+     ,fun(_) -> [test_get_master_account_id()] end
+     }.
+
+test_get_master_account_id() ->
     [{"Verify the fixture provides the master account"
-     ,?_assertEqual(?FIXTURE_MASTER_ACCOUNT_ID, kapps_util:get_master_account_id())
+     ,?_assertEqual({'ok', ?FIXTURE_MASTER_ACCOUNT_ID}, kapps_util:get_master_account_id())
      }
     ].
