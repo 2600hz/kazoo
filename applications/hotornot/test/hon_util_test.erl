8fa49d11fdf8beb07de1d10bca880ff9430197a2
James Aimonetti <james@2600hz.com>
2018-03-29 05:10:13 -0700

    KAZOO-5867: missed type conversion (#4730)
    unit testing sorting of rates

--- applications/hotornot/test/hon_util_test.erl
+++ applications/hotornot/test/hon_util_test.erl
@@ -24,3 +24,32 @@ dollars_and_units_test_() ->
     [?_assertEqual(BaseCost, wht_util:base_call_cost(RateCost, RateMin, RateSurcharge))
      || {RateCost, RateMin, RateSurcharge} <- Charges
     ].
+
+
+-define(RATE_1
+       ,kz_json:from_list([{<<"prefix">>, 432}
+                          ,{<<"weight">>, 50}
+                          ,{<<"rate_cost">>, 0.5}
+                          ,{<<"rate_name">>, <<"1">>}
+                          ])
+       ).
+-define(RATE_2
+       ,kz_json:from_list([{<<"prefix">>, 432}
+                          ,{<<"weight">>, 51}
+                          ,{<<"rate_cost">>, 0.6}
+                          ,{<<"rate_name">>, <<"2">>}
+                          ])
+       ).
+-define(RATES, [?RATE_1, ?RATE_2]).
+
+sort_rates_by_weight_test_() ->
+    Sorted = hon_util:sort_rates_by_weight(?RATES),
+    ?_assertEqual([<<"1">>, <<"2">>]
+                 ,[kzd_rates:rate_name(Rate) || Rate <- Sorted]
+                 ).
+
+sort_rates_by_cost_test_() ->
+    Sorted = hon_util:sort_rates_by_cost(?RATES),
+    ?_assertEqual([<<"2">>, <<"1">>]
+                 ,[kzd_rates:rate_name(Rate) || Rate <- Sorted]
+                 ).
