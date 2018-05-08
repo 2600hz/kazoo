55d6252413fbb82425141e20cc209e5030f069a9
James Aimonetti <james.aimonetti@gmail.com>
2018-05-08 22:33:46 +0000

    use real functions

--- applications/tasks/src/modules/kt_compactor.erl
+++ applications/tasks/src/modules/kt_compactor.erl
@@ -279,7 +279,7 @@ do_compact_db_by_nodes(?MATCH_ACCOUNT_ENCODED(_)=AccountDb, Heuristic) ->
     do_compact_db_by_nodes(kz_util:format_account_id(AccountDb, 'unencoded'), Heuristic);
 do_compact_db_by_nodes(?MATCH_MODB_SUFFIX_RAW(_AccountId, _Year, _Month)=MODB, Heuristic) ->
     lager:info("formatting raw modb ~s", [MODB]),
-    do_compact_db_by_nodes(kz_util:format_account_db(MODB, 'unencoded'), Heuristic);
+    do_compact_db_by_nodes(kz_util:format_account_modb(MODB, 'unencoded'), Heuristic);
 do_compact_db_by_nodes(Database, Heuristic) ->
     lager:debug("opening in ~s: ~s", [kazoo_couch:get_admin_dbs(), Database]),
     {'ok', DbInfo} = kz_datamgr:open_doc(kazoo_couch:get_admin_dbs(), Database),
