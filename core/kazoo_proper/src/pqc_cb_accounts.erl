431e51fc179c4117e08b6bbb987e5c9faf84955b
James Aimonetti <james.aimonetti@gmail.com>
2018-05-09 05:02:56 +0000

    update a bit of logging

--- core/kazoo_proper/src/pqc_cb_accounts.erl
+++ core/kazoo_proper/src/pqc_cb_accounts.erl
@@ -73,20 +73,20 @@ cleanup_accounts(API, AccountNames) ->
 
 -spec cleanup_account(pqc_cb_api:state(), kz_term:ne_binary()) -> 'ok'.
 cleanup_account(API, AccountName) ->
-    _ = try pqc_cb_search:search_account_by_name(API, AccountName) of
-            ?FAILED_RESPONSE ->
-                ?INFO("failed to search for account by name ~s~n", [AccountName]);
-            APIResp ->
-                Data = pqc_cb_response:data(APIResp),
-                case kz_json:get_ne_binary_value([1, <<"id">>], Data) of
-                    'undefined' ->
-                        check_accounts_db(AccountName);
-                    AccountId -> delete_account(API, AccountId)
-                end
-        catch
-            'throw':{'error', 'socket_closed_remotely'} ->
-                ?ERROR("broke the SUT cleaning up account ~s (~p)~n", [AccountName, API])
-        end,
+    _Attempt = try pqc_cb_search:search_account_by_name(API, AccountName) of
+                   ?FAILED_RESPONSE ->
+                       ?ERROR("failed to search for account by name ~s~n", [AccountName]);
+                   APIResp ->
+                       Data = pqc_cb_response:data(APIResp),
+                       case kz_json:get_ne_binary_value([1, <<"id">>], Data) of
+                           'undefined' ->
+                               check_accounts_db(AccountName);
+                           AccountId -> delete_account(API, AccountId)
+                       end
+               catch
+                   'throw':{'error', 'socket_closed_remotely'} ->
+                       ?ERROR("broke the SUT cleaning up account ~s (~p)~n", [AccountName, API])
+               end,
     timer:sleep(1000).% was needed to stop overwhelming the socket, at least locally
 
 check_accounts_db(Name) ->
@@ -94,7 +94,7 @@ check_accounts_db(Name) ->
     ViewOptions = [{'key', AccountName}],
     case kz_datamgr:get_results(?KZ_ACCOUNTS_DB, <<"accounts/listing_by_name">>, ViewOptions) of
         {'ok', []} -> 'ok';
-        {'error', _} -> 'ok';
+        {'error', _E} -> ?ERROR("failed to list by name: ~p", [_E]);
         {'ok', JObjs} ->
             ?INFO("deleting from ~s: ~p~n", [?KZ_ACCOUNTS_DB, JObjs]),
             kz_datamgr:del_docs(?KZ_ACCOUNTS_DB, JObjs)
