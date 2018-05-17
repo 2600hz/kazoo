e0b5eb57b55f01f17d2ca388e0103cf1a1700abd
Sean Wysor <sean@2600hz.com>
2018-05-17 08:57:34 -0700

    KAZOO-5898: fixed email being sent for recvfax when user has it disabled (#4851)
    * KAZOO-5898: fixed notification being sent for fax when user has it disabled
    
    * KAZOO-5898: tickie dem atoms, log the fact that user has disabled fax_to_email
    
    * KAZOO-5898: added a better log message

--- applications/fax/src/fax_request.erl
+++ applications/fax/src/fax_request.erl
@@ -313,7 +313,9 @@ maybe_update_fax_settings(#state{call=Call
                     UserEmail = kz_json:get_value(<<"email">>, JObj),
                     Notify = kz_json:set_value([<<"email">>,<<"send_to">>], [UserEmail] , kz_json:new()),
                     State#state{fax_notify=Notify};
-                'false' -> State
+                'false' ->
+                    lager:debug("user ~p has disabled fax_to_email - not going to send email", [OwnerId]),
+                    State
             end;
         {'error', _} ->
             maybe_update_fax_settings_from_account(State)
@@ -565,6 +567,8 @@ notify_failure(JObj, 'undefined', State) ->
     notify_failure(JObj, <<"unknown error">>, State);
 notify_failure(JObj, NonBinary, State) when not is_binary(NonBinary) ->
     notify_failure(JObj, kz_term:to_binary(NonBinary), State);
+notify_failure(_JObj, _Reason, #state{fax_notify='undefined'}) ->
+    'ok';
 notify_failure(JObj, Reason, #state{call=Call
                                    ,owner_id=OwnerId
                                    ,faxbox_id=FaxBoxId
@@ -588,6 +592,8 @@ notify_failure(JObj, Reason, #state{call=Call
     kapps_notify_publisher:cast(Message, fun kapi_notifications:publish_fax_inbound_error/1).
 
 -spec notify_success(state()) -> 'ok'.
+notify_success(#state{fax_notify='undefined'}) ->
+    'ok';
 notify_success(#state{call=Call
                      ,owner_id=OwnerId
                      ,faxbox_id=FaxBoxId
