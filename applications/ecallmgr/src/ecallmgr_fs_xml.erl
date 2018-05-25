f6302cf1715fafea41bd28bcd43d8aceffad9a11
Mark Magnusson <mark@2600hz.com>
2018-05-25 15:35:52 -0400

    KAZOO-32969-4two: ensure that hold_music is exported

--- applications/ecallmgr/src/ecallmgr_fs_xml.erl
+++ applications/ecallmgr/src/ecallmgr_fs_xml.erl
@@ -274,13 +274,14 @@ route_resp_xml(<<"bridge">>, Routes, JObj, Props) ->
     LogEl = route_resp_log_winning_node(),
     RingbackEl = route_resp_ringback(JObj),
     TransferEl = route_resp_transfer_ringback(JObj),
+    ExportEl   = route_resp_export_vars(JObj),
     %% format the Route based on protocol
     {_Idx, Extensions} = lists:foldr(fun route_resp_fold/2, {1, []}, Routes),
     FailRespondEl = action_el(<<"respond">>, <<"${bridge_hangup_cause}">>),
     FailConditionEl = condition_el(FailRespondEl),
     FailExtEl = extension_el(<<"failed_bridge">>, <<"false">>, [FailConditionEl]),
     Context = hunt_context(Props),
-    ContextEl = context_el(Context, [LogEl, RingbackEl, TransferEl] ++ unset_custom_sip_headers(Props) ++ Extensions ++ [FailExtEl]),
+    ContextEl = context_el(Context, [LogEl, RingbackEl, TransferEl, ExportEl] ++ unset_custom_sip_headers(Props) ++ Extensions ++ [FailExtEl]),
     SectionEl = section_el(<<"dialplan">>, <<"Route Bridge Response">>, ContextEl),
     {'ok', xmerl:export([SectionEl], 'fs_xml')};
 
@@ -432,6 +433,10 @@ route_cavs_list(CAVs) ->
          || {K, V} <- CAVs
         ],
     <<"^^|", (kz_term:to_binary(string:join(L, "|")))/binary>>.
+
+-spec route_resp_export_vars(kz_json:object()) -> kz_types:xml_el().
+route_resp_export_vars(_JObj) ->
+    action_el(<<"set">>, <<"export_vars=hold_music">>).
 
 -spec route_resp_transfer_ringback(kz_json:object()) -> kz_types:xml_el().
 route_resp_transfer_ringback(JObj) ->
