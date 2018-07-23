412ceda315e2fcf99555ccaf9d5ed5b8cc112f56
James Aimonetti <james.aimonetti@gmail.com>
2018-07-23 22:29:00 +0000

    add personal replacement file

--- make/splchk.mk
+++ make/splchk.mk
@@ -1,6 +1,7 @@
 .PHONY = splchk splchk-changed
 
-KAZOO_DICT = $(ROOT)/.aspell.en-kazoo.pws
+KAZOO_DICT = $(ROOT)/.aspell.en.pws
+KAZOO_REPL = $(ROOT)/.aspell.en.prepl
 
 $(KAZOO_DICT):
 	@$(file >$(KAZOO_DICT),personal_ws-1.1 en 0 utf-8)
@@ -10,4 +11,4 @@ splchk: $(KAZOO_DICT) $(addsuffix .chk,$(basename $(wildcard doc/*.md)))
 
 %.chk: %.md
 	@echo Spellchecking $< $@
-	aspell --home-dir=$(ROOT) --personal=$(KAZOO_DICT) --lang=en --jargon=kazoo -x check $<
+	aspell --home-dir=$(ROOT) --personal=$(KAZOO_DICT) --repl=$(KAZOO_REPL) --lang=en --jargon=kazoo -x check $<
