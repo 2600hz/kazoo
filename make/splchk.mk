46c11e0ad6dcda00716ed513d07dec62becaf0e4
James Aimonetti <james.aimonetti@gmail.com>
2018-07-24 05:44:26 +0000

    app-specific spellcheck

--- make/splchk.mk
+++ make/splchk.mk
@@ -1,15 +1,18 @@
 .PHONY = splchk splchk-changed
 
-KAZOO_DICT = $(ROOT)/.aspell.en.pws
-KAZOO_REPL = $(ROOT)/.aspell.en.prepl
+KAZOO_DICT = .aspell.en.pws
+KAZOO_REPL = .aspell.en.prepl
 
-$(KAZOO_DICT):
-	@$(file >$(KAZOO_DICT),personal_ws-1.1 en 0 utf-8)
+$(ROOT)/$(KAZOO_DICT):
+	@$(file >$(ROOT)/$(KAZOO_DICT),personal_ws-1.1 en 0 utf-8)
 
-splchk-changed: $(KAZOO_DICT) $(addsuffix .chk,$(basename $(CHANGED)))
-splchk: $(KAZOO_DICT) $(addsuffix .chk,$(basename $(wildcard doc/*.md)))
-splchk-json: $(KAZOO_DICT)
+$(ROOT)/$(KAZOO_REPL):
+	@$(file >$(ROOT)/$(KAZOO_REPL),personal_repl-1.1 en 0 utf-8)
+
+splchk-changed: $(ROOT)/$(KAZOO_DICT) $(ROOT)/$(KAZOO_REPL) $(addsuffix .chk,$(basename $(CHANGED)))
+splchk: $(ROOT)/$(KAZOO_DICT) $(ROOT)/$(KAZOO_REPL) $(addsuffix .chk,$(basename $(wildcard doc/*.md)))
+splchk-json: $(ROOT)/$(KAZOO_DICT) $(ROOT)/$(KAZOO_REPL)
 
 %.chk: %.md
 	@echo Spellchecking $<
-	aspell --home-dir=$(ROOT) --personal=$(basename $(KAZOO_DICT)) --repl=$(basename $(KAZOO_REPL)) --lang=en --jargon=kazoo -x check $<
+	aspell --home-dir=$(ROOT) --personal=$(KAZOO_DICT) --repl=$(KAZOO_REPL) --lang=en -x check $<
