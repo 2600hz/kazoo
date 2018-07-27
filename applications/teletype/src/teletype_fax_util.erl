b2c15ea80450b85e0be7b6a0d6b47b3fcbfc9dc4
swysor <sean@2600hz.com>
2018-07-27 07:16:37 +0000

    Fixed a bug in fax notification attachment filename ..pdf instead of .pdf

--- applications/teletype/src/teletype_fax_util.erl
+++ applications/teletype/src/teletype_fax_util.erl
@@ -215,7 +215,7 @@ maybe_fetch_attachments(DataJObj, FaxJObj, Macros, 'false') ->
     lager:debug("accessing fax attachment ~s at ~s", [Db, FaxId]),
     teletype_util:send_update(DataJObj, <<"pending">>),
     Format = kapps_config:get_ne_binary(?CONVERT_CONFIG_CAT, [<<"fax">>, <<"attachment_format">>], <<"pdf">>),
-    Filename = get_file_name(Macros, <<".", Format/binary>>),
+    Filename = get_file_name(Macros, Format),
     case kzd_fax:fetch_attachment_format(Format, Db, FaxJObj) of
         {'ok', Content, ContentType, _Doc} ->
             [{ContentType, Filename, Content}];
