0f1c482dbbc0c5b4939e20963d52825d4b5de8bd
Daniel Finke <danielfinke2011@gmail.com>
2018-07-30 19:42:33 +0000

    PISTON-598: add provider support to stepswitch_cnam + Telnyx mod
    Conflicts:
    	applications/stepswitch/src/stepswitch_cnam.erl

--- applications/stepswitch/src/stepswitch_util.erl
+++ applications/stepswitch/src/stepswitch_util.erl
@@ -14,6 +14,7 @@
 -export([default_realm/1]).
 -export([route_by/0]).
 -export([resources_to_endpoints/3]).
+-export([json_to_template_props/1]).
 
 -include("stepswitch.hrl").
 -include_lib("kazoo_stdlib/include/kazoo_json.hrl").
@@ -399,3 +400,35 @@ update_ccvs(Endpoint, Updates) ->
 %%maybe_update_number(Resource, Number) ->
 %%    SelectorsResult = stepswitch_resources:get_resrc_selector_marks(Resource),
 %%    props:get_value('regex_number_match', SelectorsResult, Number).
+
+%%------------------------------------------------------------------------------
+%% @doc
+%% @end
+%%------------------------------------------------------------------------------
+-spec json_to_template_props(kz_term:api_object()) -> 'undefined' | kz_term:proplist().
+json_to_template_props('undefined') -> 'undefined';
+json_to_template_props(JObj) ->
+    normalize_proplist(kz_json:recursive_to_proplist(JObj)).
+
+%%------------------------------------------------------------------------------
+%% @doc
+%% @end
+%%------------------------------------------------------------------------------
+-spec normalize_proplist(kz_term:proplist()) -> kz_term:proplist().
+normalize_proplist(Props) ->
+    [normalize_proplist_element(Elem) || Elem <- Props].
+
+-spec normalize_proplist_element({kz_term:proplist_key(), kz_term:proplist_value()}) ->
+                                        {kz_term:proplist_key(), kz_term:proplist_value()}.
+normalize_proplist_element({K, V}) when is_list(V) ->
+    {normalize_value(K), normalize_proplist(V)};
+normalize_proplist_element({K, V}) when is_binary(V) ->
+    {normalize_value(K), kz_html:escape(V)};
+normalize_proplist_element({K, V}) ->
+    {normalize_value(K), V};
+normalize_proplist_element(Else) ->
+    Else.
+
+-spec normalize_value(binary()) -> binary().
+normalize_value(Value) ->
+    binary:replace(kz_term:to_lower_binary(Value), <<"-">>, <<"_">>, ['global']).
