a0e044126fb25c5c9f33380fdbe1ea6c73a7e1fc
James Aimonetti <james.aimonetti@gmail.com>
2018-08-06 16:13:35 +0000

    build erlang

--- scripts/circleci-build-erlang.sh
+++ scripts/circleci-build-erlang.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 set -x
 set -e
+
+pushd $(dirname $0) > /dev/null
+cd $(pwd -P)/..
+
+KAZOO_OTP_VERSION=$(<./make/erlang_version)
+OTP_VERSION=${OTP_VERSION:-$KAZOO_OTP_VERSION}
+
+echo "KERL_BUILD_BACKEND=git" > ~/.kerlrc
+echo "KERL_CONFIGURE_OPTIONS=\"--without-odbc --with-ssl --disable-hipe --enable-threads --enable-smp-support --enable-kernel-poll\"" >> ~/.kerlrc
+
 if [[ ! -d ~/.kerl/$OTP_VERSION ]]; then
     if [[ ! -d ~/.kerl ]]; then
         mkdir ~/.kerl
@@ -11,3 +21,5 @@ if [[ ! -d ~/.kerl/$OTP_VERSION ]]; then
     ~/.kerl/kerl build $OTP_VERSION $OTP_VERSION
     ~/.kerl/kerl install $OTP_VERSION ~/.kerl/$OTP_VERSION
 fi
+
+popd > /dev/null
