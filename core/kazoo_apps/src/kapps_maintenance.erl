f26da9ebc14f3c83c5620ae3f3e7d2c1aa23668d
James Aimonetti <james.aimonetti@gmail.com>
2018-08-08 22:25:49 +0000

    orderly shutdown to give logging time to finish

--- core/kazoo_apps/src/kapps_maintenance.erl
+++ core/kazoo_apps/src/kapps_maintenance.erl
@@ -1153,12 +1153,12 @@ check_release() ->
     catch
         'throw':Error ->
             lager:error("check_release/0 failed: ~p", [Error]),
-            halt(1);
+            init:stop(1);
         _E:_R ->
             ST = erlang:get_stacktrace(),
             lager:error("check_release/0 crashed: ~s: ~p", [_E, _R]),
             kz_util:log_stacktrace(ST),
-            halt(1)
+            init:stop(1)
     end.
 
 -spec kapps_started() -> boolean().
