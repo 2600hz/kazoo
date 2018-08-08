c86d74a2d7ada59b06439939a90173132d2fa504
James Aimonetti <james.aimonetti@gmail.com>
2018-08-08 21:23:25 +0000

    generic file log changer

--- core/kazoo_stdlib/src/kz_log.erl
+++ core/kazoo_stdlib/src/kz_log.erl
@@ -9,6 +9,7 @@
 -export([change_console_log_level/1
         ,change_error_log_level/1
         ,change_syslog_log_level/1
+        ,change_file_log_level/2
         ]).
 
 -export_type([log_level/0]).
@@ -32,24 +33,42 @@
                    | 'debug'
                    | kz_term:ne_binary().
 
+-define(LOG(Fmt), begin lager:info(Fmt), io:format(Fmt ++ "~n") end).
+-define(LOG(Fmt, Args), begin lager:info(Fmt, Args), io:format(Fmt ++ "~n", Args) end).
 
 -spec change_console_log_level(log_level()) -> 'ok'.
 change_console_log_level(L) when is_atom(L) ->
-    lager:info("updated console_log to level ~s", [L]),
-    lager:set_loglevel('lager_console_backend', L);
+    Handlers = ['lager_console_backend'
+               ,{'lager_file_backend', "log/console.log"}
+               ],
+    update_log_level(Handlers, L);
 change_console_log_level(L) ->
     change_console_log_level(kz_term:to_atom(L)).
 
 -spec change_error_log_level(log_level()) -> 'ok'.
 change_error_log_level(L) when is_atom(L) ->
-    lager:info("updated error_log to level ~s", [L]),
-    lager:set_loglevel({'lager_file_backend', "log/error.log"}, L);
+    update_log_level([{'lager_file_backend', "log/error.log"}], L);
 change_error_log_level(L) ->
     change_error_log_level(kz_term:to_atom(L)).
 
 -spec change_syslog_log_level(log_level()) -> 'ok'.
 change_syslog_log_level(L) when is_atom(L) ->
-    lager:info("updated syslog_log to level ~s", [L]),
-    lager:set_loglevel({'lager_syslog_backend',{"2600hz",'local0'}}, L);
+    update_log_level([{'lager_syslog_backend',{"2600hz",'local0'}}], L);
 change_syslog_log_level(L) ->
     change_syslog_log_level(kz_term:to_atom(L)).
+
+-spec change_file_log_level(kz_term:text(), log_level()) -> 'ok'.
+change_file_log_level(File, Level) ->
+    update_log_level([{'lager_file_backend', kz_term:to_list(File)}], Level).
+
+update_log_level([], _Level) -> 'ok';
+update_log_level([Backend|Backends], Level) ->
+    case lager:get_loglevel(Backend) of
+        {'error', 'bad_module'} -> 'ok';
+        Level ->
+            ?LOG("handler ~p already logging at ~p", [Backend, Level]);
+        _OldLevel ->
+            ?LOG("handler ~p now logging at ~p (was ~p)", [Backend, Level, _OldLevel]),
+            lager:set_loglevel(Backend, Level)
+    end,
+    update_log_level(Backends, Level).
