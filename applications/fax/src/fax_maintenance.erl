fdc7ad701100413644a68c28847b8a10de28d273
swysor <sean@2600hz.com>
2018-08-08 02:54:17 +0000

    i gone and dun deleted somethin I shouldnta

--- applications/fax/src/fax_maintenance.erl
+++ applications/fax/src/fax_maintenance.erl
@@ -22,6 +22,8 @@
 -export([faxbox_jobs/1, faxbox_jobs/2]).
 -export([pending_jobs/0, active_jobs/0]).
 
+-export([load_smtp_attachment/2]).
+
 -define(DEFAULT_MIGRATE_OPTIONS, [{'allow_old_modb_creation', 'true'}]).
 -define(OVERRIDE_DOCS, ['override_existing_document'
                        ,{'transform', fun(_, B) -> kz_json:set_value(<<"folder">>, <<"outbox">>, B) end}
@@ -429,3 +431,23 @@ next_key(<<>>) ->
 next_key(Bin) ->
     <<Bin/binary, "\ufff0">>.
 
+-spec load_smtp_attachment(kz_term:ne_binary(), kz_term:ne_binary()) -> 'ok'.
+load_smtp_attachment(DocId, Filename) ->
+    case file:read_file(Filename) of
+        {'ok', FileContents} ->
+            load_smtp_attachment(DocId, Filename, FileContents);
+        Error ->
+            io:format("error obtaining file ~s contents for docid ~s : ~p~n", [Filename, DocId, Error])
+    end.
+
+-spec load_smtp_attachment(kz_term:ne_binary(), kz_term:ne_binary(), binary()) -> 'ok'.
+load_smtp_attachment(DocId, Filename, FileContents) ->
+    CT = kz_mime:from_filename(Filename),
+    case kz_datamgr:open_cache_doc(?KZ_FAXES_DB, DocId) of
+        {'ok', JObj} ->
+            case kz_fax_attachment:save_outbound(?KZ_FAXES_DB, JObj, FileContents, CT) of
+                {'ok', _Doc} -> io:format("attachment ~s for docid ~s recovered~n", [Filename, DocId]);
+                {'error', E} -> io:format("error attaching ~s to docid ~s : ~p~n", [Filename, DocId, E])
+            end;
+        {'error', E} -> io:format("error opening docid ~s for attaching ~s : ~p~n", [DocId, Filename, E])
+    end.
