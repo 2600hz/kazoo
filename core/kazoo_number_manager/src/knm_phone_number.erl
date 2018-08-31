8cb075516dcf4b13fec6f06c2808d56bfeb2dfcb
James Aimonetti <james.aimonetti@gmail.com>
2018-08-31 22:05:02 +0000

    formatting

--- core/kazoo_number_manager/src/knm_phone_number.erl
+++ core/kazoo_number_manager/src/knm_phone_number.erl
@@ -156,8 +156,8 @@ new(T=#{todo := Nums, options := Options}) ->
              ,?NUMBER_STATE_PORT_IN =:= knm_number_options:state(Options)
              }
         of
-            {true,false} -> [{module_name, ?PORT_IN_MODULE_NAME} | Options];
-            {_,true} -> [{module_name, ?CARRIER_LOCAL} | Options];
+            {'true', 'false'} -> [{'module_name', ?PORT_IN_MODULE_NAME} | Options];
+            {_, 'true'} ->       [{'module_name', ?CARRIER_LOCAL} | Options];
             _ -> Options
         end).
 -endif.
@@ -653,7 +653,7 @@ from_json(JObj) ->
                 ,fun ensure_features_defined/1
                 ,{fun ensure_pvt_state_legacy_undefined/2, kz_json:get_value(?PVT_STATE_LEGACY, JObj)}
 
-                 |props:filter_undefined([{fun set_rev/2, kz_doc:revision(JObj)}])
+                 | props:filter_undefined([{fun set_rev/2, kz_doc:revision(JObj)}])
                 ]),
     PN.
 
@@ -761,6 +761,13 @@ features_fold(FeatureKey, Acc, JObj) ->
 %%------------------------------------------------------------------------------
 -spec from_json_with_options(kz_json:object(), knm_phone_number() | knm_number_options:options()) ->
                                     knm_phone_number().
+from_json_with_options(JObj, #knm_phone_number{}=PN) ->
+    Options = [{'dry_run', dry_run(PN)}
+              ,{'batch_run', batch_run(PN)}
+              ,{'mdn_run', mdn_run(PN)}
+              ,{'auth_by', auth_by(PN)}
+              ],
+    from_json_with_options(JObj, Options);
 from_json_with_options(JObj, Options)
   when is_list(Options) ->
     Updates = [{fun set_assign_to/2, knm_number_options:assign_to(Options)}
@@ -775,14 +782,7 @@ from_json_with_options(JObj, Options)
                 end
               ],
     {'ok', PN} = setters(from_json(JObj), Updates),
-    PN;
-from_json_with_options(JObj, #knm_phone_number{}=PN) ->
-    Options = [{'dry_run', dry_run(PN)}
-              ,{'batch_run', batch_run(PN)}
-              ,{'mdn_run', mdn_run(PN)}
-              ,{'auth_by', auth_by(PN)}
-              ],
-    from_json_with_options(JObj, Options).
+    PN.
 
 %%------------------------------------------------------------------------------
 %% @doc
@@ -800,17 +800,15 @@ is_phone_number(_) -> 'false'.
                      knm_phone_number_return() |
                      knm_numbers:collection().
 
-setters(T0, Routines) when is_map(T0) ->
-    setters_collection(T0, Routines);
 setters(#knm_phone_number{}=PN, Routines) ->
-    setters_pn(PN, Routines).
+    setters_pn(PN, Routines);
+setters(T0, Routines) when is_map(T0) ->
+    setters_collection(T0, Routines).
 
 -spec setters_pn(knm_phone_number(), set_functions()) -> knm_phone_number_return().
 setters_pn(PN, Routines) ->
     try lists:foldl(fun setters_fold/2, PN, Routines) of
-        {'ok', _N}=Ok -> Ok;
-        {'error', _R}=Error -> Error;
-        Result -> {'ok', Result}
+        #knm_phone_number{}=NewPN -> {'ok', NewPN}
     catch
         'throw':{'stop', Error} -> Error;
         'error':'function_clause' ->
@@ -830,9 +828,9 @@ setters_pn(PN, Routines) ->
 
 -spec setters_collection(knm_numbers:collection(), set_functions()) -> knm_numbers:collection().
 setters_collection(T0=#{todo := PNs}, Routines) ->
-    F = fun (PN, T) ->
+    F = fun (#knm_phone_number{}=PN, T) ->
                 case setters(PN, Routines) of
-                    {'ok', NewPN} -> knm_numbers:ok(NewPN, T);
+                    {'ok', #knm_phone_number{}=NewPN} -> knm_numbers:ok(NewPN, T);
                     {'error', R} -> knm_numbers:ko(number(PN), R, T)
                 end
         end,
@@ -848,7 +846,7 @@ setters_collection(T0=#{todo := PNs}, Routines) ->
 -type setter_acc() :: knm_phone_number_return() |
                       knm_phone_number().
 
--spec setters_fold(set_function(), setter_acc()) -> setter_acc().
+-spec setters_fold(set_function(), knm_phone_number()) -> knm_phone_number().
 setters_fold(_, {'error', _R}=Error) ->
     throw({'stop', Error});
 setters_fold({Fun, Key, Value}, PN) when is_function(Fun, 3) ->
@@ -858,7 +856,7 @@ setters_fold({Fun, Value}, PN) when is_function(Fun, 2) ->
 setters_fold(Fun, PN) when is_function(Fun, 1) ->
     setters_fold_apply(Fun, [PN]).
 
--spec setters_fold_apply(set_function(), nonempty_list()) -> setter_acc().
+-spec setters_fold_apply(set_function(), nonempty_list()) -> knm_phone_number().
 setters_fold_apply(Fun, [{'ok',PN}|Args]) ->
     setters_fold_apply(Fun, [PN|Args]);
 setters_fold_apply(Fun, Args) ->
