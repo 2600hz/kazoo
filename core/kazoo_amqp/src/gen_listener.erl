d062fa47c9993b45b81b0b35a7d72694aab9d9a1
Mark Magnusson <mark@2600hz.com>
2018-09-17 19:19:24 -0400

    master: correctly cast bindings on inital add so that they can be removed with rm_binding later (#5127)

--- core/kazoo_amqp/src/gen_listener.erl
+++ core/kazoo_amqp/src/gen_listener.erl
@@ -1334,7 +1334,7 @@ declare_exchange(Channel, Exchange, Exchanges) ->
 -spec start_initial_bindings(state(), kz_term:proplist()) -> state().
 start_initial_bindings(State, Params) ->
     lists:foldl(fun({Binding, Props}, StateAcc) ->
-                        handle_add_binding(Binding, Props, StateAcc)
+                        handle_add_binding(kz_term:to_binary(Binding), Props, StateAcc)
                 end
                ,State
                ,props:get_value('bindings', Params, [])
