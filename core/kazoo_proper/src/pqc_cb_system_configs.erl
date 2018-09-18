157f6b3a8ff3b88b3ad657070c091bed73ee4337
James Aimonetti <james.aimonetti@gmail.com>
2018-09-18 19:56:45 +0000

    add listing and url builders

--- core/kazoo_proper/src/pqc_cb_system_configs.erl
+++ core/kazoo_proper/src/pqc_cb_system_configs.erl
@@ -6,7 +6,8 @@
        ).
 
 %% API
--export([set_default_config/3
+-export([list_configs/1
+        ,set_default_config/3
         ,get_config/2
         ,get_default_config/2
         ,delete_config/2
@@ -14,9 +15,28 @@
 
 -include("kazoo_proper.hrl").
 
+-spec configs_url() -> string().
+configs_url() ->
+    string:join([pqc_cb_api:v2_base_url(), "system_configs"], "/").
+
+-spec config_url(kz_term:ne_binary()) -> string().
+config_url(Id) ->
+    string:join([pqc_cb_api:v2_base_url(), "system_configs", kz_term:to_list(Id)], "/").
+
+-spec list_configs(pqc_cb_api:api()) -> kz_term:ne_binaries().
+list_configs(API) ->
+    URL = configs_url(),
+    io:format("GET ~s~n", [URL]),
+    Resp = pqc_cb_api:make_request([200]
+                                  ,fun kz_http:get/2
+                                  ,URL
+                                  ,pqc_cb_api:request_headers(API)
+                                  ),
+    pqc_cb_response:data(Resp).
+
 -spec get_config(pqc_cb_api:api(), kz_term:ne_binary()) -> binary().
 get_config(API, Id) ->
-    URL = string:join([pqc_cb_api:v2_base_url(), "system_configs", kz_term:to_list(Id)], "/"),
+    URL = config_url(Id),
     io:format("GET ~p:~n", [URL]),
     pqc_cb_api:make_request([200, 404]
                            ,fun kz_http:get/2
@@ -26,8 +46,7 @@ get_config(API, Id) ->
 
 -spec get_default_config(pqc_cb_api:api(), kz_term:ne_binary()) -> binary().
 get_default_config(API, Id) ->
-    URL = string:join([pqc_cb_api:v2_base_url(), "system_configs", kz_term:to_list(Id)], "/")
-        ++ "?with_defaults=true",
+    URL = config_url(Id) ++ "?with_defaults=true",
     io:format("GET ~p:~n", [URL]),
     pqc_cb_api:make_request([200, 404]
                            ,fun kz_http:get/2
@@ -38,7 +57,7 @@ get_default_config(API, Id) ->
 -spec set_default_config(pqc_cb_api:api(), kz_term:ne_binary(), kz_json:object()) -> binary().
 set_default_config(API, Id, Config) ->
     ?INFO("settings config for ~s"),
-    URL = string:join([pqc_cb_api:v2_base_url(), "system_configs", kz_term:to_list(Id)], "/"),
+    URL = config_url(Id),
     Data = pqc_cb_api:create_envelope(Config),
 
     io:format("POST ~p:~n~p~n", [URL, Data]),
@@ -51,7 +70,7 @@ set_default_config(API, Id, Config) ->
 
 -spec delete_config(pqc_cb_api:api(), kz_term:ne_binary()) -> binary().
 delete_config(API, Id) ->
-    URL = string:join([pqc_cb_api:v2_base_url(), "system_configs", kz_term:to_list(Id)], "/"),
+    URL = config_url(Id),
     io:format("DELETE ~p:~n", [URL]),
     pqc_cb_api:make_request([200, 404]
                            ,fun kz_http:delete/2
