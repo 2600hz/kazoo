09215e164cdb329d644fde971612fd0ada57226c
James Aimonetti <james.aimonetti@gmail.com>
2018-09-21 16:38:25 +0000

    Improve JSON object generation for testing
    Add a property check that reports the object depth generation
    
    Use symbolic calls to kz_json:set_value/3 to construct the object
    
    Use resize/2 to control the max depth of the object
    
    Update tests to use the new deep_object/0 generator

--- core/kazoo_stdlib/test/kz_json_generators.erl
+++ core/kazoo_stdlib/test/kz_json_generators.erl
@@ -1,20 +1,64 @@
 -module(kz_json_generators).
 
--export([test_object/0]).
+-export([test_object/0
+        ,non_empty_object/0
+        ,deep_object/0, deep_object/1
 
--include_lib("proper/include/proper.hrl").
+        ,max_depth/1
+        ]).
+
 -include_lib("kazoo_stdlib/include/kazoo_json.hrl").
+-include_lib("proper/include/proper.hrl").
+
+non_empty_object() ->
+    ?SUCHTHAT(JObj, test_object(), not kz_json:is_empty(JObj)).
 
 test_object() ->
     ?LET(JObj, object(), remove_duplicate_keys(JObj)).
 
-remove_duplicate_keys(?EMPTY_JSON_OBJECT) -> ?EMPTY_JSON_OBJECT;
 remove_duplicate_keys(?JSON_WRAPPER(_)=JObj) ->
-    kz_json:foldl(fun no_dups/3, kz_json:new(), JObj);
-remove_duplicate_keys(V) -> V.
+    kz_json:expand(kz_json:from_map(kz_json:to_map(kz_json:flatten(JObj)))).
 
-no_dups(Key, ?JSON_WRAPPER(_)=JObj, Acc) ->
-    DeDuped = remove_duplicate_keys(JObj),
-    kz_json:set_value(Key, DeDuped, Acc);
-no_dups(Key, Value, Acc) ->
-    kz_json:set_value(Key, Value, Acc).
+deep_object() ->
+    ?SIZED(Nesting, deep_object(Nesting)).
+
+deep_object(Nesting) ->
+    ?LET(DeepObject
+        ,deep_object(Nesting, {'$call', 'kz_json', 'new', []})
+        ,DeepObject
+        ).
+
+deep_object(0, JObj) ->
+    JObj;
+deep_object(Depth, JObj) ->
+    deep_object(Depth-1
+               ,{'$call', 'kz_json', 'set_value', [path(Depth), freq_value(Depth), JObj]}
+               ).
+
+path(0) ->
+    ?LET(Key, json_string(), Key);
+path(Depth) ->
+    ?LET(Path
+        ,resize(Depth, non_empty(json_string()))
+        ,Path
+        ).
+
+freq_value(0) ->
+    ?LET(Value, non_object_json_term(), Value);
+freq_value(Depth) ->
+    frequency([{90, ?LET(Value, non_object_json_term(), Value)}
+              ,{10, ?LET(JObj, deep_object(Depth-1), JObj)}
+              ]).
+
+max_depth(JObj) ->
+    Depth = kz_json:foldl(fun(Path, _, Max) ->
+                                  case length(Path) of
+                                      NewMax when NewMax > Max -> NewMax;
+                                      _Len -> Max
+                                  end
+                          end
+                         ,0
+                         ,kz_json:flatten(JObj)
+                         ),
+    Depth > 44 andalso io:format('user', "maxdepth ~p~n", [JObj]),
+    Depth.
