cfff31eaff382960e9b6e3a648089fe5db25e537
Hesaam Farhang <hesaam@2600hz.com>
2018-09-28 04:28:00 -0700

    revert oauth and system_config refresh/migrate behaviour from kazoo_maintenance app

--- core/kazoo_apps/src/kapps_maintenance.erl
+++ core/kazoo_apps/src/kapps_maintenance.erl
@@ -1024,7 +1024,9 @@ migrate_system(PreviousVersion, ThisVersion) ->
 
 -spec migrate_system_version_routines(kz_term:ne_binary(), kz_term:ne_binary()) -> [fun()].
 migrate_system_version_routines(<<"3.22">>, _) ->
-    [fun handle_module_rename/0];
+    [fun migrate_and_cleanup_system_config/0
+    ,fun handle_module_rename/0
+    ];
 migrate_system_version_routines(_, _) -> [].
 
 -spec handle_module_rename() -> boolean().
@@ -1047,6 +1049,132 @@ handle_module_rename_doc(JObj) ->
                 {'error', _Error} -> 'false'
             end
     end.
+
+-spec migrate_and_cleanup_system_config() -> 'true'.
+migrate_and_cleanup_system_config() ->
+    cleanup_invalid_notify_docs(),
+    delete_system_media_references(),
+    accounts_config_deprecate_timezone_for_default_timezone(),
+    'true'.
+
+%%------------------------------------------------------------------------------
+%% @doc
+%% @end
+%%------------------------------------------------------------------------------
+-spec cleanup_invalid_notify_docs() -> 'ok'.
+cleanup_invalid_notify_docs() ->
+    _ = kz_datamgr:db_archive(<<"system_config">>),
+    case kz_datamgr:all_docs(?KZ_CONFIG_DB, ['include_docs']) of
+        {'ok', JObjs} -> cleanup_invalid_notify_docs(JObjs);
+        {'error', _R} ->
+            lager:warning("unable to fetch all system config docs: ~p", [_R])
+    end.
+
+-spec cleanup_invalid_notify_docs(kz_json:objects()) -> 'ok'.
+cleanup_invalid_notify_docs([]) -> 'ok';
+cleanup_invalid_notify_docs([JObj|JObjs]) ->
+    Id = kz_json:get_value(<<"id">>, JObj),
+    Doc = kz_json:get_value(<<"doc">>, JObj),
+    _ = maybe_remove_invalid_notify_doc(kz_doc:type(Doc), Id, Doc),
+    cleanup_invalid_notify_docs(JObjs).
+
+-spec maybe_remove_invalid_notify_doc(kz_term:ne_binary(), kz_term:ne_binary(), kz_json:object()) -> 'ok'.
+maybe_remove_invalid_notify_doc(<<"notification">>, <<"notification", _/binary>>, _) -> 'ok';
+maybe_remove_invalid_notify_doc(<<"notification">>, _, JObj) ->
+    _ = kz_datamgr:del_doc(?KZ_CONFIG_DB, JObj),
+    'ok';
+maybe_remove_invalid_notify_doc(_Type, _Id, _Doc) -> 'ok'.
+
+
+-spec delete_system_media_references() -> 'ok'.
+delete_system_media_references() ->
+    DocId = kz_call_response:config_doc_id(),
+    case kz_datamgr:open_doc(?KZ_CONFIG_DB, DocId) of
+        {'ok', CallResponsesDoc} ->
+            delete_system_media_references(DocId, CallResponsesDoc);
+        {'error', 'not_found'} -> 'ok'
+    end.
+
+-spec delete_system_media_references(kz_term:ne_binary(), kz_json:object()) -> 'ok'.
+delete_system_media_references(DocId, CallResponsesDoc) ->
+    TheKey = <<"default">>,
+    Default = kz_json:get_value(TheKey, CallResponsesDoc),
+
+    case kz_json:map(fun remove_system_media_refs/2, Default) of
+        Default -> 'ok';
+        NewDefault ->
+            io:format("updating ~s with stripped system_media references~n", [DocId]),
+            NewCallResponsesDoc = kz_json:set_value(TheKey, NewDefault, CallResponsesDoc),
+            _Resp = kz_datamgr:save_doc(?KZ_CONFIG_DB, NewCallResponsesDoc),
+            'ok'
+    end.
+
+-spec remove_system_media_refs(kz_json:path(), kz_json:objects()) ->
+                                      {kz_json:path(), kz_json:json_term()}.
+remove_system_media_refs(HangupCause, Config) ->
+    case kz_json:is_json_object(Config) of
+        'false' -> {HangupCause, Config};
+        'true' ->
+            {HangupCause
+            ,kz_json:foldl(fun remove_system_media_ref/3, kz_json:new(), Config)
+            }
+    end.
+
+-spec remove_system_media_ref(kz_json:path(), kz_json:json_term(), kz_json:object()) ->
+                                     kz_json:object().
+remove_system_media_ref(Key, <<"/system_media/", Value/binary>>, Acc) -> kz_json:set_value(Key, Value, Acc);
+remove_system_media_ref(Key, Value, Acc) -> kz_json:set_value(Key, Value, Acc).
+
+%%------------------------------------------------------------------------------
+%% @doc Remove system_config/accounts timezone key and use only
+%% default_timezone
+%% @end
+%%------------------------------------------------------------------------------
+
+-spec accounts_config_deprecate_timezone_for_default_timezone() -> 'ok'.
+accounts_config_deprecate_timezone_for_default_timezone() ->
+    case kz_datamgr:open_cache_doc(?KZ_CONFIG_DB, <<"accounts">>) of
+        {'ok', AccountsConfig} ->
+            accounts_config_deprecate_timezone_for_default_timezone(AccountsConfig);
+        {'error', E} ->
+            lager:warning("unable to fetch system_config/accounts: ~p", [E])
+    end.
+
+-spec accounts_config_deprecate_timezone_for_default_timezone(kz_json:object()) -> 'ok'.
+accounts_config_deprecate_timezone_for_default_timezone(AccountsConfig) ->
+    PublicFields = kz_doc:public_fields(AccountsConfig),
+    case kz_json:get_keys(PublicFields) of
+        [] -> 'ok';
+        Keys ->
+            MigratedConfig = deprecate_timezone_for_default_timezone(Keys, AccountsConfig),
+            kz_datamgr:save_doc(?KZ_CONFIG_DB, MigratedConfig),
+            'ok'
+    end.
+
+-spec deprecate_timezone_for_default_timezone(kz_json:keys(), kz_json:object()) ->
+                                                     kz_json:object().
+deprecate_timezone_for_default_timezone(Nodes, AccountsConfig) ->
+    lists:foldl(fun deprecate_timezone_for_node/2, AccountsConfig, Nodes).
+
+-spec deprecate_timezone_for_node(kz_json:key(), kz_json:object()) ->
+                                         kz_json:object().
+deprecate_timezone_for_node(Node, AccountsConfig) ->
+    Timezone = kz_json:get_value([Node, <<"timezone">>], AccountsConfig),
+    DefaultTimezone = kz_json:get_value([Node, <<"default_timezone">>], AccountsConfig),
+    deprecate_timezone_for_node(Node, AccountsConfig, Timezone, DefaultTimezone).
+
+-spec deprecate_timezone_for_node(kz_json:key(), kz_json:object(), kz_term:api_ne_binary(), kz_term:api_ne_binary()) ->
+                                         kz_json:object().
+deprecate_timezone_for_node(_Node, AccountsConfig, 'undefined', _Default) ->
+    AccountsConfig;
+deprecate_timezone_for_node(Node, AccountsConfig, Timezone, 'undefined') ->
+    io:format("setting default timezone to ~s for node ~s~n", [Timezone, Node]),
+    kz_json:set_value([Node, <<"default_timezone">>]
+                     ,Timezone
+                     ,kz_json:delete_key([Node, <<"timezone">>], AccountsConfig)
+                     );
+deprecate_timezone_for_node(Node, AccountsConfig, _Timezone, _Default) ->
+    kz_json:delete_key([Node, <<"timezone">>], AccountsConfig).
 
 -spec validate_system_configs() -> [{kz_term:ne_binary(), kz_json:object()}].
 validate_system_configs() ->
@@ -1114,9 +1242,9 @@ flush_account_views() ->
 %% @doc Returns all views related to account database by first trying fetching
 %% from process dictionary otherwise reads from file system.
 %%
-%% NOTE: Views refresh functionality is handled from database now, view's
-%% definition in databse are updated during system startup and refresh is now
-%% reading view definition from database not from file system.
+%% @deprecated Views refresh functionality is now handled from database. View
+%% definitions in database are updated during system startup and refreshing views
+%% is now reading view definition from database not from file system.
 %% @see read_all_account_views/0
 %% @see flush_account_views/0
 %% @end
@@ -1132,7 +1260,9 @@ get_all_account_views() ->
     end.
 
 %%------------------------------------------------------------------------------
-%% @doc Returns all account database related views from file system.
+%% @doc Returns all account database related views from file system (only those
+%% files within {@link kazoo_apps} private couch views directory.
+%% @see read_all_account_views/0
 %% @end
 %%------------------------------------------------------------------------------
 -spec read_all_account_views() -> kz_datamgr:views_listing().
