5afcc49426f2605dd06895c9dba1064e1fe0428f
Hesaam Farhang <hesaam@2600hz.com>
2018-12-03 13:55:02 -0800

    SSSU-68: add ability to set a custom lable for BLF keys

--- core/kazoo_provisioner/src/provisioner_v5.hrl
+++ core/kazoo_provisioner/src/provisioner_v5.hrl
@@ -1,5 +1,6 @@
 -ifndef(PROVISIONER_V5_HRL).
 
+-include_lib("kazoo_stdlib/include/kz_types.hrl").
 -include_lib("kazoo_stdlib/include/kz_databases.hrl").
 
 -define(MOD_CONFIG_CAT, <<"provisioner">>).
