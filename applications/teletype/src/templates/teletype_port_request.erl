bf27e97df91b66d5af1c3c3626268c9e742e7445
Hesaam Farhang <hesaam@2600hz.com>
2019-01-02 15:40:46 -0800

    SSSU-73: remove attachments from un-related ports, set proper from address for
    admin port request report

--- applications/teletype/src/templates/teletype_port_request.erl
+++ applications/teletype/src/templates/teletype_port_request.erl
@@ -98,8 +98,7 @@ handle_port_request(DataJObj) ->
 
     Emails = teletype_util:find_addresses(DataJObj, TemplateMetaJObj, ?TEMPLATE_ID),
 
-    EmailAttachements = teletype_port_utils:get_attachments(DataJObj),
-    case teletype_util:send_email(Emails, Subject, RenderedTemplates, EmailAttachements) of
+    case teletype_util:send_email(Emails, Subject, RenderedTemplates) of
         'ok' -> teletype_util:notification_completed(?TEMPLATE_ID);
         {'error', Reason} -> teletype_util:notification_failed(?TEMPLATE_ID, Reason)
     end.
