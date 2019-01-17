b0cacc3bd9805bab4cb0ebc4f4ac2860d690f41d
James Aimonetti <james.aimonetti@gmail.com>
2019-01-17 19:33:50 +0000

    maintain patched erlang.el for now
    erlang.el pulled from melpa:erlang-20181011.1236

--- make/fmt.mk
+++ make/fmt.mk
@@ -4,8 +4,9 @@ FMT_SHA = 237604a566879bda46d55d9e74e3e66daf1b557a
 .PHONY: fmt fmt-all clean-fmt clean-$(FMT)
 
 $(FMT):
-	wget -qO - 'https://codeload.github.com/fenollp/erlang-formatter/tar.gz/$(FMT_SHA)' | tar -vxz -C $(ROOT)/make/
-	mv $(ROOT)/make/erlang-formatter-$(FMT_SHA) $(ROOT)/make/erlang-formatter
+	@wget -qO - 'https://codeload.github.com/fenollp/erlang-formatter/tar.gz/$(FMT_SHA)' | tar -vxz -C $(ROOT)/make/
+	@mv $(ROOT)/make/erlang-formatter-$(FMT_SHA) $(ROOT)/make/erlang-formatter
+	@cp $(ROOT)/make/emacs/*.el $(ROOT)/make/erlang-formatter/emacs
 
 fmt-all: $(FMT)
 	@$(FMT) $(shell find core applications scripts -name "*.erl" -or -name "*.hrl" -or -name "*.escript")
