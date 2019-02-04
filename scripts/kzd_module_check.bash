6d27ab3f9f76fb7ced1ac54a09b91fd9ac0fd32f
James Aimonetti <james.aimonetti@gmail.com>
2019-02-04 18:48:08 +0000

    check .src diffs against .erl diff
    basic check that if .src has changes but .erl doesn't, that's a
    paddlin'. No check is made to make sure the change in .src is also in
    .erl which would be next-level if needed.

--- scripts/kzd_module_check.bash
+++ scripts/kzd_module_check.bash
@@ -4,13 +4,12 @@ set -e
 
 pushd "$(dirname "$0")" >/dev/null
 
-ROOT="$(pwd -P)"/..
-cd $ROOT/core/kazoo_documents
+ROOT=$(readlink -f "$(pwd -P)"/..)
 
 errors=0
 missing=""
 
-for SRC in src/*.erl.src; do
+for SRC in $ROOT/core/kazoo_documents/src/*.erl.src; do
     ERL=$(dirname $SRC)/$(basename $SRC .src)
     if [ ! -f "$ERL" ]; then
         missing="$missing $ERL - creating it from $SRC"$'\n'
@@ -19,10 +18,31 @@ for SRC in src/*.erl.src; do
     fi
 done
 
-popd >/dev/null
-
 if [ $errors = 1 ]; then
     echo "The following accessor modules were auto-created:"
     echo "$missing"
+
+    popd >/dev/null
+
     exit $errors
 fi
+
+missing=""
+
+for SRC_DIFF in $(git --no-pager diff --name-only HEAD -- $ROOT/core/kazoo_documents/src/*.src); do
+    ERL=$(dirname $SRC_DIFF)/$(basename $SRC_DIFF .src)
+    ERL_DIFF=$(git --no-pager diff --name-only HEAD -- $ERL)
+    if [ -z $ERL_DIFF ]; then
+        missing="$missing $ERL:1: "$'\n'
+        errors=1
+    fi
+done
+
+if [ $errors = 1 ]; then
+    echo "The following .erl files are missing changes from their .src:"
+    echo $missing
+    popd >/dev/null
+    exit $errors
+fi
+
+popd >/dev/null
