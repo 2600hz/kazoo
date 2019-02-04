5c8bc33e165a0c7202b84da42bedb8061af01b91
James Aimonetti <james.aimonetti@gmail.com>
2019-02-04 19:23:29 +0000

    not so greedy on the replacement

--- scripts/check-loglines.bash
+++ scripts/check-loglines.bash
@@ -12,12 +12,12 @@ erls=""
 # grep for lager:[word]("[A-Z][a-z]...
 # ignores log lines with a first word in all caps like HELO or EHLO in fax_smtp
 
-for ERL in $(egrep -rl "lager:\w+\(\"[A-Z][a-z]" $ROOT/core $ROOT/applications); do
+for ERL in $(egrep -rl "lager:\w+\(\"[A-Z]{1}[a-z]" $ROOT/core $ROOT/applications); do
     # sed captures lager:[word](" as \1
     # captures A-Z as \2
     # captures the rest of the line as \3
-    # changes \2 to the lowercase version using \L
-    sed -E -i 's/(lager:[[:alpha:]]+\(")([A-Z])([a-z].+)/\1\L\2\3/g' $ERL
+    # changes \2 to the lowercase version using \l
+    sed -E -i 's/(lager:[[:alpha:]]+\(")([A-Z]{1})([a-z].+)/\1\l\2\3/g' $ERL
     errors=1
     erls="$erls$ERL:1: log lines starting with capital letters"$'\n'
 done
