6ec9501152d083ccee15e5c3aba5d0d909d5b90f
Shvein Anton <t0hashvein@gmail.com>
2019-02-16 09:11:36 +0500

    Added uuid_fileman command to ecallmgr_util:send_cmd/4

--- applications/ecallmgr/src/ecallmgr_util.erl
+++ applications/ecallmgr/src/ecallmgr_util.erl
@@ -114,6 +114,12 @@ send_cmd(_Node, _UUID, "kz_multiset", "^^") -> 'ok';
 send_cmd(Node, UUID, "playstop", _Args) ->
     lager:debug("execute on node ~s: uuid_break(~s all)", [Node, UUID]),
     freeswitch:api(Node, 'uuid_break', kz_term:to_list(<<UUID/binary, " all">>));
+send_cmd(Node, UUID, "playseek", Cmd) ->
+    Args = iolist_to_binary([UUID, " ", Cmd]),
+    lager:debug("execute on node ~s: uuid_fileman(~s)", [Node, Args]),
+    Resp = freeswitch:api(Node, 'uuid_fileman', kz_term:to_list(Args)),
+    lager:debug("uuid_fileman resulted in: ~p", [Resp]),
+    Resp;
 send_cmd(Node, UUID, "unbridge", _) ->
     lager:debug("execute on node ~s: uuid_park(~s)", [Node, UUID]),
     freeswitch:api(Node, 'uuid_park', kz_term:to_list(UUID));
