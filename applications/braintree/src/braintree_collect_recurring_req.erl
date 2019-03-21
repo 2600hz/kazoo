c42aaf506c70475ac631401ab8cb1a3dfdec72b3
icehess <icehess@users.noreply.github.com>
2019-03-21 14:21:21 -0700

    KAZOO-6064: [master] ask bookkeepers for early running recurring charges (#5585)
    * KAZOO-6064: ask bookkeepers for early running recurring charges
    
    * make apis and fix typo
    
    * ZOO-6064: is last/current month collected?
    
    * KAZOO-6064: better time reporting in log
    
    * KAZOO-6064: how they did not formatted?
    
    * KAZOO-6064: add due-timestamp to recurring req
    
    * fix date, specs and publishing message

---
+++ applications/braintree/src/braintree_collect_recurring_req.erl
@@ -0,0 +1,37 @@
+%%%-----------------------------------------------------------------------------
+%%% @copyright (C) 2013-2019, 2600Hz
+%%% @doc
+%%% @end
+%%%-----------------------------------------------------------------------------
+-module(braintree_collect_recurring_req).
+
+-export([handle_req/2]).
+
+-include("braintree.hrl").
+
+%%------------------------------------------------------------------------------
+%% @doc
+%% @end
+%%------------------------------------------------------------------------------
+-spec handle_req(kz_json:object(), kz_term:proplist()) -> 'ok'.
+handle_req(JObj, _Props) ->
+    'true' = kapi_bookkeepers:collect_recurring_req_v(JObj),
+    Response = kz_json:from_list(
+                 [{<<"Account-ID">>, kz_json:get_ne_binary_value(<<"Account-ID">>, JObj)}
+                 ,{<<"Bookkeeper-ID">>, kz_json:get_ne_binary_value(<<"Bookkeeper-ID">>, JObj)}
+                 ,{<<"Bookkeeper-Type">>, kz_json:get_ne_binary_value(<<"Bookkeeper-Type">>, JObj)}
+                 ,{<<"Message">>, <<"Braintree performs recurring charges automatically">>}
+                 ,{<<"Msg-ID">>, kz_json:get_value(<<"Msg-ID">>, JObj)}
+                 ,{<<"Status">>, kz_services_recurring:status_good()}
+                 ] ++ kz_api:default_headers(?APP_NAME, ?APP_VERSION)
+                ),
+    RespQ = kz_json:get_ne_binary_value(<<"Server-ID">>, JObj),
+
+    PublishFun = fun(P) -> kapi_bookkeepers:publish_collect_recurring_resp(RespQ, P) end,
+
+    case kz_json:get_ne_binary_value(<<"Bookkeeper-Type">>, JObj) =:= ?APP_NAME of
+        'false' ->
+            lager:debug("skipping collect recurring for another bookkeeper");
+        'true' ->
+            kz_amqp_worker:cast(Response, PublishFun)
+    end.
