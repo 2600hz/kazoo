eb6ea16a689b867b660768146e061aa7cfecfad7
James Aimonetti <james.aimonetti@gmail.com>
2019-05-03 03:30:40 +0000

    ci fixes

--- make/deps.mk
+++ make/deps.mk
@@ -110,7 +110,7 @@ dep_getopt = hex 1.0.1
 dep_gproc = hex 0.8.0
 # used by kazoo_events, webseq, konami, acdc, ecallmgr, callflow
 
-dep_hep = git https://github.com/2600hz/hep-erlang master
+dep_hep = git https://github.com/2600hz/hep-erlang a8f62dbc756fd3d4be1e2f0a6cec47a23f6cd18a
 # Homer encapsulation protocol
 # merged lazedo/hep changes
 
