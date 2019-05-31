cd3f576b07b0a653dadbe081eb9b4341a9b1a5c6
Kalyan Krause <kalyan@2600hz.com>
2019-05-31 23:01:08 +0000

    Small code formatting fix

--- core/kazoo_documents/src/kzd_cluster.erl
+++ core/kazoo_documents/src/kzd_cluster.erl
@@ -15,3 +15,11 @@
 -spec id() -> kz_term:ne_binary().
 id() ->
     kapps_config:get_ne_binary(?MOD_CONFIG_CLUSTER, <<"cluster_id">>, kz_binary:rand_hex(16), <<"default">>).
+
+-spec zones() -> kz_term:api_object().
+zones() ->
+    kapps_config:get_json(?MOD_CONFIG_CLUSTER, <<"zones">>, kz_json:new(), <<"default">>).
+
+-spec zones(kz_term:text() | kz_json:object()) -> {'ok', kz_json:object()}.
+zones(Value) ->
+    kapps_config:set_json(?MOD_CONFIG_CLUSTER, <<"zones">>, Value).
