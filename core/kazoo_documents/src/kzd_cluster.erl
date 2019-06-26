41d4255247a553c704d7ae6cde26e4a5ce66d911
yonkoma <kalyan@2600hz.com>
2019-06-26 14:07:02 -0700

    [4.3] Added accessors for cluster zones with priority (#5868)
    * Added cluster zone accesors

--- core/kazoo_documents/src/kzd_cluster.erl
+++ core/kazoo_documents/src/kzd_cluster.erl
@@ -8,10 +8,18 @@
 
 -define(MOD_CONFIG_CLUSTER, <<"cluster">>).
 
--export([id/0]).
+-export([id/0, zones/0, zones/1]).
 
 -include("kz_documents.hrl").
 
 -spec id() -> kz_term:ne_binary().
 id() ->
     kapps_config:get_ne_binary(?MOD_CONFIG_CLUSTER, <<"cluster_id">>, kz_binary:rand_hex(16), <<"default">>).
+
+-spec zones() -> kz_term:api_object().
+zones() ->
+    kapps_config:get_json(?MOD_CONFIG_CLUSTER, <<"zones">>, kz_json:new(), <<"default">>).
+
+-spec zones(kz_term:text() | kz_json:object()) -> {'ok', kz_json:object()}.
+zones(Value) ->
+    kapps_config:set_json(?MOD_CONFIG_CLUSTER, <<"zones">>, Value).
