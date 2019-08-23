c68a51a7a2d3e747d9d045a5e2c3f9e2441f350b
Kyle Diedrick <kyle.diedrick@pgi.com>
2019-08-23 16:46:45 -0700

    Make bics knm load the api url from the config document

--- core/kazoo_number_manager/src/knm_bics.hrl
+++ core/kazoo_number_manager/src/knm_bics.hrl
@@ -1,8 +1,10 @@
 -include("knm.hrl").
 
-%%-define(BICS_BASE_URL, "https://mynumbers.api.bics.com/sandbox/").
--define(BICS_BASE_URL, "http://localhost:3001").
 -define(KNM_BICS_CONFIG_CAT, <<(?KNM_CONFIG_CAT)/binary, ".bics">>).
+
+-define(BICS_BASE_URL
+       ,kapps_config:get_binary(?KNM_BICS_CONFIG_CAT, <<"api_url">>, <<>>)
+       ).
 
 -define(BICS_BEARER_TOKEN
        ,kapps_config:get_binary(?KNM_BICS_CONFIG_CAT, <<"api_token">>, <<>>)
