1d4b29958382f2becdb08085ba876ecd48e5c5ad
James Aimonetti <james.aimonetti@gmail.com>
2019-08-26 19:59:56 +0000

    HELP-10048: tighten up webhook usage and failing webhook disablement
    Webhook failures are tracked in a cache with an expiry set by the
    account (or system) config. Periodically the webhooks_disabler
    process will count the cache entries for a given webhook and, if
    greater than the configured threshold, will auto-disable the webhook
    to prevent needless work.
    
    However, it appears that the window between flushing old failures from
    the cache and the periodic checks can be large enough to incorporate
    failures that have techincally happened outside the current "window"
    for counting, thus causing webhooks to be disabled erroneously.
    
    This PR introduces checks on the timestamp of the cached failure
    against the calculated window instead of just counting the cached
    entry itself.

--- applications/webhooks/src/webhooks.hrl
+++ applications/webhooks/src/webhooks.hrl
@@ -4,8 +4,8 @@
 -include_lib("kazoo_stdlib/include/kz_databases.hrl").
 -include_lib("kazoo_events/include/kz_hooks.hrl").
 
--define(APP, webhooks).
--define(APP_NAME, (atom_to_binary(?APP, utf8))).
+-define(APP, 'webhooks').
+-define(APP_NAME, (atom_to_binary(?APP, 'utf8'))).
 -define(APP_VERSION, <<"4.0.0">>).
 
 -type http_verb() :: 'get' | 'post'.
@@ -31,6 +31,7 @@
 
 -define(ATTEMPT_EXPIRY_KEY, <<"attempt_failure_expiry_ms">>).
 -define(FAILURE_COUNT_KEY, <<"attempt_failure_count">>).
+-define(DEFAULT_FAILURE_COUNT, 6).
 
 -define(FAILURE_CACHE_KEY(AccountId, HookId, Timestamp)
        ,{'failure', AccountId, HookId, Timestamp}
