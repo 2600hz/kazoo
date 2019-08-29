8ab0d88777828b76e1d077d77a10b3d543230269
Harenson Henao <harenson@gmail.com>
2019-08-29 22:33:50 +0000

    Convert kapi_hangups mod to use kapi_definition mod

--- core/kazoo_amqp/src/api/kapi_hangups.erl
+++ core/kazoo_amqp/src/api/kapi_hangups.erl
@@ -10,79 +10,154 @@
 %%%-----------------------------------------------------------------------------
 -module(kapi_hangups).
 
--export([query_req/1, query_req_v/1
-        ,query_resp/1, query_resp_v/1
+-export([api_definitions/0, api_definition/1]).
 
-        ,bind_q/2, unbind_q/2
+-export([query_req/1
+        ,query_req_v/1
+        ,publish_query_req/1
+        ,publish_query_req/2
+        ]).
+
+-export([query_resp/1
+        ,query_resp_v/1
+        ,publish_query_resp/2
+        ,publish_query_resp/3
+        ]).
+
+-export([bind_q/2
+        ,unbind_q/2
         ,declare_exchanges/0
-
-        ,publish_query_req/1, publish_query_req/2
-        ,publish_query_resp/2, publish_query_resp/3
         ]).
 
 -include_lib("kz_amqp_util.hrl").
 
 -define(QUERY_REQ_ROUTING_KEY, <<"hangups.query">>).
 
--define(QUERY_REQ_HEADERS, [<<"Hangup-Cause">>]).
--define(OPTIONAL_QUERY_REQ_HEADERS, [<<"Account-ID">>, <<"Raw-Data">>]).
--define(QUERY_REQ_VALUES, [{<<"Event-Category">>, <<"hangups">>}
-                          ,{<<"Event-Name">>, <<"query_req">>}
-                          ]).
--define(QUERY_REQ_TYPES, [{<<"Raw-Data">>, fun kz_term:is_boolean/1}]).
+%%------------------------------------------------------------------------------
+%% @doc Get all API definitions of this module.
+%% @end
+%%------------------------------------------------------------------------------
+-spec api_definitions() -> kapi_definition:apis().
+api_definitions() ->
+    [query_req_definition()
+    ,query_resp_definition()
+    ].
 
--define(QUERY_RESP_HEADERS, []).
--define(OPTIONAL_QUERY_RESP_HEADERS, [<<"mean">>
-                                     ,<<"one_to_five">>, <<"five_to_fifteen">>, <<"one_to_fifteen">>
-                                     ,<<"start_time">>
-                                     ,<<"account_id">>
-                                     ,<<"hangup_cause">>
-                                     ,<<"one">>, <<"five">>, <<"fifteen">>, <<"day">>
-                                     ,<<"count">>
-                                     ,<<"meters">>
-                                     ]).
--define(QUERY_RESP_VALUES, [{<<"Event-Category">>, <<"hangups">>}
-                           ,{<<"Event-Name">>, <<"query_resp">>}
-                           ]).
--define(QUERY_RESP_TYPES, []).
+%%------------------------------------------------------------------------------
+%% @doc Get API definition of the given `Name'.
+%% @see api_definitions/0
+%% @end
+%%------------------------------------------------------------------------------
+-spec api_definition(kz_term:text()) -> kapi_definition:api().
+api_definition(Name) when not is_binary(Name) ->
+    api_definition(kz_term:to_binary(Name));
+api_definition(<<"query_req">>) ->
+    query_req_definition();
+api_definition(<<"query_resp">>) ->
+    query_resp_definition().
+
+-spec query_req_definition() -> kapi_definition:api().
+query_req_definition() ->
+    EventName = <<"query_req">>,
+    Category = <<"hangups">>,
+    Setters = [{fun kapi_definition:set_name/2, EventName}
+              ,{fun kapi_definition:set_friendly_name/2, <<"Hangups Query Request">>}
+              ,{fun kapi_definition:set_description/2, <<"Hangups Query Request">>}
+              ,{fun kapi_definition:set_category/2, Category}
+              ,{fun kapi_definition:set_build_fun/2, fun query_req/1}
+              ,{fun kapi_definition:set_validate_fun/2, fun query_req_v/1}
+              ,{fun kapi_definition:set_publish_fun/2, fun publish_query_req/1}
+              ,{fun kapi_definition:set_binding/2, ?QUERY_REQ_ROUTING_KEY}
+              ,{fun kapi_definition:set_required_headers/2, [<<"Hangup-Cause">>]}
+              ,{fun kapi_definition:set_optional_headers/2, [<<"Account-ID">>
+                                                            ,<<"Raw-Data">>
+                                                            ]}
+              ,{fun kapi_definition:set_values/2
+               ,kapi_definition:event_type_headers(Category, EventName)
+               }
+              ,{fun kapi_definition:set_types/2, [{<<"Raw-Data">>, fun kz_term:is_boolean/1}]}
+              ],
+    kapi_definition:setters(Setters).
+
+-spec query_resp_definition() -> kapi_definition:api().
+query_resp_definition() ->
+    EventName = <<"query_resp">>,
+    Category = <<"hangups">>,
+    Setters = [{fun kapi_definition:set_name/2, EventName}
+              ,{fun kapi_definition:set_friendly_name/2, <<"Hangups Query Response">>}
+              ,{fun kapi_definition:set_description/2, <<"Hangups Query Response">>}
+              ,{fun kapi_definition:set_category/2, Category}
+              ,{fun kapi_definition:set_build_fun/2, fun query_resp/1}
+              ,{fun kapi_definition:set_validate_fun/2, fun query_resp_v/1}
+              ,{fun kapi_definition:set_publish_fun/2, fun publish_query_resp/2}
+              ,{fun kapi_definition:set_required_headers/2, []}
+              ,{fun kapi_definition:set_optional_headers/2
+               ,[<<"mean">>
+                ,<<"one_to_five">>, <<"five_to_fifteen">>, <<"one_to_fifteen">>
+                ,<<"start_time">>
+                ,<<"account_id">>
+                ,<<"hangup_cause">>
+                ,<<"one">>, <<"five">>, <<"fifteen">>, <<"day">>
+                ,<<"count">>
+                ,<<"meters">>
+                ]
+               }
+              ,{fun kapi_definition:set_values/2
+               ,kapi_definition:event_type_headers(Category, EventName)
+               }
+              ,{fun kapi_definition:set_types/2, []}
+              ],
+    kapi_definition:setters(Setters).
 
 %%------------------------------------------------------------------------------
 %% @doc Takes proplist, creates JSON string or error.
 %% @end
 %%------------------------------------------------------------------------------
--spec query_req(kz_term:api_terms()) ->
-                       {'ok', iolist()} |
-                       {'error', string()}.
-query_req(Prop) when is_list(Prop) ->
-    case query_req_v(Prop) of
-        'true' -> kz_api:build_message(Prop, ?QUERY_REQ_HEADERS, ?OPTIONAL_QUERY_REQ_HEADERS);
-        'false' -> {'error', "Proplist failed validation for query_req"}
-    end;
-query_req(JObj) -> query_req(kz_json:to_proplist(JObj)).
+-spec query_req(kz_term:api_terms()) -> kz_api:api_formatter_return().
+query_req(Req) ->
+    kapi_definition:build_message(Req, query_req_definition()).
 
 -spec query_req_v(kz_term:api_terms()) -> boolean().
-query_req_v(Prop) when is_list(Prop) ->
-    kz_api:validate(Prop, ?QUERY_REQ_HEADERS, ?QUERY_REQ_VALUES, ?QUERY_REQ_TYPES);
-query_req_v(JObj) -> query_req_v(kz_json:to_proplist(JObj)).
+query_req_v(Req) ->
+    kapi_definition:validate(Req, query_req_definition()).
+
+-spec publish_query_req(kz_term:api_terms()) -> 'ok'.
+publish_query_req(JObj) ->
+    publish_query_req(JObj, ?DEFAULT_CONTENT_TYPE).
+
+-spec publish_query_req(kz_term:api_terms(), kz_term:ne_binary()) -> 'ok'.
+publish_query_req(API, ContentType) ->
+    Definition = query_req_definition(),
+    {'ok', Payload} = kz_api:prepare_api_payload(API
+                                                ,kapi_definition:values(Definition)
+                                                ,kapi_definition:build_fun(Definition)
+                                                ),
+    kz_amqp_util:kapps_publish(kapi_definition:binding(Definition), Payload, ContentType).
 
 %%------------------------------------------------------------------------------
 %% @doc Takes proplist, creates JSON string or error.
 %% @end
 %%------------------------------------------------------------------------------
--spec query_resp(kz_term:api_terms()) ->
-                        {'ok', iolist()} |
-                        {'error', string()}.
-query_resp(Prop) when is_list(Prop) ->
-    case query_resp_v(Prop) of
-        'true' -> kz_api:build_message(Prop, ?QUERY_RESP_HEADERS, ?OPTIONAL_QUERY_RESP_HEADERS);
-        'false' -> {'error', "Proplist failed validation for query_resp"}
-    end;
-query_resp(JObj) -> query_resp(kz_json:to_proplist(JObj)).
+-spec query_resp(kz_term:api_terms()) -> kz_api:api_formatter_return().
+query_resp(Req) ->
+    kapi_definition:build_message(Req, query_resp_definition()).
 
 -spec query_resp_v(kz_term:api_terms()) -> boolean().
-query_resp_v(Prop) when is_list(Prop) ->
-    kz_api:validate(Prop, ?QUERY_RESP_HEADERS, ?QUERY_RESP_VALUES, ?QUERY_RESP_TYPES);
-query_resp_v(JObj) -> query_resp_v(kz_json:to_proplist(JObj)).
+query_resp_v(Req) ->
+    kapi_definition:validate(Req, query_resp_definition()).
+
+-spec publish_query_resp(kz_term:ne_binary(), kz_term:api_terms()) -> 'ok'.
+publish_query_resp(RespQ, JObj) ->
+    publish_query_resp(RespQ, JObj, ?DEFAULT_CONTENT_TYPE).
+
+-spec publish_query_resp(kz_term:ne_binary(), kz_term:api_terms(), kz_term:ne_binary()) -> 'ok'.
+publish_query_resp(RespQ, API, ContentType) ->
+    Definition = query_resp_definition(),
+    {'ok', Payload} = kz_api:prepare_api_payload(API
+                                                ,kapi_definition:values(Definition)
+                                                ,kapi_definition:build_fun(Definition)
+                                                ),
+    kz_amqp_util:targeted_publish(RespQ, Payload, ContentType).
 
 -spec bind_q(kz_term:ne_binary(), kz_term:proplist()) -> 'ok'.
 bind_q(Queue, _Props) ->
@@ -95,21 +170,3 @@ unbind_q(Queue, _Props) ->
 -spec declare_exchanges() -> 'ok'.
 declare_exchanges() ->
     kz_amqp_util:kapps_exchange().
-
--spec publish_query_req(kz_term:api_terms()) -> 'ok'.
-publish_query_req(JObj) ->
-    publish_query_req(JObj, ?DEFAULT_CONTENT_TYPE).
-
--spec publish_query_req(kz_term:api_terms(), kz_term:ne_binary()) -> 'ok'.
-publish_query_req(API, ContentType) ->
-    {'ok', Payload} = kz_api:prepare_api_payload(API, ?QUERY_REQ_VALUES, fun query_req/1),
-    kz_amqp_util:kapps_publish(?QUERY_REQ_ROUTING_KEY, Payload, ContentType).
-
--spec publish_query_resp(kz_term:ne_binary(), kz_term:api_terms()) -> 'ok'.
-publish_query_resp(RespQ, JObj) ->
-    publish_query_resp(RespQ, JObj, ?DEFAULT_CONTENT_TYPE).
-
--spec publish_query_resp(kz_term:ne_binary(), kz_term:api_terms(), kz_term:ne_binary()) -> 'ok'.
-publish_query_resp(RespQ, API, ContentType) ->
-    {'ok', Payload} = kz_api:prepare_api_payload(API, ?QUERY_RESP_VALUES, fun query_resp/1),
-    kz_amqp_util:targeted_publish(RespQ, Payload, ContentType).
