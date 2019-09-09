9eb942bd253dc4cf759e8e0af5567625087661c3
Sergey Safarov <s.safarov@gmail.com>
2019-09-09 21:01:40 +0300

    Updated kzd_blacklists.erl to match reference

--- core/kazoo_documents/src/kzd_blacklists.erl
+++ core/kazoo_documents/src/kzd_blacklists.erl
@@ -10,9 +10,15 @@
 -module(kzd_blacklists).
 
 -export([new/0]).
+-export([action/1, action/2, set_action/2]).
+-export([enabled/1, enabled/2, set_enabled/2]).
 -export([flags/1, flags/2, set_flags/2]).
 -export([name/1, name/2, set_name/2]).
 -export([numbers/1, numbers/2, set_numbers/2]).
+-export([numbers_name/1, numbers_name/2, set_numbers_name/2]).
+-export([owner_id/1, owner_id/2, set_owner_id/2]).
+-export([patterns/1, patterns/2, set_patterns/2]).
+-export([patterns_name/1, patterns_name/2, set_patterns_name/2]).
 -export([should_block_anonymous/1, should_block_anonymous/2, set_should_block_anonymous/2]).
 
 
@@ -26,6 +32,30 @@
 -spec new() -> doc().
 new() ->
     kz_json_schema:default_object(?SCHEMA).
+
+-spec action(doc()) -> binary().
+action(Doc) ->
+    action(Doc, <<"block">>).
+
+-spec action(doc(), Default) -> binary() | Default.
+action(Doc, Default) ->
+    kz_json:get_binary_value([<<"action">>], Doc, Default).
+
+-spec set_action(doc(), binary()) -> doc().
+set_action(Doc, Action) ->
+    kz_json:set_value([<<"action">>], Action, Doc).
+
+-spec enabled(doc()) -> boolean().
+enabled(Doc) ->
+    enabled(Doc, true).
+
+-spec enabled(doc(), Default) -> boolean() | Default.
+enabled(Doc, Default) ->
+    kz_json:get_boolean_value([<<"enabled">>], Doc, Default).
+
+-spec set_enabled(doc(), boolean()) -> doc().
+set_enabled(Doc, Enabled) ->
+    kz_json:set_value([<<"enabled">>], Enabled, Doc).
 
 -spec flags(doc()) -> kz_term:api_ne_binaries().
 flags(Doc) ->
@@ -62,6 +92,54 @@ numbers(Doc, Default) ->
 -spec set_numbers(doc(), kz_json:object()) -> doc().
 set_numbers(Doc, Numbers) ->
     kz_json:set_value([<<"numbers">>], Numbers, Doc).
+
+-spec numbers_name(doc()) -> kz_term:api_binary().
+numbers_name(Doc) ->
+    numbers_name(Doc, 'undefined').
+
+-spec numbers_name(doc(), Default) -> binary() | Default.
+numbers_name(Doc, Default) ->
+    kz_json:get_binary_value([<<"numbers">>, <<"name">>], Doc, Default).
+
+-spec set_numbers_name(doc(), binary()) -> doc().
+set_numbers_name(Doc, NumbersName) ->
+    kz_json:set_value([<<"numbers">>, <<"name">>], NumbersName, Doc).
+
+-spec owner_id(doc()) -> kz_term:api_ne_binary().
+owner_id(Doc) ->
+    owner_id(Doc, 'undefined').
+
+-spec owner_id(doc(), Default) -> kz_term:ne_binary() | Default.
+owner_id(Doc, Default) ->
+    kz_json:get_ne_binary_value([<<"owner_id">>], Doc, Default).
+
+-spec set_owner_id(doc(), kz_term:ne_binary()) -> doc().
+set_owner_id(Doc, OwnerId) ->
+    kz_json:set_value([<<"owner_id">>], OwnerId, Doc).
+
+-spec patterns(doc()) -> kz_json:object().
+patterns(Doc) ->
+    patterns(Doc, kz_json:new()).
+
+-spec patterns(doc(), Default) -> kz_json:object() | Default.
+patterns(Doc, Default) ->
+    kz_json:get_json_value([<<"patterns">>], Doc, Default).
+
+-spec set_patterns(doc(), kz_json:object()) -> doc().
+set_patterns(Doc, Patterns) ->
+    kz_json:set_value([<<"patterns">>], Patterns, Doc).
+
+-spec patterns_name(doc()) -> kz_term:api_binary().
+patterns_name(Doc) ->
+    patterns_name(Doc, 'undefined').
+
+-spec patterns_name(doc(), Default) -> binary() | Default.
+patterns_name(Doc, Default) ->
+    kz_json:get_binary_value([<<"patterns">>, <<"name">>], Doc, Default).
+
+-spec set_patterns_name(doc(), binary()) -> doc().
+set_patterns_name(Doc, PatternsName) ->
+    kz_json:set_value([<<"patterns">>, <<"name">>], PatternsName, Doc).
 
 -spec should_block_anonymous(doc()) -> kz_term:api_boolean().
 should_block_anonymous(Doc) ->
