940c2b341007fc68b9b6057a89bbfd0a6009966e
Harenson Henao <harenson@gmail.com>
2019-09-14 04:56:25 -0500

    HELP-10194: Add timestamp key to pushed objects via pusher (#6021)
    * HELP-10194: Add timestamp key to pushed objects via pusher

--- applications/pusher/src/pusher_listener.erl
+++ applications/pusher/src/pusher_listener.erl
@@ -68,8 +68,10 @@ handle_push(JObj, _Props) ->
     TokenType = kz_json:get_value(<<"Token-Type">>, JObj),
     Module = kz_term:to_atom(<<"pm_",TokenType/binary>> , 'true'),
     lager:debug("pushing for token ~s(~s) to module ~s", [Token, TokenType, Module]),
-    TSBin = kz_term:to_binary(kz_time:current_tstamp()),
-    JObj1 = kz_json:insert_value([<<"Payload">>, <<"timestamp">>], TSBin, JObj),
+    JObj1 = kz_json:insert_value([<<"Payload">>, <<"utc_unix_timestamp_ms">>]
+                                ,kz_term:to_binary(os:system_time(millisecond))
+                                ,JObj
+                                ),
     gen_server:cast(Module, {'push', JObj1}).
 
 -spec handle_reg_success(kz_json:object(), kz_term:proplist()) -> 'ok'.
