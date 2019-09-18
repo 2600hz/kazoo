465827e1712a7728c693f0e5be57a58a7990d24b
Hesaam Farhang <hesaam@2600hz.com>
2019-09-17 18:43:38 -0700

    move triggrable macros to core
    If other apps want to start their own tasks module and binf to triggerable
    they need access to these macros

--- applications/tasks/src/tasks.hrl
+++ applications/tasks/src/tasks.hrl
@@ -17,19 +17,5 @@
 
 -define(OUTPUT_CSV_HEADER_ERROR, <<"error">>).
 
--define(TRIGGER_ACCOUNT, <<"tasks.triggers.account">>).
--define(TRIGGER_ACCOUNT_MOD, <<"tasks.triggers.account_mod">>).
--define(TRIGGER_DAILY, <<"tasks.triggers.day">>).
--define(TRIGGER_HOURLY, <<"tasks.triggers.hour">>).
--define(TRIGGER_MINUTELY, <<"tasks.triggers.minute">>).
--define(TRIGGER_OTHER, <<"tasks.triggers.other">>).
--define(TRIGGER_SYSTEM, <<"tasks.triggers.system">>).
-
--define(TRIGGER_ALL_DBS, [?TRIGGER_ACCOUNT
-                         ,?TRIGGER_ACCOUNT_MOD
-                         ,?TRIGGER_SYSTEM
-                         ,?TRIGGER_OTHER
-                         ]).
-
 -define(KZ_TASKS_HRL, 'true').
 -endif.
