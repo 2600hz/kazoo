f50264b21ddb7e7427ab39bbae480992781162d7
Sergey Safarov <s.safarov@gmail.com>
2020-01-08 20:48:31 +0300

    kazoo_speech: added maintenance asr api_key functions

--- core/kazoo_speech/src/kazoo_speech_maintenance.erl
+++ core/kazoo_speech/src/kazoo_speech_maintenance.erl
@@ -9,7 +9,9 @@
 %%%-----------------------------------------------------------------------------
 -module(kazoo_speech_maintenance).
 
--export([set_tts_api_key/2
+-export([set_asr_api_key/2
+        ,set_asr_provider/1
+        ,set_tts_api_key/2
         ,set_tts_provider/1
         ,set_tts_language/1
         ]).
@@ -33,3 +35,16 @@ set_tts_provider(Provider) ->
 set_tts_language(Language) ->
     kazoo_tts:set_default_language(Language),
     io:format("updated default language to ~s~n", [Language]).
+
+-spec set_asr_api_key(kz_term:ne_binary(), kz_term:ne_binary()) -> 'ok'.
+set_asr_api_key(Provider, APIKey) ->
+    (kazoo_asr:provider_module(Provider)):set_api_key(APIKey).
+
+-spec set_asr_provider(kz_term:ne_binary()) -> 'ok'.
+set_asr_provider(Provider) ->
+    case code:which(kazoo_asr:provider_module(Provider)) of
+        'non_existing' -> io:format("No provider module for ~s~n", [Provider]);
+        _Path ->
+            kazoo_asr:set_default_provider(Provider),
+            io:format("Updated ASR provider to ~s~n", [Provider])
+    end.
