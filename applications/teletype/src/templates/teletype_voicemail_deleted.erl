2d119a215edf3c54a9c6ddab72a22fda132e0d77
Shvein Anton <t0hashvein@gmail.com>
2020-01-24 21:31:47 +0500

    Fixed code_checks copyright

--- applications/teletype/src/templates/teletype_voicemail_deleted.erl
+++ applications/teletype/src/templates/teletype_voicemail_deleted.erl
@@ -1,7 +1,11 @@
 %%%-----------------------------------------------------------------------------
-%%% @copyright (C) 2014-2019, 2600Hz
+%%% @copyright (C) 2014-2020, 2600Hz
 %%% @doc
 %%% @author James Aimonetti
+%%% This Source Code Form is subject to the terms of the Mozilla Public
+%%% License, v. 2.0. If a copy of the MPL was not distributed with this
+%%% file, You can obtain one at https://mozilla.org/MPL/2.0/.
+%%%
 %%% @end
 %%%-----------------------------------------------------------------------------
 -module(teletype_voicemail_deleted).
