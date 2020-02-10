4ebf4604e0bbcb8a793807b280c0ee3c9605b1d4
Karl Anderson <karl@2600hz.com>
2020-02-10 23:36:15 +0000

    KZOO-37: match changes to 4.3 for CI issues

--- core/kazoo_apps/src/kapps_maintenance.erl
+++ core/kazoo_apps/src/kapps_maintenance.erl
@@ -620,12 +620,14 @@ ensure_aggregates([Account|Accounts], Total) ->
 -spec ensure_aggregate(kz_term:ne_binary()) -> 'ok'.
 ensure_aggregate(Account) ->
     ensure_aggregate(Account
-                    ,kz_datamgr:open_doc(Account, kzs_util:format_account_id(Account))
+                    ,kz_datamgr:open_doc(kzs_util:format_account_db(Account)
+                                        ,kzs_util:format_account_id(Account)
+                                        )
                     ).
 
 -spec ensure_aggregate(kz_term:ne_binary(), kz_datamgr:get_results_return()) -> 'ok'.
 ensure_aggregate(Account, {'error', 'not_found'}) ->
-    io:format("skipping account db in bad state '~s'~n", [Account]);
+    io:format("necessary documents from account '~s' are missing, skipping migration~n", [Account]);
 ensure_aggregate(Account, _) ->
     io:format("ensuring necessary documents from account '~s' are in aggregate dbs~n"
              ,[Account]
