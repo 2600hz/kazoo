cf28f26bf5fa50223d9a2b64ed3c2ecb2a2b2e38
mark2600 <mark@2600hz.com>
2020-02-18 11:15:23 -0500

    add qubicle webhooks modules

---
+++ applications/webhooks/src/modules/webhooks_qubicle.erl
@@ -0,0 +1,38 @@
+%%%-----------------------------------------------------------------------------
+%%% @copyright (C) 2010-2019, 2600Hz
+%%%
+%%% @end
+%%%-----------------------------------------------------------------------------
+-module(webhooks_qubicle).
+
+-export([init/0
+        ,bindings_and_responders/0
+        ]).
+
+-include("webhooks.hrl").
+
+-define(ID, kz_term:to_binary(?MODULE)).
+-define(NAME, <<"Qubicle">>).
+-define(DESC, <<"This webhook is triggered when Qubicle does magic">>).
+-define(METADATA
+       ,kz_json:from_list([{<<"_id">>, ?ID}
+                          ,{<<"name">>, ?NAME}
+                          ,{<<"description">>, ?DESC}
+                          ])
+       ).
+
+-spec init() -> 'ok'.
+init() ->
+    webhooks_util:init_metadata(?ID, ?METADATA).
+
+-spec bindings_and_responders() -> {gen_listener:bindings(), gen_listener:responders()}.
+bindings_and_responders() ->
+    {[{'qubicle', [
+               ]
+      }
+     ]
+    ,[{{'webhooks_channel_util', 'handle_event'}
+      ,[{<<"call_event">>, <<"CHANNEL_CREATE">>}]
+      }
+     ]
+    }.
