4d3037ba3894ef89774ac7b192d564556302d11b
mark2600 <mark@2600hz.com>
2020-02-21 12:20:58 -0500

    add even more

---
+++ applications/webhooks/src/modules/webhooks_mark_testfour.erl
@@ -0,0 +1,38 @@
+%%%-----------------------------------------------------------------------------
+%%% @copyright (C) 2010-2020, 2600Hz
+%%%
+%%% @end
+%%%-----------------------------------------------------------------------------
+-module(webhooks_mark_testfour).
+
+-export([init/0
+        ,bindings_and_responders/0
+        ]).
+
+-include("webhooks.hrl").
+
+-define(ID, kz_term:to_binary(?MODULE)).
+-define(NAME, <<"testfour">>).
+-define(DESC, <<"This weannel establishes two-way audio, such as a voicemail box or the called party answering">>).
+-define(METADATA
+       ,kz_json:from_list([{<<"_id">>, ?ID}
+                          ,{<<"name">>, ?NAME}
+                          ,{<<"description">>, ?DESC}
+                          ])
+       ).
+
+-spec init() -> 'ok'.
+init() ->
+    webhooks_util:init_metadata(?ID, ?METADATA).
+
+-spec bindings_and_responders() -> {gen_listener:bindings(), gen_listener:responders()}.
+bindings_and_responders() ->
+    {[{'call', [{'restrict_to', ['CHANNEL_ANSWER']}
+               ]
+      }
+     ]
+    ,[{{'webhooks_channel_util', 'handle_event'}
+      ,[{<<"call_event">>, <<"CHANNEL_ANSWER">>}]
+      }
+     ]
+    }.
