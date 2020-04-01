fde0bc38ae7485c8b107a1864686213e4e0c1a23
John White <john2600hz.com>
2020-04-01 09:18:50 -0500

    KZOO-51: Create an Emergency Call Webhook module
    - Adds new webhook module for emergency call amqp message

---
+++ applications/webhooks/src/modules/webhooks_emergency_bridge.erl
@@ -0,0 +1,86 @@
+%%%-----------------------------------------------------------------------------
+%%% @copyright (C) 2010-2020, 2600Hz
+%%%
+%%%
+%%% This Source Code Form is subject to the terms of the Mozilla Public
+%%% License, v. 2.0. If a copy of the MPL was not distributed with this
+%%% file, You can obtain one at https://mozilla.org/MPL/2.0/.
+%%%
+%%% @end
+%%%-----------------------------------------------------------------------------
+-module(webhooks_emergency_bridge).
+
+-export([init/0
+        ,bindings_and_responders/0
+        ,account_bindings/1
+        ,handle_emergency_bridge/2
+        ]).
+
+-include("webhooks.hrl").
+
+-define(ID, kz_term:to_binary(?MODULE)).
+-define(HOOK_NAME, <<"emergency_bridge">>).
+-define(NAME, <<"Emergency Bridge">>).
+-define(DESC, <<"Receive notifications when an emergency call is placed.">>).
+
+-define(METADATA
+       ,kz_json:from_list(
+          [{<<"_id">>, ?ID}
+          ,{<<"name">>, ?NAME}
+          ,{<<"description">>, ?DESC}
+          ]
+         )
+       ).
+
+%%------------------------------------------------------------------------------
+%% @doc
+%% @end
+%%------------------------------------------------------------------------------
+-spec init() -> 'ok'.
+init() ->
+    webhooks_util:init_metadata(?ID, ?METADATA).
+
+%%------------------------------------------------------------------------------
+%% @doc
+%% @end
+%%------------------------------------------------------------------------------
+-spec bindings_and_responders() -> {gen_listener:bindings(), gen_listener:responders()}.
+bindings_and_responders() ->
+    Bindings = bindings(),
+    Responders = [{{?MODULE, 'handle_emergency_bridge'}, [{<<"notification">>, <<"emergency_bridge">>}]}],
+    {Bindings, Responders}.
+
+%%------------------------------------------------------------------------------
+%% @doc
+%% @end
+%%------------------------------------------------------------------------------
+-spec account_bindings(kz_term:ne_binary()) -> gen_listener:bindings().
+account_bindings(_AccountId) -> [].
+
+%%------------------------------------------------------------------------------
+%% @doc
+%% @end
+%%------------------------------------------------------------------------------
+-spec handle_emergency_bridge(kz_json:object(), kz_term:proplist()) -> 'ok'.
+handle_emergency_bridge(Payload, _Props) ->
+    kz_log:put_callid(Payload),
+    AccountId = kz_json:get_ne_value(<<"Account-ID">>, Payload),
+    maybe_send_event(AccountId, Payload).
+
+%%%=============================================================================
+%%% Internal functions
+%%%=============================================================================
+
+%%------------------------------------------------------------------------------
+%% @doc
+%% @end
+%%------------------------------------------------------------------------------
+-spec bindings() -> gen_listener:bindings().
+bindings() ->
+    [{'notifications', [{'restrict_to', ['emergency_bridge']}]}].
+
+maybe_send_event(AccountId, JObj) ->
+  case webhooks_util:find_webhooks(?HOOK_NAME, AccountId) of
+    [] -> lager:debug("no hooks to handle for ~s", [AccountId]);
+    Hooks -> webhooks_util:fire_hooks(JObj, Hooks)
+  end.
