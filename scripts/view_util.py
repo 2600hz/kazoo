2d028ea1f50a55807cbe9aded42446f29cda16b4
Hesaam Farhang <hesaam@2600hz.com>
2020-05-02 04:53:19 -0700

    replace first occurrence

--- scripts/view_util.py
+++ scripts/view_util.py
@@ -165,7 +165,7 @@ def couchjs(js):
     ## we split and join new lines here to flatten js code into a single line
     ## so we can test that it would evaluated to a valid js code
     ## and it doesn't missed a semicolon `;`
-    JS = ''.join(js).replace('function', 'function arent_you_funny_couch') + '\n'
+    JS = ''.join(js).replace('function', 'function arent_you_funny_couch', 1) + '\n'
     if 'Object.keys' in JS:
         print('"Object.keys" which is not available until ECMA2015\n{}'.format(JS))
         exit(1)
