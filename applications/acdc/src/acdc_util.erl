d42b2088c21c89b06ed6531be9ec7ac20394c29c
Daniel Finke <danielfinke2011@gmail.com>
2020-06-02 18:34:41 +0000

    PISTON-772: do not start agents in acdc_queue_manager

--- applications/acdc/src/acdc_util.erl
+++ applications/acdc/src/acdc_util.erl
@@ -81,10 +81,14 @@ send_cdr(Url, JObj, Retries) ->
 %% Returns the list of agents configured for the queue
 -spec agents_in_queue(kz_term:ne_binary(), kz_term:ne_binary()) -> kz_json:path().
 agents_in_queue(AcctDb, QueueId) ->
-    case kz_datamgr:get_results(AcctDb, <<"queues/agents_listing">>, [{'key', QueueId}]) of
-        {'ok', []} -> [];
+    case kz_datamgr:get_results(AcctDb, <<"queues/agents_listing">>
+                               ,[{'startkey', [QueueId]}
+                                ,{'endkey', [QueueId, kz_json:new()]}
+                                ,{'reduce', 'false'}
+                                ])
+    of
         {'error', _E} -> lager:debug("failed to lookup agents for ~s: ~p", [QueueId, _E]), [];
-        {'ok', As} -> [kz_json:get_value(<<"value">>, A) || A <- As]
+        {'ok', As} -> As
     end.
 
 -spec agent_devices(kz_term:ne_binary(), kz_term:ne_binary()) -> kz_json:objects().
