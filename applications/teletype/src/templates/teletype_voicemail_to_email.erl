22e1d3d7f103a6177953087bc02b8e1cfccdd559
Harenson Henao <harenson@gmail.com>
2020-06-25 17:37:06 +0000

    HELP-15378: Add voicemail.vmbox_name macro to voicemail to email template

--- applications/teletype/src/templates/teletype_voicemail_to_email.erl
+++ applications/teletype/src/templates/teletype_voicemail_to_email.erl
@@ -27,6 +27,7 @@ id() ->
 macros() ->
     kz_json:from_list(
       [?MACRO_VALUE(<<"voicemail.vmbox_id">>, <<"voicemail_vmbox_id">>, <<"Voicemail Box Id">>, <<"Which voicemail box was the message left in">>)
+      ,?MACRO_VALUE(<<"voicemail.vmbox_name">>, <<"voicemail_vmbox_name">>, <<"Voicemail Box Name">>, <<"Name of the voicemail box were the message was left in">>)
       ,?MACRO_VALUE(<<"voicemail.msg_id">>, <<"voicemail_msg_id">>, <<"Voicemail Message ID">>, <<"Message Id of the voicemail">>)
       ,?MACRO_VALUE(<<"voicemail.transcription">>, <<"voicemail_transcription">>, <<"Voicemail Message Transcription">>, <<"Voicemail Message Transcription">>)
       ,?MACRO_VALUE(<<"voicemail.length">>, <<"voicemail_length">>, <<"Voicemail Length">>, <<"Length of the voicemail file (formatted in HH:MM:SS)">>)
