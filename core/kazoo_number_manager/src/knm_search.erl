8aa30d7dc4e5c9b93603c71b18df86aed10a35b4
Ben Bradford <bsc.benbradford@gmail.com>
2020-07-18 14:44:27 -0700

    PISTON-1108:Better use of props lib

--- core/kazoo_number_manager/src/knm_search.erl
+++ core/kazoo_number_manager/src/knm_search.erl
@@ -235,11 +235,11 @@ first(Options) ->
     QID = query_id(Options),
     gen_listener:cast(?MODULE, {'reset_search', QID}),
     Self = self(),
-    Setters = [{'quantity', ?MAX_SEARCH}
-              ,{'offset', 0}
-              ,{'normalized_prefix', normalized_prefix(Options)}
-              ],
-    Opts = lists:foldl(fun props:insert_value/2, Options, Setters),
+    Defaults = [{'quantity', ?MAX_SEARCH}
+               ,{'offset', 0}
+               ,{'normalized_prefix', normalized_prefix(Options)}
+               ],
+    Opts = props:insert_values(Defaults, Options),
     lists:foreach(fun(Carrier) -> search_spawn(Self, Carrier, Opts) end, Carriers),
     wait_for_search(length(Carriers)),
     gen_listener:call(?MODULE, {'first', Options}).
