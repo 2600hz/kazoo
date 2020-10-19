1ed1e28e4c5204028655fb089af57be7eab66a60
Sergey Safarov <s.safarov@gmail.com>
2020-10-19 21:19:48 +0300

    fixed current master build
    More info at https://github.com/2600hz/kazoo/pull/6652

--- applications/pusher/deps.mk
+++ applications/pusher/deps.mk
@@ -1,6 +1,6 @@
 DEPS = apns fcm lager
 
-dep_apns = git https://github.com/2600hz/erlang-apns4erl.git aba1fa96a4abbbb2c1628ad5d604f482aad4d12f # latest commit SHA to 2600hz branch
+dep_apns = git https://github.com/2600hz/erlang-apns4erl.git 20668a1f5fde5a7afd195d03a5cd30825786f2c5 # latest commit SHA to 2600hz branch
 
 dep_fcm = git https://github.com/2600hz/erlang-fcm.git b2f68a4c6f0f59475597a35e2dc9be13d9ba2910
 # Firebase cloud messaging
