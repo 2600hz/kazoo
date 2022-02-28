f5d5072cc3b15160ebab4d57ca858e801bf57cd2
supapo <menashesite@gmail.com>
2022-02-28 14:34:49 +0200

    Revert docs

--- applications/ecallmgr/src/ecallmgr_originate.erl
+++ applications/ecallmgr/src/ecallmgr_originate.erl
@@ -625,12 +625,12 @@ get_unset_vars(JObj) ->
              ],
     ExportCAVProps = [{K, <<>>} || K <- kz_json:get_value(<<"Export-Custom-Application-Vars">>, JObj, [])],
     FSCAVVars = lists:foldr(fun ecallmgr_fs_xml:kazoo_var_to_fs_var/2
-                        ,[]
-                        ,[{<<"Custom-Application-Vars">>, kz_json:from_list(ExportCAVProps)}]
-                        ),
+                           ,[]
+                           ,[{<<"Custom-Application-Vars">>, kz_json:from_list(ExportCAVProps)}]
+                           ),
     ExportCAV = [K || KV <- FSCAVVars,
-                   ([K, _] = string:tokens(binary_to_list(KV), "=")) =/= 'undefined'
-             ],
+                      ([K, _] = string:tokens(binary_to_list(KV), "=")) =/= 'undefined'
+                ],
     case ["unset:" ++ K
           || KV <- lists:foldr(fun ecallmgr_fs_xml:kazoo_var_to_fs_var/2, [], kz_json:to_proplist(JObj)),
              not lists:member(begin [K, _] = string:tokens(binary_to_list(KV), "="), K end, Export),
